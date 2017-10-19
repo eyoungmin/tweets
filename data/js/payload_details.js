30b2dbd6c1cae1689f0e25342c60d811b2c19418
Youngmin Lee <iameyoungmin@gmail.com>
2017-10-20 00:16:53 +0900

    Added 51 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30823,
-	"created_at": "Wed Oct 18 15:17:00 +0000 2017",
+	"tweets": 30874,
+	"created_at": "Thu Oct 19 15:16:53 +0000 2017",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_10.js",
 		"year": 2017,
 		"var_name": "tweets_2017_10",
-		"tweet_count": 594,
+		"tweet_count": 645,
 		"month": 10
 	},
 	{
