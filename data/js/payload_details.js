858f36c66545d3d3005fe1f0054e3bac18a2c885
Youngmin Lee <iameyoungmin@gmail.com>
2018-06-22 00:10:57 +0900

    Added 42 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 40117,
-	"created_at": "Wed Jun 20 15:10:56 +0000 2018",
+	"tweets": 40159,
+	"created_at": "Thu Jun 21 15:10:56 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_06.js",
 		"year": 2018,
 		"var_name": "tweets_2018_06",
-		"tweet_count": 967,
+		"tweet_count": 1009,
 		"month": 6
 	},
 	{
