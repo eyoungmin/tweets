180c22bd6b92ad7e605bc4e250c5024bde837f4c
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-06 00:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51004,
-	"created_at": "Thu Dec 05 14:02:04 +0000 2019",
+	"tweets": 51005,
+	"created_at": "Thu Dec 05 15:02:04 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 05 14:41:32 +0000 2019",
+		"id": 1202352076468080600,
+		"id_str": "1202598886121979906",
+		"full_text": "191128 Gravity\n\n➖\nhttps://t.co/rfEGitmnD0\n\n#태연 #TAEYEON https://t.co/dUCteYAPJL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						43,
+						46
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						47,
+						55
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rfEGitmnD0",
+					"expanded_url": "https://youtu.be/tjcB74nYrM8",
+					"display_url": "youtu.be/tjcB74nYrM8",
+					"indices": [
+						18,
+						41
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1202351970641559600,
+					"id_str": "1202351970641559552",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1202351970641559552/pu/img/VBw3_XS_L-mG6zys.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1202351970641559552/pu/img/VBw3_XS_L-mG6zys.jpg",
+					"url": "https://t.co/dUCteYAPJL",
+					"display_url": "pic.twitter.com/dUCteYAPJL",
+					"expanded_url": "https://twitter.com/alluring_voice/status/1202352076468080640/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1202351970641559600,
+					"id_str": "1202351970641559552",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1202351970641559552/pu/img/VBw3_XS_L-mG6zys.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1202351970641559552/pu/img/VBw3_XS_L-mG6zys.jpg",
+					"url": "https://t.co/dUCteYAPJL",
+					"display_url": "pic.twitter.com/dUCteYAPJL",
+					"expanded_url": "https://twitter.com/alluring_voice/status/1202352076468080640/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							427,
+							240
+						],
+						"duration_millis": 34209,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202351970641559552/pu/vid/480x270/DJzv2uSa0UzU-3dX.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1202351970641559552/pu/pl/-pXGR3VRWtSjwAKC.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202351970641559552/pu/vid/640x360/9bo2C7dR1WRVS6HA.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202351970641559552/pu/vid/854x480/dsydfN3vbHxtPf1e.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3247398667,
+			"id_str": "3247398667",
+			"name": "Alluring voice",
+			"screen_name": "alluring_voice",
+			"location": "𝗣𝘂𝗿𝗽𝗼𝘀𝗲",
+			"description": "",
+			"url": "https://t.co/0Hbtw9STT5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0Hbtw9STT5",
+							"expanded_url": "http://taeyeon.smtown.com",
+							"display_url": "taeyeon.smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33464,
+			"friends_count": 5,
+			"listed_count": 1050,
+			"created_at": "Wed Jun 17 02:13:44 +0000 2015",
+			"favourites_count": 224,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1745,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1185199521157898240/ch7I4imw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1185199521157898240/ch7I4imw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3247398667/1570721950",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1792,
+		"favorite_count": 2432,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "191128 Gravity\n\n➖\nhttps://t.co/rfEGitmnD0\n\n#태연 #TAEYEON https://t.co/dUCteYAPJL"
+	},
+	{
 		"created_at": "Thu Dec 05 13:21:34 +0000 2019",
 		"id": 1202564020512772000,
 		"id_str": "1202578762069446656",
