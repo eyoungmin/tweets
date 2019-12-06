0627a1388417f39d0b1fd2c2095050f06681c1f6
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-06 21:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 06 11:54:53 +0000 2019",
+		"id": 1202912604143833000,
+		"id_str": "1202919334357671936",
+		"full_text": "'0310' Teaser 2.\n⠀\nYerin Baek 1st Full-Length Album\n[𝐄𝐯𝐞𝐫𝐲 𝐥𝐞𝐭𝐭𝐞𝐫 𝐈 𝐬𝐞𝐧𝐭 𝐲𝐨𝐮.]\n⠀\n2019.12.10. TUE. 6PM (KST) Release\n\n#백예린 #yerinbaek #0310 https://t.co/HXPFl07ejH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "백예린",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "yerinbaek",
+					"indices": [
+						122,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1202912445578150000,
+					"id_str": "1202912445578149888",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1202912445578149888/pu/img/S85D5Ef0usO1FhaJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1202912445578149888/pu/img/S85D5Ef0usO1FhaJ.jpg",
+					"url": "https://t.co/HXPFl07ejH",
+					"display_url": "pic.twitter.com/HXPFl07ejH",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1202912604143833088/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1202912445578150000,
+					"id_str": "1202912445578149888",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1202912445578149888/pu/img/S85D5Ef0usO1FhaJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1202912445578149888/pu/img/S85D5Ef0usO1FhaJ.jpg",
+					"url": "https://t.co/HXPFl07ejH",
+					"display_url": "pic.twitter.com/HXPFl07ejH",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1202912604143833088/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 59307,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202912445578149888/pu/vid/640x360/ofnqS-lgCHa8Z4SV.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202912445578149888/pu/vid/480x270/yRAb2HrukoxmBpz3.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1202912445578149888/pu/pl/NoYQSzRkDw6ZsEYW.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1202912445578149888/pu/vid/1280x720/G4X4fdkQejTxZYKD.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1185935579168424000,
+			"id_str": "1185935579168423936",
+			"name": "블루바이닐 / Blue Vinyl",
+			"screen_name": "bluevinylneedle",
+			"location": "",
+			"description": "Blue Vinyl Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3326,
+			"friends_count": 0,
+			"listed_count": 28,
+			"created_at": "Sun Oct 20 15:07:39 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1185935579168423936/1572876969",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 283,
+		"favorite_count": 204,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'0310' Teaser 2.\n⠀\nYerin Baek 1st Full-Length Album\n[𝐄𝐯𝐞𝐫𝐲 𝐥𝐞𝐭𝐭𝐞𝐫 𝐈 𝐬𝐞𝐧𝐭 𝐲𝐨𝐮.]\n⠀\n2019.12.10. TUE. 6PM (KST) Release\n\n#백예린 #yerinbaek #0310 https://t.co/HXPFl07ejH"
+	},
+	{
 		"created_at": "Fri Dec 06 10:54:52 +0000 2019",
 		"id": 1202891222991245300,
 		"id_str": "1202904234200621056",
