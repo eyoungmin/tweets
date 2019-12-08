abf74ee2d44dd7e9663880436a82c62377aa5d56
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-08 19:02:04 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51076,
-	"created_at": "Sun Dec 08 09:02:03 +0000 2019",
+	"tweets": 51078,
+	"created_at": "Sun Dec 08 10:02:03 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,361 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 08 09:54:53 +0000 2019",
+		"id": 1203603191905972200,
+		"id_str": "1203613910903017472",
+		"full_text": "'Popo (How deep is our love?)' Teaser.\n⠀\nYerin Baek 1st Full-Length Album\n[𝐄𝐯𝐞𝐫𝐲 𝐥𝐞𝐭𝐭𝐞𝐫 𝐈 𝐬𝐞𝐧𝐭 𝐲𝐨𝐮.]\n⠀\n2019.12.10. TUE. 6PM (KST) Release\n\n#백예린 #yerinbaek #popo https://t.co/we0l1hVbeH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "백예린",
+					"indices": [
+						139,
+						143
+					]
+				},
+				{
+					"text": "yerinbaek",
+					"indices": [
+						144,
+						154
+					]
+				},
+				{
+					"text": "popo",
+					"indices": [
+						155,
+						160
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1203603080752685000,
+					"id_str": "1203603080752685057",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1203603080752685057/pu/img/mGONbYCvkOWzq_9w.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1203603080752685057/pu/img/mGONbYCvkOWzq_9w.jpg",
+					"url": "https://t.co/we0l1hVbeH",
+					"display_url": "pic.twitter.com/we0l1hVbeH",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1203603191905972224/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203603080752685000,
+					"id_str": "1203603080752685057",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1203603080752685057/pu/img/mGONbYCvkOWzq_9w.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1203603080752685057/pu/img/mGONbYCvkOWzq_9w.jpg",
+					"url": "https://t.co/we0l1hVbeH",
+					"display_url": "pic.twitter.com/we0l1hVbeH",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1203603191905972224/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 50432,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1203603080752685057/pu/vid/480x270/tC5pQ42GMSwHLlXh.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1203603080752685057/pu/pl/hQ0Pc72oXTfJQGMa.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1203603080752685057/pu/vid/640x360/hxpax2nWjIDyDXK0.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1203603080752685057/pu/vid/1280x720/WguGg_6Vp6rGvAiS.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1185935579168424000,
+			"id_str": "1185935579168423936",
+			"name": "블루바이닐 / Blue Vinyl",
+			"screen_name": "bluevinylneedle",
+			"location": "",
+			"description": "Blue Vinyl Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3442,
+			"friends_count": 0,
+			"listed_count": 31,
+			"created_at": "Sun Oct 20 15:07:39 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1185935579168423936/1572876969",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 336,
+		"favorite_count": 223,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "'Popo (How deep is our love?)' Teaser.\n⠀\nYerin Baek 1st Full-Length Album\n[𝐄𝐯𝐞𝐫𝐲 𝐥𝐞𝐭𝐭𝐞𝐫 𝐈 𝐬𝐞𝐧𝐭 𝐲𝐨𝐮.]\n⠀\n2019.12.10. TUE. 6PM (KST) Release\n\n#백예린 #yerinbaek #popo https://t.co/we0l1hVbeH"
+	},
+	{
+		"created_at": "Sun Dec 08 09:24:29 +0000 2019",
+		"id": 1203606262967128000,
+		"id_str": "1203606262967128065",
+		"full_text": "[STATION X] NCT U 엔시티 유 'Coming Home' Teaser Clip #DOYOUNG\nhttps://t.co/MVrY9FtlIS\n#SM_STATION #STATION #스테이션",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DOYOUNG",
+					"indices": [
+						50,
+						58
+					]
+				},
+				{
+					"text": "SM_STATION",
+					"indices": [
+						83,
+						94
+					]
+				},
+				{
+					"text": "STATION",
+					"indices": [
+						95,
+						103
+					]
+				},
+				{
+					"text": "스테이션",
+					"indices": [
+						104,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MVrY9FtlIS",
+					"expanded_url": "https://youtu.be/EoS97CRIsk0",
+					"display_url": "youtu.be/EoS97CRIsk0",
+					"indices": [
+						59,
+						82
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @solplparty",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 856,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51592,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[STATION X] NCT U 엔시티 유 'Coming Home' Teaser Clip #DOYOUNG\nhttps://t.co/MVrY9FtlIS\n#SM_STATION #STATION #스테이션"
+	},
+	{
 		"created_at": "Sun Dec 08 08:23:04 +0000 2019",
 		"id": 1203573280755478500,
 		"id_str": "1203590805153173506",
