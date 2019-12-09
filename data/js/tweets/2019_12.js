d24f10d94386358a4df07e18a886dc67d63fcee9
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-09 17:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,353 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 09 07:37:20 +0000 2019",
+		"id": 1203941684976984000,
+		"id_str": "1203941684976984069",
+		"full_text": "#SMTOWN #Artist_News\n권유리(소녀시대 유리), 연탄은행 공익 광고 참여!\nhttps://t.co/CP2uBPn6CJ\n#GirlsGeneration #소녀시대 https://t.co/jjA9D9rdsR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "Artist_News",
+					"indices": [
+						8,
+						20
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						74,
+						90
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						91,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CP2uBPn6CJ",
+					"expanded_url": "https://www.smtown.com/artist/newsDetail/2968",
+					"display_url": "smtown.com/artist/newsDet…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1203941683500658700,
+					"id_str": "1203941683500658690",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/ELVClRdXsAIa9-L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELVClRdXsAIa9-L.jpg",
+					"url": "https://t.co/jjA9D9rdsR",
+					"display_url": "pic.twitter.com/jjA9D9rdsR",
+					"expanded_url": "https://twitter.com/fromminn/status/1203941684976984069/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203941683500658700,
+					"id_str": "1203941683500658690",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/ELVClRdXsAIa9-L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELVClRdXsAIa9-L.jpg",
+					"url": "https://t.co/jjA9D9rdsR",
+					"display_url": "pic.twitter.com/jjA9D9rdsR",
+					"expanded_url": "https://twitter.com/fromminn/status/1203941684976984069/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 192,
+									"y": 115,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 278,
+									"y": 34,
+									"h": 75,
+									"w": 75
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @solplparty",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 857,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51601,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#SMTOWN #Artist_News\n권유리(소녀시대 유리), 연탄은행 공익 광고 참여!\nhttps://t.co/CP2uBPn6CJ\n#GirlsGeneration #소녀시대 https://t.co/jjA9D9rdsR"
+	},
+	{
 		"created_at": "Mon Dec 09 02:54:53 +0000 2019",
 		"id": 1203857463197331500,
 		"id_str": "1203870602890825728",
