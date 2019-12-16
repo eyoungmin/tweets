134bf27f7d7b488ca15b434df9368ee7be9b89c9
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-16 19:02:08 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51397,
-	"created_at": "Mon Dec 16 09:02:07 +0000 2019",
+	"tweets": 51399,
+	"created_at": "Mon Dec 16 10:02:08 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,586 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 16 10:00:59 +0000 2019",
+		"id": 1206514550675296300,
+		"id_str": "1206514550675296256",
+		"full_text": "[STATION X] NCT U 엔시티 유 'Coming Home' 비하인더스테이션\nhttps://t.co/pDohqfSIoz\n#SM_STATION #STATION #스테이션",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SM_STATION",
+					"indices": [
+						71,
+						82
+					]
+				},
+				{
+					"text": "STATION",
+					"indices": [
+						83,
+						91
+					]
+				},
+				{
+					"text": "스테이션",
+					"indices": [
+						92,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pDohqfSIoz",
+					"expanded_url": "https://youtu.be/Tw6C8N_Qj58",
+					"display_url": "youtu.be/Tw6C8N_Qj58",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @solplparty",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 291,
+			"friends_count": 867,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51961,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[STATION X] NCT U 엔시티 유 'Coming Home' 비하인더스테이션\nhttps://t.co/pDohqfSIoz\n#SM_STATION #STATION #스테이션"
+	},
+	{
+		"created_at": "Mon Dec 16 09:34:17 +0000 2019",
+		"id": 1206507829609283600,
+		"id_str": "1206507829609283584",
+		"full_text": "#SMTOWN #Artist_News\n‘우주 쪼꼬미’ 신곡 ‘하얀 겨울’, 15일 오후 6시 대공개!\nhttps://t.co/uWgpwxGCfk\n#STATION #SMSTATION #스테이션 https://t.co/fQiVM2gkpr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "Artist_News",
+					"indices": [
+						8,
+						20
+					]
+				},
+				{
+					"text": "STATION",
+					"indices": [
+						81,
+						89
+					]
+				},
+				{
+					"text": "SMSTATION",
+					"indices": [
+						90,
+						100
+					]
+				},
+				{
+					"text": "스테이션",
+					"indices": [
+						101,
+						106
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uWgpwxGCfk",
+					"expanded_url": "https://www.smtown.com/artist/newsDetail/2996",
+					"display_url": "smtown.com/artist/newsDet…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1206507828090982400,
+					"id_str": "1206507828090982400",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/EL5gejAXUAAxEpj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EL5gejAXUAAxEpj.jpg",
+					"url": "https://t.co/fQiVM2gkpr",
+					"display_url": "pic.twitter.com/fQiVM2gkpr",
+					"expanded_url": "https://twitter.com/fromminn/status/1206507829609283584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1206507828090982400,
+					"id_str": "1206507828090982400",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/EL5gejAXUAAxEpj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EL5gejAXUAAxEpj.jpg",
+					"url": "https://t.co/fQiVM2gkpr",
+					"display_url": "pic.twitter.com/fQiVM2gkpr",
+					"expanded_url": "https://twitter.com/fromminn/status/1206507829609283584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 161,
+									"y": 24,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 236,
+									"y": 29,
+									"h": 33,
+									"w": 33
+								},
+								{
+									"x": 368,
+									"y": 125,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 276,
+									"y": 147,
+									"h": 34,
+									"w": 34
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @solplparty",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 291,
+			"friends_count": 867,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 51961,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#SMTOWN #Artist_News\n‘우주 쪼꼬미’ 신곡 ‘하얀 겨울’, 15일 오후 6시 대공개!\nhttps://t.co/uWgpwxGCfk\n#STATION #SMSTATION #스테이션 https://t.co/fQiVM2gkpr"
+	},
+	{
 		"created_at": "Mon Dec 16 08:54:52 +0000 2019",
 		"id": 1206492983618494500,
 		"id_str": "1206497913838473217",
