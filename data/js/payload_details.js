b4d4cb0413c5d74b3dc46711a50d75ee83033eec
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-17 10:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51435,
-	"created_at": "Tue Dec 17 00:02:13 +0000 2019",
+	"tweets": 51436,
+	"created_at": "Tue Dec 17 01:02:08 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,125 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Tue Dec 17 00:54:53 +0000 2019",
+		"id": 1206738693999841300,
+		"id_str": "1206739506281533443",
+		"full_text": "'#Psycho’ 컴백 D-6! \n#레드벨벳 의 새 앨범 ''The ReVe Festival Finale'! 타이틀 곡 'Psycho’로 독보적 색깔을 선사함은 물론, 매혹적인 상대에게 빠져드는 짜릿한 심리를 달콤한 도넛을 맛보는 과정에 빗댄 수록곡 ‘In &amp; Out’으로 시크하고 감각적 음악 스타일을 선보일 예정입니다! 🎆\n\n#RedVelvet",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Psycho",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						19,
+						24
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						188,
+						198
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/UmIWRCiABI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UmIWRCiABI",
+							"expanded_url": "http://youtu.be/vHS9E6JFja8",
+							"display_url": "youtu.be/vHS9E6JFja8",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1986686,
+			"friends_count": 2,
+			"listed_count": 6016,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 23,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1064,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1204779084196089856/CQrd2I-j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1204779084196089856/CQrd2I-j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1566291835",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1988,
+		"favorite_count": 4680,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "'#Psycho’ 컴백 D-6! \n#레드벨벳 의 새 앨범 ''The ReVe Festival Finale'! 타이틀 곡 'Psycho’로 독보적 색깔을 선사함은 물론, 매혹적인 상대에게 빠져드는 짜릿한 심리를 달콤한 도넛을 맛보는 과정에 빗댄 수록곡 ‘In &amp; Out’으로 시크하고 감각적 음악 스타일을 선보일 예정입니다! 🎆\n\n#RedVelvet"
+	},
+	{
 		"created_at": "Mon Dec 16 23:54:53 +0000 2019",
 		"id": 1206710514954297300,
 		"id_str": "1206724406531231744",
