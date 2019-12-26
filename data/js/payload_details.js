252d4a151ee25d119c6ac23c1f4d41ad8c895a50
Youngmin Lee <iameyoungmin@gmail.com>
2019-12-26 12:02:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 51870,
-	"created_at": "Thu Dec 26 02:02:10 +0000 2019",
+	"tweets": 51871,
+	"created_at": "Thu Dec 26 03:02:13 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_12.js",
 		"year": 2019,
 		"var_name": "tweets_2019_12",
-		"tweet_count": 994,
+		"tweet_count": 995,
 		"month": 12
 	},
 	{
