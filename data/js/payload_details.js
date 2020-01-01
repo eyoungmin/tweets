bfcf36b2beae46764ac0dcc0c2199aff92b8956a
Youngmin Lee <iameyoungmin@gmail.com>
2020-01-01 13:02:00 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52024,
-	"created_at": "Wed Jan 01 03:02:10 +0000 2020",
+	"tweets": 52026,
+	"created_at": "Wed Jan 01 04:02:00 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,787 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Wed Jan 01 03:40:40 +0000 2020",
+		"id": 1212214453011275800,
+		"id_str": "1212217048601899010",
+		"full_text": "[이달의소녀탐구 #547]\n#이달의소녀탐구 #loonatv #이달의소녀 #LOONA https://t.co/pdJ82ZQ8Xk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀탐구",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"text": "loonatv",
+					"indices": [
+						24,
+						32
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						33,
+						39
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						40,
+						46
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212214362724716500,
+					"id_str": "1212214362724716544",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212214362724716544/pu/img/ANYWDnByK_QtipjC.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212214362724716544/pu/img/ANYWDnByK_QtipjC.jpg",
+					"url": "https://t.co/pdJ82ZQ8Xk",
+					"display_url": "pic.twitter.com/pdJ82ZQ8Xk",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1212214453011275776/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212214362724716500,
+					"id_str": "1212214362724716544",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1212214362724716544/pu/img/ANYWDnByK_QtipjC.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1212214362724716544/pu/img/ANYWDnByK_QtipjC.jpg",
+					"url": "https://t.co/pdJ82ZQ8Xk",
+					"display_url": "pic.twitter.com/pdJ82ZQ8Xk",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1212214453011275776/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 854,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							427,
+							240
+						],
+						"duration_millis": 60084,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1212214362724716544/pu/pl/UzPKRhJsjcJxQxeX.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212214362724716544/pu/vid/480x270/h3A3LdP1u0lL9H9p.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212214362724716544/pu/vid/640x360/EHN7cx5rSAYNAVFf.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1212214362724716544/pu/vid/854x480/9duZo2Bmjx81wM7-.mp4?tag=10"
+							}
+						]
+					},
+					"features": {},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 379036,
+			"friends_count": 0,
+			"listed_count": 5518,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1673,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1550571087",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3612,
+		"favorite_count": 9415,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[이달의소녀탐구 #547]\n#이달의소녀탐구 #loonatv #이달의소녀 #LOONA https://t.co/pdJ82ZQ8Xk"
+	},
+	{
+		"created_at": "Wed Jan 01 03:40:37 +0000 2020",
+		"id": 1212209768061796400,
+		"id_str": "1212217035150761986",
+		"full_text": "레드벨벳 신곡 ‘Psycho’가 빌보드 월드 디지털 송 세일즈 차트 1위를 기록, 소셜 50 차트에서도 2주 연속 2위를 기록해 K-POP 걸그룹 중 최고 순위에 오르며 2020년에도 글로벌 히트 행진을 이어가고 있습니다!\n\n#RedVelvet #레드벨벳 #RVF #TheReVeFestivalFinale #Psycho #FinaleWithPsycho https://t.co/BIiroofy8Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			196
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						126,
+						136
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						137,
+						142
+					]
+				},
+				{
+					"text": "RVF",
+					"indices": [
+						143,
+						147
+					]
+				},
+				{
+					"text": "TheReVeFestivalFinale",
+					"indices": [
+						148,
+						170
+					]
+				},
+				{
+					"text": "Psycho",
+					"indices": [
+						171,
+						178
+					]
+				},
+				{
+					"text": "FinaleWithPsycho",
+					"indices": [
+						179,
+						196
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1212209756711993300,
+					"id_str": "1212209756711993345",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/ENKiWeYUwAE6fH3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENKiWeYUwAE6fH3.jpg",
+					"url": "https://t.co/BIiroofy8Z",
+					"display_url": "pic.twitter.com/BIiroofy8Z",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1212209768061796352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 575,
+									"y": 377,
+									"h": 190,
+									"w": 190
+								},
+								{
+									"x": 562,
+									"y": 1151,
+									"h": 131,
+									"w": 131
+								},
+								{
+									"x": 407,
+									"y": 354,
+									"h": 193,
+									"w": 193
+								},
+								{
+									"x": 764,
+									"y": 392,
+									"h": 161,
+									"w": 161
+								},
+								{
+									"x": 241,
+									"y": 353,
+									"h": 210,
+									"w": 210
+								},
+								{
+									"x": 8,
+									"y": 237,
+									"h": 273,
+									"w": 273
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 460,
+									"y": 301,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 449,
+									"y": 920,
+									"h": 104,
+									"w": 104
+								},
+								{
+									"x": 325,
+									"y": 283,
+									"h": 154,
+									"w": 154
+								},
+								{
+									"x": 611,
+									"y": 313,
+									"h": 128,
+									"w": 128
+								},
+								{
+									"x": 192,
+									"y": 282,
+									"h": 168,
+									"w": 168
+								},
+								{
+									"x": 6,
+									"y": 189,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 575,
+									"y": 377,
+									"h": 190,
+									"w": 190
+								},
+								{
+									"x": 562,
+									"y": 1151,
+									"h": 131,
+									"w": 131
+								},
+								{
+									"x": 407,
+									"y": 354,
+									"h": 193,
+									"w": 193
+								},
+								{
+									"x": 764,
+									"y": 392,
+									"h": 161,
+									"w": 161
+								},
+								{
+									"x": 241,
+									"y": 353,
+									"h": 210,
+									"w": 210
+								},
+								{
+									"x": 8,
+									"y": 237,
+									"h": 273,
+									"w": 273
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 260,
+									"y": 170,
+									"h": 86,
+									"w": 86
+								},
+								{
+									"x": 254,
+									"y": 521,
+									"h": 59,
+									"w": 59
+								},
+								{
+									"x": 184,
+									"y": 160,
+									"h": 87,
+									"w": 87
+								},
+								{
+									"x": 346,
+									"y": 177,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 109,
+									"y": 159,
+									"h": 95,
+									"w": 95
+								},
+								{
+									"x": 3,
+									"y": 107,
+									"h": 123,
+									"w": 123
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212209756711993300,
+					"id_str": "1212209756711993345",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/ENKiWeYUwAE6fH3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ENKiWeYUwAE6fH3.jpg",
+					"url": "https://t.co/BIiroofy8Z",
+					"display_url": "pic.twitter.com/BIiroofy8Z",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1212209768061796352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 575,
+									"y": 377,
+									"h": 190,
+									"w": 190
+								},
+								{
+									"x": 562,
+									"y": 1151,
+									"h": 131,
+									"w": 131
+								},
+								{
+									"x": 407,
+									"y": 354,
+									"h": 193,
+									"w": 193
+								},
+								{
+									"x": 764,
+									"y": 392,
+									"h": 161,
+									"w": 161
+								},
+								{
+									"x": 241,
+									"y": 353,
+									"h": 210,
+									"w": 210
+								},
+								{
+									"x": 8,
+									"y": 237,
+									"h": 273,
+									"w": 273
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 460,
+									"y": 301,
+									"h": 152,
+									"w": 152
+								},
+								{
+									"x": 449,
+									"y": 920,
+									"h": 104,
+									"w": 104
+								},
+								{
+									"x": 325,
+									"y": 283,
+									"h": 154,
+									"w": 154
+								},
+								{
+									"x": 611,
+									"y": 313,
+									"h": 128,
+									"w": 128
+								},
+								{
+									"x": 192,
+									"y": 282,
+									"h": 168,
+									"w": 168
+								},
+								{
+									"x": 6,
+									"y": 189,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 575,
+									"y": 377,
+									"h": 190,
+									"w": 190
+								},
+								{
+									"x": 562,
+									"y": 1151,
+									"h": 131,
+									"w": 131
+								},
+								{
+									"x": 407,
+									"y": 354,
+									"h": 193,
+									"w": 193
+								},
+								{
+									"x": 764,
+									"y": 392,
+									"h": 161,
+									"w": 161
+								},
+								{
+									"x": 241,
+									"y": 353,
+									"h": 210,
+									"w": 210
+								},
+								{
+									"x": 8,
+									"y": 237,
+									"h": 273,
+									"w": 273
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 260,
+									"y": 170,
+									"h": 86,
+									"w": 86
+								},
+								{
+									"x": 254,
+									"y": 521,
+									"h": 59,
+									"w": 59
+								},
+								{
+									"x": 184,
+									"y": 160,
+									"h": 87,
+									"w": 87
+								},
+								{
+									"x": 346,
+									"y": 177,
+									"h": 72,
+									"w": 72
+								},
+								{
+									"x": 109,
+									"y": 159,
+									"h": 95,
+									"w": 95
+								},
+								{
+									"x": 3,
+									"y": 107,
+									"h": 123,
+									"w": 123
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/tNi01kT3r4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/tNi01kT3r4",
+							"expanded_url": "https://youtu.be/uR8Mrt1IpXg",
+							"display_url": "youtu.be/uR8Mrt1IpXg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2095217,
+			"friends_count": 2,
+			"listed_count": 6310,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 25,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1204779084196089856/CQrd2I-j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1204779084196089856/CQrd2I-j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1577091773",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6905,
+		"favorite_count": 19992,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "레드벨벳 신곡 ‘Psycho’가 빌보드 월드 디지털 송 세일즈 차트 1위를 기록, 소셜 50 차트에서도 2주 연속 2위를 기록해 K-POP 걸그룹 중 최고 순위에 오르며 2020년에도 글로벌 히트 행진을 이어가고 있습니다!\n\n#RedVelvet #레드벨벳 #RVF #TheReVeFestivalFinale #Psycho #FinaleWithPsycho https://t.co/BIiroofy8Z"
+	},
+	{
 		"created_at": "Wed Jan 01 03:00:09 +0000 2020",
 		"id": 1212206850927673300,
 		"id_str": "1212206850927673351",
