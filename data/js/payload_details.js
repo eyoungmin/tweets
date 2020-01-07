a5176f03961cc3400bd0e25e8b787eb9e02fae95
Youngmin Lee <iameyoungmin@gmail.com>
2020-01-07 23:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52259,
-	"created_at": "Tue Jan 07 13:02:03 +0000 2020",
+	"tweets": 52260,
+	"created_at": "Tue Jan 07 14:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,150 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 07 13:29:11 +0000 2020",
+		"id": 1214539477089243100,
+		"id_str": "1214539477089243136",
+		"full_text": "#Soyoon_Production\n[SPD] 2020 HAPPY NEW YEAR travel with my lovely nodab friends\nhttps://t.co/1uzytDXBfq\n#새소년 #SESONEON #황소윤 #Soyoon",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Soyoon_Production",
+					"indices": [
+						0,
+						18
+					]
+				},
+				{
+					"text": "새소년",
+					"indices": [
+						105,
+						109
+					]
+				},
+				{
+					"text": "SESONEON",
+					"indices": [
+						110,
+						119
+					]
+				},
+				{
+					"text": "황소윤",
+					"indices": [
+						120,
+						124
+					]
+				},
+				{
+					"text": "Soyoon",
+					"indices": [
+						125,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1uzytDXBfq",
+					"expanded_url": "https://youtu.be/nHFgsgfgGjo",
+					"display_url": "youtu.be/nHFgsgfgGjo",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 291,
+			"friends_count": 879,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36528,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 52928,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Soyoon_Production\n[SPD] 2020 HAPPY NEW YEAR travel with my lovely nodab friends\nhttps://t.co/1uzytDXBfq\n#새소년 #SESONEON #황소윤 #Soyoon"
+	},
+	{
 		"created_at": "Tue Jan 07 12:22:09 +0000 2020",
 		"id": 1214522607992819700,
 		"id_str": "1214522607992819718",
