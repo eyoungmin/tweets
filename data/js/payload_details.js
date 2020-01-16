0c8daa092f4d825f06f677bb41156f7cb298c56d
Youngmin Lee <iameyoungmin@gmail.com>
2020-01-16 17:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 52678,
-	"created_at": "Thu Jan 16 07:02:08 +0000 2020",
+	"tweets": 52679,
+	"created_at": "Thu Jan 16 08:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,353 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 16 07:05:29 +0000 2020",
+		"id": 1217704407451807700,
+		"id_str": "1217704407451807744",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 도쿄 걸즈 컬렉션 메인 아티스트 출연 확정\nhttps://t.co/uaefEJWRLF\n#블랙핑크 #BLACKPINK https://t.co/E3tV6rCIor",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						71,
+						76
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						77,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uaefEJWRLF",
+					"expanded_url": "https://yg-life.com/archives/147956?lang=ko",
+					"display_url": "yg-life.com/archives/14795…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1217704406315081700,
+					"id_str": "1217704406315081728",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/EOYntLPWoAA3YqQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOYntLPWoAA3YqQ.jpg",
+					"url": "https://t.co/E3tV6rCIor",
+					"display_url": "pic.twitter.com/E3tV6rCIor",
+					"expanded_url": "https://twitter.com/fromminn/status/1217704407451807744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1217704406315081700,
+					"id_str": "1217704406315081728",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/EOYntLPWoAA3YqQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EOYntLPWoAA3YqQ.jpg",
+					"url": "https://t.co/E3tV6rCIor",
+					"display_url": "pic.twitter.com/E3tV6rCIor",
+					"expanded_url": "https://twitter.com/fromminn/status/1217704407451807744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 287,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 194,
+									"y": 80,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 83,
+									"y": 112,
+									"h": 46,
+									"w": 46
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 292,
+			"friends_count": 882,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 36850,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53416,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 도쿄 걸즈 컬렉션 메인 아티스트 출연 확정\nhttps://t.co/uaefEJWRLF\n#블랙핑크 #BLACKPINK https://t.co/E3tV6rCIor"
+	},
+	{
 		"created_at": "Thu Jan 16 06:34:14 +0000 2020",
 		"id": 1217696542603300900,
 		"id_str": "1217696542603300869",
