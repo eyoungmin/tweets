19c3fe0514249b515d662d15d12bbac37967ae11
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-01 16:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53274,
-	"created_at": "Sat Feb 01 06:02:00 +0000 2020",
+	"tweets": 53275,
+	"created_at": "Sat Feb 01 07:02:00 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 01 06:05:43 +0000 2020",
+		"id": 1223381130998636500,
+		"id_str": "1223487572799672320",
+		"full_text": "We’ve given conversations a makeover on iOS. When people you follow are in a conversation, you’ll see their replies in a new way in your Home timeline.\n\nThis new layout makes it easier to see who's replying to who so you can join in on relevant conversations. https://t.co/xNmnAtQFeI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1223381079786184700,
+					"id_str": "1223381079786184704",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EPpSnD2W4AACsiZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EPpSnD2W4AACsiZ.jpg",
+					"url": "https://t.co/xNmnAtQFeI",
+					"display_url": "pic.twitter.com/xNmnAtQFeI",
+					"expanded_url": "https://twitter.com/TwitterSupport/status/1223381130998636549/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223381079786184700,
+					"id_str": "1223381079786184704",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EPpSnD2W4AACsiZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EPpSnD2W4AACsiZ.jpg",
+					"url": "https://t.co/xNmnAtQFeI",
+					"display_url": "pic.twitter.com/xNmnAtQFeI",
+					"expanded_url": "https://twitter.com/TwitterSupport/status/1223381130998636549/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EPpSnD2W4AACsiZ.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17874544,
+			"id_str": "17874544",
+			"name": "Twitter Support",
+			"screen_name": "TwitterSupport",
+			"location": "Twitter HQ",
+			"description": "Your official source for Twitter Support. Follow us for tips, tricks, and announcements.",
+			"url": "https://t.co/heEvRrl4yN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/heEvRrl4yN",
+							"expanded_url": "https://help.twitter.com",
+							"display_url": "help.twitter.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6093905,
+			"friends_count": 17,
+			"listed_count": 15366,
+			"created_at": "Thu Dec 04 18:51:57 +0000 2008",
+			"favourites_count": 359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 28311,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/941807338171777025/PRP6vwDq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/941807338171777025/PRP6vwDq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17874544/1499274456",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 264,
+		"favorite_count": 620,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We’ve given conversations a makeover on iOS. When people you follow are in a conversation, you’ll see their replies in a new way in your Home timeline.\n\nThis new layout makes it easier to see who's replying to who so you can join in on relevant conversations. https://t.co/xNmnAtQFeI"
+	},
+	{
 		"created_at": "Sat Feb 01 05:59:47 +0000 2020",
 		"id": 1223486079984009200,
 		"id_str": "1223486079984009217",
