1e162ab36baab9782fb59eab32c124a6a496a0ce
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-01 19:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53275,
-	"created_at": "Sat Feb 01 07:02:00 +0000 2020",
+	"tweets": 53276,
+	"created_at": "Sat Feb 01 10:02:00 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,375 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 01 09:40:38 +0000 2020",
+		"id": 1223541008056508400,
+		"id_str": "1223541657519390721",
+		"full_text": "‘Turn on that Blue Vinyl’\n신종 코로나바이러스 감염증 대비 안내\n⠀\n안녕하세요, 블루바이닐입니다. 최근 신종 코로나바이러스 감염증이 확산된 가운데, 방역 대비책 및 앵콜 공연 티켓오픈 잠정 연기에 관한 안내 말씀드립니다.\n⠀\n⠀ https://t.co/Q6EMGx6wrb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1223540995305791500,
+					"id_str": "1223540995305791488",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EPrkDXlUYAASKLl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPrkDXlUYAASKLl.jpg",
+					"url": "https://t.co/Q6EMGx6wrb",
+					"display_url": "pic.twitter.com/Q6EMGx6wrb",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1223541008056508417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2047,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1223540995305791500,
+					"id_str": "1223540995305791488",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EPrkDXlUYAASKLl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPrkDXlUYAASKLl.jpg",
+					"url": "https://t.co/Q6EMGx6wrb",
+					"display_url": "pic.twitter.com/Q6EMGx6wrb",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1223541008056508417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2047,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1223540995314159600,
+					"id_str": "1223540995314159616",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EPrkDXnUEAAaUh5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPrkDXnUEAAaUh5.jpg",
+					"url": "https://t.co/Q6EMGx6wrb",
+					"display_url": "pic.twitter.com/Q6EMGx6wrb",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1223541008056508417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1199,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2047,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1223540995314159600,
+					"id_str": "1223540995314159617",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EPrkDXnUEAEfHTg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPrkDXnUEAEfHTg.jpg",
+					"url": "https://t.co/Q6EMGx6wrb",
+					"display_url": "pic.twitter.com/Q6EMGx6wrb",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1223541008056508417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2047,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				},
+				{
+					"id": 1223540995314159600,
+					"id_str": "1223540995314159618",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/EPrkDXnUEAIbL9x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPrkDXnUEAIbL9x.jpg",
+					"url": "https://t.co/Q6EMGx6wrb",
+					"display_url": "pic.twitter.com/Q6EMGx6wrb",
+					"expanded_url": "https://twitter.com/bluevinylneedle/status/1223541008056508417/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 536,
+									"y": 175,
+									"h": 33,
+									"w": 33
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 946,
+									"y": 310,
+									"h": 58,
+									"w": 58
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1616,
+									"y": 530,
+									"h": 100,
+									"w": 100
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1616,
+									"y": 530,
+									"h": 100,
+									"w": 100
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1185935579168424000,
+			"id_str": "1185935579168423936",
+			"name": "블루바이닐 / Blue Vinyl",
+			"screen_name": "bluevinylneedle",
+			"location": "",
+			"description": "Blue Vinyl Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5138,
+			"friends_count": 0,
+			"listed_count": 52,
+			"created_at": "Sun Oct 20 15:07:39 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1191358468281683968/yKamAYrx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1185935579168423936/1572876969",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 91,
+		"favorite_count": 62,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‘Turn on that Blue Vinyl’\n신종 코로나바이러스 감염증 대비 안내\n⠀\n안녕하세요, 블루바이닐입니다. 최근 신종 코로나바이러스 감염증이 확산된 가운데, 방역 대비책 및 앵콜 공연 티켓오픈 잠정 연기에 관한 안내 말씀드립니다.\n⠀\n⠀ https://t.co/Q6EMGx6wrb"
+	},
+	{
 		"created_at": "Sat Feb 01 06:05:43 +0000 2020",
 		"id": 1223381130998636500,
 		"id_str": "1223487572799672320",
