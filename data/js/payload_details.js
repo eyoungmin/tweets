4358f9ac1b91f3913605bfddb57ff3e772f0d57d
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-02 15:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53301,
-	"created_at": "Sun Feb 02 04:02:01 +0000 2020",
+	"tweets": 53302,
+	"created_at": "Sun Feb 02 06:02:01 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 02 05:05:42 +0000 2020",
+		"id": 1223828368165982200,
+		"id_str": "1223834858725597184",
+		"full_text": "유용한 ‘데이터 시각화’ 정보 사이트 모음 https://t.co/k5SPvoihQR \n공공데이터 시각화\n1) Data USA\n2) 데이지(DAISY)\n3) ODPia\n~\n데이터 저널리즘\n1) SBS 마부작침\n2) KBS 데이터룸\n3) ProPublica\n4) The Upshot, NYT\n~\n자체 데이터 시각화 툴 보유\n1) infogram\n2) Tableau Galle...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/k5SPvoihQR",
+					"expanded_url": "http://j.mp/2uYUyGF",
+					"display_url": "j.mp/2uYUyGF",
+					"indices": [
+						24,
+						47
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://news.hada.io\" rel=\"nofollow\">NewsHada</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1149438140811116500,
+			"id_str": "1149438140811116544",
+			"name": "GeekNews",
+			"screen_name": "GeekNewsBot",
+			"location": "",
+			"description": "GeekNews 는 기술,개발,스타트업과 세상의 재미난 것들을 좋아하는 Geek들을 위한 뉴스사이트 입니다. 이 계정은 긱뉴스에 새로 등록된 뉴스들을 자동으로 트윗합니다",
+			"url": "https://t.co/Eeoeh5jF1C",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Eeoeh5jF1C",
+							"expanded_url": "https://news.hada.io",
+							"display_url": "news.hada.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2113,
+			"friends_count": 0,
+			"listed_count": 38,
+			"created_at": "Thu Jul 11 21:59:43 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_link_color": "000065",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "유용한 ‘데이터 시각화’ 정보 사이트 모음 https://t.co/k5SPvoihQR \n공공데이터 시각화\n1) Data USA\n2) 데이지(DAISY)\n3) ODPia\n~\n데이터 저널리즘\n1) SBS 마부작침\n2) KBS 데이터룸\n3) ProPublica\n4) The Upshot, NYT\n~\n자체 데이터 시각화 툴 보유\n1) infogram\n2) Tableau Galle..."
+	},
+	{
 		"created_at": "Sun Feb 02 03:40:38 +0000 2020",
 		"id": 1223806116590567400,
 		"id_str": "1223813448175443968",
