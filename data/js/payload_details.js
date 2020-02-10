1f4724764a3ecf29b7e3f25973dc7ce0be468690
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-10 22:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53623,
-	"created_at": "Mon Feb 10 11:02:04 +0000 2020",
+	"tweets": 53624,
+	"created_at": "Mon Feb 10 13:02:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 12:40:46 +0000 2020",
+		"id": 1226838386343698400,
+		"id_str": "1226848481316425728",
+		"full_text": "#이달의소녀 는 지금 아이돌 라디오 출격 준비 끝!\nDJ로 변신한 저희의 모습을 지금 바로 확인해 보세요🎙\n\n▶ https://t.co/1Y46p6tbaA\n\n#LOONA https://t.co/6Bh1Kk0fgm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						88,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1Y46p6tbaA",
+					"expanded_url": "https://www.vlive.tv/video/174335?channelCode=C6571B",
+					"display_url": "vlive.tv/video/174335?c…",
+					"indices": [
+						63,
+						86
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1226838368564080600,
+					"id_str": "1226838368564080640",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EQaa_wMU8AAn30Q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQaa_wMU8AAn30Q.jpg",
+					"url": "https://t.co/6Bh1Kk0fgm",
+					"display_url": "pic.twitter.com/6Bh1Kk0fgm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1226838386343698432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1226838368564080600,
+					"id_str": "1226838368564080640",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EQaa_wMU8AAn30Q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQaa_wMU8AAn30Q.jpg",
+					"url": "https://t.co/6Bh1Kk0fgm",
+					"display_url": "pic.twitter.com/6Bh1Kk0fgm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1226838386343698432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 414983,
+			"friends_count": 0,
+			"listed_count": 5801,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1761,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4511,
+		"favorite_count": 11000,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 는 지금 아이돌 라디오 출격 준비 끝!\nDJ로 변신한 저희의 모습을 지금 바로 확인해 보세요🎙\n\n▶ https://t.co/1Y46p6tbaA\n\n#LOONA https://t.co/6Bh1Kk0fgm"
+	},
+	{
 		"created_at": "Mon Feb 10 10:40:43 +0000 2020",
 		"id": 1226811057420804000,
 		"id_str": "1226818269560082432",
