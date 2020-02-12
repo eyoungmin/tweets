d9abceb1e6ee42c790cb833b4d29e0df0e18a29f
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-12 09:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53676,
-	"created_at": "Tue Feb 11 20:02:04 +0000 2020",
+	"tweets": 53677,
+	"created_at": "Wed Feb 12 00:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 11 23:08:22 +0000 2020",
+		"id": 1227368810778431500,
+		"id_str": "1227368810778431488",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 英 웸블리 시상식 “2019년 최고 그룹이자 가장 인상적”\nhttps://t.co/MtnBAGu1Xh\n#블랙핑크 #BLACKPINK https://t.co/17caIDSJcV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						80,
+						85
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						86,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MtnBAGu1Xh",
+					"expanded_url": "https://yg-life.com/archives/150944?lang=ko",
+					"display_url": "yg-life.com/archives/15094…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1227368809486585900,
+					"id_str": "1227368809486585858",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EQh9benXkAI14FO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQh9benXkAI14FO.jpg",
+					"url": "https://t.co/17caIDSJcV",
+					"display_url": "pic.twitter.com/17caIDSJcV",
+					"expanded_url": "https://twitter.com/fromminn/status/1227368810778431488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227368809486585900,
+					"id_str": "1227368809486585858",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EQh9benXkAI14FO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQh9benXkAI14FO.jpg",
+					"url": "https://t.co/17caIDSJcV",
+					"display_url": "pic.twitter.com/17caIDSJcV",
+					"expanded_url": "https://twitter.com/fromminn/status/1227368810778431488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 355,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 295,
+			"friends_count": 893,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 37674,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 54587,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 英 웸블리 시상식 “2019년 최고 그룹이자 가장 인상적”\nhttps://t.co/MtnBAGu1Xh\n#블랙핑크 #BLACKPINK https://t.co/17caIDSJcV"
+	},
+	{
 		"created_at": "Tue Feb 11 20:01:07 +0000 2020",
 		"id": 1227321687949095000,
 		"id_str": "1227321687949094913",
