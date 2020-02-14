0d72c925eaf926150531181ca8ac6ccf3f501354
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-15 02:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53813,
-	"created_at": "Fri Feb 14 16:02:08 +0000 2020",
+	"tweets": 53814,
+	"created_at": "Fri Feb 14 17:02:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 14 16:57:23 +0000 2020",
+		"id": 1228362614876573700,
+		"id_str": "1228362614876573701",
+		"full_text": "김앤박의 BEST NEW K-POP #100 ★펜타곤(PENTAGON), KARD, 더보이즈(THE BOYZ),★ | CASPER RADIO\nhttps://t.co/oS7eoi8MyH\n　\n#김윤하 #평론가 #박희아 #기자\n#KPOP #케이팝 #IDOL #아이돌\n#BEST_NEW_KPOP #베스트뉴케이팝\n#CASPER_RADIO #캐스퍼라디오",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김윤하",
+					"indices": [
+						105,
+						109
+					]
+				},
+				{
+					"text": "평론가",
+					"indices": [
+						110,
+						114
+					]
+				},
+				{
+					"text": "박희아",
+					"indices": [
+						115,
+						119
+					]
+				},
+				{
+					"text": "기자",
+					"indices": [
+						120,
+						123
+					]
+				},
+				{
+					"text": "KPOP",
+					"indices": [
+						124,
+						129
+					]
+				},
+				{
+					"text": "케이팝",
+					"indices": [
+						130,
+						134
+					]
+				},
+				{
+					"text": "IDOL",
+					"indices": [
+						135,
+						140
+					]
+				},
+				{
+					"text": "아이돌",
+					"indices": [
+						141,
+						145
+					]
+				},
+				{
+					"text": "BEST_NEW_KPOP",
+					"indices": [
+						146,
+						160
+					]
+				},
+				{
+					"text": "베스트뉴케이팝",
+					"indices": [
+						161,
+						169
+					]
+				},
+				{
+					"text": "CASPER_RADIO",
+					"indices": [
+						170,
+						183
+					]
+				},
+				{
+					"text": "캐스퍼라디오",
+					"indices": [
+						184,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oS7eoi8MyH",
+					"expanded_url": "http://www.vlive.tv/video/174606",
+					"display_url": "vlive.tv/video/174606",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 296,
+			"friends_count": 901,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 37836,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 54744,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "김앤박의 BEST NEW K-POP #100 ★펜타곤(PENTAGON), KARD, 더보이즈(THE BOYZ),★ | CASPER RADIO\nhttps://t.co/oS7eoi8MyH\n　\n#김윤하 #평론가 #박희아 #기자\n#KPOP #케이팝 #IDOL #아이돌\n#BEST_NEW_KPOP #베스트뉴케이팝\n#CASPER_RADIO #캐스퍼라디오"
+	},
+	{
 		"created_at": "Fri Feb 14 15:56:13 +0000 2020",
 		"id": 1228289677595041800,
 		"id_str": "1228347219725565952",
