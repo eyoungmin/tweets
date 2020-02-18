4051dedfc914204558d4178d358d2767c6c7b256
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-19 00:20:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 53919,
-	"created_at": "Tue Feb 18 14:02:45 +0000 2020",
+	"tweets": 53920,
+	"created_at": "Tue Feb 18 15:20:02 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,136 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 18 15:10:45 +0000 2020",
+		"id": 1229785329248489500,
+		"id_str": "1229785329248489473",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n32-1] 듣똑인터뷰/웹툰작가 수신지가 본 낙태죄가 합헌인 세상: '곤(GONE)'\nhttps://t.co/oMD5yDvFsX\n팟티: https://t.co/S2MnTDNQgc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oMD5yDvFsX",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1582007836196.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						66,
+						89
+					]
+				},
+				{
+					"url": "https://t.co/S2MnTDNQgc",
+					"expanded_url": "https://www.podty.me/episode/12925609",
+					"display_url": "podty.me/episode/129256…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY",
+			"url": "https://t.co/pEO2Gdl415",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2Gdl415",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301,
+			"friends_count": 901,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 37839,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 54851,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n32-1] 듣똑인터뷰/웹툰작가 수신지가 본 낙태죄가 합헌인 세상: '곤(GONE)'\nhttps://t.co/oMD5yDvFsX\n팟티: https://t.co/S2MnTDNQgc"
+	},
+	{
 		"created_at": "Tue Feb 18 13:43:14 +0000 2020",
 		"id": 1229763306006683600,
 		"id_str": "1229763306006683648",
@@ -608,7 +738,7 @@ Grailbird.data.tweets_2020_02 =
 		"created_at": "Tue Feb 18 13:30:41 +0000 2020",
 		"id": 1229745045957267500,
 		"id_str": "1229760145414139904",
-		"full_text": "오늘의 #ASC_ARCHIVE 주인공은\n💗이달의 소녀💗\nASC에 출연한 이달의 소녀의\n모든 것을 알아볼까요? 🙇🏻‍♀️🙇🏻 https://t.co/KeakBZlMPy",
+		"full_text": "오늘의 #ASC_ARCHIVE 주인공은\n💗이달의 소녀💗\nASC에 출연한 이달의 소녀의\n모든 것을 알아볼까요? 🙇🏻♀️🙇🏻 https://t.co/KeakBZlMPy",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -856,7 +986,7 @@ Grailbird.data.tweets_2020_02 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "오늘의 #ASC_ARCHIVE 주인공은\n💗이달의 소녀💗\nASC에 출연한 이달의 소녀의\n모든 것을 알아볼까요? 🙇🏻‍♀️🙇🏻 https://t.co/KeakBZlMPy"
+		"text": "오늘의 #ASC_ARCHIVE 주인공은\n💗이달의 소녀💗\nASC에 출연한 이달의 소녀의\n모든 것을 알아볼까요? 🙇🏻♀️🙇🏻 https://t.co/KeakBZlMPy"
 	},
 	{
 		"created_at": "Tue Feb 18 13:30:32 +0000 2020",
