db232a3cba40aaec6ea69981498dfbe015fa687a
Youngmin Lee <iameyoungmin@gmail.com>
2020-02-21 15:02:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54070,
-	"created_at": "Fri Feb 21 05:02:11 +0000 2020",
+	"tweets": 54071,
+	"created_at": "Fri Feb 21 06:02:12 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 21 05:31:47 +0000 2020",
+		"id": 1230719297409798100,
+		"id_str": "1230726793193091076",
+		"full_text": "코로나19 확산 방지를 위해 서울광장·청계광장·광화문광장에서의 집회 금지, 위반시 300만원 이하 벌금이 부과됩니다.(감염병예방법 제49조: 감염병 예방을 위해 도심 내 여러 사람의 집합 제한·금지)\n\n또한 서울소재 신천지교회 폐쇄 및 방역실시, 추후 안전이 확인되면 예배활동을 허용할 예정입니다 https://t.co/Rcf59dOAHC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			165
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1230719278652870700,
+					"id_str": "1230719278652870656",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/ERRkqc5UUAAXW29.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERRkqc5UUAAXW29.jpg",
+					"url": "https://t.co/Rcf59dOAHC",
+					"display_url": "pic.twitter.com/Rcf59dOAHC",
+					"expanded_url": "https://twitter.com/seoulmania/status/1230719297409798155/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 696,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 394,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1187,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1230719278652870700,
+					"id_str": "1230719278652870656",
+					"indices": [
+						166,
+						189
+					],
+					"media_url": "http://pbs.twimg.com/media/ERRkqc5UUAAXW29.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERRkqc5UUAAXW29.jpg",
+					"url": "https://t.co/Rcf59dOAHC",
+					"display_url": "pic.twitter.com/Rcf59dOAHC",
+					"expanded_url": "https://twitter.com/seoulmania/status/1230719297409798155/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 696,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 394,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1187,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 93794912,
+			"id_str": "93794912",
+			"name": "서울시",
+			"screen_name": "seoulmania",
+			"location": "대한민국 서울",
+			"description": "서울특별시에서 운영하는 대표 트위터입니다. 시민분들과 서울의 소식을 함께 나누고자 합니다. \"내 삶이 행복한 서울, 시민이 주인인 서울\"!!",
+			"url": "https://t.co/VBZ8BW24xq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/VBZ8BW24xq",
+							"expanded_url": "http://www.seoul.go.kr",
+							"display_url": "seoul.go.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 225600,
+			"friends_count": 49476,
+			"listed_count": 1675,
+			"created_at": "Tue Dec 01 05:10:38 +0000 2009",
+			"favourites_count": 1276,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 27143,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/990778005403328512/4FGyiKmv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/990778005403328512/4FGyiKmv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/93794912/1580703588",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 96,
+		"favorite_count": 49,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "코로나19 확산 방지를 위해 서울광장·청계광장·광화문광장에서의 집회 금지, 위반시 300만원 이하 벌금이 부과됩니다.(감염병예방법 제49조: 감염병 예방을 위해 도심 내 여러 사람의 집합 제한·금지)\n\n또한 서울소재 신천지교회 폐쇄 및 방역실시, 추후 안전이 확인되면 예배활동을 허용할 예정입니다 https://t.co/Rcf59dOAHC"
+	},
+	{
 		"created_at": "Fri Feb 21 04:35:24 +0000 2020",
 		"id": 1230711108488421400,
 		"id_str": "1230712600045834240",
