ff2af19b57dfb005bf9907701aea77e563cd27e4
Youngmin Lee <iameyoungmin@gmail.com>
2020-03-06 17:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 06 07:40:38 +0000 2020",
+		"id": 1235829987980112000,
+		"id_str": "1235832646950735872",
+		"full_text": "오늘 오후 5시(KST) 뮤직뱅크에서 #이달의소녀 의 #SoWhat 무대 놓치지 마세요! 오빛을 향한 강렬한 마음을 BURN🔥\n\n#LOONA https://t.co/dhMtApqYjy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						21,
+						27
+					]
+				},
+				{
+					"text": "SoWhat",
+					"indices": [
+						30,
+						37
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						72,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1235829978622644200,
+					"id_str": "1235829978622644227",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/ESaM0V6U0AMfDqS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESaM0V6U0AMfDqS.jpg",
+					"url": "https://t.co/dhMtApqYjy",
+					"display_url": "pic.twitter.com/dhMtApqYjy",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1235829987980111873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1235829978622644200,
+					"id_str": "1235829978622644227",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/ESaM0V6U0AMfDqS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESaM0V6U0AMfDqS.jpg",
+					"url": "https://t.co/dhMtApqYjy",
+					"display_url": "pic.twitter.com/dhMtApqYjy",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1235829987980111873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1235829978631000000,
+					"id_str": "1235829978631000073",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/ESaM0V8UUAkZt8q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESaM0V8UUAkZt8q.jpg",
+					"url": "https://t.co/dhMtApqYjy",
+					"display_url": "pic.twitter.com/dhMtApqYjy",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1235829987980111873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 431036,
+			"friends_count": 0,
+			"listed_count": 6020,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1842,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2511,
+		"favorite_count": 5682,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오늘 오후 5시(KST) 뮤직뱅크에서 #이달의소녀 의 #SoWhat 무대 놓치지 마세요! 오빛을 향한 강렬한 마음을 BURN🔥\n\n#LOONA https://t.co/dhMtApqYjy"
+	},
+	{
 		"created_at": "Fri Mar 06 06:20:18 +0000 2020",
 		"id": 1235762148296421400,
 		"id_str": "1235812429096865794",
