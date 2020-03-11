c473d33012c3ab914b57ab1092f8626c93e4ccdb
Youngmin Lee <iameyoungmin@gmail.com>
2020-03-11 22:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 54943,
-	"created_at": "Wed Mar 11 12:02:06 +0000 2020",
+	"tweets": 54944,
+	"created_at": "Wed Mar 11 13:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 11 12:24:32 +0000 2020",
+		"id": 1237709511013453800,
+		"id_str": "1237716031721914373",
+		"full_text": "D-4 \"Beautiful Creature\"\n2020.03.15 12PM (KST)\n\nPhoto by kimmoondog\nH&amp;M by xoxov3\nStyling by westsilver_m\nThx to Eyewear by godsomware\n\nProduced by yeseoisone\nComposed by yeseoisone\nArranged by yeseoisone\nLyrics by yeseoisone\n\nMixed &amp; Mastered by imlaykid\nDistribution by poclanos https://t.co/O16EVCCccW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1237709502658437000,
+					"id_str": "1237709502658437120",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/ES06PCkU4AACABe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ES06PCkU4AACABe.jpg",
+					"url": "https://t.co/O16EVCCccW",
+					"display_url": "pic.twitter.com/O16EVCCccW",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1237709511013453831/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1539,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 902,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1237709502658437000,
+					"id_str": "1237709502658437120",
+					"indices": [
+						289,
+						312
+					],
+					"media_url": "http://pbs.twimg.com/media/ES06PCkU4AACABe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ES06PCkU4AACABe.jpg",
+					"url": "https://t.co/O16EVCCccW",
+					"display_url": "pic.twitter.com/O16EVCCccW",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1237709511013453831/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 511,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1539,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 902,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 893011871095181300,
+			"id_str": "893011871095181312",
+			"name": "YESEO",
+			"screen_name": "yeseoofficial",
+			"location": "대한민국 서울",
+			"description": "Producer / Singer",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 532,
+			"friends_count": 2,
+			"listed_count": 9,
+			"created_at": "Thu Aug 03 07:33:16 +0000 2017",
+			"favourites_count": 40,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 54,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1026823160602095619/8gD_B9Xv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1026823160602095619/8gD_B9Xv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/893011871095181312/1533648695",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 20,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "D-4 \"Beautiful Creature\"\n2020.03.15 12PM (KST)\n\nPhoto by kimmoondog\nH&amp;M by xoxov3\nStyling by westsilver_m\nThx to Eyewear by godsomware\n\nProduced by yeseoisone\nComposed by yeseoisone\nArranged by yeseoisone\nLyrics by yeseoisone\n\nMixed &amp; Mastered by imlaykid\nDistribution by poclanos https://t.co/O16EVCCccW"
+	},
+	{
 		"created_at": "Wed Mar 11 11:24:32 +0000 2020",
 		"id": 1237687250537726000,
 		"id_str": "1237700931413856256",
