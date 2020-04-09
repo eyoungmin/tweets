589f4144016ab46be05e2930c32284106db25336
Youngmin Lee <iameyoungmin@gmail.com>
2020-04-10 00:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56457,
-	"created_at": "Thu Apr 09 12:02:09 +0000 2020",
+	"tweets": 56458,
+	"created_at": "Thu Apr 09 15:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,98 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 09 14:24:31 +0000 2020",
+		"id": 1248251556870447000,
+		"id_str": "1248255477277954049",
+		"full_text": "카톡 해킹사건!! CF출연 위약금은??\nhttps://t.co/7feRQRFpAY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7feRQRFpAY",
+					"expanded_url": "https://youtu.be/G6cOr7Nmikc",
+					"display_url": "youtu.be/G6cOr7Nmikc",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2999581440,
+			"id_str": "2999581440",
+			"name": "연투유TV",
+			"screen_name": "yeontoyou",
+			"location": "",
+			"description": "연법상 - 연예계 법률 상담소______\n*한국음악레이블산업협회_ 윤동환 부회장*_________\n*법무법인 대진_ 김민성 변호사*",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 276,
+			"friends_count": 249,
+			"listed_count": 2,
+			"created_at": "Wed Jan 28 17:09:04 +0000 2015",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1247462092187164673/mHSgyo69_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1247462092187164673/mHSgyo69_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "카톡 해킹사건!! CF출연 위약금은??\nhttps://t.co/7feRQRFpAY"
+	},
+	{
 		"created_at": "Thu Apr 09 11:24:32 +0000 2020",
 		"id": 1248196474384482300,
 		"id_str": "1248210179507789829",
