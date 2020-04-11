02dd8b4bee5f1d345d55bffe9dcfd31c4641bc1f
Youngmin Lee <iameyoungmin@gmail.com>
2020-04-12 00:02:09 +0900

    Added 1 tweets

--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 11 14:24:39 +0000 2020",
+		"id": 1248952815340245000,
+		"id_str": "1248980284445343749",
+		"full_text": "나는 나중에 유명해져도 절대 저런 행동을 하지 말아야지 https://t.co/LAuMF9W3QC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1248952663384809500,
+					"id_str": "1248952663384809472",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1248952663384809472/pu/img/EWyJv5zYJY4BbMCf.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1248952663384809472/pu/img/EWyJv5zYJY4BbMCf.jpg",
+					"url": "https://t.co/LAuMF9W3QC",
+					"display_url": "pic.twitter.com/LAuMF9W3QC",
+					"expanded_url": "https://twitter.com/taengbang/status/1248952815340244995/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1248952663384809500,
+					"id_str": "1248952663384809472",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1248952663384809472/pu/img/EWyJv5zYJY4BbMCf.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1248952663384809472/pu/img/EWyJv5zYJY4BbMCf.jpg",
+					"url": "https://t.co/LAuMF9W3QC",
+					"display_url": "pic.twitter.com/LAuMF9W3QC",
+					"expanded_url": "https://twitter.com/taengbang/status/1248952815340244995/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 44535,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1248952663384809472/pu/vid/1280x720/q1N_M7RcCpedGOgC.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1248952663384809472/pu/pl/Svxd5qjMshavAGIn.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1248952663384809472/pu/vid/480x270/8fVqaUpUOOIqjkEd.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1248952663384809472/pu/vid/640x360/8z-Wt1YpX5TWRlzV.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 756331488,
+			"id_str": "756331488",
+			"name": "ตʕ•ᴥ•ʔต",
+			"screen_name": "taengbang",
+			"location": "",
+			"description": "Archive",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18941,
+			"friends_count": 444,
+			"listed_count": 70,
+			"created_at": "Tue Aug 14 02:57:50 +0000 2012",
+			"favourites_count": 25115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21618,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1239124677861109760/KPc5jUoF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1239124677861109760/KPc5jUoF_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8547,
+		"favorite_count": 3158,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "나는 나중에 유명해져도 절대 저런 행동을 하지 말아야지 https://t.co/LAuMF9W3QC"
+	},
+	{
 		"created_at": "Sat Apr 11 13:37:21 +0000 2020",
 		"id": 1248968380083822600,
 		"id_str": "1248968380083822594",
