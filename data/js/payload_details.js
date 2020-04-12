102c6828011dc4c2d9d0a5d415699bae74e1f3cd
Youngmin Lee <iameyoungmin@gmail.com>
2020-04-13 01:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56587,
-	"created_at": "Sun Apr 12 15:19:58 +0000 2020",
+	"tweets": 56588,
+	"created_at": "Sun Apr 12 16:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sun Apr 12 15:34:37 +0000 2020",
+		"id": 1249351618455171000,
+		"id_str": "1249360281844604929",
+		"full_text": "#BTS OFFICIAL LIGHT STICK MAP OF THE SOUL SPECIAL EDITION Teaser https://t.co/Gf7SwoHJgS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BTS",
+					"indices": [
+						0,
+						4
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1249351591762612200,
+					"id_str": "1249351591762612224",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1249351591762612224/pu/img/q7anJ5RnVmEKT-mr.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1249351591762612224/pu/img/q7anJ5RnVmEKT-mr.jpg",
+					"url": "https://t.co/Gf7SwoHJgS",
+					"display_url": "pic.twitter.com/Gf7SwoHJgS",
+					"expanded_url": "https://twitter.com/bighit_merch/status/1249351618455171074/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1249351591762612200,
+					"id_str": "1249351591762612224",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1249351591762612224/pu/img/q7anJ5RnVmEKT-mr.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1249351591762612224/pu/img/q7anJ5RnVmEKT-mr.jpg",
+					"url": "https://t.co/Gf7SwoHJgS",
+					"display_url": "pic.twitter.com/Gf7SwoHJgS",
+					"expanded_url": "https://twitter.com/bighit_merch/status/1249351618455171074/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 14891,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1249351591762612224/pu/vid/480x270/5r_hm0_Da2KEOrDG.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1249351591762612224/pu/pl/PhKptiuE8woZwxgD.m3u8?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1249351591762612224/pu/vid/1280x720/kPc31qdtmIhY4tkf.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1249351591762612224/pu/vid/640x360/_kzuJvGX4PvG6P54.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1126749496082223100,
+			"id_str": "1126749496082223104",
+			"name": "BIGHIT OFFICIAL MERCH",
+			"screen_name": "bighit_merch",
+			"location": "대한민국",
+			"description": "All about the OFFICIAL MERCH from Big Hit Entertainment & More!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1133641,
+			"friends_count": 7,
+			"listed_count": 1572,
+			"created_at": "Fri May 10 07:23:08 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 184,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1164459698055024640/vI9kJtji_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164459698055024640/vI9kJtji_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1126749496082223104/1566463802",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 100246,
+		"favorite_count": 248865,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BTS OFFICIAL LIGHT STICK MAP OF THE SOUL SPECIAL EDITION Teaser https://t.co/Gf7SwoHJgS"
+	},
+	{
 		"created_at": "Sun Apr 12 15:18:14 +0000 2020",
 		"id": 1249356156272668700,
 		"id_str": "1249356156272668674",
