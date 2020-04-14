a8703a2edf85bc08a0623be903fffcb1ca382916
Youngmin Lee <iameyoungmin@gmail.com>
2020-04-14 21:02:09 +0900

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_04.js",
 		"year": 2020,
 		"var_name": "tweets_2020_04",
-		"tweet_count": 696,
+		"tweet_count": 697,
 		"month": 4
 	},
 	{
