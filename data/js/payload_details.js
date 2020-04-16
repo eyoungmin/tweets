b3e7c5ffb42753162c442e9022186fe87772bad3
Youngmin Lee <iameyoungmin@gmail.com>
2020-04-16 09:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 56675,
-	"created_at": "Wed Apr 15 21:02:43 +0000 2020",
+	"tweets": 56676,
+	"created_at": "Thu Apr 16 00:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Wed Apr 15 23:54:03 +0000 2020",
+		"id": 1250573129052758000,
+		"id_str": "1250573129052758016",
+		"full_text": "#EO\n해당되는 대표님들 꼭 신청하세요\nhttps://t.co/YOMLb4RBgA\n#스타트업 #태용 #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EO",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "스타트업",
+					"indices": [
+						46,
+						51
+					]
+				},
+				{
+					"text": "태용",
+					"indices": [
+						52,
+						55
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						56,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YOMLb4RBgA",
+					"expanded_url": "https://youtu.be/Ko5GF9BSDvw",
+					"display_url": "youtu.be/Ko5GF9BSDvw",
+					"indices": [
+						22,
+						45
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY @IDOLUV_me\n#예술경영 #뮤직비지니스 @ArtManagementKR\n#티켓오픈 @TicketOpen\n\n코로나19 관련 정보\nhttps://t.co/k5i92GDyAm",
+			"url": "https://t.co/pEO2GdCFpF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2GdCFpF",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/k5i92GDyAm",
+							"expanded_url": "http://bit.ly/COVID-19-KOREA",
+							"display_url": "bit.ly/COVID-19-KOREA",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 297,
+			"friends_count": 914,
+			"listed_count": 9,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 39139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 57760,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#EO\n해당되는 대표님들 꼭 신청하세요\nhttps://t.co/YOMLb4RBgA\n#스타트업 #태용 #minfav"
+	},
+	{
 		"created_at": "Wed Apr 15 20:20:08 +0000 2020",
 		"id": 1250519298121973800,
 		"id_str": "1250519298121973761",
@@ -322,7 +467,7 @@ Grailbird.data.tweets_2020_04 =
 		"created_at": "Wed Apr 15 20:17:18 +0000 2020",
 		"id": 1250409943787876400,
 		"id_str": "1250518581697105920",
-		"full_text": "[INSTAGRAM CAROUSEL] yoona__lim: The best homemade pizza bread ever made😝\n#피자빵 👩🏻‍🍳 https://t.co/JxBN8JqQGZ (1P+1V) #yoongstagram https://t.co/FdhCgZPduM",
+		"full_text": "[INSTAGRAM CAROUSEL] yoona__lim: The best homemade pizza bread ever made😝\n#피자빵 👩🏻🍳 https://t.co/JxBN8JqQGZ (1P+1V) #yoongstagram https://t.co/FdhCgZPduM",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -545,7 +690,7 @@ Grailbird.data.tweets_2020_04 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "[INSTAGRAM CAROUSEL] yoona__lim: The best homemade pizza bread ever made😝\n#피자빵 👩🏻‍🍳 https://t.co/JxBN8JqQGZ (1P+1V) #yoongstagram https://t.co/FdhCgZPduM"
+		"text": "[INSTAGRAM CAROUSEL] yoona__lim: The best homemade pizza bread ever made😝\n#피자빵 👩🏻🍳 https://t.co/JxBN8JqQGZ (1P+1V) #yoongstagram https://t.co/FdhCgZPduM"
 	},
 	{
 		"created_at": "Wed Apr 15 20:16:53 +0000 2020",
