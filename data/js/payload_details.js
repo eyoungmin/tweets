7c19426a7c1f26d4958ee880879a813dd5389d76
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-06 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57691,
-	"created_at": "Wed May 06 04:02:03 +0000 2020",
+	"tweets": 57692,
+	"created_at": "Wed May 06 06:02:03 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Wed May 06 05:38:08 +0000 2020",
+		"id": 1257907477812842500,
+		"id_str": "1257907477812842497",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n美 포브스 “블랙핑크, 6월 컴백+레이디 가가와 협업 기대…최고의 성과 낼 것”\nhttps://t.co/H3285GujLw\n#블랙핑크 #BLACKPINK https://t.co/NMNCncvRka",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						86,
+						91
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						92,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/H3285GujLw",
+					"expanded_url": "https://yg-life.com/archives/154841?lang=ko",
+					"display_url": "yg-life.com/archives/15484…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1257907476617461800,
+					"id_str": "1257907476617461761",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EXT8L3AXgAEl8Fr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXT8L3AXgAEl8Fr.jpg",
+					"url": "https://t.co/NMNCncvRka",
+					"display_url": "pic.twitter.com/NMNCncvRka",
+					"expanded_url": "https://twitter.com/fromminn/status/1257907477812842497/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1257907476617461800,
+					"id_str": "1257907476617461761",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EXT8L3AXgAEl8Fr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXT8L3AXgAEl8Fr.jpg",
+					"url": "https://t.co/NMNCncvRka",
+					"display_url": "pic.twitter.com/NMNCncvRka",
+					"expanded_url": "https://twitter.com/fromminn/status/1257907477812842497/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 587,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY @IDOLUV_me\n#예술경영 #뮤직비지니스 @ArtManagementKR\n#티켓오픈 @TicketOpen\n\n코로나19 관련 정보\nhttps://t.co/k5i92GDyAm\n\nRetweets & Favorites have various meanings.",
+			"url": "https://t.co/pEO2GdCFpF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2GdCFpF",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/k5i92GDyAm",
+							"expanded_url": "http://bit.ly/COVID-19-KOREA",
+							"display_url": "bit.ly/COVID-19-KOREA",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 299,
+			"friends_count": 925,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 39926,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 58829,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n美 포브스 “블랙핑크, 6월 컴백+레이디 가가와 협업 기대…최고의 성과 낼 것”\nhttps://t.co/H3285GujLw\n#블랙핑크 #BLACKPINK https://t.co/NMNCncvRka"
+	},
+	{
 		"created_at": "Wed May 06 03:52:13 +0000 2020",
 		"id": 1257868042924511200,
 		"id_str": "1257880823153545222",
