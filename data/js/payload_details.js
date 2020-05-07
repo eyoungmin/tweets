bc4c189e45d3c071b0235f2c411ab641ea2ed53c
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-07 15:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57761,
-	"created_at": "Thu May 07 05:02:03 +0000 2020",
+	"tweets": 57762,
+	"created_at": "Thu May 07 06:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 07 05:52:13 +0000 2020",
+		"id": 1258271435123900400,
+		"id_str": "1258273410720899073",
+		"full_text": "✔️\n\n#청하 #CHUNGHA\n#돌체앤가바나뷰티 #DGBeauty \n#BEAQUEEN @dolcegabbana https://t.co/LYbi1dedGr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						4,
+						7
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						8,
+						16
+					]
+				},
+				{
+					"text": "돌체앤가바나뷰티",
+					"indices": [
+						17,
+						26
+					]
+				},
+				{
+					"text": "DGBeauty",
+					"indices": [
+						27,
+						36
+					]
+				},
+				{
+					"text": "BEAQUEEN",
+					"indices": [
+						38,
+						47
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dolcegabbana",
+					"name": "Dolce & Gabbana",
+					"id": 85772748,
+					"id_str": "85772748",
+					"indices": [
+						48,
+						61
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1258271423568609300,
+					"id_str": "1258271423568609280",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EXZHMXDVAAA1mSk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXZHMXDVAAA1mSk.jpg",
+					"url": "https://t.co/LYbi1dedGr",
+					"display_url": "pic.twitter.com/LYbi1dedGr",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1258271435123900416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1258271423568609300,
+					"id_str": "1258271423568609280",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EXZHMXDVAAA1mSk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXZHMXDVAAA1mSk.jpg",
+					"url": "https://t.co/LYbi1dedGr",
+					"display_url": "pic.twitter.com/LYbi1dedGr",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1258271435123900416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1258271423581192200,
+					"id_str": "1258271423581192193",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EXZHMXGVAAEIH1X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXZHMXGVAAEIH1X.jpg",
+					"url": "https://t.co/LYbi1dedGr",
+					"display_url": "pic.twitter.com/LYbi1dedGr",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1258271435123900416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1258271423606354000,
+					"id_str": "1258271423606353925",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EXZHMXMU8AU4cAe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXZHMXMU8AU4cAe.jpg",
+					"url": "https://t.co/LYbi1dedGr",
+					"display_url": "pic.twitter.com/LYbi1dedGr",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1258271435123900416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1258271423606386700,
+					"id_str": "1258271423606386688",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/EXZHMXMVcAAFN48.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXZHMXMVcAAFN48.jpg",
+					"url": "https://t.co/LYbi1dedGr",
+					"display_url": "pic.twitter.com/LYbi1dedGr",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1258271435123900416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 748,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter 💚💙💜",
+			"url": "https://t.co/reTg51lvxv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/reTg51lvxv",
+							"expanded_url": "http://cafe.daum.net/MNH-Chungha",
+							"display_url": "cafe.daum.net/MNH-Chungha",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 257862,
+			"friends_count": 0,
+			"listed_count": 1955,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 906,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1249351708418781184/E8VsR7J1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1249351708418781184/E8VsR7J1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1588155512",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1205,
+		"favorite_count": 4042,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "✔️\n\n#청하 #CHUNGHA\n#돌체앤가바나뷰티 #DGBeauty \n#BEAQUEEN @dolcegabbana https://t.co/LYbi1dedGr"
+	},
+	{
 		"created_at": "Thu May 07 04:45:37 +0000 2020",
 		"id": 1258246036285350000,
 		"id_str": "1258256648965160961",
