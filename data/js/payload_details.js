0a382d17f1c680839346a8e8bc51c18cb541cbfb
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-10 00:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57913,
-	"created_at": "Sat May 09 14:02:05 +0000 2020",
+	"tweets": 57914,
+	"created_at": "Sat May 09 15:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 09 14:04:02 +0000 2020",
+		"id": 1258984339158659000,
+		"id_str": "1259121956516802560",
+		"full_text": "['난춘(亂春)' Commentary D-1]\n어지러운 봄, 그 역설을 담은 지금 우리 시대의 노래 ‘난춘(亂春)’\n\nhttps://t.co/9XoqZKENva - @YouTube https://t.co/CMWSUfBfHm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						92,
+						100
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9XoqZKENva",
+					"expanded_url": "https://youtu.be/JB3PHVhW6WE",
+					"display_url": "youtu.be/JB3PHVhW6WE",
+					"indices": [
+						66,
+						89
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1258984330761605000,
+					"id_str": "1258984330761605120",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EXjPlBTUEAA6R14.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXjPlBTUEAA6R14.jpg",
+					"url": "https://t.co/CMWSUfBfHm",
+					"display_url": "pic.twitter.com/CMWSUfBfHm",
+					"expanded_url": "https://twitter.com/se_so_neon/status/1258984339158659075/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 425,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 750,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1258984330761605000,
+					"id_str": "1258984330761605120",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/EXjPlBTUEAA6R14.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXjPlBTUEAA6R14.jpg",
+					"url": "https://t.co/CMWSUfBfHm",
+					"display_url": "pic.twitter.com/CMWSUfBfHm",
+					"expanded_url": "https://twitter.com/se_so_neon/status/1258984339158659075/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 425,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 750,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 718392164741472300,
+			"id_str": "718392164741472256",
+			"name": "새소년 SE SO NEON",
+			"screen_name": "se_so_neon",
+			"location": "대한민국 서울",
+			"description": "세계적인 밴드 새소년 🐤 Band SE SO NEON = Soyoon (v/g) / Usu (dr) / Park Hyunjin (b)\n\nnew EP \"비적응 (Nonadaptation)\"\nreleased on 2020. 02. 18. TUE 6PM KST",
+			"url": "https://t.co/SaQppDidSE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SaQppDidSE",
+							"expanded_url": "http://www.instagram.com/se_so_neon",
+							"display_url": "instagram.com/se_so_neon",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8741,
+			"friends_count": 15,
+			"listed_count": 54,
+			"created_at": "Fri Apr 08 10:56:53 +0000 2016",
+			"favourites_count": 327,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1340,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1230785072812314625/frCUDIe2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1230785072812314625/frCUDIe2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/718392164741472256/1581572779",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 128,
+		"favorite_count": 146,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "zh",
+		"text": "['난춘(亂春)' Commentary D-1]\n어지러운 봄, 그 역설을 담은 지금 우리 시대의 노래 ‘난춘(亂春)’\n\nhttps://t.co/9XoqZKENva - @YouTube https://t.co/CMWSUfBfHm"
+	},
+	{
 		"created_at": "Sat May 09 13:57:16 +0000 2020",
 		"id": 1259045444610519000,
 		"id_str": "1259120253021204481",
