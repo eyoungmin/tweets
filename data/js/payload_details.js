79d12e1a1dc8d76d49ebdbb8a165a2257933bacc
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-10 18:02:06 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57933,
-	"created_at": "Sun May 10 07:02:05 +0000 2020",
+	"tweets": 57936,
+	"created_at": "Sun May 10 09:02:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,440 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 10 08:36:43 +0000 2020",
+		"id": 1259347960715866000,
+		"id_str": "1259401974455230464",
+		"full_text": "최근 수 조원을 대출 받은데 이어 며칠 전 25%의 인원 감축을 발표한 에어비앤비. 창업자 브라이언 체스키는 이를 이겨낼 수 있을까요? 그는 긍정적이고 사려 깊은 리더지만, 종종 냉정하지 못하고 우유부단하다는 평을 받습니다. 특히 이런 위기 상황에서 말이죠.\n\nhttps://t.co/1yIZRtjAls",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1yIZRtjAls",
+					"expanded_url": "https://ebadak.news/2020/05/10/can-brian-chesky-save-airbnb/",
+					"display_url": "ebadak.news/2020/05/10/can…",
+					"indices": [
+						146,
+						169
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1114700851778449400,
+			"id_str": "1114700851778449408",
+			"name": "이바닥늬우스",
+			"screen_name": "ebadaknews",
+			"location": "",
+			"description": "찰지고 신나는 테크바닥 늬우스",
+			"url": "https://t.co/L0rYnRELas",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/L0rYnRELas",
+							"expanded_url": "https://ebadak.news",
+							"display_url": "ebadak.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 663,
+			"friends_count": 0,
+			"listed_count": 18,
+			"created_at": "Sun Apr 07 01:26:07 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1114700932934029313/VXZnypQZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1114700932934029313/VXZnypQZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1114700851778449408/1555415622",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "최근 수 조원을 대출 받은데 이어 며칠 전 25%의 인원 감축을 발표한 에어비앤비. 창업자 브라이언 체스키는 이를 이겨낼 수 있을까요? 그는 긍정적이고 사려 깊은 리더지만, 종종 냉정하지 못하고 우유부단하다는 평을 받습니다. 특히 이런 위기 상황에서 말이죠.\n\nhttps://t.co/1yIZRtjAls"
+	},
+	{
+		"created_at": "Sun May 10 08:36:11 +0000 2020",
+		"id": 1259345429650686000,
+		"id_str": "1259401838320730112",
+		"full_text": "[INSTAGRAM] tiffanyyoungofficial: 코로나19를 마주한 몇주는 혼란스럽고 무기력했지만, 어느날부터인가 아침에 눈을 뜨면서 감사와 희망으로 하루를 시작하게 되었습니다. 매일 아침 뉴스를 통해 코로나19의 최전선에서 싸워주고 계신 진정한 영웅들의 모습을 … https://t.co/aeA871TPqt https://t.co/Xi91ZHIK56",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/aeA871TPqt",
+					"expanded_url": "https://www.instagram.com/p/B__p0t7llx0/",
+					"display_url": "instagram.com/p/B__p0t7llx0/",
+					"indices": [
+						156,
+						179
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1259345427880697900,
+					"id_str": "1259345427880697858",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EXoX_o5XYAIkmJV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXoX_o5XYAIkmJV.jpg",
+					"url": "https://t.co/Xi91ZHIK56",
+					"display_url": "pic.twitter.com/Xi91ZHIK56",
+					"expanded_url": "https://twitter.com/miyoungstagram/status/1259345429650685953/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1259345427880697900,
+					"id_str": "1259345427880697858",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EXoX_o5XYAIkmJV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EXoX_o5XYAIkmJV.jpg",
+					"url": "https://t.co/Xi91ZHIK56",
+					"display_url": "pic.twitter.com/Xi91ZHIK56",
+					"expanded_url": "https://twitter.com/miyoungstagram/status/1259345429650685953/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://fany.soshistagram.com\" rel=\"nofollow\">Miyoungstagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1516769468,
+			"id_str": "1516769468",
+			"name": "Miyoungstagram",
+			"screen_name": "miyoungstagram",
+			"location": "A very pink phone",
+			"description": "Bot tweeting Tiffany's updates and comments on Instagram. Built by @thegoguma. Also @yulstagrm @yoongstagrm @seostagrm @syoungstagram @sicafeed.",
+			"url": "https://t.co/iWbaVsatxK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iWbaVsatxK",
+							"expanded_url": "https://myoungstagram.tumblr.com/",
+							"display_url": "myoungstagram.tumblr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58048,
+			"friends_count": 0,
+			"listed_count": 925,
+			"created_at": "Fri Jun 14 15:32:20 +0000 2013",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6824,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/512030234263031808/jUzO93D1_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/512030234263031808/jUzO93D1_normal.png",
+			"profile_link_color": "ED5084",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3009,
+		"favorite_count": 2555,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[INSTAGRAM] tiffanyyoungofficial: 코로나19를 마주한 몇주는 혼란스럽고 무기력했지만, 어느날부터인가 아침에 눈을 뜨면서 감사와 희망으로 하루를 시작하게 되었습니다. 매일 아침 뉴스를 통해 코로나19의 최전선에서 싸워주고 계신 진정한 영웅들의 모습을 … https://t.co/aeA871TPqt https://t.co/Xi91ZHIK56"
+	},
+	{
+		"created_at": "Sun May 10 08:26:05 +0000 2020",
+		"id": 1259399297679929300,
+		"id_str": "1259399297679929345",
+		"full_text": "#BBC코리아\n코로나19: 땅으로 내려온 조종사들\nhttps://t.co/xX1p1ZsviJ\n#코로나바이러스감염증19 #코로나19 #COVID19",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BBC코리아",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "코로나바이러스감염증19",
+					"indices": [
+						52,
+						65
+					]
+				},
+				{
+					"text": "코로나19",
+					"indices": [
+						66,
+						72
+					]
+				},
+				{
+					"text": "COVID19",
+					"indices": [
+						73,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xX1p1ZsviJ",
+					"expanded_url": "https://www.bbc.com/korean/news-52605053",
+					"display_url": "bbc.com/korean/news-52…",
+					"indices": [
+						28,
+						51
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY @IDOLUV_me\n#예술경영 #뮤직비지니스 @ArtManagementKR\n#티켓오픈 @TicketOpen\n\n코로나19 관련 정보\nhttps://t.co/k5i92GDyAm\n\nRetweets & Favorites have various meanings.",
+			"url": "https://t.co/pEO2GdCFpF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2GdCFpF",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/k5i92GDyAm",
+							"expanded_url": "http://bit.ly/COVID-19-KOREA",
+							"display_url": "bit.ly/COVID-19-KOREA",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 300,
+			"friends_count": 927,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 40080,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59104,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#BBC코리아\n코로나19: 땅으로 내려온 조종사들\nhttps://t.co/xX1p1ZsviJ\n#코로나바이러스감염증19 #코로나19 #COVID19"
+	},
+	{
 		"created_at": "Sun May 10 06:56:29 +0000 2020",
 		"id": 1259376746392100900,
 		"id_str": "1259376746392100865",
