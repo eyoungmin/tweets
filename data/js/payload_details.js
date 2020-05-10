54919d2b1f8137aae401ba455acf7fded6e8eed8
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-10 22:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 57953,
-	"created_at": "Sun May 10 12:02:04 +0000 2020",
+	"tweets": 57954,
+	"created_at": "Sun May 10 13:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,158 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 10 12:48:24 +0000 2020",
+		"id": 1259465311683821600,
+		"id_str": "1259465311683821570",
+		"full_text": "#대한민국 #보건복지부\n[요약] 5월10일13시40분│이태원 클럽 관련 확진자 54명 발생, 방문자 역학조사 진행 중│정부브리핑\nhttps://t.co/QN7gdi3pxc\n#코로나바이러스감염증19 #코로나19 #COVID19",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "대한민국",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "보건복지부",
+					"indices": [
+						6,
+						12
+					]
+				},
+				{
+					"text": "코로나바이러스감염증19",
+					"indices": [
+						96,
+						109
+					]
+				},
+				{
+					"text": "코로나19",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "COVID19",
+					"indices": [
+						117,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QN7gdi3pxc",
+					"expanded_url": "https://www.youtube.com/watch?v=xuWWYVKsGEQ",
+					"display_url": "youtube.com/watch?v=xuWWYV…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Founder @SOLPLPARTY @IDOLUV_me\n#예술경영 #뮤직비지니스 @ArtManagementKR\n#티켓오픈 @TicketOpen\n\n코로나19 관련 정보\nhttps://t.co/k5i92GDyAm\n\nRetweets & Favorites have various meanings.",
+			"url": "https://t.co/pEO2GdCFpF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pEO2GdCFpF",
+							"expanded_url": "http://about.solplparty.com",
+							"display_url": "about.solplparty.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/k5i92GDyAm",
+							"expanded_url": "http://bit.ly/COVID-19-KOREA",
+							"display_url": "bit.ly/COVID-19-KOREA",
+							"indices": [
+								93,
+								116
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 300,
+			"friends_count": 927,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 40096,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 59123,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1464571598",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#대한민국 #보건복지부\n[요약] 5월10일13시40분│이태원 클럽 관련 확진자 54명 발생, 방문자 역학조사 진행 중│정부브리핑\nhttps://t.co/QN7gdi3pxc\n#코로나바이러스감염증19 #코로나19 #COVID19"
+	},
+	{
 		"created_at": "Sun May 10 11:52:22 +0000 2020",
 		"id": 1259402582142914600,
 		"id_str": "1259451208432906242",
@@ -900,7 +1052,7 @@ Grailbird.data.tweets_2020_05 =
 			"name": "정소연",
 			"screen_name": "sy876",
 			"location": "여의도, 서울",
-			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️‍🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
+			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
 			"url": "https://t.co/p43kC6e82H",
 			"entities": {
 				"url": {
@@ -995,7 +1147,7 @@ Grailbird.data.tweets_2020_05 =
 			"name": "정소연",
 			"screen_name": "sy876",
 			"location": "여의도, 서울",
-			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️‍🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
+			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
 			"url": "https://t.co/p43kC6e82H",
 			"entities": {
 				"url": {
@@ -1090,7 +1242,7 @@ Grailbird.data.tweets_2020_05 =
 			"name": "정소연",
 			"screen_name": "sy876",
 			"location": "여의도, 서울",
-			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️‍🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
+			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
 			"url": "https://t.co/p43kC6e82H",
 			"entities": {
 				"url": {
@@ -1185,7 +1337,7 @@ Grailbird.data.tweets_2020_05 =
 			"name": "정소연",
 			"screen_name": "sy876",
 			"location": "여의도, 서울",
-			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️‍🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
+			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
 			"url": "https://t.co/p43kC6e82H",
 			"entities": {
 				"url": {
@@ -1280,7 +1432,7 @@ Grailbird.data.tweets_2020_05 =
 			"name": "정소연",
 			"screen_name": "sy876",
 			"location": "여의도, 서울",
-			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️‍🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
+			"description": "변호사. SF작가・번역가. 여기에는 제가 쓰고 싶은 글을 씁니다. // 온라인 법률상담 X // 상담문의 02-780-0328 //🏳️🌈// 마이붐:뉴이스트(렌) // instagram: sfwriterjeong (EN)",
 			"url": "https://t.co/p43kC6e82H",
 			"entities": {
 				"url": {
