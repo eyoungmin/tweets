3471843c446f3ff0c4f03a18b3e3d4e5761536b6
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-14 21:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58207,
-	"created_at": "Thu May 14 11:02:11 +0000 2020",
+	"tweets": 58208,
+	"created_at": "Thu May 14 12:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 14 11:52:19 +0000 2020",
+		"id": 1260888018816077800,
+		"id_str": "1260900747991855105",
+		"full_text": "✔️\n\n#청하 #CHUNGHA\n#제3기획 #제일기획 #초콩잼 https://t.co/IW6GlKTBYf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			33
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						4,
+						7
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						8,
+						16
+					]
+				},
+				{
+					"text": "제3기획",
+					"indices": [
+						17,
+						22
+					]
+				},
+				{
+					"text": "제일기획",
+					"indices": [
+						23,
+						28
+					]
+				},
+				{
+					"text": "초콩잼",
+					"indices": [
+						29,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1260887941263458300,
+					"id_str": "1260887941263458304",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-S5vCVAAAA2iN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-S5vCVAAAA2iN.jpg",
+					"url": "https://t.co/IW6GlKTBYf",
+					"display_url": "pic.twitter.com/IW6GlKTBYf",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1260888018816077824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1260887941263458300,
+					"id_str": "1260887941263458304",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-S5vCVAAAA2iN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-S5vCVAAAA2iN.jpg",
+					"url": "https://t.co/IW6GlKTBYf",
+					"display_url": "pic.twitter.com/IW6GlKTBYf",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1260888018816077824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1260887941284368400,
+					"id_str": "1260887941284368385",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-S5vHUEAEp3pQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-S5vHUEAEp3pQ.jpg",
+					"url": "https://t.co/IW6GlKTBYf",
+					"display_url": "pic.twitter.com/IW6GlKTBYf",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1260888018816077824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1260887941284376600,
+					"id_str": "1260887941284376584",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/media/EX-S5vHUMAgeBWc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX-S5vHUMAgeBWc.jpg",
+					"url": "https://t.co/IW6GlKTBYf",
+					"display_url": "pic.twitter.com/IW6GlKTBYf",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1260888018816077824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter 💚💙💜",
+			"url": "https://t.co/reTg51lvxv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/reTg51lvxv",
+							"expanded_url": "http://cafe.daum.net/MNH-Chungha",
+							"display_url": "cafe.daum.net/MNH-Chungha",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 259655,
+			"friends_count": 0,
+			"listed_count": 1590,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 918,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1249351708418781184/E8VsR7J1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1249351708418781184/E8VsR7J1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1588155512",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 855,
+		"favorite_count": 4162,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "✔️\n\n#청하 #CHUNGHA\n#제3기획 #제일기획 #초콩잼 https://t.co/IW6GlKTBYf"
+	},
+	{
 		"created_at": "Thu May 14 10:52:35 +0000 2020",
 		"id": 1260876084288999400,
 		"id_str": "1260885714218213379",
