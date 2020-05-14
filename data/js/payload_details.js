2ed30c64e00a25d754947865f4341c243811dba8
Youngmin Lee <iameyoungmin@gmail.com>
2020-05-14 16:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 58200,
-	"created_at": "Thu May 14 06:02:10 +0000 2020",
+	"tweets": 58201,
+	"created_at": "Thu May 14 07:02:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 14 06:52:12 +0000 2020",
+		"id": 1260811401594986500,
+		"id_str": "1260825223391252481",
+		"full_text": "슬로워크 데이터랩이 21대 국회의 데이터를 모아 인터랙티브 인포그래픽으로 정리했어요. 성비와 연령을 기준으로 선거인의 구성과 국회의원의 구성을 낱낱이 비교했어요🤓 국회가 국민을 얼마나 대표하는지 우리 자세히 한번 살펴보아요~ https://t.co/daXXGruv8V",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/daXXGruv8V",
+					"expanded_url": "https://datalab.slowalk.co.kr/2020-05-07-21st-assembly",
+					"display_url": "datalab.slowalk.co.kr/2020-05-07-21s…",
+					"indices": [
+						126,
+						149
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 112719160,
+			"id_str": "112719160",
+			"name": "슬로워크",
+			"screen_name": "slowalk_",
+			"location": "Seoul, Republic of Korea",
+			"description": "슬로워크는 조직과 사회의 변화에 기여하는 크리에이티브 솔루션 회사입니다",
+			"url": "https://t.co/KU57tuhEFE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/KU57tuhEFE",
+							"expanded_url": "https://slowalk.co.kr",
+							"display_url": "slowalk.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4866,
+			"friends_count": 3390,
+			"listed_count": 202,
+			"created_at": "Tue Feb 09 13:43:31 +0000 2010",
+			"favourites_count": 98,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5970,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/852079313855762434/iExI31mS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/852079313855762434/iExI31mS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/112719160/1552290167",
+			"profile_link_color": "FA5514",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "7A7A7A",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "슬로워크 데이터랩이 21대 국회의 데이터를 모아 인터랙티브 인포그래픽으로 정리했어요. 성비와 연령을 기준으로 선거인의 구성과 국회의원의 구성을 낱낱이 비교했어요🤓 국회가 국민을 얼마나 대표하는지 우리 자세히 한번 살펴보아요~ https://t.co/daXXGruv8V"
+	},
+	{
 		"created_at": "Thu May 14 05:56:13 +0000 2020",
 		"id": 1260811133881143300,
 		"id_str": "1260811133881143296",
