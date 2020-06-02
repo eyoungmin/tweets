61081d9d37cdf19956199276b0a4445f81ae9dd1
Youngmin Lee <iameyoungmin@gmail.com>
2020-06-02 09:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 01 23:52:13 +0000 2020",
+		"id": 1267591760126984200,
+		"id_str": "1267604896951517186",
+		"full_text": "\"약탈하면 발포한다\" 트럼프가 남긴 트윗의 기원을 찾아서 - https://t.co/Zq3iPbzO0x https://t.co/RU7quGpyyt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			57
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Zq3iPbzO0x",
+					"expanded_url": "https://newspeppermint.com/2020/06/01/lootingstartsshootingstarts/",
+					"display_url": "newspeppermint.com/2020/06/01/loo…",
+					"indices": [
+						34,
+						57
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1267591758436683800,
+					"id_str": "1267591758436683777",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdj_QHU0AE9afp.png",
+					"media_url_https": "https://pbs.twimg.com/media/EZdj_QHU0AE9afp.png",
+					"url": "https://t.co/RU7quGpyyt",
+					"display_url": "pic.twitter.com/RU7quGpyyt",
+					"expanded_url": "https://twitter.com/newspeppermint/status/1267591760126984193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267591758436683800,
+					"id_str": "1267591758436683777",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdj_QHU0AE9afp.png",
+					"media_url_https": "https://pbs.twimg.com/media/EZdj_QHU0AE9afp.png",
+					"url": "https://t.co/RU7quGpyyt",
+					"display_url": "pic.twitter.com/RU7quGpyyt",
+					"expanded_url": "https://twitter.com/newspeppermint/status/1267591760126984193/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 316,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://newspeppermint.com\" rel=\"nofollow\">Newspeppermint</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 636874770,
+			"id_str": "636874770",
+			"name": "NewsPeppermint",
+			"screen_name": "newspeppermint",
+			"location": "",
+			"description": "뉴스페퍼민트는 최근 화제가 된 외신 중 우리에게 새로운 시각을 알려주는 기사를 중심으로 다양한 분야의 글 4편을 골라 번역/요약하여 월~금 오전 8시에 제공합니다.",
+			"url": "http://t.co/axYuRIa8Zu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/axYuRIa8Zu",
+							"expanded_url": "http://newspeppermint.com",
+							"display_url": "newspeppermint.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97184,
+			"friends_count": 0,
+			"listed_count": 661,
+			"created_at": "Mon Jul 16 08:02:09 +0000 2012",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7820,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FAFAFA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/496340027228430336/2P45XFM8_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/496340027228430336/2P45XFM8_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/636874770/1445217836",
+			"profile_link_color": "74C9BB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"약탈하면 발포한다\" 트럼프가 남긴 트윗의 기원을 찾아서 - https://t.co/Zq3iPbzO0x https://t.co/RU7quGpyyt"
+	},
+	{
 		"created_at": "Mon Jun 01 17:01:56 +0000 2020",
 		"id": 1267366132341567500,
 		"id_str": "1267501645924982784",
