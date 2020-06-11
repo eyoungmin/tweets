f171c1450e94c5ab9c2fd44ee8d06bc22c264c8b
Youngmin Lee <iameyoungmin@gmail.com>
2020-06-11 18:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59763,
-	"created_at": "Thu Jun 11 08:02:08 +0000 2020",
+	"tweets": 59764,
+	"created_at": "Thu Jun 11 09:02:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Thu Jun 11 08:35:44 +0000 2020",
+		"id": 1270989203577245700,
+		"id_str": "1270998136949891072",
+		"full_text": "열정 만수르 공연 감독님과\n토크로 하얗게 불태운\n아이유 콘서트의 모든 것🔥\n\nEDAM YouTube ▶▶ https://t.co/1p7rVusanW\n\n#아이유 #IU #집콕시그널\n#이담_프로젝트 #EDAM_Project https://t.co/CXKYXuphkm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이유",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "IU",
+					"indices": [
+						89,
+						92
+					]
+				},
+				{
+					"text": "집콕시그널",
+					"indices": [
+						93,
+						99
+					]
+				},
+				{
+					"text": "이담_프로젝트",
+					"indices": [
+						100,
+						108
+					]
+				},
+				{
+					"text": "EDAM_Project",
+					"indices": [
+						109,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1p7rVusanW",
+					"expanded_url": "https://youtu.be/ObG4xJbbjgQ",
+					"display_url": "youtu.be/ObG4xJbbjgQ",
+					"indices": [
+						59,
+						82
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1270989165019017200,
+					"id_str": "1270989165019017216",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/EaN16V5UMAADUPN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaN16V5UMAADUPN.jpg",
+					"url": "https://t.co/CXKYXuphkm",
+					"display_url": "pic.twitter.com/CXKYXuphkm",
+					"expanded_url": "https://twitter.com/edam_ent/status/1270989203577245697/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1270989165019017200,
+					"id_str": "1270989165019017216",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/EaN16V5UMAADUPN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaN16V5UMAADUPN.jpg",
+					"url": "https://t.co/CXKYXuphkm",
+					"display_url": "pic.twitter.com/CXKYXuphkm",
+					"expanded_url": "https://twitter.com/edam_ent/status/1270989203577245697/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1214016219889553400,
+			"id_str": "1214016219889553409",
+			"name": "EDAM Ent.",
+			"screen_name": "edam_ent",
+			"location": "",
+			"description": "EDAM Entertainment Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23542,
+			"friends_count": 1,
+			"listed_count": 57,
+			"created_at": "Mon Jan 06 02:50:04 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 36,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1470,
+		"favorite_count": 4264,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "열정 만수르 공연 감독님과\n토크로 하얗게 불태운\n아이유 콘서트의 모든 것🔥\n\nEDAM YouTube ▶▶ https://t.co/1p7rVusanW\n\n#아이유 #IU #집콕시그널\n#이담_프로젝트 #EDAM_Project https://t.co/CXKYXuphkm"
+	},
+	{
 		"created_at": "Thu Jun 11 07:52:06 +0000 2020",
 		"id": 1270987156106993700,
 		"id_str": "1270987156106993667",
