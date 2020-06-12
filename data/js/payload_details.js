54cba3bcff0d14cf6fad31cf7ec34bb3a4539c09
Youngmin Lee <iameyoungmin@gmail.com>
2020-06-13 08:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59847,
-	"created_at": "Fri Jun 12 22:02:07 +0000 2020",
+	"tweets": 59848,
+	"created_at": "Fri Jun 12 23:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Fri Jun 12 22:24:49 +0000 2020",
+		"id": 1271569172166668300,
+		"id_str": "1271569172166668288",
+		"full_text": "@NotionHQ Embed is not displayed. It's a paid account. But it doesn't work. Other paid accounts work. Created as [Create Bookmark]. help me! https://t.co/IaOiIoJiqa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NotionHQ",
+					"name": "Notion",
+					"id": 708915428454576100,
+					"id_str": "708915428454576128",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1271569133897781200,
+					"id_str": "1271569133897781248",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/EaWFY-VUEAAs_zI.png",
+					"media_url_https": "https://pbs.twimg.com/media/EaWFY-VUEAAs_zI.png",
+					"url": "https://t.co/IaOiIoJiqa",
+					"display_url": "pic.twitter.com/IaOiIoJiqa",
+					"expanded_url": "https://twitter.com/fromminn/status/1271569172166668288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 186,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 169,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 186,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1271569133897781200,
+					"id_str": "1271569133897781248",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/EaWFY-VUEAAs_zI.png",
+					"media_url_https": "https://pbs.twimg.com/media/EaWFY-VUEAAs_zI.png",
+					"url": "https://t.co/IaOiIoJiqa",
+					"display_url": "pic.twitter.com/IaOiIoJiqa",
+					"expanded_url": "https://twitter.com/fromminn/status/1271569172166668288/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 750,
+							"h": 186,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 169,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 186,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 708915428454576100,
+		"in_reply_to_user_id_str": "708915428454576128",
+		"in_reply_to_screen_name": "NotionHQ",
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Retweets & Favorites have various meanings.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 300,
+			"friends_count": 929,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 41399,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 61309,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@NotionHQ Embed is not displayed. It's a paid account. But it doesn't work. Other paid accounts work. Created as [Create Bookmark]. help me! https://t.co/IaOiIoJiqa"
+	},
+	{
 		"created_at": "Fri Jun 12 21:13:24 +0000 2020",
 		"id": 1271551198118674400,
 		"id_str": "1271551198118674437",
