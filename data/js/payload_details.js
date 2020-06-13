59796437edc6eb45c162a97b2d011b678a9a1b7b
Youngmin Lee <iameyoungmin@gmail.com>
2020-06-14 00:20:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 59888,
-	"created_at": "Sat Jun 13 15:02:06 +0000 2020",
+	"tweets": 59889,
+	"created_at": "Sat Jun 13 15:20:01 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sat Jun 13 15:13:18 +0000 2020",
+		"id": 1271822965185839000,
+		"id_str": "1271822965185839106",
+		"full_text": "#BEST_NEW_KPOP #베스트뉴케이팝 #베뉴팝\n[\\uD83D\\uDCF8] 캐스퍼 라디오 사진으로 다시 보기 (6월 2주차)\nhttps://t.co/87ntTzhv12\n#CASPER_RADIO #캐스퍼라디오\n#선곡표 #셋리스트 #SETLIST https://t.co/tZnyswz2Ff",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BEST_NEW_KPOP",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "베스트뉴케이팝",
+					"indices": [
+						15,
+						23
+					]
+				},
+				{
+					"text": "베뉴팝",
+					"indices": [
+						24,
+						28
+					]
+				},
+				{
+					"text": "CASPER_RADIO",
+					"indices": [
+						96,
+						109
+					]
+				},
+				{
+					"text": "캐스퍼라디오",
+					"indices": [
+						110,
+						117
+					]
+				},
+				{
+					"text": "선곡표",
+					"indices": [
+						118,
+						122
+					]
+				},
+				{
+					"text": "셋리스트",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "SETLIST",
+					"indices": [
+						129,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/87ntTzhv12",
+					"expanded_url": "https://channels.vlive.tv/E1C3AD/celeb/1.14863407",
+					"display_url": "channels.vlive.tv/E1C3AD/celeb/1…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1271822964137353200,
+					"id_str": "1271822964137353217",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EaZsP1qXYAEewOm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaZsP1qXYAEewOm.jpg",
+					"url": "https://t.co/tZnyswz2Ff",
+					"display_url": "pic.twitter.com/tZnyswz2Ff",
+					"expanded_url": "https://twitter.com/fromminn/status/1271822965185839106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1271822964137353200,
+					"id_str": "1271822964137353217",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EaZsP1qXYAEewOm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EaZsP1qXYAEewOm.jpg",
+					"url": "https://t.co/tZnyswz2Ff",
+					"display_url": "pic.twitter.com/tZnyswz2Ff",
+					"expanded_url": "https://twitter.com/fromminn/status/1271822965185839106/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Retweets & Favorites have various meanings.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 300,
+			"friends_count": 929,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 41414,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 61357,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#BEST_NEW_KPOP #베스트뉴케이팝 #베뉴팝\n[\\uD83D\\uDCF8] 캐스퍼 라디오 사진으로 다시 보기 (6월 2주차)\nhttps://t.co/87ntTzhv12\n#CASPER_RADIO #캐스퍼라디오\n#선곡표 #셋리스트 #SETLIST https://t.co/tZnyswz2Ff"
+	},
+	{
 		"created_at": "Sat Jun 13 14:07:03 +0000 2020",
 		"id": 1258243737764769800,
 		"id_str": "1271806291984281601",
