cadad4269e722b054e755f50f72b1da1799dc225
Youngmin Lee <iameyoungmin@gmail.com>
2020-06-19 19:02:14 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 60188,
-	"created_at": "Fri Jun 19 08:02:10 +0000 2020",
+	"tweets": 60191,
+	"created_at": "Fri Jun 19 10:02:12 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_06.js",
 		"year": 2020,
 		"var_name": "tweets_2020_06",
-		"tweet_count": 980,
+		"tweet_count": 983,
 		"month": 6
 	},
 	{
