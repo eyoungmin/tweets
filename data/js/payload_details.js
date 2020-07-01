000321aa508baa3fdf897142b2adff60618d5ea1
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-01 12:02:02 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 60863,
-	"created_at": "Wed Jul 01 02:02:01 +0000 2020",
+	"tweets": 60866,
+	"created_at": "Wed Jul 01 03:02:00 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,650 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Wed Jul 01 03:01:07 +0000 2020",
+		"id": 1278161684956414000,
+		"id_str": "1278161684956413952",
+		"full_text": "#Twitter #Newsletter #Nuzzel\n트위터 뉴스레터 (Jul 1) https://t.co/5Zk5r9Dh5j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Twitter",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "Newsletter",
+					"indices": [
+						9,
+						20
+					]
+				},
+				{
+					"text": "Nuzzel",
+					"indices": [
+						21,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5Zk5r9Dh5j",
+					"expanded_url": "https://nzzl.us/mcCRocx",
+					"display_url": "nzzl.us/mcCRocx",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://nuzzel.com/\" rel=\"nofollow\">Nuzzel</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "Retweets & Favorites have various meanings.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 296,
+			"friends_count": 936,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 41672,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 62427,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/675405192091725824/M0DLTPWg_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#Twitter #Newsletter #Nuzzel\n트위터 뉴스레터 (Jul 1) https://t.co/5Zk5r9Dh5j"
+	},
+	{
+		"created_at": "Wed Jul 01 02:28:04 +0000 2020",
+		"id": 1278146683285598200,
+		"id_str": "1278153366967914502",
+		"full_text": "[#레벨업 아슬한 프로젝트]\n\n레드벨벳의 첫 유닛 아이린&amp;슬기!\n둘만의 아슬한 케미를 담은 ‘아슬한 프로젝트’가 시작된다!\n\n첫번째 프로그램 티저 공개!\n👉https://t.co/R9E1rsyyFA\n\n#레드벨벳 #아이린 #슬기 #RedVelvet #IRENE #SEULGI \n#아슬한프로젝트 #SMCC_STUDIO https://t.co/ShsdDgUcQb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레벨업",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						116,
+						121
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						122,
+						126
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						127,
+						130
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						131,
+						141
+					]
+				},
+				{
+					"text": "IRENE",
+					"indices": [
+						142,
+						148
+					]
+				},
+				{
+					"text": "SEULGI",
+					"indices": [
+						149,
+						156
+					]
+				},
+				{
+					"text": "아슬한프로젝트",
+					"indices": [
+						158,
+						166
+					]
+				},
+				{
+					"text": "SMCC_STUDIO",
+					"indices": [
+						167,
+						179
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/R9E1rsyyFA",
+					"expanded_url": "https://youtu.be/Soxv8mHcWYM",
+					"display_url": "youtu.be/Soxv8mHcWYM",
+					"indices": [
+						91,
+						114
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1278146589727445000,
+					"id_str": "1278146589727444994",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EbzjjSuU0AIpEEW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbzjjSuU0AIpEEW.jpg",
+					"url": "https://t.co/ShsdDgUcQb",
+					"display_url": "pic.twitter.com/ShsdDgUcQb",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1278146683285598209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278146589727445000,
+					"id_str": "1278146589727444994",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/EbzjjSuU0AIpEEW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbzjjSuU0AIpEEW.jpg",
+					"url": "https://t.co/ShsdDgUcQb",
+					"display_url": "pic.twitter.com/ShsdDgUcQb",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1278146683285598209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1192288122756681700,
+			"id_str": "1192288122756681728",
+			"name": "SM C&C STUDIO",
+			"screen_name": "smcc_studio",
+			"location": "",
+			"description": "SM C&C STUDIO Official Twitter Page\n\n💙슈주 리턴즈4 \n🟢NCT LIFE: DREAM in Wonderland \n♥️레벨업 아슬한 프로젝트",
+			"url": "https://t.co/vODnwmZcgP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vODnwmZcgP",
+							"expanded_url": "https://www.youtube.com/SMCCStudio",
+							"display_url": "youtube.com/SMCCStudio",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7554,
+			"friends_count": 0,
+			"listed_count": 27,
+			"created_at": "Thu Nov 07 03:50:32 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 259,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192288259226779648/ftT8gqHv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192288259226779648/ftT8gqHv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1192288122756681728/1573099495",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1504,
+		"favorite_count": 1606,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#레벨업 아슬한 프로젝트]\n\n레드벨벳의 첫 유닛 아이린&amp;슬기!\n둘만의 아슬한 케미를 담은 ‘아슬한 프로젝트’가 시작된다!\n\n첫번째 프로그램 티저 공개!\n👉https://t.co/R9E1rsyyFA\n\n#레드벨벳 #아이린 #슬기 #RedVelvet #IRENE #SEULGI \n#아슬한프로젝트 #SMCC_STUDIO https://t.co/ShsdDgUcQb"
+	},
+	{
+		"created_at": "Wed Jul 01 02:28:02 +0000 2020",
+		"id": 1278146304850329600,
+		"id_str": "1278153357727801348",
+		"full_text": "[#레벨업 아슬한 프로젝트]\n\n7월,8월 캘린더를 공개합니다!\n\n7월 8일(수) 11am(KST) 첫 방송!\n매주 수 오전 11시 #wavve 에서 만나요~\n\n많은 관심과 사랑 부탁 드립니다\n\n#레드벨벳 #아이린 #슬기 #RedVelvet #IRENE #SEULGI \n#아슬한프로젝트 #SMCC_STUDIO https://t.co/GhqraZBpIb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레벨업",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "wavve",
+					"indices": [
+						73,
+						79
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						108,
+						113
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						114,
+						118
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						119,
+						122
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						123,
+						133
+					]
+				},
+				{
+					"text": "IRENE",
+					"indices": [
+						134,
+						140
+					]
+				},
+				{
+					"text": "SEULGI",
+					"indices": [
+						141,
+						148
+					]
+				},
+				{
+					"text": "아슬한프로젝트",
+					"indices": [
+						150,
+						158
+					]
+				},
+				{
+					"text": "SMCC_STUDIO",
+					"indices": [
+						159,
+						171
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1278145385400135700,
+					"id_str": "1278145385400135680",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EbzidMQUcAADGBl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbzidMQUcAADGBl.jpg",
+					"url": "https://t.co/GhqraZBpIb",
+					"display_url": "pic.twitter.com/GhqraZBpIb",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1278146304850329600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278145385400135700,
+					"id_str": "1278145385400135680",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EbzidMQUcAADGBl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbzidMQUcAADGBl.jpg",
+					"url": "https://t.co/GhqraZBpIb",
+					"display_url": "pic.twitter.com/GhqraZBpIb",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1278146304850329600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1278145430849589200,
+					"id_str": "1278145430849589248",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/Ebzif1kUEAAO809.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ebzif1kUEAAO809.jpg",
+					"url": "https://t.co/GhqraZBpIb",
+					"display_url": "pic.twitter.com/GhqraZBpIb",
+					"expanded_url": "https://twitter.com/smcc_studio/status/1278146304850329600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1192288122756681700,
+			"id_str": "1192288122756681728",
+			"name": "SM C&C STUDIO",
+			"screen_name": "smcc_studio",
+			"location": "",
+			"description": "SM C&C STUDIO Official Twitter Page\n\n💙슈주 리턴즈4 \n🟢NCT LIFE: DREAM in Wonderland \n♥️레벨업 아슬한 프로젝트",
+			"url": "https://t.co/vODnwmZcgP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vODnwmZcgP",
+							"expanded_url": "https://www.youtube.com/SMCCStudio",
+							"display_url": "youtube.com/SMCCStudio",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7554,
+			"friends_count": 0,
+			"listed_count": 27,
+			"created_at": "Thu Nov 07 03:50:32 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 259,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192288259226779648/ftT8gqHv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192288259226779648/ftT8gqHv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1192288122756681728/1573099495",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1511,
+		"favorite_count": 1638,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#레벨업 아슬한 프로젝트]\n\n7월,8월 캘린더를 공개합니다!\n\n7월 8일(수) 11am(KST) 첫 방송!\n매주 수 오전 11시 #wavve 에서 만나요~\n\n많은 관심과 사랑 부탁 드립니다\n\n#레드벨벳 #아이린 #슬기 #RedVelvet #IRENE #SEULGI \n#아슬한프로젝트 #SMCC_STUDIO https://t.co/GhqraZBpIb"
+	},
+	{
 		"created_at": "Wed Jul 01 01:25:10 +0000 2020",
 		"id": 1278127098675118000,
 		"id_str": "1278137540449574913",
