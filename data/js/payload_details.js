109b3e50a29b3e4a718bb2ab8a88677e962de343
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-03 10:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61052,
-	"created_at": "Fri Jul 03 00:02:04 +0000 2020",
+	"tweets": 61053,
+	"created_at": "Fri Jul 03 01:02:03 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 03 00:25:11 +0000 2020",
+		"id": 1278845469003677700,
+		"id_str": "1278847217592303618",
+		"full_text": "[#예술경영 웹진: 자료읽기]\n\n&lt;포용적 예술로 내딛는 한 걸음&gt;\n\n448호 자료읽기 읽으러가기👇\nhttps://t.co/sr1VmAqGqF\n\n#예술경영 #웹진 #예술경영지원센터 https://t.co/Y1HLJ0d1ry",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "예술경영",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "예술경영",
+					"indices": [
+						86,
+						91
+					]
+				},
+				{
+					"text": "웹진",
+					"indices": [
+						92,
+						95
+					]
+				},
+				{
+					"text": "예술경영지원센터",
+					"indices": [
+						96,
+						105
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sr1VmAqGqF",
+					"expanded_url": "https://bit.ly/2ZxIQyv",
+					"display_url": "bit.ly/2ZxIQyv",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1278845434857840600,
+					"id_str": "1278845434857840640",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/Eb9fJbtU4AAlTvn.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eb9fJbtU4AAlTvn.png",
+					"url": "https://t.co/Y1HLJ0d1ry",
+					"display_url": "pic.twitter.com/Y1HLJ0d1ry",
+					"expanded_url": "https://twitter.com/weeklyAM/status/1278845469003677696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 787,
+							"h": 328,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 787,
+							"h": 328,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1278845434857840600,
+					"id_str": "1278845434857840640",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/Eb9fJbtU4AAlTvn.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eb9fJbtU4AAlTvn.png",
+					"url": "https://t.co/Y1HLJ0d1ry",
+					"display_url": "pic.twitter.com/Y1HLJ0d1ry",
+					"expanded_url": "https://twitter.com/weeklyAM/status/1278845469003677696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 787,
+							"h": 328,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 787,
+							"h": 328,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 283,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 180261024,
+			"id_str": "180261024",
+			"name": "웹진≪예술경영≫",
+			"screen_name": "weeklyAM",
+			"location": "대학로 예술경영지원센터KAMS",
+			"description": "웹진≪예술경영≫",
+			"url": "https://t.co/qL8DWFa3K7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qL8DWFa3K7",
+							"expanded_url": "http://www.gokams.or.kr/webzine/",
+							"display_url": "gokams.or.kr/webzine/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6704,
+			"friends_count": 3034,
+			"listed_count": 101,
+			"created_at": "Thu Aug 19 05:30:26 +0000 2010",
+			"favourites_count": 162,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2671,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/763905572307546112/TMZitkC5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/763905572307546112/TMZitkC5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/180261024/1553153321",
+			"profile_link_color": "94D487",
+			"profile_sidebar_border_color": "7EDB5E",
+			"profile_sidebar_fill_color": "9DEB96",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#예술경영 웹진: 자료읽기]\n\n&lt;포용적 예술로 내딛는 한 걸음&gt;\n\n448호 자료읽기 읽으러가기👇\nhttps://t.co/sr1VmAqGqF\n\n#예술경영 #웹진 #예술경영지원센터 https://t.co/Y1HLJ0d1ry"
+	},
+	{
 		"created_at": "Thu Jul 02 23:25:11 +0000 2020",
 		"id": 1278824477883682800,
 		"id_str": "1278832118035025922",
