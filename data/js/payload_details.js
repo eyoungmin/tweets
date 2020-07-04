fcba75cc33a51ac0c0ce2b1bfeb3228df3672447
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-04 18:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61162,
-	"created_at": "Sat Jul 04 08:02:04 +0000 2020",
+	"tweets": 61163,
+	"created_at": "Sat Jul 04 09:02:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 08:25:11 +0000 2020",
+		"id": 1279327637534433300,
+		"id_str": "1279330401853603840",
+		"full_text": "200704 🔍[예능연구소 4K]\n#쇼음악중심 #음중 #블랙핑크 #BLACKPINK   #HowYouLikeThat @ygofficialblink \n\n‘블랙핑크 - How You Like That’ 바로가기 🔽\nhttps://t.co/Ys5qEZEB4I https://t.co/z35lJKst7I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "쇼음악중심",
+					"indices": [
+						19,
+						25
+					]
+				},
+				{
+					"text": "음중",
+					"indices": [
+						26,
+						29
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						30,
+						35
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						36,
+						46
+					]
+				},
+				{
+					"text": "HowYouLikeThat",
+					"indices": [
+						49,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						65,
+						81
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ys5qEZEB4I",
+					"expanded_url": "https://youtu.be/8eaPVKN5284",
+					"display_url": "youtu.be/8eaPVKN5284",
+					"indices": [
+						118,
+						141
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1279327583662821400,
+					"id_str": "1279327583662821376",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/EcEVqL-U0AAQ11R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcEVqL-U0AAQ11R.jpg",
+					"url": "https://t.co/z35lJKst7I",
+					"display_url": "pic.twitter.com/z35lJKst7I",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1279327637534433280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1279327583662821400,
+					"id_str": "1279327583662821376",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/EcEVqL-U0AAQ11R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcEVqL-U0AAQ11R.jpg",
+					"url": "https://t.co/z35lJKst7I",
+					"display_url": "pic.twitter.com/z35lJKst7I",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1279327637534433280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144012572,
+			"id_str": "4144012572",
+			"name": "MBC예능연구소",
+			"screen_name": "MBC_entertain",
+			"location": "",
+			"description": "MBC 예능의 꿀잼만 모아 드림 [official]",
+			"url": "https://t.co/7U66BOeSeR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7U66BOeSeR",
+							"expanded_url": "http://facebook.com/MBCentertain",
+							"display_url": "facebook.com/MBCentertain",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 803501,
+			"friends_count": 5,
+			"listed_count": 746,
+			"created_at": "Fri Nov 06 08:21:55 +0000 2015",
+			"favourites_count": 88,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12890,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144012572/1555047387",
+			"profile_link_color": "45B7DE",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6735,
+		"favorite_count": 18014,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "200704 🔍[예능연구소 4K]\n#쇼음악중심 #음중 #블랙핑크 #BLACKPINK   #HowYouLikeThat @ygofficialblink \n\n‘블랙핑크 - How You Like That’ 바로가기 🔽\nhttps://t.co/Ys5qEZEB4I https://t.co/z35lJKst7I"
+	},
+	{
 		"created_at": "Sat Jul 04 07:34:50 +0000 2020",
 		"id": 1279317733386203100,
 		"id_str": "1279317733386203139",
