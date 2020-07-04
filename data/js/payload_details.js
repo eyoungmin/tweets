8a8d2c8608952c2845b32d6e60c24309368b037a
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-04 16:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61137,
-	"created_at": "Sat Jul 04 06:02:04 +0000 2020",
+	"tweets": 61138,
+	"created_at": "Sat Jul 04 07:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 06:25:10 +0000 2020",
+		"id": 1279295201815654400,
+		"id_str": "1279300200692056065",
+		"full_text": "#BLACKPINKINYOURAREA 잠시 후 3시 35분, MBC #쇼음악중심 에 #BLACKPINK 가 출연합니다⏰ \n#젠츄리챙 과 함께 춤추고 싶은 블링크들 모두 본방사수!!🕺💃\n#블랙핑크 #HowYouLikeThat",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINKINYOURAREA",
+					"indices": [
+						0,
+						20
+					]
+				},
+				{
+					"text": "쇼음악중심",
+					"indices": [
+						38,
+						44
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						47,
+						57
+					]
+				},
+				{
+					"text": "젠츄리챙",
+					"indices": [
+						68,
+						73
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						103,
+						108
+					]
+				},
+				{
+					"text": "HowYouLikeThat",
+					"indices": [
+						109,
+						124
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/c2mkxA118z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c2mkxA118z",
+							"expanded_url": "https://blink.blackpinkofficial.com/",
+							"display_url": "blink.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4451467,
+			"friends_count": 2,
+			"listed_count": 4962,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 978,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1592265711",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9574,
+		"favorite_count": 29618,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "#BLACKPINKINYOURAREA 잠시 후 3시 35분, MBC #쇼음악중심 에 #BLACKPINK 가 출연합니다⏰ \n#젠츄리챙 과 함께 춤추고 싶은 블링크들 모두 본방사수!!🕺💃\n#블랙핑크 #HowYouLikeThat"
+	},
+	{
 		"created_at": "Sat Jul 04 05:25:20 +0000 2020",
 		"id": 1279278767240654800,
 		"id_str": "1279285140397621253",
