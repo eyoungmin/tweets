a4d932fe79d2392e4f5c9112277841b538ce83c4
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-04 19:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61163,
-	"created_at": "Sat Jul 04 09:02:05 +0000 2020",
+	"tweets": 61164,
+	"created_at": "Sat Jul 04 10:02:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 09:25:23 +0000 2020",
+		"id": 1279343821940088800,
+		"id_str": "1279345553189548035",
+		"full_text": "200704 🔍[예능연구소 세로캠] \n#쇼음악중심 #음중 #블랙핑크 #BLACKPINK #HowYouLikeThat @ygofficialblink\n\n#제니 ▶ https://t.co/KkebiPCTJH\n#리사 ▶ https://t.co/KbNwauAlis\n#로제 ▶ https://t.co/6wdNPRsc8c\n#지수 ▶ https://t.co/fkRboKRefB https://t.co/xm9o7rP4br",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			202
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "쇼음악중심",
+					"indices": [
+						21,
+						27
+					]
+				},
+				{
+					"text": "음중",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						32,
+						37
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						38,
+						48
+					]
+				},
+				{
+					"text": "HowYouLikeThat",
+					"indices": [
+						49,
+						64
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "로제",
+					"indices": [
+						143,
+						146
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						173,
+						176
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						65,
+						81
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/KkebiPCTJH",
+					"expanded_url": "https://youtu.be/ut6L6162d1o",
+					"display_url": "youtu.be/ut6L6162d1o",
+					"indices": [
+						89,
+						112
+					]
+				},
+				{
+					"url": "https://t.co/KbNwauAlis",
+					"expanded_url": "https://youtu.be/oD56wQwciRk",
+					"display_url": "youtu.be/oD56wQwciRk",
+					"indices": [
+						119,
+						142
+					]
+				},
+				{
+					"url": "https://t.co/6wdNPRsc8c",
+					"expanded_url": "https://youtu.be/-_TUanDKSzo",
+					"display_url": "youtu.be/-_TUanDKSzo",
+					"indices": [
+						149,
+						172
+					]
+				},
+				{
+					"url": "https://t.co/fkRboKRefB",
+					"expanded_url": "https://youtu.be/VkqMDkLxfYU",
+					"display_url": "youtu.be/VkqMDkLxfYU",
+					"indices": [
+						179,
+						202
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1279343789081944000,
+					"id_str": "1279343789081944065",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/EcEkZd3U4AEOcqj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcEkZd3U4AEOcqj.jpg",
+					"url": "https://t.co/xm9o7rP4br",
+					"display_url": "pic.twitter.com/xm9o7rP4br",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1279343821940088838/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1279343789081944000,
+					"id_str": "1279343789081944065",
+					"indices": [
+						203,
+						226
+					],
+					"media_url": "http://pbs.twimg.com/media/EcEkZd3U4AEOcqj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcEkZd3U4AEOcqj.jpg",
+					"url": "https://t.co/xm9o7rP4br",
+					"display_url": "pic.twitter.com/xm9o7rP4br",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1279343821940088838/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144012572,
+			"id_str": "4144012572",
+			"name": "MBC예능연구소",
+			"screen_name": "MBC_entertain",
+			"location": "",
+			"description": "MBC 예능의 꿀잼만 모아 드림 [official]",
+			"url": "https://t.co/7U66BOeSeR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7U66BOeSeR",
+							"expanded_url": "http://facebook.com/MBCentertain",
+							"display_url": "facebook.com/MBCentertain",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 803590,
+			"friends_count": 5,
+			"listed_count": 747,
+			"created_at": "Fri Nov 06 08:21:55 +0000 2015",
+			"favourites_count": 88,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12897,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144012572/1555047387",
+			"profile_link_color": "45B7DE",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4672,
+		"favorite_count": 14049,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "200704 🔍[예능연구소 세로캠] \n#쇼음악중심 #음중 #블랙핑크 #BLACKPINK #HowYouLikeThat @ygofficialblink\n\n#제니 ▶ https://t.co/KkebiPCTJH\n#리사 ▶ https://t.co/KbNwauAlis\n#로제 ▶ https://t.co/6wdNPRsc8c\n#지수 ▶ https://t.co/fkRboKRefB https://t.co/xm9o7rP4br"
+	},
+	{
 		"created_at": "Sat Jul 04 08:25:11 +0000 2020",
 		"id": 1279327637534433300,
 		"id_str": "1279330401853603840",
