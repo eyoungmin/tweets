c5744f8cf225987ff91876518fbbf964f58bac27
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-05 18:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61214,
-	"created_at": "Sun Jul 05 08:02:06 +0000 2020",
+	"tweets": 61215,
+	"created_at": "Sun Jul 05 09:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,158 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sun Jul 05 08:25:14 +0000 2020",
+		"id": 1279686682342637600,
+		"id_str": "1279692801924567040",
+		"full_text": "#OFFICIALBLINK 박수를 왕창이 받아도 모자랄만큼의 레전드 무대로 1위🥇를 수상한 #젠츄리챙 의 #인기가요 스페셜 포토가 #블링크존 에 업로드 되었습니다👏👏👏👏📸 \n#BLACKPINK #블랙핑크 #HowYouLikeThat #HYLT1stWin",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OFFICIALBLINK",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "젠츄리챙",
+					"indices": [
+						51,
+						56
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						59,
+						64
+					]
+				},
+				{
+					"text": "블링크존",
+					"indices": [
+						73,
+						78
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						97,
+						107
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						108,
+						113
+					]
+				},
+				{
+					"text": "HowYouLikeThat",
+					"indices": [
+						114,
+						129
+					]
+				},
+				{
+					"text": "HYLT1stWin",
+					"indices": [
+						130,
+						141
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/c2mkxA118z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c2mkxA118z",
+							"expanded_url": "https://blink.blackpinkofficial.com/",
+							"display_url": "blink.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4462700,
+			"friends_count": 2,
+			"listed_count": 4964,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 986,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1592265711",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16698,
+		"favorite_count": 42025,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "#OFFICIALBLINK 박수를 왕창이 받아도 모자랄만큼의 레전드 무대로 1위🥇를 수상한 #젠츄리챙 의 #인기가요 스페셜 포토가 #블링크존 에 업로드 되었습니다👏👏👏👏📸 \n#BLACKPINK #블랙핑크 #HowYouLikeThat #HYLT1stWin"
+	},
+	{
 		"created_at": "Sun Jul 05 07:45:39 +0000 2020",
 		"id": 1279673869138194400,
 		"id_str": "1279682841077583873",
