23e10c1dd576ba3ad654d9088bbe846f37c31045
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-05 11:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61178,
-	"created_at": "Sat Jul 04 16:02:04 +0000 2020",
+	"tweets": 61179,
+	"created_at": "Sun Jul 05 02:02:03 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sun Jul 05 01:50:34 +0000 2020",
+		"id": 1279588567371493400,
+		"id_str": "1279593482999357440",
+		"full_text": "당근마켓의 사용자 경험 디자인 분석 https://t.co/adOLoRXGXP \n- 직관적인 당근 인터페이스\n- 사용자 주도성을 높인 디자인\n- 당근 Holic!\nㅤ(1) Interview\nㅤ(2) Flow 상태에 돌입하다 \nㅤ(3) 난이도와 숙련도 분석 \n- 기술수용모델(TAM)을 통해 분석한 당근마켓\nㅤ(1) 인지된 재미요소(P...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/adOLoRXGXP",
+					"expanded_url": "https://j.mp/2Buzw6L",
+					"display_url": "j.mp/2Buzw6L",
+					"indices": [
+						20,
+						43
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://news.hada.io\" rel=\"nofollow\">NewsHada</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1149438140811116500,
+			"id_str": "1149438140811116544",
+			"name": "GeekNews",
+			"screen_name": "GeekNewsBot",
+			"location": "",
+			"description": "GeekNews 는 기술,개발,스타트업과 세상의 재미난 것들을 좋아하는 Geek들을 위한 뉴스사이트 입니다. 이 계정은 긱뉴스에 새로 등록된 뉴스들을 자동으로 트윗합니다",
+			"url": "https://t.co/Eeoeh5jF1C",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Eeoeh5jF1C",
+							"expanded_url": "https://news.hada.io",
+							"display_url": "news.hada.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3251,
+			"friends_count": 0,
+			"listed_count": 47,
+			"created_at": "Thu Jul 11 21:59:43 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2283,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_link_color": "000065",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "당근마켓의 사용자 경험 디자인 분석 https://t.co/adOLoRXGXP \n- 직관적인 당근 인터페이스\n- 사용자 주도성을 높인 디자인\n- 당근 Holic!\nㅤ(1) Interview\nㅤ(2) Flow 상태에 돌입하다 \nㅤ(3) 난이도와 숙련도 분석 \n- 기술수용모델(TAM)을 통해 분석한 당근마켓\nㅤ(1) 인지된 재미요소(P..."
+	},
+	{
 		"created_at": "Sat Jul 04 15:25:25 +0000 2020",
 		"id": 1279430017680699400,
 		"id_str": "1279436159698055168",
