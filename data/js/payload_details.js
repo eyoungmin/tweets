f22480b1a3692a803e90e7c8f6da27d1003dc657
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-09 13:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61415,
-	"created_at": "Thu Jul 09 03:02:07 +0000 2020",
+	"tweets": 61416,
+	"created_at": "Thu Jul 09 04:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,176 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Thu Jul 09 03:27:57 +0000 2020",
+		"id": 1281067127407906800,
+		"id_str": "1281067539980828672",
+		"full_text": "레드벨벳-아이린&amp;슬기, 변신을 노렸다면 'ALL 10'[문완식의 톡식] (출처 : 스타뉴스 | 네이버 TV연예) \n\n👉https://t.co/od1eBKk0i2\n\n#RedVelvet_IRENE_SEULGI #레드벨벳_아이린_슬기 #IRENE #아이린 #SEULGI #슬기 #RedVelvet #레드벨벳 #Monster",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet_IRENE_SEULGI",
+					"indices": [
+						95,
+						118
+					]
+				},
+				{
+					"text": "레드벨벳_아이린_슬기",
+					"indices": [
+						119,
+						131
+					]
+				},
+				{
+					"text": "IRENE",
+					"indices": [
+						132,
+						138
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						139,
+						143
+					]
+				},
+				{
+					"text": "SEULGI",
+					"indices": [
+						144,
+						151
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						152,
+						155
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						156,
+						166
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						167,
+						172
+					]
+				},
+				{
+					"text": "Monster",
+					"indices": [
+						173,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/od1eBKk0i2",
+					"expanded_url": "http://naver.me/Igt15YRR",
+					"display_url": "naver.me/Igt15YRR",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/pjjIYiVH17",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pjjIYiVH17",
+							"expanded_url": "https://youtu.be/Ujb-gvqsoi0",
+							"display_url": "youtu.be/Ujb-gvqsoi0",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2842450,
+			"friends_count": 2,
+			"listed_count": 6111,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 36,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1357,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1265296830075527168/JLH8ZWPH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1265296830075527168/JLH8ZWPH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1577091773",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3035,
+		"favorite_count": 9310,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "레드벨벳-아이린&amp;슬기, 변신을 노렸다면 'ALL 10'[문완식의 톡식] (출처 : 스타뉴스 | 네이버 TV연예) \n\n👉https://t.co/od1eBKk0i2\n\n#RedVelvet_IRENE_SEULGI #레드벨벳_아이린_슬기 #IRENE #아이린 #SEULGI #슬기 #RedVelvet #레드벨벳 #Monster"
+	},
+	{
 		"created_at": "Thu Jul 09 03:00:48 +0000 2020",
 		"id": 1281060708663394300,
 		"id_str": "1281060708663394308",
