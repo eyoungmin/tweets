3d5ac826686a75f58d8c0974629ecfcb158e8987
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-11 13:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 61580,
-	"created_at": "Sat Jul 11 03:02:10 +0000 2020",
+	"tweets": 61581,
+	"created_at": "Sat Jul 11 04:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 11 03:25:11 +0000 2020",
+		"id": 1281785351204302800,
+		"id_str": "1281791618929098752",
+		"full_text": "[#희진 / #HeeJin] 여름엔 역시 콩국수😭👍🏻 콩국수 좋아하는 오빛 손!!!!🤚🤚\n\n#이달의소녀 #LOONA https://t.co/CjOhBzrdIa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "희진",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "HeeJin",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						51,
+						57
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						58,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1281785338663276500,
+					"id_str": "1281785338663276544",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EcnQ-W1UEAAnVF_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcnQ-W1UEAAnVF_.jpg",
+					"url": "https://t.co/CjOhBzrdIa",
+					"display_url": "pic.twitter.com/CjOhBzrdIa",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1281785351204302849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1281785338663276500,
+					"id_str": "1281785338663276544",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EcnQ-W1UEAAnVF_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcnQ-W1UEAAnVF_.jpg",
+					"url": "https://t.co/CjOhBzrdIa",
+					"display_url": "pic.twitter.com/CjOhBzrdIa",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1281785351204302849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281785338667470800,
+					"id_str": "1281785338667470850",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EcnQ-W2UEAIsw98.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcnQ-W2UEAIsw98.jpg",
+					"url": "https://t.co/CjOhBzrdIa",
+					"display_url": "pic.twitter.com/CjOhBzrdIa",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1281785351204302849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1281785338650755000,
+					"id_str": "1281785338650755072",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EcnQ-WyVAAAF89u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EcnQ-WyVAAAF89u.jpg",
+					"url": "https://t.co/CjOhBzrdIa",
+					"display_url": "pic.twitter.com/CjOhBzrdIa",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1281785351204302849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 497150,
+			"friends_count": 0,
+			"listed_count": 5116,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2024,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6505,
+		"favorite_count": 17742,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#희진 / #HeeJin] 여름엔 역시 콩국수😭👍🏻 콩국수 좋아하는 오빛 손!!!!🤚🤚\n\n#이달의소녀 #LOONA https://t.co/CjOhBzrdIa"
+	},
+	{
 		"created_at": "Sat Jul 11 03:00:36 +0000 2020",
 		"id": 1281785435245740000,
 		"id_str": "1281785435245740032",
