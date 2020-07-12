99e641a3beda48e060fe4b6d1e599d81d964d06c
Youngmin Lee <iameyoungmin@gmail.com>
2020-07-12 20:02:12 +0900

    Added 1 tweets

--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sun Jul 12 10:27:57 +0000 2020",
+		"id": 1282247922109702100,
+		"id_str": "1282260398973083652",
+		"full_text": "러비들의 심장 저격 탕탕탕⚡\n아무리 봐도 믿을 수 없는 완벽한 무대\n완벽한 착장, 완벽한 미모🧡\n\n@RVsmtown #레드벨벳 #아이린 #슬기 #Monster \n\n아이린: https://t.co/TO4Mli1RiY\n슬기: https://t.co/AJVsAmMkKo https://t.co/Dq1pKAb2dX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레드벨벳",
+					"indices": [
+						65,
+						70
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						71,
+						75
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "Monster",
+					"indices": [
+						80,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RVsmtown",
+					"name": "Red Velvet",
+					"id": 873092428755894300,
+					"id_str": "873092428755894272",
+					"indices": [
+						55,
+						64
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/TO4Mli1RiY",
+					"expanded_url": "http://youtu.be/EgkDBiqebY8",
+					"display_url": "youtu.be/EgkDBiqebY8",
+					"indices": [
+						96,
+						119
+					]
+				},
+				{
+					"url": "https://t.co/AJVsAmMkKo",
+					"expanded_url": "http://youtu.be/u9nXv_y3jts",
+					"display_url": "youtu.be/u9nXv_y3jts",
+					"indices": [
+						124,
+						147
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1282247914945839000,
+					"id_str": "1282247914945839106",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/Ect1r1xVAAIF63H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ect1r1xVAAIF63H.jpg",
+					"url": "https://t.co/Dq1pKAb2dX",
+					"display_url": "pic.twitter.com/Dq1pKAb2dX",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1282247922109702144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1282247914945839000,
+					"id_str": "1282247914945839106",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/Ect1r1xVAAIF63H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ect1r1xVAAIF63H.jpg",
+					"url": "https://t.co/Dq1pKAb2dX",
+					"display_url": "pic.twitter.com/Dq1pKAb2dX",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1282247922109702144/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1131475077650772000,
+			"id_str": "1131475077650771968",
+			"name": "스브스케이팝 / SBS KPOP",
+			"screen_name": "kpop_sbs",
+			"location": "",
+			"description": "♡ 스브스케이팝 / SBS KPOP 공식 트위터 ♡ \nSBS의 예능/음악방송을 여러분께 더 많이 공유하고 소통하기 위한 채널입니다 :)",
+			"url": "https://t.co/qapZ7nOCns",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qapZ7nOCns",
+							"expanded_url": "https://www.youtube.com/user/SBSMusic1",
+							"display_url": "youtube.com/user/SBSMusic1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 86025,
+			"friends_count": 73,
+			"listed_count": 129,
+			"created_at": "Thu May 23 08:20:55 +0000 2019",
+			"favourites_count": 1018,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 936,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1131475077650771968/1593406283",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 774,
+		"favorite_count": 906,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "러비들의 심장 저격 탕탕탕⚡\n아무리 봐도 믿을 수 없는 완벽한 무대\n완벽한 착장, 완벽한 미모🧡\n\n@RVsmtown #레드벨벳 #아이린 #슬기 #Monster \n\n아이린: https://t.co/TO4Mli1RiY\n슬기: https://t.co/AJVsAmMkKo https://t.co/Dq1pKAb2dX"
+	},
+	{
 		"created_at": "Sun Jul 12 09:25:15 +0000 2020",
 		"id": 1282238753285632000,
 		"id_str": "1282244620718678017",
