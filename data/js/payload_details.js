3eb8bd026adc5c304fa058c0b07e088b709474e5
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-01 18:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62921,
-	"created_at": "Sat Aug 01 08:02:02 +0000 2020",
+	"tweets": 62922,
+	"created_at": "Sat Aug 01 09:02:02 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 01 08:58:07 +0000 2020",
+		"id": 1289444258085183500,
+		"id_str": "1289485549254787072",
+		"full_text": "Perfect Edition - 가벼운 반응형 웹 E-북 템플릿 https://t.co/Cu8rQvc54Z \n- 페이지 기반의 오픈소스 이북 뷰어 (Ruby코드)\n- 모바일을 비롯한 대부분의 브라우저 지원\n- Swipe,Tap,화살표,스페이스,마우스 휠로 이동 가능\n- 웹사이트와 EPUB를 동시에 생성\n- 소설가인 Robin Sloan 이 직접 ...",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			197
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Cu8rQvc54Z",
+					"expanded_url": "https://j.mp/3jXHF4R",
+					"display_url": "j.mp/3jXHF4R",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://news.hada.io\" rel=\"nofollow\">NewsHada</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1149438140811116500,
+			"id_str": "1149438140811116544",
+			"name": "GeekNews",
+			"screen_name": "GeekNewsBot",
+			"location": "",
+			"description": "GeekNews 는 기술,개발,스타트업과 세상의 재미난 것들을 좋아하는 Geek들을 위한 뉴스사이트 입니다. 이 계정은 긱뉴스에 새로 등록된 뉴스들을 자동으로 트윗합니다",
+			"url": "https://t.co/Eeoeh5jF1C",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Eeoeh5jF1C",
+							"expanded_url": "https://news.hada.io",
+							"display_url": "news.hada.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3587,
+			"friends_count": 0,
+			"listed_count": 54,
+			"created_at": "Thu Jul 11 21:59:43 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2462,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149438560795107329/F1-aa31C_normal.png",
+			"profile_link_color": "000065",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Perfect Edition - 가벼운 반응형 웹 E-북 템플릿 https://t.co/Cu8rQvc54Z \n- 페이지 기반의 오픈소스 이북 뷰어 (Ruby코드)\n- 모바일을 비롯한 대부분의 브라우저 지원\n- Swipe,Tap,화살표,스페이스,마우스 휠로 이동 가능\n- 웹사이트와 EPUB를 동시에 생성\n- 소설가인 Robin Sloan 이 직접 ..."
+	},
+	{
 		"created_at": "Sat Aug 01 07:28:15 +0000 2020",
 		"id": 1289462936386318300,
 		"id_str": "1289462936386318336",
