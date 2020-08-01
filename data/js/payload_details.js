80c0d50ee25a7f4a0da21e5ffbb1dd49afdbea6b
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-01 10:02:19 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 62883,
-	"created_at": "Sat Aug 01 00:02:27 +0000 2020",
+	"tweets": 62885,
+	"created_at": "Sat Aug 01 01:02:18 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_08.js
@@ -0,0 +1,385 @@
+Grailbird.data.tweets_2020_08 = 
+[
+	{
+		"created_at": "Sat Aug 01 00:25:22 +0000 2020",
+		"id": 1289350281025613800,
+		"id_str": "1289356514629521408",
+		"full_text": "#BLACKPINK '24/365 with BLACKPINK' EP.5\n\n🎬https://t.co/imOIFkdAaR\n\n#블랙핑크 #JISOO #JENNIE #ROSÉ #LISA #24_365_WITH_BLACKPINK #EP5 #YOUTUBE #YG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						67,
+						72
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						73,
+						79
+					]
+				},
+				{
+					"text": "JENNIE",
+					"indices": [
+						80,
+						87
+					]
+				},
+				{
+					"text": "ROSÉ",
+					"indices": [
+						88,
+						93
+					]
+				},
+				{
+					"text": "LISA",
+					"indices": [
+						94,
+						99
+					]
+				},
+				{
+					"text": "24_365_WITH_BLACKPINK",
+					"indices": [
+						100,
+						122
+					]
+				},
+				{
+					"text": "EP5",
+					"indices": [
+						123,
+						127
+					]
+				},
+				{
+					"text": "YOUTUBE",
+					"indices": [
+						128,
+						136
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						137,
+						140
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/imOIFkdAaR",
+					"expanded_url": "https://youtu.be/UnGzwoYOEc8",
+					"display_url": "youtu.be/UnGzwoYOEc8",
+					"indices": [
+						42,
+						65
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 408177579,
+			"id_str": "408177579",
+			"name": "YG FAMILY",
+			"screen_name": "ygent_official",
+			"location": "Seoul, Korea",
+			"description": "#YG Entertainment official Twitter [https://t.co/06dwX0GzjL] #BIGBANG #WINNER #iKON #BLACKPINK #SECHSKIES #AKMU #DARA #TREASURE",
+			"url": "https://t.co/wXrDfyxWkS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wXrDfyxWkS",
+							"expanded_url": "https://www.instagram.com/yg_ent_official",
+							"display_url": "instagram.com/yg_ent_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/06dwX0GzjL",
+							"expanded_url": "http://youtube.com/ygentertainment",
+							"display_url": "youtube.com/ygentertainment",
+							"indices": [
+								36,
+								59
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6421572,
+			"friends_count": 38,
+			"listed_count": 6512,
+			"created_at": "Wed Nov 09 02:41:52 +0000 2011",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10317,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/869446110246428673/3vewgMjz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/869446110246428673/3vewgMjz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/408177579/1495160410",
+			"profile_link_color": "C82123",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "2E2E2E",
+			"profile_text_color": "FFFFFF",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10919,
+		"favorite_count": 29040,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK '24/365 with BLACKPINK' EP.5\n\n🎬https://t.co/imOIFkdAaR\n\n#블랙핑크 #JISOO #JENNIE #ROSÉ #LISA #24_365_WITH_BLACKPINK #EP5 #YOUTUBE #YG"
+	},
+	{
+		"created_at": "Sat Aug 01 00:25:14 +0000 2020",
+		"id": 1287909269895934000,
+		"id_str": "1289356480341139461",
+		"full_text": "Get ready for @ygofficialblink’s new album with all the essentials https://t.co/cozhtJQ07W https://t.co/palYgn0QuX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						14,
+						30
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cozhtJQ07W",
+					"expanded_url": "http://spoti.fi/thisisblackpink",
+					"display_url": "spoti.fi/thisisblackpink",
+					"indices": [
+						67,
+						90
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1287909260479742000,
+					"id_str": "1287909260479741952",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/Ed-SphXUwAAru2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ed-SphXUwAAru2t.jpg",
+					"url": "https://t.co/palYgn0QuX",
+					"display_url": "pic.twitter.com/palYgn0QuX",
+					"expanded_url": "https://twitter.com/Spotify/status/1287909269895933952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1287909260479742000,
+					"id_str": "1287909260479741952",
+					"indices": [
+						91,
+						114
+					],
+					"media_url": "http://pbs.twimg.com/media/Ed-SphXUwAAru2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ed-SphXUwAAru2t.jpg",
+					"url": "https://t.co/palYgn0QuX",
+					"display_url": "pic.twitter.com/palYgn0QuX",
+					"expanded_url": "https://twitter.com/Spotify/status/1287909269895933952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17230018,
+			"id_str": "17230018",
+			"name": "Spotify",
+			"screen_name": "Spotify",
+			"location": "",
+			"description": "Need support? We're happy to help at @SpotifyCares.\nNeed podcasts? @SpotifyPodcasts has you covered.",
+			"url": "https://t.co/jqAb654dV6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jqAb654dV6",
+							"expanded_url": "http://Spotify.com",
+							"display_url": "Spotify.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3664324,
+			"friends_count": 17766,
+			"listed_count": 15897,
+			"created_at": "Fri Nov 07 12:14:28 +0000 2008",
+			"favourites_count": 9640,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 32417,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1013614110171250688/vrPUCDn2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1013614110171250688/vrPUCDn2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17230018/1587502713",
+			"profile_link_color": "1ED760",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "ECEBE8",
+			"profile_text_color": "458DBF",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12506,
+		"favorite_count": 42631,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get ready for @ygofficialblink’s new album with all the essentials https://t.co/cozhtJQ07W https://t.co/palYgn0QuX"
+	}
+]
