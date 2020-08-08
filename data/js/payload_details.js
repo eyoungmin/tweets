21ee9242fd89a2cb4ca8813991c8bc139bb72bef
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-08 13:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63371,
-	"created_at": "Sat Aug 08 03:02:06 +0000 2020",
+	"tweets": 63372,
+	"created_at": "Sat Aug 08 04:02:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 08 03:25:11 +0000 2020",
+		"id": 1291932195141980200,
+		"id_str": "1291938479295930368",
+		"full_text": "[달로그 #12] 고원이는 오늘부터 아기치즈냥이애옹🧀🐾 \n이달의 소녀 공식 유튜브 채널에서 달로그를 확인하세요!\n⠀\n▶ https://t.co/7SUgo3Ok9R\n⠀\n#이달의소녀 #달로그 #고원 #LOONA #LOONALOG #GoWon https://t.co/xPQHwHl0bH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						93,
+						99
+					]
+				},
+				{
+					"text": "달로그",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "고원",
+					"indices": [
+						105,
+						108
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						109,
+						115
+					]
+				},
+				{
+					"text": "LOONALOG",
+					"indices": [
+						116,
+						125
+					]
+				},
+				{
+					"text": "GoWon",
+					"indices": [
+						126,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7SUgo3Ok9R",
+					"expanded_url": "https://youtu.be/mAea5fuDYV4",
+					"display_url": "youtu.be/mAea5fuDYV4",
+					"indices": [
+						67,
+						90
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1291932186854092800,
+					"id_str": "1291932186854092800",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee3dep9U8AA28mE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee3dep9U8AA28mE.jpg",
+					"url": "https://t.co/xPQHwHl0bH",
+					"display_url": "pic.twitter.com/xPQHwHl0bH",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1291932195141980160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1291932186854092800,
+					"id_str": "1291932186854092800",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee3dep9U8AA28mE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee3dep9U8AA28mE.jpg",
+					"url": "https://t.co/xPQHwHl0bH",
+					"display_url": "pic.twitter.com/xPQHwHl0bH",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1291932195141980160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 506338,
+			"friends_count": 0,
+			"listed_count": 5322,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2070,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3395,
+		"favorite_count": 9246,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[달로그 #12] 고원이는 오늘부터 아기치즈냥이애옹🧀🐾 \n이달의 소녀 공식 유튜브 채널에서 달로그를 확인하세요!\n⠀\n▶ https://t.co/7SUgo3Ok9R\n⠀\n#이달의소녀 #달로그 #고원 #LOONA #LOONALOG #GoWon https://t.co/xPQHwHl0bH"
+	},
+	{
 		"created_at": "Sat Aug 08 03:00:52 +0000 2020",
 		"id": 1291932361014349800,
 		"id_str": "1291932361014349824",
