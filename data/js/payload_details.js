21f1a9034ae1ab61b4f5608560df873b41bdf805
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-08 19:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63398,
-	"created_at": "Sat Aug 08 08:02:07 +0000 2020",
+	"tweets": 63399,
+	"created_at": "Sat Aug 08 10:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 08 09:25:12 +0000 2020",
+		"id": 1292022806939488300,
+		"id_str": "1292029079949123585",
+		"full_text": "#이달의소녀 2020 SUMMER PACKAGE의 청량함을 오프라인에서 직접 느껴볼 수 있는 CONCEPT ZONE🌴이 오는 8월 14일 오픈합니다!\n자세한 내용은 이달의 소녀 공식 팬카페를 통해 확인해 주세요💚\n\n▶️ https://t.co/XKuLryfjHE\n\n#LOONA https://t.co/suaRJ22rCb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						148,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XKuLryfjHE",
+					"expanded_url": "http://cafe.daum.net/loonatheworld",
+					"display_url": "cafe.daum.net/loonatheworld",
+					"indices": [
+						123,
+						146
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1292022798563471400,
+					"id_str": "1292022798563471360",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee4v484U8AAKrJI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee4v484U8AAKrJI.jpg",
+					"url": "https://t.co/suaRJ22rCb",
+					"display_url": "pic.twitter.com/suaRJ22rCb",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1292022806939488256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1292022798563471400,
+					"id_str": "1292022798563471360",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/Ee4v484U8AAKrJI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ee4v484U8AAKrJI.jpg",
+					"url": "https://t.co/suaRJ22rCb",
+					"display_url": "pic.twitter.com/suaRJ22rCb",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1292022806939488256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 506393,
+			"friends_count": 0,
+			"listed_count": 5318,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2071,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1873,
+		"favorite_count": 5775,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 2020 SUMMER PACKAGE의 청량함을 오프라인에서 직접 느껴볼 수 있는 CONCEPT ZONE🌴이 오는 8월 14일 오픈합니다!\n자세한 내용은 이달의 소녀 공식 팬카페를 통해 확인해 주세요💚\n\n▶️ https://t.co/XKuLryfjHE\n\n#LOONA https://t.co/suaRJ22rCb"
+	},
+	{
 		"created_at": "Sat Aug 08 07:59:22 +0000 2020",
 		"id": 1292007480143368200,
 		"id_str": "1292007480143368192",
