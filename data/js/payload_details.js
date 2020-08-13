11ecd8c3282bd32f2ab211c8dceac9e6bba31649
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-14 00:02:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63764,
-	"created_at": "Thu Aug 13 14:02:09 +0000 2020",
+	"tweets": 63765,
+	"created_at": "Thu Aug 13 15:02:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 13 14:25:17 +0000 2020",
+		"id": 1293904410486005800,
+		"id_str": "1293916538123280384",
+		"full_text": "[2020 소리바다 어워즈] 레드벨벳 아이린&amp;슬기, 본상+올해의 뮤직상 수상 \"뜻 깊은 날\"\n\n👉https://t.co/CQj6yy4ExN\n\n#레드벨벳 #RedVelvet #소리바다어워즈 #SOBA2020",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레드벨벳",
+					"indices": [
+						83,
+						88
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						89,
+						99
+					]
+				},
+				{
+					"text": "소리바다어워즈",
+					"indices": [
+						100,
+						108
+					]
+				},
+				{
+					"text": "SOBA2020",
+					"indices": [
+						109,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CQj6yy4ExN",
+					"expanded_url": "http://m.theceluv.com/article.php?aid=1597322863338056007&naver=1#_enliple",
+					"display_url": "m.theceluv.com/article.php?ai…",
+					"indices": [
+						58,
+						81
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/pjjIYiVH17",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pjjIYiVH17",
+							"expanded_url": "https://youtu.be/Ujb-gvqsoi0",
+							"display_url": "youtu.be/Ujb-gvqsoi0",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3029951,
+			"friends_count": 1,
+			"listed_count": 6449,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 59,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1485,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1265296830075527168/JLH8ZWPH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1265296830075527168/JLH8ZWPH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1577091773",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6527,
+		"favorite_count": 17411,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[2020 소리바다 어워즈] 레드벨벳 아이린&amp;슬기, 본상+올해의 뮤직상 수상 \"뜻 깊은 날\"\n\n👉https://t.co/CQj6yy4ExN\n\n#레드벨벳 #RedVelvet #소리바다어워즈 #SOBA2020"
+	},
+	{
 		"created_at": "Thu Aug 13 13:45:37 +0000 2020",
 		"id": 1293902807926415400,
 		"id_str": "1293906555599896583",
