e3ece775a13d6ff5a8262e1c7534c3cf131a1a63
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-13 20:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63760,
-	"created_at": "Thu Aug 13 10:02:10 +0000 2020",
+	"tweets": 63761,
+	"created_at": "Thu Aug 13 11:02:09 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Thu Aug 13 10:25:15 +0000 2020",
+		"id": 1293851597580058600,
+		"id_str": "1293856134856683521",
+		"full_text": "오빛! #이달의소녀 와 2020 소리바다어워즈에서 시원한 하루 보낼 준비됐어요? 보고 싶은 오빛을 위해 오늘도 멋진 무대 준비했으니 조금만 기다려 주세요✨\n\n#LOONA https://t.co/YAKkI7y5ia",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						4,
+						10
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						88,
+						94
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1293851582895808500,
+					"id_str": "1293851582895808512",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EfSvKNaU4AAN67w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfSvKNaU4AAN67w.jpg",
+					"url": "https://t.co/YAKkI7y5ia",
+					"display_url": "pic.twitter.com/YAKkI7y5ia",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1293851597580058624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1293851582895808500,
+					"id_str": "1293851582895808512",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EfSvKNaU4AAN67w.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfSvKNaU4AAN67w.jpg",
+					"url": "https://t.co/YAKkI7y5ia",
+					"display_url": "pic.twitter.com/YAKkI7y5ia",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1293851597580058624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1293851582916726800,
+					"id_str": "1293851582916726784",
+					"indices": [
+						95,
+						118
+					],
+					"media_url": "http://pbs.twimg.com/media/EfSvKNfUEAA7qJ8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfSvKNfUEAA7qJ8.jpg",
+					"url": "https://t.co/YAKkI7y5ia",
+					"display_url": "pic.twitter.com/YAKkI7y5ia",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1293851597580058624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 508048,
+			"friends_count": 0,
+			"listed_count": 5344,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2081,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6337,
+		"favorite_count": 14614,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛! #이달의소녀 와 2020 소리바다어워즈에서 시원한 하루 보낼 준비됐어요? 보고 싶은 오빛을 위해 오늘도 멋진 무대 준비했으니 조금만 기다려 주세요✨\n\n#LOONA https://t.co/YAKkI7y5ia"
+	},
+	{
 		"created_at": "Thu Aug 13 09:27:08 +0000 2020",
 		"id": 1293841507233886200,
 		"id_str": "1293841507233886209",
