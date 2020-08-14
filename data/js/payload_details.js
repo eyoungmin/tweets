a29c64e89295c977bab5e594ece6d52ea5ed24b3
Youngmin Lee <iameyoungmin@gmail.com>
2020-08-14 20:02:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 63814,
-	"created_at": "Fri Aug 14 10:02:12 +0000 2020",
+	"tweets": 63815,
+	"created_at": "Fri Aug 14 11:02:12 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 14 10:25:10 +0000 2020",
+		"id": 1294210443561463800,
+		"id_str": "1294218502086307841",
+		"full_text": "#이달의소녀\n금일 웨이팅 서비스가 종료 되었습니다.\nToday's waiting service has ended. https://t.co/7XsCJ44i9J",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1294210436984782800,
+					"id_str": "1294210436984782848",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/EfX1iREUwAAPScy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfX1iREUwAAPScy.jpg",
+					"url": "https://t.co/7XsCJ44i9J",
+					"display_url": "pic.twitter.com/7XsCJ44i9J",
+					"expanded_url": "https://twitter.com/withdrama/status/1294210443561463808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 846,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 875,
+							"h": 1241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1294210436984782800,
+					"id_str": "1294210436984782848",
+					"indices": [
+						64,
+						87
+					],
+					"media_url": "http://pbs.twimg.com/media/EfX1iREUwAAPScy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfX1iREUwAAPScy.jpg",
+					"url": "https://t.co/7XsCJ44i9J",
+					"display_url": "pic.twitter.com/7XsCJ44i9J",
+					"expanded_url": "https://twitter.com/withdrama/status/1294210443561463808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 846,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 875,
+							"h": 1241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 435785308,
+			"id_str": "435785308",
+			"name": "𝘄𝗶𝘁𝗵𝗱𝗿𝗮𝗺𝗮 위드드라마",
+			"screen_name": "withdrama",
+			"location": "",
+			"description": "Official Twitter Account of withdrama",
+			"url": "https://t.co/8u4XfVDeKL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8u4XfVDeKL",
+							"expanded_url": "http://www.withdrama.co.kr",
+							"display_url": "withdrama.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15578,
+			"friends_count": 3,
+			"listed_count": 24,
+			"created_at": "Tue Dec 13 12:54:24 +0000 2011",
+			"favourites_count": 10,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1124,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1229976407960195072/OgDWkCKt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1229976407960195072/OgDWkCKt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/435785308/1582084209",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 388,
+		"favorite_count": 2104,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀\n금일 웨이팅 서비스가 종료 되었습니다.\nToday's waiting service has ended. https://t.co/7XsCJ44i9J"
+	},
+	{
 		"created_at": "Fri Aug 14 09:27:56 +0000 2020",
 		"id": 1294197958972448800,
 		"id_str": "1294204098141716480",
