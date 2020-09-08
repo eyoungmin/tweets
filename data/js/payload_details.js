78b42f366928129bd06f870a6c060c53813e8a59
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-08 15:03:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65326,
-	"created_at": "Tue Sep 08 05:02:05 +0000 2020",
+	"tweets": 65327,
+	"created_at": "Tue Sep 08 06:03:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 08 05:44:56 +0000 2020",
+		"id": 1303199900100964400,
+		"id_str": "1303207673731715072",
+		"full_text": "짜잔~이번주 토요일 9시 #아는형님 에서 만나용🙌🏻\nJTBC수목드라마 #사생활 #서현 #고경표 https://t.co/gRNzyPhKmq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			76
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아는형님",
+					"indices": [
+						14,
+						19
+					]
+				},
+				{
+					"text": "사생활",
+					"indices": [
+						39,
+						43
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						44,
+						47
+					]
+				},
+				{
+					"text": "고경표",
+					"indices": [
+						48,
+						52
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gRNzyPhKmq",
+					"expanded_url": "https://www.instagram.com/p/CE3P68qger0/?igshid=ws3675qezv6o",
+					"display_url": "instagram.com/p/CE3P68qger0/…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2173631072,
+			"id_str": "2173631072",
+			"name": "서현(서주현)",
+			"screen_name": "sjhsjh0628",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1383356,
+			"friends_count": 31,
+			"listed_count": 2863,
+			"created_at": "Mon Nov 04 08:58:47 +0000 2013",
+			"favourites_count": 6,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1262,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1146333352682647552/0h6dsbYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2173631072/1526914876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 252,
+		"favorite_count": 590,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "짜잔~이번주 토요일 9시 #아는형님 에서 만나용🙌🏻\nJTBC수목드라마 #사생활 #서현 #고경표 https://t.co/gRNzyPhKmq"
+	},
+	{
 		"created_at": "Tue Sep 08 04:47:36 +0000 2020",
 		"id": 1303193245133004800,
 		"id_str": "1303193245133004803",
