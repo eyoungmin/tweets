167dcd3675dd09aaf20a3a736d8ad2ed002aac97
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-09 16:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65375,
-	"created_at": "Wed Sep 09 06:02:06 +0000 2020",
+	"tweets": 65376,
+	"created_at": "Wed Sep 09 07:02:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Wed Sep 09 06:44:55 +0000 2020",
+		"id": 1303576465418547200,
+		"id_str": "1303585158738608130",
+		"full_text": "[#진솔 / #JinSoul] 떠나는 여름을 보내면서💙 오빛들 잘 지내구 있죠? 언능 보고 싶다! 건강 관리 잘해요🥺\n\n#이달의소녀 #LOONA https://t.co/HlCzEVZh6Q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "진솔",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "JinSoul",
+					"indices": [
+						7,
+						15
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						67,
+						73
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						74,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1303576445625614300,
+					"id_str": "1303576445625614337",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehc73uMUwAEEw4N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehc73uMUwAEEw4N.jpg",
+					"url": "https://t.co/HlCzEVZh6Q",
+					"display_url": "pic.twitter.com/HlCzEVZh6Q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1303576465418547201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1303576445625614300,
+					"id_str": "1303576445625614337",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehc73uMUwAEEw4N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehc73uMUwAEEw4N.jpg",
+					"url": "https://t.co/HlCzEVZh6Q",
+					"display_url": "pic.twitter.com/HlCzEVZh6Q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1303576465418547201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1303576445621420000,
+					"id_str": "1303576445621420033",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehc73uLUwAEMlym.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehc73uLUwAEMlym.jpg",
+					"url": "https://t.co/HlCzEVZh6Q",
+					"display_url": "pic.twitter.com/HlCzEVZh6Q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1303576465418547201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1303576445621428200,
+					"id_str": "1303576445621428226",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/Ehc73uLU4AICy_c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ehc73uLU4AICy_c.jpg",
+					"url": "https://t.co/HlCzEVZh6Q",
+					"display_url": "pic.twitter.com/HlCzEVZh6Q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1303576465418547201/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 518086,
+			"friends_count": 0,
+			"listed_count": 5393,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2134,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5241,
+		"favorite_count": 13225,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#진솔 / #JinSoul] 떠나는 여름을 보내면서💙 오빛들 잘 지내구 있죠? 언능 보고 싶다! 건강 관리 잘해요🥺\n\n#이달의소녀 #LOONA https://t.co/HlCzEVZh6Q"
+	},
+	{
 		"created_at": "Wed Sep 09 05:44:55 +0000 2020",
 		"id": 1303560904060153900,
 		"id_str": "1303570059445571585",
