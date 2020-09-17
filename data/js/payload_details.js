c9fa03c604776704f63f01a5081c99a8468a855e
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-17 22:02:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65737,
-	"created_at": "Thu Sep 17 12:02:13 +0000 2020",
+	"tweets": 65738,
+	"created_at": "Thu Sep 17 13:02:11 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Thu Sep 17 12:57:20 +0000 2020",
+		"id": 1306577980207108000,
+		"id_str": "1306577980207108096",
+		"full_text": "#Melon #멜론 #포토\nCHUNG HA x Christopher ‘Bad Boy’\n▶ https://t.co/uQdE8YlXSz\n원본 : https://t.co/EvbVzYaA8N\n#청하 #CHUNGHA https://t.co/iJUUOGgmeS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Melon",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "멜론",
+					"indices": [
+						7,
+						10
+					]
+				},
+				{
+					"text": "포토",
+					"indices": [
+						11,
+						14
+					]
+				},
+				{
+					"text": "청하",
+					"indices": [
+						103,
+						106
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						107,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uQdE8YlXSz",
+					"expanded_url": "https://www.melon.com/artist/photo_detail.htm?artistId=968265&photoId=80111419&orderBy=NEW",
+					"display_url": "melon.com/artist/photo_d…",
+					"indices": [
+						50,
+						73
+					]
+				},
+				{
+					"url": "https://t.co/EvbVzYaA8N",
+					"expanded_url": "https://cdnimg.melon.co.kr/cm2/photo/images/000/801/11/419/80111419_20200917174405_org.jpg",
+					"display_url": "cdnimg.melon.co.kr/cm2/photo/imag…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1306577978596487200,
+					"id_str": "1306577978596487170",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/EiHlv72XcAIKKAQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiHlv72XcAIKKAQ.jpg",
+					"url": "https://t.co/iJUUOGgmeS",
+					"display_url": "pic.twitter.com/iJUUOGgmeS",
+					"expanded_url": "https://twitter.com/fromminn/status/1306577980207108096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1306577978596487200,
+					"id_str": "1306577978596487170",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/EiHlv72XcAIKKAQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiHlv72XcAIKKAQ.jpg",
+					"url": "https://t.co/iJUUOGgmeS",
+					"display_url": "pic.twitter.com/iJUUOGgmeS",
+					"expanded_url": "https://twitter.com/fromminn/status/1306577980207108096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 303,
+			"friends_count": 946,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 42931,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 67900,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Melon #멜론 #포토\nCHUNG HA x Christopher ‘Bad Boy’\n▶ https://t.co/uQdE8YlXSz\n원본 : https://t.co/EvbVzYaA8N\n#청하 #CHUNGHA https://t.co/iJUUOGgmeS"
+	},
+	{
 		"created_at": "Thu Sep 17 11:54:17 +0000 2020",
 		"id": 1306562113721315300,
 		"id_str": "1306562113721315330",
