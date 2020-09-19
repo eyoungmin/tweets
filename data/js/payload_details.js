dc347f73bd3c75984522ac7a8e9fb4da39e493f2
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-19 22:02:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65857,
-	"created_at": "Sat Sep 19 12:02:14 +0000 2020",
+	"tweets": 65858,
+	"created_at": "Sat Sep 19 13:02:12 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sat Sep 19 12:34:24 +0000 2020",
+		"id": 1307290166055198700,
+		"id_str": "1307296985347764224",
+		"full_text": "EP /BE/\n\nD-2\n\n2020.09.21 12PM (KST)\n\n✨✨✨\n\nProduced by YESEO\nComposed by YESEO\nArranged by YESEO\nLyrics by YESEO\n \nPhotograph by Kim Moon Dog\nHair &amp; Make-up by Lee Eun Seo\nStyling by Mo Eun Seo\n\nClothes Sponsored by JCHOI \nHead Piece Sponsored by Q millinery https://t.co/qpwCb8LAX2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1307290161047249000,
+					"id_str": "1307290161047248897",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EiRteaOVoAETrQp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiRteaOVoAETrQp.jpg",
+					"url": "https://t.co/qpwCb8LAX2",
+					"display_url": "pic.twitter.com/qpwCb8LAX2",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1307290166055198721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 549,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1394,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 968,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1307290161047249000,
+					"id_str": "1307290161047248897",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/EiRteaOVoAETrQp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiRteaOVoAETrQp.jpg",
+					"url": "https://t.co/qpwCb8LAX2",
+					"display_url": "pic.twitter.com/qpwCb8LAX2",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1307290166055198721/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 549,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1394,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 968,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 893011871095181300,
+			"id_str": "893011871095181312",
+			"name": "YESEO",
+			"screen_name": "yeseoofficial",
+			"location": "대한민국 서울",
+			"description": "Producer / Singer",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 680,
+			"friends_count": 2,
+			"listed_count": 13,
+			"created_at": "Thu Aug 03 07:33:16 +0000 2017",
+			"favourites_count": 54,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 88,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1305497822712623104/FY-WtTfo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1305497822712623104/FY-WtTfo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/893011871095181312/1584516876",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "EP /BE/\n\nD-2\n\n2020.09.21 12PM (KST)\n\n✨✨✨\n\nProduced by YESEO\nComposed by YESEO\nArranged by YESEO\nLyrics by YESEO\n \nPhotograph by Kim Moon Dog\nHair &amp; Make-up by Lee Eun Seo\nStyling by Mo Eun Seo\n\nClothes Sponsored by JCHOI \nHead Piece Sponsored by Q millinery https://t.co/qpwCb8LAX2"
+	},
+	{
 		"created_at": "Sat Sep 19 11:18:25 +0000 2020",
 		"id": 1307143321844768800,
 		"id_str": "1307277864262270977",
