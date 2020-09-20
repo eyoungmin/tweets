cfb264e97d2d81f46399b0e72725c1d2ab1ededf
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-20 13:02:16 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65867,
-	"created_at": "Sun Sep 20 03:02:11 +0000 2020",
+	"tweets": 65868,
+	"created_at": "Sun Sep 20 04:02:14 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Sun Sep 20 03:34:24 +0000 2020",
+		"id": 1307514959841034200,
+		"id_str": "1307523477566558209",
+		"full_text": "프로 MC #이브 를 보는 순간 단 5.24초 만에 사랑에 빠지게 될 걸요? 잠시 후 오후 12시 30분(KST) 방영되는 TBS ‘팩트인스타’에서 만나요🤍\n⠀\n#이달의소녀 #LOONA #Yves https://t.co/fB9cCZubxS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이브",
+					"indices": [
+						6,
+						9
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						90,
+						96
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						97,
+						103
+					]
+				},
+				{
+					"text": "Yves",
+					"indices": [
+						104,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1307514952379367400,
+					"id_str": "1307514952379367425",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/EiU56_MVgAEI0SS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiU56_MVgAEI0SS.jpg",
+					"url": "https://t.co/fB9cCZubxS",
+					"display_url": "pic.twitter.com/fB9cCZubxS",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1307514959841034240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1020,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1020,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1307514952379367400,
+					"id_str": "1307514952379367425",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/EiU56_MVgAEI0SS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EiU56_MVgAEI0SS.jpg",
+					"url": "https://t.co/fB9cCZubxS",
+					"display_url": "pic.twitter.com/fB9cCZubxS",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1307514959841034240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1020,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1020,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 528349,
+			"friends_count": 0,
+			"listed_count": 5552,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2151,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5516,
+		"favorite_count": 16104,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "프로 MC #이브 를 보는 순간 단 5.24초 만에 사랑에 빠지게 될 걸요? 잠시 후 오후 12시 30분(KST) 방영되는 TBS ‘팩트인스타’에서 만나요🤍\n⠀\n#이달의소녀 #LOONA #Yves https://t.co/fB9cCZubxS"
+	},
+	{
 		"created_at": "Sun Sep 20 03:01:07 +0000 2020",
 		"id": 1307515103667990500,
 		"id_str": "1307515103667990529",
