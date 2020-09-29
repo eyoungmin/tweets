36621add868e739ef612cf40b931f39c1983541e
Youngmin Lee <iameyoungmin@gmail.com>
2020-09-30 06:02:19 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66515,
-	"created_at": "Tue Sep 29 16:02:27 +0000 2020",
+	"tweets": 66516,
+	"created_at": "Tue Sep 29 21:02:17 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_09.js",
 		"year": 2020,
 		"var_name": "tweets_2020_09",
-		"tweet_count": 1563,
+		"tweet_count": 1564,
 		"month": 9
 	},
 	{
