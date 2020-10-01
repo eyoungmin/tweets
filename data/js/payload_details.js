ea60f45bd5318b74560775696d846bc0866c369b
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-01 16:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66590,
-	"created_at": "Thu Oct 01 04:02:02 +0000 2020",
+	"tweets": 66591,
+	"created_at": "Thu Oct 01 07:02:01 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 01 07:01:47 +0000 2020",
+		"id": 1311561933276016600,
+		"id_str": "1311561933276016640",
+		"full_text": "#少女時代 #GirlsGeneration_Japan #소녀시대_일본\nテヨン、オンラインイベント招待が当たるLINE MUSICキャンペーンがスタート！\nhttps://t.co/I4he5HUIoJ\n#소녀시대 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "少女時代",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "GirlsGeneration_Japan",
+					"indices": [
+						6,
+						28
+					]
+				},
+				{
+					"text": "소녀시대_일본",
+					"indices": [
+						29,
+						37
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						104,
+						109
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						110,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/I4he5HUIoJ",
+					"expanded_url": "http://girls-generation.jp/gg/news/linemusic.html",
+					"display_url": "girls-generation.jp/gg/news/linemu…",
+					"indices": [
+						80,
+						103
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301,
+			"friends_count": 952,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 68799,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "#少女時代 #GirlsGeneration_Japan #소녀시대_일본\nテヨン、オンラインイベント招待が当たるLINE MUSICキャンペーンがスタート！\nhttps://t.co/I4he5HUIoJ\n#소녀시대 #GirlsGeneration"
+	},
+	{
 		"created_at": "Thu Oct 01 03:45:42 +0000 2020",
 		"id": 1311501090320515000,
 		"id_str": "1311512587562110976",
@@ -1000,7 +1129,7 @@ Grailbird.data.tweets_2020_10 =
 			"name": "LINE MUSIC",
 			"screen_name": "LINEMUSIC_JP",
 			"location": "",
-			"description": "LINEの音楽ストリーミングサービス #LINEMUSIC \nおにし👩🏻たかってぃ🤱つっきー👩🏻‍🦱えってぃ👧🏻が旬な音楽情報をお届け🎁\n/✍️:https://t.co/r1HWxRsYBC\n/📷:https://t.co/u0zRxurTRy\n/お問合せ👉https://t.co/TFvmtwWpMO",
+			"description": "LINEの音楽ストリーミングサービス #LINEMUSIC \nおにし👩🏻たかってぃ🤱つっきー👩🏻🦱えってぃ👧🏻が旬な音楽情報をお届け🎁\n/✍️:https://t.co/r1HWxRsYBC\n/📷:https://t.co/u0zRxurTRy\n/お問合せ👉https://t.co/TFvmtwWpMO",
 			"url": "https://t.co/329xsvBLof",
 			"entities": {
 				"url": {
