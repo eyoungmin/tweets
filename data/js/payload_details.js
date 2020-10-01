6346a495d95bcb053acd8d9e736978840332e9ae
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-01 18:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66612,
-	"created_at": "Thu Oct 01 08:02:02 +0000 2020",
+	"tweets": 66613,
+	"created_at": "Thu Oct 01 09:02:01 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 01 08:27:11 +0000 2020",
+		"id": 1311576605655933000,
+		"id_str": "1311583425556602880",
+		"full_text": "오빛! 잠시 후 오후 5시 40분(KST) #츄 #OliviaHye 와 함께 모바일 세상으로 빠져들 준비됐어요? MBC ‘2020 추석특집 아이돌 e스포츠 선수권대회’ 같이 본방사수해요🕹\n\n#이달의소녀 #LOONA #Chuu https://t.co/9ooG1FBq5f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "츄",
+					"indices": [
+						24,
+						26
+					]
+				},
+				{
+					"text": "OliviaHye",
+					"indices": [
+						27,
+						37
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						106,
+						112
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						113,
+						119
+					]
+				},
+				{
+					"text": "Chuu",
+					"indices": [
+						120,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1311574991016607700,
+					"id_str": "1311574991016607744",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/EjOmgVvUwAArtUe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjOmgVvUwAArtUe.jpg",
+					"url": "https://t.co/9ooG1FBq5f",
+					"display_url": "pic.twitter.com/9ooG1FBq5f",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1311576605655932928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1311574991016607700,
+					"id_str": "1311574991016607744",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/EjOmgVvUwAArtUe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjOmgVvUwAArtUe.jpg",
+					"url": "https://t.co/9ooG1FBq5f",
+					"display_url": "pic.twitter.com/9ooG1FBq5f",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1311576605655932928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1311575007252770800,
+					"id_str": "1311575007252770817",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/EjOmhSOU8AENhNE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjOmhSOU8AENhNE.jpg",
+					"url": "https://t.co/9ooG1FBq5f",
+					"display_url": "pic.twitter.com/9ooG1FBq5f",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1311576605655932928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 750,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 750,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 543532,
+			"friends_count": 0,
+			"listed_count": 5668,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2194,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4929,
+		"favorite_count": 13023,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛! 잠시 후 오후 5시 40분(KST) #츄 #OliviaHye 와 함께 모바일 세상으로 빠져들 준비됐어요? MBC ‘2020 추석특집 아이돌 e스포츠 선수권대회’ 같이 본방사수해요🕹\n\n#이달의소녀 #LOONA #Chuu https://t.co/9ooG1FBq5f"
+	},
+	{
 		"created_at": "Thu Oct 01 07:04:34 +0000 2020",
 		"id": 1311562635289276400,
 		"id_str": "1311562635289276417",
