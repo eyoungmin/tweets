7503b0194100a25e21f39111bd5a48d20266e198
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-02 22:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66716,
-	"created_at": "Fri Oct 02 11:02:04 +0000 2020",
+	"tweets": 66717,
+	"created_at": "Fri Oct 02 13:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 02 12:11:17 +0000 2020",
+		"id": 1311916572739395600,
+		"id_str": "1312002212109848578",
+		"full_text": "“어떻게 사는 것이 맞을까? 어느 날 알 것 같다가도 정말 모르겠어.”\n\n오늘밤 11시 30분 KBS1 독립영화관에서 &lt;벌새&gt;가 방영됩니다. https://t.co/9FDdXTlIwc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1311916567819481000,
+					"id_str": "1311916567819481088",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EjTdKufUcAAHVI4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjTdKufUcAAHVI4.jpg",
+					"url": "https://t.co/9FDdXTlIwc",
+					"display_url": "pic.twitter.com/9FDdXTlIwc",
+					"expanded_url": "https://twitter.com/csangsangmadang/status/1311916572739395589/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1311916567819481000,
+					"id_str": "1311916567819481088",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EjTdKufUcAAHVI4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EjTdKufUcAAHVI4.jpg",
+					"url": "https://t.co/9FDdXTlIwc",
+					"display_url": "pic.twitter.com/9FDdXTlIwc",
+					"expanded_url": "https://twitter.com/csangsangmadang/status/1311916572739395589/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 414,
+							"h": 224,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 123470069,
+			"id_str": "123470069",
+			"name": "KT&G 상상마당 시네마",
+			"screen_name": "csangsangmadang",
+			"location": "서울 홍대 (Hongdae, Seoul)",
+			"description": "",
+			"url": "https://t.co/QfaNWod0Kp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QfaNWod0Kp",
+							"expanded_url": "http://www.sangsangmadang.com",
+							"display_url": "sangsangmadang.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 163832,
+			"friends_count": 5550,
+			"listed_count": 488,
+			"created_at": "Tue Mar 16 05:41:08 +0000 2010",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 36767,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/773840506543480833/SMq_1VKG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/773840506543480833/SMq_1VKG_normal.jpg",
+			"profile_link_color": "990000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 792,
+		"favorite_count": 406,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "“어떻게 사는 것이 맞을까? 어느 날 알 것 같다가도 정말 모르겠어.”\n\n오늘밤 11시 30분 KBS1 독립영화관에서 &lt;벌새&gt;가 방영됩니다. https://t.co/9FDdXTlIwc"
+	},
+	{
 		"created_at": "Fri Oct 02 10:57:02 +0000 2020",
 		"id": 1311633424239616000,
 		"id_str": "1311983524853899266",
