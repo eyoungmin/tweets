866d46a46c3385ac400e0cfc536696c062a78afb
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-05 21:02:05 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66880,
-	"created_at": "Mon Oct 05 11:02:06 +0000 2020",
+	"tweets": 66883,
+	"created_at": "Mon Oct 05 12:02:04 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,664 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 05 11:29:16 +0000 2020",
+		"id": 1313078802264854500,
+		"id_str": "1313078802264854528",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘아는 형님’ 8일 녹화→17일 방송..韓 예능 점령 예고\nhttps://t.co/NxfoyQ3Yn4\n#블랙핑크 #BLACKPINK https://t.co/YMJUDMEK47",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						80,
+						85
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						86,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NxfoyQ3Yn4",
+					"expanded_url": "https://yg-life.com/archives/160751?lang=ko",
+					"display_url": "yg-life.com/archives/16075…",
+					"indices": [
+						56,
+						79
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1313078801023357000,
+					"id_str": "1313078801023356929",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejj-NmkX0AEZSyx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejj-NmkX0AEZSyx.jpg",
+					"url": "https://t.co/YMJUDMEK47",
+					"display_url": "pic.twitter.com/YMJUDMEK47",
+					"expanded_url": "https://twitter.com/fromminn/status/1313078802264854528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313078801023357000,
+					"id_str": "1313078801023356929",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejj-NmkX0AEZSyx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejj-NmkX0AEZSyx.jpg",
+					"url": "https://t.co/YMJUDMEK47",
+					"display_url": "pic.twitter.com/YMJUDMEK47",
+					"expanded_url": "https://twitter.com/fromminn/status/1313078802264854528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 514,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 299,
+			"friends_count": 958,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43443,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69108,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘아는 형님’ 8일 녹화→17일 방송..韓 예능 점령 예고\nhttps://t.co/NxfoyQ3Yn4\n#블랙핑크 #BLACKPINK https://t.co/YMJUDMEK47"
+	},
+	{
+		"created_at": "Mon Oct 05 11:27:05 +0000 2020",
+		"id": 1312978383916023800,
+		"id_str": "1313078249598181376",
+		"full_text": "Sometimes all we want is a bad boy. But do we really though?\n\n@CHUNGHA_MNHent teams up with @StopherMusic and brings that story to life.\n\nWatch CHUNG HA’s interview below and enjoy their latest release~\n\n#CHUNGHA #Christopher #BadBoy\n\nhttps://t.co/3I0N77PFoR https://t.co/YSHn0JelK6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						204,
+						212
+					]
+				},
+				{
+					"text": "Christopher",
+					"indices": [
+						213,
+						225
+					]
+				},
+				{
+					"text": "BadBoy",
+					"indices": [
+						226,
+						233
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CHUNGHA_MNHent",
+					"name": "CHUNG HA",
+					"id": 1097557125742489600,
+					"id_str": "1097557125742489600",
+					"indices": [
+						62,
+						77
+					]
+				},
+				{
+					"screen_name": "StopherMusic",
+					"name": "Christopher",
+					"id": 361107109,
+					"id_str": "361107109",
+					"indices": [
+						92,
+						105
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3I0N77PFoR",
+					"expanded_url": "http://spoti.fi/BadBoy-CHUNGHA",
+					"display_url": "spoti.fi/BadBoy-CHUNGHA",
+					"indices": [
+						235,
+						258
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1312978021515579400,
+					"id_str": "1312978021515579393",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1312978021515579393/pu/img/LYyNYWgTdgHqanW5.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1312978021515579393/pu/img/LYyNYWgTdgHqanW5.jpg",
+					"url": "https://t.co/YSHn0JelK6",
+					"display_url": "pic.twitter.com/YSHn0JelK6",
+					"expanded_url": "https://twitter.com/SpotifyKDaebak/status/1312978383916023808/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1312978021515579400,
+					"id_str": "1312978021515579393",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1312978021515579393/pu/img/LYyNYWgTdgHqanW5.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1312978021515579393/pu/img/LYyNYWgTdgHqanW5.jpg",
+					"url": "https://t.co/YSHn0JelK6",
+					"display_url": "pic.twitter.com/YSHn0JelK6",
+					"expanded_url": "https://twitter.com/SpotifyKDaebak/status/1312978383916023808/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 75000,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1312978021515579393/pu/vid/720x720/8QiDFlYL_QfD6l7_.mp4?tag=10"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1312978021515579393/pu/vid/320x320/ICqPY4JU4WdDid95.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1312978021515579393/pu/vid/480x480/A2CuPLfBmtQAlk2w.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1312978021515579393/pu/pl/s77kFDv_VdoyJOad.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 978459450871697400,
+			"id_str": "978459450871697408",
+			"name": "Spotify ❤️ K-Pop",
+			"screen_name": "SpotifyKDaebak",
+			"location": "",
+			"description": "Do you eat, sleep, and breathe K-Pop like we do? For all the news, music, entertainment and more, follow our official Spotify K-pop profile right now.",
+			"url": "https://t.co/TSbcWtrINY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TSbcWtrINY",
+							"expanded_url": "http://spoti.fi/KPOP",
+							"display_url": "spoti.fi/KPOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 311386,
+			"friends_count": 207,
+			"listed_count": 195,
+			"created_at": "Tue Mar 27 02:31:26 +0000 2018",
+			"favourites_count": 760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267694939460169728/yQiFh85w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267694939460169728/yQiFh85w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/978459450871697408/1591077004",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 236,
+		"favorite_count": 995,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sometimes all we want is a bad boy. But do we really though?\n\n@CHUNGHA_MNHent teams up with @StopherMusic and brings that story to life.\n\nWatch CHUNG HA’s interview below and enjoy their latest release~\n\n#CHUNGHA #Christopher #BadBoy\n\nhttps://t.co/3I0N77PFoR https://t.co/YSHn0JelK6"
+	},
+	{
+		"created_at": "Mon Oct 05 11:19:20 +0000 2020",
+		"id": 1313076300425371600,
+		"id_str": "1313076300425371648",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘러브식 걸즈’ 뮤직비디오 1억뷰 돌파…통산 22번째 억대뷰\nhttps://t.co/5sEWJqTWUF\n#블랙핑크 #BLACKPINK https://t.co/w8l0zFHnrd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						81,
+						86
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						87,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5sEWJqTWUF",
+					"expanded_url": "https://yg-life.com/archives/160744?lang=ko",
+					"display_url": "yg-life.com/archives/16074…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1313076299414540300,
+					"id_str": "1313076299414540288",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejj77_WWkAAfd1X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejj77_WWkAAfd1X.jpg",
+					"url": "https://t.co/w8l0zFHnrd",
+					"display_url": "pic.twitter.com/w8l0zFHnrd",
+					"expanded_url": "https://twitter.com/fromminn/status/1313076300425371648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313076299414540300,
+					"id_str": "1313076299414540288",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejj77_WWkAAfd1X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejj77_WWkAAfd1X.jpg",
+					"url": "https://t.co/w8l0zFHnrd",
+					"display_url": "pic.twitter.com/w8l0zFHnrd",
+					"expanded_url": "https://twitter.com/fromminn/status/1313076300425371648/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 478,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 299,
+			"friends_count": 958,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43443,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69108,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘러브식 걸즈’ 뮤직비디오 1억뷰 돌파…통산 22번째 억대뷰\nhttps://t.co/5sEWJqTWUF\n#블랙핑크 #BLACKPINK https://t.co/w8l0zFHnrd"
+	},
+	{
 		"created_at": "Mon Oct 05 10:59:22 +0000 2020",
 		"id": 1313057808040902700,
 		"id_str": "1313071277230288896",
