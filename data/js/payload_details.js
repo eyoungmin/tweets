0428ec2f943ae361405d17938f6870f63fa1f736
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-07 14:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66987,
-	"created_at": "Wed Oct 07 04:02:07 +0000 2020",
+	"tweets": 66988,
+	"created_at": "Wed Oct 07 05:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Wed Oct 07 04:59:47 +0000 2020",
+		"id": 1313705559393284000,
+		"id_str": "1313705559393284096",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘음중’·’인가’서 ‘Lovesick Girls’·’Pretty Savage’ 무대 펼친다\nhttps://t.co/c7AEYqJxY6\n#블랙핑크 #BLACKPINK https://t.co/1atSvDHdAo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						98,
+						103
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						104,
+						114
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c7AEYqJxY6",
+					"expanded_url": "https://yg-life.com/archives/160838?lang=ko",
+					"display_url": "yg-life.com/archives/16083…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1313705558550118400,
+					"id_str": "1313705558550118406",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejs4PsTWAAYTCDk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejs4PsTWAAYTCDk.jpg",
+					"url": "https://t.co/1atSvDHdAo",
+					"display_url": "pic.twitter.com/1atSvDHdAo",
+					"expanded_url": "https://twitter.com/fromminn/status/1313705559393284096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1313705558550118400,
+					"id_str": "1313705558550118406",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/Ejs4PsTWAAYTCDk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ejs4PsTWAAYTCDk.jpg",
+					"url": "https://t.co/1atSvDHdAo",
+					"display_url": "pic.twitter.com/1atSvDHdAo",
+					"expanded_url": "https://twitter.com/fromminn/status/1313705559393284096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 298,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 961,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69232,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, ‘음중’·’인가’서 ‘Lovesick Girls’·’Pretty Savage’ 무대 펼친다\nhttps://t.co/c7AEYqJxY6\n#블랙핑크 #BLACKPINK https://t.co/1atSvDHdAo"
+	},
+	{
 		"created_at": "Wed Oct 07 04:01:28 +0000 2020",
 		"id": 1313690881610875000,
 		"id_str": "1313690881610874880",
