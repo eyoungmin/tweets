fb127614ee435110c0e95540c5c0404423d73450
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-08 10:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67043,
-	"created_at": "Thu Oct 08 00:02:06 +0000 2020",
+	"tweets": 67044,
+	"created_at": "Thu Oct 08 01:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 08 00:51:59 +0000 2020",
+		"id": 1313743361950457900,
+		"id_str": "1314005587416424448",
+		"full_text": "\"간통죄 폐지가 간통하라고 하는 것이 아니듯, 낙태죄 폐지가 낙태하라고 하는 것이 아니다. '낙태죄\n'가 두려워 낙태 않는 여성은 없다. '불법화된 낙태'로 고통받는 여성만 있을 뿐\"\n\nhttps://t.co/Vv8iG6OvFw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Vv8iG6OvFw",
+					"expanded_url": "https://news.khan.kr/g5KG",
+					"display_url": "news.khan.kr/g5KG",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 118720645,
+			"id_str": "118720645",
+			"name": "경향신문",
+			"screen_name": "kyunghyang",
+			"location": "seoul",
+			"description": "'The Kyunghyang daily news' is  a daily newspaper in South Korea. The 'Kyunghyang' means ‘Urbi et Orbi(to the city and to the world)’.",
+			"url": "https://t.co/1CeEMiCmWg",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1CeEMiCmWg",
+							"expanded_url": "http://www.khan.co.kr/",
+							"display_url": "khan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 722025,
+			"friends_count": 77625,
+			"listed_count": 5916,
+			"created_at": "Mon Mar 01 15:15:28 +0000 2010",
+			"favourites_count": 17,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 86917,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5FA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135350054401716224/hrx70Bwa_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135350054401716224/hrx70Bwa_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/118720645/1559523510",
+			"profile_link_color": "07B6EB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3688,
+		"favorite_count": 1146,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"간통죄 폐지가 간통하라고 하는 것이 아니듯, 낙태죄 폐지가 낙태하라고 하는 것이 아니다. '낙태죄\n'가 두려워 낙태 않는 여성은 없다. '불법화된 낙태'로 고통받는 여성만 있을 뿐\"\n\nhttps://t.co/Vv8iG6OvFw"
+	},
+	{
 		"created_at": "Wed Oct 07 23:54:44 +0000 2020",
 		"id": 1313887935498875000,
 		"id_str": "1313991177884954625",
