6f06d926c77c820c9555f4420c499970ac8a2359
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-10 07:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67173,
-	"created_at": "Fri Oct 09 21:02:15 +0000 2020",
+	"tweets": 67174,
+	"created_at": "Fri Oct 09 22:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Fri Oct 09 21:16:34 +0000 2020",
+		"id": 1314676148790612000,
+		"id_str": "1314676148790611968",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학] 정치인들이 TV토론에 망해도 괜찮은 이유\nhttps://t.co/6O2LR0FUKP\n#박진영 #심리학 #minfav https://t.co/ETfgTwEvFu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						71,
+						75
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						81,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6O2LR0FUKP",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=40495",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1314676147280654300,
+					"id_str": "1314676147280654340",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/Ej6q_ZFXsAQQK2H.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ej6q_ZFXsAQQK2H.png",
+					"url": "https://t.co/ETfgTwEvFu",
+					"display_url": "pic.twitter.com/ETfgTwEvFu",
+					"expanded_url": "https://twitter.com/fromminn/status/1314676148790611968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1314676147280654300,
+					"id_str": "1314676147280654340",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/Ej6q_ZFXsAQQK2H.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ej6q_ZFXsAQQK2H.png",
+					"url": "https://t.co/ETfgTwEvFu",
+					"display_url": "pic.twitter.com/ETfgTwEvFu",
+					"expanded_url": "https://twitter.com/fromminn/status/1314676148790611968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 241,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 964,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43531,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학] 정치인들이 TV토론에 망해도 괜찮은 이유\nhttps://t.co/6O2LR0FUKP\n#박진영 #심리학 #minfav https://t.co/ETfgTwEvFu"
+	},
+	{
 		"created_at": "Fri Oct 09 20:54:08 +0000 2020",
 		"id": 1314670506520449000,
 		"id_str": "1314670506520449029",
