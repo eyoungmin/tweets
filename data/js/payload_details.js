77f9ba682866c86dacc3bf5f9ecec36c8f4459d6
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-10 17:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67193,
-	"created_at": "Sat Oct 10 07:02:10 +0000 2020",
+	"tweets": 67194,
+	"created_at": "Sat Oct 10 08:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,151 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sat Oct 10 07:27:05 +0000 2020",
+		"id": 1314820031717339100,
+		"id_str": "1314829790675972103",
+		"full_text": "#BLACKPINKINYOURAREA \n#BLACKPINK 의 #LovesickGirls 이 #쇼음악중심 1위 후보에 올랐습니다!🖤💖\n\n✔️#0505 로 ‘2’ 또는 ‘블랙핑크’ 입력 후 문자메시지 전송\n\n#블랙핑크 \n#THEALBUM #1stFULLALBUM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINKINYOURAREA",
+					"indices": [
+						0,
+						20
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						22,
+						32
+					]
+				},
+				{
+					"text": "LovesickGirls",
+					"indices": [
+						35,
+						49
+					]
+				},
+				{
+					"text": "쇼음악중심",
+					"indices": [
+						52,
+						58
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						115,
+						120
+					]
+				},
+				{
+					"text": "THEALBUM",
+					"indices": [
+						122,
+						131
+					]
+				},
+				{
+					"text": "1stFULLALBUM",
+					"indices": [
+						132,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/c2mkxA118z",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c2mkxA118z",
+							"expanded_url": "https://blink.blackpinkofficial.com/",
+							"display_url": "blink.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5326210,
+			"friends_count": 2,
+			"listed_count": 5685,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1227,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1601269396",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7974,
+		"favorite_count": 26319,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "#BLACKPINKINYOURAREA \n#BLACKPINK 의 #LovesickGirls 이 #쇼음악중심 1위 후보에 올랐습니다!🖤💖\n\n✔️#0505 로 ‘2’ 또는 ‘블랙핑크’ 입력 후 문자메시지 전송\n\n#블랙핑크 \n#THEALBUM #1stFULLALBUM"
+	},
+	{
 		"created_at": "Sat Oct 10 06:30:51 +0000 2020",
 		"id": 1314810925824708600,
 		"id_str": "1314815640285270018",
