6f966d0b97f0a78c989aca45206b1767ee5130cc
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-11 15:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67243,
-	"created_at": "Sun Oct 11 05:02:08 +0000 2020",
+	"tweets": 67244,
+	"created_at": "Sun Oct 11 06:02:09 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 11 05:45:36 +0000 2020",
+		"id": 1315162768681566200,
+		"id_str": "1315166643111297026",
+		"full_text": "오빛! #이달의소녀 북미 공식 사이트가 정식 오픈되었습니다💕 북미 공식 사이트 오픈과 함께 앨범 및 MD가 판매되는 북미 공식 SHOP도 오픈되었으니 같이 구경하러 가 볼까요?😘\n\n▶️ https://t.co/02TOSNdwNZ\n▶️ https://t.co/298OOXLsvu\n\n#LOONA https://t.co/UQceQzg9VP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						4,
+						10
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						156,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/02TOSNdwNZ",
+					"expanded_url": "https://loonatheworldus.com",
+					"display_url": "loonatheworldus.com",
+					"indices": [
+						104,
+						127
+					]
+				},
+				{
+					"url": "https://t.co/298OOXLsvu",
+					"expanded_url": "https://shop.loonatheworldus.com",
+					"display_url": "shop.loonatheworldus.com",
+					"indices": [
+						131,
+						154
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1315162761043734500,
+					"id_str": "1315162761043734529",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/EkBlkCnVcAEnC_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkBlkCnVcAEnC_N.jpg",
+					"url": "https://t.co/UQceQzg9VP",
+					"display_url": "pic.twitter.com/UQceQzg9VP",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1315162768681566209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1315162761043734500,
+					"id_str": "1315162761043734529",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/EkBlkCnVcAEnC_N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkBlkCnVcAEnC_N.jpg",
+					"url": "https://t.co/UQceQzg9VP",
+					"display_url": "pic.twitter.com/UQceQzg9VP",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1315162768681566209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 551537,
+			"friends_count": 0,
+			"listed_count": 5730,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2233,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1579586496",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3847,
+		"favorite_count": 11065,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛! #이달의소녀 북미 공식 사이트가 정식 오픈되었습니다💕 북미 공식 사이트 오픈과 함께 앨범 및 MD가 판매되는 북미 공식 SHOP도 오픈되었으니 같이 구경하러 가 볼까요?😘\n\n▶️ https://t.co/02TOSNdwNZ\n▶️ https://t.co/298OOXLsvu\n\n#LOONA https://t.co/UQceQzg9VP"
+	},
+	{
 		"created_at": "Sun Oct 11 04:49:42 +0000 2020",
 		"id": 1315152572081156000,
 		"id_str": "1315152572081156096",
