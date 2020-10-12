30c0851bb3950b61e487d99edf6f55af1e0168cd
Youngmin Lee <iameyoungmin@gmail.com>
2020-10-13 05:02:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67350,
-	"created_at": "Mon Oct 12 18:02:08 +0000 2020",
+	"tweets": 67351,
+	"created_at": "Mon Oct 12 20:02:11 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Mon Oct 12 20:00:24 +0000 2020",
+		"id": 1315744147630968800,
+		"id_str": "1315744147630968834",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n130]비판 직면한 낙태죄 개정안, 무엇이 문제일까\nhttps://t.co/CR7w6btfEK\n\n▶ https://t.co/PJLvGWuzCK\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						105,
+						108
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						121,
+						126
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						127,
+						135
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						136,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CR7w6btfEK",
+					"expanded_url": "https://www.podty.me/episode/14576260",
+					"display_url": "podty.me/episode/145762…",
+					"indices": [
+						48,
+						71
+					]
+				},
+				{
+					"url": "https://t.co/PJLvGWuzCK",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1602499725650.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "정보를 능동적으로 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 299,
+			"friends_count": 968,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 43615,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 69654,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n130]비판 직면한 낙태죄 개정안, 무엇이 문제일까\nhttps://t.co/CR7w6btfEK\n\n▶ https://t.co/PJLvGWuzCK\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Mon Oct 12 17:12:54 +0000 2020",
 		"id": 1315547426934018000,
 		"id_str": "1315701992304463873",
