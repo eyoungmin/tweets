a110c7ca39f31b17aa51216a1a8f3c163b1a48a5
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-01 18:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68909,
-	"created_at": "Sun Nov 01 04:02:00 +0000 2020",
+	"tweets": 68910,
+	"created_at": "Sun Nov 01 09:02:00 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sun Nov 01 08:23:27 +0000 2020",
+		"id": 1322816509761249300,
+		"id_str": "1322816509761249280",
+		"full_text": "#팩플 #FACTPL\nYou're about to reach the monthly limit for your integrations\nhttps://t.co/lnXgtetanx\n#미디어 #뉴스레터 #Newsletter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "팩플",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "FACTPL",
+					"indices": [
+						4,
+						11
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						98,
+						102
+					]
+				},
+				{
+					"text": "뉴스레터",
+					"indices": [
+						103,
+						108
+					]
+				},
+				{
+					"text": "Newsletter",
+					"indices": [
+						109,
+						120
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lnXgtetanx",
+					"expanded_url": "https://notifier.in/item/ewapepvfohnccpsuh29s8p1so5phmmdd/2193585.html",
+					"display_url": "notifier.in/item/ewapepvfo…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "#뮤직비지니스 #minfav",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 300,
+			"friends_count": 1020,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44009,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 71437,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#팩플 #FACTPL\nYou're about to reach the monthly limit for your integrations\nhttps://t.co/lnXgtetanx\n#미디어 #뉴스레터 #Newsletter"
+	},
+	{
 		"created_at": "Sun Nov 01 03:27:06 +0000 2020",
 		"id": 1322735517268549600,
 		"id_str": "1322741933643620352",
