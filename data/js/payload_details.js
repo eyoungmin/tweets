d3e672e33c1d2d4e2d515b4eeded583d769c289a
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-05 00:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69079,
-	"created_at": "Wed Nov 04 14:02:03 +0000 2020",
+	"tweets": 69080,
+	"created_at": "Wed Nov 04 15:02:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 04 14:23:44 +0000 2020",
+		"id": 1323994342659367000,
+		"id_str": "1323994342659366913",
+		"full_text": "#Linkfire_Weekly #Linkfire\nLinkfire Weekly: Music marketing tips for independent artists (November 04, 2020 at 11:16PM)\nhttps://t.co/JzW9YUBItP\n#뮤직비지니스 #뉴스레터 #Newsletter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Linkfire_Weekly",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"text": "Linkfire",
+					"indices": [
+						17,
+						26
+					]
+				},
+				{
+					"text": "뮤직비지니스",
+					"indices": [
+						144,
+						151
+					]
+				},
+				{
+					"text": "뉴스레터",
+					"indices": [
+						152,
+						157
+					]
+				},
+				{
+					"text": "Newsletter",
+					"indices": [
+						158,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JzW9YUBItP",
+					"expanded_url": "https://mailchi.mp/1418e6516a1e/linkfire-weekly-music-marketing-tips-for-independent-artists-1342918",
+					"display_url": "mailchi.mp/1418e6516a1e/l…",
+					"indices": [
+						120,
+						143
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 1020,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44033,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 71627,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Linkfire_Weekly #Linkfire\nLinkfire Weekly: Music marketing tips for independent artists (November 04, 2020 at 11:16PM)\nhttps://t.co/JzW9YUBItP\n#뮤직비지니스 #뉴스레터 #Newsletter"
+	},
+	{
 		"created_at": "Wed Nov 04 13:03:43 +0000 2020",
 		"id": 1323974205096996900,
 		"id_str": "1323974205096996866",
