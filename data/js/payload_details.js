62d344a41228832e51d1f8e3a0c9ede3cb936e6a
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-12 01:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69433,
-	"created_at": "Wed Nov 11 15:20:00 +0000 2020",
+	"tweets": 69434,
+	"created_at": "Wed Nov 11 16:02:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,116 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 11 15:27:12 +0000 2020",
+		"id": 1326540429525840000,
+		"id_str": "1326547028449521665",
+		"full_text": "To think that there are more than 1.6M of you listening to us on Spotify makes our hearts go🥰💞 To both our longtime and new orbits, we dedicate our 7th star Orbit-N to you💫 We love you forever❤\n\n#이달의소녀 #LOONA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						195,
+						201
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						202,
+						208
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1326540426174591000,
+		"in_reply_to_status_id_str": "1326540426174590979",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 574674,
+			"friends_count": 0,
+			"listed_count": 5815,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2395,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 719,
+		"favorite_count": 5268,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "To think that there are more than 1.6M of you listening to us on Spotify makes our hearts go🥰💞 To both our longtime and new orbits, we dedicate our 7th star Orbit-N to you💫 We love you forever❤\n\n#이달의소녀 #LOONA"
+	},
+	{
 		"created_at": "Wed Nov 11 15:03:57 +0000 2020",
 		"id": 1326541177223409700,
 		"id_str": "1326541177223409673",
