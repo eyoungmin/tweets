18b585e5412ca3ff6f29ae1385ca1379f03c5dd7
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-15 06:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69652,
-	"created_at": "Sat Nov 14 16:02:09 +0000 2020",
+	"tweets": 69653,
+	"created_at": "Sat Nov 14 21:02:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 14 20:24:14 +0000 2020",
+		"id": 1327708942928056300,
+		"id_str": "1327708942928056320",
+		"full_text": "#청하 #CHUNGHA\n[청하] 오늘 밤비💕\nhttps://t.co/Fm0yYahxVU\n#다음팬카페 #공식팬카페 #From https://t.co/ZkqA6yM35W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"text": "다음팬카페",
+					"indices": [
+						49,
+						55
+					]
+				},
+				{
+					"text": "공식팬카페",
+					"indices": [
+						56,
+						62
+					]
+				},
+				{
+					"text": "From",
+					"indices": [
+						63,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Fm0yYahxVU",
+					"expanded_url": "http://cafe.daum.net/MNH-Chungha/g3oh/159",
+					"display_url": "cafe.daum.net/MNH-Chungha/g3…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1327708941472690200,
+					"id_str": "1327708941472690176",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/Emz4P22W8AAVlN7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Emz4P22W8AAVlN7.jpg",
+					"url": "https://t.co/ZkqA6yM35W",
+					"display_url": "pic.twitter.com/ZkqA6yM35W",
+					"expanded_url": "https://twitter.com/fromminn/status/1327708942928056320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 320,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 482,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 482,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1327708941472690200,
+					"id_str": "1327708941472690176",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/Emz4P22W8AAVlN7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Emz4P22W8AAVlN7.jpg",
+					"url": "https://t.co/ZkqA6yM35W",
+					"display_url": "pic.twitter.com/ZkqA6yM35W",
+					"expanded_url": "https://twitter.com/fromminn/status/1327708942928056320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 320,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 482,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 482,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 298,
+			"friends_count": 1019,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44126,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 72276,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#청하 #CHUNGHA\n[청하] 오늘 밤비💕\nhttps://t.co/Fm0yYahxVU\n#다음팬카페 #공식팬카페 #From https://t.co/ZkqA6yM35W"
+	},
+	{
 		"created_at": "Sat Nov 14 15:27:25 +0000 2020",
 		"id": 1327628012523647000,
 		"id_str": "1327634247146991623",
