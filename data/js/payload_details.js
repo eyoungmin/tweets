70ef7819b267aeaed6c65a46f7cf01b70bd605c7
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-15 00:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69646,
-	"created_at": "Sat Nov 14 14:02:12 +0000 2020",
+	"tweets": 69647,
+	"created_at": "Sat Nov 14 15:02:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 14 14:27:08 +0000 2020",
+		"id": 1327612458807173000,
+		"id_str": "1327619075191812097",
+		"full_text": "Keep it up with those Shazams, Orbits✨ Here’s to your 9th star, Orbit-O for getting us to 10K Shazams on #WhyNot? We are so thankful for everything you do~ Something magical is coming sooner than you think 😉\n\n#이달의소녀 #LOONA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WhyNot",
+					"indices": [
+						105,
+						112
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						209,
+						215
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						216,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1327612455753715700,
+		"in_reply_to_status_id_str": "1327612455753715712",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 576259,
+			"friends_count": 0,
+			"listed_count": 5840,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2415,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 449,
+		"favorite_count": 3328,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Keep it up with those Shazams, Orbits✨ Here’s to your 9th star, Orbit-O for getting us to 10K Shazams on #WhyNot? We are so thankful for everything you do~ Something magical is coming sooner than you think 😉\n\n#이달의소녀 #LOONA"
+	},
+	{
 		"created_at": "Sat Nov 14 13:27:04 +0000 2020",
 		"id": 1327598188627251200,
 		"id_str": "1327603961977823233",
