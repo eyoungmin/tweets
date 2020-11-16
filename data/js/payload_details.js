7f695f24a40c53a94b9e57bd7529106bc65175c1
Youngmin Lee <iameyoungmin@gmail.com>
2020-11-17 07:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69772,
-	"created_at": "Mon Nov 16 21:02:10 +0000 2020",
+	"tweets": 69773,
+	"created_at": "Mon Nov 16 22:02:09 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Mon Nov 16 21:28:30 +0000 2020",
+		"id": 1328449893413884000,
+		"id_str": "1328449893413883905",
+		"full_text": "#UPPITY #어피티\n💰 하이 리스크, 어쩌면 노 리턴\nhttps://t.co/j7j0Q8SFMj\n#금융경제 #뉴스레터 #Newsletter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "UPPITY",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "어피티",
+					"indices": [
+						8,
+						12
+					]
+				},
+				{
+					"text": "금융경제",
+					"indices": [
+						56,
+						61
+					]
+				},
+				{
+					"text": "뉴스레터",
+					"indices": [
+						62,
+						67
+					]
+				},
+				{
+					"text": "Newsletter",
+					"indices": [
+						68,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/j7j0Q8SFMj",
+					"expanded_url": "https://notifier.in/item/jbs1lxifi7tw9nltnw0ivmo3feq1rfku/2238987.html",
+					"display_url": "notifier.in/item/jbs1lxifi…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 299,
+			"friends_count": 1021,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 72413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283972466222620672/22eOS-n__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#UPPITY #어피티\n💰 하이 리스크, 어쩌면 노 리턴\nhttps://t.co/j7j0Q8SFMj\n#금융경제 #뉴스레터 #Newsletter"
+	},
+	{
 		"created_at": "Mon Nov 16 20:04:28 +0000 2020",
 		"id": 1328428745829585000,
 		"id_str": "1328428745829584898",
