3fd5bf9f54e334a62cf38a153d09e2398af80eae
Youngmin Lee <lyngmn@gmail.com>
2020-11-23 22:02:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70167,
-	"created_at": "Mon Nov 23 12:02:13 +0000 2020",
+	"tweets": 70168,
+	"created_at": "Mon Nov 23 13:02:13 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_11.js",
 		"year": 2020,
 		"var_name": "tweets_2020_11",
-		"tweet_count": 1262,
+		"tweet_count": 1263,
 		"month": 11
 	},
 	{
