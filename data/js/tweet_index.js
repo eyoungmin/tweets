d51b5037e86351c745cfb95d1fb31a43faa7903a
Youngmin Lee <lyngmn@gmail.com>
2020-11-30 01:02:25 +0900

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2020_11.js",
 		"year": 2020,
 		"var_name": "tweets_2020_11",
-		"tweet_count": 1681,
+		"tweet_count": 1683,
 		"month": 11
 	},
 	{
