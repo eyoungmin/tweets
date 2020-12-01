00d77d4e95c038ed3c6c20adf314161c8f885b0f
Youngmin Lee <lyngmn@gmail.com>
2020-12-02 00:19:56 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70709,
-	"created_at": "Tue Dec 01 15:02:02 +0000 2020",
+	"tweets": 70710,
+	"created_at": "Tue Dec 01 15:19:55 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 01 15:14:52 +0000 2020",
+		"id": 1333788121347936300,
+		"id_str": "1333791682391728129",
+		"full_text": "#이달의소녀 와의 추억을 사진처럼 영원히 간직하고 싶은 ’2021 이달의 소녀 (LOOПΔ) 2nd Season’s Greetings’이 오빛을 찾아갑니다📸\n자세한 상세 페이지 안내는 BlockBerryCreative SHOP을 통해 확인해 주세요!\n⠀\n#LOONA \n\n▶️ https://t.co/1jBpVDn0Ih https://t.co/kSuTXhfhl5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						142,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1jBpVDn0Ih",
+					"expanded_url": "http://blockberrycreativeshop.com/",
+					"display_url": "blockberrycreativeshop.com",
+					"indices": [
+						154,
+						177
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1333788090863812600,
+					"id_str": "1333788090863812608",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/EoKRM7GVgAAkcqr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoKRM7GVgAAkcqr.jpg",
+					"url": "https://t.co/kSuTXhfhl5",
+					"display_url": "pic.twitter.com/kSuTXhfhl5",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1333788121347936256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1333788090863812600,
+					"id_str": "1333788090863812608",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/EoKRM7GVgAAkcqr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoKRM7GVgAAkcqr.jpg",
+					"url": "https://t.co/kSuTXhfhl5",
+					"display_url": "pic.twitter.com/kSuTXhfhl5",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1333788121347936256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 590361,
+			"friends_count": 0,
+			"listed_count": 6028,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2500,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3603,
+		"favorite_count": 11611,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 와의 추억을 사진처럼 영원히 간직하고 싶은 ’2021 이달의 소녀 (LOOПΔ) 2nd Season’s Greetings’이 오빛을 찾아갑니다📸\n자세한 상세 페이지 안내는 BlockBerryCreative SHOP을 통해 확인해 주세요!\n⠀\n#LOONA \n\n▶️ https://t.co/1jBpVDn0Ih https://t.co/kSuTXhfhl5"
+	},
+	{
 		"created_at": "Tue Dec 01 14:51:10 +0000 2020",
 		"id": 1333785719764992000,
 		"id_str": "1333785719764992001",
