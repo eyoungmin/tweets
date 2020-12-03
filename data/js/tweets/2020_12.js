7d9f5749870d9d6eed718e7b657475a8574f98b8
Youngmin Lee <lyngmn@gmail.com>
2020-12-03 23:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 03 13:45:37 +0000 2020",
+		"id": 1334490096754598000,
+		"id_str": "1334493996362641412",
+		"full_text": "[#여진 / #YeoJin] 오빛~ 제가 이번 2021학년도 대학수학능력시험을 보고 왔는데요 정말 뜻깊은 하루였습니다! 사실 많이 떨린 날이었는데 끝나고 나니 홀가분한 느낌이 드는 것 같아요🐸☀️\n\n#이달의소녀 #LOONA https://t.co/MMeqer7fNv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "여진",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "YeoJin",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						111,
+						117
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						118,
+						124
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1334490086776270800,
+					"id_str": "1334490086776270848",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EoUPqdqUcAAYJc7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoUPqdqUcAAYJc7.jpg",
+					"url": "https://t.co/MMeqer7fNv",
+					"display_url": "pic.twitter.com/MMeqer7fNv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1334490096754597892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334490086776270800,
+					"id_str": "1334490086776270848",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EoUPqdqUcAAYJc7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoUPqdqUcAAYJc7.jpg",
+					"url": "https://t.co/MMeqer7fNv",
+					"display_url": "pic.twitter.com/MMeqer7fNv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1334490096754597892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1334490086784729000,
+					"id_str": "1334490086784729088",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EoUPqdsVgAA2V0_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoUPqdsVgAA2V0_.jpg",
+					"url": "https://t.co/MMeqer7fNv",
+					"display_url": "pic.twitter.com/MMeqer7fNv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1334490096754597892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1334490086784712700,
+					"id_str": "1334490086784712705",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EoUPqdsVQAEW6iT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoUPqdsVQAEW6iT.jpg",
+					"url": "https://t.co/MMeqer7fNv",
+					"display_url": "pic.twitter.com/MMeqer7fNv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1334490096754597892/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 591236,
+			"friends_count": 0,
+			"listed_count": 6046,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2512,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4071,
+		"favorite_count": 13676,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#여진 / #YeoJin] 오빛~ 제가 이번 2021학년도 대학수학능력시험을 보고 왔는데요 정말 뜻깊은 하루였습니다! 사실 많이 떨린 날이었는데 끝나고 나니 홀가분한 느낌이 드는 것 같아요🐸☀️\n\n#이달의소녀 #LOONA https://t.co/MMeqer7fNv"
+	},
+	{
 		"created_at": "Thu Dec 03 12:14:37 +0000 2020",
 		"id": 1334463270153961500,
 		"id_str": "1334471099191582720",
