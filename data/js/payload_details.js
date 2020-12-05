222908947bcc247f37e56efd21d19cbe69fafe23
Youngmin Lee <lyngmn@gmail.com>
2020-12-05 11:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 70865,
-	"created_at": "Fri Dec 04 20:02:03 +0000 2020",
+	"tweets": 70866,
+	"created_at": "Sat Dec 05 02:02:03 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Sat Dec 05 01:26:33 +0000 2020",
+		"id": 1335032780602957800,
+		"id_str": "1335032780602957824",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학] 잘못된 정보에 휘둘리지 않으려면\nhttps://t.co/6kIsSHp6Kb\n#박진영 #심리학 #minfav https://t.co/VMV124seYa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						66,
+						70
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						71,
+						75
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						76,
+						83
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6kIsSHp6Kb",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=42073",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						42,
+						65
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1335032779743129600,
+					"id_str": "1335032779743129602",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Eob9PWgXcAI9fhh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eob9PWgXcAI9fhh.jpg",
+					"url": "https://t.co/VMV124seYa",
+					"display_url": "pic.twitter.com/VMV124seYa",
+					"expanded_url": "https://twitter.com/fromminn/status/1335032780602957824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1335032779743129600,
+					"id_str": "1335032779743129602",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Eob9PWgXcAI9fhh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eob9PWgXcAI9fhh.jpg",
+					"url": "https://t.co/VMV124seYa",
+					"display_url": "pic.twitter.com/VMV124seYa",
+					"expanded_url": "https://twitter.com/fromminn/status/1335032780602957824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 565,
+							"h": 309,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다. #minfav",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 301,
+			"friends_count": 1024,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44281,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 73551,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학] 잘못된 정보에 휘둘리지 않으려면\nhttps://t.co/6kIsSHp6Kb\n#박진영 #심리학 #minfav https://t.co/VMV124seYa"
+	},
+	{
 		"created_at": "Fri Dec 04 19:38:23 +0000 2020",
 		"id": 1334248475878363100,
 		"id_str": "1334945162611421184",
