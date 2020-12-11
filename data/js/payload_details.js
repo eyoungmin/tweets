7bccf6e7e2047c6fe7dc3e78f1d43ce3e32958e0
Youngmin Lee <lyngmn@gmail.com>
2020-12-11 11:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 71302,
-	"created_at": "Fri Dec 11 01:02:07 +0000 2020",
+	"tweets": 71303,
+	"created_at": "Fri Dec 11 02:02:10 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,261 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 11 01:14:37 +0000 2020",
+		"id": 1337200429936074800,
+		"id_str": "1337204107283730432",
+		"full_text": "@1027KIISFM #KIISJingleBall Village tonight🎄 Tune in at 5:10PM(PT) to hear a special, live version of \"Star\" that we made just for you. Happy Holidays Orbits! We love you🤍 \n\n#이달의소녀 #LOONA #LOONA_Star\n\n▶️ https://t.co/dUjSG8Xq80 https://t.co/pLGylhAfrR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			227
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KIISJingleBall",
+					"indices": [
+						12,
+						27
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						174,
+						180
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						181,
+						187
+					]
+				},
+				{
+					"text": "LOONA_Star",
+					"indices": [
+						188,
+						199
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "1027KIISFM",
+					"name": "102.7 KIIS FM",
+					"id": 19046392,
+					"id_str": "19046392",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dUjSG8Xq80",
+					"expanded_url": "http://kiisfm.com/listen",
+					"display_url": "kiisfm.com/listen",
+					"indices": [
+						204,
+						227
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1337197382329454600,
+					"id_str": "1337197382329454598",
+					"indices": [
+						228,
+						251
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1337197382329454598/pu/img/RWF5-0Z73Mj78C73.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1337197382329454598/pu/img/RWF5-0Z73Mj78C73.jpg",
+					"url": "https://t.co/pLGylhAfrR",
+					"display_url": "pic.twitter.com/pLGylhAfrR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1337200429936074752/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1337197382329454600,
+					"id_str": "1337197382329454598",
+					"indices": [
+						228,
+						251
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1337197382329454598/pu/img/RWF5-0Z73Mj78C73.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1337197382329454598/pu/img/RWF5-0Z73Mj78C73.jpg",
+					"url": "https://t.co/pLGylhAfrR",
+					"display_url": "pic.twitter.com/pLGylhAfrR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1337200429936074752/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 24024,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1337197382329454598/pu/vid/480x270/wwpxkzPViN1WBddO.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1337197382329454598/pu/vid/1280x720/ox9Ln0CjOuFL85WP.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1337197382329454598/pu/vid/640x360/2FSpgO_PYeYavsLx.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1337197382329454598/pu/pl/jibrtnal0uTZZBF2.m3u8?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 19046392,
+		"in_reply_to_user_id_str": "19046392",
+		"in_reply_to_screen_name": "1027KIISFM",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 593009,
+			"friends_count": 0,
+			"listed_count": 6078,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2545,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1081,
+		"favorite_count": 2906,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@1027KIISFM #KIISJingleBall Village tonight🎄 Tune in at 5:10PM(PT) to hear a special, live version of \"Star\" that we made just for you. Happy Holidays Orbits! We love you🤍 \n\n#이달의소녀 #LOONA #LOONA_Star\n\n▶️ https://t.co/dUjSG8Xq80 https://t.co/pLGylhAfrR"
+	},
+	{
 		"created_at": "Fri Dec 11 00:14:37 +0000 2020",
 		"id": 1337181588648411100,
 		"id_str": "1337189008238055426",
