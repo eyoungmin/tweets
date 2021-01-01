207aad9106615b2149618f468d7dc3f5934f06d5
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-02 07:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Fri Jan 01 21:21:21 +0000 2021",
+		"id": 1345117936646828000,
+		"id_str": "1345117936646828037",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학]고통과 시련을 통해 성장하는 것은 '나' 보다 '우리'\nhttps://t.co/iFdRRDYjFC\n#박진영 #심리학 #minfav https://t.co/Mw4lBe1AFO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						83,
+						87
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						88,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iFdRRDYjFC",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=42829",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1345117935627546600,
+					"id_str": "1345117935627546625",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/EqrRor4WMAEPnmX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EqrRor4WMAEPnmX.jpg",
+					"url": "https://t.co/Mw4lBe1AFO",
+					"display_url": "pic.twitter.com/Mw4lBe1AFO",
+					"expanded_url": "https://twitter.com/fromminn/status/1345117936646828037/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1345117935627546600,
+					"id_str": "1345117935627546625",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/EqrRor4WMAEPnmX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EqrRor4WMAEPnmX.jpg",
+					"url": "https://t.co/Mw4lBe1AFO",
+					"display_url": "pic.twitter.com/Mw4lBe1AFO",
+					"expanded_url": "https://twitter.com/fromminn/status/1345117936646828037/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 233,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 302,
+			"friends_count": 1028,
+			"listed_count": 8,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 75431,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학]고통과 시련을 통해 성장하는 것은 '나' 보다 '우리'\nhttps://t.co/iFdRRDYjFC\n#박진영 #심리학 #minfav https://t.co/Mw4lBe1AFO"
+	},
+	{
 		"created_at": "Fri Jan 01 15:03:27 +0000 2021",
 		"id": 1345022834251362300,
 		"id_str": "1345022834251362307",
