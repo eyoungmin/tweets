4d07bb387b13d920d833c08c642128284ff3ed8d
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-03 19:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72588,
-	"created_at": "Sun Jan 03 08:02:01 +0000 2021",
+	"tweets": 72589,
+	"created_at": "Sun Jan 03 10:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 03 09:48:08 +0000 2021",
+		"id": 1345622167279390700,
+		"id_str": "1345668258460962816",
+		"full_text": "후회 최소화 프레임워크. 별 거 아닌 것 같은데 의외로 약간 정신 번쩍 드는 선택법.\n\n\"내가 지금 할까 말까 하는 일을 안 한다면 80살 때 후회할까?\" 를 생각해 보는 방법.\n\nhttps://t.co/hOAkUGzFFz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hOAkUGzFFz",
+					"expanded_url": "https://bit.ly/3hARViC",
+					"display_url": "bit.ly/3hARViC",
+					"indices": [
+						101,
+						124
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 732911923,
+			"id_str": "732911923",
+			"name": "기계인간 John Grib",
+			"screen_name": "John_Grib",
+			"location": "기하학의 이념",
+			"description": "유물론자.",
+			"url": "https://t.co/7YPkFr8iLb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7YPkFr8iLb",
+							"expanded_url": "https://johngrib.github.io",
+							"display_url": "johngrib.github.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9094,
+			"friends_count": 320,
+			"listed_count": 56,
+			"created_at": "Thu Aug 02 14:16:06 +0000 2012",
+			"favourites_count": 10596,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27051,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1244148341669158912/K1jzDqew_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1244148341669158912/K1jzDqew_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/732911923/1472973825",
+			"profile_link_color": "45619D",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 44,
+		"favorite_count": 109,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "후회 최소화 프레임워크. 별 거 아닌 것 같은데 의외로 약간 정신 번쩍 드는 선택법.\n\n\"내가 지금 할까 말까 하는 일을 안 한다면 80살 때 후회할까?\" 를 생각해 보는 방법.\n\nhttps://t.co/hOAkUGzFFz"
+	},
+	{
 		"created_at": "Sun Jan 03 07:02:05 +0000 2021",
 		"id": 1345626170310578200,
 		"id_str": "1345626471151394817",
