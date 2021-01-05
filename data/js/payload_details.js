02e911f2851cf2a289a9d1b742d121c7c8f34346
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-05 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 72657,
-	"created_at": "Tue Jan 05 05:02:03 +0000 2021",
+	"tweets": 72658,
+	"created_at": "Tue Jan 05 06:02:03 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Tue Jan 05 05:02:09 +0000 2021",
+		"id": 1346307364551499800,
+		"id_str": "1346321061357477892",
+		"full_text": "Starlight, starlight 💫 Orbits, we made a \"Star\" Instagram filter for you to use! Share your \"Star\" videos with us using #LOONA_Star. Can't wait for you to try it out❤️\n\n▶️ https://t.co/xgc17n7vXn \n\n#이달의소녀 #LOONA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA_Star",
+					"indices": [
+						120,
+						131
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						198,
+						204
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						205,
+						211
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xgc17n7vXn",
+					"expanded_url": "https://www.instagram.com/ar/1505276406341857/",
+					"display_url": "instagram.com/ar/15052764063…",
+					"indices": [
+						172,
+						195
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1346307329214488600,
+		"in_reply_to_status_id_str": "1346307329214488577",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 613217,
+			"friends_count": 0,
+			"listed_count": 6291,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2625,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 868,
+		"favorite_count": 4274,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Starlight, starlight 💫 Orbits, we made a \"Star\" Instagram filter for you to use! Share your \"Star\" videos with us using #LOONA_Star. Can't wait for you to try it out❤️\n\n▶️ https://t.co/xgc17n7vXn \n\n#이달의소녀 #LOONA"
+	},
+	{
 		"created_at": "Tue Jan 05 04:53:01 +0000 2021",
 		"id": 1346318763742920700,
 		"id_str": "1346318763742920705",
