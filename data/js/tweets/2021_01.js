9d15adf8b6a26fa8edbbffe1052c1107037df799
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-10 23:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 10 13:53:41 +0000 2021",
+		"id": 1348263734188286000,
+		"id_str": "1348266766275805185",
+		"full_text": "[MV] 자우림(Jaurim) - 스물다섯, 스물하나 https://t.co/hVxlosiXOs via @YouTube \n\n아 몰라. 요즘은 그냥",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						58,
+						66
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/hVxlosiXOs",
+					"expanded_url": "https://youtu.be/LrB-fJn-3w4",
+					"display_url": "youtu.be/LrB-fJn-3w4",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 126699468,
+			"id_str": "126699468",
+			"name": "이선규",
+			"screen_name": "2sunkyu",
+			"location": "",
+			"description": "man",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2897,
+			"friends_count": 107,
+			"listed_count": 69,
+			"created_at": "Fri Mar 26 18:20:30 +0000 2010",
+			"favourites_count": 90,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1077886139338448896/ONGm7dac_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1077886139338448896/ONGm7dac_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/126699468/1545823059",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[MV] 자우림(Jaurim) - 스물다섯, 스물하나 https://t.co/hVxlosiXOs via @YouTube \n\n아 몰라. 요즘은 그냥"
+	},
+	{
 		"created_at": "Sun Jan 10 12:02:12 +0000 2021",
 		"id": 1348223667218509800,
 		"id_str": "1348238709813112832",
