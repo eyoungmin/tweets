a1d38f1ffdd6690ffdef7ef5f53057057fe31c37
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-19 15:02:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73266,
-	"created_at": "Tue Jan 19 04:02:09 +0000 2021",
+	"tweets": 73267,
+	"created_at": "Tue Jan 19 06:02:11 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Tue Jan 19 06:02:06 +0000 2021",
+		"id": 1351409052903850000,
+		"id_str": "1351409578282545156",
+		"full_text": "Hi Orbits~ We hid some special messages in Map of Orbits for you!! Can you find them? Here's your hint: What do stars do in the night sky?⭐ Search the word in the Map of Orbits to find our messages! \n\n#이달의소녀 #LOONA #LOONA_Star✨\n\n▶ https://t.co/K782sUFsNV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			254
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						201,
+						207
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						208,
+						214
+					]
+				},
+				{
+					"text": "LOONA_Star",
+					"indices": [
+						215,
+						226
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K782sUFsNV",
+					"expanded_url": "https://LOONAtheworld.lnk.to/mapoforbitsTP",
+					"display_url": "LOONAtheworld.lnk.to/mapoforbitsTP",
+					"indices": [
+						231,
+						254
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 627019,
+			"friends_count": 0,
+			"listed_count": 6421,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2654,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 680,
+		"favorite_count": 2602,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hi Orbits~ We hid some special messages in Map of Orbits for you!! Can you find them? Here's your hint: What do stars do in the night sky?⭐ Search the word in the Map of Orbits to find our messages! \n\n#이달의소녀 #LOONA #LOONA_Star✨\n\n▶ https://t.co/K782sUFsNV"
+	},
+	{
 		"created_at": "Tue Jan 19 03:02:35 +0000 2021",
 		"id": 1351364404009439200,
 		"id_str": "1351364404009439236",
