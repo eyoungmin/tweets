230d8193f19620a9062a07d32c38bf1ba7919f07
Youngmin Lee <iameyoungmin@gmail.com>
2021-01-20 22:02:12 +0900

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Wed Jan 20 12:03:48 +0000 2021",
+		"id": 1351851583152087000,
+		"id_str": "1351862992476860417",
+		"full_text": "블랙핑크 #제니, #크리스탈, #송민호, #켄달제너 의 집에 있다는 그 오디오!\n\nhttps://t.co/e6XxL8zLlr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "제니",
+					"indices": [
+						5,
+						8
+					]
+				},
+				{
+					"text": "크리스탈",
+					"indices": [
+						10,
+						15
+					]
+				},
+				{
+					"text": "송민호",
+					"indices": [
+						17,
+						21
+					]
+				},
+				{
+					"text": "켄달제너",
+					"indices": [
+						23,
+						28
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e6XxL8zLlr",
+					"expanded_url": "http://www.wkorea.com/2021/01/20/%EC%A0%9C%EB%8B%88-%ED%81%AC%EB%A6%AC%EC%8A%A4%ED%83%88-%EC%86%A1%EB%AF%BC%ED%98%B8-%EC%BC%84%EB%8B%AC-%EC%A0%9C%EB%84%88%EC%9D%98-%EC%A7%91%EC%97%90%EB%8A%94-%EC%9D%B4%EA%B2%8C-%EC%9E%88%EB%8B%A4/",
+					"display_url": "wkorea.com/2021/01/20/%EC…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/m6zPKg47IM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m6zPKg47IM",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 256583,
+			"friends_count": 0,
+			"listed_count": 775,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 4258,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11913,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1611124823",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 160,
+		"favorite_count": 435,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블랙핑크 #제니, #크리스탈, #송민호, #켄달제너 의 집에 있다는 그 오디오!\n\nhttps://t.co/e6XxL8zLlr"
+	},
+	{
 		"created_at": "Wed Jan 20 12:02:05 +0000 2021",
 		"id": 1351850144761692200,
 		"id_str": "1351862562325999616",
