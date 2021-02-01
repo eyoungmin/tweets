32ecf54fc25226af8845959e6dd5195772f1a3d7
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-01 12:02:11 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 73819,
-	"created_at": "Mon Feb 01 00:02:16 +0000 2021",
+	"tweets": 73821,
+	"created_at": "Mon Feb 01 03:02:10 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,411 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Mon Feb 01 03:02:09 +0000 2021",
+		"id": 1356074856144466000,
+		"id_str": "1356075335016730629",
+		"full_text": "aespa 에스파 [Forever]\n➫ 2021.02.05 6PM KST\n\n#KARINA #카리나\n#aespa #æspa #에스파\n#Forever https://t.co/fjBrSypjkc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KARINA",
+					"indices": [
+						42,
+						49
+					]
+				},
+				{
+					"text": "카리나",
+					"indices": [
+						50,
+						54
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						62,
+						67
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "Forever",
+					"indices": [
+						73,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1356068187985440800,
+					"id_str": "1356068187985440770",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/EtG41QwUcAIvgtG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtG41QwUcAIvgtG.jpg",
+					"url": "https://t.co/fjBrSypjkc",
+					"display_url": "pic.twitter.com/fjBrSypjkc",
+					"expanded_url": "https://twitter.com/aespa_official/status/1356074856144465923/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1367,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1356068187985440800,
+					"id_str": "1356068187985440770",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/EtG41QwUcAIvgtG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtG41QwUcAIvgtG.jpg",
+					"url": "https://t.co/fjBrSypjkc",
+					"display_url": "pic.twitter.com/fjBrSypjkc",
+					"expanded_url": "https://twitter.com/aespa_official/status/1356074856144465923/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1367,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "I’m in SYNK",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 722872,
+			"friends_count": 1,
+			"listed_count": 2516,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1356074801421410310/CmVD_DYS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1356074801421410310/CmVD_DYS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1612148404",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3330,
+		"favorite_count": 7705,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "aespa 에스파 [Forever]\n➫ 2021.02.05 6PM KST\n\n#KARINA #카리나\n#aespa #æspa #에스파\n#Forever https://t.co/fjBrSypjkc"
+	},
+	{
+		"created_at": "Mon Feb 01 03:02:06 +0000 2021",
+		"id": 1356074828764115000,
+		"id_str": "1356075321964060673",
+		"full_text": "aespa 에스파 [Forever]\n➫ 2021.02.05 6PM KST\n\n#KARINA #카리나\n#aespa #æspa #에스파\n#Forever https://t.co/q8sp7Fif9i",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KARINA",
+					"indices": [
+						42,
+						49
+					]
+				},
+				{
+					"text": "카리나",
+					"indices": [
+						50,
+						54
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						62,
+						67
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "Forever",
+					"indices": [
+						73,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1356068136273891300,
+					"id_str": "1356068136273891328",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/EtG4yQHU0AAWuXw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtG4yQHU0AAWuXw.jpg",
+					"url": "https://t.co/q8sp7Fif9i",
+					"display_url": "pic.twitter.com/q8sp7Fif9i",
+					"expanded_url": "https://twitter.com/aespa_official/status/1356074828764114946/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1367,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1356068136273891300,
+					"id_str": "1356068136273891328",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/EtG4yQHU0AAWuXw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtG4yQHU0AAWuXw.jpg",
+					"url": "https://t.co/q8sp7Fif9i",
+					"display_url": "pic.twitter.com/q8sp7Fif9i",
+					"expanded_url": "https://twitter.com/aespa_official/status/1356074828764114946/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1367,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 801,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "I’m in SYNK",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 722872,
+			"friends_count": 1,
+			"listed_count": 2516,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1356074801421410310/CmVD_DYS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1356074801421410310/CmVD_DYS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1612148404",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3579,
+		"favorite_count": 8608,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "aespa 에스파 [Forever]\n➫ 2021.02.05 6PM KST\n\n#KARINA #카리나\n#aespa #æspa #에스파\n#Forever https://t.co/q8sp7Fif9i"
+	}
+]
