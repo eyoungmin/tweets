bf4d8d254b957b8c73851cae1f9bb9ac433a6c73
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-06 10:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74156,
-	"created_at": "Fri Feb 05 22:02:05 +0000 2021",
+	"tweets": 74157,
+	"created_at": "Sat Feb 06 01:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sat Feb 06 00:21:45 +0000 2021",
+		"id": 1357846910791483400,
+		"id_str": "1357846910791483392",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학]서로의 성공에 함께 기뻐하고 있는가\nhttps://t.co/KD2GcoHdkM\n#박진영 #심리학 #minfav https://t.co/8R8KmHOs5r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						67,
+						71
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						72,
+						76
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						77,
+						84
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KD2GcoHdkM",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=43765",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1357846909679992800,
+					"id_str": "1357846909679992832",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EtgKkfbWgAATnoa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtgKkfbWgAATnoa.jpg",
+					"url": "https://t.co/8R8KmHOs5r",
+					"display_url": "pic.twitter.com/8R8KmHOs5r",
+					"expanded_url": "https://twitter.com/fromminn/status/1357846910791483392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1357846909679992800,
+					"id_str": "1357846909679992832",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/EtgKkfbWgAATnoa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtgKkfbWgAATnoa.jpg",
+					"url": "https://t.co/8R8KmHOs5r",
+					"display_url": "pic.twitter.com/8R8KmHOs5r",
+					"expanded_url": "https://twitter.com/fromminn/status/1357846910791483392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 303,
+			"friends_count": 1041,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44723,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 77360,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학]서로의 성공에 함께 기뻐하고 있는가\nhttps://t.co/KD2GcoHdkM\n#박진영 #심리학 #minfav https://t.co/8R8KmHOs5r"
+	},
+	{
 		"created_at": "Fri Feb 05 21:48:25 +0000 2021",
 		"id": 1357808321219022800,
 		"id_str": "1357808321219022849",
