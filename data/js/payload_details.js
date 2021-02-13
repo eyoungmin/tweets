530ce8fb2ab6dafdaee85a587b81cbcdd892c799
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-13 15:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74568,
-	"created_at": "Sat Feb 13 05:02:21 +0000 2021",
+	"tweets": 74569,
+	"created_at": "Sat Feb 13 06:02:06 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sat Feb 13 05:45:37 +0000 2021",
+		"id": 1360455048875044900,
+		"id_str": "1360465127380836354",
+		"full_text": "[#최리 / #Choerry] 오빛💜 요즘 날씨가 추웠다 좋았다 아주 밀당을 하네여ㅠㅠ 이럴 때일수록 모두 건강 조심하세요!! 봄아 빨리와랑~~🥰\n\n#이달의소녀 #LOONA https://t.co/wjOE0RghfR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "최리",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "Choerry",
+					"indices": [
+						7,
+						15
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						90,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1360455039668560000,
+					"id_str": "1360455039668559879",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EuFOpoxVoAcYi0X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuFOpoxVoAcYi0X.jpg",
+					"url": "https://t.co/wjOE0RghfR",
+					"display_url": "pic.twitter.com/wjOE0RghfR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360455048875044864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1360455039668560000,
+					"id_str": "1360455039668559879",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EuFOpoxVoAcYi0X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuFOpoxVoAcYi0X.jpg",
+					"url": "https://t.co/wjOE0RghfR",
+					"display_url": "pic.twitter.com/wjOE0RghfR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360455048875044864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1360455039668527000,
+					"id_str": "1360455039668527110",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EuFOpoxVIAY-XoI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuFOpoxVIAY-XoI.jpg",
+					"url": "https://t.co/wjOE0RghfR",
+					"display_url": "pic.twitter.com/wjOE0RghfR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360455048875044864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1360455039706304500,
+					"id_str": "1360455039706304513",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EuFOpo6VkAELj8c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuFOpo6VkAELj8c.jpg",
+					"url": "https://t.co/wjOE0RghfR",
+					"display_url": "pic.twitter.com/wjOE0RghfR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360455048875044864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1360455039723004000,
+					"id_str": "1360455039723003914",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EuFOpo-UYAoWOdg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuFOpo-UYAoWOdg.jpg",
+					"url": "https://t.co/wjOE0RghfR",
+					"display_url": "pic.twitter.com/wjOE0RghfR",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360455048875044864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 645538,
+			"friends_count": 0,
+			"listed_count": 6677,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2736,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5257,
+		"favorite_count": 14656,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#최리 / #Choerry] 오빛💜 요즘 날씨가 추웠다 좋았다 아주 밀당을 하네여ㅠㅠ 이럴 때일수록 모두 건강 조심하세요!! 봄아 빨리와랑~~🥰\n\n#이달의소녀 #LOONA https://t.co/wjOE0RghfR"
+	},
+	{
 		"created_at": "Sat Feb 13 04:24:12 +0000 2021",
 		"id": 1360444637274542000,
 		"id_str": "1360444637274542084",
