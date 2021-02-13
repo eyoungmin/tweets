b862e01925ddfeccdb9b2d4e68134f08013c5895
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-13 18:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74570,
-	"created_at": "Sat Feb 13 07:02:08 +0000 2021",
+	"tweets": 74571,
+	"created_at": "Sat Feb 13 09:02:10 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sat Feb 13 09:02:06 +0000 2021",
+		"id": 1360514058793808000,
+		"id_str": "1360514573518929923",
+		"full_text": "돌아온 동거동락에서 #츄 와 함께 화려한 댄스신고식 어때요, 오빛?💫 잠시 후 오후 6시 30분(KST) 방송되는 MBC '놀면 뭐하니?' 본방사수하세요💃🏻🕺🏻\n\n#이달의소녀 #LOONA #Chuu https://t.co/3VCVeYzTER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "츄",
+					"indices": [
+						11,
+						13
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						91,
+						97
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						98,
+						104
+					]
+				},
+				{
+					"text": "Chuu",
+					"indices": [
+						105,
+						110
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1360514051395100700,
+					"id_str": "1360514051395100675",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/EuGEUklVIAMQ1J8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuGEUklVIAMQ1J8.jpg",
+					"url": "https://t.co/3VCVeYzTER",
+					"display_url": "pic.twitter.com/3VCVeYzTER",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360514058793807872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1360514051395100700,
+					"id_str": "1360514051395100675",
+					"indices": [
+						111,
+						134
+					],
+					"media_url": "http://pbs.twimg.com/media/EuGEUklVIAMQ1J8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuGEUklVIAMQ1J8.jpg",
+					"url": "https://t.co/3VCVeYzTER",
+					"display_url": "pic.twitter.com/3VCVeYzTER",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1360514058793807872/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 645621,
+			"friends_count": 0,
+			"listed_count": 6680,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2737,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1165,
+		"favorite_count": 2723,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "돌아온 동거동락에서 #츄 와 함께 화려한 댄스신고식 어때요, 오빛?💫 잠시 후 오후 6시 30분(KST) 방송되는 MBC '놀면 뭐하니?' 본방사수하세요💃🏻🕺🏻\n\n#이달의소녀 #LOONA #Chuu https://t.co/3VCVeYzTER"
+	},
+	{
 		"created_at": "Sat Feb 13 06:02:09 +0000 2021",
 		"id": 1360457422486208500,
 		"id_str": "1360469291276058624",
