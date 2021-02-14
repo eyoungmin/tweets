578494e27a63b8659d5b1078921e7af9be723dbc
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-15 05:02:08 +0900

    Added 1 tweets

--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Sun Feb 14 20:00:44 +0000 2021",
+		"id": 1361042713001525200,
+		"id_str": "1361042713001525248",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n15]'실리콘밸리 인싸'가 키운 클럽하우스, 얼마나 성장할까?(f.김정민 기자)\nhttps://t.co/hjBdd045Dg\n\n▶ https://t.co/By1BKwtQtS\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						116,
+						120
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						121,
+						124
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						125,
+						128
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						129,
+						132
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						133,
+						136
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						137,
+						142
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						143,
+						151
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						152,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hjBdd045Dg",
+					"expanded_url": "https://www.podty.me/episode/15304018",
+					"display_url": "podty.me/episode/153040…",
+					"indices": [
+						64,
+						87
+					]
+				},
+				{
+					"url": "https://t.co/By1BKwtQtS",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1613289491490.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 302,
+			"friends_count": 1039,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44809,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 77925,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n15]'실리콘밸리 인싸'가 키운 클럽하우스, 얼마나 성장할까?(f.김정민 기자)\nhttps://t.co/hjBdd045Dg\n\n▶ https://t.co/By1BKwtQtS\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Sun Feb 14 18:24:01 +0000 2021",
 		"id": 1361008137478697000,
 		"id_str": "1361018373702754306",
