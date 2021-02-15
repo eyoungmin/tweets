8fae4956ab10a7a3d39123be76467fdbec860000
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-16 06:02:13 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 74730,
-	"created_at": "Mon Feb 15 17:02:08 +0000 2021",
+	"tweets": 74731,
+	"created_at": "Mon Feb 15 21:02:12 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Mon Feb 15 20:08:36 +0000 2021",
+		"id": 1361407082528985000,
+		"id_str": "1361407082528985089",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n16]마스크부터 배달용기까지, 쓰레기 어떻게 하나(f.정희윤, 함민정 기자)\nhttps://t.co/aQgsbRIftd\n\n▶ https://t.co/JQJB33iP4l\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						114,
+						118
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						119,
+						122
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						123,
+						126
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						127,
+						130
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						131,
+						134
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						135,
+						140
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						141,
+						149
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						150,
+						157
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/aQgsbRIftd",
+					"expanded_url": "https://www.podty.me/episode/15308299",
+					"display_url": "podty.me/episode/153082…",
+					"indices": [
+						62,
+						85
+					]
+				},
+				{
+					"url": "https://t.co/JQJB33iP4l",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1613400842536.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						89,
+						112
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 303,
+			"friends_count": 1039,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44824,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 78022,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n16]마스크부터 배달용기까지, 쓰레기 어떻게 하나(f.정희윤, 함민정 기자)\nhttps://t.co/aQgsbRIftd\n\n▶ https://t.co/JQJB33iP4l\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Mon Feb 15 16:02:06 +0000 2021",
 		"id": 1361314478051090400,
 		"id_str": "1361345045547413504",
