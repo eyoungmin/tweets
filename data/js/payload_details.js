b96e915379cb8be2e8c7598fb665c8fa8b83916b
Youngmin Lee <iameyoungmin@gmail.com>
2021-02-19 16:02:14 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75002,
-	"created_at": "Fri Feb 19 06:02:13 +0000 2021",
+	"tweets": 75005,
+	"created_at": "Fri Feb 19 07:02:13 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_02.js",
 		"year": 2021,
 		"var_name": "tweets_2021_02",
-		"tweet_count": 1183,
+		"tweet_count": 1186,
 		"month": 2
 	},
 	{
