a922b1bc450d1978534e4db7cc7170875d1a016d
Youngmin Lee <iameyoungmin@gmail.com>
2021-03-02 06:02:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75367,
-	"created_at": "Mon Mar 01 08:02:03 +0000 2021",
+	"tweets": 75368,
+	"created_at": "Mon Mar 01 21:01:59 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Mon Mar 01 20:05:20 +0000 2021",
+		"id": 1366479687883640800,
+		"id_str": "1366479687883640832",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n21]인터뷰/사람을 위한 AI? AI전문가 오혜연 교수의 답은..\nhttps://t.co/ss5o7klPoz\n\n▶ https://t.co/XxyApDlJXb\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						108,
+						112
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						121,
+						124
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						125,
+						128
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						129,
+						134
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						135,
+						143
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						144,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ss5o7klPoz",
+					"expanded_url": "https://www.podty.me/episode/15388473",
+					"display_url": "podty.me/episode/153884…",
+					"indices": [
+						56,
+						79
+					]
+				},
+				{
+					"url": "https://t.co/XxyApDlJXb",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1614590847941.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 1045,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44939,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 78791,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n21]인터뷰/사람을 위한 AI? AI전문가 오혜연 교수의 답은..\nhttps://t.co/ss5o7klPoz\n\n▶ https://t.co/XxyApDlJXb\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Mon Mar 01 07:40:20 +0000 2021",
 		"id": 1366292202905739300,
 		"id_str": "1366292202905739269",
