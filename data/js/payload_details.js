100c07bfca38e454498add72b22105fbcf9bc574
Youngmin Lee <iameyoungmin@gmail.com>
2021-03-05 09:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75428,
-	"created_at": "Thu Mar 04 22:02:01 +0000 2021",
+	"tweets": 75429,
+	"created_at": "Fri Mar 05 00:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Thu Mar 04 23:43:22 +0000 2021",
+		"id": 1367621722757824500,
+		"id_str": "1367621722757824515",
+		"full_text": "#팩플 #FACTPL\n[팩플] 아기 개미들은 오늘도 뚠뚠 🐜🐜\nhttps://t.co/LGzLva4HPE\n#미디어 #중앙일보 #팩플레터\n#뉴스레터 #Newsletter",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "팩플",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "FACTPL",
+					"indices": [
+						4,
+						11
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						59,
+						63
+					]
+				},
+				{
+					"text": "중앙일보",
+					"indices": [
+						64,
+						69
+					]
+				},
+				{
+					"text": "팩플레터",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "뉴스레터",
+					"indices": [
+						76,
+						81
+					]
+				},
+				{
+					"text": "Newsletter",
+					"indices": [
+						82,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LGzLva4HPE",
+					"expanded_url": "https://notifier.in/item/g9wdkbtmsksbys81py6ut4c68gt3kx3c/2497085.html",
+					"display_url": "notifier.in/item/g9wdkbtms…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 307,
+			"friends_count": 1046,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44982,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 78870,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#팩플 #FACTPL\n[팩플] 아기 개미들은 오늘도 뚠뚠 🐜🐜\nhttps://t.co/LGzLva4HPE\n#미디어 #중앙일보 #팩플레터\n#뉴스레터 #Newsletter"
+	},
+	{
 		"created_at": "Thu Mar 04 21:28:37 +0000 2021",
 		"id": 1367587810161520600,
 		"id_str": "1367587810161520655",
