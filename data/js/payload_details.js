91595fac5b918bf3ab1b88be39fe864cf996f56c
Youngmin Lee <iameyoungmin@gmail.com>
2021-03-16 11:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75652,
-	"created_at": "Tue Mar 16 01:02:03 +0000 2021",
+	"tweets": 75653,
+	"created_at": "Tue Mar 16 02:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 16 01:49:27 +0000 2021",
+		"id": 1371639720967561200,
+		"id_str": "1371639720967561216",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크 로제, 솔로 싱글로 중국 음악 차트도 `싹쓸이`\nhttps://t.co/GvXIiwnVCz\n#블랙핑크 #BLACKPINK https://t.co/2PErCUAM6A",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						73,
+						78
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						79,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GvXIiwnVCz",
+					"expanded_url": "https://yg-life.com/archives/166419?lang=ko",
+					"display_url": "yg-life.com/archives/16641…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1371639719797395500,
+					"id_str": "1371639719797395460",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EwkLDtJXEAQ11Cx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwkLDtJXEAQ11Cx.jpg",
+					"url": "https://t.co/2PErCUAM6A",
+					"display_url": "pic.twitter.com/2PErCUAM6A",
+					"expanded_url": "https://twitter.com/fromminn/status/1371639720967561216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1371639719797395500,
+					"id_str": "1371639719797395460",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EwkLDtJXEAQ11Cx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EwkLDtJXEAQ11Cx.jpg",
+					"url": "https://t.co/2PErCUAM6A",
+					"display_url": "pic.twitter.com/2PErCUAM6A",
+					"expanded_url": "https://twitter.com/fromminn/status/1371639720967561216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 373,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312,
+			"friends_count": 1048,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 45002,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79144,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크 로제, 솔로 싱글로 중국 음악 차트도 `싹쓸이`\nhttps://t.co/GvXIiwnVCz\n#블랙핑크 #BLACKPINK https://t.co/2PErCUAM6A"
+	},
+	{
 		"created_at": "Tue Mar 16 00:49:23 +0000 2021",
 		"id": 1371624601617326000,
 		"id_str": "1371624601617326083",
