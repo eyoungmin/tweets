15496168829ad23d8cd91cca445238b2de7d5af6
Youngmin Lee <iameyoungmin@gmail.com>
2021-03-18 13:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75700,
-	"created_at": "Thu Mar 18 03:02:03 +0000 2021",
+	"tweets": 75701,
+	"created_at": "Thu Mar 18 04:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,162 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Thu Mar 18 03:24:40 +0000 2021",
+		"id": 1372388455662170000,
+		"id_str": "1372388455662170122",
+		"full_text": "#놀라운토요일 #도레미마켓\n[#놀토미방​] 키X한해X태연X피오 편 | 지구 뿌셔 우주 뿌셔 💥 보는 내내 동현 동기화 되는 막내즈 | #놀라운토요일​ Amazing Saturday Close-up Cam\nhttps://t.co/F9txLEX33H\n#태연 #탱구 #TAEYEON",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "도레미마켓",
+					"indices": [
+						8,
+						14
+					]
+				},
+				{
+					"text": "놀토미방",
+					"indices": [
+						16,
+						21
+					]
+				},
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						75,
+						82
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						138,
+						141
+					]
+				},
+				{
+					"text": "탱구",
+					"indices": [
+						142,
+						145
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						146,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/F9txLEX33H",
+					"expanded_url": "https://www.youtube.com/watch?v=AMwK5s2kKWk",
+					"display_url": "youtube.com/watch?v=AMwK5s…",
+					"indices": [
+						114,
+						137
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1048,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 45001,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79201,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#놀라운토요일 #도레미마켓\n[#놀토미방​] 키X한해X태연X피오 편 | 지구 뿌셔 우주 뿌셔 💥 보는 내내 동현 동기화 되는 막내즈 | #놀라운토요일​ Amazing Saturday Close-up Cam\nhttps://t.co/F9txLEX33H\n#태연 #탱구 #TAEYEON"
+	},
+	{
 		"created_at": "Thu Mar 18 03:01:06 +0000 2021",
 		"id": 1372382527059529700,
 		"id_str": "1372382527059529728",
