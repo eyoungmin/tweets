c938f3f142ee44a27b4d05ce61d11b24964992f8
Youngmin Lee <iameyoungmin@gmail.com>
2021-03-30 19:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75894,
-	"created_at": "Tue Mar 30 09:02:05 +0000 2021",
+	"tweets": 75895,
+	"created_at": "Tue Mar 30 10:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Tue Mar 30 09:47:49 +0000 2021",
+		"id": 1376833533960855600,
+		"id_str": "1376833533960855552",
+		"full_text": "#이달의소녀 #LOONA\n[LOONA+] 우리의 처음 돌아보기! ep.3 | 이달의 소녀(LOONA)\nhttps://t.co/0d09zV4vMc\n#VLIVE #브이앱 #브이라이브",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						7,
+						13
+					]
+				},
+				{
+					"text": "VLIVE",
+					"indices": [
+						81,
+						87
+					]
+				},
+				{
+					"text": "브이앱",
+					"indices": [
+						88,
+						92
+					]
+				},
+				{
+					"text": "브이라이브",
+					"indices": [
+						93,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0d09zV4vMc",
+					"expanded_url": "https://www.vlive.tv/video/237681",
+					"display_url": "vlive.tv/video/237681",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1049,
+			"listed_count": 7,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 45023,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79443,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 #LOONA\n[LOONA+] 우리의 처음 돌아보기! ep.3 | 이달의 소녀(LOONA)\nhttps://t.co/0d09zV4vMc\n#VLIVE #브이앱 #브이라이브"
+	},
+	{
 		"created_at": "Tue Mar 30 08:27:38 +0000 2021",
 		"id": 1376812222299578400,
 		"id_str": "1376813355814379521",
@@ -361,7 +503,7 @@ Grailbird.data.tweets_2021_03 =
 		"created_at": "Tue Mar 30 08:24:08 +0000 2021",
 		"id": 1376812474205360000,
 		"id_str": "1376812474205360132",
-		"full_text": "#놀라운토요일 #도레미마켓\n[#도레미가족] 막냉이 동태라인 졸업하나요? 👩‍🎓 신용등급 차근차근 올리는 탱구 활약 모음│#놀라운토요일 Amazing Saturday\nhttps://t.co/C9DBRAsSfQ\n#태연 #탱구 #TAEYEON",
+		"full_text": "#놀라운토요일 #도레미마켓\n[#도레미가족] 막냉이 동태라인 졸업하나요? 👩🎓 신용등급 차근차근 올리는 탱구 활약 모음│#놀라운토요일 Amazing Saturday\nhttps://t.co/C9DBRAsSfQ\n#태연 #탱구 #TAEYEON",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -511,7 +653,7 @@ Grailbird.data.tweets_2021_03 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#놀라운토요일 #도레미마켓\n[#도레미가족] 막냉이 동태라인 졸업하나요? 👩‍🎓 신용등급 차근차근 올리는 탱구 활약 모음│#놀라운토요일 Amazing Saturday\nhttps://t.co/C9DBRAsSfQ\n#태연 #탱구 #TAEYEON"
+		"text": "#놀라운토요일 #도레미마켓\n[#도레미가족] 막냉이 동태라인 졸업하나요? 👩🎓 신용등급 차근차근 올리는 탱구 활약 모음│#놀라운토요일 Amazing Saturday\nhttps://t.co/C9DBRAsSfQ\n#태연 #탱구 #TAEYEON"
 	},
 	{
 		"created_at": "Tue Mar 30 07:11:36 +0000 2021",
