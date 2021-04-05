7d7d78d46fd1a1af6eaa85ee5fa43e4022baa2d1
Youngmin Lee <iameyoungmin@gmail.com>
2021-04-06 06:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 75995,
-	"created_at": "Mon Apr 05 14:02:01 +0000 2021",
+	"tweets": 75996,
+	"created_at": "Mon Apr 05 21:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 05 20:10:27 +0000 2021",
+		"id": 1379164550738686000,
+		"id_str": "1379164550738685952",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n37] 요즘 ETF는 '액티브' 하다는데?(f.고란 기자)\nhttps://t.co/X1iKxjGuZJ\n\n▶ https://t.co/JNtP5woimx\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						109,
+						112
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						121,
+						124
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						125,
+						130
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						131,
+						139
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						140,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X1iKxjGuZJ",
+					"expanded_url": "https://www.podty.me/episode/15576038",
+					"display_url": "podty.me/episode/155760…",
+					"indices": [
+						52,
+						75
+					]
+				},
+				{
+					"url": "https://t.co/JNtP5woimx",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1617621872518.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1049,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 45014,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79548,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n37] 요즘 ETF는 '액티브' 하다는데?(f.고란 기자)\nhttps://t.co/X1iKxjGuZJ\n\n▶ https://t.co/JNtP5woimx\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Mon Apr 05 13:19:20 +0000 2021",
 		"id": 1379061090752065500,
 		"id_str": "1379061090752065543",
