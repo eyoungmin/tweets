dc704dce75d4e9e4a71bd75fa287969913fa2807
Youngmin Lee <iameyoungmin@gmail.com>
2021-04-13 11:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76091,
-	"created_at": "Tue Apr 13 00:02:02 +0000 2021",
+	"tweets": 76092,
+	"created_at": "Tue Apr 13 02:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 13 01:44:29 +0000 2021",
+		"id": 1381785330513281000,
+		"id_str": "1381785330513281025",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 유튜브 구독자 6000만명 돌파…전세계 女 아티스트 최초\nhttps://t.co/FVSnXXTjO2\n#블랙핑크 #BLACKPINK https://t.co/RFsLtzztJD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						79,
+						84
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						85,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FVSnXXTjO2",
+					"expanded_url": "https://yg-life.com/archives/167557?lang=ko",
+					"display_url": "yg-life.com/archives/16755…",
+					"indices": [
+						55,
+						78
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1381785329536016400,
+					"id_str": "1381785329536016385",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey0Wb6rXIAEwVKK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey0Wb6rXIAEwVKK.jpg",
+					"url": "https://t.co/RFsLtzztJD",
+					"display_url": "pic.twitter.com/RFsLtzztJD",
+					"expanded_url": "https://twitter.com/fromminn/status/1381785330513281025/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1381785329536016400,
+					"id_str": "1381785329536016385",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/Ey0Wb6rXIAEwVKK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ey0Wb6rXIAEwVKK.jpg",
+					"url": "https://t.co/RFsLtzztJD",
+					"display_url": "pic.twitter.com/RFsLtzztJD",
+					"expanded_url": "https://twitter.com/fromminn/status/1381785330513281025/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": "https://t.co/Z9WwU5pNg0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Z9WwU5pNg0",
+							"expanded_url": "https://minn.tify.kr",
+							"display_url": "minn.tify.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1048,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 45010,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79667,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n블랙핑크, 유튜브 구독자 6000만명 돌파…전세계 女 아티스트 최초\nhttps://t.co/FVSnXXTjO2\n#블랙핑크 #BLACKPINK https://t.co/RFsLtzztJD"
+	},
+	{
 		"created_at": "Mon Apr 12 23:53:17 +0000 2021",
 		"id": 1381757346662191000,
 		"id_str": "1381757346662191106",
