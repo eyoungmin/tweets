d9667b79a6a17edc19bcbe81b6487258c6c79680
Youngmin Lee <iameyoungmin@gmail.com>
2021-04-20 06:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76169,
-	"created_at": "Mon Apr 19 19:02:02 +0000 2021",
+	"tweets": 76170,
+	"created_at": "Mon Apr 19 21:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Mon Apr 19 20:03:57 +0000 2021",
+		"id": 1384236347544666000,
+		"id_str": "1384236347544666113",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n43]인터뷰/내가 나쁜 장애인이 되기로 한 이유(f.박정숙 활동가)\nhttps://t.co/b43GMHtGaP\n\n▶ https://t.co/932nPHbwa8\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						109,
+						113
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						114,
+						117
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						118,
+						121
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						122,
+						125
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						126,
+						129
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						130,
+						135
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						136,
+						144
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						145,
+						152
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/b43GMHtGaP",
+					"expanded_url": "https://www.podty.me/episode/15652633",
+					"display_url": "podty.me/episode/156526…",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/932nPHbwa8",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1618827773537.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1047,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44989,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79755,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n43]인터뷰/내가 나쁜 장애인이 되기로 한 이유(f.박정숙 활동가)\nhttps://t.co/b43GMHtGaP\n\n▶ https://t.co/932nPHbwa8\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Mon Apr 19 18:51:31 +0000 2021",
 		"id": 1384186560736764000,
 		"id_str": "1384218119376306193",
