86ef09784935c5f079f57828e4dd399cafc2fef3
Youngmin Lee <iameyoungmin@gmail.com>
2021-04-20 11:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76174,
-	"created_at": "Tue Apr 20 01:02:02 +0000 2021",
+	"tweets": 76175,
+	"created_at": "Tue Apr 20 02:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 20 01:39:21 +0000 2021",
+		"id": 1384320751734505500,
+		"id_str": "1384320751734505473",
+		"full_text": "#YG라이프 #YG엔터테인먼트\n“걸그룹 최다 기록” 블랙핑크 ‘Lovesick Girls’ MV도 4억뷰 달성\nhttps://t.co/UHVtry5FY5\n#블랙핑크 #BLACKPINK https://t.co/fgAOPVTrsO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						86,
+						91
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						92,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UHVtry5FY5",
+					"expanded_url": "https://yg-life.com/archives/167645?lang=ko",
+					"display_url": "yg-life.com/archives/16764…",
+					"indices": [
+						62,
+						85
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1384320750178513000,
+					"id_str": "1384320750178512899",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EzYYY0mXsAMOx7l.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzYYY0mXsAMOx7l.jpg",
+					"url": "https://t.co/fgAOPVTrsO",
+					"display_url": "pic.twitter.com/fgAOPVTrsO",
+					"expanded_url": "https://twitter.com/fromminn/status/1384320751734505473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 810,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 810,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384320750178513000,
+					"id_str": "1384320750178512899",
+					"indices": [
+						103,
+						126
+					],
+					"media_url": "http://pbs.twimg.com/media/EzYYY0mXsAMOx7l.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EzYYY0mXsAMOx7l.jpg",
+					"url": "https://t.co/fgAOPVTrsO",
+					"display_url": "pic.twitter.com/fgAOPVTrsO",
+					"expanded_url": "https://twitter.com/fromminn/status/1384320751734505473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 810,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 810,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1047,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44989,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79761,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트\n“걸그룹 최다 기록” 블랙핑크 ‘Lovesick Girls’ MV도 4억뷰 달성\nhttps://t.co/UHVtry5FY5\n#블랙핑크 #BLACKPINK https://t.co/fgAOPVTrsO"
+	},
+	{
 		"created_at": "Tue Apr 20 00:43:41 +0000 2021",
 		"id": 1384306743409201200,
 		"id_str": "1384306743409201154",
