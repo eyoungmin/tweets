132c3c5d398181e03e55eca6fcf27c8e2b85c55e
Youngmin Lee <iameyoungmin@gmail.com>
2021-04-21 16:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76191,
-	"created_at": "Wed Apr 21 04:02:02 +0000 2021",
+	"tweets": 76192,
+	"created_at": "Wed Apr 21 07:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Wed Apr 21 06:27:42 +0000 2021",
+		"id": 1384755704884519000,
+		"id_str": "1384755704884518912",
+		"full_text": "#VLIVE #POST\n이루리 (Luli Lee) - I Feel Your Love [Live Clip] | MAGIC STRAWBERRY SOUND\nhttps://t.co/tsOgz4FwtD\n#매직스트로베리사운드 #MSBSOUND #매스사 https://t.co/AeHOEFLvoO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "VLIVE",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "POST",
+					"indices": [
+						7,
+						12
+					]
+				},
+				{
+					"text": "매직스트로베리사운드",
+					"indices": [
+						108,
+						119
+					]
+				},
+				{
+					"text": "MSBSOUND",
+					"indices": [
+						120,
+						129
+					]
+				},
+				{
+					"text": "매스사",
+					"indices": [
+						130,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tsOgz4FwtD",
+					"expanded_url": "https://www.vlive.tv/video/245671",
+					"display_url": "vlive.tv/video/245671",
+					"indices": [
+						84,
+						107
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1384755703433281500,
+					"id_str": "1384755703433281537",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezej-bvWQAE2bM8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezej-bvWQAE2bM8.jpg",
+					"url": "https://t.co/AeHOEFLvoO",
+					"display_url": "pic.twitter.com/AeHOEFLvoO",
+					"expanded_url": "https://twitter.com/fromminn/status/1384755704884518912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1384755703433281500,
+					"id_str": "1384755703433281537",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/Ezej-bvWQAE2bM8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ezej-bvWQAE2bM8.jpg",
+					"url": "https://t.co/AeHOEFLvoO",
+					"display_url": "pic.twitter.com/AeHOEFLvoO",
+					"expanded_url": "https://twitter.com/fromminn/status/1384755704884518912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 576,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1047,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44987,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 79783,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#VLIVE #POST\n이루리 (Luli Lee) - I Feel Your Love [Live Clip] | MAGIC STRAWBERRY SOUND\nhttps://t.co/tsOgz4FwtD\n#매직스트로베리사운드 #MSBSOUND #매스사 https://t.co/AeHOEFLvoO"
+	},
+	{
 		"created_at": "Wed Apr 21 03:02:54 +0000 2021",
 		"id": 1384704168485654500,
 		"id_str": "1384704168485654528",
