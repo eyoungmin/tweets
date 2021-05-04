849f9ebb250a3356f237250a3c14c9036c34b3eb
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-05 03:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 04 17:24:26 +0000 2021",
+		"id": 1389077647150309400,
+		"id_str": "1389632019458498561",
+		"full_text": "스튜디오 지브리가 6월 국내 개봉 예정인 \n[ 아야와 마녀 ] 의 테마곡 \n' Don't disturb me ' 를 개사하고 가창했습니다. 작품 속에서 해당곡을 부르는 캐릭터의 더빙도 함께 맡아 하게 되었습니다.  지브리 스튜디오의 작품을 보면서 자란 저에게 큰 의미가 있는 작업이었습니다. https://t.co/7SEVVI5PQe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1389077634173206500,
+					"id_str": "1389077634173206533",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/E0b-v6ZVgAUYGaq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0b-v6ZVgAUYGaq.jpg",
+					"url": "https://t.co/7SEVVI5PQe",
+					"display_url": "pic.twitter.com/7SEVVI5PQe",
+					"expanded_url": "https://twitter.com/love_yuna/status/1389077647150309383/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1389077634173206500,
+					"id_str": "1389077634173206533",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/E0b-v6ZVgAUYGaq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E0b-v6ZVgAUYGaq.jpg",
+					"url": "https://t.co/7SEVVI5PQe",
+					"display_url": "pic.twitter.com/7SEVVI5PQe",
+					"expanded_url": "https://twitter.com/love_yuna/status/1389077647150309383/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 273076,
+			"friends_count": 531,
+			"listed_count": 2826,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 285,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6239,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1358217725089255426/J-5E7g_n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1358217725089255426/J-5E7g_n_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1612661273",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3195,
+		"favorite_count": 2834,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "스튜디오 지브리가 6월 국내 개봉 예정인 \n[ 아야와 마녀 ] 의 테마곡 \n' Don't disturb me ' 를 개사하고 가창했습니다. 작품 속에서 해당곡을 부르는 캐릭터의 더빙도 함께 맡아 하게 되었습니다.  지브리 스튜디오의 작품을 보면서 자란 저에게 큰 의미가 있는 작업이었습니다. https://t.co/7SEVVI5PQe"
+	},
+	{
 		"created_at": "Tue May 04 14:36:23 +0000 2021",
 		"id": 1389589728119464000,
 		"id_str": "1389589728119463936",
