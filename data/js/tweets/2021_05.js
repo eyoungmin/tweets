dbaf75d5530632c7b15fb9edf43f555a2ec36859
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-11 16:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 11 06:48:50 +0000 2021",
+		"id": 1391996621404004400,
+		"id_str": "1392008783585366017",
+		"full_text": "[EVENT] 태민에게 모두 체크메이트♟된 이유! 들어봐, 그의 'Advice'\n\n👉🏻 https://t.co/lZsOGYGMRT\n \n#TAEMIN #태민 \n#SHINee #샤이니\n#Advice\n#태민_Advice_0518발매\n#태민에게_체크메이트된_샤월들의_Advice",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						75,
+						82
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						88,
+						95
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "Advice",
+					"indices": [
+						101,
+						108
+					]
+				},
+				{
+					"text": "태민_Advice_0518발매",
+					"indices": [
+						109,
+						126
+					]
+				},
+				{
+					"text": "태민에게_체크메이트된_샤월들의_Advice",
+					"indices": [
+						127,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lZsOGYGMRT",
+					"expanded_url": "http://naver.me/x2PLcVbQ",
+					"display_url": "naver.me/x2PLcVbQ",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "TAEMIN - The 3rd Mini Album Advice",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1957419,
+			"friends_count": 2,
+			"listed_count": 4884,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1749,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1619967641",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3013,
+		"favorite_count": 7037,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[EVENT] 태민에게 모두 체크메이트♟된 이유! 들어봐, 그의 'Advice'\n\n👉🏻 https://t.co/lZsOGYGMRT\n \n#TAEMIN #태민 \n#SHINee #샤이니\n#Advice\n#태민_Advice_0518발매\n#태민에게_체크메이트된_샤월들의_Advice"
+	},
+	{
 		"created_at": "Tue May 11 05:28:56 +0000 2021",
 		"id": 1391988675144192000,
 		"id_str": "1391988675144192005",
