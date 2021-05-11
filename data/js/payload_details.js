dd1c477e254ba483e5c82f8d5bf9dbf50c4a9f40
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-11 23:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76543,
-	"created_at": "Tue May 11 13:02:01 +0000 2021",
+	"tweets": 76544,
+	"created_at": "Tue May 11 14:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Tue May 11 13:33:33 +0000 2021",
+		"id": 1392102191381422000,
+		"id_str": "1392110633760419841",
+		"full_text": "잠시 후 오후 10시 30분(KST) 방송되는 #강철부대 의 엄청난 야간 연합 작전에 오늘도 #츄 의 몰입력은 100%😎 오빛도 심장 부여잡고 같이 본방사수 달려 츄세요💞\n\n#이달의소녀 #LOONA #Chuu https://t.co/TM24C2xkqK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "강철부대",
+					"indices": [
+						26,
+						31
+					]
+				},
+				{
+					"text": "츄",
+					"indices": [
+						52,
+						54
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						97,
+						103
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						104,
+						110
+					]
+				},
+				{
+					"text": "Chuu",
+					"indices": [
+						111,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1392073668595904500,
+					"id_str": "1392073668595904516",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/E1GjoEXVcAQgPFn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1GjoEXVcAQgPFn.jpg",
+					"url": "https://t.co/TM24C2xkqK",
+					"display_url": "pic.twitter.com/TM24C2xkqK",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1392102191381422080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1392073668595904500,
+					"id_str": "1392073668595904516",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/E1GjoEXVcAQgPFn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1GjoEXVcAQgPFn.jpg",
+					"url": "https://t.co/TM24C2xkqK",
+					"display_url": "pic.twitter.com/TM24C2xkqK",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1392102191381422080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1392073668600111000,
+					"id_str": "1392073668600111105",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/media/E1GjoEYVoAE9fy9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E1GjoEYVoAE9fy9.jpg",
+					"url": "https://t.co/TM24C2xkqK",
+					"display_url": "pic.twitter.com/TM24C2xkqK",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1392102191381422080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/hthRlo3Dn7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hthRlo3Dn7",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 701099,
+			"friends_count": 0,
+			"listed_count": 7181,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2889,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1602772120",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4594,
+		"favorite_count": 16396,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "잠시 후 오후 10시 30분(KST) 방송되는 #강철부대 의 엄청난 야간 연합 작전에 오늘도 #츄 의 몰입력은 100%😎 오빛도 심장 부여잡고 같이 본방사수 달려 츄세요💞\n\n#이달의소녀 #LOONA #Chuu https://t.co/TM24C2xkqK"
+	},
+	{
 		"created_at": "Tue May 11 12:10:43 +0000 2021",
 		"id": 1392089785876066300,
 		"id_str": "1392089785876066306",
