2a3a6a1ed851dc0ead0253bba0c45f7148411b00
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-13 06:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76574,
-	"created_at": "Wed May 12 18:02:02 +0000 2021",
+	"tweets": 76575,
+	"created_at": "Wed May 12 21:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Wed May 12 20:12:51 +0000 2021",
+		"id": 1392573504982245400,
+		"id_str": "1392573504982245376",
+		"full_text": "#듣다보면똑똑해지는라디오 #듣똑라\n51]'빈센조'부터 '모범택시'까지 한국판 다크히어로 왜 인기?(f.민경원 기자)\nhttps://t.co/tMN04JWNdI\n\n▶ https://t.co/NpJHrv13XI\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "듣다보면똑똑해지는라디오",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "듣똑라",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "미디어",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "시사",
+					"indices": [
+						122,
+						125
+					]
+				},
+				{
+					"text": "경제",
+					"indices": [
+						126,
+						129
+					]
+				},
+				{
+					"text": "법조",
+					"indices": [
+						130,
+						133
+					]
+				},
+				{
+					"text": "문화",
+					"indices": [
+						134,
+						137
+					]
+				},
+				{
+					"text": "팟캐스트",
+					"indices": [
+						138,
+						143
+					]
+				},
+				{
+					"text": "Podcast",
+					"indices": [
+						144,
+						152
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						153,
+						160
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tMN04JWNdI",
+					"expanded_url": "https://www.podty.me/episode/15753088",
+					"display_url": "podty.me/episode/157530…",
+					"indices": [
+						65,
+						88
+					]
+				},
+				{
+					"url": "https://t.co/NpJHrv13XI",
+					"expanded_url": "https://cdn-cf.podty.me/meta/episode_audio/100007804/190484_1620842011240.mp3",
+					"display_url": "cdn-cf.podty.me/meta/episode_a…",
+					"indices": [
+						92,
+						115
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://zapier.com/\" rel=\"nofollow\">Zapier.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 317,
+			"friends_count": 1050,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44997,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80178,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#듣다보면똑똑해지는라디오 #듣똑라\n51]'빈센조'부터 '모범택시'까지 한국판 다크히어로 왜 인기?(f.민경원 기자)\nhttps://t.co/tMN04JWNdI\n\n▶ https://t.co/NpJHrv13XI\n\n#미디어 #시사 #경제 #법조 #문화\n#팟캐스트 #Podcast #minfav"
+	},
+	{
 		"created_at": "Wed May 12 17:06:44 +0000 2021",
 		"id": 1392526669722931200,
 		"id_str": "1392526669722931203",
