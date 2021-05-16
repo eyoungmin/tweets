79e0b188a416098d21b8cc06068969a7cbef504b
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-16 23:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76656,
-	"created_at": "Sun May 16 13:02:03 +0000 2021",
+	"tweets": 76657,
+	"created_at": "Sun May 16 14:02:03 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,10 +1,133 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sun May 16 13:24:27 +0000 2021",
+		"id": 1393920282499457000,
+		"id_str": "1393920282499457024",
+		"full_text": "#EO\n근거 없는 일처리에 지친 직장인들을 위한 영상\nhttps://t.co/RVZJMebVYr\n#스타트업 #태용 #minfav",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "EO",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "스타트업",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "태용",
+					"indices": [
+						60,
+						63
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						64,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RVZJMebVYr",
+					"expanded_url": "https://youtu.be/pgsAMqJ2StU",
+					"display_url": "youtu.be/pgsAMqJ2StU",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1050,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44979,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80275,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#EO\n근거 없는 일처리에 지친 직장인들을 위한 영상\nhttps://t.co/RVZJMebVYr\n#스타트업 #태용 #minfav"
+	},
+	{
 		"created_at": "Sun May 16 12:12:32 +0000 2021",
 		"id": 1393891894833258500,
 		"id_str": "1393902183909789703",
-		"full_text": "[#츄 / #Chuu] 디로롱💙 하늘색민트색에 빠졌어요 그래서 feat.민트소녀의 폴라로이드🦋👗 🧚🏻🧞‍♂️🛼🪣🛋\n\n#이달의소녀 #LOONA https://t.co/gKfkmrbAMe",
+		"full_text": "[#츄 / #Chuu] 디로롱💙 하늘색민트색에 빠졌어요 그래서 feat.민트소녀의 폴라로이드🦋👗 🧚🏻🧞♂️🛼🪣🛋\n\n#이달의소녀 #LOONA https://t.co/gKfkmrbAMe",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -310,7 +433,7 @@ Grailbird.data.tweets_2021_05 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#츄 / #Chuu] 디로롱💙 하늘색민트색에 빠졌어요 그래서 feat.민트소녀의 폴라로이드🦋👗 🧚🏻🧞‍♂️🛼🪣🛋\n\n#이달의소녀 #LOONA https://t.co/gKfkmrbAMe"
+		"text": "[#츄 / #Chuu] 디로롱💙 하늘색민트색에 빠졌어요 그래서 feat.민트소녀의 폴라로이드🦋👗 🧚🏻🧞♂️🛼🪣🛋\n\n#이달의소녀 #LOONA https://t.co/gKfkmrbAMe"
 	},
 	{
 		"created_at": "Sun May 16 09:53:13 +0000 2021",
