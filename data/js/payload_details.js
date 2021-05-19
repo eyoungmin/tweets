f7010856a2d17d60a5f6cc37681238c0f143ff66
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-19 14:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76759,
-	"created_at": "Wed May 19 04:02:05 +0000 2021",
+	"tweets": 76760,
+	"created_at": "Wed May 19 05:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Wed May 19 04:53:51 +0000 2021",
+		"id": 1394872873991381000,
+		"id_str": "1394878947595145216",
+		"full_text": "태민 새 미니앨범 ‘Advice’ 국내외 차트 1위! ‘역솔남’ 파워 입증!\n\n아이튠즈 톱 앨범 차트 전 세계 41개 지역 1위!\n\n➡️ https://t.co/EkpwALLox7\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Advice",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						102,
+						109
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						110,
+						113
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						114,
+						121
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						122,
+						126
+					]
+				},
+				{
+					"text": "Advice",
+					"indices": [
+						127,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EkpwALLox7",
+					"expanded_url": "https://bit.ly/2T5vxWJ",
+					"display_url": "bit.ly/2T5vxWJ",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "TAEMIN - The 3rd Mini Album Advice",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1974521,
+			"friends_count": 2,
+			"listed_count": 5010,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1789,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1619967641",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2702,
+		"favorite_count": 6360,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "태민 새 미니앨범 ‘Advice’ 국내외 차트 1위! ‘역솔남’ 파워 입증!\n\n아이튠즈 톱 앨범 차트 전 세계 41개 지역 1위!\n\n➡️ https://t.co/EkpwALLox7\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Advice"
+	},
+	{
 		"created_at": "Wed May 19 03:13:34 +0000 2021",
 		"id": 1394850302940045300,
 		"id_str": "1394853710115786755",
