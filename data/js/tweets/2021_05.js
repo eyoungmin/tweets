1bd65cc56b021a8773562e1607dfd4bdb49c3149
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-22 18:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,158 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 22 08:54:40 +0000 2021",
+		"id": 1396016678144671700,
+		"id_str": "1396026715256725504",
+		"full_text": "TAEMIN 태민 - ‘Advice’ l 쇼! 음악중심 210522\n\n무대 👉🏻 https://t.co/sk1KJcF1la\n직캠 👉🏻 https://t.co/KMi4ugY8rA\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Advice",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						100,
+						107
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						108,
+						111
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						112,
+						119
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						120,
+						124
+					]
+				},
+				{
+					"text": "Advice",
+					"indices": [
+						125,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sk1KJcF1la",
+					"expanded_url": "http://naver.me/Gz5ZmI1y",
+					"display_url": "naver.me/Gz5ZmI1y",
+					"indices": [
+						45,
+						68
+					]
+				},
+				{
+					"url": "https://t.co/KMi4ugY8rA",
+					"expanded_url": "https://youtu.be/qfMabZBDPTg",
+					"display_url": "youtu.be/qfMabZBDPTg",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "TAEMIN - The 3rd Mini Album Advice",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1980442,
+			"friends_count": 2,
+			"listed_count": 5028,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1798,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1619967641",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4534,
+		"favorite_count": 8912,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEMIN 태민 - ‘Advice’ l 쇼! 음악중심 210522\n\n무대 👉🏻 https://t.co/sk1KJcF1la\n직캠 👉🏻 https://t.co/KMi4ugY8rA\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Advice"
+	},
+	{
 		"created_at": "Sat May 22 07:29:27 +0000 2021",
 		"id": 1396005269805666300,
 		"id_str": "1396005269805666304",
