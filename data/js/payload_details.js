2c969ff39ecf38d512f51192e3d9a96c2b1d903f
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-23 10:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 76899,
-	"created_at": "Sat May 22 18:02:04 +0000 2021",
+	"tweets": 76900,
+	"created_at": "Sun May 23 01:02:06 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sun May 23 00:41:29 +0000 2021",
+		"id": 1396264990802419700,
+		"id_str": "1396264990802419713",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학]'두 번째 팬데믹'을 슬기롭게 극복하려면\nhttps://t.co/jnNwKVNYCt\n#박진영 #심리학 #minfav https://t.co/56VFrpzw71",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						75,
+						79
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						80,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jnNwKVNYCt",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=46729",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1396264989904736300,
+					"id_str": "1396264989904736259",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/E2CHnE-WEAMEVOE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2CHnE-WEAMEVOE.jpg",
+					"url": "https://t.co/56VFrpzw71",
+					"display_url": "pic.twitter.com/56VFrpzw71",
+					"expanded_url": "https://twitter.com/fromminn/status/1396264990802419713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1396264989904736300,
+					"id_str": "1396264989904736259",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/E2CHnE-WEAMEVOE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2CHnE-WEAMEVOE.jpg",
+					"url": "https://t.co/56VFrpzw71",
+					"display_url": "pic.twitter.com/56VFrpzw71",
+					"expanded_url": "https://twitter.com/fromminn/status/1396264990802419713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 325,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1050,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44980,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80539,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학]'두 번째 팬데믹'을 슬기롭게 극복하려면\nhttps://t.co/jnNwKVNYCt\n#박진영 #심리학 #minfav https://t.co/56VFrpzw71"
+	},
+	{
 		"created_at": "Sat May 22 17:49:47 +0000 2021",
 		"id": 1396161380844281900,
 		"id_str": "1396161380844281858",
