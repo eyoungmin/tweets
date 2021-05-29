c695752259cf52f17042cd73c0618df15b806ef7
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-29 09:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77085,
-	"created_at": "Fri May 28 19:02:08 +0000 2021",
+	"tweets": 77086,
+	"created_at": "Sat May 29 00:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 29 00:01:59 +0000 2021",
+		"id": 1398429378078515200,
+		"id_str": "1398429378078515200",
+		"full_text": "#박진영의사회심리학\n[박진영의 사회심리학]그때로 돌아가도 후회할 선택을 할 가능성이 높다\nhttps://t.co/wUZnPPnXL2\n#박진영 #심리학 #minfav https://t.co/C2JDB7eU5T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "박진영의사회심리학",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "박진영",
+					"indices": [
+						74,
+						78
+					]
+				},
+				{
+					"text": "심리학",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "minfav",
+					"indices": [
+						84,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wUZnPPnXL2",
+					"expanded_url": "http://dongascience.donga.com/news.php?idx=46875",
+					"display_url": "dongascience.donga.com/news.php?idx=4…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1398429376606314500,
+					"id_str": "1398429376606314496",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E2g4G9wXIAA39MP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2g4G9wXIAA39MP.jpg",
+					"url": "https://t.co/C2JDB7eU5T",
+					"display_url": "pic.twitter.com/C2JDB7eU5T",
+					"expanded_url": "https://twitter.com/fromminn/status/1398429378078515200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1398429376606314500,
+					"id_str": "1398429376606314496",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E2g4G9wXIAA39MP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2g4G9wXIAA39MP.jpg",
+					"url": "https://t.co/C2JDB7eU5T",
+					"display_url": "pic.twitter.com/C2JDB7eU5T",
+					"expanded_url": "https://twitter.com/fromminn/status/1398429378078515200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 565,
+							"h": 295,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악 그리고 기술에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44972,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80775,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#박진영의사회심리학\n[박진영의 사회심리학]그때로 돌아가도 후회할 선택을 할 가능성이 높다\nhttps://t.co/wUZnPPnXL2\n#박진영 #심리학 #minfav https://t.co/C2JDB7eU5T"
+	},
+	{
 		"created_at": "Fri May 28 18:39:41 +0000 2021",
 		"id": 1398348268715847700,
 		"id_str": "1398348268715847690",
