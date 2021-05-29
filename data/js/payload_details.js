dc8ced74cebd516dffcda156b58a4e5c23f91720
Youngmin Lee <iameyoungmin@gmail.com>
2021-05-29 19:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77090,
-	"created_at": "Sat May 29 09:02:08 +0000 2021",
+	"tweets": 77091,
+	"created_at": "Sat May 29 10:02:08 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 29 09:47:27 +0000 2021",
+		"id": 1398564854508638200,
+		"id_str": "1398576714179960836",
+		"full_text": "[릴레이댄스] aespa(에스파) - Next Level (4K)\n\nhttps://t.co/qHz1OCNleh\n\n#aespa #æspa #에스파\n#NextLevel https://t.co/25BV8OOIfW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						63,
+						69
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "NextLevel",
+					"indices": [
+						81,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qHz1OCNleh",
+					"expanded_url": "https://youtu.be/4bus7zF6K-Y",
+					"display_url": "youtu.be/4bus7zF6K-Y",
+					"indices": [
+						38,
+						61
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1398564849148301300,
+					"id_str": "1398564849148301312",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E2izUgTVEAA3E5d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2izUgTVEAA3E5d.jpg",
+					"url": "https://t.co/25BV8OOIfW",
+					"display_url": "pic.twitter.com/25BV8OOIfW",
+					"expanded_url": "https://twitter.com/aespa_official/status/1398564854508638208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1398564849148301300,
+					"id_str": "1398564849148301312",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/E2izUgTVEAA3E5d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2izUgTVEAA3E5d.jpg",
+					"url": "https://t.co/25BV8OOIfW",
+					"display_url": "pic.twitter.com/25BV8OOIfW",
+					"expanded_url": "https://twitter.com/aespa_official/status/1398564854508638208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Next Level",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1020706,
+			"friends_count": 1,
+			"listed_count": 3815,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 632,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1620226804",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8830,
+		"favorite_count": 19052,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[릴레이댄스] aespa(에스파) - Next Level (4K)\n\nhttps://t.co/qHz1OCNleh\n\n#aespa #æspa #에스파\n#NextLevel https://t.co/25BV8OOIfW"
+	},
+	{
 		"created_at": "Sat May 29 08:58:32 +0000 2021",
 		"id": 1398564405487489000,
 		"id_str": "1398564405487489027",
