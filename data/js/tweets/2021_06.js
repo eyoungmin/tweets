49b18f93b57e23a5efa4d6faef98ba253a7f8935
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-01 12:02:09 +0900

    Added 2 tweets

---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,396 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Tue Jun 01 02:48:07 +0000 2021",
+		"id": 1399552848308834300,
+		"id_str": "1399558347347546114",
+		"full_text": "‘Solo debut’ JOY tops various Korean music charts with her new song ‘안녕 (Hello)’ as well as recording no.1 on the iTunes Top Album charts of 26 international regions with her Special album!\n\n#JOY #조이 #안녕 #Hello #레드벨벳 #RedVelvet",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			227
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JOY",
+					"indices": [
+						191,
+						195
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						196,
+						199
+					]
+				},
+				{
+					"text": "안녕",
+					"indices": [
+						200,
+						203
+					]
+				},
+				{
+					"text": "Hello",
+					"indices": [
+						204,
+						210
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						211,
+						216
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						217,
+						227
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1399552846685634600,
+		"in_reply_to_status_id_str": "1399552846685634561",
+		"in_reply_to_user_id": 873092428755894300,
+		"in_reply_to_user_id_str": "873092428755894272",
+		"in_reply_to_screen_name": "RVsmtown",
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/U7JtipmDEo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/U7JtipmDEo",
+							"expanded_url": "https://youtu.be/lNvBbh5jDcA",
+							"display_url": "youtu.be/lNvBbh5jDcA",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4173519,
+			"friends_count": 1,
+			"listed_count": 8889,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1904,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1622019868",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1658,
+		"favorite_count": 4906,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "‘Solo debut’ JOY tops various Korean music charts with her new song ‘안녕 (Hello)’ as well as recording no.1 on the iTunes Top Album charts of 26 international regions with her Special album!\n\n#JOY #조이 #안녕 #Hello #레드벨벳 #RedVelvet"
+	},
+	{
+		"created_at": "Tue Jun 01 02:48:07 +0000 2021",
+		"id": 1399552846685634600,
+		"id_str": "1399558347322322951",
+		"full_text": "‘솔로 데뷔’ 조이, 리메이크 감성 또 통했다! ‘안녕 (Hello)’ 각종 음원차트 1위! 아이튠즈 26개 지역 1위!\n \n#JOY #조이 #안녕 #Hello #레드벨벳 #RedVelvet https://t.co/dlu7JCj5Y0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JOY",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						75,
+						78
+					]
+				},
+				{
+					"text": "안녕",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "Hello",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						90,
+						95
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						96,
+						106
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1399552842109575200,
+					"id_str": "1399552842109575168",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/E2w15Q5UYAAGm-R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2w15Q5UYAAGm-R.jpg",
+					"url": "https://t.co/dlu7JCj5Y0",
+					"display_url": "pic.twitter.com/dlu7JCj5Y0",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1399552846685634561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1066,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1399552842109575200,
+					"id_str": "1399552842109575168",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/E2w15Q5UYAAGm-R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2w15Q5UYAAGm-R.jpg",
+					"url": "https://t.co/dlu7JCj5Y0",
+					"display_url": "pic.twitter.com/dlu7JCj5Y0",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1399552846685634561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1066,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1399552844743647200,
+					"id_str": "1399552844743647238",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/E2w15atVIAYkIDm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2w15atVIAYkIDm.jpg",
+					"url": "https://t.co/dlu7JCj5Y0",
+					"display_url": "pic.twitter.com/dlu7JCj5Y0",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1399552846685634561/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1066,
+							"h": 1600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/U7JtipmDEo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/U7JtipmDEo",
+							"expanded_url": "https://youtu.be/lNvBbh5jDcA",
+							"display_url": "youtu.be/lNvBbh5jDcA",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4173519,
+			"friends_count": 1,
+			"listed_count": 8889,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1904,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1622019868",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4657,
+		"favorite_count": 12735,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‘솔로 데뷔’ 조이, 리메이크 감성 또 통했다! ‘안녕 (Hello)’ 각종 음원차트 1위! 아이튠즈 26개 지역 1위!\n \n#JOY #조이 #안녕 #Hello #레드벨벳 #RedVelvet https://t.co/dlu7JCj5Y0"
+	}
+]
