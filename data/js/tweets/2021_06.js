c38defe8b24ad1b1f7929ccd32d4f6e77bb8e519
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-03 17:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 03 07:09:30 +0000 2021",
+		"id": 1400348902868590600,
+		"id_str": "1400348902868590592",
+		"full_text": "#지켜츄 #ChuuCanDoIt \n(ENG) 인생샷 찍다가 PD언니랑 손절각...? ( ˃̣̣̥᷄⌓˂̣̣̥᷅ ) 📸 화려한 목포가 츄를 싸악 감싸네❤️| 지켜츄 EP21 \nhttps://t.co/iDyWlE1qZk \n \n#이달의소녀 #LOONA \n#김지우 #츄 #Chuu #츄튜브",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "지켜츄",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "ChuuCanDoIt",
+					"indices": [
+						5,
+						17
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						123,
+						129
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						130,
+						136
+					]
+				},
+				{
+					"text": "김지우",
+					"indices": [
+						138,
+						142
+					]
+				},
+				{
+					"text": "츄",
+					"indices": [
+						143,
+						145
+					]
+				},
+				{
+					"text": "Chuu",
+					"indices": [
+						146,
+						151
+					]
+				},
+				{
+					"text": "츄튜브",
+					"indices": [
+						152,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iDyWlE1qZk",
+					"expanded_url": "https://www.youtube.com/watch?v=qhFfGEigRB4",
+					"display_url": "youtube.com/watch?v=qhFfGE…",
+					"indices": [
+						96,
+						119
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44949,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 80904,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#지켜츄 #ChuuCanDoIt \n(ENG) 인생샷 찍다가 PD언니랑 손절각...? ( ˃̣̣̥᷄⌓˂̣̣̥᷅ ) 📸 화려한 목포가 츄를 싸악 감싸네❤️| 지켜츄 EP21 \nhttps://t.co/iDyWlE1qZk \n \n#이달의소녀 #LOONA \n#김지우 #츄 #Chuu #츄튜브"
+	},
+	{
 		"created_at": "Thu Jun 03 05:52:46 +0000 2021",
 		"id": 1400321952812658700,
 		"id_str": "1400329593295294465",
