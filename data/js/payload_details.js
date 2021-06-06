cb9688987d868086e269bcef95d8efb906d10dfd
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-06 14:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77284,
-	"created_at": "Sun Jun 06 04:02:02 +0000 2021",
+	"tweets": 77285,
+	"created_at": "Sun Jun 06 05:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sun Jun 06 04:59:33 +0000 2021",
+		"id": 1401396098963439600,
+		"id_str": "1401403363149762562",
+		"full_text": "TAEMIN 태민 ‘Heaven’ Dronecam 드론캠 Ver. ┃ Beyond LIVE – TAEMIN : N.G.D.A\n\n👉🏻 https://t.co/5bFZhezaUU\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Heaven\n#BeyondLIVE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						99,
+						106
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						107,
+						110
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						111,
+						118
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						119,
+						123
+					]
+				},
+				{
+					"text": "Heaven",
+					"indices": [
+						124,
+						131
+					]
+				},
+				{
+					"text": "BeyondLIVE",
+					"indices": [
+						132,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5bFZhezaUU",
+					"expanded_url": "https://youtu.be/Gi4jAxsk55g",
+					"display_url": "youtu.be/Gi4jAxsk55g",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "TAEMIN - The 3rd Mini Album Advice",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2014921,
+			"friends_count": 2,
+			"listed_count": 5082,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1855,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1388871061001818113/Y6wPmXrL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1619967641",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2868,
+		"favorite_count": 6009,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "TAEMIN 태민 ‘Heaven’ Dronecam 드론캠 Ver. ┃ Beyond LIVE – TAEMIN : N.G.D.A\n\n👉🏻 https://t.co/5bFZhezaUU\n\n#TAEMIN #태민\n#SHINee #샤이니\n#Heaven\n#BeyondLIVE"
+	},
+	{
 		"created_at": "Sun Jun 06 03:04:37 +0000 2021",
 		"id": 1401374440462721000,
 		"id_str": "1401374440462721027",
