713d9a90416c4a66ae02778208fbd2af99e5c6ce
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-07 12:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77302,
-	"created_at": "Sun Jun 06 21:02:01 +0000 2021",
+	"tweets": 77303,
+	"created_at": "Mon Jun 07 03:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 07 02:54:14 +0000 2021",
+		"id": 1401721054280654800,
+		"id_str": "1401734216899907588",
+		"full_text": "[NOTICE] 청하랑 별하랑이 기념일이라 그만큼 신나시는 거지~ VLIVE 시간 변경 안내\n\n금일 저녁 8시 30분 예정되어 있던 VLIVE 시간이 내부 사정에 의해 저녁 7시로 변경되었습니다.\n\n▶ https://t.co/w1h9nkQoAq\n\n#청하 #CHUNGHA \n#별하랑 #BYULHARANG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						138,
+						141
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						142,
+						150
+					]
+				},
+				{
+					"text": "별하랑",
+					"indices": [
+						152,
+						156
+					]
+				},
+				{
+					"text": "BYULHARANG",
+					"indices": [
+						157,
+						168
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/w1h9nkQoAq",
+					"expanded_url": "https://www.vlive.tv/post/0-23271566",
+					"display_url": "vlive.tv/post/0-23271566",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/uAFyf5T7B0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uAFyf5T7B0",
+							"expanded_url": "https://ffm.to/chungha_querencia",
+							"display_url": "ffm.to/chungha_queren…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 331575,
+			"friends_count": 0,
+			"listed_count": 2027,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1888,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1377637690447831041/B-NTsYbm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1377637690447831041/B-NTsYbm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1617289944",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 138,
+		"favorite_count": 674,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NOTICE] 청하랑 별하랑이 기념일이라 그만큼 신나시는 거지~ VLIVE 시간 변경 안내\n\n금일 저녁 8시 30분 예정되어 있던 VLIVE 시간이 내부 사정에 의해 저녁 7시로 변경되었습니다.\n\n▶ https://t.co/w1h9nkQoAq\n\n#청하 #CHUNGHA \n#별하랑 #BYULHARANG"
+	},
+	{
 		"created_at": "Sun Jun 06 20:54:22 +0000 2021",
 		"id": 1401643652091371500,
 		"id_str": "1401643652091371522",
