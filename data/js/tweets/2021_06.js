e41c3a8b76d6125e15118528b86b11a012b0dfd9
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-10 00:19:56 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Wed Jun 09 15:09:35 +0000 2021",
+		"id": 1402642630182269000,
+		"id_str": "1402644049643847683",
+		"full_text": "블루버스데이 (BLUEBIRTHDAY)\n\nNAVER TV ➫ 21.07.23\nYouTube ➫ 21.08.06\n\n#YERI #예리 #김예림\n#블루버스데이 #BLUEBIRTHDAY\n#레드벨벳 #RedVelvet https://t.co/OnPgduJDH1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YERI",
+					"indices": [
+						63,
+						68
+					]
+				},
+				{
+					"text": "예리",
+					"indices": [
+						69,
+						72
+					]
+				},
+				{
+					"text": "김예림",
+					"indices": [
+						73,
+						77
+					]
+				},
+				{
+					"text": "블루버스데이",
+					"indices": [
+						78,
+						85
+					]
+				},
+				{
+					"text": "BLUEBIRTHDAY",
+					"indices": [
+						86,
+						99
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						106,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1402642597114384400,
+					"id_str": "1402642597114384388",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1402642597114384388/pu/img/6_JLEj0YtPY-8pow.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1402642597114384388/pu/img/6_JLEj0YtPY-8pow.jpg",
+					"url": "https://t.co/OnPgduJDH1",
+					"display_url": "pic.twitter.com/OnPgduJDH1",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1402642630182268934/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1402642597114384400,
+					"id_str": "1402642597114384388",
+					"indices": [
+						117,
+						140
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1402642597114384388/pu/img/6_JLEj0YtPY-8pow.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1402642597114384388/pu/img/6_JLEj0YtPY-8pow.jpg",
+					"url": "https://t.co/OnPgduJDH1",
+					"display_url": "pic.twitter.com/OnPgduJDH1",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1402642630182268934/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 19720,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1402642597114384388/pu/vid/320x568/L94rKViAy3jIl48a.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1402642597114384388/pu/vid/480x852/__MA1lBS_WmtHVXp.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1402642597114384388/pu/vid/720x1280/BolLtBM1KcadRg8j.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1402642597114384388/pu/pl/twgeDSPTVDRzIqC_.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/U7JtipmDEo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/U7JtipmDEo",
+							"expanded_url": "https://youtu.be/lNvBbh5jDcA",
+							"display_url": "youtu.be/lNvBbh5jDcA",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4213173,
+			"friends_count": 1,
+			"listed_count": 8989,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1943,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1622019868",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6897,
+		"favorite_count": 14345,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블루버스데이 (BLUEBIRTHDAY)\n\nNAVER TV ➫ 21.07.23\nYouTube ➫ 21.08.06\n\n#YERI #예리 #김예림\n#블루버스데이 #BLUEBIRTHDAY\n#레드벨벳 #RedVelvet https://t.co/OnPgduJDH1"
+	},
+	{
 		"created_at": "Wed Jun 09 12:55:10 +0000 2021",
 		"id": 1402599482064183300,
 		"id_str": "1402610220384231427",
