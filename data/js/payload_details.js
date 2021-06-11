5a67262203f4e3b51403157243c89e25c50c3329
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-11 15:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77440,
-	"created_at": "Fri Jun 11 05:02:05 +0000 2021",
+	"tweets": 77441,
+	"created_at": "Fri Jun 11 06:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Fri Jun 11 05:22:24 +0000 2021",
+		"id": 1403210370953580500,
+		"id_str": "1403221052457590785",
+		"full_text": "배우로 돌아오는 '레드벨벳 조이', 이번에는 시한부 역을 맡는다?\n\n#레드벨벳 #조이 #redvelvet #joy \nhttps://t.co/jKKGatJ848",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레드벨벳",
+					"indices": [
+						38,
+						43
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						44,
+						47
+					]
+				},
+				{
+					"text": "redvelvet",
+					"indices": [
+						48,
+						58
+					]
+				},
+				{
+					"text": "joy",
+					"indices": [
+						59,
+						63
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jKKGatJ848",
+					"expanded_url": "https://www.wkorea.com/2021/06/11/%EB%A0%88%EB%93%9C%EB%B2%A8%EB%B2%B3-%EC%A1%B0%EC%9D%B4-%EC%8B%9C%ED%95%9C%EB%B6%80-%EC%9D%B8%ED%94%8C%EB%A3%A8%EC%96%B8%EC%84%9C-%EC%97%B0%EA%B8%B0/?ddw=90340&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2021/06/11/%EB…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/m6zPKg47IM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m6zPKg47IM",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 273342,
+			"friends_count": 0,
+			"listed_count": 801,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 6311,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12487,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1621505248",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 371,
+		"favorite_count": 853,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "배우로 돌아오는 '레드벨벳 조이', 이번에는 시한부 역을 맡는다?\n\n#레드벨벳 #조이 #redvelvet #joy \nhttps://t.co/jKKGatJ848"
+	},
+	{
 		"created_at": "Fri Jun 11 04:22:23 +0000 2021",
 		"id": 1403201617994731500,
 		"id_str": "1403205951335796736",
