8c1e31c200d95bcfe58fe19d0f90aa6e18b02d71
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-13 15:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77535,
-	"created_at": "Sun Jun 13 05:02:04 +0000 2021",
+	"tweets": 77536,
+	"created_at": "Sun Jun 13 06:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,118 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sun Jun 13 05:20:05 +0000 2021",
+		"id": 1403940385492148200,
+		"id_str": "1403945246136832000",
+		"full_text": "aespa ‘Next Level’ MV 조회 수 1억 뷰 돌파 시 스페셜 안무 영상이 공개됩니다💜\n스페셜 안무 영상 컨셉을 투표해주세요!\n\nOnce ‘Next Level’ MV reaches 100M views, a special choreography video will be released💜\nVote for the concept you want!\n\n#aespa #æspa #에스파\n#NextLevel",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			226
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						198,
+						204
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						205,
+						210
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						211,
+						215
+					]
+				},
+				{
+					"text": "NextLevel",
+					"indices": [
+						216,
+						226
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Next Level",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1081878,
+			"friends_count": 1,
+			"listed_count": 4108,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 679,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1620226804",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6695,
+		"favorite_count": 14421,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "aespa ‘Next Level’ MV 조회 수 1억 뷰 돌파 시 스페셜 안무 영상이 공개됩니다💜\n스페셜 안무 영상 컨셉을 투표해주세요!\n\nOnce ‘Next Level’ MV reaches 100M views, a special choreography video will be released💜\nVote for the concept you want!\n\n#aespa #æspa #에스파\n#NextLevel"
+	},
+	{
 		"created_at": "Sun Jun 13 04:20:52 +0000 2021",
 		"id": 1403921573753495600,
 		"id_str": "1403930345074876419",
