5ea72df5d8b3e1dd0764b2b14f4549f0d72116bc
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-17 22:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 17 12:57:16 +0000 2021",
+		"id": 1405505960274129000,
+		"id_str": "1405509851569037319",
+		"full_text": "오늘은 혼슬TIME! 🐻🧡\n\n#슬기 #SEULGI #슬기집 #슬기zip #레드벨벳 #RedVelvet https://t.co/AG9lftPBEv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬기",
+					"indices": [
+						16,
+						19
+					]
+				},
+				{
+					"text": "SEULGI",
+					"indices": [
+						20,
+						27
+					]
+				},
+				{
+					"text": "슬기집",
+					"indices": [
+						28,
+						32
+					]
+				},
+				{
+					"text": "슬기zip",
+					"indices": [
+						33,
+						39
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						40,
+						45
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						46,
+						56
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1405505948060323800,
+					"id_str": "1405505948060323841",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/E4FcNovVkAE7PhB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4FcNovVkAE7PhB.jpg",
+					"url": "https://t.co/AG9lftPBEv",
+					"display_url": "pic.twitter.com/AG9lftPBEv",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1405505960274128896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1123,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1916,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 636,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1405505948060323800,
+					"id_str": "1405505948060323841",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/E4FcNovVkAE7PhB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4FcNovVkAE7PhB.jpg",
+					"url": "https://t.co/AG9lftPBEv",
+					"display_url": "pic.twitter.com/AG9lftPBEv",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1405505960274128896/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1123,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1916,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 636,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "💜Happiness! Red Velvet💜",
+			"url": "https://t.co/U7JtipmDEo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/U7JtipmDEo",
+							"expanded_url": "https://youtu.be/lNvBbh5jDcA",
+							"display_url": "youtu.be/lNvBbh5jDcA",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4249227,
+			"friends_count": 1,
+			"listed_count": 9075,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 75,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1955,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394081726993899521/HX_Qo0T4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1622019868",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4667,
+		"favorite_count": 13233,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오늘은 혼슬TIME! 🐻🧡\n\n#슬기 #SEULGI #슬기집 #슬기zip #레드벨벳 #RedVelvet https://t.co/AG9lftPBEv"
+	},
+	{
 		"created_at": "Thu Jun 17 11:57:09 +0000 2021",
 		"id": 1405480592079081500,
 		"id_str": "1405494724408971265",
