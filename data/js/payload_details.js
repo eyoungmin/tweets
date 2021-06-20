3c14bfa099f2c6f129c8ec12c4ca916a31918d62
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-20 12:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77659,
-	"created_at": "Sat Jun 19 16:02:04 +0000 2021",
+	"tweets": 77660,
+	"created_at": "Sun Jun 20 03:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sun Jun 20 03:01:50 +0000 2021",
+		"id": 1406447171898069000,
+		"id_str": "1406447171898068995",
+		"full_text": "#놀라운토요일 #도레미마켓 \n[#도레미모음집] 성장서사 완벽한 수수리걸 태연의 불티 모먼트 모음 🔥 ft. 키범이 라이브│#놀라운토요일 Amazing Saturday \nhttps://t.co/mHWvbG5VXq \n \n#김태연 #태연 #탱구 #TAEYEON",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "도레미마켓",
+					"indices": [
+						8,
+						14
+					]
+				},
+				{
+					"text": "도레미모음집",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						68,
+						75
+					]
+				},
+				{
+					"text": "김태연",
+					"indices": [
+						121,
+						125
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						126,
+						129
+					]
+				},
+				{
+					"text": "탱구",
+					"indices": [
+						130,
+						133
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						134,
+						142
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mHWvbG5VXq",
+					"expanded_url": "https://www.youtube.com/watch?v=1frPwciZlMo",
+					"display_url": "youtube.com/watch?v=1frPwc…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44966,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 81357,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#놀라운토요일 #도레미마켓 \n[#도레미모음집] 성장서사 완벽한 수수리걸 태연의 불티 모먼트 모음 🔥 ft. 키범이 라이브│#놀라운토요일 Amazing Saturday \nhttps://t.co/mHWvbG5VXq \n \n#김태연 #태연 #탱구 #TAEYEON"
+	},
+	{
 		"created_at": "Sat Jun 19 15:36:48 +0000 2021",
 		"id": 1406274776516149200,
 		"id_str": "1406274776516149256",
