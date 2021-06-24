2d2c56c05ed1e2f781dfa9c1c3f234f40fb0819b
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-24 14:02:10 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,10 +1,204 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 24 04:23:10 +0000 2021",
+		"id": 1407917188703469600,
+		"id_str": "1407917188703469568",
+		"full_text": "#SM엔터테인먼트 #SMTOWN #News \nSM엔터테인먼트, 메타버스 연구 위해 KAIST와 MOU 체결 \nhttps://t.co/AuLlRlN1I0 https://t.co/rJQVx8puue",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SM엔터테인먼트",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						10,
+						17
+					]
+				},
+				{
+					"text": "News",
+					"indices": [
+						18,
+						23
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AuLlRlN1I0",
+					"expanded_url": "https://www.smtown.com/news/detail/272",
+					"display_url": "smtown.com/news/detail/272",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1407917187780825000,
+					"id_str": "1407917187780825095",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/E4ntOQrX0AcCwdF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4ntOQrX0AcCwdF.jpg",
+					"url": "https://t.co/rJQVx8puue",
+					"display_url": "pic.twitter.com/rJQVx8puue",
+					"expanded_url": "https://twitter.com/fromminn/status/1407917188703469568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407917187780825000,
+					"id_str": "1407917187780825095",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/E4ntOQrX0AcCwdF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4ntOQrX0AcCwdF.jpg",
+					"url": "https://t.co/rJQVx8puue",
+					"display_url": "pic.twitter.com/rJQVx8puue",
+					"expanded_url": "https://twitter.com/fromminn/status/1407917188703469568/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 346,
+							"h": 225,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44967,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 81491,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#SM엔터테인먼트 #SMTOWN #News \nSM엔터테인먼트, 메타버스 연구 위해 KAIST와 MOU 체결 \nhttps://t.co/AuLlRlN1I0 https://t.co/rJQVx8puue"
+	},
+	{
 		"created_at": "Thu Jun 24 03:45:37 +0000 2021",
 		"id": 1407896266210742300,
 		"id_str": "1407907738773069828",
-		"full_text": "#Orbit! Only a few days left to see @loonatheworld 🌓👯‍♀️ LIVE ONLINE for their 2 night-event as OT12 again!\n\n6.27 🌠이달의 소녀 Premier Greeting [D&amp;D]\n6.28 🌠이달의 소녀 ON WAVE [LOOΠΔTHEWORLD : &amp;]\n\nGrab your tix🎟️https://t.co/mQVDscKVPw\n\n#LOONA #이달의소녀 #PREMIERGREETING #AND https://t.co/nAtulNBOW3",
+		"full_text": "#Orbit! Only a few days left to see @loonatheworld 🌓👯♀️ LIVE ONLINE for their 2 night-event as OT12 again!\n\n6.27 🌠이달의 소녀 Premier Greeting [D&amp;D]\n6.28 🌠이달의 소녀 ON WAVE [LOOΠΔTHEWORLD : &amp;]\n\nGrab your tix🎟️https://t.co/mQVDscKVPw\n\n#LOONA #이달의소녀 #PREMIERGREETING #AND https://t.co/nAtulNBOW3",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -266,7 +460,7 @@ Grailbird.data.tweets_2021_06 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "#Orbit! Only a few days left to see @loonatheworld 🌓👯‍♀️ LIVE ONLINE for their 2 night-event as OT12 again!\n\n6.27 🌠이달의 소녀 Premier Greeting [D&amp;D]\n6.28 🌠이달의 소녀 ON WAVE [LOOΠΔTHEWORLD : &amp;]\n\nGrab your tix🎟️https://t.co/mQVDscKVPw\n\n#LOONA #이달의소녀 #PREMIERGREETING #AND https://t.co/nAtulNBOW3"
+		"text": "#Orbit! Only a few days left to see @loonatheworld 🌓👯♀️ LIVE ONLINE for their 2 night-event as OT12 again!\n\n6.27 🌠이달의 소녀 Premier Greeting [D&amp;D]\n6.28 🌠이달의 소녀 ON WAVE [LOOΠΔTHEWORLD : &amp;]\n\nGrab your tix🎟️https://t.co/mQVDscKVPw\n\n#LOONA #이달의소녀 #PREMIERGREETING #AND https://t.co/nAtulNBOW3"
 	},
 	{
 		"created_at": "Thu Jun 24 02:13:16 +0000 2021",
