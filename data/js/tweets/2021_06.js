16abe88cb332970d5deba926d087f5f5e9c23863
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-24 11:02:07 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 24 01:16:10 +0000 2021",
+		"id": 1407866265415217200,
+		"id_str": "1407870127803146241",
+		"full_text": "#BLACKPINK 5th ANNIVERSARY [4+1] THE MOVIE MAIN POSTER\n\nMore info\n@ https://t.co/W1vLIdbfgI\n@ https://t.co/bquaDSMDr0\n\n#블랙핑크 #THE_MOVIE #BLACKPINKTHEMOVIE #MAINPOSTER #20210804 #COMINGSOON #5thANNIVERSARY #4PLUS1_PROJECT #BLACKPINKINYOURCINEMA  #SCREENX #4DX #CJ4DPLEX #YG https://t.co/RYUQm2EEgW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						119,
+						124
+					]
+				},
+				{
+					"text": "THE_MOVIE",
+					"indices": [
+						125,
+						135
+					]
+				},
+				{
+					"text": "BLACKPINKTHEMOVIE",
+					"indices": [
+						136,
+						154
+					]
+				},
+				{
+					"text": "MAINPOSTER",
+					"indices": [
+						155,
+						166
+					]
+				},
+				{
+					"text": "COMINGSOON",
+					"indices": [
+						177,
+						188
+					]
+				},
+				{
+					"text": "5thANNIVERSARY",
+					"indices": [
+						189,
+						204
+					]
+				},
+				{
+					"text": "4PLUS1_PROJECT",
+					"indices": [
+						205,
+						220
+					]
+				},
+				{
+					"text": "BLACKPINKINYOURCINEMA",
+					"indices": [
+						221,
+						243
+					]
+				},
+				{
+					"text": "SCREENX",
+					"indices": [
+						245,
+						253
+					]
+				},
+				{
+					"text": "4DX",
+					"indices": [
+						254,
+						258
+					]
+				},
+				{
+					"text": "CJ4DPLEX",
+					"indices": [
+						259,
+						268
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						269,
+						272
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W1vLIdbfgI",
+					"expanded_url": "http://5th.blackpinkofficial.com",
+					"display_url": "5th.blackpinkofficial.com",
+					"indices": [
+						68,
+						91
+					]
+				},
+				{
+					"url": "https://t.co/bquaDSMDr0",
+					"expanded_url": "http://blackpinkthemovie.com",
+					"display_url": "blackpinkthemovie.com",
+					"indices": [
+						94,
+						117
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1407866157898428400,
+					"id_str": "1407866157898428421",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/E4m-z7jUcAUVjaq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4m-z7jUcAUVjaq.jpg",
+					"url": "https://t.co/RYUQm2EEgW",
+					"display_url": "pic.twitter.com/RYUQm2EEgW",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1407866265415217156/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407866157898428400,
+					"id_str": "1407866157898428421",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/E4m-z7jUcAUVjaq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4m-z7jUcAUVjaq.jpg",
+					"url": "https://t.co/RYUQm2EEgW",
+					"display_url": "pic.twitter.com/RYUQm2EEgW",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1407866265415217156/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/aWhwrQsIZl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aWhwrQsIZl",
+							"expanded_url": "http://5th.blackpinkofficial.com",
+							"display_url": "5th.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4170573,
+			"friends_count": 0,
+			"listed_count": 5831,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 325,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1623718900",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19126,
+		"favorite_count": 46920,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK 5th ANNIVERSARY [4+1] THE MOVIE MAIN POSTER\n\nMore info\n@ https://t.co/W1vLIdbfgI\n@ https://t.co/bquaDSMDr0\n\n#블랙핑크 #THE_MOVIE #BLACKPINKTHEMOVIE #MAINPOSTER #20210804 #COMINGSOON #5thANNIVERSARY #4PLUS1_PROJECT #BLACKPINKINYOURCINEMA  #SCREENX #4DX #CJ4DPLEX #YG https://t.co/RYUQm2EEgW"
+	},
+	{
 		"created_at": "Wed Jun 23 23:16:01 +0000 2021",
 		"id": 1407835675555074000,
 		"id_str": "1407839890864283651",
