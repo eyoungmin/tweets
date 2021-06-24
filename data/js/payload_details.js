3b4534c1c60e8d15a56aac974f2d7c8bce92cbe9
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-24 12:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77784,
-	"created_at": "Thu Jun 24 02:02:06 +0000 2021",
+	"tweets": 77785,
+	"created_at": "Thu Jun 24 03:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Thu Jun 24 02:13:16 +0000 2021",
+		"id": 1407881179131375600,
+		"id_str": "1407884501045243907",
+		"full_text": "[유라]\n튠업 스테이지 7월 공연\n'Oh! Summer'에 유라가 출연합니다!\n멜론티켓에서 예매하실 수 있으며\n티켓오픈은 6월 25일 14시에 되오니 많은 관심 부탁드립니다!\n\n#유라 #공연 #music #음악 https://t.co/6zdMcY3WTc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유라",
+					"indices": [
+						99,
+						102
+					]
+				},
+				{
+					"text": "공연",
+					"indices": [
+						103,
+						106
+					]
+				},
+				{
+					"text": "music",
+					"indices": [
+						107,
+						113
+					]
+				},
+				{
+					"text": "음악",
+					"indices": [
+						114,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1407878948092932000,
+					"id_str": "1407878948092932097",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/E4nKcavUcAEKDwE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4nKcavUcAEKDwE.jpg",
+					"url": "https://t.co/6zdMcY3WTc",
+					"display_url": "pic.twitter.com/6zdMcY3WTc",
+					"expanded_url": "https://twitter.com/munhwainkr/status/1407881179131375623/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1449,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1407878948092932000,
+					"id_str": "1407878948092932097",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/E4nKcavUcAEKDwE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4nKcavUcAEKDwE.jpg",
+					"url": "https://t.co/6zdMcY3WTc",
+					"display_url": "pic.twitter.com/6zdMcY3WTc",
+					"expanded_url": "https://twitter.com/munhwainkr/status/1407881179131375623/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1449,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1407878948294258700,
+					"id_str": "1407878948294258693",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/E4nKcbfUcAUuv_0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4nKcbfUcAUuv_0.jpg",
+					"url": "https://t.co/6zdMcY3WTc",
+					"display_url": "pic.twitter.com/6zdMcY3WTc",
+					"expanded_url": "https://twitter.com/munhwainkr/status/1407881179131375623/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 186,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 109,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 62,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 743014855653752800,
+			"id_str": "743014855653752833",
+			"name": "문화인",
+			"screen_name": "munhwainkr",
+			"location": "",
+			"description": "🎧뮤직레이블 문화인의 공식트위터입니다. 윤현상/유라/로지/김우주/나히/에버루아/아일",
+			"url": "https://t.co/EWOG41vu9R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/EWOG41vu9R",
+							"expanded_url": "http://www.munhwain.kr",
+							"display_url": "munhwain.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 767,
+			"friends_count": 8,
+			"listed_count": 16,
+			"created_at": "Wed Jun 15 09:38:40 +0000 2016",
+			"favourites_count": 59,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 911,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/743017072494448640/pk13sRIF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/743017072494448640/pk13sRIF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/743014855653752833/1513566567",
+			"profile_link_color": "199B2B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[유라]\n튠업 스테이지 7월 공연\n'Oh! Summer'에 유라가 출연합니다!\n멜론티켓에서 예매하실 수 있으며\n티켓오픈은 6월 25일 14시에 되오니 많은 관심 부탁드립니다!\n\n#유라 #공연 #music #음악 https://t.co/6zdMcY3WTc"
+	},
+	{
 		"created_at": "Thu Jun 24 01:16:10 +0000 2021",
 		"id": 1407866265415217200,
 		"id_str": "1407870127803146241",
