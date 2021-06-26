ea935ed47afa244e543e53953081ae0216c7239f
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-26 19:02:11 +0900

    Added 1 tweets

--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sat Jun 26 09:58:21 +0000 2021",
+		"id": 1408726315025551400,
+		"id_str": "1408726315025551363",
+		"full_text": "#이달의소녀 #LOONA \n[진솔] 오빛둥들! 진솔입니다🙋🏻 \nhttps://t.co/uuZ310ClAW \n \n#다음팬카페 #공식팬카페 #프롬글 https://t.co/QQR92OxwkR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						7,
+						13
+					]
+				},
+				{
+					"text": "다음팬카페",
+					"indices": [
+						62,
+						68
+					]
+				},
+				{
+					"text": "공식팬카페",
+					"indices": [
+						69,
+						75
+					]
+				},
+				{
+					"text": "프롬글",
+					"indices": [
+						76,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uuZ310ClAW",
+					"expanded_url": "http://cafe.daum.net/loonatheworld/F5df/129",
+					"display_url": "cafe.daum.net/loonatheworld/…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1408726313783943200,
+					"id_str": "1408726313783943172",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/E4zNHl7WYAQAUV6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4zNHl7WYAQAUV6.jpg",
+					"url": "https://t.co/QQR92OxwkR",
+					"display_url": "pic.twitter.com/QQR92OxwkR",
+					"expanded_url": "https://twitter.com/fromminn/status/1408726315025551363/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1408726313783943200,
+					"id_str": "1408726313783943172",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/E4zNHl7WYAQAUV6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4zNHl7WYAQAUV6.jpg",
+					"url": "https://t.co/QQR92OxwkR",
+					"display_url": "pic.twitter.com/QQR92OxwkR",
+					"expanded_url": "https://twitter.com/fromminn/status/1408726315025551363/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 318,
+			"friends_count": 1053,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44966,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 81564,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 #LOONA \n[진솔] 오빛둥들! 진솔입니다🙋🏻 \nhttps://t.co/uuZ310ClAW \n \n#다음팬카페 #공식팬카페 #프롬글 https://t.co/QQR92OxwkR"
+	},
+	{
 		"created_at": "Sat Jun 26 08:45:36 +0000 2021",
 		"id": 1408703562826223600,
 		"id_str": "1408708010273345536",
