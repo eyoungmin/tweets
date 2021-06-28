67d543db8f9b7f3db739386220e16fc83dc47e56
Youngmin Lee <iameyoungmin@gmail.com>
2021-06-29 00:20:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 77951,
-	"created_at": "Mon Jun 28 15:02:10 +0000 2021",
+	"tweets": 77952,
+	"created_at": "Mon Jun 28 15:20:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Mon Jun 28 15:02:11 +0000 2021",
+		"id": 1409515025258188800,
+		"id_str": "1409527553283354629",
+		"full_text": "excited to share my second little collection of music inspired by the zodiac signs, ‘’Water: Cancer’s Songs’’ out now 💛all songs have been chosen by me from my previous three albums &amp; Young Heart 💫 \nhttps://t.co/F8zJYWe1xY⁠\n\n📷: @affairintokyo https://t.co/l9Pe2LGL88",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			246
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "affairintokyo",
+					"name": "Lotta",
+					"id": 351971783,
+					"id_str": "351971783",
+					"indices": [
+						232,
+						246
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/F8zJYWe1xY",
+					"expanded_url": "http://birdy.lnk.to/Water-Cancers-Songs",
+					"display_url": "birdy.lnk.to/Water-Cancers-…",
+					"indices": [
+						203,
+						226
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1409514746429247500,
+					"id_str": "1409514746429247493",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/media/E4-aMaaXEAUgRYv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4-aMaaXEAUgRYv.jpg",
+					"url": "https://t.co/l9Pe2LGL88",
+					"display_url": "pic.twitter.com/l9Pe2LGL88",
+					"expanded_url": "https://twitter.com/birdy/status/1409515025258188801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1409514746429247500,
+					"id_str": "1409514746429247493",
+					"indices": [
+						247,
+						270
+					],
+					"media_url": "http://pbs.twimg.com/media/E4-aMaaXEAUgRYv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E4-aMaaXEAUgRYv.jpg",
+					"url": "https://t.co/l9Pe2LGL88",
+					"display_url": "pic.twitter.com/l9Pe2LGL88",
+					"expanded_url": "https://twitter.com/birdy/status/1409515025258188801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270901752,
+			"id_str": "270901752",
+			"name": "Birdy",
+			"screen_name": "birdy",
+			"location": "",
+			"description": "Young Heart - my new album, OUT NOW 💛 \nhttps://t.co/JJysUdcZGQ",
+			"url": "https://t.co/AOWSyI9JYu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AOWSyI9JYu",
+							"expanded_url": "http://officialbirdy.com",
+							"display_url": "officialbirdy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/JJysUdcZGQ",
+							"expanded_url": "http://birdy.lnk.to/Young-Heart",
+							"display_url": "birdy.lnk.to/Young-Heart",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 316305,
+			"friends_count": 181,
+			"listed_count": 1191,
+			"created_at": "Wed Mar 23 13:25:01 +0000 2011",
+			"favourites_count": 848,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2311,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270901752/1619760432",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22,
+		"favorite_count": 144,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "excited to share my second little collection of music inspired by the zodiac signs, ‘’Water: Cancer’s Songs’’ out now 💛all songs have been chosen by me from my previous three albums &amp; Young Heart 💫 \nhttps://t.co/F8zJYWe1xY⁠\n\n📷: @affairintokyo https://t.co/l9Pe2LGL88"
+	},
+	{
 		"created_at": "Mon Jun 28 14:43:49 +0000 2021",
 		"id": 1409522934553710600,
 		"id_str": "1409522934553710598",
