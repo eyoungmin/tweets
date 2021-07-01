86985b63d533555272d7dc0e9e07648b11b8d9bb
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-01 20:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78075,
-	"created_at": "Thu Jul 01 10:02:00 +0000 2021",
+	"tweets": 78076,
+	"created_at": "Thu Jul 01 11:02:00 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 01 11:01:02 +0000 2021",
+		"id": 1410553850868629500,
+		"id_str": "1410554032586772483",
+		"full_text": "잠시 후 오후 10시(KST) '이달의 소녀(LOOПΔ) OFFICIAL LIGHT STICK' 1st anniversary 기념 MD가 출시됩니다🌙 자세한 공지사항은 공식팬카페를 통해 확인해 주세요!\n\n#이달의소녀 #LOONA\n\n🏛 https://t.co/PhqPM921E2 https://t.co/2ORKhs8c6v",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						114,
+						120
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						121,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PhqPM921E2",
+					"expanded_url": "https://cafe.daum.net/loonatheworld",
+					"display_url": "cafe.daum.net/loonatheworld",
+					"indices": [
+						131,
+						154
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1410553651869884400,
+					"id_str": "1410553651869884420",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/E5NLEqxVoAQq7L_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5NLEqxVoAQq7L_.jpg",
+					"url": "https://t.co/2ORKhs8c6v",
+					"display_url": "pic.twitter.com/2ORKhs8c6v",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1410553850868629506/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1410553651869884400,
+					"id_str": "1410553651869884420",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/E5NLEqxVoAQq7L_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5NLEqxVoAQq7L_.jpg",
+					"url": "https://t.co/2ORKhs8c6v",
+					"display_url": "pic.twitter.com/2ORKhs8c6v",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1410553850868629506/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 757331,
+			"friends_count": 1,
+			"listed_count": 7601,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3025,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 718,
+		"favorite_count": 2378,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "잠시 후 오후 10시(KST) '이달의 소녀(LOOПΔ) OFFICIAL LIGHT STICK' 1st anniversary 기념 MD가 출시됩니다🌙 자세한 공지사항은 공식팬카페를 통해 확인해 주세요!\n\n#이달의소녀 #LOONA\n\n🏛 https://t.co/PhqPM921E2 https://t.co/2ORKhs8c6v"
+	},
+	{
 		"created_at": "Thu Jul 01 09:52:48 +0000 2021",
 		"id": 1410469232714731500,
 		"id_str": "1410536857746784262",
