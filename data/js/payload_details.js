0e75751c7ded281ee4632d54b7c68df201d9c5fd
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-02 04:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78092,
-	"created_at": "Thu Jul 01 18:02:01 +0000 2021",
+	"tweets": 78093,
+	"created_at": "Thu Jul 01 19:02:00 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 01 18:02:59 +0000 2021",
+		"id": 1410646418155577300,
+		"id_str": "1410660219613454336",
+		"full_text": "we could be weightless\nlike birds in the night\nwherever we go\nset the world on fire 🧡\nhttps://t.co/W4phOK3qea https://t.co/Wg9athKOQe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/W4phOK3qea",
+					"expanded_url": "https://youtu.be/BDvv9iE4L_8",
+					"display_url": "youtu.be/BDvv9iE4L_8",
+					"indices": [
+						86,
+						109
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1410645737856196600,
+					"id_str": "1410645737856196618",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1410645737856196618/img/y7tNCPDEr_4qccl2.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1410645737856196618/img/y7tNCPDEr_4qccl2.jpg",
+					"url": "https://t.co/Wg9athKOQe",
+					"display_url": "pic.twitter.com/Wg9athKOQe",
+					"expanded_url": "https://twitter.com/birdy/status/1410646418155577346/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1410645737856196600,
+					"id_str": "1410645737856196618",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1410645737856196618/img/y7tNCPDEr_4qccl2.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1410645737856196618/img/y7tNCPDEr_4qccl2.jpg",
+					"url": "https://t.co/Wg9athKOQe",
+					"display_url": "pic.twitter.com/Wg9athKOQe",
+					"expanded_url": "https://twitter.com/birdy/status/1410646418155577346/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 29162,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1410645737856196618/vid/320x320/mV3OXAUoXUi4gwTz.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1410645737856196618/pl/PrA73yV2N8HKjVgf.m3u8?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1410645737856196618/vid/540x540/C5XMav9YJuzmVRji.mp4?tag=14"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1410645737856196618/vid/720x720/C7WtIcnlx0ZAuLAS.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "Birdy - The Otherside",
+						"description": "",
+						"call_to_actions": {
+							"watch_now": {
+								"url": "https://youtu.be/BDvv9iE4L_8"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270901752,
+			"id_str": "270901752",
+			"name": "Birdy",
+			"screen_name": "birdy",
+			"location": "",
+			"description": "Young Heart - my new album, OUT NOW 💛 \nhttps://t.co/JJysUdcZGQ",
+			"url": "https://t.co/AOWSyI9JYu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AOWSyI9JYu",
+							"expanded_url": "http://officialbirdy.com",
+							"display_url": "officialbirdy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/JJysUdcZGQ",
+							"expanded_url": "http://birdy.lnk.to/Young-Heart",
+							"display_url": "birdy.lnk.to/Young-Heart",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 316262,
+			"friends_count": 181,
+			"listed_count": 1192,
+			"created_at": "Wed Mar 23 13:25:01 +0000 2011",
+			"favourites_count": 848,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2315,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270901752/1619760432",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 61,
+		"favorite_count": 254,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "we could be weightless\nlike birds in the night\nwherever we go\nset the world on fire 🧡\nhttps://t.co/W4phOK3qea https://t.co/Wg9athKOQe"
+	},
+	{
 		"created_at": "Thu Jul 01 17:52:47 +0000 2021",
 		"id": 1410646623965913000,
 		"id_str": "1410657653425061888",
