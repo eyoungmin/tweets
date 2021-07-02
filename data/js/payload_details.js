93e8bfbeec7959a5eaaeb04ad5b2940e511aba65
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-02 22:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78119,
-	"created_at": "Fri Jul 02 11:02:01 +0000 2021",
+	"tweets": 78120,
+	"created_at": "Fri Jul 02 13:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Fri Jul 02 12:03:11 +0000 2021",
+		"id": 1410931735622078500,
+		"id_str": "1410932058759733257",
+		"full_text": "WOODZ (조승연) ‘해가 될까’ Released!!\n❤️ https://t.co/L3MA8ROWSB https://t.co/wLyD2vwVxj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			57
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/L3MA8ROWSB",
+					"expanded_url": "https://www.instagram.com/p/CQ0vNxeHPts/?utm_medium=copy_link",
+					"display_url": "instagram.com/p/CQ0vNxeHPts/…",
+					"indices": [
+						34,
+						57
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1410931729502654500,
+					"id_str": "1410931729502654464",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/E5Si7rtVUAAdc69.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5Si7rtVUAAdc69.jpg",
+					"url": "https://t.co/wLyD2vwVxj",
+					"display_url": "pic.twitter.com/wLyD2vwVxj",
+					"expanded_url": "https://twitter.com/monotreemusic/status/1410931735622078470/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 666,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 811,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 811,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1410931729502654500,
+					"id_str": "1410931729502654464",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/media/E5Si7rtVUAAdc69.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5Si7rtVUAAdc69.jpg",
+					"url": "https://t.co/wLyD2vwVxj",
+					"display_url": "pic.twitter.com/wLyD2vwVxj",
+					"expanded_url": "https://twitter.com/monotreemusic/status/1410931735622078470/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 666,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 811,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 811,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 135173608,
+			"id_str": "135173608",
+			"name": "MonoTree_official",
+			"screen_name": "monotreemusic",
+			"location": "Seoul, Korea",
+			"description": "Global Music Production and Publishing Company. (monotreemusic@gmail.com)",
+			"url": "https://t.co/75NNtZGos2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/75NNtZGos2",
+							"expanded_url": "https://www.monotree.shop",
+							"display_url": "monotree.shop",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7114,
+			"friends_count": 0,
+			"listed_count": 53,
+			"created_at": "Tue Apr 20 14:55:42 +0000 2010",
+			"favourites_count": 28,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 303,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BF1238",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme20/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme20/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/777117583732375552/Mi0rVSK5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/777117583732375552/Mi0rVSK5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/135173608/1426630031",
+			"profile_link_color": "BF1238",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "WOODZ (조승연) ‘해가 될까’ Released!!\n❤️ https://t.co/L3MA8ROWSB https://t.co/wLyD2vwVxj"
+	},
+	{
 		"created_at": "Fri Jul 02 10:52:48 +0000 2021",
 		"id": 1410901064254038000,
 		"id_str": "1410914345228324865",
