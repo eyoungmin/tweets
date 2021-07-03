968c413f4f109530d036783dd92ec4c31a008198
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-03 18:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78131,
-	"created_at": "Sat Jul 03 08:02:01 +0000 2021",
+	"tweets": 78132,
+	"created_at": "Sat Jul 03 09:02:03 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,137 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sat Jul 03 08:45:43 +0000 2021",
+		"id": 1411242170778935300,
+		"id_str": "1411244751949598720",
+		"full_text": "우린 계속 달릴 거야 don't touch it🔥 'STARPASS' 앱에서 더쇼 사전 투표가 진행되고 있어요, 오빛💫 #이달의소녀 'PTT (Paint The Town)'에 꼭 투표해 주세요!\n\n🏛 https://t.co/D7Xie0q7Wp (iOS)\n🏛 https://t.co/pSqWBvvRG7 (Android)\n\n#LOONA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						67,
+						73
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						179,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/D7Xie0q7Wp",
+					"expanded_url": "https://apple.co/3wcD0RC",
+					"display_url": "apple.co/3wcD0RC",
+					"indices": [
+						112,
+						135
+					]
+				},
+				{
+					"url": "https://t.co/pSqWBvvRG7",
+					"expanded_url": "https://bit.ly/3AlZy5v",
+					"display_url": "bit.ly/3AlZy5v",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 759459,
+			"friends_count": 1,
+			"listed_count": 7624,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3039,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1300,
+		"favorite_count": 3909,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "우린 계속 달릴 거야 don't touch it🔥 'STARPASS' 앱에서 더쇼 사전 투표가 진행되고 있어요, 오빛💫 #이달의소녀 'PTT (Paint The Town)'에 꼭 투표해 주세요!\n\n🏛 https://t.co/D7Xie0q7Wp (iOS)\n🏛 https://t.co/pSqWBvvRG7 (Android)\n\n#LOONA"
+	},
+	{
 		"created_at": "Sat Jul 03 07:45:36 +0000 2021",
 		"id": 1411227203069563000,
 		"id_str": "1411229626442076160",
