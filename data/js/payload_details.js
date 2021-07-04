c4463d7bd11a13f5b6f80a6c4d6be85872e887a1
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-04 16:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78141,
-	"created_at": "Sun Jul 04 05:02:00 +0000 2021",
+	"tweets": 78142,
+	"created_at": "Sun Jul 04 07:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 04 06:45:36 +0000 2021",
+		"id": 1411572985882894300,
+		"id_str": "1411576914549133312",
+		"full_text": "잠시 후 오후 3시 50분(KST) 방송되는 '인기가요'에서 #이달의소녀 의 'PTT (Paint The Town)' 무대를 확인하세요🦢 이번 주도 힘낼 수 있도록 응원해 줘서 고마워요, 오빛💫\n\n#이달의소녀_PTT #LOONA #LOONA_PTT https://t.co/19L9kiUVsS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						34,
+						40
+					]
+				},
+				{
+					"text": "이달의소녀_PTT",
+					"indices": [
+						110,
+						120
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						121,
+						127
+					]
+				},
+				{
+					"text": "LOONA_PTT",
+					"indices": [
+						128,
+						138
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1411341580758225000,
+					"id_str": "1411341580758224897",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/E5YXsKnVEAE74DP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5YXsKnVEAE74DP.jpg",
+					"url": "https://t.co/19L9kiUVsS",
+					"display_url": "pic.twitter.com/19L9kiUVsS",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1411572985882894344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1411341580758225000,
+					"id_str": "1411341580758224897",
+					"indices": [
+						139,
+						162
+					],
+					"media_url": "http://pbs.twimg.com/media/E5YXsKnVEAE74DP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5YXsKnVEAE74DP.jpg",
+					"url": "https://t.co/19L9kiUVsS",
+					"display_url": "pic.twitter.com/19L9kiUVsS",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1411572985882894344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 760350,
+			"friends_count": 1,
+			"listed_count": 7619,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3041,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4197,
+		"favorite_count": 11680,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "잠시 후 오후 3시 50분(KST) 방송되는 '인기가요'에서 #이달의소녀 의 'PTT (Paint The Town)' 무대를 확인하세요🦢 이번 주도 힘낼 수 있도록 응원해 줘서 고마워요, 오빛💫\n\n#이달의소녀_PTT #LOONA #LOONA_PTT https://t.co/19L9kiUVsS"
+	},
+	{
 		"created_at": "Sun Jul 04 04:45:36 +0000 2021",
 		"id": 1411535237629104000,
 		"id_str": "1411546715325349888",
@@ -187,7 +391,7 @@ Grailbird.data.tweets_2021_07 =
 			"name": "dgg",
 			"screen_name": "dingo_dgg",
 			"location": "대한민국",
-			"description": "dingo global official twitter! hey DGG🙋🏻‍♀️안녕 내 이름은 디지지❤️💚",
+			"description": "dingo global official twitter! hey DGG🙋🏻♀️안녕 내 이름은 디지지❤️💚",
 			"url": "https://t.co/632yww1729",
 			"entities": {
 				"url": {
