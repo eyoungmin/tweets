da6d005d49836f500edf836bdd9f5b61b3e839d5
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-07 00:19:56 +0900

    Added 1 tweets

--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,136 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 06 15:03:21 +0000 2021",
+		"id": 1412426951101407200,
+		"id_str": "1412426951101407238",
+		"full_text": "#빅히트레이블 #BigHitLabels #PLAYLIST \nBTS (방탄소년단) 'Permission to Dance' Official Teaser \nhttps://t.co/Yapen5VFKH \n \n#방탄소년단 #BTS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "빅히트레이블",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "BigHitLabels",
+					"indices": [
+						8,
+						21
+					]
+				},
+				{
+					"text": "PLAYLIST",
+					"indices": [
+						22,
+						31
+					]
+				},
+				{
+					"text": "방탄소년단",
+					"indices": [
+						111,
+						117
+					]
+				},
+				{
+					"text": "BTS",
+					"indices": [
+						118,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Yapen5VFKH",
+					"expanded_url": "https://www.youtube.com/watch?v=iLycuRm1V0Q",
+					"display_url": "youtube.com/watch?v=iLycuR…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 317,
+			"friends_count": 1053,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44963,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 82111,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#빅히트레이블 #BigHitLabels #PLAYLIST \nBTS (방탄소년단) 'Permission to Dance' Official Teaser \nhttps://t.co/Yapen5VFKH \n \n#방탄소년단 #BTS"
+	},
+	{
 		"created_at": "Tue Jul 06 14:53:14 +0000 2021",
 		"id": 1412422524571467800,
 		"id_str": "1412424406228406282",
