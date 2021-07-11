0d8e0e8942b790dc6f5d9ecea50fdca2e6221c85
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-11 12:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78459,
-	"created_at": "Sat Jul 10 23:02:04 +0000 2021",
+	"tweets": 78460,
+	"created_at": "Sun Jul 11 03:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 11 02:52:47 +0000 2021",
+		"id": 1414054336016093200,
+		"id_str": "1414055039681896448",
+		"full_text": "오빛💫 오늘도 #이달의소녀 가 출연하는 TBS ‘팩트인스타’ 본방사수해 주실 거죠?❤️ 잠시 후 오후 12시(KST)에 만나요!\n\n#LOONA https://t.co/hiij9NFw8z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			79
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						8,
+						14
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						73,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1413925193286766600,
+					"id_str": "1413925193286766592",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/E59FeNTVUAAhTmW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E59FeNTVUAAhTmW.jpg",
+					"url": "https://t.co/hiij9NFw8z",
+					"display_url": "pic.twitter.com/hiij9NFw8z",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1414054336016093184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1190,
+							"h": 894,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1190,
+							"h": 894,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1413925193286766600,
+					"id_str": "1413925193286766592",
+					"indices": [
+						80,
+						103
+					],
+					"media_url": "http://pbs.twimg.com/media/E59FeNTVUAAhTmW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E59FeNTVUAAhTmW.jpg",
+					"url": "https://t.co/hiij9NFw8z",
+					"display_url": "pic.twitter.com/hiij9NFw8z",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1414054336016093184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1190,
+							"h": 894,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1190,
+							"h": 894,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 768112,
+			"friends_count": 1,
+			"listed_count": 7664,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3078,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3453,
+		"favorite_count": 9580,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛💫 오늘도 #이달의소녀 가 출연하는 TBS ‘팩트인스타’ 본방사수해 주실 거죠?❤️ 잠시 후 오후 12시(KST)에 만나요!\n\n#LOONA https://t.co/hiij9NFw8z"
+	},
+	{
 		"created_at": "Sat Jul 10 22:45:37 +0000 2021",
 		"id": 1413981357190041600,
 		"id_str": "1413992835213406210",
