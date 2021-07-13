4c9b2a1ec3e6265d84a3f84bfa502de2d8133f81
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-14 00:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78553,
-	"created_at": "Tue Jul 13 14:02:06 +0000 2021",
+	"tweets": 78554,
+	"created_at": "Tue Jul 13 15:02:08 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 13 14:15:26 +0000 2021",
+		"id": 1414951482781282300,
+		"id_str": "1414951605896630282",
+		"full_text": "더블유 8월호 커버 최초 공개! #블랙핑크 #리사 와 함께한 화보를 지금 바로 확인하세요! 🖤💝 #BLACKPINK #LISA \nhttps://t.co/dcSZCA66PV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						18,
+						23
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						24,
+						27
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"text": "LISA",
+					"indices": [
+						65,
+						70
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dcSZCA66PV",
+					"expanded_url": "https://www.wkorea.com/2021/07/13/w-korea-2021%EB%85%84-8%EC%9B%94%ED%98%B8-%EC%BB%A4%EB%B2%84/?ddw=91993&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2021/07/13/w-k…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5xzw0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5xzw0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 276114,
+			"friends_count": 0,
+			"listed_count": 814,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7552,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12592,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1623740686",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6076,
+		"favorite_count": 8578,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "더블유 8월호 커버 최초 공개! #블랙핑크 #리사 와 함께한 화보를 지금 바로 확인하세요! 🖤💝 #BLACKPINK #LISA \nhttps://t.co/dcSZCA66PV"
+	},
+	{
 		"created_at": "Tue Jul 13 13:03:58 +0000 2021",
 		"id": 1414930322962014200,
 		"id_str": "1414933622159646723",
