5c1bdb48c7ed4a113c0b65afe730ae54f651928d
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-14 16:03:28 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78563,
-	"created_at": "Wed Jul 14 06:03:02 +0000 2021",
+	"tweets": 78564,
+	"created_at": "Wed Jul 14 07:03:23 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Wed Jul 14 06:47:29 +0000 2021",
+		"id": 1415201267400548400,
+		"id_str": "1415201267400548353",
+		"full_text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크, 오는 8월 2일 위버스 합류 \nhttps://t.co/RISrLG5jLJ \n \n#블랙핑크 #BLACKPINK https://t.co/mXVAWADeJJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						68,
+						73
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						74,
+						84
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RISrLG5jLJ",
+					"expanded_url": "https://yg-life.com/archives/168796?lang=ko",
+					"display_url": "yg-life.com/archives/16879…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1415201266578432000,
+					"id_str": "1415201266578432003",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/E6PODcwXIAMFc_q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6PODcwXIAMFc_q.jpg",
+					"url": "https://t.co/mXVAWADeJJ",
+					"display_url": "pic.twitter.com/mXVAWADeJJ",
+					"expanded_url": "https://twitter.com/fromminn/status/1415201267400548353/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1415201266578432000,
+					"id_str": "1415201266578432003",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/E6PODcwXIAMFc_q.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E6PODcwXIAMFc_q.jpg",
+					"url": "https://t.co/mXVAWADeJJ",
+					"display_url": "pic.twitter.com/mXVAWADeJJ",
+					"expanded_url": "https://twitter.com/fromminn/status/1415201267400548353/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 374,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 317,
+			"friends_count": 1053,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 82439,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크, 오는 8월 2일 위버스 합류 \nhttps://t.co/RISrLG5jLJ \n \n#블랙핑크 #BLACKPINK https://t.co/mXVAWADeJJ"
+	},
+	{
 		"created_at": "Wed Jul 14 05:52:53 +0000 2021",
 		"id": 1415182976934051800,
 		"id_str": "1415187523651919873",
