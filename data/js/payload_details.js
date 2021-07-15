c5d03289d3bf824fbebb8e5bf952fb7f883982d1
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-16 05:02:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78612,
-	"created_at": "Thu Jul 15 17:02:08 +0000 2021",
+	"tweets": 78613,
+	"created_at": "Thu Jul 15 20:02:13 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 15 19:52:47 +0000 2021",
+		"id": 1415754990119968800,
+		"id_str": "1415761282561630208",
+		"full_text": "Some of the lyrics to Mirror explained 😉! Check out the time-synched lyric feature on @AppleMusic to learn all the words ☺️: https://t.co/qJCEviqyHf https://t.co/iYRdLQTmQO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AppleMusic",
+					"name": "Apple Music",
+					"id": 74580436,
+					"id_str": "74580436",
+					"indices": [
+						86,
+						97
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qJCEviqyHf",
+					"expanded_url": "https://sigrid.lnk.to/EssentialsTW",
+					"display_url": "sigrid.lnk.to/EssentialsTW",
+					"indices": [
+						125,
+						148
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1415754842383999000,
+					"id_str": "1415754842383998980",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1415754842383998980/pu/img/Myx-yUolwHgUf4LV.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1415754842383998980/pu/img/Myx-yUolwHgUf4LV.jpg",
+					"url": "https://t.co/iYRdLQTmQO",
+					"display_url": "pic.twitter.com/iYRdLQTmQO",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1415754990119968780/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1415754842383999000,
+					"id_str": "1415754842383998980",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1415754842383998980/pu/img/Myx-yUolwHgUf4LV.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1415754842383998980/pu/img/Myx-yUolwHgUf4LV.jpg",
+					"url": "https://t.co/iYRdLQTmQO",
+					"display_url": "pic.twitter.com/iYRdLQTmQO",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1415754990119968780/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 60160,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1415754842383998980/pu/vid/480x600/1SgyruCyAHOTgoxP.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1415754842383998980/pu/vid/720x900/xzqhJGBpJQSDuDLg.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1415754842383998980/pu/vid/320x400/kevxnmKPChhZfb41.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1415754842383998980/pu/pl/RtpOCprynItMPV0f.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 81847,
+			"friends_count": 264,
+			"listed_count": 303,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 5927,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4133,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394240555333337089/STtGqeaY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394240555333337089/STtGqeaY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1621409378",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 188,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Some of the lyrics to Mirror explained 😉! Check out the time-synched lyric feature on @AppleMusic to learn all the words ☺️: https://t.co/qJCEviqyHf https://t.co/iYRdLQTmQO"
+	},
+	{
 		"created_at": "Thu Jul 15 16:03:31 +0000 2021",
 		"id": 1415703584994431000,
 		"id_str": "1415703584994430976",
