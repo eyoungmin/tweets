3056956aa538e034d459a3475ed5c91999166fd7
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-20 04:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78687,
-	"created_at": "Mon Jul 19 18:02:12 +0000 2021",
+	"tweets": 78688,
+	"created_at": "Mon Jul 19 19:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,121 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 19 18:52:47 +0000 2021",
+		"id": 1417188567009345500,
+		"id_str": "1417195733950861318",
+		"full_text": "@mxgic42_ Anyone who appreciates JB’s guitar on TiT is my friend for life. Hope you guys are enjoying EL. Can’t tell you how happy it makes us that you like that album.  See you tomorrow 😉 PH #LunarWithColdplay",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			210
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LunarWithColdplay",
+					"indices": [
+						192,
+						210
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mxgic42_",
+					"name": "maritza👽📻|BTSPLAY SUPREMACY ✨",
+					"id": 1319029844542288000,
+					"id_str": "1319029844542287872",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1417184547704230000,
+		"in_reply_to_status_id_str": "1417184547704229890",
+		"in_reply_to_user_id": 1319029844542288000,
+		"in_reply_to_user_id_str": "1319029844542287872",
+		"in_reply_to_screen_name": "mxgic42_",
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Kaotica",
+			"description": "The official Twitter of the band Coldplay. New single, Higher Power, out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22999597,
+			"friends_count": 765,
+			"listed_count": 42733,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1309,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8588,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389264116502704131/arWiuiOR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389264116502704131/arWiuiOR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1620345079",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 409,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@mxgic42_ Anyone who appreciates JB’s guitar on TiT is my friend for life. Hope you guys are enjoying EL. Can’t tell you how happy it makes us that you like that album.  See you tomorrow 😉 PH #LunarWithColdplay"
+	},
+	{
 		"created_at": "Mon Jul 19 17:07:12 +0000 2021",
 		"id": 1417165246792409000,
 		"id_str": "1417169160744804376",
