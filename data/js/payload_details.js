d29995669f44876bb202c6cc39a95c44eaa09744
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-22 18:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78764,
-	"created_at": "Thu Jul 22 08:02:08 +0000 2021",
+	"tweets": 78765,
+	"created_at": "Thu Jul 22 09:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Thu Jul 22 08:45:37 +0000 2021",
+		"id": 1418118624636588000,
+		"id_str": "1418130096129511426",
+		"full_text": "[하이라이트] 그 누구도 이달소를 막을 순 없어 'PTT(Paint The Town) 컴백기념 이달의 소녀 노래모음♬\n\n상상만 해도 눈이 부셔 벌써 알잖니( ´͈ ॢꇴ `͈ॢ)･*♡\n밤하늘에 은하수보다 빛이 나는 이달소 노래모음♬\n📽https://t.co/WLkp9jQs4x\n\n#하이라이트 #LOONA #이달의소녀 @loonatheworld https://t.co/DdjMMXcCbe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "하이라이트",
+					"indices": [
+						155,
+						161
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						162,
+						168
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						169,
+						175
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						176,
+						190
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/WLkp9jQs4x",
+					"expanded_url": "https://youtu.be/mykCo1ZyGYE",
+					"display_url": "youtu.be/mykCo1ZyGYE",
+					"indices": [
+						130,
+						153
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1418075401339826200,
+					"id_str": "1418075401339826177",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/E64EEHGUcAEwCB1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E64EEHGUcAEwCB1.jpg",
+					"url": "https://t.co/DdjMMXcCbe",
+					"display_url": "pic.twitter.com/DdjMMXcCbe",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1418118624636588035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1418075401339826200,
+					"id_str": "1418075401339826177",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/E64EEHGUcAEwCB1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E64EEHGUcAEwCB1.jpg",
+					"url": "https://t.co/DdjMMXcCbe",
+					"display_url": "pic.twitter.com/DdjMMXcCbe",
+					"expanded_url": "https://twitter.com/ALLTHEKPOP/status/1418118624636588035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 744928926,
+			"id_str": "744928926",
+			"name": "올더케이팝",
+			"screen_name": "ALLTHEKPOP",
+			"location": "",
+			"description": "ALL about ID♡L, ALL THE K-POP",
+			"url": "https://t.co/0kbjbCEhaR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0kbjbCEhaR",
+							"expanded_url": "http://www.youtube.com/allthekpop",
+							"display_url": "youtube.com/allthekpop",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 82164,
+			"friends_count": 336,
+			"listed_count": 312,
+			"created_at": "Wed Aug 08 10:28:32 +0000 2012",
+			"favourites_count": 7909,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7304,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1035092488409632769/sDzylGOZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/744928926/1626653435",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[하이라이트] 그 누구도 이달소를 막을 순 없어 'PTT(Paint The Town) 컴백기념 이달의 소녀 노래모음♬\n\n상상만 해도 눈이 부셔 벌써 알잖니( ´͈ ॢꇴ `͈ॢ)･*♡\n밤하늘에 은하수보다 빛이 나는 이달소 노래모음♬\n📽https://t.co/WLkp9jQs4x\n\n#하이라이트 #LOONA #이달의소녀 @loonatheworld https://t.co/DdjMMXcCbe"
+	},
+	{
 		"created_at": "Thu Jul 22 07:17:40 +0000 2021",
 		"id": 1418107020998189000,
 		"id_str": "1418107962179014656",
