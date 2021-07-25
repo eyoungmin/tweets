a7567972b855834973790ae4f98180fd10c8dd87
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-26 03:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78841,
-	"created_at": "Sun Jul 25 16:02:12 +0000 2021",
+	"tweets": 78842,
+	"created_at": "Sun Jul 25 18:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 25 17:52:47 +0000 2021",
+		"id": 1419326652551663600,
+		"id_str": "1419354961989849090",
+		"full_text": "\"And up there in the heavens\nGalileo saw reflections of us too\nPluribus unum, unus mundus\nAll the satellites imbue.\" \n\n- @coldplay, #Coloratura\n\n📷 Our #WeekInImages, 19-23 July 2021 👉 https://t.co/lpgaAxeavW https://t.co/e5Bf4GrjMd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Coloratura",
+					"indices": [
+						132,
+						143
+					]
+				},
+				{
+					"text": "WeekInImages",
+					"indices": [
+						151,
+						164
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "coldplay",
+					"name": "Coldplay",
+					"id": 18863815,
+					"id_str": "18863815",
+					"indices": [
+						121,
+						130
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lpgaAxeavW",
+					"expanded_url": "https://www.esa.int/About_Us/Week_in_images/Week_in_images_19_-_23_July_2021",
+					"display_url": "esa.int/About_Us/Week_…",
+					"indices": [
+						184,
+						207
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1419326632544936000,
+					"id_str": "1419326632544935937",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E7J2DWkXoAENkXK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7J2DWkXoAENkXK.jpg",
+					"url": "https://t.co/e5Bf4GrjMd",
+					"display_url": "pic.twitter.com/e5Bf4GrjMd",
+					"expanded_url": "https://twitter.com/esa/status/1419326652551663630/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1071,
+							"h": 1073,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1071,
+							"h": 1073,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1419326632544936000,
+					"id_str": "1419326632544935937",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E7J2DWkXoAENkXK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7J2DWkXoAENkXK.jpg",
+					"url": "https://t.co/e5Bf4GrjMd",
+					"display_url": "pic.twitter.com/e5Bf4GrjMd",
+					"expanded_url": "https://twitter.com/esa/status/1419326652551663630/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1071,
+							"h": 1073,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1071,
+							"h": 1073,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419326636277837800,
+					"id_str": "1419326636277837832",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E7J2DkeXMAgnB9Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7J2DkeXMAgnB9Z.jpg",
+					"url": "https://t.co/e5Bf4GrjMd",
+					"display_url": "pic.twitter.com/e5Bf4GrjMd",
+					"expanded_url": "https://twitter.com/esa/status/1419326652551663630/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1369,
+							"h": 1347,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 669,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1181,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419326640103043000,
+					"id_str": "1419326640103043083",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E7J2DyuXMAsyXI0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7J2DyuXMAsyXI0.jpg",
+					"url": "https://t.co/e5Bf4GrjMd",
+					"display_url": "pic.twitter.com/e5Bf4GrjMd",
+					"expanded_url": "https://twitter.com/esa/status/1419326652551663630/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1009,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1009,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 677,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419326642883793000,
+					"id_str": "1419326642883792903",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/E7J2D9FWEAcv3qJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7J2D9FWEAcv3qJ.jpg",
+					"url": "https://t.co/e5Bf4GrjMd",
+					"display_url": "pic.twitter.com/e5Bf4GrjMd",
+					"expanded_url": "https://twitter.com/esa/status/1419326652551663630/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1179,
+							"h": 1181,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1179,
+							"h": 1181,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://twitter.facelift-cloud.com/\" rel=\"nofollow\">Facelift-Cloud</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21436960,
+			"id_str": "21436960",
+			"name": "ESA",
+			"screen_name": "esa",
+			"location": "Europe",
+			"description": "European Space Agency, keeping you posted on European space activities. \n\nPlease see our Privacy Notice: https://t.co/8SxD1vxxvC",
+			"url": "https://t.co/lFXw82Hghj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lFXw82Hghj",
+							"expanded_url": "https://www.esa.int/",
+							"display_url": "esa.int",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8SxD1vxxvC",
+							"expanded_url": "https://www.esa.int/ESA/Connect_with_us/Connect_with_us",
+							"display_url": "esa.int/ESA/Connect_wi…",
+							"indices": [
+								105,
+								128
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 1267883,
+			"friends_count": 816,
+			"listed_count": 14643,
+			"created_at": "Fri Feb 20 21:38:25 +0000 2009",
+			"favourites_count": 20564,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 50061,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "011A29",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1395819992399192066/kAgbNcr5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1395819992399192066/kAgbNcr5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21436960/1606919529",
+			"profile_link_color": "0099DB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "4D4F53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 279,
+		"favorite_count": 1081,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"And up there in the heavens\nGalileo saw reflections of us too\nPluribus unum, unus mundus\nAll the satellites imbue.\" \n\n- @coldplay, #Coloratura\n\n📷 Our #WeekInImages, 19-23 July 2021 👉 https://t.co/lpgaAxeavW https://t.co/e5Bf4GrjMd"
+	},
+	{
 		"created_at": "Sun Jul 25 15:38:41 +0000 2021",
 		"id": 1419321211562168300,
 		"id_str": "1419321211562168324",
