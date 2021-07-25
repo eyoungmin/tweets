dc671c442ed627a70c8787ad3920a2a89ea2c7bd
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-25 18:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78832,
-	"created_at": "Sun Jul 25 07:02:07 +0000 2021",
+	"tweets": 78833,
+	"created_at": "Sun Jul 25 09:02:10 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,171 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 25 08:52:47 +0000 2021",
+		"id": 1419209775158468600,
+		"id_str": "1419219066582179840",
+		"full_text": "🦋🌙 Glitching⚡\n\nhttps://t.co/Fd0VhM6gCr\n\n#GISELLE #NINGNING #aespa #SHINee #RingDingDong \n#glitch #버퍼링링딩동 #SHINeeglitch #PinkBlood #핑크블러드",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GISELLE",
+					"indices": [
+						40,
+						48
+					]
+				},
+				{
+					"text": "NINGNING",
+					"indices": [
+						49,
+						58
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						59,
+						65
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						66,
+						73
+					]
+				},
+				{
+					"text": "RingDingDong",
+					"indices": [
+						74,
+						87
+					]
+				},
+				{
+					"text": "glitch",
+					"indices": [
+						89,
+						96
+					]
+				},
+				{
+					"text": "버퍼링링딩동",
+					"indices": [
+						97,
+						104
+					]
+				},
+				{
+					"text": "SHINeeglitch",
+					"indices": [
+						105,
+						118
+					]
+				},
+				{
+					"text": "PinkBlood",
+					"indices": [
+						119,
+						129
+					]
+				},
+				{
+					"text": "핑크블러드",
+					"indices": [
+						130,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Fd0VhM6gCr",
+					"expanded_url": "https://vt.tiktok.com/ZSJsfpwhJ/",
+					"display_url": "vt.tiktok.com/ZSJsfpwhJ/",
+					"indices": [
+						15,
+						38
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Next Level",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1278500,
+			"friends_count": 1,
+			"listed_count": 4880,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 762,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1389958073020604416/c-BnpgQ0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1620226804",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4465,
+		"favorite_count": 12804,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🦋🌙 Glitching⚡\n\nhttps://t.co/Fd0VhM6gCr\n\n#GISELLE #NINGNING #aespa #SHINee #RingDingDong \n#glitch #버퍼링링딩동 #SHINeeglitch #PinkBlood #핑크블러드"
+	},
+	{
 		"created_at": "Sun Jul 25 07:00:48 +0000 2021",
 		"id": 1419190883375882200,
 		"id_str": "1419190883375882244",
