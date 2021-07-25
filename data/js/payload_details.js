666731e489709b6f39d00fece47f859289e5641e
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-25 23:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78837,
-	"created_at": "Sun Jul 25 13:02:12 +0000 2021",
+	"tweets": 78838,
+	"created_at": "Sun Jul 25 14:02:10 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Sun Jul 25 13:45:38 +0000 2021",
+		"id": 1419289814768054300,
+		"id_str": "1419292762927767556",
+		"full_text": "[#OliviaHye] 즐겁게 촬영했었던 bnt 화보 촬영! 화보를 찍으면 찍을수록 더 재밌는 것 같아요 나오진 않았지만 맘에 들었던 B컷 공유합니다 마지막 사진은 현진 언니가 찍어준 거ㅋㅋㅋㅋ\n\n#이달의소녀 #LOONA https://t.co/LvTuUOaiLm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OliviaHye",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						117,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1419289802243862500,
+					"id_str": "1419289802243862534",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E7JUjjAUcAYq1Vx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7JUjjAUcAYq1Vx.jpg",
+					"url": "https://t.co/LvTuUOaiLm",
+					"display_url": "pic.twitter.com/LvTuUOaiLm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419289814768054273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1419289802243862500,
+					"id_str": "1419289802243862534",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E7JUjjAUcAYq1Vx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7JUjjAUcAYq1Vx.jpg",
+					"url": "https://t.co/LvTuUOaiLm",
+					"display_url": "pic.twitter.com/LvTuUOaiLm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419289814768054273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419289802260713500,
+					"id_str": "1419289802260713473",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E7JUjjEVkAE2DQu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7JUjjEVkAE2DQu.jpg",
+					"url": "https://t.co/LvTuUOaiLm",
+					"display_url": "pic.twitter.com/LvTuUOaiLm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419289814768054273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419289802239725600,
+					"id_str": "1419289802239725574",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E7JUji_VUAY0fSq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7JUji_VUAY0fSq.jpg",
+					"url": "https://t.co/LvTuUOaiLm",
+					"display_url": "pic.twitter.com/LvTuUOaiLm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419289814768054273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419289802252329000,
+					"id_str": "1419289802252328963",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/E7JUjjCVoAMHuxd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7JUjjCVoAMHuxd.jpg",
+					"url": "https://t.co/LvTuUOaiLm",
+					"display_url": "pic.twitter.com/LvTuUOaiLm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419289814768054273/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 778468,
+			"friends_count": 1,
+			"listed_count": 7752,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3116,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6807,
+		"favorite_count": 16994,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#OliviaHye] 즐겁게 촬영했었던 bnt 화보 촬영! 화보를 찍으면 찍을수록 더 재밌는 것 같아요 나오진 않았지만 맘에 들었던 B컷 공유합니다 마지막 사진은 현진 언니가 찍어준 거ㅋㅋㅋㅋ\n\n#이달의소녀 #LOONA https://t.co/LvTuUOaiLm"
+	},
+	{
 		"created_at": "Sun Jul 25 12:24:08 +0000 2021",
 		"id": 1419266180565581800,
 		"id_str": "1419272251111661570",
