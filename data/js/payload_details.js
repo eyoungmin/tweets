0a15e60fe8aefa98887738b3068a4fec6eb37ee1
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-26 13:02:15 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78847,
-	"created_at": "Mon Jul 26 03:02:08 +0000 2021",
+	"tweets": 78848,
+	"created_at": "Mon Jul 26 04:02:14 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,156 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 26 03:45:36 +0000 2021",
+		"id": 1419500797654573000,
+		"id_str": "1419504148672811011",
+		"full_text": "레인지 로버 vs 에쿠스\n\n#아이돌패밀리 #이달의소녀 #LOONA \n#IDOLFAMILY #하슬 #HaSeul\n\nhttps://t.co/GxRXpWKHlW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이돌패밀리",
+					"indices": [
+						15,
+						22
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						23,
+						29
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						30,
+						36
+					]
+				},
+				{
+					"text": "IDOLFAMILY",
+					"indices": [
+						38,
+						49
+					]
+				},
+				{
+					"text": "하슬",
+					"indices": [
+						50,
+						53
+					]
+				},
+				{
+					"text": "HaSeul",
+					"indices": [
+						54,
+						61
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GxRXpWKHlW",
+					"expanded_url": "https://www.youtube.com/watch?v=FbR2i90HA10",
+					"display_url": "youtube.com/watch?v=FbR2i9…",
+					"indices": [
+						63,
+						86
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906055771921629200,
+			"id_str": "906055771921629184",
+			"name": "나돌(NADOL)",
+			"screen_name": "ae_ssr",
+			"location": "대한민국 중구",
+			"description": "KPOP 스타들의 매력발산 채널 '나도 아이돌이다'",
+			"url": "https://t.co/W4BmT5ww9v",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/W4BmT5ww9v",
+							"expanded_url": "https://www.youtube.com/channel/UC3pk0eDcZx_oYGCWNyJpjMQ/featured?view_as=subscriber",
+							"display_url": "youtube.com/channel/UC3pk0…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1654,
+			"friends_count": 95,
+			"listed_count": 16,
+			"created_at": "Fri Sep 08 07:25:04 +0000 2017",
+			"favourites_count": 38,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2262,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1318368541406494720/fyadTBj2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1318368541406494720/fyadTBj2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906055771921629184/1603158530",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 123,
+		"favorite_count": 318,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "레인지 로버 vs 에쿠스\n\n#아이돌패밀리 #이달의소녀 #LOONA \n#IDOLFAMILY #하슬 #HaSeul\n\nhttps://t.co/GxRXpWKHlW"
+	},
+	{
 		"created_at": "Mon Jul 26 02:52:50 +0000 2021",
 		"id": 1419487917584515000,
 		"id_str": "1419490866482360320",
@@ -122,7 +272,7 @@ Grailbird.data.tweets_2021_07 =
 		"created_at": "Mon Jul 26 02:52:47 +0000 2021",
 		"id": 1419487534984306700,
 		"id_str": "1419490857598738432",
-		"full_text": "‘8월 컴백’ 레드벨벳,  잡화점 콘셉트의 아카이빙 프로모션 ‘신비한 퀸즈잡화점’ 으로 컴백 예열!\n\n8월 1일까지 일주일간 인스타그램을 통해 진행!\n\n#레드벨벳 #RedVelvet #Queens_Mystic_General_Store  ​\n#신비한_퀸즈_잡화점 #퀸즈잡화점 #Queens https://t.co/jIO5PvRKdb",
+		"full_text": "‘8월 컴백’ 레드벨벳,  잡화점 콘셉트의 아카이빙 프로모션 ‘신비한 퀸즈잡화점’ 으로 컴백 예열!\n\n8월 1일까지 일주일간 인스타그램을 통해 진행!\n\n#레드벨벳 #RedVelvet #Queens_Mystic_General_Store  \n#신비한_퀸즈_잡화점 #퀸즈잡화점 #Queens https://t.co/jIO5PvRKdb",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -370,13 +520,13 @@ Grailbird.data.tweets_2021_07 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "‘8월 컴백’ 레드벨벳,  잡화점 콘셉트의 아카이빙 프로모션 ‘신비한 퀸즈잡화점’ 으로 컴백 예열!\n\n8월 1일까지 일주일간 인스타그램을 통해 진행!\n\n#레드벨벳 #RedVelvet #Queens_Mystic_General_Store  ​\n#신비한_퀸즈_잡화점 #퀸즈잡화점 #Queens https://t.co/jIO5PvRKdb"
+		"text": "‘8월 컴백’ 레드벨벳,  잡화점 콘셉트의 아카이빙 프로모션 ‘신비한 퀸즈잡화점’ 으로 컴백 예열!\n\n8월 1일까지 일주일간 인스타그램을 통해 진행!\n\n#레드벨벳 #RedVelvet #Queens_Mystic_General_Store  \n#신비한_퀸즈_잡화점 #퀸즈잡화점 #Queens https://t.co/jIO5PvRKdb"
 	},
 	{
 		"created_at": "Mon Jul 26 02:52:39 +0000 2021",
 		"id": 1419381041131757600,
 		"id_str": "1419490821686980608",
-		"full_text": "Jennie and Grimes go to space 🧚🏻‍♀️🚀 https://t.co/d3FaY30hOf",
+		"full_text": "Jennie and Grimes go to space 🧚🏻♀️🚀 https://t.co/d3FaY30hOf",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -617,7 +767,7 @@ Grailbird.data.tweets_2021_07 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Jennie and Grimes go to space 🧚🏻‍♀️🚀 https://t.co/d3FaY30hOf"
+		"text": "Jennie and Grimes go to space 🧚🏻♀️🚀 https://t.co/d3FaY30hOf"
 	},
 	{
 		"created_at": "Mon Jul 26 02:45:45 +0000 2021",
