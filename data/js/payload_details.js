6827bc85d17ae84f44345ec0324b1b3033ed91c9
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-26 20:02:18 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78854,
-	"created_at": "Mon Jul 26 10:02:09 +0000 2021",
+	"tweets": 78855,
+	"created_at": "Mon Jul 26 11:02:17 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Mon Jul 26 10:18:45 +0000 2021",
+		"id": 1419598371027837000,
+		"id_str": "1419603086855659527",
+		"full_text": "블핑 제니, 카일리 제너의 허리에 반짝이는 이 것은?\n\n#블랙핑크 #제니 #카일리제너 #헤일리비버 #벨라하디드 #벨리체인 #악세사리 #여름 #패션템 #BlackPink #체인 #헐리웃 #포인트 \nhttps://t.co/SWM79RjAN3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						31,
+						36
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						37,
+						40
+					]
+				},
+				{
+					"text": "카일리제너",
+					"indices": [
+						41,
+						47
+					]
+				},
+				{
+					"text": "헤일리비버",
+					"indices": [
+						48,
+						54
+					]
+				},
+				{
+					"text": "벨라하디드",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "벨리체인",
+					"indices": [
+						62,
+						67
+					]
+				},
+				{
+					"text": "악세사리",
+					"indices": [
+						68,
+						73
+					]
+				},
+				{
+					"text": "여름",
+					"indices": [
+						74,
+						77
+					]
+				},
+				{
+					"text": "패션템",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "BlackPink",
+					"indices": [
+						83,
+						93
+					]
+				},
+				{
+					"text": "체인",
+					"indices": [
+						94,
+						97
+					]
+				},
+				{
+					"text": "헐리웃",
+					"indices": [
+						98,
+						102
+					]
+				},
+				{
+					"text": "포인트",
+					"indices": [
+						103,
+						107
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SWM79RjAN3",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/57130?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/57130?…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다",
+			"url": "https://t.co/ONBx2OumIF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ONBx2OumIF",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89007,
+			"friends_count": 3,
+			"listed_count": 401,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12908,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블핑 제니, 카일리 제너의 허리에 반짝이는 이 것은?\n\n#블랙핑크 #제니 #카일리제너 #헤일리비버 #벨라하디드 #벨리체인 #악세사리 #여름 #패션템 #BlackPink #체인 #헐리웃 #포인트 \nhttps://t.co/SWM79RjAN3"
+	},
+	{
 		"created_at": "Mon Jul 26 09:30:45 +0000 2021",
 		"id": 1419591005884530700,
 		"id_str": "1419591005884530689",
