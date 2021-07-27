dc51d69eea44950ba7a7474b19857f68d9334e9e
Youngmin Lee <iameyoungmin@gmail.com>
2021-07-27 16:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 78870,
-	"created_at": "Tue Jul 27 05:02:17 +0000 2021",
+	"tweets": 78871,
+	"created_at": "Tue Jul 27 07:02:10 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_07.js
+++ data/js/tweets/2021_07.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2021_07 = 
 [
 	{
+		"created_at": "Tue Jul 27 06:45:36 +0000 2021",
+		"id": 1419911411765964800,
+		"id_str": "1419911835780894723",
+		"full_text": "[#김립 / #KimLip] 오빛 보여 줄 생각하면서 열심히 찍은 이클립스 셀카❤\n\n#이달의소녀 #LOONA https://t.co/J3guRBD6Mc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김립",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "KimLip",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						47,
+						53
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						54,
+						60
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1419911398201565200,
+					"id_str": "1419911398201565186",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/E7SJ5MZVIAIkR2-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7SJ5MZVIAIkR2-.jpg",
+					"url": "https://t.co/J3guRBD6Mc",
+					"display_url": "pic.twitter.com/J3guRBD6Mc",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419911411765964807/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1419911398201565200,
+					"id_str": "1419911398201565186",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/E7SJ5MZVIAIkR2-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7SJ5MZVIAIkR2-.jpg",
+					"url": "https://t.co/J3guRBD6Mc",
+					"display_url": "pic.twitter.com/J3guRBD6Mc",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419911411765964807/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419911398209966000,
+					"id_str": "1419911398209966085",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/E7SJ5MbVUAUIoe7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7SJ5MbVUAUIoe7.jpg",
+					"url": "https://t.co/J3guRBD6Mc",
+					"display_url": "pic.twitter.com/J3guRBD6Mc",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419911411765964807/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419911398432202800,
+					"id_str": "1419911398432202753",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/E7SJ5NQUYAE6k9O.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7SJ5NQUYAE6k9O.jpg",
+					"url": "https://t.co/J3guRBD6Mc",
+					"display_url": "pic.twitter.com/J3guRBD6Mc",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419911411765964807/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1419911398906163200,
+					"id_str": "1419911398906163202",
+					"indices": [
+						61,
+						84
+					],
+					"media_url": "http://pbs.twimg.com/media/E7SJ5PBUcAIPAoI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7SJ5PBUcAIPAoI.jpg",
+					"url": "https://t.co/J3guRBD6Mc",
+					"display_url": "pic.twitter.com/J3guRBD6Mc",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1419911411765964807/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 779419,
+			"friends_count": 1,
+			"listed_count": 7756,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4385,
+		"favorite_count": 10414,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김립 / #KimLip] 오빛 보여 줄 생각하면서 열심히 찍은 이클립스 셀카❤\n\n#이달의소녀 #LOONA https://t.co/J3guRBD6Mc"
+	},
+	{
 		"created_at": "Tue Jul 27 04:55:25 +0000 2021",
 		"id": 1419884105756512300,
 		"id_str": "1419884105756512257",
