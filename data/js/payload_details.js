d9656a75c5ab8f60f71564c27a3c00a6ef20b3fc
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-01 13:02:09 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79000,
-	"created_at": "Sun Aug 01 00:02:09 +0000 2021",
+	"tweets": 79002,
+	"created_at": "Sun Aug 01 04:02:08 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,408 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Sun Aug 01 03:52:47 +0000 2021",
+		"id": 1421667062103961600,
+		"id_str": "1421680284043513858",
+		"full_text": "8월 2일 월요일 12시, BLINK과 BLACKPINK를 위한 글로벌 공식 팬 커뮤니티, BLACKPINK 위버스가 오픈합니다.🖤💖 \n\n앞으로 #BLACKPINK위버스 를 찾아올 다양한 콘텐츠와 한층 더 가까워질 BLACKPINK와의 소통을 기대해 주세요!🙌\n\n#BLACKPINK #블랙핑크 #위버스 https://t.co/nKaIF9QD25",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK위버스",
+					"indices": [
+						81,
+						94
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						147,
+						157
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						158,
+						163
+					]
+				},
+				{
+					"text": "위버스",
+					"indices": [
+						164,
+						168
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1421666636621222000,
+					"id_str": "1421666636621221891",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/E7rGRhDVEAMmiyl.png",
+					"media_url_https": "https://pbs.twimg.com/media/E7rGRhDVEAMmiyl.png",
+					"url": "https://t.co/nKaIF9QD25",
+					"display_url": "pic.twitter.com/nKaIF9QD25",
+					"expanded_url": "https://twitter.com/weverseofficial/status/1421667062103961601/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1421666636621222000,
+					"id_str": "1421666636621221891",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/media/E7rGRhDVEAMmiyl.png",
+					"media_url_https": "https://pbs.twimg.com/media/E7rGRhDVEAMmiyl.png",
+					"url": "https://t.co/nKaIF9QD25",
+					"display_url": "pic.twitter.com/nKaIF9QD25",
+					"expanded_url": "https://twitter.com/weverseofficial/status/1421667062103961601/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 988692701791993900,
+			"id_str": "988692701791993856",
+			"name": "Weverse",
+			"screen_name": "weverseofficial",
+			"location": "대한민국 서울",
+			"description": "Official for All Fans　　　　　　　　                    \nCopyright by WEVERSE COMPANY Inc. All rights reserved.",
+			"url": "https://t.co/v5AKrQxS7o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/v5AKrQxS7o",
+							"expanded_url": "https://weverseapp.page.link/egZf",
+							"display_url": "weverseapp.page.link/egZf",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3292551,
+			"friends_count": 35,
+			"listed_count": 4858,
+			"created_at": "Tue Apr 24 08:14:43 +0000 2018",
+			"favourites_count": 39,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1746,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1134006940399366144/PtAdIdBp_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134006940399366144/PtAdIdBp_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/988692701791993856/1583732374",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4768,
+		"favorite_count": 16510,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "8월 2일 월요일 12시, BLINK과 BLACKPINK를 위한 글로벌 공식 팬 커뮤니티, BLACKPINK 위버스가 오픈합니다.🖤💖 \n\n앞으로 #BLACKPINK위버스 를 찾아올 다양한 콘텐츠와 한층 더 가까워질 BLACKPINK와의 소통을 기대해 주세요!🙌\n\n#BLACKPINK #블랙핑크 #위버스 https://t.co/nKaIF9QD25"
+	},
+	{
+		"created_at": "Sun Aug 01 03:07:33 +0000 2021",
+		"id": 1421668898663977000,
+		"id_str": "1421668898663976962",
+		"full_text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크, ‘뚜두뚜두’ 안무 영상 4억뷰 돌파..K팝 그룹 신기록 경신中 \nhttps://t.co/QQ5sv9uPQY \n \n#블랙핑크 #BLACKPINK https://t.co/GOddtGWt4d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			103
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						87,
+						92
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						93,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QQ5sv9uPQY",
+					"expanded_url": "https://yg-life.com/archives/169199?lang=ko",
+					"display_url": "yg-life.com/archives/16919…",
+					"indices": [
+						60,
+						83
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1421668897334468600,
+					"id_str": "1421668897334468608",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/E7rIVG3XoAAfqHP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7rIVG3XoAAfqHP.jpg",
+					"url": "https://t.co/GOddtGWt4d",
+					"display_url": "pic.twitter.com/GOddtGWt4d",
+					"expanded_url": "https://twitter.com/fromminn/status/1421668898663976962/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 530,
+							"h": 795,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 795,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1421668897334468600,
+					"id_str": "1421668897334468608",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/E7rIVG3XoAAfqHP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7rIVG3XoAAfqHP.jpg",
+					"url": "https://t.co/GOddtGWt4d",
+					"display_url": "pic.twitter.com/GOddtGWt4d",
+					"expanded_url": "https://twitter.com/fromminn/status/1421668898663976962/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 530,
+							"h": 795,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 795,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 317,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44937,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 83023,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크, ‘뚜두뚜두’ 안무 영상 4억뷰 돌파..K팝 그룹 신기록 경신中 \nhttps://t.co/QQ5sv9uPQY \n \n#블랙핑크 #BLACKPINK https://t.co/GOddtGWt4d"
+	}
+]
