b35957750e10fbab5b3ba714ac1194fffb563131
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-02 20:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79032,
-	"created_at": "Mon Aug 02 10:02:00 +0000 2021",
+	"tweets": 79033,
+	"created_at": "Mon Aug 02 11:02:00 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Mon Aug 02 10:20:24 +0000 2021",
+		"id": 1422135086179254300,
+		"id_str": "1422140217641738242",
+		"full_text": "올 여름은 내가 접수한다! 여자 솔로들의 컴백 젤 기대되는 건 뉴규?\n\n#블랙핑크 #BLACKPINK #리사 #아이오아이 #전소미 #선미 #덤덤 #Siren #인어 #CL #효연 #문명특급 #솔로 #아이돌\nhttps://t.co/lqQTsE4Cvs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						40,
+						45
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						46,
+						56
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						57,
+						60
+					]
+				},
+				{
+					"text": "아이오아이",
+					"indices": [
+						61,
+						67
+					]
+				},
+				{
+					"text": "전소미",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "선미",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "덤덤",
+					"indices": [
+						77,
+						80
+					]
+				},
+				{
+					"text": "Siren",
+					"indices": [
+						81,
+						87
+					]
+				},
+				{
+					"text": "인어",
+					"indices": [
+						88,
+						91
+					]
+				},
+				{
+					"text": "CL",
+					"indices": [
+						92,
+						95
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						96,
+						99
+					]
+				},
+				{
+					"text": "문명특급",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "솔로",
+					"indices": [
+						106,
+						109
+					]
+				},
+				{
+					"text": "아이돌",
+					"indices": [
+						110,
+						114
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lqQTsE4Cvs",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/57320?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/57320?…",
+					"indices": [
+						115,
+						138
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다",
+			"url": "https://t.co/ONBx2OumIF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ONBx2OumIF",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88987,
+			"friends_count": 3,
+			"listed_count": 401,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12928,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "올 여름은 내가 접수한다! 여자 솔로들의 컴백 젤 기대되는 건 뉴규?\n\n#블랙핑크 #BLACKPINK #리사 #아이오아이 #전소미 #선미 #덤덤 #Siren #인어 #CL #효연 #문명특급 #솔로 #아이돌\nhttps://t.co/lqQTsE4Cvs"
+	},
+	{
 		"created_at": "Mon Aug 02 09:29:47 +0000 2021",
 		"id": 1422119988287000600,
 		"id_str": "1422127478882066434",
