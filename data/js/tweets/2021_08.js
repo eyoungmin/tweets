bc588309fb626e1d26a26d94e96ab61ccefe8c7c
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-03 17:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 03 07:40:09 +0000 2021",
+		"id": 1422460577268473900,
+		"id_str": "1422462278717353994",
+		"full_text": "[#ViVi] 힝 PTT(Paint The Town) 활동 그리워요~\n\n#이달의소녀 #LOONA https://t.co/QVZDT36P5g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ViVi",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						40,
+						46
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						47,
+						53
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1422460570985406500,
+					"id_str": "1422460570985406464",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/E72YWlCVoAAH2_T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E72YWlCVoAAH2_T.jpg",
+					"url": "https://t.co/QVZDT36P5g",
+					"display_url": "pic.twitter.com/QVZDT36P5g",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1422460577268473859/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1422460570985406500,
+					"id_str": "1422460570985406464",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/E72YWlCVoAAH2_T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E72YWlCVoAAH2_T.jpg",
+					"url": "https://t.co/QVZDT36P5g",
+					"display_url": "pic.twitter.com/QVZDT36P5g",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1422460577268473859/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1422460570981191700,
+					"id_str": "1422460570981191687",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/E72YWlBVUAcWT1A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E72YWlBVUAcWT1A.jpg",
+					"url": "https://t.co/QVZDT36P5g",
+					"display_url": "pic.twitter.com/QVZDT36P5g",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1422460577268473859/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1422460570981126100,
+					"id_str": "1422460570981126147",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/E72YWlBUUAMW1Ct.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E72YWlBUUAMW1Ct.jpg",
+					"url": "https://t.co/QVZDT36P5g",
+					"display_url": "pic.twitter.com/QVZDT36P5g",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1422460577268473859/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1422460570981208000,
+					"id_str": "1422460570981208068",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/E72YWlBVkAQB_V8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E72YWlBVkAQB_V8.jpg",
+					"url": "https://t.co/QVZDT36P5g",
+					"display_url": "pic.twitter.com/QVZDT36P5g",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1422460577268473859/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1128,
+							"h": 1128,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 784032,
+			"friends_count": 1,
+			"listed_count": 7797,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3140,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4398,
+		"favorite_count": 11697,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#ViVi] 힝 PTT(Paint The Town) 활동 그리워요~\n\n#이달의소녀 #LOONA https://t.co/QVZDT36P5g"
+	},
+	{
 		"created_at": "Tue Aug 03 06:36:50 +0000 2021",
 		"id": 1422446341888880600,
 		"id_str": "1422446341888880648",
