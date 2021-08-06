56b822f07f59d23c1c040e48cd52d2392fe037e8
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-06 19:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Fri Aug 06 09:40:14 +0000 2021",
+		"id": 1423577802859630600,
+		"id_str": "1423579659090944000",
+		"full_text": "[#최리 / #Choerry] 힣ㅎㅎ 이제는 올려도 되네요ㅎㅎ 오빛 화보 찍은 사진이랑 제가 그린 그림이랑 언니들이랑 여진이랑 찍은 사진 가지구 왔어영!!💜 저 잘했죠!!?\n\n#이달의소녀 #LOONA https://t.co/KL2pmaX8n6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "최리",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "Choerry",
+					"indices": [
+						7,
+						15
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						98,
+						104
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						105,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1423577790343827500,
+					"id_str": "1423577790343827461",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GQdTeUUAUrq5d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GQdTeUUAUrq5d.jpg",
+					"url": "https://t.co/KL2pmaX8n6",
+					"display_url": "pic.twitter.com/KL2pmaX8n6",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1423577802859630595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1423577790343827500,
+					"id_str": "1423577790343827461",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GQdTeUUAUrq5d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GQdTeUUAUrq5d.jpg",
+					"url": "https://t.co/KL2pmaX8n6",
+					"display_url": "pic.twitter.com/KL2pmaX8n6",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1423577802859630595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1423577790348107800,
+					"id_str": "1423577790348107779",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GQdTfVoAMTBtH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GQdTfVoAMTBtH.jpg",
+					"url": "https://t.co/KL2pmaX8n6",
+					"display_url": "pic.twitter.com/KL2pmaX8n6",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1423577802859630595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1423577790360686600,
+					"id_str": "1423577790360686592",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GQdTiVkAA5qnw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GQdTiVkAA5qnw.jpg",
+					"url": "https://t.co/KL2pmaX8n6",
+					"display_url": "pic.twitter.com/KL2pmaX8n6",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1423577802859630595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1423577790343901200,
+					"id_str": "1423577790343901188",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/E8GQdTeVcAQyex_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8GQdTeVcAQyex_.jpg",
+					"url": "https://t.co/KL2pmaX8n6",
+					"display_url": "pic.twitter.com/KL2pmaX8n6",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1423577802859630595/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 785871,
+			"friends_count": 1,
+			"listed_count": 7803,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3150,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3398,
+		"favorite_count": 8727,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#최리 / #Choerry] 힣ㅎㅎ 이제는 올려도 되네요ㅎㅎ 오빛 화보 찍은 사진이랑 제가 그린 그림이랑 언니들이랑 여진이랑 찍은 사진 가지구 왔어영!!💜 저 잘했죠!!?\n\n#이달의소녀 #LOONA https://t.co/KL2pmaX8n6"
+	},
+	{
 		"created_at": "Fri Aug 06 08:45:37 +0000 2021",
 		"id": 1423554508500607000,
 		"id_str": "1423565913756999680",
