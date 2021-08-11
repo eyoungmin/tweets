57c9eb3ed8cdf164cde2e5a4395633c005a1d52b
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-12 01:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79411,
-	"created_at": "Wed Aug 11 15:19:59 +0000 2021",
+	"tweets": 79412,
+	"created_at": "Wed Aug 11 16:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 11 15:40:26 +0000 2021",
+		"id": 1425477281607262200,
+		"id_str": "1425482246149054475",
+		"full_text": "We’re so happy to announce our first ever show at the Apollo Theater in Harlem, New York for @SIriusXM + @pandoramusic #SmallStageSeries. Info: https://t.co/lOuRFTD509 https://t.co/47ZkW9IDD9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SmallStageSeries",
+					"indices": [
+						119,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SIRIUSXM",
+					"name": "SiriusXM",
+					"id": 189258194,
+					"id_str": "189258194",
+					"indices": [
+						93,
+						102
+					]
+				},
+				{
+					"screen_name": "pandoramusic",
+					"name": "Pandora",
+					"id": 5796972,
+					"id_str": "5796972",
+					"indices": [
+						105,
+						118
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lOuRFTD509",
+					"expanded_url": "https://www.siriusxm.com/coldplaylive",
+					"display_url": "siriusxm.com/coldplaylive",
+					"indices": [
+						144,
+						167
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1425477277119307800,
+					"id_str": "1425477277119307778",
+					"indices": [
+						168,
+						191
+					],
+					"media_url": "http://pbs.twimg.com/media/E8hQB_IWYAISzSl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8hQB_IWYAISzSl.jpg",
+					"url": "https://t.co/47ZkW9IDD9",
+					"display_url": "pic.twitter.com/47ZkW9IDD9",
+					"expanded_url": "https://twitter.com/coldplay/status/1425477281607262209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1425477277119307800,
+					"id_str": "1425477277119307778",
+					"indices": [
+						168,
+						191
+					],
+					"media_url": "http://pbs.twimg.com/media/E8hQB_IWYAISzSl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8hQB_IWYAISzSl.jpg",
+					"url": "https://t.co/47ZkW9IDD9",
+					"display_url": "pic.twitter.com/47ZkW9IDD9",
+					"expanded_url": "https://twitter.com/coldplay/status/1425477281607262209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Coloratura",
+			"description": "The official Twitter of the band Coldplay. New album, Music Of The Spheres, out October 15th, 2021.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23004416,
+			"friends_count": 767,
+			"listed_count": 42705,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1320,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8607,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 152,
+		"favorite_count": 1136,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We’re so happy to announce our first ever show at the Apollo Theater in Harlem, New York for @SIriusXM + @pandoramusic #SmallStageSeries. Info: https://t.co/lOuRFTD509 https://t.co/47ZkW9IDD9"
+	},
+	{
 		"created_at": "Wed Aug 11 15:10:57 +0000 2021",
 		"id": 1425465067676471300,
 		"id_str": "1425474827704410116",
