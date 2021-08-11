3f7d040bc5ae17be4a8430f6a760a75bdf7338d6
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-11 17:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79380,
-	"created_at": "Wed Aug 11 06:02:04 +0000 2021",
+	"tweets": 79381,
+	"created_at": "Wed Aug 11 08:02:03 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 11 06:11:19 +0000 2021",
+		"id": 1425339025632731100,
+		"id_str": "1425339025632731137",
+		"full_text": "#SMTOWN #Artist_News \nSM ‘STATION’ 텐 솔로곡 ‘Paint Me Naked’ 아이튠즈 톱 송 차트 16개 지역 1위! \nhttps://t.co/sAh7OdaQ9m \n \n#STATION #SMSTATION #스테이션 https://t.co/3Mhr5To6WD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "Artist_News",
+					"indices": [
+						8,
+						20
+					]
+				},
+				{
+					"text": "STATION",
+					"indices": [
+						109,
+						117
+					]
+				},
+				{
+					"text": "SMSTATION",
+					"indices": [
+						118,
+						128
+					]
+				},
+				{
+					"text": "스테이션",
+					"indices": [
+						129,
+						134
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sAh7OdaQ9m",
+					"expanded_url": "https://www.smtown.com/artist/newsDetail/5307",
+					"display_url": "smtown.com/artist/newsDet…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1425339024638685200,
+					"id_str": "1425339024638685185",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E8fSSogXMAEW88T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8fSSogXMAEW88T.jpg",
+					"url": "https://t.co/3Mhr5To6WD",
+					"display_url": "pic.twitter.com/3Mhr5To6WD",
+					"expanded_url": "https://twitter.com/fromminn/status/1425339025632731137/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1425339024638685200,
+					"id_str": "1425339024638685185",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/E8fSSogXMAEW88T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8fSSogXMAEW88T.jpg",
+					"url": "https://t.co/3Mhr5To6WD",
+					"display_url": "pic.twitter.com/3Mhr5To6WD",
+					"expanded_url": "https://twitter.com/fromminn/status/1425339025632731137/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 541,
+							"h": 295,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 1050,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44909,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 83457,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#SMTOWN #Artist_News \nSM ‘STATION’ 텐 솔로곡 ‘Paint Me Naked’ 아이튠즈 톱 송 차트 16개 지역 1위! \nhttps://t.co/sAh7OdaQ9m \n \n#STATION #SMSTATION #스테이션 https://t.co/3Mhr5To6WD"
+	},
+	{
 		"created_at": "Wed Aug 11 05:45:28 +0000 2021",
 		"id": 1425332519390662700,
 		"id_str": "1425332519390662656",
