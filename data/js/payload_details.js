d456579bb5b438c1f71ebc0b345adebdf85f168d
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-13 03:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79459,
-	"created_at": "Thu Aug 12 16:02:06 +0000 2021",
+	"tweets": 79460,
+	"created_at": "Thu Aug 12 18:02:06 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Thu Aug 12 17:33:02 +0000 2021",
+		"id": 1425872971604897800,
+		"id_str": "1425872971604897798",
+		"full_text": "#백예린 #YerinBaek #알라딘\n[굿즈] 턴온댓블루바이닐 아크릴 키홀더 (블루바이닐 | 2021년 08월 | 8,900원)\nhttps://t.co/I2ipHUTL4p https://t.co/BKqADfc9Rv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "백예린",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "YerinBaek",
+					"indices": [
+						5,
+						15
+					]
+				},
+				{
+					"text": "알라딘",
+					"indices": [
+						16,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/I2ipHUTL4p",
+					"expanded_url": "https://www.aladin.co.kr/shop/wproduct.aspx?itemid=277610871",
+					"display_url": "aladin.co.kr/shop/wproduct.…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1425872970350833700,
+					"id_str": "1425872970350833668",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/E8m36XSXsAQObKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8m36XSXsAQObKN.jpg",
+					"url": "https://t.co/BKqADfc9Rv",
+					"display_url": "pic.twitter.com/BKqADfc9Rv",
+					"expanded_url": "https://twitter.com/fromminn/status/1425872971604897798/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1425872970350833700,
+					"id_str": "1425872970350833668",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/E8m36XSXsAQObKN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E8m36XSXsAQObKN.jpg",
+					"url": "https://t.co/BKqADfc9Rv",
+					"display_url": "pic.twitter.com/BKqADfc9Rv",
+					"expanded_url": "https://twitter.com/fromminn/status/1425872971604897798/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44907,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 83553,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#백예린 #YerinBaek #알라딘\n[굿즈] 턴온댓블루바이닐 아크릴 키홀더 (블루바이닐 | 2021년 08월 | 8,900원)\nhttps://t.co/I2ipHUTL4p https://t.co/BKqADfc9Rv"
+	},
+	{
 		"created_at": "Thu Aug 12 15:22:58 +0000 2021",
 		"id": 1425836975001645000,
 		"id_str": "1425840240577507331",
