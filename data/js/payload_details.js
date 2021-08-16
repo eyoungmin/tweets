fbae3ddcc381178b2ea54c6764264bdc1227ad6a
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-17 01:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79599,
-	"created_at": "Mon Aug 16 15:02:09 +0000 2021",
+	"tweets": 79600,
+	"created_at": "Mon Aug 16 16:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Mon Aug 16 15:21:52 +0000 2021",
+		"id": 1427289514779807700,
+		"id_str": "1427289514779807749",
+		"full_text": "#레드벨벳일본 #RedVelvetJP\nRed Velvet / The 6th Mini Album『Queendom』本日より配信スタート！！ さらに各配信サイトで配信キャンペーン実施決定！！\nhttps://t.co/wzmEPsRNY1\n \n #레드벨벳 #RedVelvet https://t.co/PI57NliHpW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레드벨벳일본",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "RedVelvetJP",
+					"indices": [
+						8,
+						20
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						127,
+						132
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						133,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wzmEPsRNY1",
+					"expanded_url": "http://redvelvet-jp.net/news/detail.php?id=1093603",
+					"display_url": "redvelvet-jp.net/news/detail.ph…",
+					"indices": [
+						100,
+						123
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1427289513538293800,
+					"id_str": "1427289513538293762",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/E87AQB4XEAIQygE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E87AQB4XEAIQygE.jpg",
+					"url": "https://t.co/PI57NliHpW",
+					"display_url": "pic.twitter.com/PI57NliHpW",
+					"expanded_url": "https://twitter.com/fromminn/status/1427289514779807749/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1427289513538293800,
+					"id_str": "1427289513538293762",
+					"indices": [
+						144,
+						167
+					],
+					"media_url": "http://pbs.twimg.com/media/E87AQB4XEAIQygE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E87AQB4XEAIQygE.jpg",
+					"url": "https://t.co/PI57NliHpW",
+					"display_url": "pic.twitter.com/PI57NliHpW",
+					"expanded_url": "https://twitter.com/fromminn/status/1427289514779807749/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 452,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 681,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 313,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44899,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 83718,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "#레드벨벳일본 #RedVelvetJP\nRed Velvet / The 6th Mini Album『Queendom』本日より配信スタート！！ さらに各配信サイトで配信キャンペーン実施決定！！\nhttps://t.co/wzmEPsRNY1\n \n #레드벨벳 #RedVelvet https://t.co/PI57NliHpW"
+	},
+	{
 		"created_at": "Mon Aug 16 14:29:14 +0000 2021",
 		"id": 1427276267821404200,
 		"id_str": "1427276267821404162",
