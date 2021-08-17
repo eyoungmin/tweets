75af27e5f3126535dcbdfcf1a844cf03f87fe91f
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-17 14:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79610,
-	"created_at": "Tue Aug 17 04:02:08 +0000 2021",
+	"tweets": 79611,
+	"created_at": "Tue Aug 17 05:02:08 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 17 04:31:04 +0000 2021",
+		"id": 1427480386691502000,
+		"id_str": "1427488120581222402",
+		"full_text": "BLACKPINK 5th ANNIVERSARY [4+1] POP-UP 2ND ONLINE PRE-RESERVATION OPEN!\n\n2차 온라인 사전 예약이 오픈되었습니다!\n\n✔️ https://t.co/MnCd8sFVF9 \n🗓 08.26(THU) - 08.30(MON)\n\n#BLACKPINK #블랙핑크 #4PLUS1_POPUP #20210821 #OPEN #5thANNIVERSARY #4PLUS1_PROJECT #YG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			234
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						152,
+						162
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						163,
+						168
+					]
+				},
+				{
+					"text": "4PLUS1_POPUP",
+					"indices": [
+						169,
+						182
+					]
+				},
+				{
+					"text": "OPEN",
+					"indices": [
+						193,
+						198
+					]
+				},
+				{
+					"text": "5thANNIVERSARY",
+					"indices": [
+						199,
+						214
+					]
+				},
+				{
+					"text": "4PLUS1_PROJECT",
+					"indices": [
+						215,
+						230
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						231,
+						234
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MnCd8sFVF9",
+					"expanded_url": "https://bit.ly/2VUUbKT",
+					"display_url": "bit.ly/2VUUbKT",
+					"indices": [
+						100,
+						123
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/QTHPhjMXWT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QTHPhjMXWT",
+							"expanded_url": "http://5th.blackpinkofficial.com",
+							"display_url": "5th.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7135727,
+			"friends_count": 7,
+			"listed_count": 8231,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1759,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1623718821",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1632,
+		"favorite_count": 8367,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "BLACKPINK 5th ANNIVERSARY [4+1] POP-UP 2ND ONLINE PRE-RESERVATION OPEN!\n\n2차 온라인 사전 예약이 오픈되었습니다!\n\n✔️ https://t.co/MnCd8sFVF9 \n🗓 08.26(THU) - 08.30(MON)\n\n#BLACKPINK #블랙핑크 #4PLUS1_POPUP #20210821 #OPEN #5thANNIVERSARY #4PLUS1_PROJECT #YG"
+	},
+	{
 		"created_at": "Tue Aug 17 03:52:12 +0000 2021",
 		"id": 1427478341238538200,
 		"id_str": "1427478341238538241",
