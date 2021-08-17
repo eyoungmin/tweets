7c7042afb3868d18b32169d28843c428ad27d33c
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-18 01:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79667,
-	"created_at": "Tue Aug 17 15:20:02 +0000 2021",
+	"tweets": 79668,
+	"created_at": "Tue Aug 17 16:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Tue Aug 17 15:40:17 +0000 2021",
+		"id": 1427646699158245400,
+		"id_str": "1427656534838894593",
+		"full_text": "💚#HappyHaSeulday💚\nOrbitみんなで #ハスル の誕生日をお祝いしましょう🎶🎂\n\n#LOONA #今月の少女 #HaSeul https://t.co/05PZV0MUnP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HappyHaSeulday",
+					"indices": [
+						1,
+						16
+					]
+				},
+				{
+					"text": "ハスル",
+					"indices": [
+						28,
+						32
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						50,
+						56
+					]
+				},
+				{
+					"text": "今月の少女",
+					"indices": [
+						57,
+						63
+					]
+				},
+				{
+					"text": "HaSeul",
+					"indices": [
+						64,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1427646693202440200,
+					"id_str": "1427646693202440194",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/E9AFGn0XsAI7n-z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9AFGn0XsAI7n-z.jpg",
+					"url": "https://t.co/05PZV0MUnP",
+					"display_url": "pic.twitter.com/05PZV0MUnP",
+					"expanded_url": "https://twitter.com/loonaJPofficial/status/1427646699158245393/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1427646693202440200,
+					"id_str": "1427646693202440194",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/E9AFGn0XsAI7n-z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E9AFGn0XsAI7n-z.jpg",
+					"url": "https://t.co/05PZV0MUnP",
+					"display_url": "pic.twitter.com/05PZV0MUnP",
+					"expanded_url": "https://twitter.com/loonaJPofficial/status/1427646699158245393/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1407603879026462700,
+			"id_str": "1407603879026462722",
+			"name": "loonatheworld_jp_official",
+			"screen_name": "loonaJPofficial",
+			"location": "",
+			"description": "LOONA（今月の少女）の日本公式アカウントです。",
+			"url": "https://t.co/u7biZCX3K9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/u7biZCX3K9",
+							"expanded_url": "http://orbitjapan.jp",
+							"display_url": "orbitjapan.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42976,
+			"friends_count": 1,
+			"listed_count": 258,
+			"created_at": "Wed Jun 23 07:38:28 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1407682642242248705/Eefvmkkz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1407682642242248705/Eefvmkkz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1407603879026462722/1624452787",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1943,
+		"favorite_count": 5439,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "💚#HappyHaSeulday💚\nOrbitみんなで #ハスル の誕生日をお祝いしましょう🎶🎂\n\n#LOONA #今月の少女 #HaSeul https://t.co/05PZV0MUnP"
+	},
+	{
 		"created_at": "Tue Aug 17 15:12:25 +0000 2021",
 		"id": 1427646580266619000,
 		"id_str": "1427649525229920256",
