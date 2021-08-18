957baea9a0eefbfb77d09bd465581276b8aec60c
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-18 21:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 79700,
-	"created_at": "Wed Aug 18 11:02:07 +0000 2021",
+	"tweets": 79701,
+	"created_at": "Wed Aug 18 12:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Wed Aug 18 11:44:19 +0000 2021",
+		"id": 1427959543359811600,
+		"id_str": "1427959543359811587",
+		"full_text": "#엠넷 #Mnet #Mnet_KPOP\n[80회] '작품을 함께 만들면서 결성된 안무팀' 라치카의 이름을 알린 청하의 대표곡은?#TMINEWS | EP.80 | Mnet 210818 방송\nhttps://t.co/fINktGprI1\n#청하 #CHUNGHA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "엠넷",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "Mnet",
+					"indices": [
+						4,
+						9
+					]
+				},
+				{
+					"text": "Mnet_KPOP",
+					"indices": [
+						10,
+						20
+					]
+				},
+				{
+					"text": "TMINEWS",
+					"indices": [
+						70,
+						78
+					]
+				},
+				{
+					"text": "청하",
+					"indices": [
+						128,
+						131
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						132,
+						140
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fINktGprI1",
+					"expanded_url": "https://www.youtube.com/watch?v=AXVJwi3LIoQ",
+					"display_url": "youtube.com/watch?v=AXVJwi…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 314,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44898,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 83841,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#엠넷 #Mnet #Mnet_KPOP\n[80회] '작품을 함께 만들면서 결성된 안무팀' 라치카의 이름을 알린 청하의 대표곡은?#TMINEWS | EP.80 | Mnet 210818 방송\nhttps://t.co/fINktGprI1\n#청하 #CHUNGHA"
+	},
+	{
 		"created_at": "Wed Aug 18 10:58:17 +0000 2021",
 		"id": 1427947956615860200,
 		"id_str": "1427947956615860227",
