30c11ac71a101c4789abcabed55c935d7bdd6076
Youngmin Lee <iameyoungmin@gmail.com>
2021-08-26 19:02:13 +0900

    Added 5 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_08.js",
 		"year": 2021,
 		"var_name": "tweets_2021_08",
-		"tweet_count": 942,
+		"tweet_count": 947,
 		"month": 8
 	},
 	{
