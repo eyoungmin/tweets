ec45228ce7c71f1e7a2b704e9471baf18b3c85fa
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-02 07:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80176,
-	"created_at": "Wed Sep 01 20:02:01 +0000 2021",
+	"tweets": 80177,
+	"created_at": "Wed Sep 01 22:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 01 21:40:09 +0000 2021",
+		"id": 1433174986743816200,
+		"id_str": "1433182919422619654",
+		"full_text": "@swiftxyloto @coldployx I feel you. I literally can’t go two hours without a snack. Not too long to wait , I promise… PH",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "swiftxyloto",
+					"name": "augustø",
+					"id": 1388765449177993200,
+					"id_str": "1388765449177993217",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "coldployx",
+					"name": "r 🛸✨🪐countess of ultra",
+					"id": 1405677456586068000,
+					"id_str": "1405677456586067978",
+					"indices": [
+						13,
+						23
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1433173954785685500,
+		"in_reply_to_status_id_str": "1433173954785685512",
+		"in_reply_to_user_id": 1388765449177993200,
+		"in_reply_to_user_id_str": "1388765449177993217",
+		"in_reply_to_screen_name": "swiftxyloto",
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Coloratura",
+			"description": "The official Twitter of the band Coldplay. New album, Music Of The Spheres, out October 15th, 2021.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22997713,
+			"friends_count": 767,
+			"listed_count": 42672,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8630,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 109,
+		"favorite_count": 644,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@swiftxyloto @coldployx I feel you. I literally can’t go two hours without a snack. Not too long to wait , I promise… PH"
+	},
+	{
 		"created_at": "Wed Sep 01 19:40:09 +0000 2021",
 		"id": 1433142351875084300,
 		"id_str": "1433152720370864133",
