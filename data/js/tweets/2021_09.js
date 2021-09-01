f9261e85ceb8daf65d4f717ad574942a945edb8f
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-02 05:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 01 19:40:09 +0000 2021",
+		"id": 1433142351875084300,
+		"id_str": "1433152720370864133",
+		"full_text": "@mxgic42_ Not too long now. Just waiting til Epiphane, Calypso and Floris line up in the right way. 🪐 PH",
+		"truncated": false,
+		"display_text_range": [
+			10,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mxgic42_",
+					"name": "maritza | president of calypso 🪐",
+					"id": 1319029844542288000,
+					"id_str": "1319029844542287872",
+					"indices": [
+						0,
+						9
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1433111846295257000,
+		"in_reply_to_status_id_str": "1433111846295257088",
+		"in_reply_to_user_id": 1319029844542288000,
+		"in_reply_to_user_id_str": "1319029844542287872",
+		"in_reply_to_screen_name": "mxgic42_",
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Coloratura",
+			"description": "The official Twitter of the band Coldplay. New album, Music Of The Spheres, out October 15th, 2021.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22997666,
+			"friends_count": 766,
+			"listed_count": 42672,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8629,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 50,
+		"favorite_count": 317,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@mxgic42_ Not too long now. Just waiting til Epiphane, Calypso and Floris line up in the right way. 🪐 PH"
+	},
+	{
 		"created_at": "Wed Sep 01 17:40:09 +0000 2021",
 		"id": 1433110582698389500,
 		"id_str": "1433122521394688009",
