55cc6d2ee6cb24cff078acc4bfcb74e44fe024a4
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-01 17:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,10 +1,193 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 01 07:40:10 +0000 2021",
+		"id": 1432916124979576800,
+		"id_str": "1432971526974943235",
+		"full_text": "태어나보니 호적 메이트?! 😂💦\n본격 남의 집 형제 자매 탐구 프로젝트👀\n#호적메이트 추석 연휴 2회 특별 편성!✨ https://t.co/vf2IEsMHz9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "호적메이트",
+					"indices": [
+						41,
+						47
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1432891193738686500,
+					"id_str": "1432891193738686472",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Km8uyUcAgt1km.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Km8uyUcAgt1km.jpg",
+					"url": "https://t.co/vf2IEsMHz9",
+					"display_url": "pic.twitter.com/vf2IEsMHz9",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1432916124979576833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1432891193738686500,
+					"id_str": "1432891193738686472",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Km8uyUcAgt1km.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Km8uyUcAgt1km.jpg",
+					"url": "https://t.co/vf2IEsMHz9",
+					"display_url": "pic.twitter.com/vf2IEsMHz9",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1432916124979576833/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144012572,
+			"id_str": "4144012572",
+			"name": "MBC 예능연구소",
+			"screen_name": "MBC_entertain",
+			"location": "",
+			"description": "MBC 예능의 꿀잼만 모아 드림 [official]",
+			"url": "https://t.co/7U66BOeSeR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7U66BOeSeR",
+							"expanded_url": "http://facebook.com/MBCentertain",
+							"display_url": "facebook.com/MBCentertain",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 903937,
+			"friends_count": 6,
+			"listed_count": 934,
+			"created_at": "Fri Nov 06 08:21:55 +0000 2015",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15681,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144012572/1555047387",
+			"profile_link_color": "45B7DE",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 550,
+		"favorite_count": 2354,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "태어나보니 호적 메이트?! 😂💦\n본격 남의 집 형제 자매 탐구 프로젝트👀\n#호적메이트 추석 연휴 2회 특별 편성!✨ https://t.co/vf2IEsMHz9"
+	},
+	{
 		"created_at": "Wed Sep 01 06:16:14 +0000 2021",
 		"id": 1432947771431010300,
 		"id_str": "1432950406594777089",
-		"full_text": "Say hello to Orbit💫 #이달의소녀 가 전하는 오빛을 향한 진심❤️‍🔥 OSEN Kpop 라인 채널에 순차적으로 업데이트될 예정이니 놓치지 말고 확인하세요!\n\n#LOONA\n\n▶️ https://t.co/PAW0U1McYU",
+		"full_text": "Say hello to Orbit💫 #이달의소녀 가 전하는 오빛을 향한 진심❤️🔥 OSEN Kpop 라인 채널에 순차적으로 업데이트될 예정이니 놓치지 말고 확인하세요!\n\n#LOONA\n\n▶️ https://t.co/PAW0U1McYU",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -120,7 +303,7 @@ Grailbird.data.tweets_2021_09 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "Say hello to Orbit💫 #이달의소녀 가 전하는 오빛을 향한 진심❤️‍🔥 OSEN Kpop 라인 채널에 순차적으로 업데이트될 예정이니 놓치지 말고 확인하세요!\n\n#LOONA\n\n▶️ https://t.co/PAW0U1McYU"
+		"text": "Say hello to Orbit💫 #이달의소녀 가 전하는 오빛을 향한 진심❤️🔥 OSEN Kpop 라인 채널에 순차적으로 업데이트될 예정이니 놓치지 말고 확인하세요!\n\n#LOONA\n\n▶️ https://t.co/PAW0U1McYU"
 	},
 	{
 		"created_at": "Wed Sep 01 05:40:18 +0000 2021",
