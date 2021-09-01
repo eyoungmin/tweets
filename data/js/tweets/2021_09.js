dac49606246e3bb9323dc5942123b46f772361f3
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-01 12:02:00 +0900

    Added 3 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,10 +1,576 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Wed Sep 01 02:48:41 +0000 2021",
+		"id": 1432898174763552800,
+		"id_str": "1432898174763552768",
+		"full_text": "#KBS #공지사항 #가요심의 \n[#가요심의] 가요심의결과(2021.09.01) \nhttps://t.co/lBg9jQZsb0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KBS",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "공지사항",
+					"indices": [
+						5,
+						10
+					]
+				},
+				{
+					"text": "가요심의",
+					"indices": [
+						11,
+						16
+					]
+				},
+				{
+					"text": "가요심의",
+					"indices": [
+						19,
+						24
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/lBg9jQZsb0",
+					"expanded_url": "https://www.kbs.co.kr/board/board.html?bbs_loc=X2017-0031-04-699231,read,%EA%B0%80%EC%9A%94%EC%8B%AC%EC%9D%98,2217,1067429",
+					"display_url": "kbs.co.kr/board/board.ht…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 316,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44877,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 84387,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#KBS #공지사항 #가요심의 \n[#가요심의] 가요심의결과(2021.09.01) \nhttps://t.co/lBg9jQZsb0"
+	},
+	{
+		"created_at": "Wed Sep 01 02:31:13 +0000 2021",
+		"id": 1432888898212958200,
+		"id_str": "1432893777249595394",
+		"full_text": "KIM YERIM participates in the OST of ‘Blue Birthday’!\n‘It’s You(예리 of Red Velvet Ver.)’ to be released on 9/2 12PM KST!\n\n#김예림 #KIMYERIM\n#예리 #YERI \n#레드벨벳 #RedVelvet\n#블루버스데이 #BLUEBIRTHDAY https://t.co/DG2zC9YKVW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김예림",
+					"indices": [
+						121,
+						125
+					]
+				},
+				{
+					"text": "KIMYERIM",
+					"indices": [
+						126,
+						135
+					]
+				},
+				{
+					"text": "예리",
+					"indices": [
+						136,
+						139
+					]
+				},
+				{
+					"text": "YERI",
+					"indices": [
+						140,
+						145
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						147,
+						152
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						153,
+						163
+					]
+				},
+				{
+					"text": "블루버스데이",
+					"indices": [
+						164,
+						171
+					]
+				},
+				{
+					"text": "BLUEBIRTHDAY",
+					"indices": [
+						172,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1432887871782854700,
+					"id_str": "1432887871782854664",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Kj7XiVIAglMMP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Kj7XiVIAglMMP.jpg",
+					"url": "https://t.co/DG2zC9YKVW",
+					"display_url": "pic.twitter.com/DG2zC9YKVW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1432888898212958210/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1300,
+							"h": 1300,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1432887871782854700,
+					"id_str": "1432887871782854664",
+					"indices": [
+						186,
+						209
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Kj7XiVIAglMMP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Kj7XiVIAglMMP.jpg",
+					"url": "https://t.co/DG2zC9YKVW",
+					"display_url": "pic.twitter.com/DG2zC9YKVW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1432888898212958210/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1300,
+							"h": 1300,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9845893,
+			"friends_count": 55,
+			"listed_count": 10527,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10710,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4557,
+		"favorite_count": 12251,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "KIM YERIM participates in the OST of ‘Blue Birthday’!\n‘It’s You(예리 of Red Velvet Ver.)’ to be released on 9/2 12PM KST!\n\n#김예림 #KIMYERIM\n#예리 #YERI \n#레드벨벳 #RedVelvet\n#블루버스데이 #BLUEBIRTHDAY https://t.co/DG2zC9YKVW"
+	},
+	{
+		"created_at": "Wed Sep 01 02:29:39 +0000 2021",
+		"id": 1432885866871984000,
+		"id_str": "1432893383777951744",
+		"full_text": "LIM YOONA to appear in the upcoming tvN series ‘빅마우스’!\n“I was drawn by the wise and proactive character”\nTo play ‘고미호’, the nurse who strives to clear her husband’s name from a false accusation\n\n#LIMYOONA #임윤아 \n#GirlsGeneration #소녀시대 \n#빅마우스 https://t.co/qX6pyQFtlW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			240
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LIMYOONA",
+					"indices": [
+						195,
+						204
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						205,
+						209
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						211,
+						227
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						228,
+						233
+					]
+				},
+				{
+					"text": "빅마우스",
+					"indices": [
+						235,
+						240
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1432883410561683500,
+					"id_str": "1432883410561683458",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Kf3sMUUAITvAP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Kf3sMUUAITvAP.jpg",
+					"url": "https://t.co/qX6pyQFtlW",
+					"display_url": "pic.twitter.com/qX6pyQFtlW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1432885866871984130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2000,
+							"h": 1376,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 468,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 826,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1432883410561683500,
+					"id_str": "1432883410561683458",
+					"indices": [
+						241,
+						264
+					],
+					"media_url": "http://pbs.twimg.com/media/E-Kf3sMUUAITvAP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-Kf3sMUUAITvAP.jpg",
+					"url": "https://t.co/qX6pyQFtlW",
+					"display_url": "pic.twitter.com/qX6pyQFtlW",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1432885866871984130/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2000,
+							"h": 1376,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 468,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 826,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/vbjfZGuM9Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vbjfZGuM9Y",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9845893,
+			"friends_count": 55,
+			"listed_count": 10527,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 10710,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/925281250792824832/dfj3nYUe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1509443118",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1614,
+		"favorite_count": 4987,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "LIM YOONA to appear in the upcoming tvN series ‘빅마우스’!\n“I was drawn by the wise and proactive character”\nTo play ‘고미호’, the nurse who strives to clear her husband’s name from a false accusation\n\n#LIMYOONA #임윤아 \n#GirlsGeneration #소녀시대 \n#빅마우스 https://t.co/qX6pyQFtlW"
+	},
+	{
 		"created_at": "Wed Sep 01 01:45:37 +0000 2021",
 		"id": 1432879690088210400,
 		"id_str": "1432882301936062469",
-		"full_text": "《#빅이슈》 258호 #신간 발매✨\n\n[COVER STORY] 소녀들, 뛰어오르다 #이달의소녀🌙\n[SPECIAL] 해보자, 후회하지 말고🏃‍♀️💪\n[INTERVIEW] &lt;최선의 삶&gt; #이우정감독🎬\n\n📕구매하기▶https://t.co/eHtrcqTGB6\n\n#bigissue #빅이슈258호 #이달소 #LOONA #오빛 #nct성찬 #골때리는그녀들 #박은지 #마녀체력 https://t.co/M6YbNtqwIh",
+		"full_text": "《#빅이슈》 258호 #신간 발매✨\n\n[COVER STORY] 소녀들, 뛰어오르다 #이달의소녀🌙\n[SPECIAL] 해보자, 후회하지 말고🏃♀️💪\n[INTERVIEW] &lt;최선의 삶&gt; #이우정감독🎬\n\n📕구매하기▶https://t.co/eHtrcqTGB6\n\n#bigissue #빅이슈258호 #이달소 #LOONA #오빛 #nct성찬 #골때리는그녀들 #박은지 #마녀체력 https://t.co/M6YbNtqwIh",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -393,7 +959,7 @@ Grailbird.data.tweets_2021_09 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "《#빅이슈》 258호 #신간 발매✨\n\n[COVER STORY] 소녀들, 뛰어오르다 #이달의소녀🌙\n[SPECIAL] 해보자, 후회하지 말고🏃‍♀️💪\n[INTERVIEW] &lt;최선의 삶&gt; #이우정감독🎬\n\n📕구매하기▶https://t.co/eHtrcqTGB6\n\n#bigissue #빅이슈258호 #이달소 #LOONA #오빛 #nct성찬 #골때리는그녀들 #박은지 #마녀체력 https://t.co/M6YbNtqwIh"
+		"text": "《#빅이슈》 258호 #신간 발매✨\n\n[COVER STORY] 소녀들, 뛰어오르다 #이달의소녀🌙\n[SPECIAL] 해보자, 후회하지 말고🏃♀️💪\n[INTERVIEW] &lt;최선의 삶&gt; #이우정감독🎬\n\n📕구매하기▶https://t.co/eHtrcqTGB6\n\n#bigissue #빅이슈258호 #이달소 #LOONA #오빛 #nct성찬 #골때리는그녀들 #박은지 #마녀체력 https://t.co/M6YbNtqwIh"
 	},
 	{
 		"created_at": "Wed Sep 01 01:40:12 +0000 2021",
