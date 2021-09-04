4a7d43c07e2162bae0f656f9bbf6509281f7e404
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-04 18:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80286,
-	"created_at": "Sat Sep 04 08:02:02 +0000 2021",
+	"tweets": 80287,
+	"created_at": "Sat Sep 04 09:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 04 08:17:03 +0000 2021",
+		"id": 1434059396758192000,
+		"id_str": "1434067974206357508",
+		"full_text": "[#OliviaHye] 그리고 음식이 주는 힐링~~ 외국 오빛들 수수부꾸미 알아요? 한국에 오면 꼭 먹어 보세요 진짜 맛있어요 사실 저도 처음 먹어 봄ㅋㅡㅋ 한정식 최고\n\n#이달의소녀 #LOONA https://t.co/HIGVCzLxNV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OliviaHye",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						96,
+						102
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						103,
+						109
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1434059386926731300,
+					"id_str": "1434059386926731265",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNahiVEAEbLGz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNahiVEAEbLGz.jpg",
+					"url": "https://t.co/HIGVCzLxNV",
+					"display_url": "pic.twitter.com/HIGVCzLxNV",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059396758192128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1434059386926731300,
+					"id_str": "1434059386926731265",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNahiVEAEbLGz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNahiVEAEbLGz.jpg",
+					"url": "https://t.co/HIGVCzLxNV",
+					"display_url": "pic.twitter.com/HIGVCzLxNV",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059396758192128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059386909913000,
+					"id_str": "1434059386909913091",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNaheUcAM52X7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNaheUcAM52X7.jpg",
+					"url": "https://t.co/HIGVCzLxNV",
+					"display_url": "pic.twitter.com/HIGVCzLxNV",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059396758192128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059387530715100,
+					"id_str": "1434059387530715137",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNajyVIAEQeDW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNajyVIAEQeDW.jpg",
+					"url": "https://t.co/HIGVCzLxNV",
+					"display_url": "pic.twitter.com/HIGVCzLxNV",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059396758192128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1080,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059387530743800,
+					"id_str": "1434059387530743809",
+					"indices": [
+						110,
+						133
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNajyVkAEMC3f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNajyVkAEMC3f.jpg",
+					"url": "https://t.co/HIGVCzLxNV",
+					"display_url": "pic.twitter.com/HIGVCzLxNV",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059396758192128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1434059341175222300,
+		"in_reply_to_status_id_str": "1434059341175222272",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 803474,
+			"friends_count": 1,
+			"listed_count": 7884,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3236,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1595,
+		"favorite_count": 6095,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#OliviaHye] 그리고 음식이 주는 힐링~~ 외국 오빛들 수수부꾸미 알아요? 한국에 오면 꼭 먹어 보세요 진짜 맛있어요 사실 저도 처음 먹어 봄ㅋㅡㅋ 한정식 최고\n\n#이달의소녀 #LOONA https://t.co/HIGVCzLxNV"
+	},
+	{
 		"created_at": "Sat Sep 04 07:45:37 +0000 2021",
 		"id": 1434059341175222300,
 		"id_str": "1434060062872440832",
