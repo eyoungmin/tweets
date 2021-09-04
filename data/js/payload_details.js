b8e8294b296ec01feeb2c7289066215f199210df
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-04 17:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80285,
-	"created_at": "Sat Sep 04 06:02:01 +0000 2021",
+	"tweets": 80286,
+	"created_at": "Sat Sep 04 08:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sat Sep 04 07:45:37 +0000 2021",
+		"id": 1434059341175222300,
+		"id_str": "1434060062872440832",
+		"full_text": "[#OliviaHye] 자연이 주는 힐링🍂 500년 된 나무라고 들었어요 오빛들의 소원을 빌어요\n\n#이달의소녀 #LOONA https://t.co/ChYT4x579M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OliviaHye",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						62,
+						68
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1434059317078945800,
+					"id_str": "1434059317078945792",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNWdVUcAAFRly.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNWdVUcAAFRly.jpg",
+					"url": "https://t.co/ChYT4x579M",
+					"display_url": "pic.twitter.com/ChYT4x579M",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059341175222272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1434059317078945800,
+					"id_str": "1434059317078945792",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNWdVUcAAFRly.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNWdVUcAAFRly.jpg",
+					"url": "https://t.co/ChYT4x579M",
+					"display_url": "pic.twitter.com/ChYT4x579M",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059341175222272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059317079015400,
+					"id_str": "1434059317079015427",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNWdVVgAMLNpX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNWdVVgAMLNpX.jpg",
+					"url": "https://t.co/ChYT4x579M",
+					"display_url": "pic.twitter.com/ChYT4x579M",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059341175222272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059317938847700,
+					"id_str": "1434059317938847746",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNWgiVgAIXkPj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNWgiVgAIXkPj.jpg",
+					"url": "https://t.co/ChYT4x579M",
+					"display_url": "pic.twitter.com/ChYT4x579M",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059341175222272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2044,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1198,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434059317917864000,
+					"id_str": "1434059317917863943",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/E-bNWgdVUActpxO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-bNWgdVUActpxO.jpg",
+					"url": "https://t.co/ChYT4x579M",
+					"display_url": "pic.twitter.com/ChYT4x579M",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434059341175222272/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 580,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1023,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1746,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 803425,
+			"friends_count": 1,
+			"listed_count": 7884,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3236,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2950,
+		"favorite_count": 7376,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#OliviaHye] 자연이 주는 힐링🍂 500년 된 나무라고 들었어요 오빛들의 소원을 빌어요\n\n#이달의소녀 #LOONA https://t.co/ChYT4x579M"
+	},
+	{
 		"created_at": "Sat Sep 04 05:40:09 +0000 2021",
 		"id": 1434013310836379600,
 		"id_str": "1434028491100536832",
