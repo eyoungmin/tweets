3fa747676369de6faac73092de8ee3288b42d9fc
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-06 00:19:56 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,280 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 05 15:19:04 +0000 2021",
+		"id": 1434531779407343600,
+		"id_str": "1434536565968031750",
+		"full_text": "#LISA FIRST SINGLE ALBUM LALISA TRACKLIST POSTER\n\n#리사 #BLACKPINK #블랙핑크 #FIRSTSINGLEALBUM #LALISA #TRACKLIST #TITLE #LALISA #MONEY #20210910_12amEST #20210910_1pmKST #RELEASE #YG https://t.co/9qqJOGcimO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LISA",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						50,
+						53
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						65,
+						70
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						71,
+						88
+					]
+				},
+				{
+					"text": "LALISA",
+					"indices": [
+						89,
+						96
+					]
+				},
+				{
+					"text": "TRACKLIST",
+					"indices": [
+						97,
+						107
+					]
+				},
+				{
+					"text": "TITLE",
+					"indices": [
+						108,
+						114
+					]
+				},
+				{
+					"text": "LALISA",
+					"indices": [
+						115,
+						122
+					]
+				},
+				{
+					"text": "MONEY",
+					"indices": [
+						123,
+						129
+					]
+				},
+				{
+					"text": "20210910_12amEST",
+					"indices": [
+						130,
+						147
+					]
+				},
+				{
+					"text": "20210910_1pmKST",
+					"indices": [
+						148,
+						164
+					]
+				},
+				{
+					"text": "RELEASE",
+					"indices": [
+						165,
+						173
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						174,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1434531447688228900,
+					"id_str": "1434531447688228870",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/E-h6wE6VcAYJIO_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-h6wE6VcAYJIO_.jpg",
+					"url": "https://t.co/9qqJOGcimO",
+					"display_url": "pic.twitter.com/9qqJOGcimO",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1434531779407343616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1434531447688228900,
+					"id_str": "1434531447688228870",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/E-h6wE6VcAYJIO_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-h6wE6VcAYJIO_.jpg",
+					"url": "https://t.co/9qqJOGcimO",
+					"display_url": "pic.twitter.com/9qqJOGcimO",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1434531779407343616/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1620,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/aWhwrQsIZl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aWhwrQsIZl",
+							"expanded_url": "http://5th.blackpinkofficial.com",
+							"display_url": "5th.blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4625117,
+			"friends_count": 0,
+			"listed_count": 6468,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 374,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1629903714",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 58286,
+		"favorite_count": 95232,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "et",
+		"text": "#LISA FIRST SINGLE ALBUM LALISA TRACKLIST POSTER\n\n#리사 #BLACKPINK #블랙핑크 #FIRSTSINGLEALBUM #LALISA #TRACKLIST #TITLE #LALISA #MONEY #20210910_12amEST #20210910_1pmKST #RELEASE #YG https://t.co/9qqJOGcimO"
+	},
+	{
 		"created_at": "Sun Sep 05 13:48:50 +0000 2021",
 		"id": 1434513858253791200,
 		"id_str": "1434513858253791232",
