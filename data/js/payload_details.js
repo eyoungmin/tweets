699e39f3e8ad4d86106544d8d5f1807ff70dec43
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-06 22:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80333,
-	"created_at": "Mon Sep 06 11:02:03 +0000 2021",
+	"tweets": 80334,
+	"created_at": "Mon Sep 06 13:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,311 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Mon Sep 06 12:17:34 +0000 2021",
+		"id": 1434848148761952300,
+		"id_str": "1434853277770731527",
+		"full_text": "[#ViVi] 귀여미들🥰❤\n\nCuties 🥰❤ \n\n#이달의소녀 #LOONA https://t.co/GDbcEqUWx9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ViVi",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						28,
+						34
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						35,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1434848106609197000,
+					"id_str": "1434848106609197060",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E-mawDUVQAQl917.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-mawDUVQAQl917.jpg",
+					"url": "https://t.co/GDbcEqUWx9",
+					"display_url": "pic.twitter.com/GDbcEqUWx9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434848148761952261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 811,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1434848106609197000,
+					"id_str": "1434848106609197060",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E-mawDUVQAQl917.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-mawDUVQAQl917.jpg",
+					"url": "https://t.co/GDbcEqUWx9",
+					"display_url": "pic.twitter.com/GDbcEqUWx9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434848148761952261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 811,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434848106768572400,
+					"id_str": "1434848106768572424",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E-mawD6VIAgCyyx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-mawD6VIAgCyyx.jpg",
+					"url": "https://t.co/GDbcEqUWx9",
+					"display_url": "pic.twitter.com/GDbcEqUWx9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434848148761952261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 811,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434848106651132000,
+					"id_str": "1434848106651131910",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E-mawDeVIAYOf9o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-mawDeVIAYOf9o.jpg",
+					"url": "https://t.co/GDbcEqUWx9",
+					"display_url": "pic.twitter.com/GDbcEqUWx9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434848148761952261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 811,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1434848106667917300,
+					"id_str": "1434848106667917312",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/E-mawDiVQAAQet9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-mawDiVQAAQet9.jpg",
+					"url": "https://t.co/GDbcEqUWx9",
+					"display_url": "pic.twitter.com/GDbcEqUWx9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1434848148761952261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 811,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 804407,
+			"friends_count": 1,
+			"listed_count": 7879,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3241,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6639,
+		"favorite_count": 19213,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#ViVi] 귀여미들🥰❤\n\nCuties 🥰❤ \n\n#이달의소녀 #LOONA https://t.co/GDbcEqUWx9"
+	},
+	{
 		"created_at": "Mon Sep 06 10:19:17 +0000 2021",
 		"id": 1434818659587870700,
 		"id_str": "1434823510363238406",
