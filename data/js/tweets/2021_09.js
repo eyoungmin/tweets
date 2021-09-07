23ed413a56a41f03b4dd0dfe41452791d090da3b
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-07 16:02:03 +0900

    Added 2 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,430 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Tue Sep 07 06:16:31 +0000 2021",
+		"id": 1435122620161089500,
+		"id_str": "1435124804240109571",
+		"full_text": "TAEMIN Object Set SALES NOTICE\n\n▶ Sales Day\n2021. 09. 07 (TUE) 15:00 ~ [KST]  \n\n▶ Online Store \nSMTOWN &amp;STORE : https://t.co/qPFqhm1YuR\nSM Global Shop : https://t.co/pHPBYdYbF5\nYIZHIYU : https://t.co/colKSXrqmq https://t.co/tfAzPbUXMv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qPFqhm1YuR",
+					"expanded_url": "https://smtownandstore.com/",
+					"display_url": "smtownandstore.com",
+					"indices": [
+						116,
+						139
+					]
+				},
+				{
+					"url": "https://t.co/pHPBYdYbF5",
+					"expanded_url": "http://smglobalshop.com/",
+					"display_url": "smglobalshop.com",
+					"indices": [
+						157,
+						180
+					]
+				},
+				{
+					"url": "https://t.co/colKSXrqmq",
+					"expanded_url": "https://www.withfans.com/web/index.html#/",
+					"display_url": "withfans.com/web/index.html…",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1435122615987806200,
+					"id_str": "1435122615987806210",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/E-qUamcVIAI5kS_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-qUamcVIAI5kS_.jpg",
+					"url": "https://t.co/tfAzPbUXMv",
+					"display_url": "pic.twitter.com/tfAzPbUXMv",
+					"expanded_url": "https://twitter.com/SHINee/status/1435122620161089539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 396,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 675,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 224,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1435122615987806200,
+					"id_str": "1435122615987806210",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/E-qUamcVIAI5kS_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-qUamcVIAI5kS_.jpg",
+					"url": "https://t.co/tfAzPbUXMv",
+					"display_url": "pic.twitter.com/tfAzPbUXMv",
+					"expanded_url": "https://twitter.com/SHINee/status/1435122620161089539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 396,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 675,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 224,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "KEY 키 The 1st Mini Album ‘BAD LOVE’",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2088494,
+			"friends_count": 2,
+			"listed_count": 5376,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 64,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1987,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1434531843987017733/tV0LKcbZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1434531843987017733/tV0LKcbZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1630854020",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2505,
+		"favorite_count": 5876,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TAEMIN Object Set SALES NOTICE\n\n▶ Sales Day\n2021. 09. 07 (TUE) 15:00 ~ [KST]  \n\n▶ Online Store \nSMTOWN &amp;STORE : https://t.co/qPFqhm1YuR\nSM Global Shop : https://t.co/pHPBYdYbF5\nYIZHIYU : https://t.co/colKSXrqmq https://t.co/tfAzPbUXMv"
+	},
+	{
+		"created_at": "Tue Sep 07 06:16:31 +0000 2021",
+		"id": 1435121509622685700,
+		"id_str": "1435124804235902976",
+		"full_text": "TAEMIN Object Set Making Film\n\n🎬 https://t.co/CBjk9B2bRQ\n\n#TAEMIN #태민 \n#SHINee #샤이니 \n#ObjectSet https://t.co/s1D9XqyLNy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TAEMIN",
+					"indices": [
+						58,
+						65
+					]
+				},
+				{
+					"text": "태민",
+					"indices": [
+						66,
+						69
+					]
+				},
+				{
+					"text": "SHINee",
+					"indices": [
+						71,
+						78
+					]
+				},
+				{
+					"text": "샤이니",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "ObjectSet",
+					"indices": [
+						85,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CBjk9B2bRQ",
+					"expanded_url": "https://youtu.be/ZnrjagFjd6I",
+					"display_url": "youtu.be/ZnrjagFjd6I",
+					"indices": [
+						33,
+						56
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1435121502387536000,
+					"id_str": "1435121502387535873",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/E-qTZx9VgAEhV5-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-qTZx9VgAEhV5-.jpg",
+					"url": "https://t.co/s1D9XqyLNy",
+					"display_url": "pic.twitter.com/s1D9XqyLNy",
+					"expanded_url": "https://twitter.com/SHINee/status/1435121509622685700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1435121502387536000,
+					"id_str": "1435121502387535873",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/E-qTZx9VgAEhV5-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-qTZx9VgAEhV5-.jpg",
+					"url": "https://t.co/s1D9XqyLNy",
+					"display_url": "pic.twitter.com/s1D9XqyLNy",
+					"expanded_url": "https://twitter.com/SHINee/status/1435121509622685700/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 887973863824306200,
+			"id_str": "887973863824306176",
+			"name": "SHINee",
+			"screen_name": "SHINee",
+			"location": "SHINeeisBACK",
+			"description": "KEY 키 The 1st Mini Album ‘BAD LOVE’",
+			"url": "https://t.co/vhjnrVMUsq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vhjnrVMUsq",
+							"expanded_url": "https://www.youtube.com/SHINee",
+							"display_url": "youtube.com/SHINee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2088494,
+			"friends_count": 2,
+			"listed_count": 5376,
+			"created_at": "Thu Jul 20 09:54:01 +0000 2017",
+			"favourites_count": 64,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1987,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1434531843987017733/tV0LKcbZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1434531843987017733/tV0LKcbZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/887973863824306176/1630854020",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3626,
+		"favorite_count": 7679,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TAEMIN Object Set Making Film\n\n🎬 https://t.co/CBjk9B2bRQ\n\n#TAEMIN #태민 \n#SHINee #샤이니 \n#ObjectSet https://t.co/s1D9XqyLNy"
+	},
+	{
 		"created_at": "Tue Sep 07 05:49:48 +0000 2021",
 		"id": 1435118082683441200,
 		"id_str": "1435118082683441152",
