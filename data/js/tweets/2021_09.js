b354aebc2bc654b5383a6e2ce9be39991c11d9b7
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-13 07:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 12 21:14:17 +0000 2021",
+		"id": 1437162674534637600,
+		"id_str": "1437162674534637571",
+		"full_text": "#청하 #CHUNGHA \n[청하] 잘자~ 알라뷰❤️ \nhttps://t.co/GAEtGLWqGw \n \n#다음팬카페 #공식팬카페 #프롬글 https://t.co/pk2N8gfR8F",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			75
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"text": "다음팬카페",
+					"indices": [
+						57,
+						63
+					]
+				},
+				{
+					"text": "공식팬카페",
+					"indices": [
+						64,
+						70
+					]
+				},
+				{
+					"text": "프롬글",
+					"indices": [
+						71,
+						75
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GAEtGLWqGw",
+					"expanded_url": "https://cafe.daum.net/MNH-Chungha/g3oh/227",
+					"display_url": "cafe.daum.net/MNH-Chungha/g3…",
+					"indices": [
+						30,
+						53
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1437162672844329000,
+					"id_str": "1437162672844328968",
+					"indices": [
+						76,
+						99
+					],
+					"media_url": "http://pbs.twimg.com/media/E_HT1idWUAg9Ob2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_HT1idWUAg9Ob2.jpg",
+					"url": "https://t.co/pk2N8gfR8F",
+					"display_url": "pic.twitter.com/pk2N8gfR8F",
+					"expanded_url": "https://twitter.com/fromminn/status/1437162674534637571/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1437162672844329000,
+					"id_str": "1437162672844328968",
+					"indices": [
+						76,
+						99
+					],
+					"media_url": "http://pbs.twimg.com/media/E_HT1idWUAg9Ob2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_HT1idWUAg9Ob2.jpg",
+					"url": "https://t.co/pk2N8gfR8F",
+					"display_url": "pic.twitter.com/pk2N8gfR8F",
+					"expanded_url": "https://twitter.com/fromminn/status/1437162674534637571/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 768,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 316,
+			"friends_count": 1051,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44860,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 84901,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#청하 #CHUNGHA \n[청하] 잘자~ 알라뷰❤️ \nhttps://t.co/GAEtGLWqGw \n \n#다음팬카페 #공식팬카페 #프롬글 https://t.co/pk2N8gfR8F"
+	},
+	{
 		"created_at": "Sun Sep 12 17:40:09 +0000 2021",
 		"id": 1437105987614347300,
 		"id_str": "1437108787941347339",
