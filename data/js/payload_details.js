266642d76d0cfd78d6be9a3b3f5803dc8265f9e4
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-14 09:03:14 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80589,
-	"created_at": "Mon Sep 13 18:02:08 +0000 2021",
+	"tweets": 80590,
+	"created_at": "Tue Sep 14 00:03:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Mon Sep 13 23:40:10 +0000 2021",
+		"id": 1437556924854247400,
+		"id_str": "1437561773322874881",
+		"full_text": "My Universe // Coldplay X BTS // Exclusive song preview on TikTok now https://t.co/DPuYw9mFRQ https://t.co/oDRPcObPcs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DPuYw9mFRQ",
+					"expanded_url": "https://vm.tiktok.com/ZMR9PH2FR/",
+					"display_url": "vm.tiktok.com/ZMR9PH2FR/",
+					"indices": [
+						70,
+						93
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1437556920148275200,
+					"id_str": "1437556920148275204",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E_M6ZwHXsAQVanD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_M6ZwHXsAQVanD.jpg",
+					"url": "https://t.co/oDRPcObPcs",
+					"display_url": "pic.twitter.com/oDRPcObPcs",
+					"expanded_url": "https://twitter.com/coldplay/status/1437556924854247425/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 621,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1060,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 352,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1437556920148275200,
+					"id_str": "1437556920148275204",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E_M6ZwHXsAQVanD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_M6ZwHXsAQVanD.jpg",
+					"url": "https://t.co/oDRPcObPcs",
+					"display_url": "pic.twitter.com/oDRPcObPcs",
+					"expanded_url": "https://twitter.com/coldplay/status/1437556924854247425/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 621,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1060,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 352,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Coloratura",
+			"description": "The official Twitter of the band Coldplay. New album, Music Of The Spheres, out October 15th, 2021.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23044894,
+			"friends_count": 769,
+			"listed_count": 42683,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1333,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8648,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 28598,
+		"favorite_count": 51690,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My Universe // Coldplay X BTS // Exclusive song preview on TikTok now https://t.co/DPuYw9mFRQ https://t.co/oDRPcObPcs"
+	},
+	{
 		"created_at": "Mon Sep 13 17:21:36 +0000 2021",
 		"id": 1437452886405959700,
 		"id_str": "1437466504736358402",
