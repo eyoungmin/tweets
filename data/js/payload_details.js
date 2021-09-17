9cc3ed59f1de1338d788e9231d11bedd7c121ae8
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-17 09:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80749,
-	"created_at": "Thu Sep 16 23:02:09 +0000 2021",
+	"tweets": 80750,
+	"created_at": "Fri Sep 17 00:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Thu Sep 16 23:52:23 +0000 2021",
+		"id": 1437342665591296000,
+		"id_str": "1438652014586593285",
+		"full_text": "싱글 ‘Dive Baby, Dive’가 발매되었습니다. 링크를 통해 뮤직비디오 풀버전을 확인해보세요!\n\nhttps://t.co/U69ejXnQVo\n\nNew Single ‘Dive Baby, Dive’ is now available on every streaming platform! Check the Music video through the link.\n\n#글렌체크 #GlenCheck\n#DiveBabyDive #EMA https://t.co/LDRUynS8AV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			234
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "글렌체크",
+					"indices": [
+						199,
+						204
+					]
+				},
+				{
+					"text": "GlenCheck",
+					"indices": [
+						205,
+						215
+					]
+				},
+				{
+					"text": "DiveBabyDive",
+					"indices": [
+						216,
+						229
+					]
+				},
+				{
+					"text": "EMA",
+					"indices": [
+						230,
+						234
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U69ejXnQVo",
+					"expanded_url": "https://youtu.be/ylakJHYCp4I",
+					"display_url": "youtu.be/ylakJHYCp4I",
+					"indices": [
+						58,
+						81
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1437341078357639200,
+					"id_str": "1437341078357639170",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1437341078357639170/pu/img/iy4dh_fuHETDBvi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1437341078357639170/pu/img/iy4dh_fuHETDBvi_.jpg",
+					"url": "https://t.co/LDRUynS8AV",
+					"display_url": "pic.twitter.com/LDRUynS8AV",
+					"expanded_url": "https://twitter.com/bandglencheck/status/1437342665591296004/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1437341078357639200,
+					"id_str": "1437341078357639170",
+					"indices": [
+						235,
+						258
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1437341078357639170/pu/img/iy4dh_fuHETDBvi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1437341078357639170/pu/img/iy4dh_fuHETDBvi_.jpg",
+					"url": "https://t.co/LDRUynS8AV",
+					"display_url": "pic.twitter.com/LDRUynS8AV",
+					"expanded_url": "https://twitter.com/bandglencheck/status/1437342665591296004/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 29863,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1437341078357639170/pu/vid/1280x720/4txe03xw0_lNGOIO.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1437341078357639170/pu/pl/CBpTgTLxT-sv8rwj.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1437341078357639170/pu/vid/640x360/Ri6xM_34ljSARmVH.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1437341078357639170/pu/vid/480x270/nJUuVY0Sa72_2f3d.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 258798345,
+			"id_str": "258798345",
+			"name": "Glen Check",
+			"screen_name": "bandglencheck",
+			"location": "Seoul, KR",
+			"description": "Glen Check Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5027,
+			"friends_count": 0,
+			"listed_count": 108,
+			"created_at": "Mon Feb 28 14:36:33 +0000 2011",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1433352828618043395/HU3cgWIp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1433352828618043395/HU3cgWIp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/258798345/1631004834",
+			"profile_link_color": "A23AAB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DCF5E3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 107,
+		"favorite_count": 124,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "싱글 ‘Dive Baby, Dive’가 발매되었습니다. 링크를 통해 뮤직비디오 풀버전을 확인해보세요!\n\nhttps://t.co/U69ejXnQVo\n\nNew Single ‘Dive Baby, Dive’ is now available on every streaming platform! Check the Music video through the link.\n\n#글렌체크 #GlenCheck\n#DiveBabyDive #EMA https://t.co/LDRUynS8AV"
+	},
+	{
 		"created_at": "Thu Sep 16 22:40:10 +0000 2021",
 		"id": 1438620802782539800,
 		"id_str": "1438633837706326018",
