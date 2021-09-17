7d8ad2a484fcde62637d174bf8482d584e4e6869
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-17 16:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80777,
-	"created_at": "Fri Sep 17 06:02:07 +0000 2021",
+	"tweets": 80778,
+	"created_at": "Fri Sep 17 07:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 17 06:40:10 +0000 2021",
+		"id": 1438715782821601300,
+		"id_str": "1438754633644711943",
+		"full_text": "오늘(17일) 오후 1시 롤링스톤 코리아 웹진을 통해 신곡 ‘LALISA’로 새로운 역사를 쓰고 있는 블랙핑크 리사의 인터뷰가 공개됐습니다.\n\n리사의 인터뷰 전문은 link를 통해 확인해 주세요!\n▶️https://t.co/DhkHItgZvy\n\n #RollingStone #RollingStoneKorea #RSK #리사 #lisa #lalisa \n@BLACKPINK https://t.co/La4REpjlC8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RollingStone",
+					"indices": [
+						138,
+						151
+					]
+				},
+				{
+					"text": "RollingStoneKorea",
+					"indices": [
+						152,
+						170
+					]
+				},
+				{
+					"text": "RSK",
+					"indices": [
+						171,
+						175
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						176,
+						179
+					]
+				},
+				{
+					"text": "lisa",
+					"indices": [
+						180,
+						185
+					]
+				},
+				{
+					"text": "lalisa",
+					"indices": [
+						186,
+						193
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BLACKPINK",
+					"name": "BLACKPINKOFFICIAL",
+					"id": 1273850774380884000,
+					"id_str": "1273850774380883975",
+					"indices": [
+						195,
+						205
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/DhkHItgZvy",
+					"expanded_url": "http://bit.ly/2XpX1sF",
+					"display_url": "bit.ly/2XpX1sF",
+					"indices": [
+						112,
+						135
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1438715776572149800,
+					"id_str": "1438715776572149767",
+					"indices": [
+						206,
+						229
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dYYEtVUAc0hdM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dYYEtVUAc0hdM.jpg",
+					"url": "https://t.co/La4REpjlC8",
+					"display_url": "pic.twitter.com/La4REpjlC8",
+					"expanded_url": "https://twitter.com/rollingstonekor/status/1438715782821601282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438715776572149800,
+					"id_str": "1438715776572149767",
+					"indices": [
+						206,
+						229
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dYYEtVUAc0hdM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dYYEtVUAc0hdM.jpg",
+					"url": "https://t.co/La4REpjlC8",
+					"display_url": "pic.twitter.com/La4REpjlC8",
+					"expanded_url": "https://twitter.com/rollingstonekor/status/1438715782821601282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 960,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1438715776572149800,
+					"id_str": "1438715776572149764",
+					"indices": [
+						206,
+						229
+					],
+					"media_url": "http://pbs.twimg.com/media/E_dYYEtVUAQj9om.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_dYYEtVUAQj9om.jpg",
+					"url": "https://t.co/La4REpjlC8",
+					"display_url": "pic.twitter.com/La4REpjlC8",
+					"expanded_url": "https://twitter.com/rollingstonekor/status/1438715782821601282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1440,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1342798927364427800,
+			"id_str": "1342798927364427776",
+			"name": "Rolling Stone Korea",
+			"screen_name": "rollingstonekor",
+			"location": "",
+			"description": "The number one brand in music publishing launching in Korea.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4447,
+			"friends_count": 40,
+			"listed_count": 7,
+			"created_at": "Sat Dec 26 11:46:38 +0000 2020",
+			"favourites_count": 289,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 189,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1342800452841193473/HLjoiuMx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1342800452841193473/HLjoiuMx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1342798927364427776/1608983636",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2390,
+		"favorite_count": 7054,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "오늘(17일) 오후 1시 롤링스톤 코리아 웹진을 통해 신곡 ‘LALISA’로 새로운 역사를 쓰고 있는 블랙핑크 리사의 인터뷰가 공개됐습니다.\n\n리사의 인터뷰 전문은 link를 통해 확인해 주세요!\n▶️https://t.co/DhkHItgZvy\n\n #RollingStone #RollingStoneKorea #RSK #리사 #lisa #lalisa \n@BLACKPINK https://t.co/La4REpjlC8"
+	},
+	{
 		"created_at": "Fri Sep 17 05:40:10 +0000 2021",
 		"id": 1438736101510836200,
 		"id_str": "1438739533827354626",
