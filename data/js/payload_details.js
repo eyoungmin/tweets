f20dc83c3aab90bffc91fb8891f2d12eb2d276fa
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-17 23:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80795,
-	"created_at": "Fri Sep 17 13:02:08 +0000 2021",
+	"tweets": 80796,
+	"created_at": "Fri Sep 17 14:02:07 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Fri Sep 17 13:22:35 +0000 2021",
+		"id": 1438851477368361000,
+		"id_str": "1438855906662309893",
+		"full_text": "evergreen official music video premieres at 5pm bst. \nset a reminder below ✨\nhttps://t.co/fRFfJ0bKUb https://t.co/KYhkMEa3JV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fRFfJ0bKUb",
+					"expanded_url": "http://youtu.be/vNAeyve9fLI",
+					"display_url": "youtu.be/vNAeyve9fLI",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1438851449610514400,
+					"id_str": "1438851449610514437",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/E_fTxSKXMAUza6q.png",
+					"media_url_https": "https://pbs.twimg.com/media/E_fTxSKXMAUza6q.png",
+					"url": "https://t.co/KYhkMEa3JV",
+					"display_url": "pic.twitter.com/KYhkMEa3JV",
+					"expanded_url": "https://twitter.com/birdy/status/1438851477368360963/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 620,
+							"h": 803,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 803,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 525,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1438851449610514400,
+					"id_str": "1438851449610514437",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/E_fTxSKXMAUza6q.png",
+					"media_url_https": "https://pbs.twimg.com/media/E_fTxSKXMAUza6q.png",
+					"url": "https://t.co/KYhkMEa3JV",
+					"display_url": "pic.twitter.com/KYhkMEa3JV",
+					"expanded_url": "https://twitter.com/birdy/status/1438851477368360963/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 620,
+							"h": 803,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 620,
+							"h": 803,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 525,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270901752,
+			"id_str": "270901752",
+			"name": "Birdy",
+			"screen_name": "birdy",
+			"location": "",
+			"description": "Young Heart - my new album, OUT NOW 💛 \nhttps://t.co/JJysUdcZGQ",
+			"url": "https://t.co/AOWSyI9JYu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AOWSyI9JYu",
+							"expanded_url": "http://officialbirdy.com",
+							"display_url": "officialbirdy.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/JJysUdcZGQ",
+							"expanded_url": "http://birdy.lnk.to/Young-Heart",
+							"display_url": "birdy.lnk.to/Young-Heart",
+							"indices": [
+								39,
+								62
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 315013,
+			"friends_count": 179,
+			"listed_count": 1185,
+			"created_at": "Wed Mar 23 13:25:01 +0000 2011",
+			"favourites_count": 851,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2355,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1352526552010027009/XJy4RlYX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270901752/1619760432",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 221,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "evergreen official music video premieres at 5pm bst. \nset a reminder below ✨\nhttps://t.co/fRFfJ0bKUb https://t.co/KYhkMEa3JV"
+	},
+	{
 		"created_at": "Fri Sep 17 12:33:33 +0000 2021",
 		"id": 1438843566332203000,
 		"id_str": "1438843566332203010",
