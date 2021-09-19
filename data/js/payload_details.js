2e30cb98d1c6c4acee79ca559110861d63c78bf8
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-20 00:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80842,
-	"created_at": "Sun Sep 19 14:02:09 +0000 2021",
+	"tweets": 80843,
+	"created_at": "Sun Sep 19 15:02:08 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,276 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Sun Sep 19 14:40:10 +0000 2021",
+		"id": 1439587576994615300,
+		"id_str": "1439600205456945156",
+		"full_text": "2021.09.18 📸\n단독 컷 먼저 출근! https://t.co/wCUouoXdcT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1439587556610375700,
+					"id_str": "1439587556610375682",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/E_pxQWbVgAIBmtV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_pxQWbVgAIBmtV.jpg",
+					"url": "https://t.co/wCUouoXdcT",
+					"display_url": "pic.twitter.com/wCUouoXdcT",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1439587576994615303/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1439587556610375700,
+					"id_str": "1439587556610375682",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/E_pxQWbVgAIBmtV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_pxQWbVgAIBmtV.jpg",
+					"url": "https://t.co/wCUouoXdcT",
+					"display_url": "pic.twitter.com/wCUouoXdcT",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1439587576994615303/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1439587556614557700,
+					"id_str": "1439587556614557699",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/E_pxQWcVUAMcwVo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_pxQWcVUAMcwVo.jpg",
+					"url": "https://t.co/wCUouoXdcT",
+					"display_url": "pic.twitter.com/wCUouoXdcT",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1439587576994615303/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1439587556610297900,
+					"id_str": "1439587556610297857",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/E_pxQWbUUAE-4sY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_pxQWbUUAE-4sY.jpg",
+					"url": "https://t.co/wCUouoXdcT",
+					"display_url": "pic.twitter.com/wCUouoXdcT",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1439587576994615303/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1439587556627140600,
+					"id_str": "1439587556627140614",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/E_pxQWfVUAYu3dc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_pxQWfVUAYu3dc.jpg",
+					"url": "https://t.co/wCUouoXdcT",
+					"display_url": "pic.twitter.com/wCUouoXdcT",
+					"expanded_url": "https://twitter.com/yeseoofficial/status/1439587576994615303/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 893011871095181300,
+			"id_str": "893011871095181312",
+			"name": "YESEO",
+			"screen_name": "yeseoofficial",
+			"location": "대한민국 서울",
+			"description": "Producer / Singer",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 807,
+			"friends_count": 6,
+			"listed_count": 22,
+			"created_at": "Thu Aug 03 07:33:16 +0000 2017",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 136,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1427208161912528905/Yhh9ewCe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1427208161912528905/Yhh9ewCe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/893011871095181312/1629637585",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2021.09.18 📸\n단독 컷 먼저 출근! https://t.co/wCUouoXdcT"
+	},
+	{
 		"created_at": "Sun Sep 19 13:40:09 +0000 2021",
 		"id": 1439583396267761700,
 		"id_str": "1439585105450881031",
