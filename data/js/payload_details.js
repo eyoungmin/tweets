876dd665a3ac2e337647a372d8e34f0a8a214d97
Youngmin Lee <iameyoungmin@gmail.com>
2021-09-21 20:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 80883,
-	"created_at": "Tue Sep 21 10:02:06 +0000 2021",
+	"tweets": 80884,
+	"created_at": "Tue Sep 21 11:02:08 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_09.js
+++ data/js/tweets/2021_09.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2021_09 = 
 [
 	{
+		"created_at": "Tue Sep 21 10:20:48 +0000 2021",
+		"id": 1440254914232340500,
+		"id_str": "1440259708955230210",
+		"full_text": "오빛💫 잠시 후 오후 7시 30분(KST) #이달의소녀 #츄 가 출연한 MBC '호적 메이트'가 방송됩니다🥰 추석에도 본방사수하면서 함께해 츄세요❤️\n\n#LOONA #Chuu https://t.co/SVmijkCtQ4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						24,
+						30
+					]
+				},
+				{
+					"text": "츄",
+					"indices": [
+						31,
+						33
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						85,
+						91
+					]
+				},
+				{
+					"text": "Chuu",
+					"indices": [
+						92,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1440254827112534000,
+					"id_str": "1440254827112534022",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/E_zQImwVkAY1kJs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_zQImwVkAY1kJs.jpg",
+					"url": "https://t.co/SVmijkCtQ4",
+					"display_url": "pic.twitter.com/SVmijkCtQ4",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1440254914232340484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1440254827112534000,
+					"id_str": "1440254827112534022",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/E_zQImwVkAY1kJs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_zQImwVkAY1kJs.jpg",
+					"url": "https://t.co/SVmijkCtQ4",
+					"display_url": "pic.twitter.com/SVmijkCtQ4",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1440254914232340484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1440254827120889900,
+					"id_str": "1440254827120889869",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/E_zQImyVEA0G9pP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E_zQImyVEA0G9pP.jpg",
+					"url": "https://t.co/SVmijkCtQ4",
+					"display_url": "pic.twitter.com/SVmijkCtQ4",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1440254914232340484/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 810754,
+			"friends_count": 1,
+			"listed_count": 7902,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3315,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3091,
+		"favorite_count": 9316,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛💫 잠시 후 오후 7시 30분(KST) #이달의소녀 #츄 가 출연한 MBC '호적 메이트'가 방송됩니다🥰 추석에도 본방사수하면서 함께해 츄세요❤️\n\n#LOONA #Chuu https://t.co/SVmijkCtQ4"
+	},
+	{
 		"created_at": "Tue Sep 21 09:40:10 +0000 2021",
 		"id": 1440239477805314000,
 		"id_str": "1440249483334340611",
