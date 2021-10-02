add0152563de2d1e2e4789e13c7e68a749bb7691
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-03 04:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81300,
-	"created_at": "Sat Oct 02 17:02:00 +0000 2021",
+	"tweets": 81301,
+	"created_at": "Sat Oct 02 19:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,10 +1,242 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 02 18:40:09 +0000 2021",
+		"id": 1444368299245113300,
+		"id_str": "1444371644957536258",
+		"full_text": "✨ // #MusicOfTheSpheres // The new album, October 15th // Pre-order now https://t.co/ENpHKgXtHW https://t.co/KLoA8JugGf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MusicOfTheSpheres",
+					"indices": [
+						5,
+						23
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ENpHKgXtHW",
+					"expanded_url": "https://coldplay.lnk.to/MOTS",
+					"display_url": "coldplay.lnk.to/MOTS",
+					"indices": [
+						72,
+						95
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1444366807507288000,
+					"id_str": "1444366807507288070",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/FAtsvJvVUAAPFZq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAtsvJvVUAAPFZq.jpg",
+					"url": "https://t.co/KLoA8JugGf",
+					"display_url": "pic.twitter.com/KLoA8JugGf",
+					"expanded_url": "https://twitter.com/coldplay/status/1444368299245113351/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1444366807507288000,
+					"id_str": "1444366807507288070",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/media/FAtsvJvVUAAPFZq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FAtsvJvVUAAPFZq.jpg",
+					"url": "https://t.co/KLoA8JugGf",
+					"display_url": "pic.twitter.com/KLoA8JugGf",
+					"expanded_url": "https://twitter.com/coldplay/status/1444368299245113351/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 15040,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1444366807507288070/pl/fksWStZ0_ZnfCNjs.m3u8?tag=14"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1444366807507288070/vid/320x320/X_rz9_bZvZA94Z_p.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1444366807507288070/vid/540x540/wnpqxn5C_lDpaI8v.mp4?tag=14"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1444366807507288070/vid/720x720/f_POe1qfstTuSHnG.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://coldplay.lnk.to/MOTS"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Epiphane",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out October 15th, 2021. New single My Universe with BTS out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23356975,
+			"friends_count": 772,
+			"listed_count": 42749,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1351,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8705,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1249,
+		"favorite_count": 8393,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "✨ // #MusicOfTheSpheres // The new album, October 15th // Pre-order now https://t.co/ENpHKgXtHW https://t.co/KLoA8JugGf"
+	},
+	{
 		"created_at": "Sat Oct 02 16:23:26 +0000 2021",
 		"id": 1444330751554588700,
 		"id_str": "1444337238439960580",
-		"full_text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻‍♀️👩🏻‍🎤\n\n#이달의소녀 #LOONA https://t.co/Ux7byYX5D6",
+		"full_text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻♀️👩🏻🎤\n\n#이달의소녀 #LOONA https://t.co/Ux7byYX5D6",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -238,13 +470,13 @@ Grailbird.data.tweets_2021_10 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻‍♀️👩🏻‍🎤\n\n#이달의소녀 #LOONA https://t.co/Ux7byYX5D6"
+		"text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻♀️👩🏻🎤\n\n#이달의소녀 #LOONA https://t.co/Ux7byYX5D6"
 	},
 	{
 		"created_at": "Sat Oct 02 16:23:26 +0000 2021",
 		"id": 1444330698714730500,
 		"id_str": "1444337238070861826",
-		"full_text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻‍♀️👩🏻‍🎤\n\n#이달의소녀 #LOONA https://t.co/3kJB2sYIDo",
+		"full_text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻♀️👩🏻🎤\n\n#이달의소녀 #LOONA https://t.co/3kJB2sYIDo",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -514,7 +746,7 @@ Grailbird.data.tweets_2021_10 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻‍♀️👩🏻‍🎤\n\n#이달의소녀 #LOONA https://t.co/3kJB2sYIDo"
+		"text": "[#고원 / #GoWon] 우㉣ㅣ는 넘㉧ㅓ지지 않ㅇr...☆ 🛼🏃🏻♀️👩🏻🎤\n\n#이달의소녀 #LOONA https://t.co/3kJB2sYIDo"
 	},
 	{
 		"created_at": "Sat Oct 02 15:26:48 +0000 2021",
