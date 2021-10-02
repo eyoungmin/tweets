3ebd4d4a8929fbb005f6800ede0f6fbf206ba3a5
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-02 18:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81288,
-	"created_at": "Sat Oct 02 07:02:00 +0000 2021",
+	"tweets": 81289,
+	"created_at": "Sat Oct 02 09:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 02 08:26:44 +0000 2021",
+		"id": 1444210686238679000,
+		"id_str": "1444217270725582850",
+		"full_text": "SYNK DIVE: aespa ‘Savage’ SHOWCASE\n➫2021.10.5 8PM KST\n\n📍aespa YouTube Channel\nhttps://t.co/NsLU32lrBC\n \n#aespa #æspa #에스파\n#Savage #SYNKDIVE https://t.co/rqOZwKRoCf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			139
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						104,
+						110
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						111,
+						116
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "Savage",
+					"indices": [
+						122,
+						129
+					]
+				},
+				{
+					"text": "SYNKDIVE",
+					"indices": [
+						130,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NsLU32lrBC",
+					"expanded_url": "http://youtube.com/c/aespa",
+					"display_url": "youtube.com/c/aespa",
+					"indices": [
+						78,
+						101
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1444210676398899200,
+					"id_str": "1444210676398899203",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/FArd9VuVQAMI3EP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FArd9VuVQAMI3EP.jpg",
+					"url": "https://t.co/rqOZwKRoCf",
+					"display_url": "pic.twitter.com/rqOZwKRoCf",
+					"expanded_url": "https://twitter.com/aespa_official/status/1444210686238679042/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1444210676398899200,
+					"id_str": "1444210676398899203",
+					"indices": [
+						140,
+						163
+					],
+					"media_url": "http://pbs.twimg.com/media/FArd9VuVQAMI3EP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FArd9VuVQAMI3EP.jpg",
+					"url": "https://t.co/rqOZwKRoCf",
+					"display_url": "pic.twitter.com/rqOZwKRoCf",
+					"expanded_url": "https://twitter.com/aespa_official/status/1444210686238679042/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Savage",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1563305,
+			"friends_count": 1,
+			"listed_count": 5719,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 882,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1437430875566710784/8vJwI1Ey_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1437430875566710784/8vJwI1Ey_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1631545203",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12023,
+		"favorite_count": 26957,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SYNK DIVE: aespa ‘Savage’ SHOWCASE\n➫2021.10.5 8PM KST\n\n📍aespa YouTube Channel\nhttps://t.co/NsLU32lrBC\n \n#aespa #æspa #에스파\n#Savage #SYNKDIVE https://t.co/rqOZwKRoCf"
+	},
+	{
 		"created_at": "Sat Oct 02 06:05:41 +0000 2021",
 		"id": 1444159835310751700,
 		"id_str": "1444181774523777025",
