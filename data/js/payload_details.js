ce1dacd3fa20ecc072ef5ea799d3eef720bdaeee
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-03 16:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81302,
-	"created_at": "Sun Oct 03 06:02:01 +0000 2021",
+	"tweets": 81303,
+	"created_at": "Sun Oct 03 07:02:00 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sun Oct 03 06:06:16 +0000 2021",
+		"id": 1444544308175069200,
+		"id_str": "1444544308175069184",
+		"full_text": "#놀라운토요일 #도레미마켓 \n[#도레미모음집] 대체불가 보컬리스트 태연의 본업 모먼트✨ 제 고막이 감격스러워서 울어요... | #놀라운토요일 Amazing Saturday \nhttps://t.co/S7b9z8B60a \n \n#김태연 #태연 #탱구 #TAEYEON",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"text": "도레미마켓",
+					"indices": [
+						8,
+						14
+					]
+				},
+				{
+					"text": "도레미모음집",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"text": "놀라운토요일",
+					"indices": [
+						71,
+						78
+					]
+				},
+				{
+					"text": "김태연",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						129,
+						132
+					]
+				},
+				{
+					"text": "탱구",
+					"indices": [
+						133,
+						136
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						137,
+						145
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/S7b9z8B60a",
+					"expanded_url": "https://www.youtube.com/watch?v=vVjaUkjTDL8",
+					"display_url": "youtube.com/watch?v=vVjaUk…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 316,
+			"friends_count": 1050,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44843,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 85843,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#놀라운토요일 #도레미마켓 \n[#도레미모음집] 대체불가 보컬리스트 태연의 본업 모먼트✨ 제 고막이 감격스러워서 울어요... | #놀라운토요일 Amazing Saturday \nhttps://t.co/S7b9z8B60a \n \n#김태연 #태연 #탱구 #TAEYEON"
+	},
+	{
 		"created_at": "Sun Oct 03 05:26:38 +0000 2021",
 		"id": 1444533374089982000,
 		"id_str": "1444534337886507008",
