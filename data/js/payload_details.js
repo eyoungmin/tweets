8cf820ff44871a5d9f7c32fab9df9a8d7f920279
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-08 17:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81493,
-	"created_at": "Fri Oct 08 07:02:02 +0000 2021",
+	"tweets": 81494,
+	"created_at": "Fri Oct 08 08:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,310 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 08 07:27:47 +0000 2021",
+		"id": 1446374204408926200,
+		"id_str": "1446376764964364290",
+		"full_text": "조이 TOD’S 팝업스토어 방문\nin 10 Corso Como\n(with 헨더 스킴 컬랙션)\n\n#조이 #JOY\n#레드벨벳 #RedVelvet \n#10꼬르소꼬모 #10CorsoComo\n#토즈 #TODS https://t.co/GfpBKbRzUL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "조이",
+					"indices": [
+						53,
+						56
+					]
+				},
+				{
+					"text": "JOY",
+					"indices": [
+						57,
+						61
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						62,
+						67
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						68,
+						78
+					]
+				},
+				{
+					"text": "10꼬르소꼬모",
+					"indices": [
+						80,
+						88
+					]
+				},
+				{
+					"text": "10CorsoComo",
+					"indices": [
+						89,
+						101
+					]
+				},
+				{
+					"text": "토즈",
+					"indices": [
+						102,
+						105
+					]
+				},
+				{
+					"text": "TODS",
+					"indices": [
+						106,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1446374185257738200,
+					"id_str": "1446374185257738240",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FBKNqISVIAAUPoX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBKNqISVIAAUPoX.jpg",
+					"url": "https://t.co/GfpBKbRzUL",
+					"display_url": "pic.twitter.com/GfpBKbRzUL",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1446374204408926209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 959,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 543,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1802,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1446374185257738200,
+					"id_str": "1446374185257738240",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FBKNqISVIAAUPoX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBKNqISVIAAUPoX.jpg",
+					"url": "https://t.co/GfpBKbRzUL",
+					"display_url": "pic.twitter.com/GfpBKbRzUL",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1446374204408926209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 959,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 543,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1802,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1446374191645659100,
+					"id_str": "1446374191645659136",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FBKNqgFVEAAHCK3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBKNqgFVEAAHCK3.jpg",
+					"url": "https://t.co/GfpBKbRzUL",
+					"display_url": "pic.twitter.com/GfpBKbRzUL",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1446374204408926209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 543,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1803,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 958,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1446374198360752000,
+					"id_str": "1446374198360752129",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/FBKNq5GVQAEVSdi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBKNq5GVQAEVSdi.jpg",
+					"url": "https://t.co/GfpBKbRzUL",
+					"display_url": "pic.twitter.com/GfpBKbRzUL",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1446374204408926209/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1803,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 543,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 958,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet The 6th Mini Album ‘Queendom’\n\n2021.8.16. 6PM (KST)",
+			"url": "https://t.co/0hh6LbqXQp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0hh6LbqXQp",
+							"expanded_url": "https://www.youtube.com/redvelvet",
+							"display_url": "youtube.com/redvelvet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4662022,
+			"friends_count": 1,
+			"listed_count": 9843,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 77,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2313,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1421848248352862216/RXFqEwwB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1421848248352862216/RXFqEwwB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1627830015",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4236,
+		"favorite_count": 11062,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "조이 TOD’S 팝업스토어 방문\nin 10 Corso Como\n(with 헨더 스킴 컬랙션)\n\n#조이 #JOY\n#레드벨벳 #RedVelvet \n#10꼬르소꼬모 #10CorsoComo\n#토즈 #TODS https://t.co/GfpBKbRzUL"
+	},
+	{
 		"created_at": "Fri Oct 08 06:36:54 +0000 2021",
 		"id": 1446362735021752300,
 		"id_str": "1446363959964028929",
