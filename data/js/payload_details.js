461ef1b4909b42cfd499e37c2e5e397b20ac5290
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-08 09:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81483,
-	"created_at": "Thu Oct 07 23:02:03 +0000 2021",
+	"tweets": 81484,
+	"created_at": "Fri Oct 08 00:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 07 23:40:10 +0000 2021",
+		"id": 1446249940296953900,
+		"id_str": "1446259082168578048",
+		"full_text": "@weareKING @GlblCtzn @jacobcollier Always such a joy to have you with us, Paris and Amber. And so special to share this moment with Jacob too. 💙❤️💚 PH",
+		"truncated": false,
+		"display_text_range": [
+			35,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "weareKING",
+					"name": "We Are KING",
+					"id": 183070914,
+					"id_str": "183070914",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "GlblCtzn",
+					"name": "Global Citizen ⭕",
+					"id": 596893898,
+					"id_str": "596893898",
+					"indices": [
+						11,
+						20
+					]
+				},
+				{
+					"screen_name": "jacobcollier",
+					"name": "Jacob Collier",
+					"id": 1322735690,
+					"id_str": "1322735690",
+					"indices": [
+						21,
+						34
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1446213021450260500,
+		"in_reply_to_status_id_str": "1446213021450260482",
+		"in_reply_to_user_id": 183070914,
+		"in_reply_to_user_id_str": "183070914",
+		"in_reply_to_screen_name": "weareKING",
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Epiphane",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out October 15th, 2021. New single My Universe with BTS out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23401125,
+			"friends_count": 772,
+			"listed_count": 42762,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1369,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8721,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1626794531",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 101,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@weareKING @GlblCtzn @jacobcollier Always such a joy to have you with us, Paris and Amber. And so special to share this moment with Jacob too. 💙❤️💚 PH"
+	},
+	{
 		"created_at": "Thu Oct 07 22:40:09 +0000 2021",
 		"id": 1446187603963883500,
 		"id_str": "1446243982300876802",
