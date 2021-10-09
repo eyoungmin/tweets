509646672b8ba81751c3bafe06e835eec10f56af
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-10 04:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81544,
-	"created_at": "Sat Oct 09 17:02:01 +0000 2021",
+	"tweets": 81545,
+	"created_at": "Sat Oct 09 19:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,187 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 09 18:40:10 +0000 2021",
+		"id": 1446890374245634000,
+		"id_str": "1446908360947871747",
+		"full_text": "@OhWonderMusic just finished the 22 Break film and that was the most terrific thing you have ever done. #22Break https://t.co/mCMTk09OvD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "22Break",
+					"indices": [
+						104,
+						112
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OhWonderMusic",
+					"name": "Oh Wonder",
+					"id": 2740435740,
+					"id_str": "2740435740",
+					"indices": [
+						0,
+						14
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1446890366788063200,
+					"id_str": "1446890366788063234",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FBRjH2VWUAIb3Vi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBRjH2VWUAIb3Vi.jpg",
+					"url": "https://t.co/mCMTk09OvD",
+					"display_url": "pic.twitter.com/mCMTk09OvD",
+					"expanded_url": "https://twitter.com/ragged_reeling/status/1446890374245634051/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1446890366788063200,
+					"id_str": "1446890366788063234",
+					"indices": [
+						113,
+						136
+					],
+					"media_url": "http://pbs.twimg.com/media/FBRjH2VWUAIb3Vi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FBRjH2VWUAIb3Vi.jpg",
+					"url": "https://t.co/mCMTk09OvD",
+					"display_url": "pic.twitter.com/mCMTk09OvD",
+					"expanded_url": "https://twitter.com/ragged_reeling/status/1446890374245634051/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 2740435740,
+		"in_reply_to_user_id_str": "2740435740",
+		"in_reply_to_screen_name": "OhWonderMusic",
+		"user": {
+			"id": 1372977807337357300,
+			"id_str": "1372977807337357313",
+			"name": "Felix ²² ⚡️",
+			"screen_name": "ragged_reeling",
+			"location": "Over Heaven Under Hell",
+			"description": "tell me how to reach you - he/they- 23 - INFP - FATM 09/19, Halsey 02/20, PFP by AG Rojas",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 229,
+			"friends_count": 859,
+			"listed_count": 1,
+			"created_at": "Fri Mar 19 18:26:49 +0000 2021",
+			"favourites_count": 14803,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3915,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1445433941729718280/HxbKaXjw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1445433941729718280/HxbKaXjw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1372977807337357313/1633453282",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@OhWonderMusic just finished the 22 Break film and that was the most terrific thing you have ever done. #22Break https://t.co/mCMTk09OvD"
+	},
+	{
 		"created_at": "Sat Oct 09 16:40:13 +0000 2021",
 		"id": 1446876671555129300,
 		"id_str": "1446878174311329794",
