004523019f69e9b84c68972171ced8589b36409d
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-16 03:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81786,
-	"created_at": "Fri Oct 15 17:02:04 +0000 2021",
+	"tweets": 81787,
+	"created_at": "Fri Oct 15 18:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 15 17:40:09 +0000 2021",
+		"id": 1448649142843035600,
+		"id_str": "1449067587703386116",
+		"full_text": "JUST ANNOUNCED 📣 @coldplay's Music of the Spheres World Tour w/ special guest @HERMusicx! Register now for access to tickets at https://t.co/Kl4Fd3Sc46 and drop a 🌐 if you're going! https://t.co/ogXf7vwr4h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "coldplay",
+					"name": "Coldplay",
+					"id": 18863815,
+					"id_str": "18863815",
+					"indices": [
+						17,
+						26
+					]
+				},
+				{
+					"screen_name": "HERMusicx",
+					"name": "H.E.R.",
+					"id": 774357990711316500,
+					"id_str": "774357990711316481",
+					"indices": [
+						78,
+						88
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Kl4Fd3Sc46",
+					"expanded_url": "https://bit.ly/3mTUFL0",
+					"display_url": "bit.ly/3mTUFL0",
+					"indices": [
+						128,
+						151
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1448648881206542300,
+					"id_str": "1448648881206542343",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1448648881206542343/pu/img/vLlgivi8pjn58BiP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1448648881206542343/pu/img/vLlgivi8pjn58BiP.jpg",
+					"url": "https://t.co/ogXf7vwr4h",
+					"display_url": "pic.twitter.com/ogXf7vwr4h",
+					"expanded_url": "https://twitter.com/LiveNation/status/1448649142843035653/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1448648881206542300,
+					"id_str": "1448648881206542343",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1448648881206542343/pu/img/vLlgivi8pjn58BiP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1448648881206542343/pu/img/vLlgivi8pjn58BiP.jpg",
+					"url": "https://t.co/ogXf7vwr4h",
+					"display_url": "pic.twitter.com/ogXf7vwr4h",
+					"expanded_url": "https://twitter.com/LiveNation/status/1448649142843035653/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 15015,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1448648881206542343/pu/vid/480x270/fehFvsjX_3VnyC4B.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1448648881206542343/pu/pl/53b6v3XN7_gAKprg.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1448648881206542343/pu/vid/1280x720/CDlI655rmLomMlaT.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1448648881206542343/pu/vid/640x360/gcPKe0rLjpKt51wz.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9629222,
+			"id_str": "9629222",
+			"name": "Live Nation",
+			"screen_name": "LiveNation",
+			"location": "Worldwide",
+			"description": "Live Nation is the largest concert producer. Follow for Concert Announcements, On Sales & Presales, Promotions, Artist Exclusives.",
+			"url": "https://t.co/oQbll9uo8a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oQbll9uo8a",
+							"expanded_url": "http://livemu.sc/GetConcertTickets",
+							"display_url": "livemu.sc/GetConcertTick…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 523227,
+			"friends_count": 24005,
+			"listed_count": 5091,
+			"created_at": "Tue Oct 23 17:37:27 +0000 2007",
+			"favourites_count": 11695,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 79391,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1410696171123662849/oYqsoXQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410696171123662849/oYqsoXQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9629222/1605916924",
+			"profile_link_color": "C90000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "CCCCCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 135,
+		"favorite_count": 1741,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "JUST ANNOUNCED 📣 @coldplay's Music of the Spheres World Tour w/ special guest @HERMusicx! Register now for access to tickets at https://t.co/Kl4Fd3Sc46 and drop a 🌐 if you're going! https://t.co/ogXf7vwr4h"
+	},
+	{
 		"created_at": "Fri Oct 15 16:45:36 +0000 2021",
 		"id": 1449045626243596300,
 		"id_str": "1449053858920022022",
