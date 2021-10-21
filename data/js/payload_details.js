d2e58c7f9e00367eddcd40d9017d2827aefa5b6a
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-21 13:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 81951,
-	"created_at": "Thu Oct 21 03:02:06 +0000 2021",
+	"tweets": 81952,
+	"created_at": "Thu Oct 21 04:02:06 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Thu Oct 21 03:45:39 +0000 2021",
+		"id": 1451031274357883000,
+		"id_str": "1451031906263392259",
+		"full_text": "#GeniusCharts | Most Followed Korean Girl Groups on Genius\n1. BLACKPINK\n2. TWICE\n3. Red Velvet\n4. MAMAMOO\n5. ITZY\n6. LOONA\n7. (G)I-DLE\n8. Girls' Generation\n9. 2NE1\n10. GFriend https://t.co/4Da3Yih8Cp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GeniusCharts",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1451030946132594700,
+					"id_str": "1451030946132594689",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/FCMY9SZVEAEUDTc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCMY9SZVEAEUDTc.jpg",
+					"url": "https://t.co/4Da3Yih8Cp",
+					"display_url": "pic.twitter.com/4Da3Yih8Cp",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1451031274357882883/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1451030946132594700,
+					"id_str": "1451030946132594689",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/FCMY9SZVEAEUDTc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCMY9SZVEAEUDTc.jpg",
+					"url": "https://t.co/4Da3Yih8Cp",
+					"display_url": "pic.twitter.com/4Da3Yih8Cp",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1451031274357882883/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288205677488255000,
+			"id_str": "1288205677488254976",
+			"name": "Genius Korea",
+			"screen_name": "Genius_kor",
+			"location": "Worldwide",
+			"description": "Lyrics, Romanization, English Translations for K-Pop & Korean music on the biggest lyric website @Genius 🇰🇷",
+			"url": "https://t.co/hrRPuLDjon",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hrRPuLDjon",
+							"expanded_url": "https://genius.com/artists/Genius-korea",
+							"display_url": "genius.com/artists/Genius…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10875,
+			"friends_count": 1206,
+			"listed_count": 50,
+			"created_at": "Tue Jul 28 20:13:28 +0000 2020",
+			"favourites_count": 1917,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3079,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1410641902597132290/PhsF5tbU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410641902597132290/PhsF5tbU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288205677488254976/1595971297",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#GeniusCharts | Most Followed Korean Girl Groups on Genius\n1. BLACKPINK\n2. TWICE\n3. Red Velvet\n4. MAMAMOO\n5. ITZY\n6. LOONA\n7. (G)I-DLE\n8. Girls' Generation\n9. 2NE1\n10. GFriend https://t.co/4Da3Yih8Cp"
+	},
+	{
 		"created_at": "Thu Oct 21 03:01:43 +0000 2021",
 		"id": 1451020847490142200,
 		"id_str": "1451020847490142211",
