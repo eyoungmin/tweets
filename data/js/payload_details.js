b3ae8baf93c8ddf850b3bd0defdbf0514624719e
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-24 14:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82065,
-	"created_at": "Sun Oct 24 04:02:06 +0000 2021",
+	"tweets": 82066,
+	"created_at": "Sun Oct 24 05:02:06 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,258 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sun Oct 24 04:45:36 +0000 2021",
+		"id": 1452122678895259600,
+		"id_str": "1452134156218028032",
+		"full_text": "📣SPECIAL ANNOUNCEMENT FROM #이달의소녀(#LOONA)\n\n2021.10.30.(토) 8PM~\nYoutube라이브 &lt;MBCkpop&gt; &amp; &lt;it’s Live [MDR]&gt;채널 동시 생중계\n\n#WorldisOne #월드이즈원 #WorldisOne2021 #WorldisOne2021콘서트 https://t.co/xbabrujrzK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						27,
+						33
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						34,
+						40
+					]
+				},
+				{
+					"text": "WorldisOne",
+					"indices": [
+						130,
+						141
+					]
+				},
+				{
+					"text": "월드이즈원",
+					"indices": [
+						142,
+						148
+					]
+				},
+				{
+					"text": "WorldisOne2021",
+					"indices": [
+						149,
+						164
+					]
+				},
+				{
+					"text": "WorldisOne2021콘서트",
+					"indices": [
+						165,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1451430920745939000,
+					"id_str": "1451430920745938944",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FCSHZqoUcAMPJpa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCSHZqoUcAMPJpa.jpg",
+					"url": "https://t.co/xbabrujrzK",
+					"display_url": "pic.twitter.com/xbabrujrzK",
+					"expanded_url": "https://twitter.com/withMBC/status/1452122678895259651/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1451430920745939000,
+					"id_str": "1451430920745938944",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FCSHZqoUcAMPJpa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FCSHZqoUcAMPJpa.jpg",
+					"url": "https://t.co/xbabrujrzK",
+					"display_url": "pic.twitter.com/xbabrujrzK",
+					"expanded_url": "https://twitter.com/withMBC/status/1452122678895259651/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 49382,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1451430920745938944/vid/480x270/LfKK756BfydwOahg.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1451430920745938944/pl/pfL0uEqZDPC1pTj5.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1451430920745938944/vid/1280x720/UVDmmc8sgd9alHyJ.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1451430920745938944/vid/640x360/BcE3-sHYSaF_6FZ3.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 104673579,
+			"id_str": "104673579",
+			"name": "withMBC",
+			"screen_name": "withMBC",
+			"location": "상암동",
+			"description": "MBC 방송 프로그램 홍보 트위터 MBC PR",
+			"url": "https://t.co/yOEvgSpDjG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yOEvgSpDjG",
+							"expanded_url": "http://www.imbc.com",
+							"display_url": "imbc.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1006147,
+			"friends_count": 10023,
+			"listed_count": 4226,
+			"created_at": "Thu Jan 14 01:40:58 +0000 2010",
+			"favourites_count": 126,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 46034,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FCFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/104673579/1633573365",
+			"profile_link_color": "1B3E8F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FCFCFC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 286,
+		"favorite_count": 717,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📣SPECIAL ANNOUNCEMENT FROM #이달의소녀(#LOONA)\n\n2021.10.30.(토) 8PM~\nYoutube라이브 &lt;MBCkpop&gt; &amp; &lt;it’s Live [MDR]&gt;채널 동시 생중계\n\n#WorldisOne #월드이즈원 #WorldisOne2021 #WorldisOne2021콘서트 https://t.co/xbabrujrzK"
+	},
+	{
 		"created_at": "Sun Oct 24 03:40:28 +0000 2021",
 		"id": 1452115690442276900,
 		"id_str": "1452117762411728897",
