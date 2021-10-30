6c13f315429e7ea3d7a0bbf12fcacb1a08b64ac1
Youngmin Lee <iameyoungmin@gmail.com>
2021-10-30 13:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82255,
-	"created_at": "Sat Oct 30 03:02:12 +0000 2021",
+	"tweets": 82256,
+	"created_at": "Sat Oct 30 04:02:11 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_10.js",
 		"year": 2021,
 		"var_name": "tweets_2021_10",
-		"tweet_count": 1000,
+		"tweet_count": 1001,
 		"month": 10
 	},
 	{
