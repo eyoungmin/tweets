f30421554e40c084fda17281734e58d70deb9e6a
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-05 08:02:01 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Thu Nov 04 22:40:09 +0000 2021",
+		"id": 1456081834203172900,
+		"id_str": "1456390842508267529",
+		"full_text": "[#2021MAMA] Best Collaboration Nominee l #coldplay #bts #방탄소년단 #MyUniverse\n\nMake Some Noise, 2021 MAMA\n2021.12.11 (SAT)\n\n#MnetASIANMUSICAWARDS #MAMA #MakeSomeNoise #Mnet https://t.co/6Hp85MLpl2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "2021MAMA",
+					"indices": [
+						1,
+						10
+					]
+				},
+				{
+					"text": "coldplay",
+					"indices": [
+						41,
+						50
+					]
+				},
+				{
+					"text": "bts",
+					"indices": [
+						51,
+						55
+					]
+				},
+				{
+					"text": "방탄소년단",
+					"indices": [
+						56,
+						62
+					]
+				},
+				{
+					"text": "MyUniverse",
+					"indices": [
+						63,
+						74
+					]
+				},
+				{
+					"text": "MnetASIANMUSICAWARDS",
+					"indices": [
+						121,
+						142
+					]
+				},
+				{
+					"text": "MAMA",
+					"indices": [
+						143,
+						148
+					]
+				},
+				{
+					"text": "MakeSomeNoise",
+					"indices": [
+						149,
+						163
+					]
+				},
+				{
+					"text": "Mnet",
+					"indices": [
+						164,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1456081815396302800,
+					"id_str": "1456081815396302848",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FDUKskqaQAAdkcf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDUKskqaQAAdkcf.jpg",
+					"url": "https://t.co/6Hp85MLpl2",
+					"display_url": "pic.twitter.com/6Hp85MLpl2",
+					"expanded_url": "https://twitter.com/MnetMAMA/status/1456081834203172864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1456081815396302800,
+					"id_str": "1456081815396302848",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FDUKskqaQAAdkcf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDUKskqaQAAdkcf.jpg",
+					"url": "https://t.co/6Hp85MLpl2",
+					"display_url": "pic.twitter.com/6Hp85MLpl2",
+					"expanded_url": "https://twitter.com/MnetMAMA/status/1456081834203172864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128487133,
+			"id_str": "128487133",
+			"name": "MAMA(엠넷아시안뮤직어워즈)",
+			"screen_name": "MnetMAMA",
+			"location": "Seoul, South Korea",
+			"description": "MAMA(Mnet Asian Music Awards) Official Twitter 2021.12.11",
+			"url": "https://t.co/klitpCZ91k",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/klitpCZ91k",
+							"expanded_url": "http://2021mama.com",
+							"display_url": "2021mama.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2333615,
+			"friends_count": 105,
+			"listed_count": 2089,
+			"created_at": "Thu Apr 01 07:54:34 +0000 2010",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10775,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1450332871143555076/fWEqukGE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1450332871143555076/fWEqukGE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128487133/1634621277",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4D4D4",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13253,
+		"favorite_count": 30388,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "[#2021MAMA] Best Collaboration Nominee l #coldplay #bts #방탄소년단 #MyUniverse\n\nMake Some Noise, 2021 MAMA\n2021.12.11 (SAT)\n\n#MnetASIANMUSICAWARDS #MAMA #MakeSomeNoise #Mnet https://t.co/6Hp85MLpl2"
+	},
+	{
 		"created_at": "Thu Nov 04 21:48:41 +0000 2021",
 		"id": 1456377888501076000,
 		"id_str": "1456377888501075970",
