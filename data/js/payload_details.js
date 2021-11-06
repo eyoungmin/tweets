96c5c2056e7aaeb7a7ffcca5d26c10443cf36eea
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-06 20:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82518,
-	"created_at": "Sat Nov 06 10:02:02 +0000 2021",
+	"tweets": 82519,
+	"created_at": "Sat Nov 06 11:02:01 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sat Nov 06 10:46:37 +0000 2021",
+		"id": 1456934442937639000,
+		"id_str": "1456936049918230534",
+		"full_text": "#이달의소녀 가 출연한 'K-COMMUNITY FESTIVAL 2021'이 잠시 후 오후 8시(KST) 개최됩니다🎉❤ 케이팝을 사랑하는 전 세계 오빛들💫 우리 늦지 않게 만나기로 약속해요🤙\n\n#LOONA\n\n▶️ https://t.co/ocZBEtNELd https://t.co/Hdl18AcDzX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						107,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ocZBEtNELd",
+					"expanded_url": "https://youtube.com/c/kcommunityfestival",
+					"display_url": "youtube.com/c/kcommunityfe…",
+					"indices": [
+						118,
+						141
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1456934433223999500,
+					"id_str": "1456934433223999488",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/FDgSJdlaAAApK52.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDgSJdlaAAApK52.jpg",
+					"url": "https://t.co/Hdl18AcDzX",
+					"display_url": "pic.twitter.com/Hdl18AcDzX",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456934442937638918/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1456934433223999500,
+					"id_str": "1456934433223999488",
+					"indices": [
+						142,
+						165
+					],
+					"media_url": "http://pbs.twimg.com/media/FDgSJdlaAAApK52.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDgSJdlaAAApK52.jpg",
+					"url": "https://t.co/Hdl18AcDzX",
+					"display_url": "pic.twitter.com/Hdl18AcDzX",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456934442937638918/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 828689,
+			"friends_count": 1,
+			"listed_count": 8039,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3488,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2028,
+		"favorite_count": 5345,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 가 출연한 'K-COMMUNITY FESTIVAL 2021'이 잠시 후 오후 8시(KST) 개최됩니다🎉❤ 케이팝을 사랑하는 전 세계 오빛들💫 우리 늦지 않게 만나기로 약속해요🤙\n\n#LOONA\n\n▶️ https://t.co/ocZBEtNELd https://t.co/Hdl18AcDzX"
+	},
+	{
 		"created_at": "Sat Nov 06 09:45:40 +0000 2021",
 		"id": 1456909221010813000,
 		"id_str": "1456920712493637638",
