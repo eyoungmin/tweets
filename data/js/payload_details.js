42070c0753b0467fc0e5043ebed19741392a5e6f
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-06 23:02:11 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82520,
-	"created_at": "Sat Nov 06 12:02:03 +0000 2021",
+	"tweets": 82521,
+	"created_at": "Sat Nov 06 14:02:10 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Sat Nov 06 13:32:50 +0000 2021",
+		"id": 1456977822862364700,
+		"id_str": "1456977879162503176",
+		"full_text": "[#진솔/ #JinSoul] 거울 셀카를 찍으려고 하면 애들이 몰려와요,,🥴😳\n\n#이달의소녀 #LOONA https://t.co/QAo4cXdWBz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			58
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "진솔",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "JinSoul",
+					"indices": [
+						6,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						45,
+						51
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						52,
+						58
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1456977814851559400,
+					"id_str": "1456977814851559424",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FDg5mmvaIAAj2U3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDg5mmvaIAAj2U3.jpg",
+					"url": "https://t.co/QAo4cXdWBz",
+					"display_url": "pic.twitter.com/QAo4cXdWBz",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456977822862364679/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1456977814851559400,
+					"id_str": "1456977814851559424",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FDg5mmvaIAAj2U3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDg5mmvaIAAj2U3.jpg",
+					"url": "https://t.co/QAo4cXdWBz",
+					"display_url": "pic.twitter.com/QAo4cXdWBz",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456977822862364679/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456977815384244200,
+					"id_str": "1456977815384244227",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FDg5mouaQAMIhHg.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDg5mouaQAMIhHg.jpg",
+					"url": "https://t.co/QAo4cXdWBz",
+					"display_url": "pic.twitter.com/QAo4cXdWBz",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456977822862364679/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456977815900164000,
+					"id_str": "1456977815900164096",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FDg5mqpakAAtGcj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDg5mqpakAAtGcj.jpg",
+					"url": "https://t.co/QAo4cXdWBz",
+					"display_url": "pic.twitter.com/QAo4cXdWBz",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456977822862364679/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1456977816462196700,
+					"id_str": "1456977816462196736",
+					"indices": [
+						59,
+						82
+					],
+					"media_url": "http://pbs.twimg.com/media/FDg5msvagAAEUv4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDg5msvagAAEUv4.jpg",
+					"url": "https://t.co/QAo4cXdWBz",
+					"display_url": "pic.twitter.com/QAo4cXdWBz",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1456977822862364679/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1456977782127227000,
+		"in_reply_to_status_id_str": "1456977782127226887",
+		"in_reply_to_user_id": 770959712791597000,
+		"in_reply_to_user_id_str": "770959712791597056",
+		"in_reply_to_screen_name": "loonatheworld",
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 828755,
+			"friends_count": 1,
+			"listed_count": 8040,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3490,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2710,
+		"favorite_count": 7205,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#진솔/ #JinSoul] 거울 셀카를 찍으려고 하면 애들이 몰려와요,,🥴😳\n\n#이달의소녀 #LOONA https://t.co/QAo4cXdWBz"
+	},
+	{
 		"created_at": "Sat Nov 06 11:15:35 +0000 2021",
 		"id": 1456943339102670800,
 		"id_str": "1456943339102670849",
