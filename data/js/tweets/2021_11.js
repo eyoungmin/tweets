069a7b8ba58ef0005eb90ee45f52de93beea1e0d
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-08 14:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Mon Nov 08 04:46:23 +0000 2021",
+		"id": 1457568763025387500,
+		"id_str": "1457570168931037187",
+		"full_text": "믹싱 스튜디오에서의 첫날 사진.  \n믹싱도 이제 끝이 보이긴 보인다. \n엄청난 작업량에도 \n평정심을 유지하며 몇개월이나\n휴식 없이 일만 할 수 있었던 것은 \n동료와 가족, 친구들의 덕분. \n\n#자우림 \n#자우림11영원한사랑 \n#JAURIM11\n#JAURIM_LOVE_FOREVER https://t.co/7oEieWkZwj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "자우림",
+					"indices": [
+						107,
+						111
+					]
+				},
+				{
+					"text": "자우림11영원한사랑",
+					"indices": [
+						113,
+						124
+					]
+				},
+				{
+					"text": "JAURIM11",
+					"indices": [
+						126,
+						135
+					]
+				},
+				{
+					"text": "JAURIM_LOVE_FOREVER",
+					"indices": [
+						136,
+						156
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1457568755756986400,
+					"id_str": "1457568755756986368",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/FDpTD5KacAAirm4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDpTD5KacAAirm4.jpg",
+					"url": "https://t.co/7oEieWkZwj",
+					"display_url": "pic.twitter.com/7oEieWkZwj",
+					"expanded_url": "https://twitter.com/love_yuna/status/1457568763025387523/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1457568755756986400,
+					"id_str": "1457568755756986368",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/FDpTD5KacAAirm4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDpTD5KacAAirm4.jpg",
+					"url": "https://t.co/7oEieWkZwj",
+					"display_url": "pic.twitter.com/7oEieWkZwj",
+					"expanded_url": "https://twitter.com/love_yuna/status/1457568763025387523/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1457568755752775700,
+					"id_str": "1457568755752775680",
+					"indices": [
+						157,
+						180
+					],
+					"media_url": "http://pbs.twimg.com/media/FDpTD5JaMAAVbam.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FDpTD5JaMAAVbam.jpg",
+					"url": "https://t.co/7oEieWkZwj",
+					"display_url": "pic.twitter.com/7oEieWkZwj",
+					"expanded_url": "https://twitter.com/love_yuna/status/1457568763025387523/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 271418,
+			"friends_count": 548,
+			"listed_count": 2809,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 302,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6372,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1635493925",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 29,
+		"favorite_count": 71,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "믹싱 스튜디오에서의 첫날 사진.  \n믹싱도 이제 끝이 보이긴 보인다. \n엄청난 작업량에도 \n평정심을 유지하며 몇개월이나\n휴식 없이 일만 할 수 있었던 것은 \n동료와 가족, 친구들의 덕분. \n\n#자우림 \n#자우림11영원한사랑 \n#JAURIM11\n#JAURIM_LOVE_FOREVER https://t.co/7oEieWkZwj"
+	},
+	{
 		"created_at": "Mon Nov 08 01:03:33 +0000 2021",
 		"id": 1457514092533624800,
 		"id_str": "1457514092533624836",
