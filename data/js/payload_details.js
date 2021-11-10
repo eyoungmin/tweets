b909119fe25a41f6df58cad50ef703cb9a541701
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-10 16:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82611,
-	"created_at": "Wed Nov 10 05:02:03 +0000 2021",
+	"tweets": 82612,
+	"created_at": "Wed Nov 10 07:02:03 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 10 06:40:09 +0000 2021",
+		"id": 1458311402670620700,
+		"id_str": "1458323577833459712",
+		"full_text": "티르리르의 새로운 가을 컬렉션, \n따스한 온기와 함께 건네는 \n치유와 위로를 주제로 균형감있는 \n&lt;𝐑𝐞𝐦𝐞𝐝𝐲 𝐟𝐫𝐨𝐦 𝐭𝐡𝐞 𝐰𝐚𝐫𝐦𝐭𝐡&gt;를 소개합니다.\n\nhttps://t.co/BnntiLGjVA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BnntiLGjVA",
+					"expanded_url": "https://www.instagram.com/p/CWFS4zkLCRk/?utm_source=ig_web_copy_link",
+					"display_url": "instagram.com/p/CWFS4zkLCRk/…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1413160034456858600,
+			"id_str": "1413160034456858627",
+			"name": "신세경 공식 트위터",
+			"screen_name": "_sskofficial",
+			"location": "",
+			"description": "신세경(Shin Sae Kyeong) Official Twitter",
+			"url": "https://t.co/Ttrvkr9K9e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ttrvkr9K9e",
+							"expanded_url": "https://www.instagram.com/sjkuksee",
+							"display_url": "instagram.com/sjkuksee",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3149,
+			"friends_count": 0,
+			"listed_count": 5,
+			"created_at": "Thu Jul 08 15:36:39 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 53,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1427870554543984643/wITjsAgv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1427870554543984643/wITjsAgv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1413160034456858627/1629265844",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 34,
+		"favorite_count": 118,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "티르리르의 새로운 가을 컬렉션, \n따스한 온기와 함께 건네는 \n치유와 위로를 주제로 균형감있는 \n&lt;𝐑𝐞𝐦𝐞𝐝𝐲 𝐟𝐫𝐨𝐦 𝐭𝐡𝐞 𝐰𝐚𝐫𝐦𝐭𝐡&gt;를 소개합니다.\n\nhttps://t.co/BnntiLGjVA"
+	},
+	{
 		"created_at": "Wed Nov 10 04:44:27 +0000 2021",
 		"id": 1458283275433042000,
 		"id_str": "1458294459183951880",
