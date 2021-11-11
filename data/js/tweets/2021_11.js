a351a24fc8e9a8cd9e5582ff7da4c5cd3cb1db23
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-12 00:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Thu Nov 11 14:45:36 +0000 2021",
+		"id": 1458808132151480300,
+		"id_str": "1458808132151480329",
+		"full_text": "#새소년 #SESONEON \n골 때리는 소윤 #shootingstar \nhttps://t.co/3IeXXnsbBi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			63
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "새소년",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "SESONEON",
+					"indices": [
+						5,
+						14
+					]
+				},
+				{
+					"text": "shootingstar",
+					"indices": [
+						25,
+						38
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3IeXXnsbBi",
+					"expanded_url": "https://www.youtube.com/watch?v=rXtSjPJlq-o",
+					"display_url": "youtube.com/watch?v=rXtSjP…",
+					"indices": [
+						40,
+						63
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 321,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 87506,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#새소년 #SESONEON \n골 때리는 소윤 #shootingstar \nhttps://t.co/3IeXXnsbBi"
+	},
+	{
 		"created_at": "Thu Nov 11 13:40:16 +0000 2021",
 		"id": 1458786959829045200,
 		"id_str": "1458791687979945987",
@@ -273,7 +389,7 @@ Grailbird.data.tweets_2021_11 =
 		"created_at": "Thu Nov 11 13:37:03 +0000 2021",
 		"id": 1458776473066619000,
 		"id_str": "1458790880693739524",
-		"full_text": "#슬기zip 빼빼로데이 선물💝 \n쏘스윗 갓기천사🐻 뭐라구요?! \n쥔님이 손님들 위해 뭘 만든다구요❓ \n앞치마 입고👩🏻‍🍳 우당탕탕💥 \n초코과자 만들  #슬기 벌써 귀엽다...😍 \n#SEULGI 🏡 더보기 #NOW온에어 ⬇️\nhttps://t.co/6P1FXNknTr https://t.co/3H098AwpQO",
+		"full_text": "#슬기zip 빼빼로데이 선물💝 \n쏘스윗 갓기천사🐻 뭐라구요?! \n쥔님이 손님들 위해 뭘 만든다구요❓ \n앞치마 입고👩🏻🍳 우당탕탕💥 \n초코과자 만들  #슬기 벌써 귀엽다...😍 \n#SEULGI 🏡 더보기 #NOW온에어 ⬇️\nhttps://t.co/6P1FXNknTr https://t.co/3H098AwpQO",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -517,13 +633,13 @@ Grailbird.data.tweets_2021_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "#슬기zip 빼빼로데이 선물💝 \n쏘스윗 갓기천사🐻 뭐라구요?! \n쥔님이 손님들 위해 뭘 만든다구요❓ \n앞치마 입고👩🏻‍🍳 우당탕탕💥 \n초코과자 만들  #슬기 벌써 귀엽다...😍 \n#SEULGI 🏡 더보기 #NOW온에어 ⬇️\nhttps://t.co/6P1FXNknTr https://t.co/3H098AwpQO"
+		"text": "#슬기zip 빼빼로데이 선물💝 \n쏘스윗 갓기천사🐻 뭐라구요?! \n쥔님이 손님들 위해 뭘 만든다구요❓ \n앞치마 입고👩🏻🍳 우당탕탕💥 \n초코과자 만들  #슬기 벌써 귀엽다...😍 \n#SEULGI 🏡 더보기 #NOW온에어 ⬇️\nhttps://t.co/6P1FXNknTr https://t.co/3H098AwpQO"
 	},
 	{
 		"created_at": "Thu Nov 11 13:09:36 +0000 2021",
 		"id": 1458781557532016600,
 		"id_str": "1458783970179440650",
-		"full_text": "슬기가 만드는👩‍🍳\n우당탕탕 빼빼로\n\nhttps://t.co/QX2m23fQWR\n\n#슬기 #SEULGI    \n#슬기집 #슬기zip \n#레드벨벳 #RedVelvet",
+		"full_text": "슬기가 만드는👩🍳\n우당탕탕 빼빼로\n\nhttps://t.co/QX2m23fQWR\n\n#슬기 #SEULGI    \n#슬기집 #슬기zip \n#레드벨벳 #RedVelvet",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -667,7 +783,7 @@ Grailbird.data.tweets_2021_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "슬기가 만드는👩‍🍳\n우당탕탕 빼빼로\n\nhttps://t.co/QX2m23fQWR\n\n#슬기 #SEULGI    \n#슬기집 #슬기zip \n#레드벨벳 #RedVelvet"
+		"text": "슬기가 만드는👩🍳\n우당탕탕 빼빼로\n\nhttps://t.co/QX2m23fQWR\n\n#슬기 #SEULGI    \n#슬기집 #슬기zip \n#레드벨벳 #RedVelvet"
 	},
 	{
 		"created_at": "Thu Nov 11 11:45:36 +0000 2021",
