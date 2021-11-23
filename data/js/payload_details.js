7f8139ea6e15cb865138b54e70d1b65dd58f781f
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-23 22:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 82999,
-	"created_at": "Tue Nov 23 12:02:10 +0000 2021",
+	"tweets": 83000,
+	"created_at": "Tue Nov 23 13:02:09 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Tue Nov 23 12:42:37 +0000 2021",
+		"id": 1463115116342296600,
+		"id_str": "1463125837746774023",
+		"full_text": "따숩고 예쁘고 다하네! 이하이, 리정 픽 뽀글뽀글 아우터 따라잡기\n\n#이하이 #시어링 #아우터 #뽀글이 #퍼 #퍼자켓 #에스파 #윈터 #리정 #스우파 #스트릿우먼파이터 #정채연 #조이 #레드벨벳\nhttps://t.co/Pva2lr9zRS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이하이",
+					"indices": [
+						38,
+						42
+					]
+				},
+				{
+					"text": "시어링",
+					"indices": [
+						43,
+						47
+					]
+				},
+				{
+					"text": "아우터",
+					"indices": [
+						48,
+						52
+					]
+				},
+				{
+					"text": "뽀글이",
+					"indices": [
+						53,
+						57
+					]
+				},
+				{
+					"text": "퍼",
+					"indices": [
+						58,
+						60
+					]
+				},
+				{
+					"text": "퍼자켓",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						66,
+						70
+					]
+				},
+				{
+					"text": "윈터",
+					"indices": [
+						71,
+						74
+					]
+				},
+				{
+					"text": "리정",
+					"indices": [
+						75,
+						78
+					]
+				},
+				{
+					"text": "스우파",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "스트릿우먼파이터",
+					"indices": [
+						84,
+						93
+					]
+				},
+				{
+					"text": "정채연",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						99,
+						102
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						103,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Pva2lr9zRS",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/61392?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/61392?…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다",
+			"url": "https://t.co/ONBx2OumIF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ONBx2OumIF",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88653,
+			"friends_count": 3,
+			"listed_count": 403,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 123,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13244,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "따숩고 예쁘고 다하네! 이하이, 리정 픽 뽀글뽀글 아우터 따라잡기\n\n#이하이 #시어링 #아우터 #뽀글이 #퍼 #퍼자켓 #에스파 #윈터 #리정 #스우파 #스트릿우먼파이터 #정채연 #조이 #레드벨벳\nhttps://t.co/Pva2lr9zRS"
+	},
+	{
 		"created_at": "Tue Nov 23 11:46:27 +0000 2021",
 		"id": 1463111701772050400,
 		"id_str": "1463111701772050434",
