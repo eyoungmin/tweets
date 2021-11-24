10b843359db054f3434246773d867a1c0cf0e45d
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-25 03:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 83042,
-	"created_at": "Wed Nov 24 16:02:10 +0000 2021",
+	"tweets": 83043,
+	"created_at": "Wed Nov 24 18:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,10 +1,126 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 24 17:07:54 +0000 2021",
+		"id": 1463554984553980000,
+		"id_str": "1463554984553979914",
+		"full_text": "#이달의소녀 #LOONA #VLIVE \n[이달의 소녀] 달의 주파수 ep.64 | 이달의 소녀(LOONA) \nhttps://t.co/6ldldpxdZe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						7,
+						13
+					]
+				},
+				{
+					"text": "VLIVE",
+					"indices": [
+						14,
+						20
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6ldldpxdZe",
+					"expanded_url": "https://www.vlive.tv/video/269883",
+					"display_url": "vlive.tv/video/269883",
+					"indices": [
+						61,
+						84
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 320,
+			"friends_count": 1051,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44754,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 87996,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#이달의소녀 #LOONA #VLIVE \n[이달의 소녀] 달의 주파수 ep.64 | 이달의 소녀(LOONA) \nhttps://t.co/6ldldpxdZe"
+	},
+	{
 		"created_at": "Wed Nov 24 15:40:24 +0000 2021",
 		"id": 1463522811100680200,
 		"id_str": "1463532962859958274",
-		"full_text": "K-POPガールズグループ #LOONA の皆さんに #ABCマート 限定カラーの #NIKEコートビジョン を履いて頂きました👯‍♀️\n\n皆さんの心躍る瞬間はどのような時ですか❓\nNIKEコートビジョンを履いて、心躍る毎日を過ごしましょう👟✨\n\n#心躍らそう\n\n👇👇チェックお忘れなく🧐\nhttps://t.co/4RDbvkxujn https://t.co/N2dq6F0eJO",
+		"full_text": "K-POPガールズグループ #LOONA の皆さんに #ABCマート 限定カラーの #NIKEコートビジョン を履いて頂きました👯♀️\n\n皆さんの心躍る瞬間はどのような時ですか❓\nNIKEコートビジョンを履いて、心躍る毎日を過ごしましょう👟✨\n\n#心躍らそう\n\n👇👇チェックお忘れなく🧐\nhttps://t.co/4RDbvkxujn https://t.co/N2dq6F0eJO",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -212,7 +328,7 @@ Grailbird.data.tweets_2021_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ja",
-		"text": "K-POPガールズグループ #LOONA の皆さんに #ABCマート 限定カラーの #NIKEコートビジョン を履いて頂きました👯‍♀️\n\n皆さんの心躍る瞬間はどのような時ですか❓\nNIKEコートビジョンを履いて、心躍る毎日を過ごしましょう👟✨\n\n#心躍らそう\n\n👇👇チェックお忘れなく🧐\nhttps://t.co/4RDbvkxujn https://t.co/N2dq6F0eJO"
+		"text": "K-POPガールズグループ #LOONA の皆さんに #ABCマート 限定カラーの #NIKEコートビジョン を履いて頂きました👯♀️\n\n皆さんの心躍る瞬間はどのような時ですか❓\nNIKEコートビジョンを履いて、心躍る毎日を過ごしましょう👟✨\n\n#心躍らそう\n\n👇👇チェックお忘れなく🧐\nhttps://t.co/4RDbvkxujn https://t.co/N2dq6F0eJO"
 	},
 	{
 		"created_at": "Wed Nov 24 15:40:18 +0000 2021",
