d9e6406b3c94ac6a393674524184ebe837a3d180
Youngmin Lee <iameyoungmin@gmail.com>
2021-11-24 21:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 83036,
-	"created_at": "Wed Nov 24 11:02:11 +0000 2021",
+	"tweets": 83037,
+	"created_at": "Wed Nov 24 12:02:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 24 11:40:10 +0000 2021",
+		"id": 1463465392811438000,
+		"id_str": "1463472505788805120",
+		"full_text": "영스로 오면\n포근하고 따수운 완디를\n만날 수 있어요❄\n얼른 오세요💙\n\n#웬디 #WENDY #완디\n#RedVelvet  #레드벨벳 \n#웬디의영스트리트 #영스트리트 \n#영스 #SBS https://t.co/6YvD4xxPqq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						39,
+						42
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						43,
+						49
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						50,
+						53
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						73,
+						82
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						91,
+						94
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						95,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1463465383441363000,
+					"id_str": "1463465383441362945",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FE9GAzEacAEUwNq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FE9GAzEacAEUwNq.jpg",
+					"url": "https://t.co/6YvD4xxPqq",
+					"display_url": "pic.twitter.com/6YvD4xxPqq",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1463465392811438080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1413,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 469,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1463465383441363000,
+					"id_str": "1463465383441362945",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FE9GAzEacAEUwNq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FE9GAzEacAEUwNq.jpg",
+					"url": "https://t.co/6YvD4xxPqq",
+					"display_url": "pic.twitter.com/6YvD4xxPqq",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1463465392811438080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1413,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 469,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet The 6th Mini Album ‘Queendom’\n\n2021.8.16. 6PM (KST)",
+			"url": "https://t.co/0hh6LbqXQp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0hh6LbqXQp",
+							"expanded_url": "https://www.youtube.com/redvelvet",
+							"display_url": "youtube.com/redvelvet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4810299,
+			"friends_count": 1,
+			"listed_count": 9976,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 96,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2389,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1421848248352862216/RXFqEwwB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1421848248352862216/RXFqEwwB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1627830015",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3207,
+		"favorite_count": 10842,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "영스로 오면\n포근하고 따수운 완디를\n만날 수 있어요❄\n얼른 오세요💙\n\n#웬디 #WENDY #완디\n#RedVelvet  #레드벨벳 \n#웬디의영스트리트 #영스트리트 \n#영스 #SBS https://t.co/6YvD4xxPqq"
+	},
+	{
 		"created_at": "Wed Nov 24 10:40:21 +0000 2021",
 		"id": 1463447301481918500,
 		"id_str": "1463457453094354946",
