33f0c76e1b773b3e6729392b40f632f59fd6be7d
Youngmin Lee <iameyoungmin@gmail.com>
2021-12-04 15:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 83466,
-	"created_at": "Sat Dec 04 05:02:02 +0000 2021",
+	"tweets": 83467,
+	"created_at": "Sat Dec 04 06:02:02 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,174 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sat Dec 04 05:14:32 +0000 2021",
+		"id": 1466999252828438500,
+		"id_str": "1466999336655630337",
+		"full_text": "[💡] 청하 Killing Me Challenge🙌\nWith DANCER (Feat. AB)\n\n▶ https://t.co/QE0CCqLaLC\n▶ https://t.co/rQEzPjexpJ\n▶ https://t.co/wzA80hbGkN\n\n#청하 #CHUNGHA\n#KillingMe #킬링미 \n#KillingMe_Challenge #킬링미챌린지",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						132,
+						135
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						136,
+						144
+					]
+				},
+				{
+					"text": "KillingMe",
+					"indices": [
+						145,
+						155
+					]
+				},
+				{
+					"text": "킬링미",
+					"indices": [
+						156,
+						160
+					]
+				},
+				{
+					"text": "KillingMe_Challenge",
+					"indices": [
+						162,
+						182
+					]
+				},
+				{
+					"text": "킬링미챌린지",
+					"indices": [
+						183,
+						190
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QE0CCqLaLC",
+					"expanded_url": "https://youtu.be/qc0SZTQ4xLw",
+					"display_url": "youtu.be/qc0SZTQ4xLw",
+					"indices": [
+						55,
+						78
+					]
+				},
+				{
+					"url": "https://t.co/rQEzPjexpJ",
+					"expanded_url": "https://www.instagram.com/reel/CXDHfBFFlcp/?utm_medium=copy_link",
+					"display_url": "instagram.com/reel/CXDHfBFFl…",
+					"indices": [
+						81,
+						104
+					]
+				},
+				{
+					"url": "https://t.co/wzA80hbGkN",
+					"expanded_url": "https://vt.tiktok.com/ZSeUdfkCr/",
+					"display_url": "vt.tiktok.com/ZSeUdfkCr/",
+					"indices": [
+						107,
+						130
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/ul2Z0qlgiq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ul2Z0qlgiq",
+							"expanded_url": "https://chungha.lnk.to/KillingMe",
+							"display_url": "chungha.lnk.to/KillingMe",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 338914,
+			"friends_count": 1,
+			"listed_count": 2104,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2098,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1462979473352310785/lBDUaRBi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1462979473352310785/lBDUaRBi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1637636723",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 111,
+		"favorite_count": 466,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[💡] 청하 Killing Me Challenge🙌\nWith DANCER (Feat. AB)\n\n▶ https://t.co/QE0CCqLaLC\n▶ https://t.co/rQEzPjexpJ\n▶ https://t.co/wzA80hbGkN\n\n#청하 #CHUNGHA\n#KillingMe #킬링미 \n#KillingMe_Challenge #킬링미챌린지"
+	},
+	{
 		"created_at": "Sat Dec 04 04:48:25 +0000 2021",
 		"id": 1466992765200482300,
 		"id_str": "1466992765200482304",
