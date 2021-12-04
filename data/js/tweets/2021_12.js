3e984163694fbca36feaef8df5186bdaee0f5929
Youngmin Lee <iameyoungmin@gmail.com>
2021-12-04 18:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,10 +1,200 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Sat Dec 04 08:40:10 +0000 2021",
+		"id": 1467048747779903500,
+		"id_str": "1467051086125092866",
+		"full_text": "오빛과 같은 공간에서 행복을 나눌 수 있어 더욱 따뜻했던 시간🤍 오늘 함께 해줘서 고마워요, 오빛💫\n\n#이달의소녀 #LOONA https://t.co/M6pkEgahFm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						57,
+						63
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						64,
+						70
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1467048739906883600,
+					"id_str": "1467048739906883585",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FFwBDmJVkAEgvIr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFwBDmJVkAEgvIr.jpg",
+					"url": "https://t.co/M6pkEgahFm",
+					"display_url": "pic.twitter.com/M6pkEgahFm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1467048747779903490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 811,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1467048739906883600,
+					"id_str": "1467048739906883585",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FFwBDmJVkAEgvIr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FFwBDmJVkAEgvIr.jpg",
+					"url": "https://t.co/M6pkEgahFm",
+					"display_url": "pic.twitter.com/M6pkEgahFm",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1467048747779903490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 676,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 811,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 836925,
+			"friends_count": 1,
+			"listed_count": 8088,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3619,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2819,
+		"favorite_count": 7108,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오빛과 같은 공간에서 행복을 나눌 수 있어 더욱 따뜻했던 시간🤍 오늘 함께 해줘서 고마워요, 오빛💫\n\n#이달의소녀 #LOONA https://t.co/M6pkEgahFm"
+	},
+	{
 		"created_at": "Sat Dec 04 07:29:38 +0000 2021",
 		"id": 1467025884087402500,
 		"id_str": "1467033339060273156",
-		"full_text": "[#탱키박스🎁]\n\n열려라 ~ 비하인드 박스!!🎁\n1-2년만 지나면 우는 니트…지겨와🤦🏻‍♀️\n그래서 준비한 일명 : 오프숄더 니트(?) 피하는 방법!\n\n🦋🔑와 함께 니트 수납법 알고싶은 사람~?\n👉 https://t.co/OyiXjNfZwx\n\n✔️ 매주 금 12 PM (본방송)\n#wavve 에서 만나요~🌊\n\n#태연 #키 #웨이브오리지널 https://t.co/TZ9373u2UE",
+		"full_text": "[#탱키박스🎁]\n\n열려라 ~ 비하인드 박스!!🎁\n1-2년만 지나면 우는 니트…지겨와🤦🏻♀️\n그래서 준비한 일명 : 오프숄더 니트(?) 피하는 방법!\n\n🦋🔑와 함께 니트 수납법 알고싶은 사람~?\n👉 https://t.co/OyiXjNfZwx\n\n✔️ 매주 금 12 PM (본방송)\n#wavve 에서 만나요~🌊\n\n#태연 #키 #웨이브오리지널 https://t.co/TZ9373u2UE",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -219,7 +409,7 @@ Grailbird.data.tweets_2021_12 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "[#탱키박스🎁]\n\n열려라 ~ 비하인드 박스!!🎁\n1-2년만 지나면 우는 니트…지겨와🤦🏻‍♀️\n그래서 준비한 일명 : 오프숄더 니트(?) 피하는 방법!\n\n🦋🔑와 함께 니트 수납법 알고싶은 사람~?\n👉 https://t.co/OyiXjNfZwx\n\n✔️ 매주 금 12 PM (본방송)\n#wavve 에서 만나요~🌊\n\n#태연 #키 #웨이브오리지널 https://t.co/TZ9373u2UE"
+		"text": "[#탱키박스🎁]\n\n열려라 ~ 비하인드 박스!!🎁\n1-2년만 지나면 우는 니트…지겨와🤦🏻♀️\n그래서 준비한 일명 : 오프숄더 니트(?) 피하는 방법!\n\n🦋🔑와 함께 니트 수납법 알고싶은 사람~?\n👉 https://t.co/OyiXjNfZwx\n\n✔️ 매주 금 12 PM (본방송)\n#wavve 에서 만나요~🌊\n\n#태연 #키 #웨이브오리지널 https://t.co/TZ9373u2UE"
 	},
 	{
 		"created_at": "Sat Dec 04 06:40:09 +0000 2021",
