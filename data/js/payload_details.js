52aa756e56ff356f28f35b1eb0651835f2650d0e
Youngmin Lee <iameyoungmin@gmail.com>
2021-12-06 11:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 83531,
-	"created_at": "Mon Dec 06 00:02:03 +0000 2021",
+	"tweets": 83532,
+	"created_at": "Mon Dec 06 02:02:04 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Mon Dec 06 01:59:14 +0000 2021",
+		"id": 1467672117844279300,
+		"id_str": "1467674966564999175",
+		"full_text": "에스파, 美 트레버 노아의 ‘더 데일리 쇼’ 출연! 7일 방송! \n글로벌 인기 행진 ING!\n\naespa to appear on ‘The Daily Show with Trevor Noah’ on 12/7! \n\nhttps://t.co/yab0Rn3koz\n\n#aespa #æspa #에스파",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						142,
+						148
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						149,
+						154
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						155,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yab0Rn3koz",
+					"expanded_url": "https://bit.ly/31sOfvG",
+					"display_url": "bit.ly/31sOfvG",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Savage",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1811947,
+			"friends_count": 1,
+			"listed_count": 6364,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1119,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1437430875566710784/8vJwI1Ey_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1437430875566710784/8vJwI1Ey_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1631545203",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1906,
+		"favorite_count": 6110,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "에스파, 美 트레버 노아의 ‘더 데일리 쇼’ 출연! 7일 방송! \n글로벌 인기 행진 ING!\n\naespa to appear on ‘The Daily Show with Trevor Noah’ on 12/7! \n\nhttps://t.co/yab0Rn3koz\n\n#aespa #æspa #에스파"
+	},
+	{
 		"created_at": "Sun Dec 05 23:44:24 +0000 2021",
 		"id": 1467633352396001300,
 		"id_str": "1467641035039907840",
