025d044defde3d30705c98b305979a882bceea9b
Youngmin Lee <iameyoungmin@gmail.com>
2021-12-14 00:02:38 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 83845,
-	"created_at": "Mon Dec 13 13:02:11 +0000 2021",
+	"tweets": 83846,
+	"created_at": "Mon Dec 13 15:02:11 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Mon Dec 13 14:40:09 +0000 2021",
+		"id": 1470399449361154000,
+		"id_str": "1470403173156327425",
+		"full_text": "[#TikTok] 냅다 대나무 헬리콥터 #도라에몽\n\n▶ https://t.co/IwKHzaocHw\n\n#Seori #서리 https://t.co/ylc2BNDhaZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TikTok",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "도라에몽",
+					"indices": [
+						22,
+						27
+					]
+				},
+				{
+					"text": "Seori",
+					"indices": [
+						56,
+						62
+					]
+				},
+				{
+					"text": "서리",
+					"indices": [
+						63,
+						66
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IwKHzaocHw",
+					"expanded_url": "https://vt.tiktok.com/ZSefbgS81/",
+					"display_url": "vt.tiktok.com/ZSefbgS81/",
+					"indices": [
+						31,
+						54
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1470399405186453500,
+					"id_str": "1470399405186453504",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/FGfod_AVkAAxgNx.png",
+					"media_url_https": "https://pbs.twimg.com/media/FGfod_AVkAAxgNx.png",
+					"url": "https://t.co/ylc2BNDhaZ",
+					"display_url": "pic.twitter.com/ylc2BNDhaZ",
+					"expanded_url": "https://twitter.com/seori_official/status/1470399449361154051/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1470399405186453500,
+					"id_str": "1470399405186453504",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/FGfod_AVkAAxgNx.png",
+					"media_url_https": "https://pbs.twimg.com/media/FGfod_AVkAAxgNx.png",
+					"url": "https://t.co/ylc2BNDhaZ",
+					"display_url": "pic.twitter.com/ylc2BNDhaZ",
+					"expanded_url": "https://twitter.com/seori_official/status/1470399449361154051/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 327,
+							"h": 592,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/o2NawGlgIj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/o2NawGlgIj",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38947,
+			"friends_count": 0,
+			"listed_count": 162,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 428,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 96,
+		"favorite_count": 547,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#TikTok] 냅다 대나무 헬리콥터 #도라에몽\n\n▶ https://t.co/IwKHzaocHw\n\n#Seori #서리 https://t.co/ylc2BNDhaZ"
+	},
+	{
 		"created_at": "Mon Dec 13 12:08:05 +0000 2021",
 		"id": 1470364902304977000,
 		"id_str": "1470364902304976899",
