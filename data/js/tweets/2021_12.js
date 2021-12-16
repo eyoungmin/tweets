4af4d05db2d9cbeed3bccd61c4f6e2a50a60fbb0
Youngmin Lee <iameyoungmin@gmail.com>
2021-12-16 12:02:09 +0900

    Added 1 tweets

--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 16 02:59:17 +0000 2021",
+		"id": 1471313955712864300,
+		"id_str": "1471313955712864256",
+		"full_text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크 리사, 英 기관 선정 세계서 존경받는 여성 ‘톱20’ \nhttps://t.co/uke8W1DNQj \n \n#블랙핑크 #BLACKPINK https://t.co/KiIwqQknER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						81,
+						86
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						87,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uke8W1DNQj",
+					"expanded_url": "https://yg-life.com/archives/171748?lang=ko",
+					"display_url": "yg-life.com/archives/17174…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1471313954207064000,
+					"id_str": "1471313954207064064",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FGsoPvmWYAAlvOP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGsoPvmWYAAlvOP.jpg",
+					"url": "https://t.co/KiIwqQknER",
+					"display_url": "pic.twitter.com/KiIwqQknER",
+					"expanded_url": "https://twitter.com/fromminn/status/1471313955712864256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1471313954207064000,
+					"id_str": "1471313954207064064",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FGsoPvmWYAAlvOP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FGsoPvmWYAAlvOP.jpg",
+					"url": "https://t.co/KiIwqQknER",
+					"display_url": "pic.twitter.com/KiIwqQknER",
+					"expanded_url": "https://twitter.com/fromminn/status/1471313955712864256/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 405,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 321,
+			"friends_count": 1050,
+			"listed_count": 0,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44738,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 89108,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크 리사, 英 기관 선정 세계서 존경받는 여성 ‘톱20’ \nhttps://t.co/uke8W1DNQj \n \n#블랙핑크 #BLACKPINK https://t.co/KiIwqQknER"
+	},
+	{
 		"created_at": "Thu Dec 16 02:01:23 +0000 2021",
 		"id": 1471299045138534400,
 		"id_str": "1471299384868585487",
