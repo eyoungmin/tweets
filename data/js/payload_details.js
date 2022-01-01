af733c440c63f1b5590982d7f1d8bc3934aea14c
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-01 22:02:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84669,
-	"created_at": "Sat Jan 01 12:02:00 +0000 2022",
+	"tweets": 84670,
+	"created_at": "Sat Jan 01 13:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 01 12:40:10 +0000 2022",
+		"id": 1477248300609052700,
+		"id_str": "1477258344725368832",
+		"full_text": "SMTOWN '빛 (Hope from KWANGYA)' MV\n\nhttps://t.co/DdA0Ta4skZ\n\n#SMTOWN_LIVE #SMCU_EXPRESS \n#SMTOWN2022 #SMTOWN #빛 #Hope_from_KWANGYA \n#2021_WINTER_SMTOWN https://t.co/EBbCUf1xjS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN_LIVE",
+					"indices": [
+						60,
+						72
+					]
+				},
+				{
+					"text": "SMCU_EXPRESS",
+					"indices": [
+						73,
+						86
+					]
+				},
+				{
+					"text": "SMTOWN2022",
+					"indices": [
+						88,
+						99
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						100,
+						107
+					]
+				},
+				{
+					"text": "빛",
+					"indices": [
+						108,
+						110
+					]
+				},
+				{
+					"text": "Hope_from_KWANGYA",
+					"indices": [
+						111,
+						129
+					]
+				},
+				{
+					"text": "2021_WINTER_SMTOWN",
+					"indices": [
+						131,
+						150
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DdA0Ta4skZ",
+					"expanded_url": "https://youtu.be/2fmxTXvPmEk",
+					"display_url": "youtu.be/2fmxTXvPmEk",
+					"indices": [
+						35,
+						58
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1477248292644073500,
+					"id_str": "1477248292644073475",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FIA9ftAaMAMZpHo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIA9ftAaMAMZpHo.jpg",
+					"url": "https://t.co/EBbCUf1xjS",
+					"display_url": "pic.twitter.com/EBbCUf1xjS",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1477248300609052677/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1477248292644073500,
+					"id_str": "1477248292644073475",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FIA9ftAaMAMZpHo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIA9ftAaMAMZpHo.jpg",
+					"url": "https://t.co/EBbCUf1xjS",
+					"display_url": "pic.twitter.com/EBbCUf1xjS",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1477248300609052677/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YWlJE5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YWlJE5",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10393583,
+			"friends_count": 56,
+			"listed_count": 11482,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2026,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11579,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468958715566985226/eed5B2Wp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468958715566985226/eed5B2Wp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1639062025",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 22797,
+		"favorite_count": 50523,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SMTOWN '빛 (Hope from KWANGYA)' MV\n\nhttps://t.co/DdA0Ta4skZ\n\n#SMTOWN_LIVE #SMCU_EXPRESS \n#SMTOWN2022 #SMTOWN #빛 #Hope_from_KWANGYA \n#2021_WINTER_SMTOWN https://t.co/EBbCUf1xjS"
+	},
+	{
 		"created_at": "Sat Jan 01 11:19:51 +0000 2022",
 		"id": 1477230891223363600,
 		"id_str": "1477238134492565510",
