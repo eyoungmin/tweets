f35c0a3010551901d20f5f6fb321d758d6c67b35
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-02 03:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84674,
-	"created_at": "Sat Jan 01 16:02:00 +0000 2022",
+	"tweets": 84675,
+	"created_at": "Sat Jan 01 18:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 01 17:40:09 +0000 2022",
+		"id": 1477318316976914400,
+		"id_str": "1477333840511254537",
+		"full_text": "📣 WERE PUTTING OUT A SONG TOGETHER  its called Head on Fire 👯‍♀️🔥and we love it and it’s coming out but you must presave it https://t.co/x0RUvmpfzV\nG&amp;S xx @thisissigrid https://t.co/ig3cIlPXU5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "thisissigrid",
+					"name": "sigrid",
+					"id": 819042492,
+					"id_str": "819042492",
+					"indices": [
+						159,
+						172
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/x0RUvmpfzV",
+					"expanded_url": "https://griff.lol/HeadOnFire-PreRelease",
+					"display_url": "griff.lol/HeadOnFire-Pre…",
+					"indices": [
+						124,
+						147
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1477317895835238400,
+					"id_str": "1477317895835238401",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1477317895835238401/pu/img/8EKQRF38CA3PxrYs.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1477317895835238401/pu/img/8EKQRF38CA3PxrYs.jpg",
+					"url": "https://t.co/ig3cIlPXU5",
+					"display_url": "pic.twitter.com/ig3cIlPXU5",
+					"expanded_url": "https://twitter.com/wiffygriffy/status/1477318316976914436/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 848,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 385,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 848,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1477317895835238400,
+					"id_str": "1477317895835238401",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1477317895835238401/pu/img/8EKQRF38CA3PxrYs.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1477317895835238401/pu/img/8EKQRF38CA3PxrYs.jpg",
+					"url": "https://t.co/ig3cIlPXU5",
+					"display_url": "pic.twitter.com/ig3cIlPXU5",
+					"expanded_url": "https://twitter.com/wiffygriffy/status/1477318316976914436/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 480,
+							"h": 848,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 385,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 848,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							30,
+							53
+						],
+						"duration_millis": 21898,
+						"variants": [
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1477317895835238401/pu/vid/480x848/VWJCfnR0C3BToIAb.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1477317895835238401/pu/pl/XoHeQYtGYqtXv18L.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1477317895835238401/pu/vid/320x564/PWmgf6NIZeCJawA6.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1065546915406848000,
+			"id_str": "1065546915406848001",
+			"name": "Griff",
+			"screen_name": "wiffygriffy",
+			"location": "",
+			"description": "Presave mine & Sigrid’s song 💕🔥💕👯‍♀️ ⬇️⬇️",
+			"url": "https://t.co/uD3Z03eCwp",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uD3Z03eCwp",
+							"expanded_url": "https://griff.lol/HeadOnFire-PreRelease",
+							"display_url": "griff.lol/HeadOnFire-Pre…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17884,
+			"friends_count": 102,
+			"listed_count": 72,
+			"created_at": "Thu Nov 22 10:05:56 +0000 2018",
+			"favourites_count": 561,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 617,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1430881870221168641/BRcbjQAy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1430881870221168641/BRcbjQAy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1065546915406848001/1629998115",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 64,
+		"favorite_count": 487,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "📣 WERE PUTTING OUT A SONG TOGETHER  its called Head on Fire 👯‍♀️🔥and we love it and it’s coming out but you must presave it https://t.co/x0RUvmpfzV\nG&amp;S xx @thisissigrid https://t.co/ig3cIlPXU5"
+	},
+	{
 		"created_at": "Sat Jan 01 15:29:39 +0000 2022",
 		"id": 1477293547766395000,
 		"id_str": "1477300996212760578",
