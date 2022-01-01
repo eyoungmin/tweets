f7d880b74309c9d7377cb2389f13eee6e50d58d5
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-01 11:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84649,
-	"created_at": "Sat Jan 01 01:02:10 +0000 2022",
+	"tweets": 84650,
+	"created_at": "Sat Jan 01 02:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,251 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 01 01:29:38 +0000 2022",
+		"id": 1477078250228953000,
+		"id_str": "1477089602943000576",
+		"full_text": "[선공개] 이게 원조 빨간 맛!💓\n2학년 VS 3학년의 본격 끼 대결🙌\n\n&lt;#방과후설렘&gt; 일요일 밤 9시 방송\n#옥주현 #권유리 #아이키 #소연\nMBC220102방송 https://t.co/9rtJXxxpjc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "방과후설렘",
+					"indices": [
+						45,
+						51
+					]
+				},
+				{
+					"text": "옥주현",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "권유리",
+					"indices": [
+						73,
+						77
+					]
+				},
+				{
+					"text": "아이키",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "소연",
+					"indices": [
+						83,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1477077669498192000,
+					"id_str": "1477077669498191873",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/FH-iy5macAYHRCB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FH-iy5macAYHRCB.jpg",
+					"url": "https://t.co/9rtJXxxpjc",
+					"display_url": "pic.twitter.com/9rtJXxxpjc",
+					"expanded_url": "https://twitter.com/withMBC/status/1477078250228953094/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1477077669498192000,
+					"id_str": "1477077669498191873",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/FH-iy5macAYHRCB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FH-iy5macAYHRCB.jpg",
+					"url": "https://t.co/9rtJXxxpjc",
+					"display_url": "pic.twitter.com/9rtJXxxpjc",
+					"expanded_url": "https://twitter.com/withMBC/status/1477078250228953094/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 289122,
+						"variants": [
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1477077669498191873/vid/480x270/XISRuGlpTH20Di2t.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1477077669498191873/vid/640x360/-IAU17KneRmn5qQL.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1477077669498191873/pl/xQ-29Aj3ae-NtPCd.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1477077669498191873/vid/1280x720/WdPdt6JXOkdm3bJJ.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 104673579,
+			"id_str": "104673579",
+			"name": "withMBC",
+			"screen_name": "withMBC",
+			"location": "상암동",
+			"description": "📺𝖮𝖿𝖿𝗂𝖼𝗂𝖺𝗅 Twitter 𝗈𝖿 𝖬𝗎𝗇𝗁𝗐𝖺 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍𝗂𝗇𝗀 𝖢𝗈𝗋𝗉.",
+			"url": "https://t.co/6dtNDitrVn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6dtNDitrVn",
+							"expanded_url": "http://www.imbc.com",
+							"display_url": "imbc.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1010240,
+			"friends_count": 9987,
+			"listed_count": 4229,
+			"created_at": "Thu Jan 14 01:40:58 +0000 2010",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 46759,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FCFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1326085413245612034/W0IFxoZo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/104673579/1636707798",
+			"profile_link_color": "1B3E8F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FCFCFC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 25,
+		"favorite_count": 83,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[선공개] 이게 원조 빨간 맛!💓\n2학년 VS 3학년의 본격 끼 대결🙌\n\n&lt;#방과후설렘&gt; 일요일 밤 9시 방송\n#옥주현 #권유리 #아이키 #소연\nMBC220102방송 https://t.co/9rtJXxxpjc"
+	},
+	{
 		"created_at": "Sat Jan 01 00:59:49 +0000 2022",
 		"id": 1477082098469154800,
 		"id_str": "1477082098469154818",
