42c302564e3782fd847dbf61ad69757db19ab87a
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-02 20:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84684,
-	"created_at": "Sun Jan 02 10:02:00 +0000 2022",
+	"tweets": 84685,
+	"created_at": "Sun Jan 02 11:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 02 10:11:41 +0000 2022",
+		"id": 1477580431436484600,
+		"id_str": "1477583365314392066",
+		"full_text": "어흥🐯 진짜 동물 발로 변신한 제니, 두아리파 패션\n\n#부츠 #겨울신발 #두아리파 #릴체리 #로타볼코바 #오눅 #저스틴 #헤일리비버 #제니 #블랙핑크 #블핑\nhttps://t.co/LDaXX0eKCR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "부츠",
+					"indices": [
+						30,
+						33
+					]
+				},
+				{
+					"text": "겨울신발",
+					"indices": [
+						34,
+						39
+					]
+				},
+				{
+					"text": "두아리파",
+					"indices": [
+						40,
+						45
+					]
+				},
+				{
+					"text": "릴체리",
+					"indices": [
+						46,
+						50
+					]
+				},
+				{
+					"text": "로타볼코바",
+					"indices": [
+						51,
+						57
+					]
+				},
+				{
+					"text": "오눅",
+					"indices": [
+						58,
+						61
+					]
+				},
+				{
+					"text": "저스틴",
+					"indices": [
+						62,
+						66
+					]
+				},
+				{
+					"text": "헤일리비버",
+					"indices": [
+						67,
+						73
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						74,
+						77
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						78,
+						83
+					]
+				},
+				{
+					"text": "블핑",
+					"indices": [
+						84,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LDaXX0eKCR",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/62409?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/62409?…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88723,
+			"friends_count": 3,
+			"listed_count": 400,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 127,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "어흥🐯 진짜 동물 발로 변신한 제니, 두아리파 패션\n\n#부츠 #겨울신발 #두아리파 #릴체리 #로타볼코바 #오눅 #저스틴 #헤일리비버 #제니 #블랙핑크 #블핑\nhttps://t.co/LDaXX0eKCR"
+	},
+	{
 		"created_at": "Sun Jan 02 09:56:37 +0000 2022",
 		"id": 1477579574615625700,
 		"id_str": "1477579574615625729",
