5ad29c35938b9588211fd39d2dbb2862bc8dc852
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-05 10:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84764,
-	"created_at": "Tue Jan 04 20:02:01 +0000 2022",
+	"tweets": 84765,
+	"created_at": "Wed Jan 05 01:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,115 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 05 00:31:55 +0000 2022",
+		"id": 1478524625969721300,
+		"id_str": "1478524625969721348",
+		"full_text": "#정밀아 #Jeongmilla\n[MV]방랑Wandering\nhttps://t.co/akn93IBc0H",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "정밀아",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "Jeongmilla",
+					"indices": [
+						5,
+						16
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/akn93IBc0H",
+					"expanded_url": "https://www.youtube.com/watch?v=48oBJ_nurl8",
+					"display_url": "youtube.com/watch?v=48oBJ_…",
+					"indices": [
+						33,
+						56
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 319,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44648,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 90026,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#정밀아 #Jeongmilla\n[MV]방랑Wandering\nhttps://t.co/akn93IBc0H"
+	},
+	{
 		"created_at": "Tue Jan 04 19:40:09 +0000 2022",
 		"id": 1478443992593358800,
 		"id_str": "1478451203470147586",
@@ -258,7 +367,7 @@ Grailbird.data.tweets_2022_01 =
 				"name": "Griff",
 				"screen_name": "wiffygriffy",
 				"location": "",
-				"description": "Presave mine & Sigrid’s song 💕🔥💕👯‍♀️ ⬇️⬇️",
+				"description": "Presave mine & Sigrid’s song 💕🔥💕👯♀️ ⬇️⬇️",
 				"url": "https://t.co/uD3Z03eCwp",
 				"entities": {
 					"url": {
