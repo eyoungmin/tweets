41e9f86bf0092de1a55479ab011495b64a973042
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-09 19:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 09 09:40:10 +0000 2022",
+		"id": 1480103683283374000,
+		"id_str": "1480112148567187458",
+		"full_text": "2컷 만화\nㅋㅋㅋㅋ\n\n#뻬옹 #뻬옹뻬옹 https://t.co/hKxHXV7sdp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			21
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "뻬옹",
+					"indices": [
+						12,
+						15
+					]
+				},
+				{
+					"text": "뻬옹뻬옹",
+					"indices": [
+						16,
+						21
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1480103676329234400,
+					"id_str": "1480103676329234432",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/FIpic6JakAANpW6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIpic6JakAANpW6.jpg",
+					"url": "https://t.co/hKxHXV7sdp",
+					"display_url": "pic.twitter.com/hKxHXV7sdp",
+					"expanded_url": "https://twitter.com/love_yuna/status/1480103683283374080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1547,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 633,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1117,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1480103676329234400,
+					"id_str": "1480103676329234432",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/FIpic6JakAANpW6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIpic6JakAANpW6.jpg",
+					"url": "https://t.co/hKxHXV7sdp",
+					"display_url": "pic.twitter.com/hKxHXV7sdp",
+					"expanded_url": "https://twitter.com/love_yuna/status/1480103683283374080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1547,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 633,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1117,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1480103676371177500,
+					"id_str": "1480103676371177473",
+					"indices": [
+						22,
+						45
+					],
+					"media_url": "http://pbs.twimg.com/media/FIpic6TakAEKXl2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIpic6TakAEKXl2.jpg",
+					"url": "https://t.co/hKxHXV7sdp",
+					"display_url": "pic.twitter.com/hKxHXV7sdp",
+					"expanded_url": "https://twitter.com/love_yuna/status/1480103683283374080/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 634,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 772,
+							"h": 828,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 772,
+							"h": 828,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274148,
+			"friends_count": 554,
+			"listed_count": 2802,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 360,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6617,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1635493925",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 74,
+		"favorite_count": 157,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2컷 만화\nㅋㅋㅋㅋ\n\n#뻬옹 #뻬옹뻬옹 https://t.co/hKxHXV7sdp"
+	},
+	{
 		"created_at": "Sun Jan 09 08:21:47 +0000 2022",
 		"id": 1480090023496396800,
 		"id_str": "1480092426152910848",
