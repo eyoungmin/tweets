3fdf6a0bfc5bbe2ab79f0e73bde0874b6c0e5ff3
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-11 11:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 11 01:29:39 +0000 2022",
+		"id": 1480706856767357000,
+		"id_str": "1480713482299453440",
+		"full_text": "#해피뉴이어 를 본다 vs ✔두 번 본다\n밸런스 게임은 역시 과몰입해야 제맛👍\n\n📌 &lt;해피 뉴 이어&gt; 과몰입 가보자고!\nhttps://t.co/kbTEEq3B3V\n\n #한지민 #이동욱 #강하늘 #임윤아 #원진아 #이혜영 #정진영 #김영광 #서강준 #이광수 #고성희 #이진욱 #조준영 #원지안 #티빙과극장에서대공개 https://t.co/tLYBPhy0Zm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "해피뉴이어",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "한지민",
+					"indices": [
+						99,
+						103
+					]
+				},
+				{
+					"text": "이동욱",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "강하늘",
+					"indices": [
+						109,
+						113
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						114,
+						118
+					]
+				},
+				{
+					"text": "원진아",
+					"indices": [
+						119,
+						123
+					]
+				},
+				{
+					"text": "이혜영",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "정진영",
+					"indices": [
+						129,
+						133
+					]
+				},
+				{
+					"text": "김영광",
+					"indices": [
+						134,
+						138
+					]
+				},
+				{
+					"text": "서강준",
+					"indices": [
+						139,
+						143
+					]
+				},
+				{
+					"text": "이광수",
+					"indices": [
+						144,
+						148
+					]
+				},
+				{
+					"text": "고성희",
+					"indices": [
+						149,
+						153
+					]
+				},
+				{
+					"text": "이진욱",
+					"indices": [
+						154,
+						158
+					]
+				},
+				{
+					"text": "조준영",
+					"indices": [
+						159,
+						163
+					]
+				},
+				{
+					"text": "원지안",
+					"indices": [
+						164,
+						168
+					]
+				},
+				{
+					"text": "티빙과극장에서대공개",
+					"indices": [
+						169,
+						180
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kbTEEq3B3V",
+					"expanded_url": "https://linktr.ee/cjenmmovie",
+					"display_url": "linktr.ee/cjenmmovie",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1480706604253458400,
+					"id_str": "1480706604253458432",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1480706604253458432/pu/img/Iy7v6Tq0ieY1HrbP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1480706604253458432/pu/img/Iy7v6Tq0ieY1HrbP.jpg",
+					"url": "https://t.co/tLYBPhy0Zm",
+					"display_url": "pic.twitter.com/tLYBPhy0Zm",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1480706856767356931/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1480706604253458400,
+					"id_str": "1480706604253458432",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1480706604253458432/pu/img/Iy7v6Tq0ieY1HrbP.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1480706604253458432/pu/img/Iy7v6Tq0ieY1HrbP.jpg",
+					"url": "https://t.co/tLYBPhy0Zm",
+					"display_url": "pic.twitter.com/tLYBPhy0Zm",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1480706856767356931/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 94761,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1480706604253458432/pu/vid/1280x720/WP367CVHnQO1hrlV.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1480706604253458432/pu/pl/TPoGjg6vcd7sA5ha.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1480706604253458432/pu/vid/480x270/lrD8rYhwucY_XS7N.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1480706604253458432/pu/vid/640x360/WYaUJ6g9_7uQPM77.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 452664114,
+			"id_str": "452664114",
+			"name": "CJ ENM Movie",
+			"screen_name": "CJENMMOVIE",
+			"location": "",
+			"description": "아시아 No.1 스튜디오 CJ ENM_Movie 공식 트위터입니다. 27년 동안 한 우물만 파고 있는 C네마을🏡",
+			"url": "https://t.co/RJkRVcpfbL",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RJkRVcpfbL",
+							"expanded_url": "https://www.youtube.com/user/cjenmmovie",
+							"display_url": "youtube.com/user/cjenmmovie",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 329416,
+			"friends_count": 10763,
+			"listed_count": 231,
+			"created_at": "Mon Jan 02 03:32:49 +0000 2012",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6394,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/452664114/1625636011",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 92,
+		"favorite_count": 113,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#해피뉴이어 를 본다 vs ✔두 번 본다\n밸런스 게임은 역시 과몰입해야 제맛👍\n\n📌 &lt;해피 뉴 이어&gt; 과몰입 가보자고!\nhttps://t.co/kbTEEq3B3V\n\n #한지민 #이동욱 #강하늘 #임윤아 #원진아 #이혜영 #정진영 #김영광 #서강준 #이광수 #고성희 #이진욱 #조준영 #원지안 #티빙과극장에서대공개 https://t.co/tLYBPhy0Zm"
+	},
+	{
 		"created_at": "Mon Jan 10 17:05:03 +0000 2022",
 		"id": 1480586498554704000,
 		"id_str": "1480586498554703874",
