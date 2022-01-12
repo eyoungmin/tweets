de4bbbb5857ffd46f2412d8d27bbdcef1f830a8f
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-13 05:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84922,
-	"created_at": "Wed Jan 12 16:02:04 +0000 2022",
+	"tweets": 84923,
+	"created_at": "Wed Jan 12 20:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 12 19:40:12 +0000 2022",
+		"id": 1481336769941586000,
+		"id_str": "1481350316415242245",
+		"full_text": "make sure you pre save it and if you do, there’s a prize 😏 https://t.co/3DHXDGXpKL https://t.co/qI5WCqZpBk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3DHXDGXpKL",
+					"expanded_url": "https://griff.lol/HeadOnFire-PreRelease",
+					"display_url": "griff.lol/HeadOnFire-Pre…",
+					"indices": [
+						59,
+						82
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1481336761964019700,
+					"id_str": "1481336761964019712",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FI7D78GXwAAma2P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FI7D78GXwAAma2P.jpg",
+					"url": "https://t.co/qI5WCqZpBk",
+					"display_url": "pic.twitter.com/qI5WCqZpBk",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1481336769941585921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1481336761964019700,
+					"id_str": "1481336761964019712",
+					"indices": [
+						83,
+						106
+					],
+					"media_url": "http://pbs.twimg.com/media/FI7D78GXwAAma2P.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FI7D78GXwAAma2P.jpg",
+					"url": "https://t.co/qI5WCqZpBk",
+					"display_url": "pic.twitter.com/qI5WCqZpBk",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1481336769941585921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1481336446199943200,
+		"in_reply_to_status_id_str": "1481336446199943177",
+		"in_reply_to_user_id": 819042492,
+		"in_reply_to_user_id_str": "819042492",
+		"in_reply_to_screen_name": "thisissigrid",
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "Home To You (This Christmas) is out now ❤️🎄",
+			"url": "https://t.co/H90SsfhYEH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H90SsfhYEH",
+							"expanded_url": "https://Sigrid.lnk.to/HomeToYouThisChristmasTW",
+							"display_url": "Sigrid.lnk.to/HomeToYouThisC…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84487,
+			"friends_count": 265,
+			"listed_count": 312,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 5944,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4244,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1636108169",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 86,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "make sure you pre save it and if you do, there’s a prize 😏 https://t.co/3DHXDGXpKL https://t.co/qI5WCqZpBk"
+	},
+	{
 		"created_at": "Wed Jan 12 15:40:10 +0000 2022",
 		"id": 1481288885552382000,
 		"id_str": "1481289910799716357",
