cce59d43ef5e3db5d67aca7f7d36aec35bcc8c40
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-16 04:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 84997,
-	"created_at": "Sat Jan 15 16:02:03 +0000 2022",
+	"tweets": 84998,
+	"created_at": "Sat Jan 15 19:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sat Jan 15 18:40:10 +0000 2022",
+		"id": 1482408137596493800,
+		"id_str": "1482422371856310277",
+		"full_text": "TONIGHT // @iHeartRadio's ALTer EGO show live from LA // Performance time 8pm PT / 11pm ET. Tune in or watch live on @livexlive: https://t.co/gcCUmZ6XTD #iHeartALT2022 https://t.co/78DiV2CDhV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "iHeartALT2022",
+					"indices": [
+						153,
+						167
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iHeartRadio",
+					"name": "iHeartRadio",
+					"id": 46116615,
+					"id_str": "46116615",
+					"indices": [
+						11,
+						23
+					]
+				},
+				{
+					"screen_name": "livexlive",
+					"name": "LiveXLive",
+					"id": 3148559064,
+					"id_str": "3148559064",
+					"indices": [
+						117,
+						127
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gcCUmZ6XTD",
+					"expanded_url": "https://ihr.fm/iHeartALT2022",
+					"display_url": "ihr.fm/iHeartALT2022",
+					"indices": [
+						129,
+						152
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1482408127626686500,
+					"id_str": "1482408127626686465",
+					"indices": [
+						168,
+						191
+					],
+					"media_url": "http://pbs.twimg.com/media/FJKSVoNXMAEJWd_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJKSVoNXMAEJWd_.jpg",
+					"url": "https://t.co/78DiV2CDhV",
+					"display_url": "pic.twitter.com/78DiV2CDhV",
+					"expanded_url": "https://twitter.com/coldplay/status/1482408137596493830/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1482408127626686500,
+					"id_str": "1482408127626686465",
+					"indices": [
+						168,
+						191
+					],
+					"media_url": "http://pbs.twimg.com/media/FJKSVoNXMAEJWd_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJKSVoNXMAEJWd_.jpg",
+					"url": "https://t.co/78DiV2CDhV",
+					"display_url": "pic.twitter.com/78DiV2CDhV",
+					"expanded_url": "https://twitter.com/coldplay/status/1482408137596493830/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Epiphane",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23543072,
+			"friends_count": 775,
+			"listed_count": 42658,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1534,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8950,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 211,
+		"favorite_count": 1904,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TONIGHT // @iHeartRadio's ALTer EGO show live from LA // Performance time 8pm PT / 11pm ET. Tune in or watch live on @livexlive: https://t.co/gcCUmZ6XTD #iHeartALT2022 https://t.co/78DiV2CDhV"
+	},
+	{
 		"created_at": "Sat Jan 15 15:54:49 +0000 2022",
 		"id": 1482380760199925800,
 		"id_str": "1482380760199925766",
