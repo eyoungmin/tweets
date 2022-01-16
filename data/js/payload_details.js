48ccfc051d98309de06e6852b192a70cd94a5775
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-16 21:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85007,
-	"created_at": "Sun Jan 16 10:02:06 +0000 2022",
+	"tweets": 85008,
+	"created_at": "Sun Jan 16 12:02:09 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 16 11:37:29 +0000 2022",
+		"id": 1482671104426909700,
+		"id_str": "1482678387848228870",
+		"full_text": "#THANKUBLINK \n\n고마운 #BLINK 에게 전하는\n#JENNIE의 메시지와 특별한 생일 계획🖍이\n#BLACKPINK Weverse에 공개되었습니다💌\n🥟젠득이의 생일을 축하해주신 BLINK 여러분 감사드립니다🖤💓\n\n▶️ https://t.co/KdAjfv1Wq8\n\n#블랙핑크 #제니\n#빛이나는제니_오늘제일빛나\n#OurStarJennieDay",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "THANKUBLINK",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"text": "BLINK",
+					"indices": [
+						19,
+						25
+					]
+				},
+				{
+					"text": "JENNIE의",
+					"indices": [
+						33,
+						41
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						59,
+						69
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						152,
+						157
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						158,
+						161
+					]
+				},
+				{
+					"text": "빛이나는제니_오늘제일빛나",
+					"indices": [
+						162,
+						176
+					]
+				},
+				{
+					"text": "OurStarJennieDay",
+					"indices": [
+						177,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KdAjfv1Wq8",
+					"expanded_url": "https://www.weverse.io/blackpink/media/12707",
+					"display_url": "weverse.io/blackpink/medi…",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/2Kh5HZQu7Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2Kh5HZQu7Y",
+							"expanded_url": "http://blackpinkofficial.com",
+							"display_url": "blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7867178,
+			"friends_count": 9,
+			"listed_count": 8700,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 51,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2027,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1631247166",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2106,
+		"favorite_count": 7318,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#THANKUBLINK \n\n고마운 #BLINK 에게 전하는\n#JENNIE의 메시지와 특별한 생일 계획🖍이\n#BLACKPINK Weverse에 공개되었습니다💌\n🥟젠득이의 생일을 축하해주신 BLINK 여러분 감사드립니다🖤💓\n\n▶️ https://t.co/KdAjfv1Wq8\n\n#블랙핑크 #제니\n#빛이나는제니_오늘제일빛나\n#OurStarJennieDay"
+	},
+	{
 		"created_at": "Sun Jan 16 09:40:10 +0000 2022",
 		"id": 1482607924115558400,
 		"id_str": "1482648864167542784",
