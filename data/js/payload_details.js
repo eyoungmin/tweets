41f7cc7ae1408c1bd3b5a1d2c65e2e2c7b0be07f
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-16 13:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85000,
-	"created_at": "Sun Jan 16 03:02:07 +0000 2022",
+	"tweets": 85001,
+	"created_at": "Sun Jan 16 04:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 16 03:44:23 +0000 2022",
+		"id": 1482559331317043200,
+		"id_str": "1482559331317043200",
+		"full_text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크 리사, 2주 차 가온 소셜차트 TOP2 등극…솔로가수 1위 \nhttps://t.co/3lA7GAbiKO \n \n#블랙핑크 #BLACKPINK https://t.co/ZgmpWNdur4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YG라이프",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "YG엔터테인먼트",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						84,
+						89
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						90,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3lA7GAbiKO",
+					"expanded_url": "https://yg-life.com/archives/172048?lang=ko",
+					"display_url": "yg-life.com/archives/17204…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1482559329257545700,
+					"id_str": "1482559329257545730",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FJMb2uLWYAIYQdx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJMb2uLWYAIYQdx.jpg",
+					"url": "https://t.co/ZgmpWNdur4",
+					"display_url": "pic.twitter.com/ZgmpWNdur4",
+					"expanded_url": "https://twitter.com/fromminn/status/1482559331317043200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1482559329257545700,
+					"id_str": "1482559329257545730",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FJMb2uLWYAIYQdx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJMb2uLWYAIYQdx.jpg",
+					"url": "https://t.co/ZgmpWNdur4",
+					"display_url": "pic.twitter.com/ZgmpWNdur4",
+					"expanded_url": "https://twitter.com/fromminn/status/1482559331317043200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4531360753,
+			"id_str": "4531360753",
+			"name": "MINN",
+			"screen_name": "fromminn",
+			"location": "",
+			"description": "덕질과 음악에 대해 이야기 합니다.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 320,
+			"friends_count": 1052,
+			"listed_count": 6,
+			"created_at": "Fri Dec 11 19:47:40 +0000 2015",
+			"favourites_count": 44640,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 90332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E4E4E4",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1332735793580806144/qUzxqfzV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4531360753/1591910258",
+			"profile_link_color": "848484",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#YG라이프 #YG엔터테인먼트 \n블랙핑크 리사, 2주 차 가온 소셜차트 TOP2 등극…솔로가수 1위 \nhttps://t.co/3lA7GAbiKO \n \n#블랙핑크 #BLACKPINK https://t.co/ZgmpWNdur4"
+	},
+	{
 		"created_at": "Sun Jan 16 02:29:39 +0000 2022",
 		"id": 1482533062940315600,
 		"id_str": "1482540521268690944",
