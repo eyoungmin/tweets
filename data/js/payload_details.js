c08fcfe9a061348d2b120c3a6817451374af771b
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-17 04:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85017,
-	"created_at": "Sun Jan 16 18:02:03 +0000 2022",
+	"tweets": 85018,
+	"created_at": "Sun Jan 16 19:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,300 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 16 18:40:10 +0000 2022",
+		"id": 1482782540276637700,
+		"id_str": "1482784759822573569",
+		"full_text": "Last night's @iHeartRadio ALTer EGO show in LA. \n\n📷 Jeff Kravitz, Kevin Winter, Anna Lee, Kevin Mazur https://t.co/KLOvw4wHLM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iHeartRadio",
+					"name": "iHeartRadio",
+					"id": 46116615,
+					"id_str": "46116615",
+					"indices": [
+						13,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1482782443321110500,
+					"id_str": "1482782443321110529",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FJPmxq1XMAEosYC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJPmxq1XMAEosYC.jpg",
+					"url": "https://t.co/KLOvw4wHLM",
+					"display_url": "pic.twitter.com/KLOvw4wHLM",
+					"expanded_url": "https://twitter.com/coldplay/status/1482782540276637699/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1482782443321110500,
+					"id_str": "1482782443321110529",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FJPmxq1XMAEosYC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJPmxq1XMAEosYC.jpg",
+					"url": "https://t.co/KLOvw4wHLM",
+					"display_url": "pic.twitter.com/KLOvw4wHLM",
+					"expanded_url": "https://twitter.com/coldplay/status/1482782540276637699/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1482782443316957200,
+					"id_str": "1482782443316957184",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FJPmxq0X0AAOqp0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJPmxq0X0AAOqp0.jpg",
+					"url": "https://t.co/KLOvw4wHLM",
+					"display_url": "pic.twitter.com/KLOvw4wHLM",
+					"expanded_url": "https://twitter.com/coldplay/status/1482782540276637699/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1482782443316912000,
+					"id_str": "1482782443316912131",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FJPmxq0XIAMDfYK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJPmxq0XIAMDfYK.jpg",
+					"url": "https://t.co/KLOvw4wHLM",
+					"display_url": "pic.twitter.com/KLOvw4wHLM",
+					"expanded_url": "https://twitter.com/coldplay/status/1482782540276637699/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 1334,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1482782443564380200,
+					"id_str": "1482782443564380161",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/FJPmxrvXMAEXpxJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJPmxrvXMAEXpxJ.jpg",
+					"url": "https://t.co/KLOvw4wHLM",
+					"display_url": "pic.twitter.com/KLOvw4wHLM",
+					"expanded_url": "https://twitter.com/coldplay/status/1482782540276637699/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Epiphane",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23543920,
+			"friends_count": 775,
+			"listed_count": 42654,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1537,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8953,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 284,
+		"favorite_count": 2596,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Last night's @iHeartRadio ALTer EGO show in LA. \n\n📷 Jeff Kravitz, Kevin Winter, Anna Lee, Kevin Mazur https://t.co/KLOvw4wHLM"
+	},
+	{
 		"created_at": "Sun Jan 16 17:53:28 +0000 2022",
 		"id": 1482773007945048000,
 		"id_str": "1482773007945048064",
