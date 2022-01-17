7692cac37aa3b3b50e7776b5373ba7e95417cc46
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-17 21:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85035,
-	"created_at": "Mon Jan 17 11:02:04 +0000 2022",
+	"tweets": 85036,
+	"created_at": "Mon Jan 17 12:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 17 11:27:06 +0000 2022",
+		"id": 1483037804552945700,
+		"id_str": "1483038165724241920",
+		"full_text": "[#김립 / #KimLip] 1. 저런 글을 적은 비비언니가 너무 귀여워서 끝나고 몰래 찰칵,,사랑해 언니,,,😗 2. 단 거 그만먹고싶어,,,누가 방법을 좀 제발 알려줘,,,혼자 열심히 싸웠어요,,근데 ,,4개나 먹음\n\n#이달의소녀 #LOONA https://t.co/XkeSaTCCze",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김립",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "KimLip",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						124,
+						130
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						131,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483037793962295300,
+					"id_str": "1483037793962295297",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/FJTPBCGaAAE0Dqo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJTPBCGaAAE0Dqo.jpg",
+					"url": "https://t.co/XkeSaTCCze",
+					"display_url": "pic.twitter.com/XkeSaTCCze",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1483037804552945665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483037793962295300,
+					"id_str": "1483037793962295297",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/FJTPBCGaAAE0Dqo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJTPBCGaAAE0Dqo.jpg",
+					"url": "https://t.co/XkeSaTCCze",
+					"display_url": "pic.twitter.com/XkeSaTCCze",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1483037804552945665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1483037793958137900,
+					"id_str": "1483037793958137856",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/FJTPBCFakAAG_SR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJTPBCFakAAG_SR.jpg",
+					"url": "https://t.co/XkeSaTCCze",
+					"display_url": "pic.twitter.com/XkeSaTCCze",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1483037804552945665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 845611,
+			"friends_count": 1,
+			"listed_count": 8223,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3754,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1684,
+		"favorite_count": 5830,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김립 / #KimLip] 1. 저런 글을 적은 비비언니가 너무 귀여워서 끝나고 몰래 찰칵,,사랑해 언니,,,😗 2. 단 거 그만먹고싶어,,,누가 방법을 좀 제발 알려줘,,,혼자 열심히 싸웠어요,,근데 ,,4개나 먹음\n\n#이달의소녀 #LOONA https://t.co/XkeSaTCCze"
+	},
+	{
 		"created_at": "Mon Jan 17 10:40:10 +0000 2022",
 		"id": 1483013628244934700,
 		"id_str": "1483026351355535360",
