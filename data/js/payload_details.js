4d7b68af925d406274daef3d3100ad27fa21a46d
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-18 09:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85044,
-	"created_at": "Mon Jan 17 21:02:04 +0000 2022",
+	"tweets": 85045,
+	"created_at": "Tue Jan 18 00:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,265 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Mon Jan 17 23:29:39 +0000 2022",
+		"id": 1483204987387891700,
+		"id_str": "1483219999087026179",
+		"full_text": "[선공개] 사실 형은 오늘 많이 참았다 😇\n길가에 동생 버리고 가는 친형 모먼트 🚗\n#호적메이트 오늘 밤 9시 💞\n#이경규 #김정은 #딘딘 \n#김진우 #최수영 #조준호\nMBC220118방송 https://t.co/8vl8PoYl6W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "호적메이트",
+					"indices": [
+						47,
+						53
+					]
+				},
+				{
+					"text": "이경규",
+					"indices": [
+						64,
+						68
+					]
+				},
+				{
+					"text": "김정은",
+					"indices": [
+						69,
+						73
+					]
+				},
+				{
+					"text": "딘딘",
+					"indices": [
+						74,
+						77
+					]
+				},
+				{
+					"text": "김진우",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						84,
+						88
+					]
+				},
+				{
+					"text": "조준호",
+					"indices": [
+						89,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483096815214166000,
+					"id_str": "1483096815214166019",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/FJUF0hUagAA3mgp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJUF0hUagAA3mgp.jpg",
+					"url": "https://t.co/8vl8PoYl6W",
+					"display_url": "pic.twitter.com/8vl8PoYl6W",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1483204987387891716/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483096815214166000,
+					"id_str": "1483096815214166019",
+					"indices": [
+						106,
+						129
+					],
+					"media_url": "http://pbs.twimg.com/media/FJUF0hUagAA3mgp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJUF0hUagAA3mgp.jpg",
+					"url": "https://t.co/8vl8PoYl6W",
+					"display_url": "pic.twitter.com/8vl8PoYl6W",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1483204987387891716/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 179746,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1483096815214166019/vid/1280x720/lS-1A-ek1vQYXMXX.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1483096815214166019/vid/480x270/bQZs3UCZj8t4vlku.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1483096815214166019/vid/640x360/5O2jSyyVsbMY_trT.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1483096815214166019/pl/76wfivykdwv5voa5.m3u8?tag=14&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144012572,
+			"id_str": "4144012572",
+			"name": "MBC 예능연구소",
+			"screen_name": "MBC_entertain",
+			"location": "",
+			"description": "MBC 예능의 꿀잼만 모아 드림 [official]",
+			"url": "https://t.co/7U66BOeSeR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7U66BOeSeR",
+							"expanded_url": "http://facebook.com/MBCentertain",
+							"display_url": "facebook.com/MBCentertain",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 929356,
+			"friends_count": 6,
+			"listed_count": 995,
+			"created_at": "Fri Nov 06 08:21:55 +0000 2015",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 16807,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144012572/1555047387",
+			"profile_link_color": "45B7DE",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[선공개] 사실 형은 오늘 많이 참았다 😇\n길가에 동생 버리고 가는 친형 모먼트 🚗\n#호적메이트 오늘 밤 9시 💞\n#이경규 #김정은 #딘딘 \n#김진우 #최수영 #조준호\nMBC220118방송 https://t.co/8vl8PoYl6W"
+	},
+	{
 		"created_at": "Mon Jan 17 20:40:10 +0000 2022",
 		"id": 1483168064397099000,
 		"id_str": "1483177346354102279",
@@ -274,7 +533,7 @@ Grailbird.data.tweets_2022_01 =
 				"name": "Griff",
 				"screen_name": "wiffygriffy",
 				"location": "",
-				"description": "Presave mine & Sigrid’s song 💕🔥💕👯‍♀️ ⬇️⬇️",
+				"description": "Presave mine & Sigrid’s song 💕🔥💕👯♀️ ⬇️⬇️",
 				"url": "https://t.co/uD3Z03eCwp",
 				"entities": {
 					"url": {
