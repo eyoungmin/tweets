b9720c33081ebe95b8b483250e37e911e14f1a2b
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-19 03:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85079,
-	"created_at": "Tue Jan 18 15:02:05 +0000 2022",
+	"tweets": 85080,
+	"created_at": "Tue Jan 18 18:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 18 17:40:10 +0000 2022",
+		"id": 1483485437817933800,
+		"id_str": "1483494436533325828",
+		"full_text": "고양이 키우는 마나. https://t.co/GfOjadoH1t",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483485431526473700,
+					"id_str": "1483485431526473734",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FJZmI9-aQAY5LGC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJZmI9-aQAY5LGC.jpg",
+					"url": "https://t.co/GfOjadoH1t",
+					"display_url": "pic.twitter.com/GfOjadoH1t",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1483485437817933828/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1712,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1003,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 568,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483485431526473700,
+					"id_str": "1483485431526473734",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FJZmI9-aQAY5LGC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJZmI9-aQAY5LGC.jpg",
+					"url": "https://t.co/GfOjadoH1t",
+					"display_url": "pic.twitter.com/GfOjadoH1t",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1483485437817933828/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1712,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1003,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 568,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1483485431505522700,
+					"id_str": "1483485431505522690",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/FJZmI95akAI4WKG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJZmI95akAI4WKG.jpg",
+					"url": "https://t.co/GfOjadoH1t",
+					"display_url": "pic.twitter.com/GfOjadoH1t",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1483485437817933828/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 512,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 904,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1542,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1448497121867436000,
+			"id_str": "1448497121867436037",
+			"name": "nakedbibi",
+			"screen_name": "nakedbibi",
+			"location": "",
+			"description": "BIBI💧💦",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 28209,
+			"friends_count": 0,
+			"listed_count": 112,
+			"created_at": "Thu Oct 14 03:53:27 +0000 2021",
+			"favourites_count": 31,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 106,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1467734861318213632/dJXyzwkV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1467734861318213632/dJXyzwkV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1448497121867436037/1638770235",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 155,
+		"favorite_count": 470,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "고양이 키우는 마나. https://t.co/GfOjadoH1t"
+	},
+	{
 		"created_at": "Tue Jan 18 14:40:10 +0000 2022",
 		"id": 1483439038220619800,
 		"id_str": "1483449136770953226",
