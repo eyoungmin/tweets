7727a340df7a43fe74c093be9ebce8150ff20469
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-19 05:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85080,
-	"created_at": "Tue Jan 18 18:02:05 +0000 2022",
+	"tweets": 85081,
+	"created_at": "Tue Jan 18 20:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,218 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 18 19:40:19 +0000 2022",
+		"id": 1483522547408900000,
+		"id_str": "1483524672989900802",
+		"full_text": "yeah i wasn’t very helpful tbh lol @wiffygriffy https://t.co/abENSh8qnp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wiffygriffy",
+					"name": "Griff",
+					"id": 1065546915406848000,
+					"id_str": "1065546915406848001",
+					"indices": [
+						35,
+						47
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1483522528601596000,
+					"id_str": "1483522528601595909",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1483522528601595909/pu/img/m2_tIfJaqa-cxy1m.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1483522528601595909/pu/img/m2_tIfJaqa-cxy1m.jpg",
+					"url": "https://t.co/abENSh8qnp",
+					"display_url": "pic.twitter.com/abENSh8qnp",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1483522547408900100/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483522528601596000,
+					"id_str": "1483522528601595909",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1483522528601595909/pu/img/m2_tIfJaqa-cxy1m.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1483522528601595909/pu/img/m2_tIfJaqa-cxy1m.jpg",
+					"url": "https://t.co/abENSh8qnp",
+					"display_url": "pic.twitter.com/abENSh8qnp",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1483522547408900100/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 480,
+							"h": 560,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							6,
+							7
+						],
+						"duration_millis": 11886,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1483522528601595909/pu/vid/320x372/SE3HWfjRmBC9MR2c.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1483522528601595909/pu/vid/480x560/hJOM4pqe_w0rCzGq.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1483522528601595909/pu/pl/rfxrezzbh5eo6ntH.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "Home To You (This Christmas) is out now ❤️🎄",
+			"url": "https://t.co/H90SsfhYEH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/H90SsfhYEH",
+							"expanded_url": "https://Sigrid.lnk.to/HomeToYouThisChristmasTW",
+							"display_url": "Sigrid.lnk.to/HomeToYouThisC…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84508,
+			"friends_count": 265,
+			"listed_count": 315,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 5944,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4249,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1636108169",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 140,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "yeah i wasn’t very helpful tbh lol @wiffygriffy https://t.co/abENSh8qnp"
+	},
+	{
 		"created_at": "Tue Jan 18 17:40:10 +0000 2022",
 		"id": 1483485437817933800,
 		"id_str": "1483494436533325828",
