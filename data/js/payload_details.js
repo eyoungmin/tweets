2e249c7f17a5fe3ffe44b76e71ed71b318a95202
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-20 13:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85125,
-	"created_at": "Thu Jan 20 03:02:06 +0000 2022",
+	"tweets": 85126,
+	"created_at": "Thu Jan 20 04:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 20 03:27:54 +0000 2022",
+		"id": 1483997717718204400,
+		"id_str": "1484004734906286086",
+		"full_text": "[달의 일기 #10] ABC MART PROMOTION BEHIND📸이곳이 이달의 소녀의 무대이자 런웨이...💃 활력 넘치는 다이나믹한 이달의 소녀의 비하인드 영상을 지금 바로 확인하세요!\n\n#이달의소녀 #달의일기 #LOONA #LOONANOTE\n\n▶ https://t.co/4Q0mRB12x1 https://t.co/5UMDE69v8z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						107,
+						113
+					]
+				},
+				{
+					"text": "달의일기",
+					"indices": [
+						114,
+						119
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						120,
+						126
+					]
+				},
+				{
+					"text": "LOONANOTE",
+					"indices": [
+						127,
+						137
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4Q0mRB12x1",
+					"expanded_url": "https://youtu.be/6X6BY9BaFnc",
+					"display_url": "youtu.be/6X6BY9BaFnc",
+					"indices": [
+						141,
+						164
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1483316739069083600,
+					"id_str": "1483316739069083650",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/FJXMtxmagAI52Av.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJXMtxmagAI52Av.jpg",
+					"url": "https://t.co/5UMDE69v8z",
+					"display_url": "pic.twitter.com/5UMDE69v8z",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1483997717718204421/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1483316739069083600,
+					"id_str": "1483316739069083650",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/FJXMtxmagAI52Av.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJXMtxmagAI52Av.jpg",
+					"url": "https://t.co/5UMDE69v8z",
+					"display_url": "pic.twitter.com/5UMDE69v8z",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1483997717718204421/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 846223,
+			"friends_count": 1,
+			"listed_count": 8228,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3774,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1698,
+		"favorite_count": 5307,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[달의 일기 #10] ABC MART PROMOTION BEHIND📸이곳이 이달의 소녀의 무대이자 런웨이...💃 활력 넘치는 다이나믹한 이달의 소녀의 비하인드 영상을 지금 바로 확인하세요!\n\n#이달의소녀 #달의일기 #LOONA #LOONANOTE\n\n▶ https://t.co/4Q0mRB12x1 https://t.co/5UMDE69v8z"
+	},
+	{
 		"created_at": "Thu Jan 20 02:55:27 +0000 2022",
 		"id": 1483994657139101700,
 		"id_str": "1483996567535329283",
