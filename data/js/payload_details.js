071dabd19e2bdabe6cbedbaea8701e54ab09970c
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-25 13:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85222,
-	"created_at": "Tue Jan 25 03:02:06 +0000 2022",
+	"tweets": 85223,
+	"created_at": "Tue Jan 25 04:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 03:30:08 +0000 2022",
+		"id": 1485814237570408400,
+		"id_str": "1485817235268587524",
+		"full_text": "[달의 일기 #11] 2022 Season’s Greetings BEHIND 📸  밴드부 이달소🎸 VS 모범생 이달소✏ 오빛들의 선택은????\n이달의 소녀 시즌그리팅 비하인드 영상을 지금 바로 확인하세요!\n\n#이달의소녀 #달의일기 #LOONA #LOONANOTE \n\n▶️ https://t.co/NG0BnTQyD2 https://t.co/7s21QhXVH1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						116,
+						122
+					]
+				},
+				{
+					"text": "달의일기",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						129,
+						135
+					]
+				},
+				{
+					"text": "LOONANOTE",
+					"indices": [
+						136,
+						146
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NG0BnTQyD2",
+					"expanded_url": "http://youtu.be/5LV063CsH08",
+					"display_url": "youtu.be/5LV063CsH08",
+					"indices": [
+						152,
+						175
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1485814228330356700,
+					"id_str": "1485814228330356738",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ6sKyHaIAI2BfD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ6sKyHaIAI2BfD.jpg",
+					"url": "https://t.co/7s21QhXVH1",
+					"display_url": "pic.twitter.com/7s21QhXVH1",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1485814237570408450/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1485814228330356700,
+					"id_str": "1485814228330356738",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/FJ6sKyHaIAI2BfD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FJ6sKyHaIAI2BfD.jpg",
+					"url": "https://t.co/7s21QhXVH1",
+					"display_url": "pic.twitter.com/7s21QhXVH1",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1485814237570408450/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 847291,
+			"friends_count": 1,
+			"listed_count": 8231,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3794,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1042,
+		"favorite_count": 3492,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[달의 일기 #11] 2022 Season’s Greetings BEHIND 📸  밴드부 이달소🎸 VS 모범생 이달소✏ 오빛들의 선택은????\n이달의 소녀 시즌그리팅 비하인드 영상을 지금 바로 확인하세요!\n\n#이달의소녀 #달의일기 #LOONA #LOONANOTE \n\n▶️ https://t.co/NG0BnTQyD2 https://t.co/7s21QhXVH1"
+	},
+	{
 		"created_at": "Tue Jan 25 02:40:10 +0000 2022",
 		"id": 1485800502067888000,
 		"id_str": "1485804658576789508",
