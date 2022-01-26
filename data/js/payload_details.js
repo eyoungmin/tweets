6c9a0e9202308e4f2f2d002883dddaabd6ed11fc
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-26 17:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85247,
-	"created_at": "Wed Jan 26 07:02:06 +0000 2022",
+	"tweets": 85248,
+	"created_at": "Wed Jan 26 08:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 26 07:29:39 +0000 2022",
+		"id": 1486229651433623600,
+		"id_str": "1486239896595537922",
+		"full_text": "소녀시대 태연이 2월 14일, 밸런타인데이 선물처럼 정규 3집 ‘INVU’를 발표한다.\n#태연 #소녀시대 #INVU #taeyeon\n\nhttps://t.co/RXpxYiHjkC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						49,
+						52
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						53,
+						58
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						59,
+						64
+					]
+				},
+				{
+					"text": "taeyeon",
+					"indices": [
+						65,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RXpxYiHjkC",
+					"expanded_url": "https://www.wkorea.com/2022/01/26/%EB%AF%BF%EA%B3%A0-%EB%93%A3%EB%8A%94-%ED%83%9C%EC%97%B0-%EC%A0%95%EA%B7%9C-3%EC%A7%91-invu-2%EC%9B%94-14%EC%9D%BC-%EB%B0%9C%EB%A7%A4/?ddw=103028&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2022/01/26/%EB…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5xzw0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5xzw0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 294327,
+			"friends_count": 0,
+			"listed_count": 820,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7516,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13141,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1634707563",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 314,
+		"favorite_count": 569,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "소녀시대 태연이 2월 14일, 밸런타인데이 선물처럼 정규 3집 ‘INVU’를 발표한다.\n#태연 #소녀시대 #INVU #taeyeon\n\nhttps://t.co/RXpxYiHjkC"
+	},
+	{
 		"created_at": "Wed Jan 26 06:29:38 +0000 2022",
 		"id": 1486210573352435700,
 		"id_str": "1486224796400721920",
