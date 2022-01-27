505e9b0ca2bcdccfffc1265bd3ad61859f99f94f
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-28 06:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85287,
-	"created_at": "Thu Jan 27 18:02:09 +0000 2022",
+	"tweets": 85288,
+	"created_at": "Thu Jan 27 21:02:09 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,10 +1,242 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Thu Jan 27 20:40:10 +0000 2022",
+		"id": 1486798292759502800,
+		"id_str": "1486801225710460930",
+		"full_text": "#BRITs 2022 Best Rock / Alternative voting is open now on TikTok at https://t.co/y1yu395olF ❤️♾⭕️ \n\n(You are allowed to vote 10 times per day. Thank you, as always, for the support.) https://t.co/Rs9lP0qfWx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BRITs",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/y1yu395olF",
+					"expanded_url": "https://brits.lnk.to/TikTokVoting",
+					"display_url": "brits.lnk.to/TikTokVoting",
+					"indices": [
+						68,
+						91
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1486795973816983600,
+					"id_str": "1486795973816983557",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1486795973816983557/img/TUPJwy5hkWaZ2xES.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1486795973816983557/img/TUPJwy5hkWaZ2xES.jpg",
+					"url": "https://t.co/Rs9lP0qfWx",
+					"display_url": "pic.twitter.com/Rs9lP0qfWx",
+					"expanded_url": "https://twitter.com/coldplay/status/1486798292759502848/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1486795973816983600,
+					"id_str": "1486795973816983557",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1486795973816983557/img/TUPJwy5hkWaZ2xES.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1486795973816983557/img/TUPJwy5hkWaZ2xES.jpg",
+					"url": "https://t.co/Rs9lP0qfWx",
+					"display_url": "pic.twitter.com/Rs9lP0qfWx",
+					"expanded_url": "https://twitter.com/coldplay/status/1486798292759502848/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 29066,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1486795973816983557/vid/720x720/fKo3wj3LyrebGITy.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1486795973816983557/vid/540x540/ss8gJ79XurHyIg5e.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1486795973816983557/pl/y-UNeL14CBnoKB7T.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1486795973816983557/vid/320x320/-hE3VONFxAqUd1zF.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"call_to_actions": {
+							"visit_site": {
+								"url": "https://brits.lnk.to/TikTokVoting"
+							}
+						},
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Epiphane",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23540782,
+			"friends_count": 774,
+			"listed_count": 42629,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1539,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 8958,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 214,
+		"favorite_count": 1421,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BRITs 2022 Best Rock / Alternative voting is open now on TikTok at https://t.co/y1yu395olF ❤️♾⭕️ \n\n(You are allowed to vote 10 times per day. Thank you, as always, for the support.) https://t.co/Rs9lP0qfWx"
+	},
+	{
 		"created_at": "Thu Jan 27 17:40:09 +0000 2022",
 		"id": 1486750967320256500,
 		"id_str": "1486755925125963782",
-		"full_text": "wooo @NME!! ❤️‍🔥🥳 @wiffygriffy https://t.co/gjK1Desx4b",
+		"full_text": "wooo @NME!! ❤️🔥🥳 @wiffygriffy https://t.co/gjK1Desx4b",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -127,7 +359,7 @@ Grailbird.data.tweets_2022_01 =
 			"name": "sigrid",
 			"screen_name": "thisissigrid",
 			"location": "",
-			"description": "Head on Fire out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"description": "Head on Fire out now ❤️🔥❤️🔥❤️🔥",
 			"url": "https://t.co/S4ulGLypNB",
 			"entities": {
 				"url": {
@@ -194,7 +426,7 @@ Grailbird.data.tweets_2022_01 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "und",
-		"text": "wooo @NME!! ❤️‍🔥🥳 @wiffygriffy https://t.co/gjK1Desx4b"
+		"text": "wooo @NME!! ❤️🔥🥳 @wiffygriffy https://t.co/gjK1Desx4b"
 	},
 	{
 		"created_at": "Thu Jan 27 15:40:09 +0000 2022",
