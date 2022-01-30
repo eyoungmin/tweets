0bf69c13e4a7e59235eafea3d69a049fa023c239
Youngmin Lee <iameyoungmin@gmail.com>
2022-01-30 10:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85313,
-	"created_at": "Sat Jan 29 13:02:10 +0000 2022",
+	"tweets": 85314,
+	"created_at": "Sun Jan 30 01:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,251 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 30 00:29:39 +0000 2022",
+		"id": 1487576296616042500,
+		"id_str": "1487583752217894915",
+		"full_text": "[선공개] 이긴 팀만 데뷔조와 대결 가능?!\n도전조A,B의 이기기 위한 전략 총 동원 🙌\n#방과후설렘 일요일 밤 9시 💕\n#옥주현 #권유리 #아이키 #소연\nMBC220130방송 https://t.co/PPdNBWUhIs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "방과후설렘",
+					"indices": [
+						50,
+						56
+					]
+				},
+				{
+					"text": "옥주현",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "권유리",
+					"indices": [
+						73,
+						77
+					]
+				},
+				{
+					"text": "아이키",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "소연",
+					"indices": [
+						83,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1487380918713340000,
+					"id_str": "1487380918713339907",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/FKQ90--acAAdRry.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKQ90--acAAdRry.jpg",
+					"url": "https://t.co/PPdNBWUhIs",
+					"display_url": "pic.twitter.com/PPdNBWUhIs",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1487576296616042502/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1487380918713340000,
+					"id_str": "1487380918713339907",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/FKQ90--acAAdRry.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKQ90--acAAdRry.jpg",
+					"url": "https://t.co/PPdNBWUhIs",
+					"display_url": "pic.twitter.com/PPdNBWUhIs",
+					"expanded_url": "https://twitter.com/MBC_entertain/status/1487576296616042502/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 271604,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1487380918713339907/vid/640x360/NjtA-icMekq8Syrq.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1487380918713339907/pl/QfD8ld9nXgSgV4xN.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1487380918713339907/vid/1280x720/glOSm9Q4C6kPR8eP.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1487380918713339907/vid/480x270/TDJUGkwWHPzFTNtq.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4144012572,
+			"id_str": "4144012572",
+			"name": "MBC 예능연구소",
+			"screen_name": "MBC_entertain",
+			"location": "",
+			"description": "MBC 예능의 꿀잼만 모아 드림 [official]",
+			"url": "https://t.co/7U66BOeSeR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7U66BOeSeR",
+							"expanded_url": "http://facebook.com/MBCentertain",
+							"display_url": "facebook.com/MBCentertain",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 931137,
+			"friends_count": 6,
+			"listed_count": 998,
+			"created_at": "Fri Nov 06 08:21:55 +0000 2015",
+			"favourites_count": 89,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 16907,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112884226515300353/LvfV_hgR_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4144012572/1555047387",
+			"profile_link_color": "45B7DE",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 68,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[선공개] 이긴 팀만 데뷔조와 대결 가능?!\n도전조A,B의 이기기 위한 전략 총 동원 🙌\n#방과후설렘 일요일 밤 9시 💕\n#옥주현 #권유리 #아이키 #소연\nMBC220130방송 https://t.co/PPdNBWUhIs"
+	},
+	{
 		"created_at": "Sat Jan 29 12:40:37 +0000 2022",
 		"id": 1487399372812087300,
 		"id_str": "1487405319819083778",
