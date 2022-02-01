695db80ea746b614529d18de6c6389a530208ffc
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-01 11:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85335,
-	"created_at": "Mon Jan 31 15:02:15 +0000 2022",
+	"tweets": 85336,
+	"created_at": "Tue Feb 01 02:02:05 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_02.js
@@ -0,0 +1,231 @@
+Grailbird.data.tweets_2022_02 = 
+[
+	{
+		"created_at": "Tue Feb 01 01:40:10 +0000 2022",
+		"id": 1488316229589020700,
+		"id_str": "1488326273797664773",
+		"full_text": "[#Notice] 제가 Grammys Global Spin에 선정 되다니 믿겨지지가 않네요…!!!!! \n\n영광스러운 마음으로 Lovers in the night 을 불렀습니다. Happy new year💟\n\n곧 봬요 여러분!\n\n2/1 10am (PST), 2/2 3am(KST)\n\n#Seori #GRAMMYs https://t.co/Ut6L1baXNu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Notice",
+					"indices": [
+						1,
+						8
+					]
+				},
+				{
+					"text": "Seori",
+					"indices": [
+						157,
+						163
+					]
+				},
+				{
+					"text": "GRAMMYs",
+					"indices": [
+						164,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1488316181002190800,
+					"id_str": "1488316181002190851",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1488316181002190851/pu/img/oydzN2pwZ0XHp6Ft.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1488316181002190851/pu/img/oydzN2pwZ0XHp6Ft.jpg",
+					"url": "https://t.co/Ut6L1baXNu",
+					"display_url": "pic.twitter.com/Ut6L1baXNu",
+					"expanded_url": "https://twitter.com/seori_official/status/1488316229589020672/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1488316181002190800,
+					"id_str": "1488316181002190851",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1488316181002190851/pu/img/oydzN2pwZ0XHp6Ft.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1488316181002190851/pu/img/oydzN2pwZ0XHp6Ft.jpg",
+					"url": "https://t.co/Ut6L1baXNu",
+					"display_url": "pic.twitter.com/Ut6L1baXNu",
+					"expanded_url": "https://twitter.com/seori_official/status/1488316229589020672/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 30030,
+						"variants": [
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1488316181002190851/pu/vid/540x540/DdUKgdD9C-dCsAxD.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1488316181002190851/pu/vid/720x720/w-6YK4Yk6aaO_R9A.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1488316181002190851/pu/pl/FJZ97q1i0cIHJlBo.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1488316181002190851/pu/vid/320x320/gMX8MLb70MrURIjv.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/2jDAeGXOz0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2jDAeGXOz0",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42204,
+			"friends_count": 0,
+			"listed_count": 177,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 481,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 255,
+		"favorite_count": 766,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Notice] 제가 Grammys Global Spin에 선정 되다니 믿겨지지가 않네요…!!!!! \n\n영광스러운 마음으로 Lovers in the night 을 불렀습니다. Happy new year💟\n\n곧 봬요 여러분!\n\n2/1 10am (PST), 2/2 3am(KST)\n\n#Seori #GRAMMYs https://t.co/Ut6L1baXNu"
+	}
+]
