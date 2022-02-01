1ebe9971380f1f6a14ce1c60a56ae6de52c0f702
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-01 20:02:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85342,
-	"created_at": "Tue Feb 01 10:02:00 +0000 2022",
+	"tweets": 85343,
+	"created_at": "Tue Feb 01 11:01:59 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Tue Feb 01 10:20:25 +0000 2022",
+		"id": 1488452061775614000,
+		"id_str": "1488457200624410626",
+		"full_text": "제니, 유나 허리 잘록함 배가 되는 볼레로 패션 입어 볼래?\n\n#볼레로 #Y2K #블랙핑크 #제니 #가디건 #유나 #리사 #에스파 #닝닝 #연예인사복 #OOTD\nhttps://t.co/0OFdC6QLJx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "볼레로",
+					"indices": [
+						35,
+						39
+					]
+				},
+				{
+					"text": "Y2K",
+					"indices": [
+						40,
+						44
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						45,
+						50
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						51,
+						54
+					]
+				},
+				{
+					"text": "가디건",
+					"indices": [
+						55,
+						59
+					]
+				},
+				{
+					"text": "유나",
+					"indices": [
+						60,
+						63
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						64,
+						67
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "닝닝",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "연예인사복",
+					"indices": [
+						77,
+						83
+					]
+				},
+				{
+					"text": "OOTD",
+					"indices": [
+						84,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0OFdC6QLJx",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63345?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63345?…",
+					"indices": [
+						90,
+						113
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88913,
+			"friends_count": 4,
+			"listed_count": 400,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 165,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13474,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "제니, 유나 허리 잘록함 배가 되는 볼레로 패션 입어 볼래?\n\n#볼레로 #Y2K #블랙핑크 #제니 #가디건 #유나 #리사 #에스파 #닝닝 #연예인사복 #OOTD\nhttps://t.co/0OFdC6QLJx"
+	},
+	{
 		"created_at": "Tue Feb 01 09:40:22 +0000 2022",
 		"id": 1488438083766816800,
 		"id_str": "1488447122391318528",
