e94ae34dafcdf4c3020564b495193f3b3a560281
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-04 02:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85391,
-	"created_at": "Thu Feb 03 16:02:01 +0000 2022",
+	"tweets": 85392,
+	"created_at": "Thu Feb 03 17:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 03 16:21:06 +0000 2022",
+		"id": 1489269951160406000,
+		"id_str": "1489272745951211529",
+		"full_text": "장원영, 제니 볼 수록 빛이나는 블링블링 공주님 헤어스타일👸\n\n#장원영 #아이브 #제니 #블랙핑크 #태연 #소녀시대 #소시 #웨이브 #공주 #프린세스 #리본 #헤어밴드 #머리띠 #우주소녀 #다영\nhttps://t.co/j00T5vjd9i",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "장원영",
+					"indices": [
+						35,
+						39
+					]
+				},
+				{
+					"text": "아이브",
+					"indices": [
+						40,
+						44
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						45,
+						48
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						49,
+						54
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						55,
+						58
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						59,
+						64
+					]
+				},
+				{
+					"text": "소시",
+					"indices": [
+						65,
+						68
+					]
+				},
+				{
+					"text": "웨이브",
+					"indices": [
+						69,
+						73
+					]
+				},
+				{
+					"text": "공주",
+					"indices": [
+						74,
+						77
+					]
+				},
+				{
+					"text": "프린세스",
+					"indices": [
+						78,
+						83
+					]
+				},
+				{
+					"text": "리본",
+					"indices": [
+						84,
+						87
+					]
+				},
+				{
+					"text": "헤어밴드",
+					"indices": [
+						88,
+						93
+					]
+				},
+				{
+					"text": "머리띠",
+					"indices": [
+						94,
+						98
+					]
+				},
+				{
+					"text": "우주소녀",
+					"indices": [
+						99,
+						104
+					]
+				},
+				{
+					"text": "다영",
+					"indices": [
+						105,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/j00T5vjd9i",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63396?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63396?…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88904,
+			"friends_count": 4,
+			"listed_count": 400,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 165,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13478,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "장원영, 제니 볼 수록 빛이나는 블링블링 공주님 헤어스타일👸\n\n#장원영 #아이브 #제니 #블랙핑크 #태연 #소녀시대 #소시 #웨이브 #공주 #프린세스 #리본 #헤어밴드 #머리띠 #우주소녀 #다영\nhttps://t.co/j00T5vjd9i"
+	},
+	{
 		"created_at": "Thu Feb 03 15:40:38 +0000 2022",
 		"id": 1489185241663168500,
 		"id_str": "1489262559546937349",
