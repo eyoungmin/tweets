21aab58f56180ab076e0788e1f2557567bb444f0
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-05 00:02:13 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 04 14:40:12 +0000 2022",
+		"id": 1489599288212848600,
+		"id_str": "1489609739487039490",
+		"full_text": "내일 오전 10시 30분 \n월간 커넥트 시즌 2 출발합니다! \n\n첫회 인터뷰이님들의 말씀들은\n지금의 우리가 깊이 곱씹어 봐야 할 이야기라고 생각했습니다. \n\n진심으로 \n많은 분들이 시청해주시면 좋겠습니다! \n\n#월간커넥트 #tvn\n#건축학김자영\n#뇌과학장동선 \n#현재사는심용환 \n#김윤아 https://t.co/76riFk4vTG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "월간커넥트",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "tvn",
+					"indices": [
+						124,
+						128
+					]
+				},
+				{
+					"text": "건축학김자영",
+					"indices": [
+						129,
+						136
+					]
+				},
+				{
+					"text": "뇌과학장동선",
+					"indices": [
+						137,
+						144
+					]
+				},
+				{
+					"text": "현재사는심용환",
+					"indices": [
+						146,
+						154
+					]
+				},
+				{
+					"text": "김윤아",
+					"indices": [
+						156,
+						160
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1489599275747377200,
+					"id_str": "1489599275747377159",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/media/FKwepiiaAAcrgiO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKwepiiaAAcrgiO.jpg",
+					"url": "https://t.co/76riFk4vTG",
+					"display_url": "pic.twitter.com/76riFk4vTG",
+					"expanded_url": "https://twitter.com/love_yuna/status/1489599288212848644/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 546,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1284,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 963,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489599275747377200,
+					"id_str": "1489599275747377159",
+					"indices": [
+						161,
+						184
+					],
+					"media_url": "http://pbs.twimg.com/media/FKwepiiaAAcrgiO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FKwepiiaAAcrgiO.jpg",
+					"url": "https://t.co/76riFk4vTG",
+					"display_url": "pic.twitter.com/76riFk4vTG",
+					"expanded_url": "https://twitter.com/love_yuna/status/1489599288212848644/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 546,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1284,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 963,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274365,
+			"friends_count": 560,
+			"listed_count": 2798,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 362,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6638,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1635493925",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 49,
+		"favorite_count": 78,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "내일 오전 10시 30분 \n월간 커넥트 시즌 2 출발합니다! \n\n첫회 인터뷰이님들의 말씀들은\n지금의 우리가 깊이 곱씹어 봐야 할 이야기라고 생각했습니다. \n\n진심으로 \n많은 분들이 시청해주시면 좋겠습니다! \n\n#월간커넥트 #tvn\n#건축학김자영\n#뇌과학장동선 \n#현재사는심용환 \n#김윤아 https://t.co/76riFk4vTG"
+	},
+	{
 		"created_at": "Fri Feb 04 13:40:12 +0000 2022",
 		"id": 1489592272236601300,
 		"id_str": "1489594640449875974",
