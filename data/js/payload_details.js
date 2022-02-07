ab91cb92936e34bcd2c5d13b64f13a77c6a55ade
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-07 21:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85455,
-	"created_at": "Mon Feb 07 11:02:02 +0000 2022",
+	"tweets": 85456,
+	"created_at": "Mon Feb 07 12:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Mon Feb 07 11:49:45 +0000 2022",
+		"id": 1490641496642568200,
+		"id_str": "1490654007982972928",
+		"full_text": "충격적인 무대에 바로 과몰입 되는 본업 존잘 언니오빠들\n\n#아이돌 #원더걸스 #선미 #선예 #소녀시대 #윤아 #2pm #준호 #걸스온탑 #태연 #효연 #소시 \nhttps://t.co/zxYptrAQxs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			112
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이돌",
+					"indices": [
+						32,
+						36
+					]
+				},
+				{
+					"text": "원더걸스",
+					"indices": [
+						37,
+						42
+					]
+				},
+				{
+					"text": "선미",
+					"indices": [
+						43,
+						46
+					]
+				},
+				{
+					"text": "선예",
+					"indices": [
+						47,
+						50
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						51,
+						56
+					]
+				},
+				{
+					"text": "윤아",
+					"indices": [
+						57,
+						60
+					]
+				},
+				{
+					"text": "2pm",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "준호",
+					"indices": [
+						66,
+						69
+					]
+				},
+				{
+					"text": "걸스온탑",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "태연",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						80,
+						83
+					]
+				},
+				{
+					"text": "소시",
+					"indices": [
+						84,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zxYptrAQxs",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63428?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63428?…",
+					"indices": [
+						89,
+						112
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88920,
+			"friends_count": 4,
+			"listed_count": 401,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 166,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "충격적인 무대에 바로 과몰입 되는 본업 존잘 언니오빠들\n\n#아이돌 #원더걸스 #선미 #선예 #소녀시대 #윤아 #2pm #준호 #걸스온탑 #태연 #효연 #소시 \nhttps://t.co/zxYptrAQxs"
+	},
+	{
 		"created_at": "Mon Feb 07 10:33:34 +0000 2022",
 		"id": 1490634340836933600,
 		"id_str": "1490634835542556675",
