471a909ac6f3e84d694ca1bc2f7459d890a80332
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-09 00:02:07 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Tue Feb 08 14:45:36 +0000 2022",
+		"id": 1491056725755105300,
+		"id_str": "1491060652546347012",
+		"full_text": "Sweater paws and cute slogans with @loonatheworld!\nCome see them on FEB.12 at 3PM KST!\n\nTickets | 티켓 구매👇\n🎟️https://t.co/Wr5yhRkHOg\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/R2KQFrJZm9\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/AZ3bH0TwSj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						222,
+						228
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						229,
+						235
+					]
+				},
+				{
+					"text": "LOONAVERSE",
+					"indices": [
+						236,
+						247
+					]
+				},
+				{
+					"text": "FROM",
+					"indices": [
+						248,
+						253
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						35,
+						49
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Wr5yhRkHOg",
+					"expanded_url": "https://mmt.fans/hdyB",
+					"display_url": "mmt.fans/hdyB",
+					"indices": [
+						107,
+						130
+					]
+				},
+				{
+					"url": "https://t.co/R2KQFrJZm9",
+					"expanded_url": "https://mmt.fans/hpOe",
+					"display_url": "mmt.fans/hpOe",
+					"indices": [
+						197,
+						220
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1491003169412841500,
+					"id_str": "1491003169412841475",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/FLEbe5_VgAMeSU0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLEbe5_VgAMeSU0.jpg",
+					"url": "https://t.co/AZ3bH0TwSj",
+					"display_url": "pic.twitter.com/AZ3bH0TwSj",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1491056725755105282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1311,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1491003169412841500,
+					"id_str": "1491003169412841475",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/FLEbe5_VgAMeSU0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLEbe5_VgAMeSU0.jpg",
+					"url": "https://t.co/AZ3bH0TwSj",
+					"display_url": "pic.twitter.com/AZ3bH0TwSj",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1491056725755105282/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1311,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 768,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 435,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIgF6O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIgF6O",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143120,
+			"friends_count": 103,
+			"listed_count": 299,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28656,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 314,
+		"favorite_count": 718,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Sweater paws and cute slogans with @loonatheworld!\nCome see them on FEB.12 at 3PM KST!\n\nTickets | 티켓 구매👇\n🎟️https://t.co/Wr5yhRkHOg\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/R2KQFrJZm9\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/AZ3bH0TwSj"
+	},
+	{
 		"created_at": "Tue Feb 08 13:45:36 +0000 2022",
 		"id": 1491041626298491000,
 		"id_str": "1491045553584676871",
