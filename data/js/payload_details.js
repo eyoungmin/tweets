5e3a0a1b9ef893d0039131e78edeaba1be5d3f7a
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-09 16:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85496,
-	"created_at": "Wed Feb 09 06:02:01 +0000 2022",
+	"tweets": 85497,
+	"created_at": "Wed Feb 09 07:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Wed Feb 09 06:45:36 +0000 2022",
+		"id": 1491290770179076000,
+		"id_str": "1491302244863590404",
+		"full_text": "Moment: When @loonatheworld makes hoodies a total 10/10 fit\nSee them live on FEB.12 at 3PM KST!\n\nTix | 티켓 구매👇\n🎟️https://t.co/2p3T5bq8HB\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/nXbhv8zOIC\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/XfthRjqTAP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						227,
+						233
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						234,
+						240
+					]
+				},
+				{
+					"text": "LOONAVERSE",
+					"indices": [
+						241,
+						252
+					]
+				},
+				{
+					"text": "FROM",
+					"indices": [
+						253,
+						258
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						13,
+						27
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2p3T5bq8HB",
+					"expanded_url": "https://mmt.fans/hpc5",
+					"display_url": "mmt.fans/hpc5",
+					"indices": [
+						112,
+						135
+					]
+				},
+				{
+					"url": "https://t.co/nXbhv8zOIC",
+					"expanded_url": "https://mmt.fans/hp04",
+					"display_url": "mmt.fans/hp04",
+					"indices": [
+						202,
+						225
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1491047501364818000,
+					"id_str": "1491047501364817929",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/FLFDzXYVcAkk1B7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLFDzXYVcAkk1B7.jpg",
+					"url": "https://t.co/XfthRjqTAP",
+					"display_url": "pic.twitter.com/XfthRjqTAP",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1491290770179076096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1491047501364818000,
+					"id_str": "1491047501364817929",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/FLFDzXYVcAkk1B7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLFDzXYVcAkk1B7.jpg",
+					"url": "https://t.co/XfthRjqTAP",
+					"display_url": "pic.twitter.com/XfthRjqTAP",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1491290770179076096/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIgF6O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIgF6O",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143172,
+			"friends_count": 103,
+			"listed_count": 301,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28662,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 103,
+		"favorite_count": 334,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Moment: When @loonatheworld makes hoodies a total 10/10 fit\nSee them live on FEB.12 at 3PM KST!\n\nTix | 티켓 구매👇\n🎟️https://t.co/2p3T5bq8HB\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/nXbhv8zOIC\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/XfthRjqTAP"
+	},
+	{
 		"created_at": "Wed Feb 09 05:29:39 +0000 2022",
 		"id": 1491276244553728000,
 		"id_str": "1491283127947468803",
