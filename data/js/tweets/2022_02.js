748b986388bf40e05c6cca839d63113e17d58c6f
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-12 07:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 21:45:40 +0000 2022",
+		"id": 1492249585841938400,
+		"id_str": "1492253526323572745",
+		"full_text": "[NOTICE📢 for VIP &amp; B TIER Ticket Purchasers]\n\nRehearsal LIVE for the 2022 이달의 소녀 CONCERT［LOONAVERSE：FROM] \n🕛Time change (delayed by one hour):\n\nFebruary 12, 2022 at 12:00 PM KST\n\nLive Rehearsal Streaming🔗\nhttps://t.co/Q0zgjrEvPy\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/tq5nFYWRVy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						234,
+						240
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						241,
+						247
+					]
+				},
+				{
+					"text": "LOONAVERSE",
+					"indices": [
+						248,
+						259
+					]
+				},
+				{
+					"text": "FROM",
+					"indices": [
+						260,
+						265
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Q0zgjrEvPy",
+					"expanded_url": "https://mmt.fans/hp53",
+					"display_url": "mmt.fans/hp53",
+					"indices": [
+						209,
+						232
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1492195771776737300,
+					"id_str": "1492195771776737280",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FLVYJgAXoAAlg3M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLVYJgAXoAAlg3M.jpg",
+					"url": "https://t.co/tq5nFYWRVy",
+					"display_url": "pic.twitter.com/tq5nFYWRVy",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1492249585841938432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1492195771776737300,
+					"id_str": "1492195771776737280",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FLVYJgAXoAAlg3M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLVYJgAXoAAlg3M.jpg",
+					"url": "https://t.co/tq5nFYWRVy",
+					"display_url": "pic.twitter.com/tq5nFYWRVy",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1492249585841938432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 652,
+							"h": 296,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIgF6O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIgF6O",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143342,
+			"friends_count": 103,
+			"listed_count": 301,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5136,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28707,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 80,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[NOTICE📢 for VIP &amp; B TIER Ticket Purchasers]\n\nRehearsal LIVE for the 2022 이달의 소녀 CONCERT［LOONAVERSE：FROM] \n🕛Time change (delayed by one hour):\n\nFebruary 12, 2022 at 12:00 PM KST\n\nLive Rehearsal Streaming🔗\nhttps://t.co/Q0zgjrEvPy\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/tq5nFYWRVy"
+	},
+	{
 		"created_at": "Fri Feb 11 20:45:36 +0000 2022",
 		"id": 1492226935895822300,
 		"id_str": "1492238413415780365",
