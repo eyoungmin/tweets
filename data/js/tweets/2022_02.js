c3e7f95fe67199bb9c6dbe138afd4fe9048183b1
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-11 16:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,229 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 06:45:37 +0000 2022",
+		"id": 1492015544299671600,
+		"id_str": "1492027022436147218",
+		"full_text": "Only 24 hours⏰ left until tomorrow’s online\n2022 이달의 소녀 CONCERT [LOONAVERSE : FROM]💫\n\nTix | 티켓 구매👇\n🎟️https://t.co/9RFWLTScND\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/vDpSVdkrUb\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/KPUnljg55f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						216,
+						222
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						223,
+						229
+					]
+				},
+				{
+					"text": "LOONAVERSE",
+					"indices": [
+						230,
+						241
+					]
+				},
+				{
+					"text": "FROM",
+					"indices": [
+						242,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9RFWLTScND",
+					"expanded_url": "https://mmt.fans/hX9B",
+					"display_url": "mmt.fans/hX9B",
+					"indices": [
+						101,
+						124
+					]
+				},
+				{
+					"url": "https://t.co/vDpSVdkrUb",
+					"expanded_url": "https://mmt.fans/hXoT",
+					"display_url": "mmt.fans/hXoT",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1491930824929079300,
+					"id_str": "1491930824929079296",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/FLRnLkFacAAvRaf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLRnLkFacAAvRaf.jpg",
+					"url": "https://t.co/KPUnljg55f",
+					"display_url": "pic.twitter.com/KPUnljg55f",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1492015544299671554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1491930824929079300,
+					"id_str": "1491930824929079296",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/FLRnLkFacAAvRaf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLRnLkFacAAvRaf.jpg",
+					"url": "https://t.co/KPUnljg55f",
+					"display_url": "pic.twitter.com/KPUnljg55f",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1492015544299671554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1448,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIgF6O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIgF6O",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 143316,
+			"friends_count": 103,
+			"listed_count": 301,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5136,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 28693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/969494262701219840/QQ4LLVIJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 57,
+		"favorite_count": 266,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Only 24 hours⏰ left until tomorrow’s online\n2022 이달의 소녀 CONCERT [LOONAVERSE : FROM]💫\n\nTix | 티켓 구매👇\n🎟️https://t.co/9RFWLTScND\n\n💡Official Lightstick (응원봉) &amp; 🎴Trading Cards (트레이딩 카드 세트)\nMD🛍️https://t.co/vDpSVdkrUb\n\n#LOONA #이달의소녀 #LOONAVERSE #FROM https://t.co/KPUnljg55f"
+	},
+	{
 		"created_at": "Fri Feb 11 05:29:44 +0000 2022",
 		"id": 1492000445618741200,
 		"id_str": "1492007925619044352",
