36e011f0d1cddbc229f6b58b455d49822ceacf3b
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-11 22:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85572,
-	"created_at": "Fri Feb 11 12:02:03 +0000 2022",
+	"tweets": 85573,
+	"created_at": "Fri Feb 11 13:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Fri Feb 11 12:23:25 +0000 2022",
+		"id": 1492106141828010000,
+		"id_str": "1492112033801723907",
+		"full_text": "그때 그 시절 패션 떠오르게 하는 태연, 리사 스카프 연출법\n\n#태연 #소녀시대 #레드벨벳 #슬기 #리사 #블랙핑크 #블핑 #문별 #마마무 #스카프 #스타일링 #패션 #ootd\nhttps://t.co/XddKn4Bk07",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						35,
+						38
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						39,
+						44
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						45,
+						50
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						51,
+						54
+					]
+				},
+				{
+					"text": "리사",
+					"indices": [
+						55,
+						58
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						59,
+						64
+					]
+				},
+				{
+					"text": "블핑",
+					"indices": [
+						65,
+						68
+					]
+				},
+				{
+					"text": "문별",
+					"indices": [
+						69,
+						72
+					]
+				},
+				{
+					"text": "마마무",
+					"indices": [
+						73,
+						77
+					]
+				},
+				{
+					"text": "스카프",
+					"indices": [
+						78,
+						82
+					]
+				},
+				{
+					"text": "스타일링",
+					"indices": [
+						83,
+						88
+					]
+				},
+				{
+					"text": "패션",
+					"indices": [
+						89,
+						92
+					]
+				},
+				{
+					"text": "ootd",
+					"indices": [
+						93,
+						98
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XddKn4Bk07",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63725?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63725?…",
+					"indices": [
+						99,
+						122
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 88975,
+			"friends_count": 4,
+			"listed_count": 404,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13507,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "그때 그 시절 패션 떠오르게 하는 태연, 리사 스카프 연출법\n\n#태연 #소녀시대 #레드벨벳 #슬기 #리사 #블랙핑크 #블핑 #문별 #마마무 #스카프 #스타일링 #패션 #ootd\nhttps://t.co/XddKn4Bk07"
+	},
+	{
 		"created_at": "Fri Feb 11 11:40:16 +0000 2022",
 		"id": 1492093105050628000,
 		"id_str": "1492101173448478750",
