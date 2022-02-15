25d1cca5b0f0eed0f1282c51c482455c133f7087
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-15 20:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85661,
-	"created_at": "Tue Feb 15 10:02:03 +0000 2022",
+	"tweets": 85662,
+	"created_at": "Tue Feb 15 11:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Tue Feb 15 10:24:45 +0000 2022",
+		"id": 1493525504053297200,
+		"id_str": "1493531722285203458",
+		"full_text": "제니에게 선글라스 선물 받고 행복 무드 인증샷 릴레이-ing\n\n#블랙핑크 #블핑 #제니 #BLACKPINK #정호연 #오징어게임 #지수 #신현지 #임수향 #엘라 #키즈모델 #송민호 #마이노\nhttps://t.co/MiGr2cAzSM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						35,
+						40
+					]
+				},
+				{
+					"text": "블핑",
+					"indices": [
+						41,
+						44
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						45,
+						48
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						49,
+						59
+					]
+				},
+				{
+					"text": "정호연",
+					"indices": [
+						60,
+						64
+					]
+				},
+				{
+					"text": "오징어게임",
+					"indices": [
+						65,
+						71
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						72,
+						75
+					]
+				},
+				{
+					"text": "신현지",
+					"indices": [
+						76,
+						80
+					]
+				},
+				{
+					"text": "임수향",
+					"indices": [
+						81,
+						85
+					]
+				},
+				{
+					"text": "엘라",
+					"indices": [
+						86,
+						89
+					]
+				},
+				{
+					"text": "키즈모델",
+					"indices": [
+						90,
+						95
+					]
+				},
+				{
+					"text": "송민호",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "마이노",
+					"indices": [
+						101,
+						105
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MiGr2cAzSM",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63805?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63805?…",
+					"indices": [
+						106,
+						129
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89038,
+			"friends_count": 4,
+			"listed_count": 403,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13518,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 41,
+		"favorite_count": 58,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "제니에게 선글라스 선물 받고 행복 무드 인증샷 릴레이-ing\n\n#블랙핑크 #블핑 #제니 #BLACKPINK #정호연 #오징어게임 #지수 #신현지 #임수향 #엘라 #키즈모델 #송민호 #마이노\nhttps://t.co/MiGr2cAzSM"
+	},
+	{
 		"created_at": "Tue Feb 15 09:31:14 +0000 2022",
 		"id": 1493513385832779800,
 		"id_str": "1493518254391894017",
