dd3a0f7888fd1746c1c835a5e6e6d5c81bdcec48
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-17 22:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 17 12:40:09 +0000 2022",
+		"id": 1494289728564252700,
+		"id_str": "1494290573334921218",
+		"full_text": "yellooo! me and @wiffygriffy have signed a load of Head on Fire CDs for you and you can get them here if you’d like one! 😏❤️‍🔥 https://t.co/9uZcFCjlBR https://t.co/fKiewWqWoi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wiffygriffy",
+					"name": "Griff",
+					"id": 1065546915406848000,
+					"id_str": "1065546915406848001",
+					"indices": [
+						16,
+						28
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9uZcFCjlBR",
+					"expanded_url": "https://sigrid.lnk.to/HOFSignedCDsIN",
+					"display_url": "sigrid.lnk.to/HOFSignedCDsIN",
+					"indices": [
+						127,
+						150
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1494289724730712000,
+					"id_str": "1494289724730712073",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FLzIlmoXIAk2lvx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLzIlmoXIAk2lvx.jpg",
+					"url": "https://t.co/fKiewWqWoi",
+					"display_url": "pic.twitter.com/fKiewWqWoi",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1494289728564252678/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1494289724730712000,
+					"id_str": "1494289724730712073",
+					"indices": [
+						151,
+						174
+					],
+					"media_url": "http://pbs.twimg.com/media/FLzIlmoXIAk2lvx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FLzIlmoXIAk2lvx.jpg",
+					"url": "https://t.co/fKiewWqWoi",
+					"display_url": "pic.twitter.com/fKiewWqWoi",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1494289728564252678/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 579,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "Head on Fire out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"url": "https://t.co/S4ulGLypNB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/S4ulGLypNB",
+							"expanded_url": "https://Sigrid.lnk.to/HeadOnFireTW",
+							"display_url": "Sigrid.lnk.to/HeadOnFireTW",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 84860,
+			"friends_count": 264,
+			"listed_count": 314,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 5958,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4271,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1427255384616624138/ZmgTeEsb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1642618759",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 100,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "yellooo! me and @wiffygriffy have signed a load of Head on Fire CDs for you and you can get them here if you’d like one! 😏❤️‍🔥 https://t.co/9uZcFCjlBR https://t.co/fKiewWqWoi"
+	},
+	{
 		"created_at": "Thu Feb 17 11:40:20 +0000 2022",
 		"id": 1494275210815500300,
 		"id_str": "1494275519185051651",
