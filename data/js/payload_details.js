e3deacc3aae8b93d242c81f1efac2bfcd8655d47
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-17 12:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85686,
-	"created_at": "Thu Feb 17 02:02:02 +0000 2022",
+	"tweets": 85687,
+	"created_at": "Thu Feb 17 03:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,262 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Thu Feb 17 02:45:57 +0000 2022",
+		"id": 1494130948233130000,
+		"id_str": "1494141037261905929",
+		"full_text": "#제작비지원 코스모 3월호 커버 주인공은 드라마 &lt;한사람만&gt;에서\n미도로 새로운 모습을 보인 #레드벨벳 #조이\n화면 속에서 상큼함 뚫고 나오는 토즈와 함께한 무빙 커버 공개❤️\n\n✔️조이의 요즘이 담긴 인터뷰와 화보는 코스모 3월호에서 확인해보자고☺️\n\n#redvelvet #joy #parksooyoung #박수영 https://t.co/yhDiUKwg5j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "제작비지원",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						57,
+						62
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						63,
+						66
+					]
+				},
+				{
+					"text": "redvelvet",
+					"indices": [
+						147,
+						157
+					]
+				},
+				{
+					"text": "joy",
+					"indices": [
+						158,
+						162
+					]
+				},
+				{
+					"text": "parksooyoung",
+					"indices": [
+						163,
+						176
+					]
+				},
+				{
+					"text": "박수영",
+					"indices": [
+						177,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1494130883900903400,
+					"id_str": "1494130883900903425",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1494130883900903425/pu/img/YarXXza4RHX5Pbi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1494130883900903425/pu/img/YarXXza4RHX5Pbi_.jpg",
+					"url": "https://t.co/yhDiUKwg5j",
+					"display_url": "pic.twitter.com/yhDiUKwg5j",
+					"expanded_url": "https://twitter.com/cosmokorea/status/1494130948233129984/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1494130883900903400,
+					"id_str": "1494130883900903425",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1494130883900903425/pu/img/YarXXza4RHX5Pbi_.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1494130883900903425/pu/img/YarXXza4RHX5Pbi_.jpg",
+					"url": "https://t.co/yhDiUKwg5j",
+					"display_url": "pic.twitter.com/yhDiUKwg5j",
+					"expanded_url": "https://twitter.com/cosmokorea/status/1494130948233129984/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 16544,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1494130883900903425/pu/vid/320x400/E3kcaUF4xhz87Xa7.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1494130883900903425/pu/pl/VnzrR6tZSfsQ70iH.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1494130883900903425/pu/vid/480x600/hbgQkYAIFIc16dPF.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1494130883900903425/pu/vid/720x900/gpschybdbk1KQu3x.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89064,
+			"friends_count": 4,
+			"listed_count": 403,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13524,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 248,
+		"favorite_count": 369,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#제작비지원 코스모 3월호 커버 주인공은 드라마 &lt;한사람만&gt;에서\n미도로 새로운 모습을 보인 #레드벨벳 #조이\n화면 속에서 상큼함 뚫고 나오는 토즈와 함께한 무빙 커버 공개❤️\n\n✔️조이의 요즘이 담긴 인터뷰와 화보는 코스모 3월호에서 확인해보자고☺️\n\n#redvelvet #joy #parksooyoung #박수영 https://t.co/yhDiUKwg5j"
+	},
+	{
 		"created_at": "Thu Feb 17 01:45:37 +0000 2022",
 		"id": 1494122888198168600,
 		"id_str": "1494125852254257153",
