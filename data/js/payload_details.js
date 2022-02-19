d661dc1fc19f285b56f0274ef7c63f0d809eca8b
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-19 20:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85725,
-	"created_at": "Sat Feb 19 10:02:03 +0000 2022",
+	"tweets": 85726,
+	"created_at": "Sat Feb 19 11:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 19 10:52:36 +0000 2022",
+		"id": 1494975049664798700,
+		"id_str": "1494988281846673415",
+		"full_text": "달나라 요정님 등판! 이 갈고 나온 태연의 갓벽한 뉴 앨범\n\n#태연 #아이앤비유 #INVU #앨범 #사랑 #노래 #음악 #가수 #가요 #멜로디 #댄스\nhttps://t.co/2XyQ0O1c8h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						34,
+						37
+					]
+				},
+				{
+					"text": "아이앤비유",
+					"indices": [
+						38,
+						44
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						45,
+						50
+					]
+				},
+				{
+					"text": "앨범",
+					"indices": [
+						51,
+						54
+					]
+				},
+				{
+					"text": "사랑",
+					"indices": [
+						55,
+						58
+					]
+				},
+				{
+					"text": "노래",
+					"indices": [
+						59,
+						62
+					]
+				},
+				{
+					"text": "음악",
+					"indices": [
+						63,
+						66
+					]
+				},
+				{
+					"text": "가수",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "가요",
+					"indices": [
+						71,
+						74
+					]
+				},
+				{
+					"text": "멜로디",
+					"indices": [
+						75,
+						79
+					]
+				},
+				{
+					"text": "댄스",
+					"indices": [
+						80,
+						83
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2XyQ0O1c8h",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/63835?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/63835?…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89096,
+			"friends_count": 4,
+			"listed_count": 404,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 170,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13534,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "달나라 요정님 등판! 이 갈고 나온 태연의 갓벽한 뉴 앨범\n\n#태연 #아이앤비유 #INVU #앨범 #사랑 #노래 #음악 #가수 #가요 #멜로디 #댄스\nhttps://t.co/2XyQ0O1c8h"
+	},
+	{
 		"created_at": "Sat Feb 19 09:40:22 +0000 2022",
 		"id": 1494966813217931300,
 		"id_str": "1494970101883453446",
