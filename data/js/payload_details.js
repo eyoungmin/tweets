b333b3c79d87fe65b6d51ba2b80261013aea5401
Youngmin Lee <iameyoungmin@gmail.com>
2022-02-27 16:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85823,
-	"created_at": "Sun Feb 27 06:02:07 +0000 2022",
+	"tweets": 85824,
+	"created_at": "Sun Feb 27 07:02:09 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sun Feb 27 06:40:09 +0000 2022",
+		"id": 1497808970806804500,
+		"id_str": "1497823854969905155",
+		"full_text": "태양 바다 하늘 그리고 고양이 https://t.co/sTUEp4Tg1e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1497808962108158000,
+					"id_str": "1497808962108157953",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FMlJULfacAEymVw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMlJULfacAEymVw.jpg",
+					"url": "https://t.co/sTUEp4Tg1e",
+					"display_url": "pic.twitter.com/sTUEp4Tg1e",
+					"expanded_url": "https://twitter.com/love_yuna/status/1497808970806804488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 983,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1758,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 557,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1497808962108158000,
+					"id_str": "1497808962108157953",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FMlJULfacAEymVw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMlJULfacAEymVw.jpg",
+					"url": "https://t.co/sTUEp4Tg1e",
+					"display_url": "pic.twitter.com/sTUEp4Tg1e",
+					"expanded_url": "https://twitter.com/love_yuna/status/1497808970806804488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 983,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1758,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 557,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1497808962120712200,
+					"id_str": "1497808962120712198",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FMlJULiaAAYMrk-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMlJULiaAAYMrk-.jpg",
+					"url": "https://t.co/sTUEp4Tg1e",
+					"display_url": "pic.twitter.com/sTUEp4Tg1e",
+					"expanded_url": "https://twitter.com/love_yuna/status/1497808970806804488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 983,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1758,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 557,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1497808962095546400,
+					"id_str": "1497808962095546376",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FMlJULcaAAgmKlH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMlJULcaAAgmKlH.jpg",
+					"url": "https://t.co/sTUEp4Tg1e",
+					"display_url": "pic.twitter.com/sTUEp4Tg1e",
+					"expanded_url": "https://twitter.com/love_yuna/status/1497808970806804488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1758,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 983,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 557,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1497808962095546400,
+					"id_str": "1497808962095546373",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FMlJULcaAAUs-5D.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FMlJULcaAAUs-5D.jpg",
+					"url": "https://t.co/sTUEp4Tg1e",
+					"display_url": "pic.twitter.com/sTUEp4Tg1e",
+					"expanded_url": "https://twitter.com/love_yuna/status/1497808970806804488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1758,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 557,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 983,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274359,
+			"friends_count": 562,
+			"listed_count": 2797,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 365,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6656,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1453993019011899392/99qVm7Op_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1635493925",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 176,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "태양 바다 하늘 그리고 고양이 https://t.co/sTUEp4Tg1e"
+	},
+	{
 		"created_at": "Sun Feb 27 05:21:34 +0000 2022",
 		"id": 1497798266297016300,
 		"id_str": "1497804078511919106",
