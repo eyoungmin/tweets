677432c816ae0827af5e06aa897f01b51cab4f80
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-02 01:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85851,
-	"created_at": "Tue Mar 01 15:19:53 +0000 2022",
+	"tweets": 85852,
+	"created_at": "Tue Mar 01 16:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,233 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 01 15:40:17 +0000 2022",
+		"id": 1498682841600864300,
+		"id_str": "1498684559885586433",
+		"full_text": "“Lost track” out now. Directed by Paul Thomas Anderson. Produced by Danielle Haim, Ariel Rechtshaid, and John DeBold. Video up on on @wmag\n\nhttps://t.co/Md2HkGLxIG https://t.co/FWOZzuJsLj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			163
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "wmag",
+					"name": "W Magazine",
+					"id": 14791162,
+					"id_str": "14791162",
+					"indices": [
+						133,
+						138
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Md2HkGLxIG",
+					"expanded_url": "https://www.wmagazine.com/culture/haim-lost-track-music-video-paul-thomas-anderson",
+					"display_url": "wmagazine.com/culture/haim-l…",
+					"indices": [
+						140,
+						163
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1498682752329265200,
+					"id_str": "1498682752329265171",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1498682752329265171/pu/img/eNumkh4iCi3FyV8h.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1498682752329265171/pu/img/eNumkh4iCi3FyV8h.jpg",
+					"url": "https://t.co/FWOZzuJsLj",
+					"display_url": "pic.twitter.com/FWOZzuJsLj",
+					"expanded_url": "https://twitter.com/HAIMtheband/status/1498682841600864256/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1498682752329265200,
+					"id_str": "1498682752329265171",
+					"indices": [
+						164,
+						187
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1498682752329265171/pu/img/eNumkh4iCi3FyV8h.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1498682752329265171/pu/img/eNumkh4iCi3FyV8h.jpg",
+					"url": "https://t.co/FWOZzuJsLj",
+					"display_url": "pic.twitter.com/FWOZzuJsLj",
+					"expanded_url": "https://twitter.com/HAIMtheband/status/1498682841600864256/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 22583,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1498682752329265171/pu/vid/720x720/Iu5zSx0jhZMFGrnW.mp4?tag=12"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1498682752329265171/pu/vid/320x320/yU-gJyIguNFEUeaQ.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1498682752329265171/pu/pl/uHtNbVQ2b_AMjhN9.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1498682752329265171/pu/vid/540x540/Hbl-36bG5GYIKqM9.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 126632581,
+			"id_str": "126632581",
+			"name": "HAIM",
+			"screen_name": "HAIMtheband",
+			"location": "",
+			"description": "gasoline (feat. taylor swift) + 3 am (feat. thundercat) out now",
+			"url": "https://t.co/Symo2VZ73P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Symo2VZ73P",
+							"expanded_url": "https://haim.lnk.to/WIMPIIIExpanded",
+							"display_url": "haim.lnk.to/WIMPIIIExpanded",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1191882,
+			"friends_count": 433,
+			"listed_count": 2324,
+			"created_at": "Fri Mar 26 13:59:02 +0000 2010",
+			"favourites_count": 2297,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3731,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1234510519593136128/e995Dxra_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1234510519593136128/e995Dxra_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/126632581/1583165220",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 293,
+		"favorite_count": 1323,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Lost track” out now. Directed by Paul Thomas Anderson. Produced by Danielle Haim, Ariel Rechtshaid, and John DeBold. Video up on on @wmag\n\nhttps://t.co/Md2HkGLxIG https://t.co/FWOZzuJsLj"
+	},
+	{
 		"created_at": "Tue Mar 01 15:06:11 +0000 2022",
 		"id": 1498674420671467500,
 		"id_str": "1498675976808636420",
