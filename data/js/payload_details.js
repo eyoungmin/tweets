41b9503786635a01670e2f07e74d2a17c52ea35b
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-01 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85842,
-	"created_at": "Mon Feb 28 20:02:09 +0000 2022",
+	"tweets": 85843,
+	"created_at": "Tue Mar 01 06:02:03 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,131 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Tue Mar 01 05:40:13 +0000 2022",
+		"id": 1498532618006306800,
+		"id_str": "1498533545652133894",
+		"full_text": "[NEWS] \n\n이달의 소녀 올리비아 혜, 1일 공기남 ‘봄이 되어줄게’ 리메이크 신곡 발매…봄을 머금은 따뜻한 감성 선사\n\n▶️ https://t.co/6c1Qp14ZN7\n\n#이달의소녀 #LOONA #OliviaHye",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						98,
+						104
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						105,
+						111
+					]
+				},
+				{
+					"text": "OliviaHye",
+					"indices": [
+						112,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6c1Qp14ZN7",
+					"expanded_url": "https://n.news.naver.com/entertain/article/215/0001017368",
+					"display_url": "n.news.naver.com/entertain/arti…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496008272398418000,
+			"id_str": "1496008272398417930",
+			"name": "BlockBerryCreative",
+			"screen_name": "BBC_blockberry",
+			"location": "",
+			"description": "BlockBerryCreative official twitter / 이달의 소녀 LOONA / 선예",
+			"url": "https://t.co/bt27T8GawB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bt27T8GawB",
+							"expanded_url": "http://blockberrycreative.com",
+							"display_url": "blockberrycreative.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12033,
+			"friends_count": 2,
+			"listed_count": 28,
+			"created_at": "Tue Feb 22 06:26:05 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 136,
+		"favorite_count": 458,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NEWS] \n\n이달의 소녀 올리비아 혜, 1일 공기남 ‘봄이 되어줄게’ 리메이크 신곡 발매…봄을 머금은 따뜻한 감성 선사\n\n▶️ https://t.co/6c1Qp14ZN7\n\n#이달의소녀 #LOONA #OliviaHye"
+	}
+]
