e11c6b78ee61406cd602a9dc57cbf9291b4fc5a5
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-02 23:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,207 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 02 13:40:10 +0000 2022",
+		"id": 1499008418107240400,
+		"id_str": "1499016716441071620",
+		"full_text": "[D-1]\nGlen Check Album 'Bleach'\n-\n🎧3/3 6PM KST \n🐇Pre-save link: https://t.co/zZC0YsDeoh\n-\n#글렌체크 #GlenCheck #Bleach #Sins https://t.co/pBEYt5plvJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "글렌체크",
+					"indices": [
+						90,
+						95
+					]
+				},
+				{
+					"text": "GlenCheck",
+					"indices": [
+						96,
+						106
+					]
+				},
+				{
+					"text": "Bleach",
+					"indices": [
+						107,
+						114
+					]
+				},
+				{
+					"text": "Sins",
+					"indices": [
+						115,
+						120
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zZC0YsDeoh",
+					"expanded_url": "https://orcd.co/glencheck_bleach",
+					"display_url": "orcd.co/glencheck_blea…",
+					"indices": [
+						64,
+						87
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1499008407671808000,
+					"id_str": "1499008407671808002",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FM2MNGaaIAIZgqd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM2MNGaaIAIZgqd.jpg",
+					"url": "https://t.co/pBEYt5plvJ",
+					"display_url": "pic.twitter.com/pBEYt5plvJ",
+					"expanded_url": "https://twitter.com/bandglencheck/status/1499008418107240450/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499008407671808000,
+					"id_str": "1499008407671808002",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FM2MNGaaIAIZgqd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM2MNGaaIAIZgqd.jpg",
+					"url": "https://t.co/pBEYt5plvJ",
+					"display_url": "pic.twitter.com/pBEYt5plvJ",
+					"expanded_url": "https://twitter.com/bandglencheck/status/1499008418107240450/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 258798345,
+			"id_str": "258798345",
+			"name": "Glen Check",
+			"screen_name": "bandglencheck",
+			"location": "Seoul, KR",
+			"description": "Glen Check Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5191,
+			"friends_count": 0,
+			"listed_count": 108,
+			"created_at": "Mon Feb 28 14:36:33 +0000 2011",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 35,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1433352828618043395/HU3cgWIp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1433352828618043395/HU3cgWIp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/258798345/1631004834",
+			"profile_link_color": "A23AAB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DCF5E3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 27,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[D-1]\nGlen Check Album 'Bleach'\n-\n🎧3/3 6PM KST \n🐇Pre-save link: https://t.co/zZC0YsDeoh\n-\n#글렌체크 #GlenCheck #Bleach #Sins https://t.co/pBEYt5plvJ"
+	},
+	{
 		"created_at": "Wed Mar 02 11:40:15 +0000 2022",
 		"id": 1498979920172634000,
 		"id_str": "1498986540667650057",
