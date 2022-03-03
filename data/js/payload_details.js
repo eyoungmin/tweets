9b5b35ca80d5245151028463ffb075ed28d61140
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-03 22:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85880,
-	"created_at": "Thu Mar 03 12:02:00 +0000 2022",
+	"tweets": 85881,
+	"created_at": "Thu Mar 03 13:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 03 12:40:09 +0000 2022",
+		"id": 1499355148053000200,
+		"id_str": "1499364002312380416",
+		"full_text": "[📹] Seori's Music-log #Shorts \n\n&lt;조명 있vs없🙄 &gt;\n👉 https://t.co/YdSB6Sf9Bz\n\n#Seori #서리\n#뮤직로그 #쇼츠 https://t.co/Ul9DyHg3jR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Shorts",
+					"indices": [
+						22,
+						29
+					]
+				},
+				{
+					"text": "Seori",
+					"indices": [
+						77,
+						83
+					]
+				},
+				{
+					"text": "서리",
+					"indices": [
+						84,
+						87
+					]
+				},
+				{
+					"text": "뮤직로그",
+					"indices": [
+						88,
+						93
+					]
+				},
+				{
+					"text": "쇼츠",
+					"indices": [
+						94,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YdSB6Sf9Bz",
+					"expanded_url": "https://youtu.be/YJtMv02E3rU",
+					"display_url": "youtu.be/YJtMv02E3rU",
+					"indices": [
+						52,
+						75
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1499355130281758700,
+					"id_str": "1499355130281758721",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FM7HjAyakAExD57.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM7HjAyakAExD57.jpg",
+					"url": "https://t.co/Ul9DyHg3jR",
+					"display_url": "pic.twitter.com/Ul9DyHg3jR",
+					"expanded_url": "https://twitter.com/seori_official/status/1499355148053000194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499355130281758700,
+					"id_str": "1499355130281758721",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/FM7HjAyakAExD57.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM7HjAyakAExD57.jpg",
+					"url": "https://t.co/Ul9DyHg3jR",
+					"display_url": "pic.twitter.com/Ul9DyHg3jR",
+					"expanded_url": "https://twitter.com/seori_official/status/1499355148053000194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/2jDAeGXOz0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2jDAeGXOz0",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43117,
+			"friends_count": 0,
+			"listed_count": 177,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 508,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 107,
+		"favorite_count": 425,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[📹] Seori's Music-log #Shorts \n\n&lt;조명 있vs없🙄 &gt;\n👉 https://t.co/YdSB6Sf9Bz\n\n#Seori #서리\n#뮤직로그 #쇼츠 https://t.co/Ul9DyHg3jR"
+	},
+	{
 		"created_at": "Thu Mar 03 11:40:10 +0000 2022",
 		"id": 1499338950414004200,
 		"id_str": "1499348905426886656",
