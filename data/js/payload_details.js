89329d07d5695c81c16094582059cbe65a2cf99a
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-05 02:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85899,
-	"created_at": "Fri Mar 04 15:02:00 +0000 2022",
+	"tweets": 85900,
+	"created_at": "Fri Mar 04 17:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 04 16:40:10 +0000 2022",
+		"id": 1499461783509164000,
+		"id_str": "1499786790558998541",
+		"full_text": "https://t.co/n6N6AtWHjn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1499461765926727700,
+					"id_str": "1499461765926727680",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8oiBhXsAA4wYq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8oiBhXsAA4wYq.jpg",
+					"url": "https://t.co/n6N6AtWHjn",
+					"display_url": "pic.twitter.com/n6N6AtWHjn",
+					"expanded_url": "https://twitter.com/pantone/status/1499461783509164035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1499461765926727700,
+					"id_str": "1499461765926727680",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FM8oiBhXsAA4wYq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FM8oiBhXsAA4wYq.jpg",
+					"url": "https://t.co/n6N6AtWHjn",
+					"display_url": "pic.twitter.com/n6N6AtWHjn",
+					"expanded_url": "https://twitter.com/pantone/status/1499461783509164035/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17446304,
+			"id_str": "17446304",
+			"name": "PANTONE",
+			"screen_name": "pantone",
+			"location": "Carlstadt, NJ",
+			"description": "Pantone, the global authority on color; inspiring artists, designers and stylists to color the world. #VeryPeri, our #ColorOfTheYear 2022",
+			"url": "https://t.co/QwqjZPVkIh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QwqjZPVkIh",
+							"expanded_url": "https://linktr.ee/pantone",
+							"display_url": "linktr.ee/pantone",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 219548,
+			"friends_count": 2307,
+			"listed_count": 3682,
+			"created_at": "Mon Nov 17 17:55:10 +0000 2008",
+			"favourites_count": 4948,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12532,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F0F5F3",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1468738976630689793/11iafsP9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1468738976630689793/11iafsP9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17446304/1639009635",
+			"profile_link_color": "92A8D1",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "ECEEEF",
+			"profile_text_color": "0B4A63",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 57012,
+		"favorite_count": 134993,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/n6N6AtWHjn"
+	},
+	{
 		"created_at": "Fri Mar 04 14:40:15 +0000 2022",
 		"id": 1499752508838666200,
 		"id_str": "1499756614861172738",
