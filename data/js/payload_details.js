2866d44ebf7674ba508c320ef0033806e967a6c2
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-05 21:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 85911,
-	"created_at": "Sat Mar 05 10:02:01 +0000 2022",
+	"tweets": 85912,
+	"created_at": "Sat Mar 05 12:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sat Mar 05 11:36:41 +0000 2022",
+		"id": 1500068805514772500,
+		"id_str": "1500072807132246019",
+		"full_text": "YouTube Originals \"Re:MASTERPIECE\"\nSpecial Episode Trailer\n👉 https://t.co/Vuv5SPdAgd\n\n2022.03.11 11PM KST\nSMTOWN YouTube channel\n\n#BoA #aespa #DreamsComeTrue\n#리마스터피스 #ReMASTERPIECE https://t.co/WhVdCOK6nC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						130,
+						134
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						135,
+						141
+					]
+				},
+				{
+					"text": "DreamsComeTrue",
+					"indices": [
+						142,
+						157
+					]
+				},
+				{
+					"text": "리마스터피스",
+					"indices": [
+						158,
+						165
+					]
+				},
+				{
+					"text": "ReMASTERPIECE",
+					"indices": [
+						166,
+						180
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Vuv5SPdAgd",
+					"expanded_url": "https://youtu.be/GW7ikG1mFAg",
+					"display_url": "youtu.be/GW7ikG1mFAg",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1500068786954977300,
+					"id_str": "1500068786954977283",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FNFQnTEaQAMF6Mx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNFQnTEaQAMF6Mx.jpg",
+					"url": "https://t.co/WhVdCOK6nC",
+					"display_url": "pic.twitter.com/WhVdCOK6nC",
+					"expanded_url": "https://twitter.com/BoA_Official/status/1500068805514772486/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1500068786954977300,
+					"id_str": "1500068786954977283",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FNFQnTEaQAMF6Mx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNFQnTEaQAMF6Mx.jpg",
+					"url": "https://t.co/WhVdCOK6nC",
+					"display_url": "pic.twitter.com/WhVdCOK6nC",
+					"expanded_url": "https://twitter.com/BoA_Official/status/1500068805514772486/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 66988,
+			"friends_count": 1,
+			"listed_count": 252,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 274,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 146,
+		"favorite_count": 335,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "YouTube Originals \"Re:MASTERPIECE\"\nSpecial Episode Trailer\n👉 https://t.co/Vuv5SPdAgd\n\n2022.03.11 11PM KST\nSMTOWN YouTube channel\n\n#BoA #aespa #DreamsComeTrue\n#리마스터피스 #ReMASTERPIECE https://t.co/WhVdCOK6nC"
+	},
+	{
 		"created_at": "Sat Mar 05 09:45:55 +0000 2022",
 		"id": 1500033959320711200,
 		"id_str": "1500044932073074692",
