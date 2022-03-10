4010b8cdc4786271351a8a0c34823c0b3ab51177
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-11 01:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86007,
-	"created_at": "Thu Mar 10 15:19:57 +0000 2022",
+	"tweets": 86008,
+	"created_at": "Thu Mar 10 16:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 10 15:40:16 +0000 2022",
+		"id": 1501935912611029000,
+		"id_str": "1501946044048764949",
+		"full_text": "Seori 5th Digital Single \n[Can't Stop This Party]\nPhoto Teaser 3\n\n📀Release\n2022. 03. 22. TUE\n1PM (KST) / 12AM (EST)\n\n#Seori #서리 #Cant_Stop_This_Party https://t.co/t1YJ0VDzAl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Seori",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "서리",
+					"indices": [
+						124,
+						127
+					]
+				},
+				{
+					"text": "Cant_Stop_This_Party",
+					"indices": [
+						128,
+						149
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1501780673065271300,
+					"id_str": "1501780673065271297",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FNdlkLyaQAETe2B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNdlkLyaQAETe2B.jpg",
+					"url": "https://t.co/t1YJ0VDzAl",
+					"display_url": "pic.twitter.com/t1YJ0VDzAl",
+					"expanded_url": "https://twitter.com/seori_official/status/1501935912611028992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1501780673065271300,
+					"id_str": "1501780673065271297",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/FNdlkLyaQAETe2B.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNdlkLyaQAETe2B.jpg",
+					"url": "https://t.co/t1YJ0VDzAl",
+					"display_url": "pic.twitter.com/t1YJ0VDzAl",
+					"expanded_url": "https://twitter.com/seori_official/status/1501935912611028992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/2jDAeGXOz0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2jDAeGXOz0",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43490,
+			"friends_count": 0,
+			"listed_count": 182,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 527,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 444,
+		"favorite_count": 1155,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Seori 5th Digital Single \n[Can't Stop This Party]\nPhoto Teaser 3\n\n📀Release\n2022. 03. 22. TUE\n1PM (KST) / 12AM (EST)\n\n#Seori #서리 #Cant_Stop_This_Party https://t.co/t1YJ0VDzAl"
+	},
+	{
 		"created_at": "Thu Mar 10 15:08:26 +0000 2022",
 		"id": 1501935912451719200,
 		"id_str": "1501938035423801364",
