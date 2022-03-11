d981c51602310f9e90f9b9fb14c3bc5256172351
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-11 13:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86014,
-	"created_at": "Fri Mar 11 03:02:02 +0000 2022",
+	"tweets": 86015,
+	"created_at": "Fri Mar 11 04:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 11 03:40:09 +0000 2022",
+		"id": 1502123978982592500,
+		"id_str": "1502127210710376450",
+		"full_text": "[🗞] 서리 ‘Can’t Stop This Party’ 신비롭다\n\n👉 https://t.co/ZIjSt2L9Fu\n\n#Seori #서리\n#서리news #news https://t.co/D87MVltVgo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Seori",
+					"indices": [
+						64,
+						70
+					]
+				},
+				{
+					"text": "서리",
+					"indices": [
+						71,
+						74
+					]
+				},
+				{
+					"text": "서리news",
+					"indices": [
+						75,
+						82
+					]
+				},
+				{
+					"text": "news",
+					"indices": [
+						83,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZIjSt2L9Fu",
+					"expanded_url": "http://naver.me/FDlbpgDM",
+					"display_url": "naver.me/FDlbpgDM",
+					"indices": [
+						39,
+						62
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1502123970262597600,
+					"id_str": "1502123970262597632",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FNidytgaAAAI3iL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNidytgaAAAI3iL.jpg",
+					"url": "https://t.co/D87MVltVgo",
+					"display_url": "pic.twitter.com/D87MVltVgo",
+					"expanded_url": "https://twitter.com/seori_official/status/1502123978982592515/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1502123970262597600,
+					"id_str": "1502123970262597632",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FNidytgaAAAI3iL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNidytgaAAAI3iL.jpg",
+					"url": "https://t.co/D87MVltVgo",
+					"display_url": "pic.twitter.com/D87MVltVgo",
+					"expanded_url": "https://twitter.com/seori_official/status/1502123978982592515/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/2jDAeGXOz0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2jDAeGXOz0",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43509,
+			"friends_count": 0,
+			"listed_count": 183,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 528,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 104,
+		"favorite_count": 404,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[🗞] 서리 ‘Can’t Stop This Party’ 신비롭다\n\n👉 https://t.co/ZIjSt2L9Fu\n\n#Seori #서리\n#서리news #news https://t.co/D87MVltVgo"
+	},
+	{
 		"created_at": "Fri Mar 11 02:08:50 +0000 2022",
 		"id": 1502098842480611300,
 		"id_str": "1502104227266457600",
