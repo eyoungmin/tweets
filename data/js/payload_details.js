ccc95bd0a513866314750425558f842ad716d9dd
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-14 17:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86061,
-	"created_at": "Mon Mar 14 07:02:02 +0000 2022",
+	"tweets": 86062,
+	"created_at": "Mon Mar 14 08:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,242 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 14 07:37:20 +0000 2022",
+		"id": 1503262265155649500,
+		"id_str": "1503274061492752387",
+		"full_text": "Vogue Korea April Issue with #JISOO  #BLACKPINK \nPre-orders Available Now!\n📎https://t.co/L9gmHDZoLY\n*Click on the pink “알라딘 US” (Aladin US) button on the top right of the webpage https://t.co/2I4ftgYKKC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JISOO",
+					"indices": [
+						29,
+						35
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						37,
+						47
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/L9gmHDZoLY",
+					"expanded_url": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=290855144",
+					"display_url": "aladin.co.kr/shop/wproduct.…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1503260671861223400,
+					"id_str": "1503260671861223424",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/FNynnc8acAAJmJi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNynnc8acAAJmJi.jpg",
+					"url": "https://t.co/2I4ftgYKKC",
+					"display_url": "pic.twitter.com/2I4ftgYKKC",
+					"expanded_url": "https://twitter.com/VogueKorea/status/1503262265155649539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 941,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 941,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 542,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503260671861223400,
+					"id_str": "1503260671861223424",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/FNynnc8acAAJmJi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNynnc8acAAJmJi.jpg",
+					"url": "https://t.co/2I4ftgYKKC",
+					"display_url": "pic.twitter.com/2I4ftgYKKC",
+					"expanded_url": "https://twitter.com/VogueKorea/status/1503262265155649539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 941,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 941,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 542,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1503260693684158500,
+					"id_str": "1503260693684158464",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/FNynouPaAAAj625.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FNynouPaAAAj625.jpg",
+					"url": "https://t.co/2I4ftgYKKC",
+					"display_url": "pic.twitter.com/2I4ftgYKKC",
+					"expanded_url": "https://twitter.com/VogueKorea/status/1503262265155649539/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 753,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 753,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 542,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 159102271,
+			"id_str": "159102271",
+			"name": "VOGUE KOREA",
+			"screen_name": "VogueKorea",
+			"location": "",
+			"description": "Vogue Korea official twitter",
+			"url": "https://t.co/76FZW60Cc4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/76FZW60Cc4",
+							"expanded_url": "http://www.vogue.co.kr",
+							"display_url": "vogue.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 150447,
+			"friends_count": 0,
+			"listed_count": 381,
+			"created_at": "Thu Jun 24 13:11:05 +0000 2010",
+			"favourites_count": 33,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14669,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1320587185767116802/S3U8-krQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1320587185767116802/S3U8-krQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/159102271/1640228342",
+			"profile_link_color": "F5ABB5",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DCF5EF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2941,
+		"favorite_count": 5541,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Vogue Korea April Issue with #JISOO  #BLACKPINK \nPre-orders Available Now!\n📎https://t.co/L9gmHDZoLY\n*Click on the pink “알라딘 US” (Aladin US) button on the top right of the webpage https://t.co/2I4ftgYKKC"
+	},
+	{
 		"created_at": "Mon Mar 14 06:52:06 +0000 2022",
 		"id": 1503251481050611700,
 		"id_str": "1503262677820325894",
