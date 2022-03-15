48c8e4869ad937ea391057020f935b21ce8f6e97
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-15 19:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86077,
-	"created_at": "Tue Mar 15 08:02:03 +0000 2022",
+	"tweets": 86078,
+	"created_at": "Tue Mar 15 10:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 15 09:05:59 +0000 2022",
+		"id": 1503657709354496000,
+		"id_str": "1503658761352716290",
+		"full_text": "단발로 변신한\n바닐라코의 곤듀님👸\n세경씨를 닮은\n예쁜 꽃들이 만개하는 봄이🌸\n쓱크립트에 왔세경💛\n\n📝 https://t.co/jYu5RcCjON\n\n#신세경 #ShinSaeKyeong\n#쓱크립트 #SSKcript https://t.co/wAQzD45hfi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "신세경",
+					"indices": [
+						82,
+						86
+					]
+				},
+				{
+					"text": "ShinSaeKyeong",
+					"indices": [
+						87,
+						101
+					]
+				},
+				{
+					"text": "쓱크립트",
+					"indices": [
+						102,
+						107
+					]
+				},
+				{
+					"text": "SSKcript",
+					"indices": [
+						108,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jYu5RcCjON",
+					"expanded_url": "https://youtu.be/OU0rPAJB3fQ",
+					"display_url": "youtu.be/OU0rPAJB3fQ",
+					"indices": [
+						57,
+						80
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1503657661686227000,
+					"id_str": "1503657661686226949",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/FN4QrTSaIAUWbGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN4QrTSaIAUWbGi.jpg",
+					"url": "https://t.co/wAQzD45hfi",
+					"display_url": "pic.twitter.com/wAQzD45hfi",
+					"expanded_url": "https://twitter.com/edam_ent/status/1503657709354496000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1503657661686227000,
+					"id_str": "1503657661686226949",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/FN4QrTSaIAUWbGi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FN4QrTSaIAUWbGi.jpg",
+					"url": "https://t.co/wAQzD45hfi",
+					"display_url": "pic.twitter.com/wAQzD45hfi",
+					"expanded_url": "https://twitter.com/edam_ent/status/1503657709354496000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1214016219889553400,
+			"id_str": "1214016219889553409",
+			"name": "EDAM Ent.",
+			"screen_name": "edam_ent",
+			"location": "",
+			"description": "EDAM Entertainment Official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 61755,
+			"friends_count": 2,
+			"listed_count": 227,
+			"created_at": "Mon Jan 06 02:50:04 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 200,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1229315447058653184/paC9xrhE_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 124,
+		"favorite_count": 536,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "단발로 변신한\n바닐라코의 곤듀님👸\n세경씨를 닮은\n예쁜 꽃들이 만개하는 봄이🌸\n쓱크립트에 왔세경💛\n\n📝 https://t.co/jYu5RcCjON\n\n#신세경 #ShinSaeKyeong\n#쓱크립트 #SSKcript https://t.co/wAQzD45hfi"
+	},
+	{
 		"created_at": "Tue Mar 15 07:52:21 +0000 2022",
 		"id": 1503634981826207700,
 		"id_str": "1503640228333989890",
