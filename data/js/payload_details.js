f102303f4f01c11d6f22c2354d03a82a940b9aeb
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-15 13:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86074,
-	"created_at": "Tue Mar 15 03:02:03 +0000 2022",
+	"tweets": 86075,
+	"created_at": "Tue Mar 15 04:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 15 03:52:19 +0000 2022",
+		"id": 1503574857560506400,
+		"id_str": "1503579821065981955",
+		"full_text": "허쉬컷,,,? 어렸을 적 유행했던 그 샤기 컷?! \n\n#허쉬컷 #여자헤어스타일링 #허쉬컷스타일 #2022헤어트렌드 #아이유 #웬디 #윤아 #수지 #최소라 \n\nhttps://t.co/PKhPlAz1L9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "허쉬컷",
+					"indices": [
+						30,
+						34
+					]
+				},
+				{
+					"text": "여자헤어스타일링",
+					"indices": [
+						35,
+						44
+					]
+				},
+				{
+					"text": "허쉬컷스타일",
+					"indices": [
+						45,
+						52
+					]
+				},
+				{
+					"text": "2022헤어트렌드",
+					"indices": [
+						53,
+						63
+					]
+				},
+				{
+					"text": "아이유",
+					"indices": [
+						64,
+						68
+					]
+				},
+				{
+					"text": "웬디",
+					"indices": [
+						69,
+						72
+					]
+				},
+				{
+					"text": "윤아",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "수지",
+					"indices": [
+						77,
+						80
+					]
+				},
+				{
+					"text": "최소라",
+					"indices": [
+						81,
+						85
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PKhPlAz1L9",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/64633",
+					"display_url": "cosmopolitan.co.kr/article/64633",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89397,
+			"friends_count": 3,
+			"listed_count": 401,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 228,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13596,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "허쉬컷,,,? 어렸을 적 유행했던 그 샤기 컷?! \n\n#허쉬컷 #여자헤어스타일링 #허쉬컷스타일 #2022헤어트렌드 #아이유 #웬디 #윤아 #수지 #최소라 \n\nhttps://t.co/PKhPlAz1L9"
+	},
+	{
 		"created_at": "Tue Mar 15 02:16:54 +0000 2022",
 		"id": 1503549055615795200,
 		"id_str": "1503555810101669891",
