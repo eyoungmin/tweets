a79fe11ce4d46f65ba21fe1506ce043d612ecab3
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-17 12:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86108,
-	"created_at": "Thu Mar 17 01:02:01 +0000 2022",
+	"tweets": 86109,
+	"created_at": "Thu Mar 17 03:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,131 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 17 02:40:14 +0000 2022",
+		"id": 1504271983588278300,
+		"id_str": "1504286459100028932",
+		"full_text": "TAEYEON’s 3rd album ‘INVU’ won two awards on Gaon charts for four weeks in a row &amp; 7 awards on music shows! \nProve the long-run popularity of 'Unrivaled Music Queen'!\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			205
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "태연",
+					"indices": [
+						172,
+						175
+					]
+				},
+				{
+					"text": "TAEYEON",
+					"indices": [
+						176,
+						184
+					]
+				},
+				{
+					"text": "INVU",
+					"indices": [
+						186,
+						191
+					]
+				},
+				{
+					"text": "TAEYEON_INVU",
+					"indices": [
+						192,
+						205
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1504271982153846800,
+		"in_reply_to_status_id_str": "1504271982153846784",
+		"in_reply_to_user_id": 1496499777965822000,
+		"in_reply_to_user_id_str": "1496499777965821956",
+		"in_reply_to_screen_name": "TAEYEONsmtown",
+		"user": {
+			"id": 1496499777965822000,
+			"id_str": "1496499777965821956",
+			"name": "TAEYEON Official",
+			"screen_name": "TAEYEONsmtown",
+			"location": "",
+			"description": "TAEYEON The 3rd Album 'INVU'",
+			"url": "https://t.co/c1nmkKMaGi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/c1nmkKMaGi",
+							"expanded_url": "https://youtu.be/AbZH7XWDW_k",
+							"display_url": "youtu.be/AbZH7XWDW_k",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 80064,
+			"friends_count": 2,
+			"listed_count": 215,
+			"created_at": "Wed Feb 23 14:59:02 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 27,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496687188041678849/ngLIpota_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1496499777965821956/1645673016",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 894,
+		"favorite_count": 2037,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "TAEYEON’s 3rd album ‘INVU’ won two awards on Gaon charts for four weeks in a row &amp; 7 awards on music shows! \nProve the long-run popularity of 'Unrivaled Music Queen'!\n\n#태연 #TAEYEON \n#INVU #TAEYEON_INVU"
+	},
+	{
 		"created_at": "Thu Mar 17 00:29:38 +0000 2022",
 		"id": 1504251677180072000,
 		"id_str": "1504253592391278593",
