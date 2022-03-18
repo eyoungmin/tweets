fa2f4f3eb5ec3d5b401d8740415186317e7717e8
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-18 13:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86132,
-	"created_at": "Fri Mar 18 03:02:03 +0000 2022",
+	"tweets": 86133,
+	"created_at": "Fri Mar 18 04:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 18 03:09:35 +0000 2022",
+		"id": 1504653824207704000,
+		"id_str": "1504656233873162242",
+		"full_text": "Red Velvet ‘The ReVe Festival 2022 - Feel My Rhythm’ Countdown Live\n\n➫ 2022.03.21 5PM (KST)\n\n➫ https://t.co/0qQ8uMs2Wl\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#레드벨벳 #RedVelvet https://t.co/wPwQkbi6kp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			183
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						120,
+						143
+					]
+				},
+				{
+					"text": "Feel_My_Rhythm",
+					"indices": [
+						144,
+						159
+					]
+				},
+				{
+					"text": "필마이리듬",
+					"indices": [
+						160,
+						166
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						167,
+						172
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						173,
+						183
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0qQ8uMs2Wl",
+					"expanded_url": "https://youtu.be/a5YztqutGsc",
+					"display_url": "youtu.be/a5YztqutGsc",
+					"indices": [
+						95,
+						118
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1504289153365655600,
+					"id_str": "1504289153365655552",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FOBPA9HVQAAN2vJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOBPA9HVQAAN2vJ.jpg",
+					"url": "https://t.co/wPwQkbi6kp",
+					"display_url": "pic.twitter.com/wPwQkbi6kp",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1504653824207704068/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 865,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 490,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1477,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504289153365655600,
+					"id_str": "1504289153365655552",
+					"indices": [
+						184,
+						207
+					],
+					"media_url": "http://pbs.twimg.com/media/FOBPA9HVQAAN2vJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOBPA9HVQAAN2vJ.jpg",
+					"url": "https://t.co/wPwQkbi6kp",
+					"display_url": "pic.twitter.com/wPwQkbi6kp",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1504653824207704068/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 865,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 490,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1477,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet  'The ReVe Festival 2022 - Feel My Rhythm'\n\n2022.3.21. 6PM KST",
+			"url": "https://t.co/0hh6Lb9mYR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0hh6Lb9mYR",
+							"expanded_url": "https://www.youtube.com/redvelvet",
+							"display_url": "youtube.com/redvelvet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5164623,
+			"friends_count": 2,
+			"listed_count": 10563,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2581,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1646148486",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7348,
+		"favorite_count": 17687,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Red Velvet ‘The ReVe Festival 2022 - Feel My Rhythm’ Countdown Live\n\n➫ 2022.03.21 5PM (KST)\n\n➫ https://t.co/0qQ8uMs2Wl\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#레드벨벳 #RedVelvet https://t.co/wPwQkbi6kp"
+	},
+	{
 		"created_at": "Fri Mar 18 02:10:10 +0000 2022",
 		"id": 1504635031628644400,
 		"id_str": "1504641277127659523",
