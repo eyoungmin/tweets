fbce120b195327db2576d6c06ca8c63328b7d022
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-18 22:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 18 12:40:16 +0000 2022",
+		"id": 1504799579262566400,
+		"id_str": "1504799847886598147",
+		"full_text": "[📸] 띠..뿌...띠뿌...씰?\n저는 '쁘띠쁘띠설'만 있으면 됩니다🙌\n\n서로들 행복한 주말 보내세요!😊\n\n#Seori #서리 #Cant_Stop_This_Party https://t.co/fbpJQkf8mO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Seori",
+					"indices": [
+						60,
+						66
+					]
+				},
+				{
+					"text": "서리",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "Cant_Stop_This_Party",
+					"indices": [
+						71,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1504799392406306800,
+					"id_str": "1504799392406306820",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/FOIfExqaAAQbJIZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOIfExqaAAQbJIZ.jpg",
+					"url": "https://t.co/fbpJQkf8mO",
+					"display_url": "pic.twitter.com/fbpJQkf8mO",
+					"expanded_url": "https://twitter.com/seori_official/status/1504799579262566400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504799392406306800,
+					"id_str": "1504799392406306820",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/media/FOIfExqaAAQbJIZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOIfExqaAAQbJIZ.jpg",
+					"url": "https://t.co/fbpJQkf8mO",
+					"display_url": "pic.twitter.com/fbpJQkf8mO",
+					"expanded_url": "https://twitter.com/seori_official/status/1504799579262566400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143072158467231700,
+			"id_str": "1143072158467231745",
+			"name": "Seori",
+			"screen_name": "seori_official",
+			"location": "contact@atispaus.com",
+			"description": "#Seori official twitter",
+			"url": "https://t.co/2jDAeGXOz0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2jDAeGXOz0",
+							"expanded_url": "https://www.youtube.com/seori_official",
+							"display_url": "youtube.com/seori_official",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 43758,
+			"friends_count": 0,
+			"listed_count": 186,
+			"created_at": "Mon Jun 24 08:23:34 +0000 2019",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 547,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1402862612077899777/aq-6OvIA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143072158467231745/1629335546",
+			"profile_link_color": "6D67AD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 158,
+		"favorite_count": 598,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[📸] 띠..뿌...띠뿌...씰?\n저는 '쁘띠쁘띠설'만 있으면 됩니다🙌\n\n서로들 행복한 주말 보내세요!😊\n\n#Seori #서리 #Cant_Stop_This_Party https://t.co/fbpJQkf8mO"
+	},
+	{
 		"created_at": "Fri Mar 18 11:40:19 +0000 2022",
 		"id": 1504779670558228500,
 		"id_str": "1504784760077488129",
