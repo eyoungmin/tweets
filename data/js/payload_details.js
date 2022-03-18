7b6db2806a95804a56ce17d47e63f1e4592a0231
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-19 02:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86143,
-	"created_at": "Fri Mar 18 13:02:03 +0000 2022",
+	"tweets": 86144,
+	"created_at": "Fri Mar 18 17:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Fri Mar 18 16:40:15 +0000 2022",
+		"id": 1504850281267409000,
+		"id_str": "1504860241917272070",
+		"full_text": "Interceptor 005, also known as Neon Moon 1, adopted by @coldplay, is catching trash in Klang, Malaysia. 🇲🇾\n\nThis is our first deployment of the third-generation Interceptor updated for efficiency, quicker production, and, therefore, quicker scale up. https://t.co/TK79kfhZ7h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			250
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "coldplay",
+					"name": "Coldplay",
+					"id": 18863815,
+					"id_str": "18863815",
+					"indices": [
+						55,
+						64
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1504849132439097300,
+					"id_str": "1504849132439097344",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1504849132439097344/pu/img/oJtzDb3OQIL9rOkj.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1504849132439097344/pu/img/oJtzDb3OQIL9rOkj.jpg",
+					"url": "https://t.co/TK79kfhZ7h",
+					"display_url": "pic.twitter.com/TK79kfhZ7h",
+					"expanded_url": "https://twitter.com/TheOceanCleanup/status/1504850281267408901/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504849132439097300,
+					"id_str": "1504849132439097344",
+					"indices": [
+						251,
+						274
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1504849132439097344/pu/img/oJtzDb3OQIL9rOkj.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1504849132439097344/pu/img/oJtzDb3OQIL9rOkj.jpg",
+					"url": "https://t.co/TK79kfhZ7h",
+					"display_url": "pic.twitter.com/TK79kfhZ7h",
+					"expanded_url": "https://twitter.com/TheOceanCleanup/status/1504850281267408901/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 71080,
+						"variants": [
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1504849132439097344/pu/vid/320x320/M0rV4JdeQyhm-20J.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1504849132439097344/pu/vid/540x540/chOKXUBgIWuMzbDk.mp4?tag=12"
+							},
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1504849132439097344/pu/vid/720x720/j9VDmXEPCssiFEcW.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1504849132439097344/pu/pl/I0Nh8W1cLc4T18ZX.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1037262350,
+			"id_str": "1037262350",
+			"name": "The Ocean Cleanup",
+			"screen_name": "TheOceanCleanup",
+			"location": "Rotterdam, The Netherlands",
+			"description": "The Largest Cleanup in History. Follow the latest updates on The Ocean Cleanup project. #TheOceanCleanup",
+			"url": "https://t.co/LFFbx8xWQY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LFFbx8xWQY",
+							"expanded_url": "http://www.theoceancleanup.com",
+							"display_url": "theoceancleanup.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 189343,
+			"friends_count": 126,
+			"listed_count": 1363,
+			"created_at": "Wed Dec 26 14:28:49 +0000 2012",
+			"favourites_count": 425,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2740,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1481976444800667649/BOle_jTw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1481976444800667649/BOle_jTw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1037262350/1633958411",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 111,
+		"favorite_count": 697,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interceptor 005, also known as Neon Moon 1, adopted by @coldplay, is catching trash in Klang, Malaysia. 🇲🇾\n\nThis is our first deployment of the third-generation Interceptor updated for efficiency, quicker production, and, therefore, quicker scale up. https://t.co/TK79kfhZ7h"
+	},
+	{
 		"created_at": "Fri Mar 18 12:40:16 +0000 2022",
 		"id": 1504799579262566400,
 		"id_str": "1504799847886598147",
