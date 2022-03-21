5b62157ef4534c4d45e3474f7cb160d3e6c7f0b4
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-21 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86177,
-	"created_at": "Mon Mar 21 05:02:02 +0000 2022",
+	"tweets": 86178,
+	"created_at": "Mon Mar 21 06:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,184 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 21 05:53:44 +0000 2022",
+		"id": 1505775572865400800,
+		"id_str": "1505784705345462274",
+		"full_text": "곧 컴백 예정인, 황금 비주얼✨ 막내라인 보고 가실게요~\n\n#아이돌 #컴백 #레드벨벳 #예리 #하이라이트 #손동운 #오마이걸 #아린 #스트레이키즈 #아이엔\n\nhttps://t.co/B4LjKJYm5K",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이돌",
+					"indices": [
+						33,
+						37
+					]
+				},
+				{
+					"text": "컴백",
+					"indices": [
+						38,
+						41
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"text": "예리",
+					"indices": [
+						48,
+						51
+					]
+				},
+				{
+					"text": "하이라이트",
+					"indices": [
+						52,
+						58
+					]
+				},
+				{
+					"text": "손동운",
+					"indices": [
+						59,
+						63
+					]
+				},
+				{
+					"text": "오마이걸",
+					"indices": [
+						64,
+						69
+					]
+				},
+				{
+					"text": "아린",
+					"indices": [
+						70,
+						73
+					]
+				},
+				{
+					"text": "스트레이키즈",
+					"indices": [
+						74,
+						81
+					]
+				},
+				{
+					"text": "아이엔",
+					"indices": [
+						82,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/B4LjKJYm5K",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/64794",
+					"display_url": "cosmopolitan.co.kr/article/64794",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89478,
+			"friends_count": 3,
+			"listed_count": 401,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 245,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13617,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "곧 컴백 예정인, 황금 비주얼✨ 막내라인 보고 가실게요~\n\n#아이돌 #컴백 #레드벨벳 #예리 #하이라이트 #손동운 #오마이걸 #아린 #스트레이키즈 #아이엔\n\nhttps://t.co/B4LjKJYm5K"
+	},
+	{
 		"created_at": "Mon Mar 21 04:10:52 +0000 2022",
 		"id": 1505751954009469000,
 		"id_str": "1505758816985509893",
