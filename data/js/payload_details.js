455ae1c9f71c1c6aaab4edafbe235bb1a0574644
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-21 20:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86188,
-	"created_at": "Mon Mar 21 10:02:03 +0000 2022",
+	"tweets": 86189,
+	"created_at": "Mon Mar 21 11:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,258 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Mon Mar 21 10:45:40 +0000 2022",
+		"id": 1505855581944385500,
+		"id_str": "1505858174452125696",
+		"full_text": "[#퀸덤2] Queen's 리액션캠👀 - 구름 위에서도 칼각 세우는 #이달의 소녀(#LOONA) 오프닝 쇼\n\n각 팀의 색깔이 돋보이는 오프닝 퍼포먼스 맛보기!\n\n#냅다주는스포 #진짜는내일부터\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 3월 31일 (목) 밤 9시 20분 첫방송\n\n#QUEENDOM2 https://t.co/yHcBbvPcuq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "퀸덤2",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "이달의",
+					"indices": [
+						38,
+						42
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						46,
+						52
+					]
+				},
+				{
+					"text": "냅다주는스포",
+					"indices": [
+						90,
+						97
+					]
+				},
+				{
+					"text": "진짜는내일부터",
+					"indices": [
+						98,
+						106
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						168,
+						178
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1505844647741964300,
+					"id_str": "1505844647741964290",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/FOXfoRiVQAICpQ5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOXfoRiVQAICpQ5.jpg",
+					"url": "https://t.co/yHcBbvPcuq",
+					"display_url": "pic.twitter.com/yHcBbvPcuq",
+					"expanded_url": "https://twitter.com/MnetKR/status/1505855581944385537/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1505844647741964300,
+					"id_str": "1505844647741964290",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/FOXfoRiVQAICpQ5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOXfoRiVQAICpQ5.jpg",
+					"url": "https://t.co/yHcBbvPcuq",
+					"display_url": "pic.twitter.com/yHcBbvPcuq",
+					"expanded_url": "https://twitter.com/MnetKR/status/1505855581944385537/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 54854,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1505844647741964290/pl/Iy-KGQ1z6mFRMFNp.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1505844647741964290/vid/1280x720/5qgsjrrPbRPAUeHA.mp4?tag=14"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1505844647741964290/vid/640x360/Lfgx6ZEKKoNYod6u.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1505844647741964290/vid/480x270/c4RFAtPYsZRVOZEe.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128466223,
+			"id_str": "128466223",
+			"name": "Mnet",
+			"screen_name": "MnetKR",
+			"location": "Seoul, Korea",
+			"description": "Official Twitter of Mnet, Asia`s #1 Music Channel.",
+			"url": "http://t.co/596v5W1yWN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/596v5W1yWN",
+							"expanded_url": "http://www.mnet.com",
+							"display_url": "mnet.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3054572,
+			"friends_count": 20993,
+			"listed_count": 2985,
+			"created_at": "Thu Apr 01 05:53:04 +0000 2010",
+			"favourites_count": 457,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 28738,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128466223/1592183300",
+			"profile_link_color": "E40076",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F5DCE7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1478,
+		"favorite_count": 2369,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#퀸덤2] Queen's 리액션캠👀 - 구름 위에서도 칼각 세우는 #이달의 소녀(#LOONA) 오프닝 쇼\n\n각 팀의 색깔이 돋보이는 오프닝 퍼포먼스 맛보기!\n\n#냅다주는스포 #진짜는내일부터\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 3월 31일 (목) 밤 9시 20분 첫방송\n\n#QUEENDOM2 https://t.co/yHcBbvPcuq"
+	},
+	{
 		"created_at": "Mon Mar 21 09:42:59 +0000 2022",
 		"id": 1505836792289050600,
 		"id_str": "1505842397044953094",
