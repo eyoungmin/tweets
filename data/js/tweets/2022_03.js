3880a8d0388051c3e412a1762a0ea6ae3358eb5a
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-22 23:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Tue Mar 22 13:09:35 +0000 2022",
+		"id": 1506254708511367200,
+		"id_str": "1506256779696644102",
+		"full_text": "Red Velvet 레드벨벳 'The ReVe Festival 2022 - Feel My Rhythm' Jacket Behind I RV Collection\n\nhttps://t.co/u6oMTswKoA\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#Jacket_Behind #Behind\n#RVCollection #레벨컬렉션\n#레드벨벳 #RedVelvet https://t.co/FcHrRYl9AT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			221
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						114,
+						137
+					]
+				},
+				{
+					"text": "Feel_My_Rhythm",
+					"indices": [
+						138,
+						153
+					]
+				},
+				{
+					"text": "필마이리듬",
+					"indices": [
+						154,
+						160
+					]
+				},
+				{
+					"text": "Jacket_Behind",
+					"indices": [
+						161,
+						175
+					]
+				},
+				{
+					"text": "Behind",
+					"indices": [
+						176,
+						183
+					]
+				},
+				{
+					"text": "RVCollection",
+					"indices": [
+						184,
+						197
+					]
+				},
+				{
+					"text": "레벨컬렉션",
+					"indices": [
+						198,
+						204
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						205,
+						210
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						211,
+						221
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/u6oMTswKoA",
+					"expanded_url": "https://youtu.be/w-oM6YtjUIc",
+					"display_url": "youtu.be/w-oM6YtjUIc",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1506254702882672600,
+					"id_str": "1506254702882672642",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/FOdKq_qVUAIt5f1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOdKq_qVUAIt5f1.jpg",
+					"url": "https://t.co/FcHrRYl9AT",
+					"display_url": "pic.twitter.com/FcHrRYl9AT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1506254708511367182/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506254702882672600,
+					"id_str": "1506254702882672642",
+					"indices": [
+						222,
+						245
+					],
+					"media_url": "http://pbs.twimg.com/media/FOdKq_qVUAIt5f1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOdKq_qVUAIt5f1.jpg",
+					"url": "https://t.co/FcHrRYl9AT",
+					"display_url": "pic.twitter.com/FcHrRYl9AT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1506254708511367182/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet  'The ReVe Festival 2022 - Feel My Rhythm'\n\n2022.3.21. 6PM KST",
+			"url": "https://t.co/0hh6Lb9mYR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0hh6Lb9mYR",
+							"expanded_url": "https://www.youtube.com/redvelvet",
+							"display_url": "youtube.com/redvelvet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5191724,
+			"friends_count": 2,
+			"listed_count": 10633,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2599,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1646148486",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9677,
+		"favorite_count": 20015,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Red Velvet 레드벨벳 'The ReVe Festival 2022 - Feel My Rhythm' Jacket Behind I RV Collection\n\nhttps://t.co/u6oMTswKoA\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#Jacket_Behind #Behind\n#RVCollection #레벨컬렉션\n#레드벨벳 #RedVelvet https://t.co/FcHrRYl9AT"
+	},
+	{
 		"created_at": "Tue Mar 22 11:40:21 +0000 2022",
 		"id": 1506233455306109000,
 		"id_str": "1506234323887968263",
