9282cc3d0673fddfa1b12df1168edf254e74d8a9
Youngmin Lee <iameyoungmin@gmail.com>
2022-03-26 20:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,174 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Sat Mar 26 10:09:35 +0000 2022",
+		"id": 1507654211869474800,
+		"id_str": "1507661033355288589",
+		"full_text": "[예능연구소 4K] 레드벨벳 직캠 ‘Feel My Rhythm’ (Red Velvet FanCam) @Show!MusicCore 220326\n\nhttps://t.co/oC207SHcIn\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#음악중심 #MusicCore\n#레드벨벳 #RedVelvet",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						105,
+						128
+					]
+				},
+				{
+					"text": "Feel_My_Rhythm",
+					"indices": [
+						129,
+						144
+					]
+				},
+				{
+					"text": "필마이리듬",
+					"indices": [
+						145,
+						151
+					]
+				},
+				{
+					"text": "음악중심",
+					"indices": [
+						152,
+						157
+					]
+				},
+				{
+					"text": "MusicCore",
+					"indices": [
+						158,
+						168
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						169,
+						174
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						175,
+						185
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "show",
+					"name": "Creature",
+					"id": 82946051,
+					"id_str": "82946051",
+					"indices": [
+						56,
+						61
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oC207SHcIn",
+					"expanded_url": "https://youtu.be/iuVWkWiAGlY",
+					"display_url": "youtu.be/iuVWkWiAGlY",
+					"indices": [
+						80,
+						103
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet  'The ReVe Festival 2022 - Feel My Rhythm'\n\n2022.3.21. 6PM KST",
+			"url": "https://t.co/0hh6Lb9mYR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/0hh6Lb9mYR",
+							"expanded_url": "https://www.youtube.com/redvelvet",
+							"display_url": "youtube.com/redvelvet",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5209914,
+			"friends_count": 2,
+			"listed_count": 10651,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2618,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1498674568898170888/AVBRR9k-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1646148486",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3479,
+		"favorite_count": 7994,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[예능연구소 4K] 레드벨벳 직캠 ‘Feel My Rhythm’ (Red Velvet FanCam) @Show!MusicCore 220326\n\nhttps://t.co/oC207SHcIn\n\n#The_ReVe_Festival_2022\n#Feel_My_Rhythm #필마이리듬\n#음악중심 #MusicCore\n#레드벨벳 #RedVelvet"
+	},
+	{
 		"created_at": "Sat Mar 26 08:40:10 +0000 2022",
 		"id": 1507626309962576000,
 		"id_str": "1507638527382671365",
