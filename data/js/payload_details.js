0cbc75b6f1ce753e2819002c21b5e48c04bd8e58
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-07 14:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86446,
-	"created_at": "Thu Apr 07 03:02:00 +0000 2022",
+	"tweets": 86447,
+	"created_at": "Thu Apr 07 05:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,247 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Thu Apr 07 04:45:36 +0000 2022",
+		"id": 1511916679084740600,
+		"id_str": "1511928154604150787",
+		"full_text": "[Pops in Seoul]\n#LOONA's Interview for 'So What'🤍\n\n✔Go check the full episode right now!\n👉👉 https://t.co/2bKacNzDmM\n\n#SoWhat #이달의소녀 https://t.co/bdJzxVTNIG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			131
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						16,
+						22
+					]
+				},
+				{
+					"text": "SoWhat",
+					"indices": [
+						117,
+						124
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						125,
+						131
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2bKacNzDmM",
+					"expanded_url": "https://youtu.be/kogwWxOtfOo",
+					"display_url": "youtu.be/kogwWxOtfOo",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1511884797442130000,
+					"id_str": "1511884797442129920",
+					"indices": [
+						132,
+						155
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1511884797442129920/img/hEQdwUgjwA-mtQ1L.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1511884797442129920/img/hEQdwUgjwA-mtQ1L.jpg",
+					"url": "https://t.co/bdJzxVTNIG",
+					"display_url": "pic.twitter.com/bdJzxVTNIG",
+					"expanded_url": "https://twitter.com/arirangworld/status/1511916679084740608/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1511884797442130000,
+					"id_str": "1511884797442129920",
+					"indices": [
+						132,
+						155
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1511884797442129920/img/hEQdwUgjwA-mtQ1L.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1511884797442129920/img/hEQdwUgjwA-mtQ1L.jpg",
+					"url": "https://t.co/bdJzxVTNIG",
+					"display_url": "pic.twitter.com/bdJzxVTNIG",
+					"expanded_url": "https://twitter.com/arirangworld/status/1511916679084740608/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 24357,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1511884797442129920/vid/320x568/obc5I-t1frF7mtzD.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1511884797442129920/pl/lj8P-wu6oEvqxZ5V.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1511884797442129920/vid/480x852/-E2VRLJvC4HTVHQr.mp4?tag=14"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1511884797442129920/vid/720x1280/jKBzDwtyvIEy3Wcc.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": true
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 66625290,
+			"id_str": "66625290",
+			"name": "Arirang World",
+			"screen_name": "arirangworld",
+			"location": "South Korea",
+			"description": "Arirang TV & Radio",
+			"url": "https://t.co/eRqGeUFAHR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eRqGeUFAHR",
+							"expanded_url": "https://www.youtube.com/arirangworld",
+							"display_url": "youtube.com/arirangworld",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 157105,
+			"friends_count": 134,
+			"listed_count": 1233,
+			"created_at": "Tue Aug 18 07:40:53 +0000 2009",
+			"favourites_count": 6339,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 26746,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E6E6E6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1410884603825455105/PsIcEN-m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1410884603825455105/PsIcEN-m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/66625290/1625216120",
+			"profile_link_color": "ED1846",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFB2D7",
+			"profile_text_color": "0A0A0A",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[Pops in Seoul]\n#LOONA's Interview for 'So What'🤍\n\n✔Go check the full episode right now!\n👉👉 https://t.co/2bKacNzDmM\n\n#SoWhat #이달의소녀 https://t.co/bdJzxVTNIG"
+	},
+	{
 		"created_at": "Thu Apr 07 02:40:15 +0000 2022",
 		"id": 1511894153004683300,
 		"id_str": "1511896608165748736",
