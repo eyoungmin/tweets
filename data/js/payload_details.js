63448f3c213f3914fb779ce76dc3a0abcbd158a2
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-08 04:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86471,
-	"created_at": "Thu Apr 07 17:02:02 +0000 2022",
+	"tweets": 86472,
+	"created_at": "Thu Apr 07 19:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Thu Apr 07 18:40:09 +0000 2022",
+		"id": 1512128622299123700,
+		"id_str": "1512138175501815822",
+		"full_text": "Music Of The Spheres World Tour shows just announced for Peru, Chile and Brazil! NB: the Lima date originally scheduled for September 20th has now been rescheduled for September 13th. Tickets for the new shows on sale next week. Full info at https://t.co/zvmn4wjE3J https://t.co/LKeM5UyHvc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zvmn4wjE3J",
+					"expanded_url": "http://coldplay.com/tour",
+					"display_url": "coldplay.com/tour",
+					"indices": [
+						242,
+						265
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1512128604393689000,
+					"id_str": "1512128604393689088",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FPwo9D-X0AAtyFW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPwo9D-X0AAtyFW.jpg",
+					"url": "https://t.co/LKeM5UyHvc",
+					"display_url": "pic.twitter.com/LKeM5UyHvc",
+					"expanded_url": "https://twitter.com/coldplay/status/1512128622299123723/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1512128604393689000,
+					"id_str": "1512128604393689088",
+					"indices": [
+						266,
+						289
+					],
+					"media_url": "http://pbs.twimg.com/media/FPwo9D-X0AAtyFW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FPwo9D-X0AAtyFW.jpg",
+					"url": "https://t.co/LKeM5UyHvc",
+					"display_url": "pic.twitter.com/LKeM5UyHvc",
+					"expanded_url": "https://twitter.com/coldplay/status/1512128622299123723/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Calypso",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23588142,
+			"friends_count": 778,
+			"listed_count": 42597,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1568,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9102,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 682,
+		"favorite_count": 4858,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Music Of The Spheres World Tour shows just announced for Peru, Chile and Brazil! NB: the Lima date originally scheduled for September 20th has now been rescheduled for September 13th. Tickets for the new shows on sale next week. Full info at https://t.co/zvmn4wjE3J https://t.co/LKeM5UyHvc"
+	},
+	{
 		"created_at": "Thu Apr 07 16:40:10 +0000 2022",
 		"id": 1512098221509779500,
 		"id_str": "1512107978631098380",
