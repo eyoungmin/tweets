c9a705c7e6c6c000b84a9b36314365cc9ddb88dd
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-09 03:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86493,
-	"created_at": "Fri Apr 08 16:02:02 +0000 2022",
+	"tweets": 86494,
+	"created_at": "Fri Apr 08 18:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 08 17:40:09 +0000 2022",
+		"id": 1512474032947572700,
+		"id_str": "1512485463591903239",
+		"full_text": "It was pure JOYYYY playing the first night of our tour last night - Southampton you were so beautiful! Feels unreal to be back on stage playing music with these legendary boys after 2 years of sitting at home dreaming of touring again. Cardiff, who's coming down tonight? ✨ https://t.co/ZMylrLeFUJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1512473849576755200,
+					"id_str": "1512473849576755205",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FP1i8-gXEAUDW2p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP1i8-gXEAUDW2p.jpg",
+					"url": "https://t.co/ZMylrLeFUJ",
+					"display_url": "pic.twitter.com/ZMylrLeFUJ",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1512474032947572744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1512473849576755200,
+					"id_str": "1512473849576755205",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FP1i8-gXEAUDW2p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP1i8-gXEAUDW2p.jpg",
+					"url": "https://t.co/ZMylrLeFUJ",
+					"display_url": "pic.twitter.com/ZMylrLeFUJ",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1512474032947572744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512473849572511700,
+					"id_str": "1512473849572511745",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FP1i8-fWUAEDy45.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP1i8-fWUAEDy45.jpg",
+					"url": "https://t.co/ZMylrLeFUJ",
+					"display_url": "pic.twitter.com/ZMylrLeFUJ",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1512474032947572744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 960,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512473849568407600,
+					"id_str": "1512473849568407562",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FP1i8-eXsAo_ZFM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP1i8-eXsAo_ZFM.jpg",
+					"url": "https://t.co/ZMylrLeFUJ",
+					"display_url": "pic.twitter.com/ZMylrLeFUJ",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1512474032947572744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1512473849572507600,
+					"id_str": "1512473849572507662",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FP1i8-fWQA4ZVT0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FP1i8-fWQA4ZVT0.jpg",
+					"url": "https://t.co/ZMylrLeFUJ",
+					"display_url": "pic.twitter.com/ZMylrLeFUJ",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1512474032947572744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 960,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2740435740,
+			"id_str": "2740435740",
+			"name": "Oh Wonder",
+			"screen_name": "OhWonderMusic",
+			"location": "London",
+			"description": "Our new single 'Fuck It I Love You' is out now",
+			"url": "https://t.co/Lxj940Lmim",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Lxj940Lmim",
+							"expanded_url": "https://OhWonder.lnk.to/22MakeAlbum",
+							"display_url": "OhWonder.lnk.to/22MakeAlbum",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 166890,
+			"friends_count": 203,
+			"listed_count": 430,
+			"created_at": "Sun Aug 17 20:45:13 +0000 2014",
+			"favourites_count": 3539,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 5354,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2740435740/1649423793",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 102,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It was pure JOYYYY playing the first night of our tour last night - Southampton you were so beautiful! Feels unreal to be back on stage playing music with these legendary boys after 2 years of sitting at home dreaming of touring again. Cardiff, who's coming down tonight? ✨ https://t.co/ZMylrLeFUJ"
+	},
+	{
 		"created_at": "Fri Apr 08 15:40:38 +0000 2022",
 		"id": 1512451947139326000,
 		"id_str": "1512455385654964231",
