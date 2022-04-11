47ba1e740f4975ad608cea1779e054c31c573b85
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-11 12:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Mon Apr 11 02:40:19 +0000 2022",
+		"id": 1513333181403234300,
+		"id_str": "1513346174127910924",
+		"full_text": "BoA 보아 [DO THE MOTION (The Greatest Ver.)]\n\n🎧Melon: https://t.co/v1XZ4jwB5l \n🎧FLO: https://t.co/MqoB6Ea2WL \n🎧genie: https://t.co/hYMsGUJnLm \n🎧VIBE: https://t.co/rsgNg164GO \n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#DOTHEMOTION",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			226
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						174,
+						178
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						179,
+						182
+					]
+				},
+				{
+					"text": "TheGreatest",
+					"indices": [
+						184,
+						196
+					]
+				},
+				{
+					"text": "BoA_TheGreatest",
+					"indices": [
+						197,
+						213
+					]
+				},
+				{
+					"text": "DOTHEMOTION",
+					"indices": [
+						214,
+						226
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v1XZ4jwB5l",
+					"expanded_url": "https://bit.ly/3vaF3a8",
+					"display_url": "bit.ly/3vaF3a8",
+					"indices": [
+						52,
+						75
+					]
+				},
+				{
+					"url": "https://t.co/MqoB6Ea2WL",
+					"expanded_url": "https://bit.ly/3vaFbXa",
+					"display_url": "bit.ly/3vaFbXa",
+					"indices": [
+						83,
+						106
+					]
+				},
+				{
+					"url": "https://t.co/hYMsGUJnLm",
+					"expanded_url": "https://bit.ly/3uqpa0g",
+					"display_url": "bit.ly/3uqpa0g",
+					"indices": [
+						116,
+						139
+					]
+				},
+				{
+					"url": "https://t.co/rsgNg164GO",
+					"expanded_url": "https://bit.ly/37zPiMZ",
+					"display_url": "bit.ly/37zPiMZ",
+					"indices": [
+						148,
+						171
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 68786,
+			"friends_count": 1,
+			"listed_count": 255,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 285,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 129,
+		"favorite_count": 267,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BoA 보아 [DO THE MOTION (The Greatest Ver.)]\n\n🎧Melon: https://t.co/v1XZ4jwB5l \n🎧FLO: https://t.co/MqoB6Ea2WL \n🎧genie: https://t.co/hYMsGUJnLm \n🎧VIBE: https://t.co/rsgNg164GO \n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#DOTHEMOTION"
+	},
+	{
 		"created_at": "Sun Apr 10 15:40:16 +0000 2022",
 		"id": 1513169998864617500,
 		"id_str": "1513180068293525506",
