94e948e88817caeed23290eda348d11a814d75a8
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-13 17:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86528,
-	"created_at": "Wed Apr 13 04:02:03 +0000 2022",
+	"tweets": 86529,
+	"created_at": "Wed Apr 13 08:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,251 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 13 07:40:09 +0000 2022",
+		"id": 1514136548328976400,
+		"id_str": "1514146407757791234",
+		"full_text": "[예고/3회] '효린의 산을 넘어라' 판을 뒤집으려는 퀸들의 2차 경연 무대는? | 매주 목요일 밤 9시 20분 \n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 매주 목요일 밤 9시 20분\n\n#퀸덤2 #QUEENDOM2 #RUNTHEWORLD #엠넷 #Mnet https://t.co/mY0fmn3bdI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "퀸덤2",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						122,
+						132
+					]
+				},
+				{
+					"text": "RUNTHEWORLD",
+					"indices": [
+						133,
+						145
+					]
+				},
+				{
+					"text": "엠넷",
+					"indices": [
+						146,
+						149
+					]
+				},
+				{
+					"text": "Mnet",
+					"indices": [
+						150,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1514131464186577000,
+					"id_str": "1514131464186576898",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FQNHFZjVIAEpbvk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQNHFZjVIAEpbvk.jpg",
+					"url": "https://t.co/mY0fmn3bdI",
+					"display_url": "pic.twitter.com/mY0fmn3bdI",
+					"expanded_url": "https://twitter.com/MnetKR/status/1514136548328976385/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1514131464186577000,
+					"id_str": "1514131464186576898",
+					"indices": [
+						156,
+						179
+					],
+					"media_url": "http://pbs.twimg.com/media/FQNHFZjVIAEpbvk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQNHFZjVIAEpbvk.jpg",
+					"url": "https://t.co/mY0fmn3bdI",
+					"display_url": "pic.twitter.com/mY0fmn3bdI",
+					"expanded_url": "https://twitter.com/MnetKR/status/1514136548328976385/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 60060,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1514131464186576898/vid/1280x720/niY7sTsrJKWmNzCf.mp4?tag=14"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1514131464186576898/vid/480x270/dA-EUWafYJkuDj-m.mp4?tag=14"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1514131464186576898/pl/4aloFv-KE31AkJHa.m3u8?tag=14&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1514131464186576898/vid/640x360/twdXPS3xu27nv_VA.mp4?tag=14"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128466223,
+			"id_str": "128466223",
+			"name": "Mnet",
+			"screen_name": "MnetKR",
+			"location": "Seoul, Korea",
+			"description": "Official Twitter of Mnet, Asia`s #1 Music Channel.",
+			"url": "http://t.co/596v5W1yWN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/596v5W1yWN",
+							"expanded_url": "http://www.mnet.com",
+							"display_url": "mnet.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3062313,
+			"friends_count": 20973,
+			"listed_count": 2996,
+			"created_at": "Thu Apr 01 05:53:04 +0000 2010",
+			"favourites_count": 458,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 29149,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128466223/1592183300",
+			"profile_link_color": "E40076",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F5DCE7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3786,
+		"favorite_count": 8967,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[예고/3회] '효린의 산을 넘어라' 판을 뒤집으려는 퀸들의 2차 경연 무대는? | 매주 목요일 밤 9시 20분 \n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 매주 목요일 밤 9시 20분\n\n#퀸덤2 #QUEENDOM2 #RUNTHEWORLD #엠넷 #Mnet https://t.co/mY0fmn3bdI"
+	},
+	{
 		"created_at": "Wed Apr 13 03:44:06 +0000 2022",
 		"id": 1513719020738199600,
 		"id_str": "1514087001146167300",
