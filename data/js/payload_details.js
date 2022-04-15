0a4eea3fdb9f5ef0252d77aed5937c781dbb0f02
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-15 15:02:04 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86563,
-	"created_at": "Fri Apr 15 05:02:01 +0000 2022",
+	"tweets": 86565,
+	"created_at": "Fri Apr 15 06:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,406 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 15 05:45:37 +0000 2022",
+		"id": 1514841097092350000,
+		"id_str": "1514842358243893250",
+		"full_text": "[LOOΠΔ NEWS] \n\n이달의 소녀, '퀸덤2'서 씨스타 'Shake It' 완벽 재해석..'퍼포먼스 장인' 입증\n\n▶️ https://t.co/xbSrnerLFt\n\n#이달의소녀 #LOONA \n#퀸덤2 #QUEENDOM2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						95,
+						101
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						102,
+						108
+					]
+				},
+				{
+					"text": "퀸덤2",
+					"indices": [
+						110,
+						114
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						115,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xbSrnerLFt",
+					"expanded_url": "http://naver.me/xA3sjFPG",
+					"display_url": "naver.me/xA3sjFPG",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 880841,
+			"friends_count": 2,
+			"listed_count": 8485,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4048,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 779,
+		"favorite_count": 2399,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[LOOΠΔ NEWS] \n\n이달의 소녀, '퀸덤2'서 씨스타 'Shake It' 완벽 재해석..'퍼포먼스 장인' 입증\n\n▶️ https://t.co/xbSrnerLFt\n\n#이달의소녀 #LOONA \n#퀸덤2 #QUEENDOM2"
+	},
+	{
+		"created_at": "Fri Apr 15 05:40:13 +0000 2022",
+		"id": 1514835371947102200,
+		"id_str": "1514840999058624513",
+		"full_text": "[#퀸덤2] 🔥3차 경연 2라운드 글로벌 온라인 평가단 모집 안내🔥\n\n*3차 경연 1라운드는 글로벌 온라인 평가를 진행하지 않습니다.\n\n🔗https://t.co/k1XBRgK9uk\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n&lt;퀸덤2&gt; 매주 목요일 밤 9시 20분\n\n#퀸덤2 #QUEENDOM2 #RUNTHEWORLD #Mnet #엠넷 https://t.co/GYi6ToyMY5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			198
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "퀸덤2",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "퀸덤2",
+					"indices": [
+						160,
+						164
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						165,
+						175
+					]
+				},
+				{
+					"text": "RUNTHEWORLD",
+					"indices": [
+						176,
+						188
+					]
+				},
+				{
+					"text": "Mnet",
+					"indices": [
+						189,
+						194
+					]
+				},
+				{
+					"text": "엠넷",
+					"indices": [
+						195,
+						198
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/k1XBRgK9uk",
+					"expanded_url": "https://forms.gle/SLhXf9pPwM1FqXhJA",
+					"display_url": "forms.gle/SLhXf9pPwM1FqX…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1514832903288803300,
+					"id_str": "1514832903288803329",
+					"indices": [
+						199,
+						222
+					],
+					"media_url": "http://pbs.twimg.com/media/FQXEf-aaUAEPOxo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQXEf-aaUAEPOxo.jpg",
+					"url": "https://t.co/GYi6ToyMY5",
+					"display_url": "pic.twitter.com/GYi6ToyMY5",
+					"expanded_url": "https://twitter.com/MnetKR/status/1514835371947102211/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1514832903288803300,
+					"id_str": "1514832903288803329",
+					"indices": [
+						199,
+						222
+					],
+					"media_url": "http://pbs.twimg.com/media/FQXEf-aaUAEPOxo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQXEf-aaUAEPOxo.jpg",
+					"url": "https://t.co/GYi6ToyMY5",
+					"display_url": "pic.twitter.com/GYi6ToyMY5",
+					"expanded_url": "https://twitter.com/MnetKR/status/1514835371947102211/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1514832930274967600,
+					"id_str": "1514832930274967553",
+					"indices": [
+						199,
+						222
+					],
+					"media_url": "http://pbs.twimg.com/media/FQXEhi8agAE0UH3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQXEhi8agAE0UH3.jpg",
+					"url": "https://t.co/GYi6ToyMY5",
+					"display_url": "pic.twitter.com/GYi6ToyMY5",
+					"expanded_url": "https://twitter.com/MnetKR/status/1514835371947102211/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128466223,
+			"id_str": "128466223",
+			"name": "Mnet",
+			"screen_name": "MnetKR",
+			"location": "Seoul, Korea",
+			"description": "Official Twitter of Mnet, Asia`s #1 Music Channel.",
+			"url": "http://t.co/596v5W1yWN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/596v5W1yWN",
+							"expanded_url": "http://www.mnet.com",
+							"display_url": "mnet.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3063295,
+			"friends_count": 20977,
+			"listed_count": 3001,
+			"created_at": "Thu Apr 01 05:53:04 +0000 2010",
+			"favourites_count": 458,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 29198,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128466223/1592183300",
+			"profile_link_color": "E40076",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F5DCE7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 989,
+		"favorite_count": 3226,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#퀸덤2] 🔥3차 경연 2라운드 글로벌 온라인 평가단 모집 안내🔥\n\n*3차 경연 1라운드는 글로벌 온라인 평가를 진행하지 않습니다.\n\n🔗https://t.co/k1XBRgK9uk\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n&lt;퀸덤2&gt; 매주 목요일 밤 9시 20분\n\n#퀸덤2 #QUEENDOM2 #RUNTHEWORLD #Mnet #엠넷 https://t.co/GYi6ToyMY5"
+	},
+	{
 		"created_at": "Fri Apr 15 04:40:09 +0000 2022",
 		"id": 1514822325505441800,
 		"id_str": "1514825885844656135",
