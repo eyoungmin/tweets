69b3faede502733a4d48733b196d92620a24e7c4
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-19 14:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86602,
-	"created_at": "Tue Apr 19 03:02:04 +0000 2022",
+	"tweets": 86603,
+	"created_at": "Tue Apr 19 05:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,146 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 19 04:40:09 +0000 2022",
+		"id": 1516073579762364400,
+		"id_str": "1516275437130899456",
+		"full_text": "#RedVelvet is ready for spring 🌸  \n\nIn an exclusive interview with Elite Daily, @RVsmtown talks about their latest EP, what's next this year — and the possibility of a tour 😏  #The_ReVe_Festival_2022 #Feel_My_Rhythm\nhttps://t.co/7hy8fIt6eu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			239
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						176,
+						199
+					]
+				},
+				{
+					"text": "Feel_My_Rhythm",
+					"indices": [
+						200,
+						215
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RVsmtown",
+					"name": "Red Velvet",
+					"id": 873092428755894300,
+					"id_str": "873092428755894272",
+					"indices": [
+						80,
+						89
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/7hy8fIt6eu",
+					"expanded_url": "https://www.elitedaily.com/entertainment/red-velvet-reve-festival-2022-feel-my-rhythm-touring-interview?utm_campaign=elitedaily&utm_content=1650295083&utm_medium=owned&utm_source=twitter",
+					"display_url": "elitedaily.com/entertainment/…",
+					"indices": [
+						216,
+						239
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://sproutsocial.com\" rel=\"nofollow\">Sprout Social</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 331057915,
+			"id_str": "331057915",
+			"name": "Elite Daily",
+			"screen_name": "EliteDaily",
+			"location": "",
+			"description": "instagram: @elitedaily",
+			"url": "https://t.co/Dl6BPc4Fu3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Dl6BPc4Fu3",
+							"expanded_url": "http://www.elitedaily.com",
+							"display_url": "elitedaily.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 180695,
+			"friends_count": 624,
+			"listed_count": 1794,
+			"created_at": "Thu Jul 07 15:49:01 +0000 2011",
+			"favourites_count": 48122,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 142555,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1394647241416232960/wco2qSYx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1394647241416232960/wco2qSYx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/331057915/1648213373",
+			"profile_link_color": "424242",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1629,
+		"favorite_count": 4218,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#RedVelvet is ready for spring 🌸  \n\nIn an exclusive interview with Elite Daily, @RVsmtown talks about their latest EP, what's next this year — and the possibility of a tour 😏  #The_ReVe_Festival_2022 #Feel_My_Rhythm\nhttps://t.co/7hy8fIt6eu"
+	},
+	{
 		"created_at": "Tue Apr 19 03:01:41 +0000 2022",
 		"id": 1516250231607804000,
 		"id_str": "1516250654875938817",
