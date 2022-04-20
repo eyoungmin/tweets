3a634ffae5e24886b5930371a3b8bd145442cb5a
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-21 03:02:03 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86625,
-	"created_at": "Wed Apr 20 17:02:11 +0000 2022",
+	"tweets": 86627,
+	"created_at": "Wed Apr 20 18:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,542 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Wed Apr 20 17:40:28 +0000 2022",
+		"id": 1516824937394188300,
+		"id_str": "1516834194474872838",
+		"full_text": ".@aespa_official will make their Coachella debut this Saturday 4/23, joining another momentous lineup for @88rising’s ℌ𝔢𝔞𝔡 ℑ𝔫 𝔗𝔥𝔢 ℭ𝔩𝔬𝔲𝔡𝔰 𝔉𝔬𝔯𝔢𝔳𝔢𝔯.\n\nFor their livestream performance and schedule updates, head to https://t.co/369m1pEHng https://t.co/IBjYvBnuvf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			233
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "aespa_official",
+					"name": "aespa",
+					"id": 1277453652924366800,
+					"id_str": "1277453652924366848",
+					"indices": [
+						1,
+						16
+					]
+				},
+				{
+					"screen_name": "88rising",
+					"name": "88rising",
+					"id": 710838951989346300,
+					"id_str": "710838951989346304",
+					"indices": [
+						106,
+						115
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/369m1pEHng",
+					"expanded_url": "http://youtube.com/coachella",
+					"display_url": "youtube.com/coachella",
+					"indices": [
+						210,
+						233
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1516824933573161000,
+					"id_str": "1516824933573160961",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FQzYPZgVcAESqof.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQzYPZgVcAESqof.jpg",
+					"url": "https://t.co/IBjYvBnuvf",
+					"display_url": "pic.twitter.com/IBjYvBnuvf",
+					"expanded_url": "https://twitter.com/coachella/status/1516824937394188293/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1516824933573161000,
+					"id_str": "1516824933573160961",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/FQzYPZgVcAESqof.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQzYPZgVcAESqof.jpg",
+					"url": "https://t.co/IBjYvBnuvf",
+					"display_url": "pic.twitter.com/IBjYvBnuvf",
+					"expanded_url": "https://twitter.com/coachella/status/1516824937394188293/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 688583,
+			"id_str": "688583",
+			"name": "Coachella",
+			"screen_name": "coachella",
+			"location": "Indio, CA",
+			"description": "Coachella Valley Music and Arts Festival 2022\nApril 15-17 & April 22-24",
+			"url": "https://t.co/lHIhCk54bd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lHIhCk54bd",
+							"expanded_url": "http://youtube.com/coachella",
+							"display_url": "youtube.com/coachella",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1022316,
+			"friends_count": 162,
+			"listed_count": 7052,
+			"created_at": "Tue Jan 23 20:11:53 +0000 2007",
+			"favourites_count": 1004,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4632,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "DBE9ED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1516476998482153474/u8alVpBJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1516476998482153474/u8alVpBJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/688583/1650391268",
+			"profile_link_color": "3D75C4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DFE2EB",
+			"profile_text_color": "435969",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8533,
+		"favorite_count": 14282,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".@aespa_official will make their Coachella debut this Saturday 4/23, joining another momentous lineup for @88rising’s ℌ𝔢𝔞𝔡 ℑ𝔫 𝔗𝔥𝔢 ℭ𝔩𝔬𝔲𝔡𝔰 𝔉𝔬𝔯𝔢𝔳𝔢𝔯.\n\nFor their livestream performance and schedule updates, head to https://t.co/369m1pEHng https://t.co/IBjYvBnuvf"
+	},
+	{
+		"created_at": "Wed Apr 20 17:40:18 +0000 2022",
+		"id": 1516824259741782000,
+		"id_str": "1516834156075958277",
+		"full_text": "Use #aespaCoachella to ask questions to aespa https://t.co/Rnz3SpBZun https://t.co/ZbIjRsc0mS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespaCoachella",
+					"indices": [
+						4,
+						19
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Rnz3SpBZun",
+					"expanded_url": "https://twitter.com/aespa_official/status/1516824039750533121",
+					"display_url": "twitter.com/aespa_official…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1516824250262646800,
+					"id_str": "1516824250262646786",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/FQzXnn-acAIxifV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQzXnn-acAIxifV.jpg",
+					"url": "https://t.co/ZbIjRsc0mS",
+					"display_url": "pic.twitter.com/ZbIjRsc0mS",
+					"expanded_url": "https://twitter.com/aespa_official/status/1516824259741782016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1516824250262646800,
+					"id_str": "1516824250262646786",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/FQzXnn-acAIxifV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FQzXnn-acAIxifV.jpg",
+					"url": "https://t.co/ZbIjRsc0mS",
+					"display_url": "pic.twitter.com/ZbIjRsc0mS",
+					"expanded_url": "https://twitter.com/aespa_official/status/1516824259741782016/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Dreams Come True",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2464857,
+			"friends_count": 1,
+			"listed_count": 8119,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1366,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1639558806",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1516824039750533000,
+		"quoted_status_id_str": "1516824039750533121",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Rnz3SpBZun",
+			"expanded": "https://twitter.com/aespa_official/status/1516824039750533121",
+			"display": "twitter.com/aespa_official…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Apr 20 17:00:06 +0000 2022",
+			"id": 1516824039750533000,
+			"id_str": "1516824039750533121",
+			"full_text": "aespa on the way to Coachella🏜🌵🎡\n\nhttps://t.co/1XMnr1xkUb\n\nThu Apr 21 2022 | 9:00AM KST\nWed Apr 20 2022 | 5:00PM PDT\n\n#aespa #æspa #에스파\n#aespaCoachella \n#aespa_TwitterSpaces\n#HITCforever #88rising",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				196
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "aespa",
+						"indices": [
+							118,
+							124
+						]
+					},
+					{
+						"text": "æspa",
+						"indices": [
+							125,
+							130
+						]
+					},
+					{
+						"text": "에스파",
+						"indices": [
+							131,
+							135
+						]
+					},
+					{
+						"text": "aespaCoachella",
+						"indices": [
+							136,
+							151
+						]
+					},
+					{
+						"text": "aespa_TwitterSpaces",
+						"indices": [
+							153,
+							173
+						]
+					},
+					{
+						"text": "HITCforever",
+						"indices": [
+							174,
+							186
+						]
+					},
+					{
+						"text": "88rising",
+						"indices": [
+							187,
+							196
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/1XMnr1xkUb",
+						"expanded_url": "https://twitter.com/i/spaces/1YqxopavyZNKv",
+						"display_url": "twitter.com/i/spaces/1Yqxo…",
+						"indices": [
+							34,
+							57
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1277453652924366800,
+				"id_str": "1277453652924366848",
+				"name": "aespa",
+				"screen_name": "aespa_official",
+				"location": "",
+				"description": "Dreams Come True",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2464857,
+				"friends_count": 1,
+				"listed_count": 8119,
+				"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+				"favourites_count": 0,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1366,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1639558806",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7326,
+			"favorite_count": 13608,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 5818,
+		"favorite_count": 13435,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Use #aespaCoachella to ask questions to aespa https://t.co/Rnz3SpBZun https://t.co/ZbIjRsc0mS"
+	},
+	{
 		"created_at": "Wed Apr 20 16:40:10 +0000 2022",
 		"id": 1516809405018566700,
 		"id_str": "1516819020904869888",
