e767455436c2dfd541ab53518b682d604787d379
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-23 10:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86662,
-	"created_at": "Sat Apr 23 00:02:02 +0000 2022",
+	"tweets": 86663,
+	"created_at": "Sat Apr 23 01:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sat Apr 23 00:40:09 +0000 2022",
+		"id": 1517662729217052700,
+		"id_str": "1517664590292733953",
+		"full_text": "Prepping for festival🌵🎡🏜\nStarting TikTok LIVE soon👀✨\n\n👉https://t.co/EPaxM5SjeX \n\nLet's get ready to go to Coachella with aespa💙🌙⭐️🦋\n23 Apr 2022 | 10:00AM(KST)\n22 Apr 2022 | 06:00PM(PDT)\n\n#aespa #æspa #에스파\n#aespaCoachella \n#HITCforever  #88rising \n#ArtistBackstage #IRL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						187,
+						193
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						194,
+						199
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						200,
+						204
+					]
+				},
+				{
+					"text": "aespaCoachella",
+					"indices": [
+						205,
+						220
+					]
+				},
+				{
+					"text": "HITCforever",
+					"indices": [
+						222,
+						234
+					]
+				},
+				{
+					"text": "88rising",
+					"indices": [
+						236,
+						245
+					]
+				},
+				{
+					"text": "ArtistBackstage",
+					"indices": [
+						247,
+						263
+					]
+				},
+				{
+					"text": "IRL",
+					"indices": [
+						264,
+						268
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EPaxM5SjeX",
+					"expanded_url": "http://bit.ly/3L1v7GN",
+					"display_url": "bit.ly/3L1v7GN",
+					"indices": [
+						55,
+						78
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Dreams Come True",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2477745,
+			"friends_count": 1,
+			"listed_count": 8123,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1639558806",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3080,
+		"favorite_count": 7590,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Prepping for festival🌵🎡🏜\nStarting TikTok LIVE soon👀✨\n\n👉https://t.co/EPaxM5SjeX \n\nLet's get ready to go to Coachella with aespa💙🌙⭐️🦋\n23 Apr 2022 | 10:00AM(KST)\n22 Apr 2022 | 06:00PM(PDT)\n\n#aespa #æspa #에스파\n#aespaCoachella \n#HITCforever  #88rising \n#ArtistBackstage #IRL"
+	},
+	{
 		"created_at": "Fri Apr 22 23:40:10 +0000 2022",
 		"id": 1517639450188468200,
 		"id_str": "1517649492270620680",
