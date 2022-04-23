28b127a3dea9be75025d831bda8e76dcfda893b9
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-23 15:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86664,
-	"created_at": "Sat Apr 23 05:02:03 +0000 2022",
+	"tweets": 86665,
+	"created_at": "Sat Apr 23 06:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,136 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sat Apr 23 05:40:09 +0000 2022",
+		"id": 1517738419035963400,
+		"id_str": "1517740087538298880",
+		"full_text": "보아, 日 '셀프 커버 프로젝트' 여섯 번째 곡 'Every Heart -민나노키모치-' 25일 공개!\n\nThe 6th song of BoA's Japanese 'Self Cover Project', 'Every Heart -ミンナノキモチ-’, will be released on the 25th!\n\nhttps://t.co/1U8kIpELow\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#EveryHeart",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						193,
+						197
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						198,
+						201
+					]
+				},
+				{
+					"text": "TheGreatest",
+					"indices": [
+						203,
+						215
+					]
+				},
+				{
+					"text": "BoA_TheGreatest",
+					"indices": [
+						216,
+						232
+					]
+				},
+				{
+					"text": "EveryHeart",
+					"indices": [
+						233,
+						244
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1U8kIpELow",
+					"expanded_url": "https://bit.ly/3vhxvnz",
+					"display_url": "bit.ly/3vhxvnz",
+					"indices": [
+						168,
+						191
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69252,
+			"friends_count": 1,
+			"listed_count": 0,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 288,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 100,
+		"favorite_count": 158,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "보아, 日 '셀프 커버 프로젝트' 여섯 번째 곡 'Every Heart -민나노키모치-' 25일 공개!\n\nThe 6th song of BoA's Japanese 'Self Cover Project', 'Every Heart -ミンナノキモチ-’, will be released on the 25th!\n\nhttps://t.co/1U8kIpELow\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#EveryHeart"
+	},
+	{
 		"created_at": "Sat Apr 23 04:40:10 +0000 2022",
 		"id": 1517716977455796200,
 		"id_str": "1517724989650313217",
