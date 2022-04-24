c38c5734943eed5ab68e4547b6d90330f77ac9b2
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-24 13:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86680,
-	"created_at": "Sun Apr 24 00:02:04 +0000 2022",
+	"tweets": 86681,
+	"created_at": "Sun Apr 24 04:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,315 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sun Apr 24 03:40:11 +0000 2022",
+		"id": 1518070257247678500,
+		"id_str": "1518072285579595777",
+		"full_text": "Thank you Coachella ❤️‍🔥\n\nCheck out the live stream on YouTube Coachella LIVE Channel 1\n🔗https://t.co/rwTr12OC7g\n\n24 Apr 2022 | 2:00PM (KST)\n23 Apr 2022 | 10:00PM (PDT)\n\n📸 @ ai.visuals\n\n#aespa #æspa #에스파 #aespaCoachella https://t.co/2qUMwG1NFQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			219
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						186,
+						192
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						193,
+						198
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						199,
+						203
+					]
+				},
+				{
+					"text": "aespaCoachella",
+					"indices": [
+						204,
+						219
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rwTr12OC7g",
+					"expanded_url": "http://youtu.be/CLy0wA4If2k",
+					"display_url": "youtu.be/CLy0wA4If2k",
+					"indices": [
+						89,
+						112
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1518070245675585500,
+					"id_str": "1518070245675585539",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/FRFE2GmVUAMvB8o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRFE2GmVUAMvB8o.jpg",
+					"url": "https://t.co/2qUMwG1NFQ",
+					"display_url": "pic.twitter.com/2qUMwG1NFQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1518070257247678464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1518070245675585500,
+					"id_str": "1518070245675585539",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/FRFE2GmVUAMvB8o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRFE2GmVUAMvB8o.jpg",
+					"url": "https://t.co/2qUMwG1NFQ",
+					"display_url": "pic.twitter.com/2qUMwG1NFQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1518070257247678464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1518070245688184800,
+					"id_str": "1518070245688184832",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/FRFE2GpVkAA2RhD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRFE2GpVkAA2RhD.jpg",
+					"url": "https://t.co/2qUMwG1NFQ",
+					"display_url": "pic.twitter.com/2qUMwG1NFQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1518070257247678464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1518070245692371000,
+					"id_str": "1518070245692370944",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/FRFE2GqVcAAix4Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRFE2GqVcAAix4Z.jpg",
+					"url": "https://t.co/2qUMwG1NFQ",
+					"display_url": "pic.twitter.com/2qUMwG1NFQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1518070257247678464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1518070246480879600,
+					"id_str": "1518070246480879616",
+					"indices": [
+						220,
+						243
+					],
+					"media_url": "http://pbs.twimg.com/media/FRFE2JmVIAAFyqf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRFE2JmVIAAFyqf.jpg",
+					"url": "https://t.co/2qUMwG1NFQ",
+					"display_url": "pic.twitter.com/2qUMwG1NFQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1518070257247678464/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Dreams Come True",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2499474,
+			"friends_count": 1,
+			"listed_count": 8170,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1390,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1639558806",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32013,
+		"favorite_count": 54861,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you Coachella ❤️‍🔥\n\nCheck out the live stream on YouTube Coachella LIVE Channel 1\n🔗https://t.co/rwTr12OC7g\n\n24 Apr 2022 | 2:00PM (KST)\n23 Apr 2022 | 10:00PM (PDT)\n\n📸 @ ai.visuals\n\n#aespa #æspa #에스파 #aespaCoachella https://t.co/2qUMwG1NFQ"
+	},
+	{
 		"created_at": "Sat Apr 23 23:40:18 +0000 2022",
 		"id": 1518000932193435600,
 		"id_str": "1518011914437242881",
