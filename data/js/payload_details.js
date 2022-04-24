039e0e8db3cab3ab82767fae835dedcde3593173
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-25 01:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86684,
-	"created_at": "Sun Apr 24 09:02:04 +0000 2022",
+	"tweets": 86685,
+	"created_at": "Sun Apr 24 16:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Sun Apr 24 15:52:40 +0000 2022",
+		"id": 1518255545958170600,
+		"id_str": "1518256618273591298",
+		"full_text": "[#이달의소녀] 📣 \n2022년 04월 25일 (월) Mnet ‘퀸덤2’ 3회차 2라운드 무대 모임 시간이 2:45PM으로 변경되었습니다.\n현장 평가단에 참여하시는 오빛 분들께서는 변경된 시간까지 추후 안내되는 모임 장소로 늦지 않게 모여주세요!\n\n👉🏻👉🏻 https://t.co/JiQLkZVtIG\n\n#LOONA #퀸덤2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						169,
+						175
+					]
+				},
+				{
+					"text": "퀸덤2",
+					"indices": [
+						176,
+						180
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JiQLkZVtIG",
+					"expanded_url": "https://m.cafe.daum.net/loonatheworld/F6Fq/388?svc=cafeapp",
+					"display_url": "m.cafe.daum.net/loonatheworld/…",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 773078392660856800,
+			"id_str": "773078392660856832",
+			"name": "이달의 소녀 Staff",
+			"screen_name": "loonaStaff",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 30589,
+			"friends_count": 1,
+			"listed_count": 531,
+			"created_at": "Tue Sep 06 08:40:26 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 328,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/773079419527180288/9ulDQr_w_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/773079419527180288/9ulDQr_w_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/773078392660856832/1602772146",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 100,
+		"favorite_count": 311,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#이달의소녀] 📣 \n2022년 04월 25일 (월) Mnet ‘퀸덤2’ 3회차 2라운드 무대 모임 시간이 2:45PM으로 변경되었습니다.\n현장 평가단에 참여하시는 오빛 분들께서는 변경된 시간까지 추후 안내되는 모임 장소로 늦지 않게 모여주세요!\n\n👉🏻👉🏻 https://t.co/JiQLkZVtIG\n\n#LOONA #퀸덤2"
+	},
+	{
 		"created_at": "Sun Apr 24 08:40:10 +0000 2022",
 		"id": 1518142018870194200,
 		"id_str": "1518147775418048517",
