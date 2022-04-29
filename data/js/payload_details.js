70cf8675097b15735ebafc6c23b5858c180dfcae
Youngmin Lee <iameyoungmin@gmail.com>
2022-04-30 01:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86786,
-	"created_at": "Fri Apr 29 12:02:06 +0000 2022",
+	"tweets": 86787,
+	"created_at": "Fri Apr 29 16:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Fri Apr 29 15:40:09 +0000 2022",
+		"id": 1520050599366172700,
+		"id_str": "1520065410258935810",
+		"full_text": "When the adrenaline kicks in 2 minutes before you're about to walk on stage 🤯\n\nCan't believe we get to do it all again next week in America and Canada! 🇺🇸🇨🇦 https://t.co/jhWcaggRbM https://t.co/HTtrKiAhTw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jhWcaggRbM",
+					"expanded_url": "http://ohwondermusic.com/tour",
+					"display_url": "ohwondermusic.com/tour",
+					"indices": [
+						157,
+						180
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1520050587873820700,
+					"id_str": "1520050587873820673",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FRhN9MLXwAEkgG3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRhN9MLXwAEkgG3.jpg",
+					"url": "https://t.co/HTtrKiAhTw",
+					"display_url": "pic.twitter.com/HTtrKiAhTw",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1520050599366172673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1441,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1520050587873820700,
+					"id_str": "1520050587873820673",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FRhN9MLXwAEkgG3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRhN9MLXwAEkgG3.jpg",
+					"url": "https://t.co/HTtrKiAhTw",
+					"display_url": "pic.twitter.com/HTtrKiAhTw",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1520050599366172673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1199,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1441,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520050591510245400,
+					"id_str": "1520050591510245376",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FRhN9ZuXMAAA30X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRhN9ZuXMAAA30X.jpg",
+					"url": "https://t.co/HTtrKiAhTw",
+					"display_url": "pic.twitter.com/HTtrKiAhTw",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1520050599366172673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520050594907631600,
+					"id_str": "1520050594907631619",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FRhN9mYXMAM0tiN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRhN9mYXMAM0tiN.jpg",
+					"url": "https://t.co/HTtrKiAhTw",
+					"display_url": "pic.twitter.com/HTtrKiAhTw",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1520050599366172673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1440,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520050597495513000,
+					"id_str": "1520050597495513091",
+					"indices": [
+						181,
+						204
+					],
+					"media_url": "http://pbs.twimg.com/media/FRhN9wBXIAMd10g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRhN9wBXIAMd10g.jpg",
+					"url": "https://t.co/HTtrKiAhTw",
+					"display_url": "pic.twitter.com/HTtrKiAhTw",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1520050599366172673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 905,
+							"h": 905,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 905,
+							"h": 905,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2740435740,
+			"id_str": "2740435740",
+			"name": "Oh Wonder",
+			"screen_name": "OhWonderMusic",
+			"location": "London",
+			"description": "Our new single 'Fuck It I Love You' is out now",
+			"url": "https://t.co/aWDekOEjgr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aWDekOEjgr",
+							"expanded_url": "https://linktr.ee/ohwondermusic",
+							"display_url": "linktr.ee/ohwondermusic",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 166730,
+			"friends_count": 204,
+			"listed_count": 431,
+			"created_at": "Sun Aug 17 20:45:13 +0000 2014",
+			"favourites_count": 3553,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 5373,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2740435740/1649423793",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 79,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When the adrenaline kicks in 2 minutes before you're about to walk on stage 🤯\n\nCan't believe we get to do it all again next week in America and Canada! 🇺🇸🇨🇦 https://t.co/jhWcaggRbM https://t.co/HTtrKiAhTw"
+	},
+	{
 		"created_at": "Fri Apr 29 11:22:00 +0000 2022",
 		"id": 1519995160079519700,
 		"id_str": "1520000441471229952",
