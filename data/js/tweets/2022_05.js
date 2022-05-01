42481518165665459bc085fd768eae05b184ad7f
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-02 01:02:01 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sun May 01 15:40:12 +0000 2022",
+		"id": 1520780653159092200,
+		"id_str": "1520790195494440960",
+		"full_text": "BoA 보아 [QUINCY (The Greatest Ver.)]\n\n🎧Melon: https://t.co/e5dS8a48x4 \n🎧FLO: https://t.co/7LVWD7cWfl \n🎧genie: https://t.co/wKo3itZ1gh \n🎧VIBE: https://t.co/R9GeE7AdBC \n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#QUINCY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						167,
+						171
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						172,
+						175
+					]
+				},
+				{
+					"text": "TheGreatest",
+					"indices": [
+						177,
+						189
+					]
+				},
+				{
+					"text": "BoA_TheGreatest",
+					"indices": [
+						190,
+						206
+					]
+				},
+				{
+					"text": "QUINCY",
+					"indices": [
+						207,
+						214
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e5dS8a48x4",
+					"expanded_url": "https://bit.ly/3MGsNoV",
+					"display_url": "bit.ly/3MGsNoV",
+					"indices": [
+						45,
+						68
+					]
+				},
+				{
+					"url": "https://t.co/7LVWD7cWfl",
+					"expanded_url": "https://bit.ly/3y2G3QH",
+					"display_url": "bit.ly/3y2G3QH",
+					"indices": [
+						76,
+						99
+					]
+				},
+				{
+					"url": "https://t.co/wKo3itZ1gh",
+					"expanded_url": "https://bit.ly/3LBk5sa",
+					"display_url": "bit.ly/3LBk5sa",
+					"indices": [
+						109,
+						132
+					]
+				},
+				{
+					"url": "https://t.co/R9GeE7AdBC",
+					"expanded_url": "https://bit.ly/3F2imtv",
+					"display_url": "bit.ly/3F2imtv",
+					"indices": [
+						141,
+						164
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69597,
+			"friends_count": 1,
+			"listed_count": 261,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 291,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 163,
+		"favorite_count": 251,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BoA 보아 [QUINCY (The Greatest Ver.)]\n\n🎧Melon: https://t.co/e5dS8a48x4 \n🎧FLO: https://t.co/7LVWD7cWfl \n🎧genie: https://t.co/wKo3itZ1gh \n🎧VIBE: https://t.co/R9GeE7AdBC \n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#QUINCY"
+	},
+	{
 		"created_at": "Sun May 01 15:18:42 +0000 2022",
 		"id": 1520780093639266300,
 		"id_str": "1520784786180100096",
