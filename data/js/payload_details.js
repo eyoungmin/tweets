952de987c0549f79d3ff6429d11e22af2f5f6c2c
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-01 11:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86798,
-	"created_at": "Sat Apr 30 15:19:58 +0000 2022",
+	"tweets": 86799,
+	"created_at": "Sun May 01 02:02:04 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_05.js
@@ -0,0 +1,153 @@
+Grailbird.data.tweets_2022_05 = 
+[
+	{
+		"created_at": "Sun May 01 01:40:10 +0000 2022",
+		"id": 1520568800004898800,
+		"id_str": "1520578794377068545",
+		"full_text": "A huge shout out to each and every one of my #BIBIbullets for all the 🔥 questions you sent in for my @iHeartRadio @MostRequestLive @AskAnythingChat w/ @OnAirRomeo.  Watch here: https://t.co/NSM2yfaL8K\n#BestLover",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			211
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BIBIbullets",
+					"indices": [
+						45,
+						57
+					]
+				},
+				{
+					"text": "BestLover",
+					"indices": [
+						201,
+						211
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "iHeartRadio",
+					"name": "iHeartRadio",
+					"id": 46116615,
+					"id_str": "46116615",
+					"indices": [
+						101,
+						113
+					]
+				},
+				{
+					"screen_name": "MostRequestLive",
+					"name": "Most Requested Live",
+					"id": 108775893,
+					"id_str": "108775893",
+					"indices": [
+						114,
+						130
+					]
+				},
+				{
+					"screen_name": "AskAnythingChat",
+					"name": "Ask Anything",
+					"id": 450186253,
+					"id_str": "450186253",
+					"indices": [
+						131,
+						147
+					]
+				},
+				{
+					"screen_name": "OnAirRomeo",
+					"name": "On Air Romeo",
+					"id": 21590830,
+					"id_str": "21590830",
+					"indices": [
+						151,
+						162
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NSM2yfaL8K",
+					"expanded_url": "https://www.askanythingchat.com/chat/bibi-mrl-ask-anything-chat-2/",
+					"display_url": "askanythingchat.com/chat/bibi-mrl-…",
+					"indices": [
+						177,
+						200
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1448497121867436000,
+			"id_str": "1448497121867436037",
+			"name": "nakedbibi",
+			"screen_name": "nakedbibi",
+			"location": "",
+			"description": "BIBI💧💦",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 53157,
+			"friends_count": 2,
+			"listed_count": 138,
+			"created_at": "Thu Oct 14 03:53:27 +0000 2021",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1517974570023628800/j8sH9b3L_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1517974570023628800/j8sH9b3L_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1448497121867436037/1650748326",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 41,
+		"favorite_count": 226,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A huge shout out to each and every one of my #BIBIbullets for all the 🔥 questions you sent in for my @iHeartRadio @MostRequestLive @AskAnythingChat w/ @OnAirRomeo.  Watch here: https://t.co/NSM2yfaL8K\n#BestLover"
+	}
+]
