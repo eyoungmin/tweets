aaaa901f16a8da0dea08ec7733d541f8ef51ef93
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-02 12:02:01 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86804,
-	"created_at": "Sun May 01 16:01:59 +0000 2022",
+	"tweets": 86806,
+	"created_at": "Mon May 02 03:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,412 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Mon May 02 02:29:39 +0000 2022",
+		"id": 1520942181225091000,
+		"id_str": "1520953635273912322",
+		"full_text": "[#NEWS] ‘징크스의 연인’ 서현 첫 스틸, 행운의 여신 비주얼\n▶ https://t.co/Sq4iJDnryV\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/Rx9Dqw9Kxc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						65,
+						68
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						69,
+						76
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						77,
+						85
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						86,
+						100
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Sq4iJDnryV",
+					"expanded_url": "http://naver.me/FErpK8QT",
+					"display_url": "naver.me/FErpK8QT",
+					"indices": [
+						40,
+						63
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1520942090519150600,
+					"id_str": "1520942090519150593",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FRt4xeVVgAE-rb7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRt4xeVVgAE-rb7.jpg",
+					"url": "https://t.co/Rx9Dqw9Kxc",
+					"display_url": "pic.twitter.com/Rx9Dqw9Kxc",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1520942181225091072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1520942090519150600,
+					"id_str": "1520942090519150593",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FRt4xeVVgAE-rb7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRt4xeVVgAE-rb7.jpg",
+					"url": "https://t.co/Rx9Dqw9Kxc",
+					"display_url": "pic.twitter.com/Rx9Dqw9Kxc",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1520942181225091072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1520942090514927600,
+					"id_str": "1520942090514927617",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/FRt4xeUVEAEqjx8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FRt4xeUVEAEqjx8.jpg",
+					"url": "https://t.co/Rx9Dqw9Kxc",
+					"display_url": "pic.twitter.com/Rx9Dqw9Kxc",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1520942181225091072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 59915,
+			"friends_count": 12,
+			"listed_count": 268,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15501,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 334,
+		"favorite_count": 507,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘징크스의 연인’ 서현 첫 스틸, 행운의 여신 비주얼\n▶ https://t.co/Sq4iJDnryV\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/Rx9Dqw9Kxc"
+	},
+	{
+		"created_at": "Mon May 02 02:18:48 +0000 2022",
+		"id": 1520949730733224000,
+		"id_str": "1520950906648338433",
+		"full_text": "'퍼포먼스 퀸' 효연 (HYO), 첫 미니앨범 '휩 (DEEP)' 5월 16일 공개!\n오늘부터 예약 판매 시작!\n\n‘Performance Queen’ HYO’s 1st mini album ‘휩 (DEEP)’ will be released on 5/16!  Pre-order starts today!\n\nhttps://t.co/SixzJfeIvy\n\n#HYO #효연\n#휩_DEEP #HYO_휩_DEEP\n#GirlsGeneration #소녀시대",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						193,
+						197
+					]
+				},
+				{
+					"text": "효연",
+					"indices": [
+						198,
+						201
+					]
+				},
+				{
+					"text": "휩_DEEP",
+					"indices": [
+						202,
+						209
+					]
+				},
+				{
+					"text": "HYO_휩_DEEP",
+					"indices": [
+						210,
+						221
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						222,
+						238
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						239,
+						244
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SixzJfeIvy",
+					"expanded_url": "https://bit.ly/375qRHD",
+					"display_url": "bit.ly/375qRHD",
+					"indices": [
+						168,
+						191
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3756010,
+			"friends_count": 5,
+			"listed_count": 5870,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2780,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520784620396367872/n-mDvaB1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1651417198",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1895,
+		"favorite_count": 3604,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "'퍼포먼스 퀸' 효연 (HYO), 첫 미니앨범 '휩 (DEEP)' 5월 16일 공개!\n오늘부터 예약 판매 시작!\n\n‘Performance Queen’ HYO’s 1st mini album ‘휩 (DEEP)’ will be released on 5/16!  Pre-order starts today!\n\nhttps://t.co/SixzJfeIvy\n\n#HYO #효연\n#휩_DEEP #HYO_휩_DEEP\n#GirlsGeneration #소녀시대"
+	},
+	{
 		"created_at": "Sun May 01 15:40:12 +0000 2022",
 		"id": 1520780653159092200,
 		"id_str": "1520790195494440960",
