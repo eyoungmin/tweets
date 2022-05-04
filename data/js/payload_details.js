42237b1bffd0cdb7c7d6930d3a53a8e8ebc4ddda
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-05 03:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86858,
-	"created_at": "Wed May 04 15:19:55 +0000 2022",
+	"tweets": 86859,
+	"created_at": "Wed May 04 18:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 04 17:40:16 +0000 2022",
+		"id": 1521906137699860500,
+		"id_str": "1521907575339593728",
+		"full_text": "Just two days until #MusicOfTheSpheresWorldTour lands in 🇺🇸! \n\nDallas, TX • Houston, TX • Phoenix, AZ • Santa Clara, CA • Chicago, IL • Washington, DC • East Rutherford, NJ • Philadelphia, PA • Atlanta, GA • Tampa, FL\n\nDates / final tickets at https://t.co/8g4RmOWSGp https://t.co/91yjnIoLjP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MusicOfTheSpheresWorldTour",
+					"indices": [
+						20,
+						47
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8g4RmOWSGp",
+					"expanded_url": "https://www.coldplay.com/tour/",
+					"display_url": "coldplay.com/tour/",
+					"indices": [
+						244,
+						267
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1521906131924402200,
+					"id_str": "1521906131924402176",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FR7lkEbXwAALGux.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR7lkEbXwAALGux.jpg",
+					"url": "https://t.co/91yjnIoLjP",
+					"display_url": "pic.twitter.com/91yjnIoLjP",
+					"expanded_url": "https://twitter.com/coldplay/status/1521906137699860481/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1521906131924402200,
+					"id_str": "1521906131924402176",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FR7lkEbXwAALGux.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FR7lkEbXwAALGux.jpg",
+					"url": "https://t.co/91yjnIoLjP",
+					"display_url": "pic.twitter.com/91yjnIoLjP",
+					"expanded_url": "https://twitter.com/coldplay/status/1521906137699860481/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Calypso",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23604966,
+			"friends_count": 775,
+			"listed_count": 42549,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1575,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9117,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 144,
+		"favorite_count": 1352,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just two days until #MusicOfTheSpheresWorldTour lands in 🇺🇸! \n\nDallas, TX • Houston, TX • Phoenix, AZ • Santa Clara, CA • Chicago, IL • Washington, DC • East Rutherford, NJ • Philadelphia, PA • Atlanta, GA • Tampa, FL\n\nDates / final tickets at https://t.co/8g4RmOWSGp https://t.co/91yjnIoLjP"
+	},
+	{
 		"created_at": "Wed May 04 15:19:22 +0000 2022",
 		"id": 1521867658584461300,
 		"id_str": "1521872119428050944",
