8f71a04bd5632d3f8f0ba0ea7125285a1e3406a5
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-07 15:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86932,
-	"created_at": "Sat May 07 05:02:01 +0000 2022",
+	"tweets": 86933,
+	"created_at": "Sat May 07 06:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,136 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sat May 07 05:40:10 +0000 2022",
+		"id": 1522798602363752400,
+		"id_str": "1522813519636357120",
+		"full_text": "보아, 日 '셀프 커버 프로젝트' 여덟 번째 곡 'LOVE LETTER' 9일 공개!\n \n‘LOVE LETTER’, the 8th track of BoA's Japanese 'Self Cover Project', will be released on 5/9!\n\nhttps://t.co/QeshfaFwtw\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#LOVE_LETTER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						170,
+						174
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						175,
+						178
+					]
+				},
+				{
+					"text": "TheGreatest",
+					"indices": [
+						180,
+						192
+					]
+				},
+				{
+					"text": "BoA_TheGreatest",
+					"indices": [
+						193,
+						209
+					]
+				},
+				{
+					"text": "LOVE_LETTER",
+					"indices": [
+						210,
+						222
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QeshfaFwtw",
+					"expanded_url": "https://bit.ly/384WKRe",
+					"display_url": "bit.ly/384WKRe",
+					"indices": [
+						145,
+						168
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69860,
+			"friends_count": 1,
+			"listed_count": 261,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 292,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 109,
+		"favorite_count": 227,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "보아, 日 '셀프 커버 프로젝트' 여덟 번째 곡 'LOVE LETTER' 9일 공개!\n \n‘LOVE LETTER’, the 8th track of BoA's Japanese 'Self Cover Project', will be released on 5/9!\n\nhttps://t.co/QeshfaFwtw\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#LOVE_LETTER"
+	},
+	{
 		"created_at": "Sat May 07 04:40:34 +0000 2022",
 		"id": 1522796651169333200,
 		"id_str": "1522798521434398720",
