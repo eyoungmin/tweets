f4794a80479724848352546fc0d40c0b86990aaf
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-11 00:20:01 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,259 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 10 15:06:23 +0000 2022",
+		"id": 1524042916662235100,
+		"id_str": "1524043175672700930",
+		"full_text": "#BLACKPINK 'How You Like That' DANCE PERFORMANCE VIDEO HITS 1.1 BILLION VIEWS @YouTube\nBLINKs worldwide, thank you so much!\n\n'How You Like That' DANCE PERFORMANCE VIDEO\n▶️ https://t.co/Ls060qAQ9E\n\n#블랙핑크 #HOWYOULIKETHAT #DANCEPERFORMANCE #안무영상 #1_1BILLION #YOUTUBE #YG https://t.co/DcdprpE1Ub",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						197,
+						202
+					]
+				},
+				{
+					"text": "HOWYOULIKETHAT",
+					"indices": [
+						203,
+						218
+					]
+				},
+				{
+					"text": "DANCEPERFORMANCE",
+					"indices": [
+						219,
+						236
+					]
+				},
+				{
+					"text": "안무영상",
+					"indices": [
+						237,
+						242
+					]
+				},
+				{
+					"text": "1_1BILLION",
+					"indices": [
+						243,
+						254
+					]
+				},
+				{
+					"text": "YOUTUBE",
+					"indices": [
+						255,
+						263
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						264,
+						267
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						78,
+						86
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Ls060qAQ9E",
+					"expanded_url": "http://youtu.be/32si5cfrCNc",
+					"display_url": "youtu.be/32si5cfrCNc",
+					"indices": [
+						172,
+						195
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1524039358583808000,
+					"id_str": "1524039358583808000",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZ5uNAaIAAoTee.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZ5uNAaIAAoTee.jpg",
+					"url": "https://t.co/DcdprpE1Ub",
+					"display_url": "pic.twitter.com/DcdprpE1Ub",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1524042916662235136/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1620,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524039358583808000,
+					"id_str": "1524039358583808000",
+					"indices": [
+						268,
+						291
+					],
+					"media_url": "http://pbs.twimg.com/media/FSZ5uNAaIAAoTee.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSZ5uNAaIAAoTee.jpg",
+					"url": "https://t.co/DcdprpE1Ub",
+					"display_url": "pic.twitter.com/DcdprpE1Ub",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1524042916662235136/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1620,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/5WJvVn84lr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5WJvVn84lr",
+							"expanded_url": "https://youtu.be/awkkyBH2zEo",
+							"display_url": "youtu.be/awkkyBH2zEo",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6330352,
+			"friends_count": 0,
+			"listed_count": 7184,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 497,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1311878593278996481/WPvQDNTP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1631247360",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6660,
+		"favorite_count": 18706,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK 'How You Like That' DANCE PERFORMANCE VIDEO HITS 1.1 BILLION VIEWS @YouTube\nBLINKs worldwide, thank you so much!\n\n'How You Like That' DANCE PERFORMANCE VIDEO\n▶️ https://t.co/Ls060qAQ9E\n\n#블랙핑크 #HOWYOULIKETHAT #DANCEPERFORMANCE #안무영상 #1_1BILLION #YOUTUBE #YG https://t.co/DcdprpE1Ub"
+	},
+	{
 		"created_at": "Tue May 10 12:40:09 +0000 2022",
 		"id": 1523993572449656800,
 		"id_str": "1524006377286479872",
@@ -158,7 +411,7 @@ Grailbird.data.tweets_2022_05 =
 			"name": "sigrid",
 			"screen_name": "thisissigrid",
 			"location": "",
-			"description": "How To Let Go out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"description": "How To Let Go out now ❤️🔥❤️🔥❤️🔥",
 			"url": "https://t.co/qJ8McBmikD",
 			"entities": {
 				"url": {
