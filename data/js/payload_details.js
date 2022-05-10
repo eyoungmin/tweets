ca59162a7fd5378d5595739c495d197607406606
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-10 14:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 86989,
-	"created_at": "Tue May 10 04:02:03 +0000 2022",
+	"tweets": 86990,
+	"created_at": "Tue May 10 05:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,267 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 10 04:29:39 +0000 2022",
+		"id": 1523876916373385200,
+		"id_str": "1523882936281534464",
+		"full_text": "HYO Tampilkan Lagu Dance EDM yang Kuat Lewat Mini-album Pertama ‘DEEP’\n\nInfo lengkap di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/BKJZGoEu5Y\n\n@GirlsGeneration \n#HYO #DEEP #HYO_DEEP\n#GirlsGeneration https://t.co/d7w8Ugfweo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						173,
+						177
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						178,
+						183
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						184,
+						193
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						194,
+						210
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						155,
+						171
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BKJZGoEu5Y",
+					"expanded_url": "https://www.facebook.com/smtown.idn/posts/1032425460707801",
+					"display_url": "facebook.com/smtown.idn/pos…",
+					"indices": [
+						130,
+						153
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1523876905862467600,
+					"id_str": "1523876905862467585",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/FSXl-NdakAE3gts.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSXl-NdakAE3gts.jpg",
+					"url": "https://t.co/d7w8Ugfweo",
+					"display_url": "pic.twitter.com/d7w8Ugfweo",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1523876916373385216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1523876905862467600,
+					"id_str": "1523876905862467585",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/FSXl-NdakAE3gts.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSXl-NdakAE3gts.jpg",
+					"url": "https://t.co/d7w8Ugfweo",
+					"display_url": "pic.twitter.com/d7w8Ugfweo",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1523876916373385216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1523876905862467600,
+					"id_str": "1523876905862467584",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/FSXl-NdakAAsj6h.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSXl-NdakAAsj6h.jpg",
+					"url": "https://t.co/d7w8Ugfweo",
+					"display_url": "pic.twitter.com/d7w8Ugfweo",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1523876916373385216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1064828006487609300,
+			"id_str": "1064828006487609345",
+			"name": "SMTOWN Indonesia",
+			"screen_name": "SMTOWN_Idn",
+			"location": "FX Sudirman Mall F5 #18B",
+			"description": "SM Entertainment Indonesia Official Twitter Page",
+			"url": "https://t.co/BGCHmjauKM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BGCHmjauKM",
+							"expanded_url": "http://bit.ly/AngketSM2022",
+							"display_url": "bit.ly/AngketSM2022",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 976361,
+			"friends_count": 36,
+			"listed_count": 602,
+			"created_at": "Tue Nov 20 10:29:15 +0000 2018",
+			"favourites_count": 88,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3057,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1064828006487609345/1551316595",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 23,
+		"favorite_count": 292,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "HYO Tampilkan Lagu Dance EDM yang Kuat Lewat Mini-album Pertama ‘DEEP’\n\nInfo lengkap di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/BKJZGoEu5Y\n\n@GirlsGeneration \n#HYO #DEEP #HYO_DEEP\n#GirlsGeneration https://t.co/d7w8Ugfweo"
+	},
+	{
 		"created_at": "Tue May 10 03:40:10 +0000 2022",
 		"id": 1523860627785076700,
 		"id_str": "1523870483984392192",
