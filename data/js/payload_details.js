7e8628a95a0f1f5edbdad2bddcd3dda409ccf1bb
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-11 13:02:04 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87011,
-	"created_at": "Wed May 11 03:02:02 +0000 2022",
+	"tweets": 87013,
+	"created_at": "Wed May 11 04:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,530 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Wed May 11 03:40:10 +0000 2022",
+		"id": 1524217122481905700,
+		"id_str": "1524232871921205248",
+		"full_text": "📢5/19(목) 오후 8시! &lt;브로커&gt; #송강호 #강동원 #이지은 #이주영 이 트위터 #블루룸 으로 여러분을 찾아옵니다! \n\n해시태그 #Ask_Broker 와 함께 트윗으로 질문을 남겨주세요! &lt;#브로커&gt; 블루룸에서 직접 대답해 드릴게요!💙\n\n#TwitterBlueroom LIVE Q&amp;A\n#배두나 #고레에다히로카즈감독 #6월8일대개봉 https://t.co/9gmBPUAxyK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "송강호",
+					"indices": [
+						28,
+						32
+					]
+				},
+				{
+					"text": "강동원",
+					"indices": [
+						33,
+						37
+					]
+				},
+				{
+					"text": "이지은",
+					"indices": [
+						38,
+						42
+					]
+				},
+				{
+					"text": "이주영",
+					"indices": [
+						43,
+						47
+					]
+				},
+				{
+					"text": "블루룸",
+					"indices": [
+						54,
+						58
+					]
+				},
+				{
+					"text": "Ask_Broker",
+					"indices": [
+						81,
+						92
+					]
+				},
+				{
+					"text": "브로커",
+					"indices": [
+						118,
+						122
+					]
+				},
+				{
+					"text": "TwitterBlueroom",
+					"indices": [
+						148,
+						164
+					]
+				},
+				{
+					"text": "배두나",
+					"indices": [
+						178,
+						182
+					]
+				},
+				{
+					"text": "고레에다히로카즈감독",
+					"indices": [
+						183,
+						194
+					]
+				},
+				{
+					"text": "6월8일대개봉",
+					"indices": [
+						195,
+						203
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1524216616120352800,
+					"id_str": "1524216616120352769",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/media/FSca78yaAAEACnE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSca78yaAAEACnE.jpg",
+					"url": "https://t.co/9gmBPUAxyK",
+					"display_url": "pic.twitter.com/9gmBPUAxyK",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1524217122481905664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524216616120352800,
+					"id_str": "1524216616120352769",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/media/FSca78yaAAEACnE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSca78yaAAEACnE.jpg",
+					"url": "https://t.co/9gmBPUAxyK",
+					"display_url": "pic.twitter.com/9gmBPUAxyK",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1524217122481905664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 452664114,
+			"id_str": "452664114",
+			"name": "CJ ENM Movie",
+			"screen_name": "CJENMMOVIE",
+			"location": "",
+			"description": "아시아 No.1 스튜디오 CJ ENM_Movie 공식 트위터입니다. 28년 동안 한 우물만 파고 있는 C네마을🏡",
+			"url": "https://t.co/RJkRVcHopT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/RJkRVcHopT",
+							"expanded_url": "https://www.youtube.com/user/cjenmmovie",
+							"display_url": "youtube.com/user/cjenmmovie",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 328973,
+			"friends_count": 10716,
+			"listed_count": 238,
+			"created_at": "Mon Jan 02 03:32:49 +0000 2012",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6487,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/452664114/1625636011",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 869,
+		"favorite_count": 2146,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📢5/19(목) 오후 8시! &lt;브로커&gt; #송강호 #강동원 #이지은 #이주영 이 트위터 #블루룸 으로 여러분을 찾아옵니다! \n\n해시태그 #Ask_Broker 와 함께 트윗으로 질문을 남겨주세요! &lt;#브로커&gt; 블루룸에서 직접 대답해 드릴게요!💙\n\n#TwitterBlueroom LIVE Q&amp;A\n#배두나 #고레에다히로카즈감독 #6월8일대개봉 https://t.co/9gmBPUAxyK"
+	},
+	{
+		"created_at": "Wed May 11 03:29:45 +0000 2022",
+		"id": 1524221393508106200,
+		"id_str": "1524230253765443589",
+		"full_text": "HYO Tampilkan Energi Positif lewat Lagu ‘Stupid’\n\nInfo lengkap di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/NezAeovads\n\n@GirlsGeneration @Hyoyeon_djhyo \n#HYO #DEEP #HYO_DEEP\n#GirlsGeneration https://t.co/FX4Nn1R6k8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						166,
+						170
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						171,
+						176
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						177,
+						186
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						187,
+						203
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GirlsGeneration",
+					"name": "Girls' Generation",
+					"id": 391115625,
+					"id_str": "391115625",
+					"indices": [
+						133,
+						149
+					]
+				},
+				{
+					"screen_name": "Hyoyeon_djhyo",
+					"name": "Hyoyeon",
+					"id": 792687101842186200,
+					"id_str": "792687101842186240",
+					"indices": [
+						150,
+						164
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NezAeovads",
+					"expanded_url": "https://www.facebook.com/smtown.idn/posts/1033033673980313",
+					"display_url": "facebook.com/smtown.idn/pos…",
+					"indices": [
+						108,
+						131
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1524221375476801500,
+					"id_str": "1524221375476801536",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/media/FScfQ-xaIAA8A8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FScfQ-xaIAA8A8f.jpg",
+					"url": "https://t.co/FX4Nn1R6k8",
+					"display_url": "pic.twitter.com/FX4Nn1R6k8",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1524221393508106240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 456,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1372,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 804,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524221375476801500,
+					"id_str": "1524221375476801536",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/media/FScfQ-xaIAA8A8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FScfQ-xaIAA8A8f.jpg",
+					"url": "https://t.co/FX4Nn1R6k8",
+					"display_url": "pic.twitter.com/FX4Nn1R6k8",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1524221393508106240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 456,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1372,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 804,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1524221375476826000,
+					"id_str": "1524221375476826112",
+					"indices": [
+						204,
+						227
+					],
+					"media_url": "http://pbs.twimg.com/media/FScfQ-xagAAz9o7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FScfQ-xagAAz9o7.jpg",
+					"url": "https://t.co/FX4Nn1R6k8",
+					"display_url": "pic.twitter.com/FX4Nn1R6k8",
+					"expanded_url": "https://twitter.com/SMTOWN_Idn/status/1524221393508106240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1462,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 485,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1064828006487609300,
+			"id_str": "1064828006487609345",
+			"name": "SMTOWN Indonesia",
+			"screen_name": "SMTOWN_Idn",
+			"location": "FX Sudirman Mall F5 #18B",
+			"description": "SM Entertainment Indonesia Official Twitter Page",
+			"url": "https://t.co/BGCHmjauKM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BGCHmjauKM",
+							"expanded_url": "http://bit.ly/AngketSM2022",
+							"display_url": "bit.ly/AngketSM2022",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 977222,
+			"friends_count": 36,
+			"listed_count": 602,
+			"created_at": "Tue Nov 20 10:29:15 +0000 2018",
+			"favourites_count": 88,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3059,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1100927655610744832/D4iRym3b_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1064828006487609345/1551316595",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 282,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "HYO Tampilkan Energi Positif lewat Lagu ‘Stupid’\n\nInfo lengkap di Official Facebook Page SMTOWN Indonesia\n👉🏻https://t.co/NezAeovads\n\n@GirlsGeneration @Hyoyeon_djhyo \n#HYO #DEEP #HYO_DEEP\n#GirlsGeneration https://t.co/FX4Nn1R6k8"
+	},
+	{
 		"created_at": "Wed May 11 02:57:18 +0000 2022",
 		"id": 1524210302547800000,
 		"id_str": "1524222083906957312",
