fc2841dfd8b7372b05ba2521bd71502ec13682ca
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-12 19:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87045,
-	"created_at": "Thu May 12 09:02:03 +0000 2022",
+	"tweets": 87046,
+	"created_at": "Thu May 12 10:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,282 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 12 09:24:55 +0000 2022",
+		"id": 1524675931868377000,
+		"id_str": "1524682020906885121",
+		"full_text": "너는 어딘가에서 막내야\n사랑을 받고 크는 나무야\n⠀\n🐠 Yerin Baek Single [물고기]\n2022.05.24(Tue) 6PM KST\n\n#백예린 #yerinbaek #물고기 #pisces https://t.co/fd1fYlrQ56",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "백예린",
+					"indices": [
+						80,
+						84
+					]
+				},
+				{
+					"text": "yerinbaek",
+					"indices": [
+						85,
+						95
+					]
+				},
+				{
+					"text": "물고기",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "pisces",
+					"indices": [
+						101,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1524675749479055400,
+					"id_str": "1524675749479055362",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FSi8hB1aAAIo2rr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSi8hB1aAAIo2rr.jpg",
+					"url": "https://t.co/fd1fYlrQ56",
+					"display_url": "pic.twitter.com/fd1fYlrQ56",
+					"expanded_url": "https://twitter.com/yerinbaek/status/1524675931868377089/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1524675749479055400,
+					"id_str": "1524675749479055362",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FSi8hB1aAAIo2rr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSi8hB1aAAIo2rr.jpg",
+					"url": "https://t.co/fd1fYlrQ56",
+					"display_url": "pic.twitter.com/fd1fYlrQ56",
+					"expanded_url": "https://twitter.com/yerinbaek/status/1524675931868377089/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1524675749474881500,
+					"id_str": "1524675749474881536",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FSi8hB0aUAAL4yT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSi8hB0aUAAL4yT.jpg",
+					"url": "https://t.co/fd1fYlrQ56",
+					"display_url": "pic.twitter.com/fd1fYlrQ56",
+					"expanded_url": "https://twitter.com/yerinbaek/status/1524675931868377089/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1524675749479055400,
+					"id_str": "1524675749479055360",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FSi8hB1aAAAghs5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSi8hB1aAAAghs5.jpg",
+					"url": "https://t.co/fd1fYlrQ56",
+					"display_url": "pic.twitter.com/fd1fYlrQ56",
+					"expanded_url": "https://twitter.com/yerinbaek/status/1524675931868377089/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1364,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 799,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1185935579168424000,
+			"id_str": "1185935579168423936",
+			"name": "Yerin Baek",
+			"screen_name": "yerinbaek",
+			"location": "",
+			"description": "#YerinBaek Official Twitter",
+			"url": "https://t.co/ocnoiYrjXq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ocnoiYrjXq",
+							"expanded_url": "https://www.youtube.com/YerinBaekOfficial",
+							"display_url": "youtube.com/YerinBaekOffic…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40467,
+			"friends_count": 2,
+			"listed_count": 209,
+			"created_at": "Sun Oct 20 15:07:39 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 194,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1524289217148297216/YygSWaMw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1524289217148297216/YygSWaMw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1185935579168423936/1652253459",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1607,
+		"favorite_count": 1841,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "너는 어딘가에서 막내야\n사랑을 받고 크는 나무야\n⠀\n🐠 Yerin Baek Single [물고기]\n2022.05.24(Tue) 6PM KST\n\n#백예린 #yerinbaek #물고기 #pisces https://t.co/fd1fYlrQ56"
+	},
+	{
 		"created_at": "Thu May 12 08:40:13 +0000 2022",
 		"id": 1524662535685025800,
 		"id_str": "1524670772496801794",
