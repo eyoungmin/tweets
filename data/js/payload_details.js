219d557a4d30539c374db7b58f8deeaa99cfa7bc
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-14 04:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87096,
-	"created_at": "Fri May 13 18:02:04 +0000 2022",
+	"tweets": 87097,
+	"created_at": "Fri May 13 19:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 13 18:40:10 +0000 2022",
+		"id": 1525179943482687500,
+		"id_str": "1525184139925897216",
+		"full_text": "OH MY FUCKING GOD MY ALBUM WENT #2 IN THE UK AND #1 IN NORWAY !!!!!!!!!!! u legends!!!! thanks so much for listening, i’m so happy proud overwhelmed and just GODDAMNIT GUYS 😭🤩🤘 https://t.co/n6MkvIlkLR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1525179937707155500,
+					"id_str": "1525179937707155457",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/FSqHEpWWYAE61UC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSqHEpWWYAE61UC.jpg",
+					"url": "https://t.co/n6MkvIlkLR",
+					"display_url": "pic.twitter.com/n6MkvIlkLR",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1525179943482687490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 671,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1155,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1155,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1525179937707155500,
+					"id_str": "1525179937707155457",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/FSqHEpWWYAE61UC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FSqHEpWWYAE61UC.jpg",
+					"url": "https://t.co/n6MkvIlkLR",
+					"display_url": "pic.twitter.com/n6MkvIlkLR",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1525179943482687490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 671,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1155,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1155,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "How To Let Go out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"url": "https://t.co/qJ8McBmikD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qJ8McBmikD",
+							"expanded_url": "https://Sigrid.lnk.to/HowToLetGoTW",
+							"display_url": "Sigrid.lnk.to/HowToLetGoTW",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 86982,
+			"friends_count": 269,
+			"listed_count": 327,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 6179,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4395,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1651819275",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 900,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "OH MY FUCKING GOD MY ALBUM WENT #2 IN THE UK AND #1 IN NORWAY !!!!!!!!!!! u legends!!!! thanks so much for listening, i’m so happy proud overwhelmed and just GODDAMNIT GUYS 😭🤩🤘 https://t.co/n6MkvIlkLR"
+	},
+	{
 		"created_at": "Fri May 13 17:40:18 +0000 2022",
 		"id": 1525160044941164500,
 		"id_str": "1525169075021815812",
