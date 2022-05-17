9f760df9bbfbafe48f48c3274a742ade0238c5ad
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-17 22:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87164,
-	"created_at": "Tue May 17 12:02:06 +0000 2022",
+	"tweets": 87165,
+	"created_at": "Tue May 17 13:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 17 12:29:39 +0000 2022",
+		"id": 1526526561356480500,
+		"id_str": "1526540448726147077",
+		"full_text": "#HYO จะขึ้นแสดงเพลงไตเติล ‘DEEP’ บนรายการเพลงเกาหลีในสัปดาห์นี้\n\nมินิอัลบั้มแรก ‘#DEEP’ ครองอันดับ 1 บนชาร์ต Top Albums ของ iTunes ใน 10 ประเทศ!\n\n🕷 https://t.co/MliO5jDtkl\n\nMV 🎬 https://t.co/Q7H5oyBT4R\n\nมินิอัลบั้มแรก ‘DEEP’🎧 https://t.co/dzDkQF2k2e\n\n#HYO_DEEP\n#GirlsGeneration https://t.co/O2nMGkMafc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HYO",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "DEEP",
+					"indices": [
+						81,
+						86
+					]
+				},
+				{
+					"text": "HYO_DEEP",
+					"indices": [
+						251,
+						260
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						261,
+						277
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MliO5jDtkl",
+					"expanded_url": "http://facebook.com/smtruethailand/posts/pfbid07GckuMTuL6pgU3kpEtU8zrDEaLpsA9zMdCvYmC1agNA3ALZPDRGJR8kmLHrp4ejMl",
+					"display_url": "facebook.com/smtruethailand…",
+					"indices": [
+						148,
+						171
+					]
+				},
+				{
+					"url": "https://t.co/Q7H5oyBT4R",
+					"expanded_url": "http://youtu.be/4DTkTJPtDZI",
+					"display_url": "youtu.be/4DTkTJPtDZI",
+					"indices": [
+						178,
+						201
+					]
+				},
+				{
+					"url": "https://t.co/dzDkQF2k2e",
+					"expanded_url": "http://smarturl.it/HYO_DEEP",
+					"display_url": "smarturl.it/HYO_DEEP",
+					"indices": [
+						226,
+						249
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1526525144398962700,
+					"id_str": "1526525144398962688",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FS9Oh-yaIAAKnlS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS9Oh-yaIAAKnlS.jpg",
+					"url": "https://t.co/O2nMGkMafc",
+					"display_url": "pic.twitter.com/O2nMGkMafc",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1526526561356480513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1462,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 485,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526525144398962700,
+					"id_str": "1526525144398962688",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FS9Oh-yaIAAKnlS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS9Oh-yaIAAKnlS.jpg",
+					"url": "https://t.co/O2nMGkMafc",
+					"display_url": "pic.twitter.com/O2nMGkMafc",
+					"expanded_url": "https://twitter.com/SMTrueThailand/status/1526526561356480513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1462,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 485,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1093456514700828700,
+			"id_str": "1093456514700828672",
+			"name": "SM True",
+			"screen_name": "SMTrueThailand",
+			"location": "Bangkok, Thailand",
+			"description": "SM True Official Twitter Page",
+			"url": "https://t.co/fe1MwqbQdx",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fe1MwqbQdx",
+							"expanded_url": "https://www.facebook.com/smtruethailand",
+							"display_url": "facebook.com/smtruethailand",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 93068,
+			"friends_count": 50,
+			"listed_count": 150,
+			"created_at": "Thu Feb 07 10:28:43 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 11145,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093456621932503041/xWi5ewdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1093456514700828672/1562743620",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 62,
+		"favorite_count": 26,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "th",
+		"text": "#HYO จะขึ้นแสดงเพลงไตเติล ‘DEEP’ บนรายการเพลงเกาหลีในสัปดาห์นี้\n\nมินิอัลบั้มแรก ‘#DEEP’ ครองอันดับ 1 บนชาร์ต Top Albums ของ iTunes ใน 10 ประเทศ!\n\n🕷 https://t.co/MliO5jDtkl\n\nMV 🎬 https://t.co/Q7H5oyBT4R\n\nมินิอัลบั้มแรก ‘DEEP’🎧 https://t.co/dzDkQF2k2e\n\n#HYO_DEEP\n#GirlsGeneration https://t.co/O2nMGkMafc"
+	},
+	{
 		"created_at": "Tue May 17 11:24:41 +0000 2022",
 		"id": 1526522345648640000,
 		"id_str": "1526524100524445701",
