75bf0b1c7ab62bb2c9141c74265b2a59c34a4f72
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-17 15:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 17 05:24:48 +0000 2022",
+		"id": 1526424307815284700,
+		"id_str": "1526433533056536576",
+		"full_text": "STAND-BY\n\n#JAURIM\n#HAPPY25thJAURIM https://t.co/OXahdCLut3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JAURIM",
+					"indices": [
+						10,
+						17
+					]
+				},
+				{
+					"text": "HAPPY25thJAURIM",
+					"indices": [
+						18,
+						34
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1526424297719611400,
+					"id_str": "1526424297719611392",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/FS7yz7oaQAAwPcX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS7yz7oaQAAwPcX.jpg",
+					"url": "https://t.co/OXahdCLut3",
+					"display_url": "pic.twitter.com/OXahdCLut3",
+					"expanded_url": "https://twitter.com/love_yuna/status/1526424307815284736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1083,
+							"h": 815,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1083,
+							"h": 815,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526424297719611400,
+					"id_str": "1526424297719611392",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/FS7yz7oaQAAwPcX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS7yz7oaQAAwPcX.jpg",
+					"url": "https://t.co/OXahdCLut3",
+					"display_url": "pic.twitter.com/OXahdCLut3",
+					"expanded_url": "https://twitter.com/love_yuna/status/1526424307815284736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1083,
+							"h": 815,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1083,
+							"h": 815,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1526424297744777200,
+					"id_str": "1526424297744777216",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/FS7yz7uaQAA_jJ-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS7yz7uaQAA_jJ-.jpg",
+					"url": "https://t.co/OXahdCLut3",
+					"display_url": "pic.twitter.com/OXahdCLut3",
+					"expanded_url": "https://twitter.com/love_yuna/status/1526424307815284736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 810,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 810,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275398,
+			"friends_count": 570,
+			"listed_count": 2784,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 387,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6692,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1651312206",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 113,
+		"favorite_count": 222,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "STAND-BY\n\n#JAURIM\n#HAPPY25thJAURIM https://t.co/OXahdCLut3"
+	},
+	{
 		"created_at": "Tue May 17 04:29:39 +0000 2022",
 		"id": 1526411391044710400,
 		"id_str": "1526419651915403264",
