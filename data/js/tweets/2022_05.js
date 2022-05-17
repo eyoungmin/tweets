e60610d54c9a5919be04a6e515038529f1c94c0d
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-17 23:02:06 +0900

    Added 1 tweets

--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 17 13:40:16 +0000 2022",
+		"id": 1526549169309364200,
+		"id_str": "1526558219224682496",
+		"full_text": "5월 16일 월요일\n지금 종료 😵😵😵\n유체이탈 \n어질어질\n졸려…\n\n#자우림스튜디오타래\nfeat. #김홍집음악감독 https://t.co/L8LF6BGtOs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "자우림스튜디오타래",
+					"indices": [
+						37,
+						47
+					]
+				},
+				{
+					"text": "김홍집음악감독",
+					"indices": [
+						54,
+						62
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1526549156223156200,
+					"id_str": "1526549156223156225",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/FS9kXpzagAE8730.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS9kXpzagAE8730.jpg",
+					"url": "https://t.co/L8LF6BGtOs",
+					"display_url": "pic.twitter.com/L8LF6BGtOs",
+					"expanded_url": "https://twitter.com/love_yuna/status/1526549169309364225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1526549156223156200,
+					"id_str": "1526549156223156225",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/FS9kXpzagAE8730.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FS9kXpzagAE8730.jpg",
+					"url": "https://t.co/L8LF6BGtOs",
+					"display_url": "pic.twitter.com/L8LF6BGtOs",
+					"expanded_url": "https://twitter.com/love_yuna/status/1526549169309364225/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1525442149172400000,
+		"in_reply_to_status_id_str": "1525442149172400128",
+		"in_reply_to_user_id": 120741919,
+		"in_reply_to_user_id_str": "120741919",
+		"in_reply_to_screen_name": "love_yuna",
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275392,
+			"friends_count": 570,
+			"listed_count": 2784,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 387,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6693,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1651312206",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 18,
+		"favorite_count": 51,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "5월 16일 월요일\n지금 종료 😵😵😵\n유체이탈 \n어질어질\n졸려…\n\n#자우림스튜디오타래\nfeat. #김홍집음악감독 https://t.co/L8LF6BGtOs"
+	},
+	{
 		"created_at": "Tue May 17 12:29:39 +0000 2022",
 		"id": 1526526561356480500,
 		"id_str": "1526540448726147077",
