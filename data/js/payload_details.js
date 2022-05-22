cda594e26e977f246f9217b0233551ef47386b66
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-23 01:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87245,
-	"created_at": "Sun May 22 15:02:06 +0000 2022",
+	"tweets": 87246,
+	"created_at": "Sun May 22 16:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sun May 22 15:40:10 +0000 2022",
+		"id": 1528394637022732300,
+		"id_str": "1528400332409143298",
+		"full_text": "BoA 보아 [LISTEN TO MY HEART (The Greatest Ver.)]\n\n🎧Melon: https://t.co/tQyZzvO44e\n🎧FLO: https://t.co/EIxauMgUmm\n🎧genie: https://t.co/hdWW8pqp1V\n🎧VIBE: https://t.co/UEHVc3B1SP\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#Listen_To_My_Heart",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			234
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						175,
+						179
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						180,
+						183
+					]
+				},
+				{
+					"text": "TheGreatest",
+					"indices": [
+						185,
+						197
+					]
+				},
+				{
+					"text": "BoA_TheGreatest",
+					"indices": [
+						198,
+						214
+					]
+				},
+				{
+					"text": "Listen_To_My_Heart",
+					"indices": [
+						215,
+						234
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tQyZzvO44e",
+					"expanded_url": "https://bit.ly/3wCRldA",
+					"display_url": "bit.ly/3wCRldA",
+					"indices": [
+						57,
+						80
+					]
+				},
+				{
+					"url": "https://t.co/EIxauMgUmm",
+					"expanded_url": "https://bit.ly/3wAgHse",
+					"display_url": "bit.ly/3wAgHse",
+					"indices": [
+						87,
+						110
+					]
+				},
+				{
+					"url": "https://t.co/hdWW8pqp1V",
+					"expanded_url": "https://bit.ly/3wCQSI0",
+					"display_url": "bit.ly/3wCQSI0",
+					"indices": [
+						119,
+						142
+					]
+				},
+				{
+					"url": "https://t.co/UEHVc3B1SP",
+					"expanded_url": "https://bit.ly/3LvHpGU",
+					"display_url": "bit.ly/3LvHpGU",
+					"indices": [
+						150,
+						173
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288694479197581300,
+			"id_str": "1288694479197581312",
+			"name": "BoA Official",
+			"screen_name": "BoA_Official",
+			"location": "",
+			"description": "🎉 축 BoA Official 계정 OPEN! 🎉 \n지난 20년간 우리와 함께 한 BoA의 모습과, 앞으로 보여줄 다양한 소식을 만나보세요! 💛",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 70791,
+			"friends_count": 1,
+			"listed_count": 271,
+			"created_at": "Thu Jul 30 04:36:37 +0000 2020",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 299,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1469321183178883076/ACp3Gstn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288694479197581312/1597027694",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 152,
+		"favorite_count": 242,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BoA 보아 [LISTEN TO MY HEART (The Greatest Ver.)]\n\n🎧Melon: https://t.co/tQyZzvO44e\n🎧FLO: https://t.co/EIxauMgUmm\n🎧genie: https://t.co/hdWW8pqp1V\n🎧VIBE: https://t.co/UEHVc3B1SP\n\n#BoA #보아 \n#TheGreatest #BoA_TheGreatest\n#Listen_To_My_Heart"
+	},
+	{
 		"created_at": "Sun May 22 14:40:10 +0000 2022",
 		"id": 1528375128031207400,
 		"id_str": "1528385232751173633",
