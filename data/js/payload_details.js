f91e44e5472519cc11bb1766d2ad8919306b1789
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-26 13:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87314,
-	"created_at": "Thu May 26 03:02:04 +0000 2022",
+	"tweets": 87315,
+	"created_at": "Thu May 26 04:02:06 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Thu May 26 03:40:13 +0000 2022",
+		"id": 1529448744332120000,
+		"id_str": "1529668701938761728",
+		"full_text": "On May 28th, 5PM KST, join HYO of GIRLS’ GENERATION’s ED LIVE Artist Class to learn the choreography of “DEEP” directly from  HYO of GIRLS’ GENERATION and receive real-time feedback from the artist herself!\n\nShe will be teaching our members her new choreography for “DEEP”. https://t.co/nTnWUqdLCs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1529448717392044000,
+					"id_str": "1529448717392044032",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FTmxgU5UYAAc2Ph.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTmxgU5UYAAc2Ph.jpg",
+					"url": "https://t.co/nTnWUqdLCs",
+					"display_url": "pic.twitter.com/nTnWUqdLCs",
+					"expanded_url": "https://twitter.com/edkpop_official/status/1529448744332120065/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1529448717392044000,
+					"id_str": "1529448717392044032",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FTmxgU5UYAAc2Ph.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTmxgU5UYAAc2Ph.jpg",
+					"url": "https://t.co/nTnWUqdLCs",
+					"display_url": "pic.twitter.com/nTnWUqdLCs",
+					"expanded_url": "https://twitter.com/edkpop_official/status/1529448744332120065/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1143774936340656100,
+			"id_str": "1143774936340656128",
+			"name": "ED KPOP Online Training",
+			"screen_name": "edkpop_official",
+			"location": "all over",
+			"description": "Experience real KPOP idol training anytime, anywhere: Dance, vocal, fitness and more classes by real trainers who have taught KPOP stars.",
+			"url": "https://t.co/r4mqZ3WCZn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/r4mqZ3WCZn",
+							"expanded_url": "http://edkpop.com",
+							"display_url": "edkpop.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5561,
+			"friends_count": 37,
+			"listed_count": 2,
+			"created_at": "Wed Jun 26 06:56:09 +0000 2019",
+			"favourites_count": 1760,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1377,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1143775421038649346/ZJsGkrjo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1143775421038649346/ZJsGkrjo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1143774936340656128/1641539596",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 522,
+		"favorite_count": 1158,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On May 28th, 5PM KST, join HYO of GIRLS’ GENERATION’s ED LIVE Artist Class to learn the choreography of “DEEP” directly from  HYO of GIRLS’ GENERATION and receive real-time feedback from the artist herself!\n\nShe will be teaching our members her new choreography for “DEEP”. https://t.co/nTnWUqdLCs"
+	},
+	{
 		"created_at": "Thu May 26 02:40:09 +0000 2022",
 		"id": 1529650916839727000,
 		"id_str": "1529653589165199360",
