e0f262ac34e8be875a42d0c309b1229d4df02297
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-27 13:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87343,
-	"created_at": "Fri May 27 03:02:07 +0000 2022",
+	"tweets": 87344,
+	"created_at": "Fri May 27 04:02:08 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 27 03:40:10 +0000 2022",
+		"id": 1530021011554308000,
+		"id_str": "1530031077275877383",
+		"full_text": "[#퀸덤2]파이널 음원 안내👑\n\n💿 𝗧𝗥𝗔𝗖𝗞 𝗟𝗜𝗦𝗧 \n1. Waka Boom (feat.이영지)-효린\n2. AURA-우주소녀 \n3. THE GIRLS-케플러\n4. 환상-비비지\n5. POSE-이달의 소녀\n6. Whistle-브레이브걸스\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 매주 목요일 밤 9시 20분\n\n#QUEENDOM2 #엠넷 https://t.co/xkwiWPBtTY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			200
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "퀸덤2",
+					"indices": [
+						1,
+						5
+					]
+				},
+				{
+					"text": "QUEENDOM2",
+					"indices": [
+						186,
+						196
+					]
+				},
+				{
+					"text": "엠넷",
+					"indices": [
+						197,
+						200
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1530005142862303200,
+					"id_str": "1530005142862303232",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/FTurkjTUEAAMo69.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTurkjTUEAAMo69.jpg",
+					"url": "https://t.co/xkwiWPBtTY",
+					"display_url": "pic.twitter.com/xkwiWPBtTY",
+					"expanded_url": "https://twitter.com/MnetKR/status/1530021011554308097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1530005142862303200,
+					"id_str": "1530005142862303232",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/FTurkjTUEAAMo69.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTurkjTUEAAMo69.jpg",
+					"url": "https://t.co/xkwiWPBtTY",
+					"display_url": "pic.twitter.com/xkwiWPBtTY",
+					"expanded_url": "https://twitter.com/MnetKR/status/1530021011554308097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1530005167738790000,
+					"id_str": "1530005167738789893",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/FTurl_-VIAUQPxR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTurl_-VIAUQPxR.jpg",
+					"url": "https://t.co/xkwiWPBtTY",
+					"display_url": "pic.twitter.com/xkwiWPBtTY",
+					"expanded_url": "https://twitter.com/MnetKR/status/1530021011554308097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1530005177788338200,
+					"id_str": "1530005177788338176",
+					"indices": [
+						201,
+						224
+					],
+					"media_url": "http://pbs.twimg.com/media/FTurmlaVEAAwH8c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTurmlaVEAAwH8c.jpg",
+					"url": "https://t.co/xkwiWPBtTY",
+					"display_url": "pic.twitter.com/xkwiWPBtTY",
+					"expanded_url": "https://twitter.com/MnetKR/status/1530021011554308097/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 128466223,
+			"id_str": "128466223",
+			"name": "Mnet",
+			"screen_name": "MnetKR",
+			"location": "Seoul, Korea",
+			"description": "Official Twitter of Mnet, Asia`s #1 Music Channel.",
+			"url": "http://t.co/596v5W1yWN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/596v5W1yWN",
+							"expanded_url": "http://www.mnet.com",
+							"display_url": "mnet.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3064003,
+			"friends_count": 20944,
+			"listed_count": 3021,
+			"created_at": "Thu Apr 01 05:53:04 +0000 2010",
+			"favourites_count": 461,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 29660,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1272335418474721280/wWS9b90j_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/128466223/1592183300",
+			"profile_link_color": "E40076",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F5DCE7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4485,
+		"favorite_count": 9812,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#퀸덤2]파이널 음원 안내👑\n\n💿 𝗧𝗥𝗔𝗖𝗞 𝗟𝗜𝗦𝗧 \n1. Waka Boom (feat.이영지)-효린\n2. AURA-우주소녀 \n3. THE GIRLS-케플러\n4. 환상-비비지\n5. POSE-이달의 소녀\n6. Whistle-브레이브걸스\n\nK-POP 대표 걸그룹들의 글로벌 동시 컴백 전쟁!\n〈퀸덤2〉 매주 목요일 밤 9시 20분\n\n#QUEENDOM2 #엠넷 https://t.co/xkwiWPBtTY"
+	},
+	{
 		"created_at": "Fri May 27 02:40:09 +0000 2022",
 		"id": 1530006703043129300,
 		"id_str": "1530015976598667264",
