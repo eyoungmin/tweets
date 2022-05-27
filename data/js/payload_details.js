813c9fa1d1f28cb53abfe0bbe632f18ede51741d
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-27 15:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87348,
-	"created_at": "Fri May 27 05:02:07 +0000 2022",
+	"tweets": 87349,
+	"created_at": "Fri May 27 06:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Fri May 27 05:40:14 +0000 2022",
+		"id": 1530048741054394400,
+		"id_str": "1530061295562674178",
+		"full_text": "에스파, 美 포브스 ‘아시아 30세 이하 리더 30인’ 선정! K팝 아티스트 유일!\n\naespa is the only K-pop artist to place on the U.S. Forbes '30 Under 30 Asia 2022’ list! \n\nhttps://t.co/uvgVTr4mKL\n\n#aespa #æspa #에스파 https://t.co/RbXzHIBOyQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						164,
+						170
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						171,
+						176
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						177,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uvgVTr4mKL",
+					"expanded_url": "https://bit.ly/3LREecK",
+					"display_url": "bit.ly/3LREecK",
+					"indices": [
+						139,
+						162
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1530048244671082500,
+					"id_str": "1530048244671082496",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/FTvSxaDUcAAXb7i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTvSxaDUcAAXb7i.jpg",
+					"url": "https://t.co/RbXzHIBOyQ",
+					"display_url": "pic.twitter.com/RbXzHIBOyQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1530048741054394369/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1530048244671082500,
+					"id_str": "1530048244671082496",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/FTvSxaDUcAAXb7i.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTvSxaDUcAAXb7i.jpg",
+					"url": "https://t.co/RbXzHIBOyQ",
+					"display_url": "pic.twitter.com/RbXzHIBOyQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1530048741054394369/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1530048244662689800,
+					"id_str": "1530048244662689792",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/FTvSxaBUYAABWRu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FTvSxaBUYAABWRu.jpg",
+					"url": "https://t.co/RbXzHIBOyQ",
+					"display_url": "pic.twitter.com/RbXzHIBOyQ",
+					"expanded_url": "https://twitter.com/aespa_official/status/1530048741054394369/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 789,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1159,
+							"h": 1762,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 447,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Dreams Come True",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2650651,
+			"friends_count": 1,
+			"listed_count": 8440,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1442,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1471042359995498497/yQFDcdzy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1639558806",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6254,
+		"favorite_count": 18202,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "에스파, 美 포브스 ‘아시아 30세 이하 리더 30인’ 선정! K팝 아티스트 유일!\n\naespa is the only K-pop artist to place on the U.S. Forbes '30 Under 30 Asia 2022’ list! \n\nhttps://t.co/uvgVTr4mKL\n\n#aespa #æspa #에스파 https://t.co/RbXzHIBOyQ"
+	},
+	{
 		"created_at": "Fri May 27 04:40:38 +0000 2022",
 		"id": 1530039999093444600,
 		"id_str": "1530046297209806849",
