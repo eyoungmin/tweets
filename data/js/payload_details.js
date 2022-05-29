a560975b5decbb2b0ac993b00db5f55879fa7cd8
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-29 21:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87381,
-	"created_at": "Sun May 29 11:02:07 +0000 2022",
+	"tweets": 87382,
+	"created_at": "Sun May 29 12:02:06 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sun May 29 11:10:53 +0000 2022",
+		"id": 1530868646503129000,
+		"id_str": "1530869280564162560",
+		"full_text": "#OFFICIALBLINK\n\n띵-동🛎\nBLINK에게 남긴 로제의 보이스 메일이\n지금 BLINK MEMBERSHIP에 도착했습니다😉\n바로 확인해 보세요! 💌\n\n▶️ https://t.co/Xp2AY36wPJ\n\n#ROSÉ #로제 #BLACKPINK #블랙핑크\n#BLINK #블링크 #BLACKPINKVOICEMAIL📻",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OFFICIALBLINK",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "ROSÉ",
+					"indices": [
+						116,
+						121
+					]
+				},
+				{
+					"text": "로제",
+					"indices": [
+						122,
+						125
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						126,
+						136
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						137,
+						142
+					]
+				},
+				{
+					"text": "BLINK",
+					"indices": [
+						143,
+						149
+					]
+				},
+				{
+					"text": "블링크",
+					"indices": [
+						150,
+						154
+					]
+				},
+				{
+					"text": "BLACKPINKVOICEMAIL",
+					"indices": [
+						155,
+						174
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Xp2AY36wPJ",
+					"expanded_url": "https://www.weverse.io/blackpink/only/16142",
+					"display_url": "weverse.io/blackpink/only…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/2Kh5HZQu7Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2Kh5HZQu7Y",
+							"expanded_url": "http://blackpinkofficial.com",
+							"display_url": "blackpinkofficial.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8154570,
+			"friends_count": 9,
+			"listed_count": 9009,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 51,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2078,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1631247166",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1384,
+		"favorite_count": 5688,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#OFFICIALBLINK\n\n띵-동🛎\nBLINK에게 남긴 로제의 보이스 메일이\n지금 BLINK MEMBERSHIP에 도착했습니다😉\n바로 확인해 보세요! 💌\n\n▶️ https://t.co/Xp2AY36wPJ\n\n#ROSÉ #로제 #BLACKPINK #블랙핑크\n#BLINK #블링크 #BLACKPINKVOICEMAIL📻"
+	},
+	{
 		"created_at": "Sun May 29 09:40:10 +0000 2022",
 		"id": 1530836347098869800,
 		"id_str": "1530846451265323008",
