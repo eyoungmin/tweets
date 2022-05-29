91f066d5d7ba1bdb7a71af14c4e48d3be0c39cf9
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-30 05:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87382,
-	"created_at": "Sun May 29 12:02:06 +0000 2022",
+	"tweets": 87383,
+	"created_at": "Sun May 29 20:02:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Sun May 29 19:40:10 +0000 2022",
+		"id": 1530988084720029700,
+		"id_str": "1530997445643366400",
+		"full_text": "Genuinely can't believe how lucky we are to do this as a job. Meeting so many of you on this tour, we are so humbled by your use of our songs to soundtrack your weddings, road trips, family breakfasts, low moments where you want to give up, ethereal highs... it's unreal 🙏💫 https://t.co/SP8OFNJ5PX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1530988073877745700,
+					"id_str": "1530988073877745669",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FT8piqrXsAUPHUC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT8piqrXsAUPHUC.jpg",
+					"url": "https://t.co/SP8OFNJ5PX",
+					"display_url": "pic.twitter.com/SP8OFNJ5PX",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1530988084720029698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1092,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1092,
+							"h": 1092,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1530988073877745700,
+					"id_str": "1530988073877745669",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FT8piqrXsAUPHUC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FT8piqrXsAUPHUC.jpg",
+					"url": "https://t.co/SP8OFNJ5PX",
+					"display_url": "pic.twitter.com/SP8OFNJ5PX",
+					"expanded_url": "https://twitter.com/OhWonderMusic/status/1530988084720029698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1092,
+							"h": 1092,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1092,
+							"h": 1092,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2740435740,
+			"id_str": "2740435740",
+			"name": "Oh Wonder",
+			"screen_name": "OhWonderMusic",
+			"location": "London",
+			"description": "Our new song 'True Romance' is out now",
+			"url": "https://t.co/aWDekOmHRR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aWDekOmHRR",
+							"expanded_url": "https://linktr.ee/ohwondermusic",
+							"display_url": "linktr.ee/ohwondermusic",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 166522,
+			"friends_count": 203,
+			"listed_count": 427,
+			"created_at": "Sun Aug 17 20:45:13 +0000 2014",
+			"favourites_count": 3567,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 5412,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501967196595048451/sXXwT4h1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2740435740/1652968649",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 149,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Genuinely can't believe how lucky we are to do this as a job. Meeting so many of you on this tour, we are so humbled by your use of our songs to soundtrack your weddings, road trips, family breakfasts, low moments where you want to give up, ethereal highs... it's unreal 🙏💫 https://t.co/SP8OFNJ5PX"
+	},
+	{
 		"created_at": "Sun May 29 11:10:53 +0000 2022",
 		"id": 1530868646503129000,
 		"id_str": "1530869280564162560",
