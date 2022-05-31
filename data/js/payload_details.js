4f627eb9de1e61b07b4875c4d1774b0c6b08bb99
Youngmin Lee <iameyoungmin@gmail.com>
2022-05-31 16:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87422,
-	"created_at": "Tue May 31 06:02:05 +0000 2022",
+	"tweets": 87423,
+	"created_at": "Tue May 31 07:02:08 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 31 06:44:33 +0000 2022",
+		"id": 1531515433823895600,
+		"id_str": "1531527031464243201",
+		"full_text": "화면 뚫고 나오는 크리스탈의 인생네컷✨\n\n✔ &lt;아레나&gt; 6월호 '뜨겁게 정수정'\nhttps://t.co/F8QSW7EmpC\n\n#KRYSTAL #JUNGSOOJUNG https://t.co/siq2cZnAHK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KRYSTAL",
+					"indices": [
+						76,
+						84
+					]
+				},
+				{
+					"text": "JUNGSOOJUNG",
+					"indices": [
+						85,
+						97
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/F8QSW7EmpC",
+					"expanded_url": "https://smlounge.co.kr/arena/article/51041",
+					"display_url": "smlounge.co.kr/arena/article/…",
+					"indices": [
+						51,
+						74
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1531515356157997000,
+					"id_str": "1531515356157997056",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1531515356157997056/pu/img/qjybr2LwZaAZm_o8.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1531515356157997056/pu/img/qjybr2LwZaAZm_o8.jpg",
+					"url": "https://t.co/siq2cZnAHK",
+					"display_url": "pic.twitter.com/siq2cZnAHK",
+					"expanded_url": "https://twitter.com/arenahommeplus/status/1531515433823895552/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 864,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 864,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1531515356157997000,
+					"id_str": "1531515356157997056",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1531515356157997056/pu/img/qjybr2LwZaAZm_o8.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1531515356157997056/pu/img/qjybr2LwZaAZm_o8.jpg",
+					"url": "https://t.co/siq2cZnAHK",
+					"display_url": "pic.twitter.com/siq2cZnAHK",
+					"expanded_url": "https://twitter.com/arenahommeplus/status/1531515433823895552/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 864,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 864,
+							"h": 1080,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 7907,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1531515356157997056/pu/vid/720x900/Y4nrtVvqfV3KWTb2.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1531515356157997056/pu/vid/480x600/SJrmoEnSoIPn5uM9.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1531515356157997056/pu/pl/vYdTyfswjUNsLTW0.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1531515356157997056/pu/vid/320x400/_GrzgVb3fgKktWAx.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 127117031,
+			"id_str": "127117031",
+			"name": "아레나옴므플러스코리아",
+			"screen_name": "arenahommeplus",
+			"location": "Seoul, Korea",
+			"description": "Arena Homme+ Korea\nMen's Fashion & lifestyle Magazine \nFashion & Style, Success, Beauty, Accessories, Car, Tech, Liquor and Party",
+			"url": "https://t.co/S1NqQ7ixBi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/S1NqQ7ixBi",
+							"expanded_url": "https://www.smlounge.co.kr/arena",
+							"display_url": "smlounge.co.kr/arena",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 21538,
+			"friends_count": 0,
+			"listed_count": 95,
+			"created_at": "Sun Mar 28 03:26:36 +0000 2010",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6205,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1251074669043568640/enbNxDrB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1251074669043568640/enbNxDrB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/127117031/1652919747",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 105,
+		"favorite_count": 169,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "화면 뚫고 나오는 크리스탈의 인생네컷✨\n\n✔ &lt;아레나&gt; 6월호 '뜨겁게 정수정'\nhttps://t.co/F8QSW7EmpC\n\n#KRYSTAL #JUNGSOOJUNG https://t.co/siq2cZnAHK"
+	},
+	{
 		"created_at": "Tue May 31 05:40:38 +0000 2022",
 		"id": 1531508208325492700,
 		"id_str": "1531510947428327434",
