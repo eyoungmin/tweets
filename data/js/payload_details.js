a24f1eed50870adcfd79db04677e62b19124d1dc
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-02 08:02:00 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87453,
-	"created_at": "Wed Jun 01 22:02:00 +0000 2022",
+	"tweets": 87454,
+	"created_at": "Wed Jun 01 23:01:59 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 01 22:40:09 +0000 2022",
+		"id": 1531885354524893200,
+		"id_str": "1532129906209038338",
+		"full_text": "Saw @OhWonderMusic tonight easily one of the best shows I’ve ever seen in my life",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OhWonderMusic",
+					"name": "Oh Wonder",
+					"id": 2740435740,
+					"id_str": "2740435740",
+					"indices": [
+						4,
+						18
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 436782436,
+			"id_str": "436782436",
+			"name": "saintvicious",
+			"screen_name": "LolStvicious",
+			"location": "",
+			"description": "@teamliquid , https://t.co/d6FhyvQvTC , Caster, Player, League of Legends, TFT partnerships@teamliquid.net for business inquiries",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/d6FhyvQvTC",
+							"expanded_url": "http://Twitch.tv/saintvicious",
+							"display_url": "Twitch.tv/saintvicious",
+							"indices": [
+								14,
+								37
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 149140,
+			"friends_count": 726,
+			"listed_count": 767,
+			"created_at": "Wed Dec 14 15:33:44 +0000 2011",
+			"favourites_count": 3674,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 20209,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1478388117161877516/9Ytl2gCW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1478388117161877516/9Ytl2gCW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/436782436/1643845407",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 67,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Saw @OhWonderMusic tonight easily one of the best shows I’ve ever seen in my life"
+	},
+	{
 		"created_at": "Wed Jun 01 21:40:10 +0000 2022",
 		"id": 1532105999267553300,
 		"id_str": "1532114807884812293",
