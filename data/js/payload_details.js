eb55e591a29ceaf26b1bfab7e3ae471e9d0ac86a
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-01 13:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87436,
-	"created_at": "Wed Jun 01 03:02:04 +0000 2022",
+	"tweets": 87437,
+	"created_at": "Wed Jun 01 04:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,118 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 01 03:40:14 +0000 2022",
+		"id": 1531829798259282000,
+		"id_str": "1531843034018635777",
+		"full_text": "SM signs a partnership contract with Warner Records for distribution and marketing promotions of aespa's music in the U.S.! \n\n#aespa to release their English single ‘Life’s Too Short’ on June 24th prior to their their 2nd mini album comeback on July 8th! \n\n#æspa #에스파 #Girls",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						126,
+						132
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						257,
+						262
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						263,
+						267
+					]
+				},
+				{
+					"text": "Girls",
+					"indices": [
+						268,
+						274
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1531829793440006100,
+		"in_reply_to_status_id_str": "1531829793440006144",
+		"in_reply_to_user_id": 1277453652924366800,
+		"in_reply_to_user_id_str": "1277453652924366848",
+		"in_reply_to_screen_name": "aespa_official",
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Girls",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2678005,
+			"friends_count": 2,
+			"listed_count": 8481,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1447,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1654009204",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4902,
+		"favorite_count": 15043,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "SM signs a partnership contract with Warner Records for distribution and marketing promotions of aespa's music in the U.S.! \n\n#aespa to release their English single ‘Life’s Too Short’ on June 24th prior to their their 2nd mini album comeback on July 8th! \n\n#æspa #에스파 #Girls"
+	},
+	{
 		"created_at": "Wed Jun 01 02:40:10 +0000 2022",
 		"id": 1531822054332571600,
 		"id_str": "1531827918229454848",
