77c6aeb4322333543d0b355ca818fdc4a7a02961
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-03 08:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87480,
-	"created_at": "Thu Jun 02 16:02:01 +0000 2022",
+	"tweets": 87481,
+	"created_at": "Thu Jun 02 23:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Thu Jun 02 22:40:12 +0000 2022",
+		"id": 1532490346734579700,
+		"id_str": "1532492303356645396",
+		"full_text": "@nakedbi_bing 오잉 https://t.co/C3K4YKikNv",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nakedbi_bing",
+					"name": "빙빙:",
+					"id": 1373120381888782300,
+					"id_str": "1373120381888782336",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1532490343005925400,
+					"id_str": "1532490343005925376",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FUR_2PSVsAAJ1Ll.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUR_2PSVsAAJ1Ll.jpg",
+					"url": "https://t.co/C3K4YKikNv",
+					"display_url": "pic.twitter.com/C3K4YKikNv",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1532490346734579712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1201,
+							"h": 1631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 884,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 501,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1532490343005925400,
+					"id_str": "1532490343005925376",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/FUR_2PSVsAAJ1Ll.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUR_2PSVsAAJ1Ll.jpg",
+					"url": "https://t.co/C3K4YKikNv",
+					"display_url": "pic.twitter.com/C3K4YKikNv",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1532490346734579712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1201,
+							"h": 1631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 884,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 501,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1457302855216361500,
+		"in_reply_to_status_id_str": "1457302855216361472",
+		"in_reply_to_user_id": 1373120381888782300,
+		"in_reply_to_user_id_str": "1373120381888782336",
+		"in_reply_to_screen_name": "nakedbi_bing",
+		"user": {
+			"id": 1448497121867436000,
+			"id_str": "1448497121867436037",
+			"name": "nakedbibi",
+			"screen_name": "nakedbibi",
+			"location": "",
+			"description": "BIBI💧💦",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 59354,
+			"friends_count": 2,
+			"listed_count": 157,
+			"created_at": "Thu Oct 14 03:53:27 +0000 2021",
+			"favourites_count": 37,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1517974570023628800/j8sH9b3L_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1517974570023628800/j8sH9b3L_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1448497121867436037/1650748326",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 53,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "@nakedbi_bing 오잉 https://t.co/C3K4YKikNv"
+	},
+	{
 		"created_at": "Thu Jun 02 15:45:41 +0000 2022",
 		"id": 1532387830001201200,
 		"id_str": "1532387987069317121",
