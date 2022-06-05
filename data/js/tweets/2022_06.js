11434fce1cb1df0effd26da9e5285379d89885b9
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-05 23:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sun Jun 05 13:09:08 +0000 2022",
+		"id": 1533433597977567200,
+		"id_str": "1533435753073479680",
+		"full_text": "Concept Photo - \"HyunJin\" \n\nLOOΠΔ Summer Special Mini Album [Flip That]\n2022.06.20 6PM [KST] Release \n\n#이달의소녀 #현진 #LOONA #HyunJin #FlipThat\n\nLOOΠΔ\nhttps://t.co/5CfbXHR54V https://t.co/c7yZwOiHOQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						103,
+						109
+					]
+				},
+				{
+					"text": "현진",
+					"indices": [
+						110,
+						113
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						114,
+						120
+					]
+				},
+				{
+					"text": "HyunJin",
+					"indices": [
+						121,
+						129
+					]
+				},
+				{
+					"text": "FlipThat",
+					"indices": [
+						130,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5CfbXHR54V",
+					"expanded_url": "http://www.loonatheworld.com",
+					"display_url": "loonatheworld.com",
+					"indices": [
+						147,
+						170
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1533433594949271600,
+					"id_str": "1533433594949271552",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FUfZuumaAAAXCG1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUfZuumaAAAXCG1.jpg",
+					"url": "https://t.co/c7yZwOiHOQ",
+					"display_url": "pic.twitter.com/c7yZwOiHOQ",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1533433597977567233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1533433594949271600,
+					"id_str": "1533433594949271552",
+					"indices": [
+						171,
+						194
+					],
+					"media_url": "http://pbs.twimg.com/media/FUfZuumaAAAXCG1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUfZuumaAAAXCG1.jpg",
+					"url": "https://t.co/c7yZwOiHOQ",
+					"display_url": "pic.twitter.com/c7yZwOiHOQ",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1533433597977567233/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 917140,
+			"friends_count": 2,
+			"listed_count": 8696,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4314,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1624948256",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13943,
+		"favorite_count": 29196,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Concept Photo - \"HyunJin\" \n\nLOOΠΔ Summer Special Mini Album [Flip That]\n2022.06.20 6PM [KST] Release \n\n#이달의소녀 #현진 #LOONA #HyunJin #FlipThat\n\nLOOΠΔ\nhttps://t.co/5CfbXHR54V https://t.co/c7yZwOiHOQ"
+	},
+	{
 		"created_at": "Sun Jun 05 07:45:37 +0000 2022",
 		"id": 1533353477811806200,
 		"id_str": "1533354337698779136",
