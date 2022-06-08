10cd602ad8ba3602f144930668a8b5478d443a71
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-08 20:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 08 10:29:39 +0000 2022",
+		"id": 1534472803944083500,
+		"id_str": "1534482782801514496",
+		"full_text": "[#🌳] 행운의 슬비 설명서ʕ๑•̀ᴗ-ʔ\n▶ https://t.co/etwPmc1YQF\n \n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/98WRBHKnCY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						51,
+						54
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						55,
+						63
+					]
+				},
+				{
+					"text": "NAMOOCONTENTS",
+					"indices": [
+						64,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/etwPmc1YQF",
+					"expanded_url": "http://naver.me/xcu3B8UQ",
+					"display_url": "naver.me/xcu3B8UQ",
+					"indices": [
+						25,
+						48
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1534400587059839000,
+					"id_str": "1534400587059838976",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/FUtJNE7aQAA4AYz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUtJNE7aQAA4AYz.jpg",
+					"url": "https://t.co/98WRBHKnCY",
+					"display_url": "pic.twitter.com/98WRBHKnCY",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1534472803944083457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1534400587059839000,
+					"id_str": "1534400587059838976",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/FUtJNE7aQAA4AYz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FUtJNE7aQAA4AYz.jpg",
+					"url": "https://t.co/98WRBHKnCY",
+					"display_url": "pic.twitter.com/98WRBHKnCY",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1534472803944083457/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60464,
+			"friends_count": 12,
+			"listed_count": 269,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15731,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 225,
+		"favorite_count": 373,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#🌳] 행운의 슬비 설명서ʕ๑•̀ᴗ-ʔ\n▶ https://t.co/etwPmc1YQF\n \n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/98WRBHKnCY"
+	},
+	{
 		"created_at": "Wed Jun 08 09:45:44 +0000 2022",
 		"id": 1534458125193203700,
 		"id_str": "1534471733180252160",
@@ -393,7 +599,7 @@ Grailbird.data.tweets_2022_06 =
 		"created_at": "Wed Jun 08 09:40:17 +0000 2022",
 		"id": 1534460222865125400,
 		"id_str": "1534470361537994754",
-		"full_text": "[📢]\n\naespa OFFICIAL MAIL MAGAZINEにご登録のみなさんへ、本日はWINTERのメッセージを公開！⭐️\nぜひチェックしてください🧚🏻‍♀️✨\n\n※メッセージ動画は下記サイトにログイン後、「NEWS」よりご覧ください👀\nhttps://t.co/mLUyzoaE2v\n\n#aespa #æspa #에스파 #エスパ\n#WINTER",
+		"full_text": "[📢]\n\naespa OFFICIAL MAIL MAGAZINEにご登録のみなさんへ、本日はWINTERのメッセージを公開！⭐️\nぜひチェックしてください🧚🏻♀️✨\n\n※メッセージ動画は下記サイトにログイン後、「NEWS」よりご覧ください👀\nhttps://t.co/mLUyzoaE2v\n\n#aespa #æspa #에스파 #エスパ\n#WINTER",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -530,7 +736,7 @@ Grailbird.data.tweets_2022_06 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ja",
-		"text": "[📢]\n\naespa OFFICIAL MAIL MAGAZINEにご登録のみなさんへ、本日はWINTERのメッセージを公開！⭐️\nぜひチェックしてください🧚🏻‍♀️✨\n\n※メッセージ動画は下記サイトにログイン後、「NEWS」よりご覧ください👀\nhttps://t.co/mLUyzoaE2v\n\n#aespa #æspa #에스파 #エスパ\n#WINTER"
+		"text": "[📢]\n\naespa OFFICIAL MAIL MAGAZINEにご登録のみなさんへ、本日はWINTERのメッセージを公開！⭐️\nぜひチェックしてください🧚🏻♀️✨\n\n※メッセージ動画は下記サイトにログイン後、「NEWS」よりご覧ください👀\nhttps://t.co/mLUyzoaE2v\n\n#aespa #æspa #에스파 #エスパ\n#WINTER"
 	},
 	{
 		"created_at": "Wed Jun 08 08:40:39 +0000 2022",
