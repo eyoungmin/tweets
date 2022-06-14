b89c7ec5b1d640714f549644f3dd97e84b780db0
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-14 22:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87681,
-	"created_at": "Tue Jun 14 12:02:04 +0000 2022",
+	"tweets": 87682,
+	"created_at": "Tue Jun 14 13:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 14 12:40:13 +0000 2022",
+		"id": 1536682035082891300,
+		"id_str": "1536689966897631232",
+		"full_text": "The Queen @CHUNGHA_MNHent is sending love to you💜\n\nIt’s not 12AM yet so get ready to fall into the #KillingMe charming of #CHUNGHA at MIK FESTIVAL on the 30th July in Southwark Park, London💗\n\nGrab your tickets, dress up and come to become a K-POP Insider with the MIK style🫧 https://t.co/DWZlvRulkP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KillingMe",
+					"indices": [
+						99,
+						109
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						122,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CHUNGHA_MNHent",
+					"name": "CHUNG HA",
+					"id": 1097557125742489600,
+					"id_str": "1097557125742489600",
+					"indices": [
+						10,
+						25
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1536681815720833000,
+					"id_str": "1536681815720833029",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1536681815720833029/pu/img/o5w9rN9FVuuxeE86.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1536681815720833029/pu/img/o5w9rN9FVuuxeE86.jpg",
+					"url": "https://t.co/DWZlvRulkP",
+					"display_url": "pic.twitter.com/DWZlvRulkP",
+					"expanded_url": "https://twitter.com/mikfestival/status/1536682035082891265/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1536681815720833000,
+					"id_str": "1536681815720833029",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1536681815720833029/pu/img/o5w9rN9FVuuxeE86.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1536681815720833029/pu/img/o5w9rN9FVuuxeE86.jpg",
+					"url": "https://t.co/DWZlvRulkP",
+					"display_url": "pic.twitter.com/DWZlvRulkP",
+					"expanded_url": "https://twitter.com/mikfestival/status/1536682035082891265/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 33960,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1536681815720833029/pu/vid/480x270/c3NUH4221wVejgGp.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1536681815720833029/pu/pl/hEfmDTlxQIPZSknf.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1536681815720833029/pu/vid/1280x720/rR0XchfcBRHZux9d.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1536681815720833029/pu/vid/640x360/h_tnjpSCnIR_lZPx.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1513545422404468700,
+			"id_str": "1513545422404468743",
+			"name": "MIK FESTIVAL",
+			"screen_name": "mikfestival",
+			"location": "",
+			"description": "The first outdoor Korean music festival in Europe🔥 Let’s embrace nature with the HOTTEST K-pop artists on 30th and 31st of July in Southwark Park, London🇬🇧",
+			"url": "https://t.co/jLPfR57wV8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jLPfR57wV8",
+							"expanded_url": "http://www.mikfestival.com",
+							"display_url": "mikfestival.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8119,
+			"friends_count": 20,
+			"listed_count": 4,
+			"created_at": "Mon Apr 11 15:52:59 +0000 2022",
+			"favourites_count": 95,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 93,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1515340718197329920/riWAxX3u_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1515340718197329920/riWAxX3u_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1513545422404468743/1650101386",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 141,
+		"favorite_count": 472,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Queen @CHUNGHA_MNHent is sending love to you💜\n\nIt’s not 12AM yet so get ready to fall into the #KillingMe charming of #CHUNGHA at MIK FESTIVAL on the 30th July in Southwark Park, London💗\n\nGrab your tickets, dress up and come to become a K-POP Insider with the MIK style🫧 https://t.co/DWZlvRulkP"
+	},
+	{
 		"created_at": "Tue Jun 14 11:40:09 +0000 2022",
 		"id": 1536664905968799700,
 		"id_str": "1536674854086262784",
