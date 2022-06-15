1c5939de4021e0e88bea607e4f9cb1ae72b6e6ba
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-16 04:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Wed Jun 15 18:40:12 +0000 2022",
+		"id": 1537139742357475300,
+		"id_str": "1537142948021821443",
+		"full_text": "꺅!!!!!!!! \n믹싱 끝났다아아아아!!!!!!!!\n나 오늘 22시간 일 했어어어어!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274940,
+			"friends_count": 577,
+			"listed_count": 2779,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6704,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1520339655363002369/4PaeqXgP_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1651312206",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 96,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "꺅!!!!!!!! \n믹싱 끝났다아아아아!!!!!!!!\n나 오늘 22시간 일 했어어어어!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n🖤🖤🖤🖤🖤🖤🖤🖤🖤🖤"
+	},
+	{
 		"created_at": "Wed Jun 15 15:40:37 +0000 2022",
 		"id": 1537093352620929000,
 		"id_str": "1537097755096715266",
