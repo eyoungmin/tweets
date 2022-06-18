48afb503d4ce587736dc06f2b4ff9d686ce9b78b
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-18 12:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 18 02:40:10 +0000 2022",
+		"id": 1537978422114144300,
+		"id_str": "1537988511730524161",
+		"full_text": "✨[LOONATHEWORLD] Meet &amp; Greet Event in US✨\n\nTICKETS OPEN NOW 🎟️\n(*Mexico City does not apply - see thread)\n\n🌌Come see #LOONA after the show\nfor a hi-touch, special benefits, and more!\n\nGET YOUR US🇺🇸 M&amp;G PASSES HERE🔽\n📎https://t.co/2aLU8XLZ9I\n\n#LOONA #이달의소녀 #LOONATHEWORLD https://t.co/2d5JXw4SFw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						122,
+						128
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						250,
+						256
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						257,
+						263
+					]
+				},
+				{
+					"text": "LOONATHEWORLD",
+					"indices": [
+						264,
+						278
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2aLU8XLZ9I",
+					"expanded_url": "https://mmt.fans/b1RY",
+					"display_url": "mmt.fans/b1RY",
+					"indices": [
+						225,
+						248
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1537978417924018200,
+					"id_str": "1537978417924018176",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FVf_OPiUEAACrS7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVf_OPiUEAACrS7.jpg",
+					"url": "https://t.co/2d5JXw4SFw",
+					"display_url": "pic.twitter.com/2d5JXw4SFw",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1537978422114144258/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1537978417924018200,
+					"id_str": "1537978417924018176",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FVf_OPiUEAACrS7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVf_OPiUEAACrS7.jpg",
+					"url": "https://t.co/2d5JXw4SFw",
+					"display_url": "pic.twitter.com/2d5JXw4SFw",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1537978422114144258/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://typefully.com/\" rel=\"nofollow\">Typefully</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIyOkW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIyOkW",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 148687,
+			"friends_count": 103,
+			"listed_count": 320,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29136,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1525038851755892736/qc8dyM_J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1525038851755892736/qc8dyM_J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 270,
+		"favorite_count": 1658,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "✨[LOONATHEWORLD] Meet &amp; Greet Event in US✨\n\nTICKETS OPEN NOW 🎟️\n(*Mexico City does not apply - see thread)\n\n🌌Come see #LOONA after the show\nfor a hi-touch, special benefits, and more!\n\nGET YOUR US🇺🇸 M&amp;G PASSES HERE🔽\n📎https://t.co/2aLU8XLZ9I\n\n#LOONA #이달의소녀 #LOONATHEWORLD https://t.co/2d5JXw4SFw"
+	},
+	{
 		"created_at": "Fri Jun 17 23:40:10 +0000 2022",
 		"id": 1537933291793555500,
 		"id_str": "1537943212739817472",
