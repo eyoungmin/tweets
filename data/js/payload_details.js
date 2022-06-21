d7ce924e72a05ab5a7cc34396db83794dbfa1bc6
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-22 02:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87829,
-	"created_at": "Tue Jun 21 16:02:06 +0000 2022",
+	"tweets": 87830,
+	"created_at": "Tue Jun 21 17:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Tue Jun 21 16:45:37 +0000 2022",
+		"id": 1539277910460145700,
+		"id_str": "1539288439304204291",
+		"full_text": "#GeniusCharts | LOONA takes the hottest debut on Genius Korea charts as “Flip That” lands at #1 on the K-Pop chart &amp; “Need U” debuts at #3 on the K-R&amp;B chart! \n\n“Pale Blue Dot,” “Pose,” and “Playback” also debuts in the Top 20 of the K-Pop chart!\n\n#LOONA_FLIPTHAT @loonatheworld https://t.co/TRvEfDNbkd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			286
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GeniusCharts",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"text": "LOONA_FLIPTHAT",
+					"indices": [
+						256,
+						271
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "loonatheworld",
+					"name": "이달의 소녀(LOOΠΔ)",
+					"id": 770959712791597000,
+					"id_str": "770959712791597056",
+					"indices": [
+						272,
+						286
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1539277894291050500,
+					"id_str": "1539277894291050497",
+					"indices": [
+						287,
+						310
+					],
+					"media_url": "http://pbs.twimg.com/media/FVydFuPUUAEcQ6-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVydFuPUUAEcQ6-.jpg",
+					"url": "https://t.co/TRvEfDNbkd",
+					"display_url": "pic.twitter.com/TRvEfDNbkd",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1539277910460145664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1539277894291050500,
+					"id_str": "1539277894291050497",
+					"indices": [
+						287,
+						310
+					],
+					"media_url": "http://pbs.twimg.com/media/FVydFuPUUAEcQ6-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVydFuPUUAEcQ6-.jpg",
+					"url": "https://t.co/TRvEfDNbkd",
+					"display_url": "pic.twitter.com/TRvEfDNbkd",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1539277910460145664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2000,
+							"h": 2000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1539277894249091000,
+					"id_str": "1539277894249091072",
+					"indices": [
+						287,
+						310
+					],
+					"media_url": "http://pbs.twimg.com/media/FVydFuFUEAAj-UB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVydFuFUEAAj-UB.jpg",
+					"url": "https://t.co/TRvEfDNbkd",
+					"display_url": "pic.twitter.com/TRvEfDNbkd",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1539277910460145664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1539277894244962300,
+					"id_str": "1539277894244962304",
+					"indices": [
+						287,
+						310
+					],
+					"media_url": "http://pbs.twimg.com/media/FVydFuEVEAAjdls.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FVydFuEVEAAjdls.jpg",
+					"url": "https://t.co/TRvEfDNbkd",
+					"display_url": "pic.twitter.com/TRvEfDNbkd",
+					"expanded_url": "https://twitter.com/Genius_kor/status/1539277910460145664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1288205677488255000,
+			"id_str": "1288205677488254976",
+			"name": "Genius Korea",
+			"screen_name": "Genius_kor",
+			"location": "Worldwide",
+			"description": "Lyrics, Romanizations, English Translations for K-Pop & Korean music | @Genius Community Based Account, not affiliated with Genius Inc. | 🇰🇷 DM for inquiries",
+			"url": "https://t.co/hrRPuLlIwP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hrRPuLlIwP",
+							"expanded_url": "https://genius.com/artists/Genius-korea",
+							"display_url": "genius.com/artists/Genius…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16596,
+			"friends_count": 1593,
+			"listed_count": 81,
+			"created_at": "Tue Jul 28 20:13:28 +0000 2020",
+			"favourites_count": 3125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5229,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1477967058356764673/NBhDCQRM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1477967058356764673/NBhDCQRM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1288205677488254976/1595971297",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#GeniusCharts | LOONA takes the hottest debut on Genius Korea charts as “Flip That” lands at #1 on the K-Pop chart &amp; “Need U” debuts at #3 on the K-R&amp;B chart! \n\n“Pale Blue Dot,” “Pose,” and “Playback” also debuts in the Top 20 of the K-Pop chart!\n\n#LOONA_FLIPTHAT @loonatheworld https://t.co/TRvEfDNbkd"
+	},
+	{
 		"created_at": "Tue Jun 21 15:40:32 +0000 2022",
 		"id": 1539271390154887200,
 		"id_str": "1539272062086402049",
