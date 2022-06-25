82b36846b866ef6b45be6017515fb56e6b0ab13d
Youngmin Lee <iameyoungmin@gmail.com>
2022-06-25 17:02:08 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 87991,
-	"created_at": "Sat Jun 25 06:02:07 +0000 2022",
+	"tweets": 87993,
+	"created_at": "Sat Jun 25 08:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_06.js
+++ data/js/tweets/2022_06.js
@@ -1,6 +1,404 @@
 Grailbird.data.tweets_2022_06 = 
 [
 	{
+		"created_at": "Sat Jun 25 07:45:36 +0000 2022",
+		"id": 1540600744747081700,
+		"id_str": "1540602094197129216",
+		"full_text": "[#김립 / #KimLip] 음악중심💙\n\n#이달의소녀 #LOONA https://t.co/vZWYDRCR9q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			36
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "김립",
+					"indices": [
+						1,
+						4
+					]
+				},
+				{
+					"text": "KimLip",
+					"indices": [
+						7,
+						14
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						23,
+						29
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						30,
+						36
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1540600729022627800,
+					"id_str": "1540600729022627840",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FWFQM1oaAAA-XRh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWFQM1oaAAA-XRh.jpg",
+					"url": "https://t.co/vZWYDRCR9q",
+					"display_url": "pic.twitter.com/vZWYDRCR9q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1540600744747081729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1540600729022627800,
+					"id_str": "1540600729022627840",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FWFQM1oaAAA-XRh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWFQM1oaAAA-XRh.jpg",
+					"url": "https://t.co/vZWYDRCR9q",
+					"display_url": "pic.twitter.com/vZWYDRCR9q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1540600744747081729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1540600729026850800,
+					"id_str": "1540600729026850817",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FWFQM1pacAELLUv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWFQM1pacAELLUv.jpg",
+					"url": "https://t.co/vZWYDRCR9q",
+					"display_url": "pic.twitter.com/vZWYDRCR9q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1540600744747081729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1540600729039417300,
+					"id_str": "1540600729039417344",
+					"indices": [
+						37,
+						60
+					],
+					"media_url": "http://pbs.twimg.com/media/FWFQM1saMAA84n7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWFQM1saMAA84n7.jpg",
+					"url": "https://t.co/vZWYDRCR9q",
+					"display_url": "pic.twitter.com/vZWYDRCR9q",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1540600744747081729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 509,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 899,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1534,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 943026,
+			"friends_count": 2,
+			"listed_count": 8776,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2095,
+		"favorite_count": 5824,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#김립 / #KimLip] 음악중심💙\n\n#이달의소녀 #LOONA https://t.co/vZWYDRCR9q"
+	},
+	{
+		"created_at": "Sat Jun 25 07:40:18 +0000 2022",
+		"id": 1540590618602139600,
+		"id_str": "1540600760366469120",
+		"full_text": "[💡]ORBIT JAPAN members-only \"Flip That: Summer Special Mini Album\" will be on sale!\nIt comes with FC-only benefits, so please take this opportunity to purchase it ♪\n\nhttps://t.co/z5MrULSbHA\n\n[Reservation acceptance start date]\nJune 25 (Sat) 18:00 (JST) ~\n\n#ORBITJAPAN #LOONA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ORBITJAPAN",
+					"indices": [
+						256,
+						267
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						268,
+						274
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/z5MrULSbHA",
+					"expanded_url": "https://orbitjapan.jp/orbit_japan_news/3551.html",
+					"display_url": "orbitjapan.jp/orbit_japan_ne…",
+					"indices": [
+						166,
+						189
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1237779164469399600,
+			"id_str": "1237779164469399552",
+			"name": "LOONA OFFICIAL FANCLUB ORBIT JAPAN",
+			"screen_name": "orbitjapan_jp",
+			"location": "",
+			"description": "#LOONA OFFICIAL FANCLUB ORBIT JAPAN の公式Twitterアカウントです。最新情報をお届けいたします。※リプライやDMでのご質問にはお答えできませんのでご了承ください。*English tweets use the translation function.",
+			"url": "https://t.co/8nSYgodvvA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8nSYgodvvA",
+							"expanded_url": "https://orbitjapan.jp/",
+							"display_url": "orbitjapan.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9706,
+			"friends_count": 2,
+			"listed_count": 92,
+			"created_at": "Wed Mar 11 16:38:25 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 124,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1443446798954078210/xAonip8i_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1443446798954078210/xAonip8i_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1237779164469399552/1634182534",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 348,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[💡]ORBIT JAPAN members-only \"Flip That: Summer Special Mini Album\" will be on sale!\nIt comes with FC-only benefits, so please take this opportunity to purchase it ♪\n\nhttps://t.co/z5MrULSbHA\n\n[Reservation acceptance start date]\nJune 25 (Sat) 18:00 (JST) ~\n\n#ORBITJAPAN #LOONA"
+	},
+	{
 		"created_at": "Sat Jun 25 05:40:18 +0000 2022",
 		"id": 1540565499251024000,
 		"id_str": "1540570558382972928",
