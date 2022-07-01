63f2c83f20344f7dac32c5126605a5426a5e0423
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-01 11:02:00 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88266,
-	"created_at": "Fri Jul 01 01:02:08 +0000 2022",
+	"tweets": 88268,
+	"created_at": "Fri Jul 01 02:01:59 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,470 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 01 01:52:51 +0000 2022",
+		"id": 1542683657407791000,
+		"id_str": "1542687646438174720",
+		"full_text": "[#NEWS] ‘징크스의 연인’ 서현♥나인우, 뜨거운 입맞춤…수목극 최강자 굳혔다\n▶ https://t.co/Dhs7VrEoBy\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/aXDiPP5fUx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "징크스의연인",
+					"indices": [
+						77,
+						84
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						85,
+						93
+					]
+				},
+				{
+					"text": "JinxedatFirst",
+					"indices": [
+						94,
+						108
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Dhs7VrEoBy",
+					"expanded_url": "http://naver.me/GX0jjU79",
+					"display_url": "naver.me/GX0jjU79",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1542681259884744700,
+					"id_str": "1542681259884744707",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"url": "https://t.co/aXDiPP5fUx",
+					"display_url": "pic.twitter.com/aXDiPP5fUx",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542683657407791107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542681259884744700,
+					"id_str": "1542681259884744707",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWi0brEVEAM2o_c.jpg",
+					"url": "https://t.co/aXDiPP5fUx",
+					"display_url": "pic.twitter.com/aXDiPP5fUx",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1542683657407791107/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 499,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60955,
+			"friends_count": 11,
+			"listed_count": 268,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15898,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 83,
+		"favorite_count": 107,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] ‘징크스의 연인’ 서현♥나인우, 뜨거운 입맞춤…수목극 최강자 굳혔다\n▶ https://t.co/Dhs7VrEoBy\n\n#서현 #징크스의연인 #SEOHYUN #JinxedatFirst https://t.co/aXDiPP5fUx"
+	},
+	{
+		"created_at": "Fri Jul 01 01:45:36 +0000 2022",
+		"id": 1542674354302881800,
+		"id_str": "1542685823744761856",
+		"full_text": "[#LOONA 싸인폴라로이드 이벤트]\n\n이달의~~\n폴라로이드 주인공은~!\n접니다!(빠밤)\n\nQuiz👉https://t.co/AY5QKt5CY8\n#아이돌챔프 #LOONA #이달의소녀 #폴라로이드 https://t.co/QohIorcj1E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			107
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						1,
+						7
+					]
+				},
+				{
+					"text": "아이돌챔프",
+					"indices": [
+						80,
+						86
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						87,
+						93
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						94,
+						100
+					]
+				},
+				{
+					"text": "폴라로이드",
+					"indices": [
+						101,
+						107
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AY5QKt5CY8",
+					"expanded_url": "https://bit.ly/3QYs99q",
+					"display_url": "bit.ly/3QYs99q",
+					"indices": [
+						56,
+						79
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1542671861900750800,
+					"id_str": "1542671861900750850",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/FWir4o2VsAIV4cI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWir4o2VsAIV4cI.jpg",
+					"url": "https://t.co/QohIorcj1E",
+					"display_url": "pic.twitter.com/QohIorcj1E",
+					"expanded_url": "https://twitter.com/idolchamp1/status/1542674354302881792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1542671861900750800,
+					"id_str": "1542671861900750850",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/FWir4o2VsAIV4cI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWir4o2VsAIV4cI.jpg",
+					"url": "https://t.co/QohIorcj1E",
+					"display_url": "pic.twitter.com/QohIorcj1E",
+					"expanded_url": "https://twitter.com/idolchamp1/status/1542674354302881792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1542671882469617700,
+					"id_str": "1542671882469617665",
+					"indices": [
+						108,
+						131
+					],
+					"media_url": "http://pbs.twimg.com/media/FWir51eVsAExhIe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWir51eVsAExhIe.jpg",
+					"url": "https://t.co/QohIorcj1E",
+					"display_url": "pic.twitter.com/QohIorcj1E",
+					"expanded_url": "https://twitter.com/idolchamp1/status/1542674354302881792/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 676,
+							"h": 380,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 842271014520148000,
+			"id_str": "842271014520147968",
+			"name": "아이돌챔프",
+			"screen_name": "idolchamp1",
+			"location": "아이돌 맴 속",
+			"description": "MBC M 쇼챔피언 사전투표, 주간아이돌 출연진 싸인사진 이벤트 진행 중",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 97656,
+			"friends_count": 108,
+			"listed_count": 259,
+			"created_at": "Thu Mar 16 07:07:13 +0000 2017",
+			"favourites_count": 286,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6126,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1224930538693120002/l0ij6CAt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1224930538693120002/l0ij6CAt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/842271014520147968/1655689519",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 38,
+		"favorite_count": 44,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#LOONA 싸인폴라로이드 이벤트]\n\n이달의~~\n폴라로이드 주인공은~!\n접니다!(빠밤)\n\nQuiz👉https://t.co/AY5QKt5CY8\n#아이돌챔프 #LOONA #이달의소녀 #폴라로이드 https://t.co/QohIorcj1E"
+	},
+	{
 		"created_at": "Fri Jul 01 00:40:12 +0000 2022",
 		"id": 1542656822753894400,
 		"id_str": "1542669364628971521",
