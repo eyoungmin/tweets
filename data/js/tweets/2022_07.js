c6b75ef27913061ce770b5eb2b75dc949725827c
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-02 18:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,299 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sat Jul 02 08:40:09 +0000 2022",
+		"id": 1543149750471594000,
+		"id_str": "1543152536646975488",
+		"full_text": "⚡️ The European leg of the Music Of The Spheres World Tour begins in Frankfurt today! If you're coming to a show, download our free tour companion app for exclusive pics &amp; videos, games, AR filters, news and carbon calculator for planet-friendly travel ⚡️ https://t.co/kaSSnHvX9u https://t.co/Uv5mrzMEFy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			283
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kaSSnHvX9u",
+					"expanded_url": "https://cldp.ly/app",
+					"display_url": "cldp.ly/app",
+					"indices": [
+						260,
+						283
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1543149746130387000,
+					"id_str": "1543149746130386947",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FWpehKVWAAM4ikI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWpehKVWAAM4ikI.jpg",
+					"url": "https://t.co/Uv5mrzMEFy",
+					"display_url": "pic.twitter.com/Uv5mrzMEFy",
+					"expanded_url": "https://twitter.com/coldplay/status/1543149750471593985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1543149746130387000,
+					"id_str": "1543149746130386947",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FWpehKVWAAM4ikI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWpehKVWAAM4ikI.jpg",
+					"url": "https://t.co/Uv5mrzMEFy",
+					"display_url": "pic.twitter.com/Uv5mrzMEFy",
+					"expanded_url": "https://twitter.com/coldplay/status/1543149750471593985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1543149746126291000,
+					"id_str": "1543149746126290944",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FWpehKUXgAAwY21.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWpehKUXgAAwY21.jpg",
+					"url": "https://t.co/Uv5mrzMEFy",
+					"display_url": "pic.twitter.com/Uv5mrzMEFy",
+					"expanded_url": "https://twitter.com/coldplay/status/1543149750471593985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1153,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1543149746130411500,
+					"id_str": "1543149746130411520",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FWpehKVWYAAwpxf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWpehKVWYAAwpxf.jpg",
+					"url": "https://t.co/Uv5mrzMEFy",
+					"display_url": "pic.twitter.com/Uv5mrzMEFy",
+					"expanded_url": "https://twitter.com/coldplay/status/1543149750471593985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1543149746126192600,
+					"id_str": "1543149746126192640",
+					"indices": [
+						284,
+						307
+					],
+					"media_url": "http://pbs.twimg.com/media/FWpehKUWAAAKidj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FWpehKUWAAAKidj.jpg",
+					"url": "https://t.co/Uv5mrzMEFy",
+					"display_url": "pic.twitter.com/Uv5mrzMEFy",
+					"expanded_url": "https://twitter.com/coldplay/status/1543149750471593985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1152,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Calypso",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23600090,
+			"friends_count": 776,
+			"listed_count": 42479,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1604,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9226,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 89,
+		"favorite_count": 543,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "⚡️ The European leg of the Music Of The Spheres World Tour begins in Frankfurt today! If you're coming to a show, download our free tour companion app for exclusive pics &amp; videos, games, AR filters, news and carbon calculator for planet-friendly travel ⚡️ https://t.co/kaSSnHvX9u https://t.co/Uv5mrzMEFy"
+	},
+	{
 		"created_at": "Sat Jul 02 06:16:00 +0000 2022",
 		"id": 1543115554961387500,
 		"id_str": "1543116260614889473",
