d2674f7bebad078093cccf84de88bf3fe0f9a770
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-09 00:20:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88526,
-	"created_at": "Fri Jul 08 15:02:02 +0000 2022",
+	"tweets": 88527,
+	"created_at": "Fri Jul 08 15:20:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 08 15:14:42 +0000 2022",
+		"id": 1545422565467832300,
+		"id_str": "1545426155183820801",
+		"full_text": "CHUNG HA 청하 'Sparkling' M/V Teaser #2\n\n2022.07.11. 6:00PM (KST)\n\n2022.07.11. 5:00AM (EST)\n\n🔎https://t.co/d0kNXWQ6Ut\n\n#청하 #CHUNGHA #Bare #Sparkling #2nd #Album #Part1 #MV #Teaser https://t.co/GOVFkDA3nZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			177
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						121,
+						129
+					]
+				},
+				{
+					"text": "Bare",
+					"indices": [
+						130,
+						135
+					]
+				},
+				{
+					"text": "Sparkling",
+					"indices": [
+						136,
+						146
+					]
+				},
+				{
+					"text": "2nd",
+					"indices": [
+						147,
+						151
+					]
+				},
+				{
+					"text": "Album",
+					"indices": [
+						152,
+						158
+					]
+				},
+				{
+					"text": "Part1",
+					"indices": [
+						159,
+						165
+					]
+				},
+				{
+					"text": "MV",
+					"indices": [
+						166,
+						169
+					]
+				},
+				{
+					"text": "Teaser",
+					"indices": [
+						170,
+						177
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/d0kNXWQ6Ut",
+					"expanded_url": "https://youtu.be/xhzdzu9COWs",
+					"display_url": "youtu.be/xhzdzu9COWs",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1545422554835198000,
+					"id_str": "1545422554835197952",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FXJxoCjUIAAOyTl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXJxoCjUIAAOyTl.jpg",
+					"url": "https://t.co/GOVFkDA3nZ",
+					"display_url": "pic.twitter.com/GOVFkDA3nZ",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1545422565467832321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1545422554835198000,
+					"id_str": "1545422554835197952",
+					"indices": [
+						178,
+						201
+					],
+					"media_url": "http://pbs.twimg.com/media/FXJxoCjUIAAOyTl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXJxoCjUIAAOyTl.jpg",
+					"url": "https://t.co/GOVFkDA3nZ",
+					"display_url": "pic.twitter.com/GOVFkDA3nZ",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1545422565467832321/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/ul2Z0qlgiq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ul2Z0qlgiq",
+							"expanded_url": "https://chungha.lnk.to/KillingMe",
+							"display_url": "chungha.lnk.to/KillingMe",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 351040,
+			"friends_count": 1,
+			"listed_count": 2164,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2284,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1656688172",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1222,
+		"favorite_count": 2321,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "CHUNG HA 청하 'Sparkling' M/V Teaser #2\n\n2022.07.11. 6:00PM (KST)\n\n2022.07.11. 5:00AM (EST)\n\n🔎https://t.co/d0kNXWQ6Ut\n\n#청하 #CHUNGHA #Bare #Sparkling #2nd #Album #Part1 #MV #Teaser https://t.co/GOVFkDA3nZ"
+	},
+	{
 		"created_at": "Fri Jul 08 14:40:32 +0000 2022",
 		"id": 1545414799743500300,
 		"id_str": "1545417555774513152",
