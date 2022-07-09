063e1d95b136a58f36af4cd9ac2156ca3e7a53ad
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-10 00:19:57 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88558,
-	"created_at": "Sat Jul 09 15:02:03 +0000 2022",
+	"tweets": 88559,
+	"created_at": "Sat Jul 09 15:19:57 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Sat Jul 09 15:17:42 +0000 2022",
+		"id": 1545784949978062800,
+		"id_str": "1545789298170925057",
+		"full_text": "#0710_HBD_Orbit\n#Happy_Orbit_Day\n\n오빛 4주년을 축하합니다🎉\n이달의 소녀와 함께 해온 4년\n40년 400년도 함께해요💕\n\n오빛 4ever💫\n\n#이달의소녀 #LOONA #오빛 #Orbit https://t.co/lsrS2V1xm9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "0710_HBD_Orbit",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"text": "Happy_Orbit_Day",
+					"indices": [
+						16,
+						32
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						94,
+						100
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						101,
+						107
+					]
+				},
+				{
+					"text": "오빛",
+					"indices": [
+						108,
+						111
+					]
+				},
+				{
+					"text": "Orbit",
+					"indices": [
+						112,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1545784946534793200,
+					"id_str": "1545784946534793217",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/FXO7OA1aQAEs9T_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXO7OA1aQAEs9T_.jpg",
+					"url": "https://t.co/lsrS2V1xm9",
+					"display_url": "pic.twitter.com/lsrS2V1xm9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1545784949978062852/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 850,
+							"h": 850,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 850,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1545784946534793200,
+					"id_str": "1545784946534793217",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/FXO7OA1aQAEs9T_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXO7OA1aQAEs9T_.jpg",
+					"url": "https://t.co/lsrS2V1xm9",
+					"display_url": "pic.twitter.com/lsrS2V1xm9",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1545784949978062852/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 850,
+							"h": 850,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 850,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 952736,
+			"friends_count": 2,
+			"listed_count": 8830,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4784,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4059,
+		"favorite_count": 8891,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#0710_HBD_Orbit\n#Happy_Orbit_Day\n\n오빛 4주년을 축하합니다🎉\n이달의 소녀와 함께 해온 4년\n40년 400년도 함께해요💕\n\n오빛 4ever💫\n\n#이달의소녀 #LOONA #오빛 #Orbit https://t.co/lsrS2V1xm9"
+	},
+	{
 		"created_at": "Sat Jul 09 14:40:09 +0000 2022",
 		"id": 1545771884184277000,
 		"id_str": "1545779849427648515",
