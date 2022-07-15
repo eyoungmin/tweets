49ac2daa7656570238d048a9b68699c36251dbd9
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-16 03:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 88791,
-	"created_at": "Fri Jul 15 17:02:04 +0000 2022",
+	"tweets": 88792,
+	"created_at": "Fri Jul 15 18:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Fri Jul 15 17:40:10 +0000 2022",
+		"id": 1547984771334094800,
+		"id_str": "1547999477327228928",
+		"full_text": "📣 LIVE AUDIO STREAM FROM PARIS THIS WEEKEND\n\nOn Sunday, hear the band's Paris Stade de France show for free via the official #MusicOfTheSpheresWorldTour app, presented by @sap. \n\n8.15pm BST / 9.15pm CET / 3.15pm ET / 12.15pm PT\n\n📱 Download the app now at https://t.co/qEL4DANsta https://t.co/mG6WcfquJu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MusicOfTheSpheresWorldTour",
+					"indices": [
+						125,
+						152
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SAP",
+					"name": "SAP",
+					"id": 76117579,
+					"id_str": "76117579",
+					"indices": [
+						171,
+						175
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qEL4DANsta",
+					"expanded_url": "http://coldplay.com/app",
+					"display_url": "coldplay.com/app",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1547984762748317700,
+					"id_str": "1547984762748317696",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FXuL8K1UsAAEHK0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXuL8K1UsAAEHK0.jpg",
+					"url": "https://t.co/mG6WcfquJu",
+					"display_url": "pic.twitter.com/mG6WcfquJu",
+					"expanded_url": "https://twitter.com/coldplay/status/1547984771334094849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1547984762748317700,
+					"id_str": "1547984762748317696",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/FXuL8K1UsAAEHK0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FXuL8K1UsAAEHK0.jpg",
+					"url": "https://t.co/mG6WcfquJu",
+					"display_url": "pic.twitter.com/mG6WcfquJu",
+					"expanded_url": "https://twitter.com/coldplay/status/1547984771334094849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Floris",
+			"description": "The official Twitter of the band Coldplay. New album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23600846,
+			"friends_count": 776,
+			"listed_count": 42465,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1617,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9279,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 179,
+		"favorite_count": 1427,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "📣 LIVE AUDIO STREAM FROM PARIS THIS WEEKEND\n\nOn Sunday, hear the band's Paris Stade de France show for free via the official #MusicOfTheSpheresWorldTour app, presented by @sap. \n\n8.15pm BST / 9.15pm CET / 3.15pm ET / 12.15pm PT\n\n📱 Download the app now at https://t.co/qEL4DANsta https://t.co/mG6WcfquJu"
+	},
+	{
 		"created_at": "Fri Jul 15 16:40:17 +0000 2022",
 		"id": 1547977031564271600,
 		"id_str": "1547984407377559552",
