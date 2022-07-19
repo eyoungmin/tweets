59774f41f29bb23f222e453e52e47556ed6389c2
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-20 00:02:12 +0900

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Tue Jul 19 14:29:39 +0000 2022",
+		"id": 1549393733107712000,
+		"id_str": "1549401082299916291",
+		"full_text": "[🎬풀영상] #임윤아 (YOONA),#박재찬 (JAECHAN), #한효주, #김고은, #정호연 (Hoyeon),#이정재 #설현 @제1회 청룡시리즈어워즈 레드카펫 풀영상 Red Carpet Full ver.\n\n🔗 https://t.co/8hFRSL3HMB\n\n#청룡시리즈어워즈 #레드카펫\n#BSA #BSA2022 https://t.co/2wtsnx2pso",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "임윤아",
+					"indices": [
+						7,
+						11
+					]
+				},
+				{
+					"text": "박재찬",
+					"indices": [
+						20,
+						24
+					]
+				},
+				{
+					"text": "한효주",
+					"indices": [
+						36,
+						40
+					]
+				},
+				{
+					"text": "김고은",
+					"indices": [
+						42,
+						46
+					]
+				},
+				{
+					"text": "정호연",
+					"indices": [
+						48,
+						52
+					]
+				},
+				{
+					"text": "이정재",
+					"indices": [
+						62,
+						66
+					]
+				},
+				{
+					"text": "설현",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "청룡시리즈어워즈",
+					"indices": [
+						143,
+						152
+					]
+				},
+				{
+					"text": "레드카펫",
+					"indices": [
+						153,
+						158
+					]
+				},
+				{
+					"text": "BSA",
+					"indices": [
+						159,
+						163
+					]
+				},
+				{
+					"text": "BSA2022",
+					"indices": [
+						164,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8hFRSL3HMB",
+					"expanded_url": "https://youtu.be/SsJzaMBm-do",
+					"display_url": "youtu.be/SsJzaMBm-do",
+					"indices": [
+						118,
+						141
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1549385867118391300,
+					"id_str": "1549385867118391296",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FYCGPLWaMAA0GoW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYCGPLWaMAA0GoW.jpg",
+					"url": "https://t.co/2wtsnx2pso",
+					"display_url": "pic.twitter.com/2wtsnx2pso",
+					"expanded_url": "https://twitter.com/TONGTONG_C/status/1549393733107712001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1549385867118391300,
+					"id_str": "1549385867118391296",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FYCGPLWaMAA0GoW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FYCGPLWaMAA0GoW.jpg",
+					"url": "https://t.co/2wtsnx2pso",
+					"display_url": "pic.twitter.com/2wtsnx2pso",
+					"expanded_url": "https://twitter.com/TONGTONG_C/status/1549393733107712001/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1247773150856630300,
+			"id_str": "1247773150856630275",
+			"name": "TongTong_Culture",
+			"screen_name": "TONGTONG_C",
+			"location": "",
+			"description": "통통컬쳐 공식 트위터✨ TONGTONG CULTURE Official Twitter.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6715,
+			"friends_count": 11,
+			"listed_count": 25,
+			"created_at": "Wed Apr 08 06:28:52 +0000 2020",
+			"favourites_count": 157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1160,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1484007108710641664/X4R-NQgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1484007108710641664/X4R-NQgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1247773150856630275/1655457151",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[🎬풀영상] #임윤아 (YOONA),#박재찬 (JAECHAN), #한효주, #김고은, #정호연 (Hoyeon),#이정재 #설현 @제1회 청룡시리즈어워즈 레드카펫 풀영상 Red Carpet Full ver.\n\n🔗 https://t.co/8hFRSL3HMB\n\n#청룡시리즈어워즈 #레드카펫\n#BSA #BSA2022 https://t.co/2wtsnx2pso"
+	},
+	{
 		"created_at": "Tue Jul 19 12:40:20 +0000 2022",
 		"id": 1549369229387997200,
 		"id_str": "1549373572623253506",
