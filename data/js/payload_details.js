26fbc58cd538f5392f1a156a5b66344beee76c4a
Youngmin Lee <iameyoungmin@gmail.com>
2022-07-28 13:02:13 +0900

    Added 4 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89241,
-	"created_at": "Thu Jul 28 03:02:08 +0000 2022",
+	"tweets": 89245,
+	"created_at": "Thu Jul 28 04:02:12 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2022_07.js",
 		"year": 2022,
 		"var_name": "tweets_2022_07",
-		"tweet_count": 976,
+		"tweet_count": 980,
 		"month": 7
 	},
 	{
