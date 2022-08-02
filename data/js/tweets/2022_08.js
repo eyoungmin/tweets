110fd1bb7328a44e83c66d93f7938782eb17bcf9
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-02 18:02:02 +0900

    Added 2 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,332 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Tue Aug 02 08:40:21 +0000 2022",
+		"id": 1554383551277445000,
+		"id_str": "1554386611617538049",
+		"full_text": "#선예 안에서 피어나는 자유로운 Feel을 느껴봐✨\nDancer들과 함께하는 #JustADancer #후크선장_댄스 💃🏻\n\n▶️ https://t.co/2Gl9WiZc7M\n\n#Sunye\n#제뉴인 #Genuine\n#저스트어댄서_챌린지 #JustADancer_Challenge",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "선예",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "JustADancer",
+					"indices": [
+						43,
+						55
+					]
+				},
+				{
+					"text": "후크선장_댄스",
+					"indices": [
+						56,
+						64
+					]
+				},
+				{
+					"text": "Sunye",
+					"indices": [
+						97,
+						103
+					]
+				},
+				{
+					"text": "제뉴인",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "Genuine",
+					"indices": [
+						109,
+						117
+					]
+				},
+				{
+					"text": "저스트어댄서_챌린지",
+					"indices": [
+						118,
+						129
+					]
+				},
+				{
+					"text": "JustADancer_Challenge",
+					"indices": [
+						130,
+						152
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2Gl9WiZc7M",
+					"expanded_url": "https://www.tiktok.com/@sunye.m/video/7127191805635923202?_t=8UUcEUWjIkz&_r=1",
+					"display_url": "tiktok.com/@sunye.m/video…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496008272398418000,
+			"id_str": "1496008272398417930",
+			"name": "BlockBerryCreative",
+			"screen_name": "BBC_blockberry",
+			"location": "",
+			"description": "BlockBerryCreative official twitter / 이달의 소녀 LOONA / 선예 SUNYE",
+			"url": "https://t.co/zoTXadc3lA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zoTXadc3lA",
+							"expanded_url": "http://blockberrycreative.com",
+							"display_url": "blockberrycreative.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18661,
+			"friends_count": 2,
+			"listed_count": 64,
+			"created_at": "Tue Feb 22 06:26:05 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 114,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 38,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#선예 안에서 피어나는 자유로운 Feel을 느껴봐✨\nDancer들과 함께하는 #JustADancer #후크선장_댄스 💃🏻\n\n▶️ https://t.co/2Gl9WiZc7M\n\n#Sunye\n#제뉴인 #Genuine\n#저스트어댄서_챌린지 #JustADancer_Challenge"
+	},
+	{
+		"created_at": "Tue Aug 02 08:40:12 +0000 2022",
+		"id": 1554380754528125000,
+		"id_str": "1554386572790857731",
+		"full_text": "Run-a Run-a Run away🕺🏻\n#선예 와 Real Dancer가 함께하는 #JustADancer #후크선장_댄스 ✨\n\n▶️ https://t.co/y1Sq7svOKf\n\n#Sunye\n#제뉴인 #Genuine\n#저스트어댄서_챌린지 #JustADancer_Challenge",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			155
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "선예",
+					"indices": [
+						23,
+						26
+					]
+				},
+				{
+					"text": "JustADancer",
+					"indices": [
+						47,
+						59
+					]
+				},
+				{
+					"text": "후크선장_댄스",
+					"indices": [
+						60,
+						68
+					]
+				},
+				{
+					"text": "Sunye",
+					"indices": [
+						100,
+						106
+					]
+				},
+				{
+					"text": "제뉴인",
+					"indices": [
+						107,
+						111
+					]
+				},
+				{
+					"text": "Genuine",
+					"indices": [
+						112,
+						120
+					]
+				},
+				{
+					"text": "저스트어댄서_챌린지",
+					"indices": [
+						121,
+						132
+					]
+				},
+				{
+					"text": "JustADancer_Challenge",
+					"indices": [
+						133,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/y1Sq7svOKf",
+					"expanded_url": "https://www.tiktok.com/@sunye.m/video/7127188729894096130?_t=8UUbLuYwzX0&_r=1",
+					"display_url": "tiktok.com/@sunye.m/video…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1496008272398418000,
+			"id_str": "1496008272398417930",
+			"name": "BlockBerryCreative",
+			"screen_name": "BBC_blockberry",
+			"location": "",
+			"description": "BlockBerryCreative official twitter / 이달의 소녀 LOONA / 선예 SUNYE",
+			"url": "https://t.co/zoTXadc3lA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zoTXadc3lA",
+							"expanded_url": "http://blockberrycreative.com",
+							"display_url": "blockberrycreative.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18661,
+			"friends_count": 2,
+			"listed_count": 64,
+			"created_at": "Tue Feb 22 06:26:05 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 114,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496008427323400194/aZbqjujC_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Run-a Run-a Run away🕺🏻\n#선예 와 Real Dancer가 함께하는 #JustADancer #후크선장_댄스 ✨\n\n▶️ https://t.co/y1Sq7svOKf\n\n#Sunye\n#제뉴인 #Genuine\n#저스트어댄서_챌린지 #JustADancer_Challenge"
+	},
+	{
 		"created_at": "Tue Aug 02 05:29:51 +0000 2022",
 		"id": 1554330858890612700,
 		"id_str": "1554338668088856576",
