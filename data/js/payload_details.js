bcd5d7687945293d7f2727080bed0ec5ebba0b9b
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-03 07:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89416,
-	"created_at": "Tue Aug 02 18:02:00 +0000 2022",
+	"tweets": 89417,
+	"created_at": "Tue Aug 02 22:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Tue Aug 02 21:45:37 +0000 2022",
+		"id": 1554573425657712600,
+		"id_str": "1554584228268068865",
+		"full_text": "💫🇺🇸US Orbit!💫\nReady for show day 2 with #LOONA?\n\nTo enjoy the show at its fullest safely, please take note of the prohibited items for 2022 LOONA 1st World Tour : [LOONATHEWORLD] in LA.\n\n**IMPORTANT: Professional cameras as well as any live stream recordings are not permitted. https://t.co/VyAs2EV1Su",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LOONA",
+					"indices": [
+						40,
+						46
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1554573347832230000,
+					"id_str": "1554573347832229888",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FZL0OS7UcAAm5mU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZL0OS7UcAAm5mU.jpg",
+					"url": "https://t.co/VyAs2EV1Su",
+					"display_url": "pic.twitter.com/VyAs2EV1Su",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1554573425657712641/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1554573347832230000,
+					"id_str": "1554573347832229888",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FZL0OS7UcAAm5mU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZL0OS7UcAAm5mU.jpg",
+					"url": "https://t.co/VyAs2EV1Su",
+					"display_url": "pic.twitter.com/VyAs2EV1Su",
+					"expanded_url": "https://twitter.com/_mymusictaste/status/1554573425657712641/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1664037517,
+			"id_str": "1664037517",
+			"name": "MyMusicTaste",
+			"screen_name": "_mymusictaste",
+			"location": "STOP WISHING, START MAKING!✨",
+			"description": "MMT for all K-pop entertainment needs. \n🎤Fan-initiated Concert Prod. & Tour Ops. \n📺Online Event Platform \n🌏Global PR & Marketing \n📰K-Ent. News \n🛒 @_mmtshop",
+			"url": "https://t.co/LzblWIyOkW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LzblWIyOkW",
+							"expanded_url": "http://mymusictaste.com",
+							"display_url": "mymusictaste.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 149433,
+			"friends_count": 104,
+			"listed_count": 319,
+			"created_at": "Mon Aug 12 03:09:22 +0000 2013",
+			"favourites_count": 5098,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 29321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1525038851755892736/qc8dyM_J_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1525038851755892736/qc8dyM_J_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1664037517/1627643810",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 34,
+		"favorite_count": 238,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "💫🇺🇸US Orbit!💫\nReady for show day 2 with #LOONA?\n\nTo enjoy the show at its fullest safely, please take note of the prohibited items for 2022 LOONA 1st World Tour : [LOONATHEWORLD] in LA.\n\n**IMPORTANT: Professional cameras as well as any live stream recordings are not permitted. https://t.co/VyAs2EV1Su"
+	},
+	{
 		"created_at": "Tue Aug 02 17:40:28 +0000 2022",
 		"id": 1554511961257279500,
 		"id_str": "1554522533411532800",
