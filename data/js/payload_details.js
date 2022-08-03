30882a6fd2bf4553e4740e84e454f3322cffc845
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-03 15:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89436,
-	"created_at": "Wed Aug 03 05:02:02 +0000 2022",
+	"tweets": 89437,
+	"created_at": "Wed Aug 03 06:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 03 05:40:10 +0000 2022",
+		"id": 1554693567880507400,
+		"id_str": "1554703654690689024",
+		"full_text": "2022 IU CONCERT\n&lt;The Golden Hour : 오렌지 태양 아래&gt;\n티켓 오픈 일정 안내\n\n✨ 팬클럽 선예매:\n22.08.08 (월) 8PM ~ 11:59PM(KST) \n✨ 일반예매:\n22.08.11 (목) 8PM ~ 09.15 (목) 11:59PM(KST)\n\n⌛ https://t.co/TsMSBYLsa9\n\n#아이유 #IU\n#The_Golden_Hour #오렌지_태양_아래 https://t.co/yV7eizLluJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			223
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이유",
+					"indices": [
+						187,
+						191
+					]
+				},
+				{
+					"text": "IU",
+					"indices": [
+						192,
+						195
+					]
+				},
+				{
+					"text": "The_Golden_Hour",
+					"indices": [
+						196,
+						212
+					]
+				},
+				{
+					"text": "오렌지_태양_아래",
+					"indices": [
+						213,
+						223
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TsMSBYLsa9",
+					"expanded_url": "https://ticket.melon.com/csoon/detail.htm?csoonId=6360",
+					"display_url": "ticket.melon.com/csoon/detail.h…",
+					"indices": [
+						162,
+						185
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1554690956179701800,
+					"id_str": "1554690956179701760",
+					"indices": [
+						224,
+						247
+					],
+					"media_url": "http://pbs.twimg.com/media/FZNfMAKagAAVYtx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZNfMAKagAAVYtx.jpg",
+					"url": "https://t.co/yV7eizLluJ",
+					"display_url": "pic.twitter.com/yV7eizLluJ",
+					"expanded_url": "https://twitter.com/_IUofficial/status/1554693567880507392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1554690956179701800,
+					"id_str": "1554690956179701760",
+					"indices": [
+						224,
+						247
+					],
+					"media_url": "http://pbs.twimg.com/media/FZNfMAKagAAVYtx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZNfMAKagAAVYtx.jpg",
+					"url": "https://t.co/yV7eizLluJ",
+					"display_url": "pic.twitter.com/yV7eizLluJ",
+					"expanded_url": "https://twitter.com/_IUofficial/status/1554693567880507392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 838,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1430,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 475,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024486566096326700,
+			"id_str": "1024486566096326656",
+			"name": "아이유(IU) 공식 트위터",
+			"screen_name": "_IUofficial",
+			"location": "",
+			"description": "아이유(IU) Official Twitter",
+			"url": "https://t.co/26KBouiO60",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/26KBouiO60",
+							"expanded_url": "http://cafe.daum.net/IU",
+							"display_url": "cafe.daum.net/IU",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1403092,
+			"friends_count": 0,
+			"listed_count": 2312,
+			"created_at": "Wed Aug 01 02:46:46 +0000 2018",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1366,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024486566096326656/1633359681",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4544,
+		"favorite_count": 9453,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2022 IU CONCERT\n&lt;The Golden Hour : 오렌지 태양 아래&gt;\n티켓 오픈 일정 안내\n\n✨ 팬클럽 선예매:\n22.08.08 (월) 8PM ~ 11:59PM(KST) \n✨ 일반예매:\n22.08.11 (목) 8PM ~ 09.15 (목) 11:59PM(KST)\n\n⌛ https://t.co/TsMSBYLsa9\n\n#아이유 #IU\n#The_Golden_Hour #오렌지_태양_아래 https://t.co/yV7eizLluJ"
+	},
+	{
 		"created_at": "Wed Aug 03 04:45:37 +0000 2022",
 		"id": 1554679702584848400,
 		"id_str": "1554689924728758273",
