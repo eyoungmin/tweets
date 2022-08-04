cafa35b9e4eacb5d7914bc1ed56f1676915d5f94
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-05 04:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89492,
-	"created_at": "Thu Aug 04 18:02:02 +0000 2022",
+	"tweets": 89493,
+	"created_at": "Thu Aug 04 19:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 04 18:40:09 +0000 2022",
+		"id": 1555253884658286600,
+		"id_str": "1555262331776638976",
+		"full_text": "📢뮤직뱅크 인원체크 장소 안내\n\n220805 뮤직뱅크 사전녹화에 참여하시는 분들은 📍여의도 KBS 신관 정문 앞으로 늦지않게 3:30까지 모여주세요!\n\n감사합니다! https://t.co/i6obJMXTxv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1555253876676505600,
+					"id_str": "1555253876676505600",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FZVfKSbaMAAW6C9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZVfKSbaMAAW6C9.jpg",
+					"url": "https://t.co/i6obJMXTxv",
+					"display_url": "pic.twitter.com/i6obJMXTxv",
+					"expanded_url": "https://twitter.com/NewJeans_ADOR/status/1555253884658286592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1143,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 647,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1555253876676505600,
+					"id_str": "1555253876676505600",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FZVfKSbaMAAW6C9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZVfKSbaMAAW6C9.jpg",
+					"url": "https://t.co/i6obJMXTxv",
+					"display_url": "pic.twitter.com/i6obJMXTxv",
+					"expanded_url": "https://twitter.com/NewJeans_ADOR/status/1555253884658286592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1143,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 647,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1950,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1476476838180696000,
+			"id_str": "1476476838180696073",
+			"name": "NewJeans",
+			"screen_name": "NewJeans_ADOR",
+			"location": "",
+			"description": "We are NewJeans👖",
+			"url": "https://t.co/wITF1PpBHO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wITF1PpBHO",
+							"expanded_url": "http://newjeans.kr",
+							"display_url": "newjeans.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 250773,
+			"friends_count": 2,
+			"listed_count": 866,
+			"created_at": "Thu Dec 30 08:54:56 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 56,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1476476838180696073/1658415621",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 282,
+		"favorite_count": 1394,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "📢뮤직뱅크 인원체크 장소 안내\n\n220805 뮤직뱅크 사전녹화에 참여하시는 분들은 📍여의도 KBS 신관 정문 앞으로 늦지않게 3:30까지 모여주세요!\n\n감사합니다! https://t.co/i6obJMXTxv"
+	},
+	{
 		"created_at": "Thu Aug 04 17:40:53 +0000 2022",
 		"id": 1555234711638884400,
 		"id_str": "1555247413564125184",
