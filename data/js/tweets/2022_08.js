62fa2daeec844f02045072affc82097ed36f7337
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-06 02:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Fri Aug 05 16:40:10 +0000 2022",
+		"id": 1555584415929188400,
+		"id_str": "1555594521496457216",
+		"full_text": "Make sure to tweet @MostRequestLive &amp; @OnAirRomeo if you want to hear “Life’s Too Short” on #MostRequestedLive this weekend!\n\n#aespa #LifesTooShort #LTS #aespa_LTS\n#Girls #aespa_Girls https://t.co/Vh9FISi2sp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MostRequestedLive",
+					"indices": [
+						96,
+						114
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						130,
+						136
+					]
+				},
+				{
+					"text": "LifesTooShort",
+					"indices": [
+						137,
+						151
+					]
+				},
+				{
+					"text": "LTS",
+					"indices": [
+						152,
+						156
+					]
+				},
+				{
+					"text": "aespa_LTS",
+					"indices": [
+						157,
+						167
+					]
+				},
+				{
+					"text": "Girls",
+					"indices": [
+						168,
+						174
+					]
+				},
+				{
+					"text": "aespa_Girls",
+					"indices": [
+						175,
+						187
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MostRequestLive",
+					"name": "Most Requested Live",
+					"id": 108775893,
+					"id_str": "108775893",
+					"indices": [
+						19,
+						35
+					]
+				},
+				{
+					"screen_name": "OnAirRomeo",
+					"name": "On Air Romeo",
+					"id": 21590830,
+					"id_str": "21590830",
+					"indices": [
+						42,
+						53
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1555523494410350600,
+					"id_str": "1555523494410350592",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/FZZUYGwacAA4hdl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZZUYGwacAA4hdl.jpg",
+					"url": "https://t.co/Vh9FISi2sp",
+					"display_url": "pic.twitter.com/Vh9FISi2sp",
+					"expanded_url": "https://twitter.com/aespa_official/status/1555584415929188352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1555523494410350600,
+					"id_str": "1555523494410350592",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/FZZUYGwacAA4hdl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZZUYGwacAA4hdl.jpg",
+					"url": "https://t.co/Vh9FISi2sp",
+					"display_url": "pic.twitter.com/Vh9FISi2sp",
+					"expanded_url": "https://twitter.com/aespa_official/status/1555584415929188352/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Girls",
+			"url": "https://t.co/rz16VWutuk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rz16VWutuk",
+							"expanded_url": "https://aespa.lnk.to/GirlsEP",
+							"display_url": "aespa.lnk.to/GirlsEP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3037524,
+			"friends_count": 2,
+			"listed_count": 9235,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1938,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1654009204",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1535,
+		"favorite_count": 7248,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Make sure to tweet @MostRequestLive &amp; @OnAirRomeo if you want to hear “Life’s Too Short” on #MostRequestedLive this weekend!\n\n#aespa #LifesTooShort #LTS #aespa_LTS\n#Girls #aespa_Girls https://t.co/Vh9FISi2sp"
+	},
+	{
 		"created_at": "Fri Aug 05 15:41:05 +0000 2022",
 		"id": 1555566166629929000,
 		"id_str": "1555579655435866112",
