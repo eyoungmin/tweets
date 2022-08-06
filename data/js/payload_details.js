6678f90ccdfb4a8e781600c5544bfbb4cde5d678
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-07 06:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89582,
-	"created_at": "Sat Aug 06 20:02:03 +0000 2022",
+	"tweets": 89583,
+	"created_at": "Sat Aug 06 21:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 06 20:40:17 +0000 2022",
+		"id": 1556012195749965800,
+		"id_str": "1556017337052053504",
+		"full_text": "[공방공지] \n2200807 SBS 인기가요에 참여하시는 NewJeans 팬 분들께서는 등촌동 공개홀 하기의 사진 공간으로 모여주시기 바랍니다.\n\n#NewJeans #뉴진스 #NewJeans_1st_EP https://t.co/9Odn04IxtY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NewJeans",
+					"indices": [
+						82,
+						91
+					]
+				},
+				{
+					"text": "뉴진스",
+					"indices": [
+						92,
+						96
+					]
+				},
+				{
+					"text": "NewJeans_1st_EP",
+					"indices": [
+						97,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1556012188800008200,
+					"id_str": "1556012188800008192",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/FZgQ13OaQAA9yus.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZgQ13OaQAA9yus.jpg",
+					"url": "https://t.co/9Odn04IxtY",
+					"display_url": "pic.twitter.com/9Odn04IxtY",
+					"expanded_url": "https://twitter.com/NewJeans_ADOR/status/1556012195749965824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1556012188800008200,
+					"id_str": "1556012188800008192",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/FZgQ13OaQAA9yus.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZgQ13OaQAA9yus.jpg",
+					"url": "https://t.co/9Odn04IxtY",
+					"display_url": "pic.twitter.com/9Odn04IxtY",
+					"expanded_url": "https://twitter.com/NewJeans_ADOR/status/1556012195749965824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1476476838180696000,
+			"id_str": "1476476838180696073",
+			"name": "NewJeans",
+			"screen_name": "NewJeans_ADOR",
+			"location": "",
+			"description": "We are NewJeans👖",
+			"url": "https://t.co/wITF1PpBHO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wITF1PpBHO",
+							"expanded_url": "http://newjeans.kr",
+							"display_url": "newjeans.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 269104,
+			"friends_count": 2,
+			"listed_count": 929,
+			"created_at": "Thu Dec 30 08:54:56 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 67,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1476476838180696073/1658415621",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 287,
+		"favorite_count": 1852,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[공방공지] \n2200807 SBS 인기가요에 참여하시는 NewJeans 팬 분들께서는 등촌동 공개홀 하기의 사진 공간으로 모여주시기 바랍니다.\n\n#NewJeans #뉴진스 #NewJeans_1st_EP https://t.co/9Odn04IxtY"
+	},
+	{
 		"created_at": "Sat Aug 06 19:40:10 +0000 2022",
 		"id": 1555994377331302400,
 		"id_str": "1556002208906625024",
