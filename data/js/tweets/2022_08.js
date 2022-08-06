d3aa125b6cb079c54158f9a0a0d9a61eaf853d02
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-06 10:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 06 00:45:41 +0000 2022",
+		"id": 1555707726176690200,
+		"id_str": "1555716707699892226",
+		"full_text": "KCON 2022 LA DREAM STAGE with LOONA☁️\n💙Take this opportunity to be on stage with LOONA💚\n\nAny KCON-ers who love LOONA can apply!\nWaiting for KCON-ers who will &lt;PTT (Paint The Town)&gt; with LOONA🤗\n\n*Please refer to the image for more details.\n\n#KCON #KCON2022LA https://t.co/b21fLbl1i5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "KCON",
+					"indices": [
+						246,
+						251
+					]
+				},
+				{
+					"text": "KCON2022LA",
+					"indices": [
+						252,
+						263
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1555667916460212200,
+					"id_str": "1555667916460212224",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/FZbXuk0VQAABeDB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZbXuk0VQAABeDB.jpg",
+					"url": "https://t.co/b21fLbl1i5",
+					"display_url": "pic.twitter.com/b21fLbl1i5",
+					"expanded_url": "https://twitter.com/kconusa/status/1555707726176690176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1555667916460212200,
+					"id_str": "1555667916460212224",
+					"indices": [
+						264,
+						287
+					],
+					"media_url": "http://pbs.twimg.com/media/FZbXuk0VQAABeDB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZbXuk0VQAABeDB.jpg",
+					"url": "https://t.co/b21fLbl1i5",
+					"display_url": "pic.twitter.com/b21fLbl1i5",
+					"expanded_url": "https://twitter.com/kconusa/status/1555707726176690176/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 739451875,
+			"id_str": "739451875",
+			"name": "KCONUSA",
+			"screen_name": "kconusa",
+			"location": "Los Angeles, CA",
+			"description": "The official Twitter account of KCONUSA. #LetsKCON",
+			"url": "https://t.co/FLWmbXsnw0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FLWmbXsnw0",
+							"expanded_url": "http://www.kconusa.com",
+							"display_url": "kconusa.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 224163,
+			"friends_count": 7,
+			"listed_count": 471,
+			"created_at": "Sun Aug 05 23:40:55 +0000 2012",
+			"favourites_count": 3055,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 20449,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme17/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1537845271031205888/TQ5o6UCj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1537845271031205888/TQ5o6UCj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/739451875/1655163038",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "E6F6F9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 134,
+		"favorite_count": 553,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "KCON 2022 LA DREAM STAGE with LOONA☁️\n💙Take this opportunity to be on stage with LOONA💚\n\nAny KCON-ers who love LOONA can apply!\nWaiting for KCON-ers who will &lt;PTT (Paint The Town)&gt; with LOONA🤗\n\n*Please refer to the image for more details.\n\n#KCON #KCON2022LA https://t.co/b21fLbl1i5"
+	},
+	{
 		"created_at": "Fri Aug 05 23:29:41 +0000 2022",
 		"id": 1555696510146211800,
 		"id_str": "1555697583175176200",
