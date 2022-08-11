aa5496731fb4024e8471a8bd771e3e5e0b9e312a
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-11 14:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 11 04:39:26 +0000 2022",
+		"id": 1557577549944803300,
+		"id_str": "1557587473185718272",
+		"full_text": "[달로그 #40] 희소식 그리웠던 오빛들 손 🙋🏻‍♀️🙋🏻‍♂️\n\n오빛을 위해 바쁜 해외투어 와중에도 달로그를 보내 온 #희진 🥹🥹\n멤버들과 함께한 휴일도 궁금하다면 지금 바로 이달의 소녀 공식 유튜브 채널로 Go!\n\n#이달의소녀 #달로그 #LOONA #LOONALOG #HeeJin\n\n▶️ https://t.co/9GetvToLKX https://t.co/fJ5dAFEKPX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "희진",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						122,
+						128
+					]
+				},
+				{
+					"text": "달로그",
+					"indices": [
+						129,
+						133
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						134,
+						140
+					]
+				},
+				{
+					"text": "LOONALOG",
+					"indices": [
+						141,
+						150
+					]
+				},
+				{
+					"text": "HeeJin",
+					"indices": [
+						151,
+						158
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9GetvToLKX",
+					"expanded_url": "https://youtu.be/j7SYXiusZdk",
+					"display_url": "youtu.be/j7SYXiusZdk",
+					"indices": [
+						163,
+						186
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1557570252208484400,
+					"id_str": "1557570252208484353",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ2Z5FuaMAEkzPu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ2Z5FuaMAEkzPu.jpg",
+					"url": "https://t.co/fJ5dAFEKPX",
+					"display_url": "pic.twitter.com/fJ5dAFEKPX",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1557577549944803328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1557570252208484400,
+					"id_str": "1557570252208484353",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/FZ2Z5FuaMAEkzPu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FZ2Z5FuaMAEkzPu.jpg",
+					"url": "https://t.co/fJ5dAFEKPX",
+					"display_url": "pic.twitter.com/fJ5dAFEKPX",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1557577549944803328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 970577,
+			"friends_count": 3,
+			"listed_count": 8917,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4920,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1287,
+		"favorite_count": 4008,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[달로그 #40] 희소식 그리웠던 오빛들 손 🙋🏻‍♀️🙋🏻‍♂️\n\n오빛을 위해 바쁜 해외투어 와중에도 달로그를 보내 온 #희진 🥹🥹\n멤버들과 함께한 휴일도 궁금하다면 지금 바로 이달의 소녀 공식 유튜브 채널로 Go!\n\n#이달의소녀 #달로그 #LOONA #LOONALOG #HeeJin\n\n▶️ https://t.co/9GetvToLKX https://t.co/fJ5dAFEKPX"
+	},
+	{
 		"created_at": "Thu Aug 11 03:45:37 +0000 2022",
 		"id": 1557570726185558000,
 		"id_str": "1557573927773372418",
