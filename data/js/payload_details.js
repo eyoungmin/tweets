522b5344a05895e04428f138228e6b54503de931
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-11 22:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89769,
-	"created_at": "Thu Aug 11 12:02:06 +0000 2022",
+	"tweets": 89770,
+	"created_at": "Thu Aug 11 13:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Thu Aug 11 12:40:19 +0000 2022",
+		"id": 1557700961266675700,
+		"id_str": "1557708490831003649",
+		"full_text": "[TIkTok📹]\nNewJeans wardrobe edit✨\n\n🔗https://t.co/nHHw94mXpr\n\n#NewJeans #NewJeans_Attention \n#MINJI #HAERIN #DANIELLE #HYEIN #HANNI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			130
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NewJeans",
+					"indices": [
+						61,
+						70
+					]
+				},
+				{
+					"text": "NewJeans_Attention",
+					"indices": [
+						71,
+						90
+					]
+				},
+				{
+					"text": "MINJI",
+					"indices": [
+						92,
+						98
+					]
+				},
+				{
+					"text": "HAERIN",
+					"indices": [
+						99,
+						106
+					]
+				},
+				{
+					"text": "DANIELLE",
+					"indices": [
+						107,
+						116
+					]
+				},
+				{
+					"text": "HYEIN",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "HANNI",
+					"indices": [
+						124,
+						130
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nHHw94mXpr",
+					"expanded_url": "https://vt.tiktok.com/ZSRrsA6b3/?k=1",
+					"display_url": "vt.tiktok.com/ZSRrsA6b3/?k=1",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1476476838180696000,
+			"id_str": "1476476838180696073",
+			"name": "NewJeans",
+			"screen_name": "NewJeans_ADOR",
+			"location": "",
+			"description": "We are NewJeans👖",
+			"url": "https://t.co/wITF1PpBHO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wITF1PpBHO",
+							"expanded_url": "http://newjeans.kr",
+							"display_url": "newjeans.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 312119,
+			"friends_count": 2,
+			"listed_count": 1042,
+			"created_at": "Thu Dec 30 08:54:56 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 103,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1476476838180696073/1658415621",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 703,
+		"favorite_count": 1933,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "[TIkTok📹]\nNewJeans wardrobe edit✨\n\n🔗https://t.co/nHHw94mXpr\n\n#NewJeans #NewJeans_Attention \n#MINJI #HAERIN #DANIELLE #HYEIN #HANNI"
+	},
+	{
 		"created_at": "Thu Aug 11 11:40:22 +0000 2022",
 		"id": 1557687280625627100,
 		"id_str": "1557693402975715328",
