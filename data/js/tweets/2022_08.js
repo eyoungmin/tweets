63df94932343bc1628e72c3e87c0c576a1852032
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-14 02:02:10 +0900

    Added 1 tweets

--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,346 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sat Aug 13 16:22:40 +0000 2022",
+		"id": 1558479358800453600,
+		"id_str": "1558489222956195840",
+		"full_text": "#HallyuPopFest2022 \n\n시드니 별하랑들의 열기로 \nGosh 화끈 달아오른 듯해 🔥❤\n\n!! Love U Sydney q(≧▽≦q) !! 🇦🇺 💖\n\n@hallyupopfest\n@hpfsydney2022\n\n#HPFSydney2022 #KPOP #FeelNewSydney #iLoveSydney https://t.co/D5ssradxDu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HallyuPopFest2022",
+					"indices": [
+						0,
+						18
+					]
+				},
+				{
+					"text": "HPFSydney2022",
+					"indices": [
+						121,
+						135
+					]
+				},
+				{
+					"text": "KPOP",
+					"indices": [
+						136,
+						141
+					]
+				},
+				{
+					"text": "FeelNewSydney",
+					"indices": [
+						142,
+						156
+					]
+				},
+				{
+					"text": "iLoveSydney",
+					"indices": [
+						157,
+						169
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "HallyuPopFest",
+					"name": "HallyuPopFest",
+					"id": 989518000918163500,
+					"id_str": "989518000918163459",
+					"indices": [
+						90,
+						104
+					]
+				},
+				{
+					"screen_name": "hpfsydney2022",
+					"name": "HallyuPopFest Sydney 2022",
+					"id": 1503561504544546800,
+					"id_str": "1503561504544546818",
+					"indices": [
+						105,
+						119
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1558479349837312000,
+					"id_str": "1558479349837312000",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDUtiTVsAAG9d1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDUtiTVsAAG9d1.jpg",
+					"url": "https://t.co/D5ssradxDu",
+					"display_url": "pic.twitter.com/D5ssradxDu",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1558479358800453633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1558479349837312000,
+					"id_str": "1558479349837312000",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDUtiTVsAAG9d1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDUtiTVsAAG9d1.jpg",
+					"url": "https://t.co/D5ssradxDu",
+					"display_url": "pic.twitter.com/D5ssradxDu",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1558479358800453633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558479349833027600,
+					"id_str": "1558479349833027584",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDUtiSUUAA0ZY3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDUtiSUUAA0ZY3.jpg",
+					"url": "https://t.co/D5ssradxDu",
+					"display_url": "pic.twitter.com/D5ssradxDu",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1558479358800453633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558479349828882400,
+					"id_str": "1558479349828882432",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDUtiRVEAAzIDR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDUtiRVEAAzIDR.jpg",
+					"url": "https://t.co/D5ssradxDu",
+					"display_url": "pic.twitter.com/D5ssradxDu",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1558479358800453633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1558479349812121600,
+					"id_str": "1558479349812121602",
+					"indices": [
+						170,
+						193
+					],
+					"media_url": "http://pbs.twimg.com/media/FaDUtiNVUAIf3TO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaDUtiNVUAIf3TO.jpg",
+					"url": "https://t.co/D5ssradxDu",
+					"display_url": "pic.twitter.com/D5ssradxDu",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1558479358800453633/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/r3B14nlZdd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/r3B14nlZdd",
+							"expanded_url": "http://CHUNGHA.lnk.to/BareRarePt1",
+							"display_url": "CHUNGHA.lnk.to/BareRarePt1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 355101,
+			"friends_count": 1,
+			"listed_count": 2190,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1656688172",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 281,
+		"favorite_count": 1082,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#HallyuPopFest2022 \n\n시드니 별하랑들의 열기로 \nGosh 화끈 달아오른 듯해 🔥❤\n\n!! Love U Sydney q(≧▽≦q) !! 🇦🇺 💖\n\n@hallyupopfest\n@hpfsydney2022\n\n#HPFSydney2022 #KPOP #FeelNewSydney #iLoveSydney https://t.co/D5ssradxDu"
+	},
+	{
 		"created_at": "Sat Aug 13 15:40:19 +0000 2022",
 		"id": 1558468443422134300,
 		"id_str": "1558478564135260161",
