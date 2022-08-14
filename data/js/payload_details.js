bb05603cc9af9578ca9f36a1eef6dccd62b00987
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-14 23:02:10 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89865,
-	"created_at": "Sun Aug 14 13:02:06 +0000 2022",
+	"tweets": 89866,
+	"created_at": "Sun Aug 14 14:02:09 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Sun Aug 14 13:40:10 +0000 2022",
+		"id": 1558800870191808500,
+		"id_str": "1558810715850506242",
+		"full_text": "aespa Showcase SYNK in LA✨ | We Them Girls, aespa Take #01\n\nhttps://t.co/7lmkMouyqb\n\n#aespa #æspa #에스파\n#Girls #aespa_Girls\n#WeThemGirls_aespa\n#aespaShowcaseSYNKinLA https://t.co/sj9CaXEGE2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						85,
+						91
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						92,
+						97
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						98,
+						102
+					]
+				},
+				{
+					"text": "Girls",
+					"indices": [
+						103,
+						109
+					]
+				},
+				{
+					"text": "aespa_Girls",
+					"indices": [
+						110,
+						122
+					]
+				},
+				{
+					"text": "WeThemGirls_aespa",
+					"indices": [
+						123,
+						141
+					]
+				},
+				{
+					"text": "aespaShowcaseSYNKinLA",
+					"indices": [
+						142,
+						164
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7lmkMouyqb",
+					"expanded_url": "https://youtu.be/5nx_JBeaWF4",
+					"display_url": "youtu.be/5nx_JBeaWF4",
+					"indices": [
+						60,
+						83
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1558800865011863600,
+					"id_str": "1558800865011863552",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/FaH5ILqakAA9jSO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaH5ILqakAA9jSO.jpg",
+					"url": "https://t.co/sj9CaXEGE2",
+					"display_url": "pic.twitter.com/sj9CaXEGE2",
+					"expanded_url": "https://twitter.com/aespa_official/status/1558800870191808512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1558800865011863600,
+					"id_str": "1558800865011863552",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/FaH5ILqakAA9jSO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaH5ILqakAA9jSO.jpg",
+					"url": "https://t.co/sj9CaXEGE2",
+					"display_url": "pic.twitter.com/sj9CaXEGE2",
+					"expanded_url": "https://twitter.com/aespa_official/status/1558800870191808512/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Girls",
+			"url": "https://t.co/rz16VWutuk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rz16VWutuk",
+							"expanded_url": "https://aespa.lnk.to/GirlsEP",
+							"display_url": "aespa.lnk.to/GirlsEP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3072442,
+			"friends_count": 2,
+			"listed_count": 9295,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1974,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1654009204",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4409,
+		"favorite_count": 13409,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "aespa Showcase SYNK in LA✨ | We Them Girls, aespa Take #01\n\nhttps://t.co/7lmkMouyqb\n\n#aespa #æspa #에스파\n#Girls #aespa_Girls\n#WeThemGirls_aespa\n#aespaShowcaseSYNKinLA https://t.co/sj9CaXEGE2"
+	},
+	{
 		"created_at": "Sun Aug 14 12:02:02 +0000 2022",
 		"id": 1558784169593180200,
 		"id_str": "1558786018500755456",
