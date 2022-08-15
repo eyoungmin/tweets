3e9c8bb6320137e107114f3db3b9cf2a59fc6745
Youngmin Lee <iameyoungmin@gmail.com>
2022-08-15 17:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 89873,
-	"created_at": "Mon Aug 15 07:02:05 +0000 2022",
+	"tweets": 89874,
+	"created_at": "Mon Aug 15 08:02:06 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Mon Aug 15 07:40:10 +0000 2022",
+		"id": 1559081254053564400,
+		"id_str": "1559082506405773312",
+		"full_text": "#배철수의음악캠프 \n8월 15일 광복절🇰🇷부터 일주일 동안 \n알바 DJ 김윤아와 함께 합니다!! \n많은 청취 부탁드립니다!!! \n\n#고양이손 \n#출근길 https://t.co/UqgsbSlodU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "배철수의음악캠프",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"text": "고양이손",
+					"indices": [
+						73,
+						78
+					]
+				},
+				{
+					"text": "출근길",
+					"indices": [
+						80,
+						84
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1559081244616380400,
+					"id_str": "1559081244616380418",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/FaL4IbFVUAIog1X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaL4IbFVUAIog1X.jpg",
+					"url": "https://t.co/UqgsbSlodU",
+					"display_url": "pic.twitter.com/UqgsbSlodU",
+					"expanded_url": "https://twitter.com/love_yuna/status/1559081254053564416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 626,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1105,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1886,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1559081244616380400,
+					"id_str": "1559081244616380418",
+					"indices": [
+						85,
+						108
+					],
+					"media_url": "http://pbs.twimg.com/media/FaL4IbFVUAIog1X.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FaL4IbFVUAIog1X.jpg",
+					"url": "https://t.co/UqgsbSlodU",
+					"display_url": "pic.twitter.com/UqgsbSlodU",
+					"expanded_url": "https://twitter.com/love_yuna/status/1559081254053564416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 626,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1105,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1886,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274776,
+			"friends_count": 585,
+			"listed_count": 2761,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 401,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6788,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1655803174",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 71,
+		"favorite_count": 127,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#배철수의음악캠프 \n8월 15일 광복절🇰🇷부터 일주일 동안 \n알바 DJ 김윤아와 함께 합니다!! \n많은 청취 부탁드립니다!!! \n\n#고양이손 \n#출근길 https://t.co/UqgsbSlodU"
+	},
+	{
 		"created_at": "Mon Aug 15 06:40:10 +0000 2022",
 		"id": 1559064886268702700,
 		"id_str": "1559067407225872385",
