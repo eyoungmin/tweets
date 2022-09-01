df39b40f16f6ef997c139ef6c02b33aefd343796
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-01 19:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90591,
-	"created_at": "Thu Sep 01 09:02:01 +0000 2022",
+	"tweets": 90592,
+	"created_at": "Thu Sep 01 10:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 01 09:40:10 +0000 2022",
+		"id": 1565263196302237700,
+		"id_str": "1565273297369153536",
+		"full_text": "[📢]\n\n『aespa JAPAN PREMIUM SHOWCASE 2022 ～SYNK～』コンビニプリントブロマイド発売決定！\n\n9月5日（月）18：00より全国のファミリーマート、ローソンの店内に設置されているマルチコピー機「らいぶろ」にて発売が決定🙌\n\nぜひチェックしてくださいね🧚‍♀️✨\n\n🔽詳細はこちら\nhttps://t.co/VdP3x69uvR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VdP3x69uvR",
+					"expanded_url": "https://aespa-official.jp/news/synk-bromide/",
+					"display_url": "aespa-official.jp/news/synk-brom…",
+					"indices": [
+						159,
+						182
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1518523440814198800,
+			"id_str": "1518523440814198785",
+			"name": "aespa JAPAN OFFICIAL",
+			"screen_name": "aespaJPofficial",
+			"location": "",
+			"description": "Girls",
+			"url": "https://t.co/XTZiyfF4xZ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/XTZiyfF4xZ",
+							"expanded_url": "https://aespa-official.jp/",
+							"display_url": "aespa-official.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 208171,
+			"friends_count": 1,
+			"listed_count": 569,
+			"created_at": "Mon Apr 25 09:33:15 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 102,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531833190905958400/daNnJIOx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531833190905958400/daNnJIOx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1518523440814198785/1654052470",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 757,
+		"favorite_count": 3860,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "[📢]\n\n『aespa JAPAN PREMIUM SHOWCASE 2022 ～SYNK～』コンビニプリントブロマイド発売決定！\n\n9月5日（月）18：00より全国のファミリーマート、ローソンの店内に設置されているマルチコピー機「らいぶろ」にて発売が決定🙌\n\nぜひチェックしてくださいね🧚‍♀️✨\n\n🔽詳細はこちら\nhttps://t.co/VdP3x69uvR"
+	},
+	{
 		"created_at": "Thu Sep 01 08:40:22 +0000 2022",
 		"id": 1565248224650072000,
 		"id_str": "1565258250081239042",
