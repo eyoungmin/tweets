41e33c391305a0c584aeaf0b04f576011fcf3384
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-01 11:02:09 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90576,
-	"created_at": "Thu Sep 01 01:02:07 +0000 2022",
+	"tweets": 90578,
+	"created_at": "Thu Sep 01 02:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,531 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 01 01:32:40 +0000 2022",
+		"id": 1565143592464568300,
+		"id_str": "1565150613704364033",
+		"full_text": "[#Notice]\n자우림 9월 스케줄을 알려드립니다.\n이후 추가되는 일정은 지속적으로 업데이트 될 예정입니다.\n\n9월에도,\n𝑆𝑡𝑎𝑦 𝑤𝑖𝑡ℎ 𝑱𝑨𝑼𝑹𝑰𝑴 🫶🏻 https://t.co/TBuXoXpRRa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Notice",
+					"indices": [
+						1,
+						8
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1565143587427217400,
+					"id_str": "1565143587427217408",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiBzN8UcAAauK_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiBzN8UcAAauK_.jpg",
+					"url": "https://t.co/TBuXoXpRRa",
+					"display_url": "pic.twitter.com/TBuXoXpRRa",
+					"expanded_url": "https://twitter.com/JaurimOfficial/status/1565143592464568320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1565143587427217400,
+					"id_str": "1565143587427217408",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiBzN8UcAAauK_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiBzN8UcAAauK_.jpg",
+					"url": "https://t.co/TBuXoXpRRa",
+					"display_url": "pic.twitter.com/TBuXoXpRRa",
+					"expanded_url": "https://twitter.com/JaurimOfficial/status/1565143592464568320/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1500,
+							"h": 1500,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 923507155860299800,
+			"id_str": "923507155860299776",
+			"name": "자우림 Jaurim",
+			"screen_name": "JaurimOfficial",
+			"location": "",
+			"description": "▼ 𝐇𝐀𝐏𝐏𝐘 𝟐𝟓𝐭𝐡 𝐉𝐀𝐔𝐑𝐈𝐌 ㅣ𝖬𝖣 온라인 판매",
+			"url": "https://t.co/uKDsnIJttC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uKDsnIJttC",
+							"expanded_url": "https://bit.ly/3BkgXhN",
+							"display_url": "bit.ly/3BkgXhN",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4049,
+			"friends_count": 5,
+			"listed_count": 0,
+			"created_at": "Thu Oct 26 11:10:38 +0000 2017",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 274,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1546734155525869568/z7QclmtR_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1546734155525869568/z7QclmtR_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/923507155860299776/1657605145",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 37,
+		"favorite_count": 51,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#Notice]\n자우림 9월 스케줄을 알려드립니다.\n이후 추가되는 일정은 지속적으로 업데이트 될 예정입니다.\n\n9월에도,\n𝑆𝑡𝑎𝑦 𝑤𝑖𝑡ℎ 𝑱𝑨𝑼𝑹𝑰𝑴 🫶🏻 https://t.co/TBuXoXpRRa"
+	},
+	{
+		"created_at": "Thu Sep 01 01:29:47 +0000 2022",
+		"id": 1565147010768195600,
+		"id_str": "1565149889113329681",
+		"full_text": "&lt;#공조2: 인터내셔날&gt;로 돌아온 배우 #현빈 과 #유해진, #임윤아 그리고 #진선규 와 &lt;#퍼스트룩&gt;이 조우했습니다. 글로벌하게 확장된 스토리만큼이나 화보에서도 압도적인 합을 보여줬는데요. 짜릿한 공조 현장을 245호에서 확인하세요!\n\n#1stLook https://t.co/KhZ7Gx7kHE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "공조2",
+					"indices": [
+						4,
+						8
+					]
+				},
+				{
+					"text": "현빈",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "유해진",
+					"indices": [
+						34,
+						38
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						40,
+						44
+					]
+				},
+				{
+					"text": "진선규",
+					"indices": [
+						49,
+						53
+					]
+				},
+				{
+					"text": "퍼스트룩",
+					"indices": [
+						60,
+						65
+					]
+				},
+				{
+					"text": "1stLook",
+					"indices": [
+						145,
+						153
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1565146997388374000,
+					"id_str": "1565146997388374016",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiE5tCacAAriHB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiE5tCacAAriHB.jpg",
+					"url": "https://t.co/KhZ7Gx7kHE",
+					"display_url": "pic.twitter.com/KhZ7Gx7kHE",
+					"expanded_url": "https://twitter.com/1stlookofficial/status/1565147010768195584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1398,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1565146997388374000,
+					"id_str": "1565146997388374016",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiE5tCacAAriHB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiE5tCacAAriHB.jpg",
+					"url": "https://t.co/KhZ7Gx7kHE",
+					"display_url": "pic.twitter.com/KhZ7Gx7kHE",
+					"expanded_url": "https://twitter.com/1stlookofficial/status/1565147010768195584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1398,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1565146997359018000,
+					"id_str": "1565146997359017984",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiE5s7agAAWK9m.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiE5s7agAAWK9m.jpg",
+					"url": "https://t.co/KhZ7Gx7kHE",
+					"display_url": "pic.twitter.com/KhZ7Gx7kHE",
+					"expanded_url": "https://twitter.com/1stlookofficial/status/1565147010768195584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1398,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1565146997379965000,
+					"id_str": "1565146997379964929",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiE5tAaIAEHFAA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiE5tAaIAEHFAA.jpg",
+					"url": "https://t.co/KhZ7Gx7kHE",
+					"display_url": "pic.twitter.com/KhZ7Gx7kHE",
+					"expanded_url": "https://twitter.com/1stlookofficial/status/1565147010768195584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 819,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 464,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1398,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1565146997363191800,
+					"id_str": "1565146997363191808",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FbiE5s8aMAA0vJi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbiE5s8aMAA0vJi.jpg",
+					"url": "https://t.co/KhZ7Gx7kHE",
+					"display_url": "pic.twitter.com/KhZ7Gx7kHE",
+					"expanded_url": "https://twitter.com/1stlookofficial/status/1565147010768195584/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2036,
+							"h": 1347,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 450,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 794,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 309565415,
+			"id_str": "309565415",
+			"name": "1stlookofficial",
+			"screen_name": "1stlookofficial",
+			"location": "대한민국 서울",
+			"description": "ENTERTAINMENT & LIFESTYLE MAGAZINE  https://t.co/uuKyT8hawL",
+			"url": "http://www.firstlook.co.kr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://www.firstlook.co.kr",
+							"expanded_url": null,
+							"indices": [
+								0,
+								26
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/uuKyT8hawL",
+							"expanded_url": "http://Instagram.com/1stlookofficial",
+							"display_url": "Instagram.com/1stlookofficial",
+							"indices": [
+								36,
+								59
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 206644,
+			"friends_count": 2,
+			"listed_count": 386,
+			"created_at": "Thu Jun 02 09:15:49 +0000 2011",
+			"favourites_count": 489,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7500,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/424003706678296576/C7782RwT_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/424003706678296576/C7782RwT_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/309565415/1660815260",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 407,
+		"favorite_count": 635,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;#공조2: 인터내셔날&gt;로 돌아온 배우 #현빈 과 #유해진, #임윤아 그리고 #진선규 와 &lt;#퍼스트룩&gt;이 조우했습니다. 글로벌하게 확장된 스토리만큼이나 화보에서도 압도적인 합을 보여줬는데요. 짜릿한 공조 현장을 245호에서 확인하세요!\n\n#1stLook https://t.co/KhZ7Gx7kHE"
+	},
+	{
 		"created_at": "Thu Sep 01 00:40:10 +0000 2022",
 		"id": 1565123297066360800,
 		"id_str": "1565137403777515521",
