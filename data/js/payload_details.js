e1e028555c19ec70b689d0fd01f4fccb25b93072
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-03 02:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90645,
-	"created_at": "Fri Sep 02 15:19:54 +0000 2022",
+	"tweets": 90646,
+	"created_at": "Fri Sep 02 17:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Fri Sep 02 16:40:10 +0000 2022",
+		"id": 1565730488886902800,
+		"id_str": "1565741381217296385",
+		"full_text": "병을 앓다. \n\n병이 이끄는대로 \n열을 앓을 때만 꾸는 꿈을 꾸다\n병과 정들 무렵 이틀\n누군가의 꿈에 건너 갔다 오니\n지금이 되었다. \n\n이제는 지금 당장\n꿈에서 나와야 할 시간. \n오늘은 아무 꿈 꾸지 않고 \n죽은 듯이 자라. https://t.co/7EUyeSEOhb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1565730483127795700,
+					"id_str": "1565730483127795712",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/FbqXlCzVQAAwVD9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbqXlCzVQAAwVD9.jpg",
+					"url": "https://t.co/7EUyeSEOhb",
+					"display_url": "pic.twitter.com/7EUyeSEOhb",
+					"expanded_url": "https://twitter.com/love_yuna/status/1565730488886902784/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1565730483127795700,
+					"id_str": "1565730483127795712",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/FbqXlCzVQAAwVD9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbqXlCzVQAAwVD9.jpg",
+					"url": "https://t.co/7EUyeSEOhb",
+					"display_url": "pic.twitter.com/7EUyeSEOhb",
+					"expanded_url": "https://twitter.com/love_yuna/status/1565730488886902784/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274304,
+			"friends_count": 589,
+			"listed_count": 2754,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 406,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6797,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1655803174",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 93,
+		"favorite_count": 216,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "병을 앓다. \n\n병이 이끄는대로 \n열을 앓을 때만 꾸는 꿈을 꾸다\n병과 정들 무렵 이틀\n누군가의 꿈에 건너 갔다 오니\n지금이 되었다. \n\n이제는 지금 당장\n꿈에서 나와야 할 시간. \n오늘은 아무 꿈 꾸지 않고 \n죽은 듯이 자라. https://t.co/7EUyeSEOhb"
+	},
+	{
 		"created_at": "Fri Sep 02 15:09:36 +0000 2022",
 		"id": 1565716248528167000,
 		"id_str": "1565718589759528961",
