4d489a9f12cfbad19f40f25d7ee0cd9a6dc37387
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-04 00:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sat Sep 03 14:40:13 +0000 2022",
+		"id": 1566063335577366500,
+		"id_str": "1566073583251636225",
+		"full_text": "TOKYO GIRLS COLLECTION 🤍\n\n#aespa #æspa #에스파 \n#aespaTGC #TGC https://t.co/4jDAA51xiH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						26,
+						32
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						33,
+						38
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						39,
+						43
+					]
+				},
+				{
+					"text": "aespaTGC",
+					"indices": [
+						45,
+						54
+					]
+				},
+				{
+					"text": "TGC",
+					"indices": [
+						55,
+						59
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1566063320754716700,
+					"id_str": "1566063320754716672",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FbvGSvlagAAzOI-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbvGSvlagAAzOI-.jpg",
+					"url": "https://t.co/4jDAA51xiH",
+					"display_url": "pic.twitter.com/4jDAA51xiH",
+					"expanded_url": "https://twitter.com/aespa_official/status/1566063335577366529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566063320754716700,
+					"id_str": "1566063320754716672",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FbvGSvlagAAzOI-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FbvGSvlagAAzOI-.jpg",
+					"url": "https://t.co/4jDAA51xiH",
+					"display_url": "pic.twitter.com/4jDAA51xiH",
+					"expanded_url": "https://twitter.com/aespa_official/status/1566063335577366529/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Girls",
+			"url": "https://t.co/rz16VWutuk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rz16VWutuk",
+							"expanded_url": "https://aespa.lnk.to/GirlsEP",
+							"display_url": "aespa.lnk.to/GirlsEP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3131499,
+			"friends_count": 2,
+			"listed_count": 9430,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2006,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1654009204",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10004,
+		"favorite_count": 39801,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TOKYO GIRLS COLLECTION 🤍\n\n#aespa #æspa #에스파 \n#aespaTGC #TGC https://t.co/4jDAA51xiH"
+	},
+	{
 		"created_at": "Sat Sep 03 13:40:38 +0000 2022",
 		"id": 1566055977035501600,
 		"id_str": "1566058588405211138",
