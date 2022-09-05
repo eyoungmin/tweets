88f8cca96e3c58fff03f28205c3ac9464e1d11b6
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-05 11:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90702,
-	"created_at": "Sun Sep 04 18:02:02 +0000 2022",
+	"tweets": 90703,
+	"created_at": "Mon Sep 05 02:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,252 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Mon Sep 05 01:29:38 +0000 2022",
+		"id": 1566591950228263000,
+		"id_str": "1566599405741957122",
+		"full_text": "&lt;#공조2_인터내셔날&gt; 개봉 D-2!🔥\nC네마을지기는 예매율에 심장이 철령중😆! #다들_기다렸냐구! \n\n📌지금 바로 예매하고 대세 영화에 탑승 GO!\n\n#현빈 #유해진 #임윤아\n#다니엘헤니 #진선규 #이석훈감독\n#공조 #9월7일대개봉 https://t.co/5OFsV5AFce",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "공조2_인터내셔날",
+					"indices": [
+						4,
+						14
+					]
+				},
+				{
+					"text": "다들_기다렸냐구",
+					"indices": [
+						51,
+						60
+					]
+				},
+				{
+					"text": "현빈",
+					"indices": [
+						91,
+						94
+					]
+				},
+				{
+					"text": "유해진",
+					"indices": [
+						95,
+						99
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "다니엘헤니",
+					"indices": [
+						105,
+						111
+					]
+				},
+				{
+					"text": "진선규",
+					"indices": [
+						112,
+						116
+					]
+				},
+				{
+					"text": "이석훈감독",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "공조",
+					"indices": [
+						124,
+						127
+					]
+				},
+				{
+					"text": "9월7일대개봉",
+					"indices": [
+						128,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1566247696826257400,
+					"id_str": "1566247696826257408",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbxt-1-aUAASqFl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbxt-1-aUAASqFl.jpg",
+					"url": "https://t.co/5OFsV5AFce",
+					"display_url": "pic.twitter.com/5OFsV5AFce",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1566591950228262914/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566247696826257400,
+					"id_str": "1566247696826257408",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/Fbxt-1-aUAASqFl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fbxt-1-aUAASqFl.jpg",
+					"url": "https://t.co/5OFsV5AFce",
+					"display_url": "pic.twitter.com/5OFsV5AFce",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1566591950228262914/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 452664114,
+			"id_str": "452664114",
+			"name": "CJ ENM Movie",
+			"screen_name": "CJENMMOVIE",
+			"location": "",
+			"description": "아시아 No.1 스튜디오 CJ ENM Movie 공식 트위터입니다.\n28년 동안 한 우물만 파고 있는 C네마을🏡",
+			"url": "https://t.co/ag1esso0r7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ag1esso0r7",
+							"expanded_url": "https://linktr.ee/cjenmmovie",
+							"display_url": "linktr.ee/cjenmmovie",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 331776,
+			"friends_count": 10643,
+			"listed_count": 260,
+			"created_at": "Mon Jan 02 03:32:49 +0000 2012",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7427,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/452664114/1662085906",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 24,
+		"favorite_count": 61,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;#공조2_인터내셔날&gt; 개봉 D-2!🔥\nC네마을지기는 예매율에 심장이 철령중😆! #다들_기다렸냐구! \n\n📌지금 바로 예매하고 대세 영화에 탑승 GO!\n\n#현빈 #유해진 #임윤아\n#다니엘헤니 #진선규 #이석훈감독\n#공조 #9월7일대개봉 https://t.co/5OFsV5AFce"
+	},
+	{
 		"created_at": "Sun Sep 04 17:40:10 +0000 2022",
 		"id": 1566174920681349000,
 		"id_str": "1566481259135746054",
