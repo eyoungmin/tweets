84135c7dcf3df632e340b67deaee8dffadca6b3f
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-07 11:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,266 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 07 01:29:39 +0000 2022",
+		"id": 1567316847162171400,
+		"id_str": "1567324183037726722",
+		"full_text": "#공조보유국 에 해가 떴습니다! 🌞 모두 일어나세요~!\n드디어 기다려왔던 &lt;#공조2_인터내셔날&gt;을 만날 시간!🔥\n\n📢#개봉일 극장에서 공조하라! #예매GOGO\n\n#현빈 #유해진 #임윤아\n#다니엘헤니 #진선규 #이석훈감독\n#공조2 #절찬상영중 https://t.co/gcbOBfLwnh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "공조보유국",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "공조2_인터내셔날",
+					"indices": [
+						45,
+						55
+					]
+				},
+				{
+					"text": "개봉일",
+					"indices": [
+						71,
+						75
+					]
+				},
+				{
+					"text": "예매GOGO",
+					"indices": [
+						87,
+						94
+					]
+				},
+				{
+					"text": "현빈",
+					"indices": [
+						96,
+						99
+					]
+				},
+				{
+					"text": "유해진",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "임윤아",
+					"indices": [
+						105,
+						109
+					]
+				},
+				{
+					"text": "다니엘헤니",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "진선규",
+					"indices": [
+						117,
+						121
+					]
+				},
+				{
+					"text": "이석훈감독",
+					"indices": [
+						122,
+						128
+					]
+				},
+				{
+					"text": "공조2",
+					"indices": [
+						129,
+						133
+					]
+				},
+				{
+					"text": "절찬상영중",
+					"indices": [
+						134,
+						140
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1567280323213553700,
+					"id_str": "1567280323213553665",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FcAZJnCacAEvYp-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcAZJnCacAEvYp-.jpg",
+					"url": "https://t.co/gcbOBfLwnh",
+					"display_url": "pic.twitter.com/gcbOBfLwnh",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1567316847162171392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1567280323213553700,
+					"id_str": "1567280323213553665",
+					"indices": [
+						141,
+						164
+					],
+					"media_url": "http://pbs.twimg.com/media/FcAZJnCacAEvYp-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcAZJnCacAEvYp-.jpg",
+					"url": "https://t.co/gcbOBfLwnh",
+					"display_url": "pic.twitter.com/gcbOBfLwnh",
+					"expanded_url": "https://twitter.com/CJENMMOVIE/status/1567316847162171392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 452664114,
+			"id_str": "452664114",
+			"name": "CJ ENM Movie",
+			"screen_name": "CJENMMOVIE",
+			"location": "",
+			"description": "아시아 No.1 스튜디오 CJ ENM Movie 공식 트위터입니다.\n28년 동안 한 우물만 파고 있는 C네마을🏡",
+			"url": "https://t.co/ag1esso0r7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ag1esso0r7",
+							"expanded_url": "https://linktr.ee/cjenmmovie",
+							"display_url": "linktr.ee/cjenmmovie",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 331822,
+			"friends_count": 10640,
+			"listed_count": 260,
+			"created_at": "Mon Jan 02 03:32:49 +0000 2012",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7438,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1412645930050805762/NLrBIhfv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/452664114/1662512201",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 182,
+		"favorite_count": 305,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#공조보유국 에 해가 떴습니다! 🌞 모두 일어나세요~!\n드디어 기다려왔던 &lt;#공조2_인터내셔날&gt;을 만날 시간!🔥\n\n📢#개봉일 극장에서 공조하라! #예매GOGO\n\n#현빈 #유해진 #임윤아\n#다니엘헤니 #진선규 #이석훈감독\n#공조2 #절찬상영중 https://t.co/gcbOBfLwnh"
+	},
+	{
 		"created_at": "Tue Sep 06 23:40:10 +0000 2022",
 		"id": 1567292834272657400,
 		"id_str": "1567296630503636995",
