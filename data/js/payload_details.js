cb7edd118f65d34c2e57945453e87ff6498b1f79
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-07 19:02:03 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90728,
-	"created_at": "Wed Sep 07 09:02:02 +0000 2022",
+	"tweets": 90730,
+	"created_at": "Wed Sep 07 10:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,463 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 07 09:40:17 +0000 2022",
+		"id": 1567437647739584500,
+		"id_str": "1567447654983995397",
+		"full_text": "나경이는 녹음 중🎙️\n\nNaKyoung is recording🎙️\n\nナギョンは録音中🎙️\n\n#tripleS #트리플에스 #トリプルS #KimNaKyoung https://t.co/vvi2ORJoCE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tripleS",
+					"indices": [
+						50,
+						58
+					]
+				},
+				{
+					"text": "트리플에스",
+					"indices": [
+						59,
+						65
+					]
+				},
+				{
+					"text": "トリプルS",
+					"indices": [
+						66,
+						72
+					]
+				},
+				{
+					"text": "KimNaKyoung",
+					"indices": [
+						73,
+						85
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1567436727882764300,
+					"id_str": "1567436727882764288",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/FcCnZj1aUAAitCv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcCnZj1aUAAitCv.jpg",
+					"url": "https://t.co/vvi2ORJoCE",
+					"display_url": "pic.twitter.com/vvi2ORJoCE",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1567437647739584513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1567436727882764300,
+					"id_str": "1567436727882764288",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/media/FcCnZj1aUAAitCv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcCnZj1aUAAitCv.jpg",
+					"url": "https://t.co/vvi2ORJoCE",
+					"display_url": "pic.twitter.com/vvi2ORJoCE",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1567437647739584513/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1509037477369024500,
+			"id_str": "1509037477369024517",
+			"name": "tripleS official",
+			"screen_name": "triplescosmos",
+			"location": "",
+			"description": "tripleS official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 13559,
+			"friends_count": 0,
+			"listed_count": 93,
+			"created_at": "Wed Mar 30 05:19:14 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 418,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1562456104483065857/jNEwBWIt_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1562456104483065857/jNEwBWIt_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1509037477369024517/1661770802",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 39,
+		"favorite_count": 159,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "나경이는 녹음 중🎙️\n\nNaKyoung is recording🎙️\n\nナギョンは録音中🎙️\n\n#tripleS #트리플에스 #トリプルS #KimNaKyoung https://t.co/vvi2ORJoCE"
+	},
+	{
+		"created_at": "Wed Sep 07 09:28:03 +0000 2022",
+		"id": 1567437652382695400,
+		"id_str": "1567444577682071556",
+		"full_text": "[📹] #C_HAind\n청하팀의 릴레이댄스 연습\n\n▶️ https://t.co/U7R2P30vyJ\n\n#청하 #CHUNGHA\n#BareRare #Sparkling\n#CHUNGHA_BareRare\n#CHUNGHA_Sparkling https://t.co/mG3eJzMDnR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "C_HAind",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"text": "청하",
+					"indices": [
+						56,
+						59
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						60,
+						68
+					]
+				},
+				{
+					"text": "BareRare",
+					"indices": [
+						69,
+						78
+					]
+				},
+				{
+					"text": "Sparkling",
+					"indices": [
+						79,
+						89
+					]
+				},
+				{
+					"text": "CHUNGHA_BareRare",
+					"indices": [
+						90,
+						107
+					]
+				},
+				{
+					"text": "CHUNGHA_Sparkling",
+					"indices": [
+						108,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U7R2P30vyJ",
+					"expanded_url": "https://youtu.be/ExJI-ReG5bU",
+					"display_url": "youtu.be/ExJI-ReG5bU",
+					"indices": [
+						31,
+						54
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1566598911740895200,
+					"id_str": "1566598911740895233",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1566598911740895233/pu/img/toM37mKW-Glili2u.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1566598911740895233/pu/img/toM37mKW-Glili2u.jpg",
+					"url": "https://t.co/mG3eJzMDnR",
+					"display_url": "pic.twitter.com/mG3eJzMDnR",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1567437652382695424/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1566598911740895200,
+					"id_str": "1566598911740895233",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1566598911740895233/pu/img/toM37mKW-Glili2u.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1566598911740895233/pu/img/toM37mKW-Glili2u.jpg",
+					"url": "https://t.co/mG3eJzMDnR",
+					"display_url": "pic.twitter.com/mG3eJzMDnR",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1567437652382695424/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 37237,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1566598911740895233/pu/vid/480x270/BiGyjYB0iMCA0jI6.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1566598911740895233/pu/vid/640x360/LGMTARtLYsp1VVv3.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1566598911740895233/pu/pl/Py5cPZMWa4L9dO1g.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1566598911740895233/pu/vid/1280x720/cCxzKiWuwDLE6l9-.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/r3B14nlZdd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/r3B14nlZdd",
+							"expanded_url": "http://CHUNGHA.lnk.to/BareRarePt1",
+							"display_url": "CHUNGHA.lnk.to/BareRarePt1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 356369,
+			"friends_count": 1,
+			"listed_count": 2202,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2542,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1656688172",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 102,
+		"favorite_count": 364,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[📹] #C_HAind\n청하팀의 릴레이댄스 연습\n\n▶️ https://t.co/U7R2P30vyJ\n\n#청하 #CHUNGHA\n#BareRare #Sparkling\n#CHUNGHA_BareRare\n#CHUNGHA_Sparkling https://t.co/mG3eJzMDnR"
+	},
+	{
 		"created_at": "Wed Sep 07 08:40:10 +0000 2022",
 		"id": 1567422874042105900,
 		"id_str": "1567432527584845825",
