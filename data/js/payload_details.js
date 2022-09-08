2e8c3666c4294ca7d412d6e8ccc612f79eff8539
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-08 14:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 90746,
-	"created_at": "Thu Sep 08 04:02:02 +0000 2022",
+	"tweets": 90747,
+	"created_at": "Thu Sep 08 05:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 08 04:34:48 +0000 2022",
+		"id": 1567722771526086700,
+		"id_str": "1567733167951589378",
+		"full_text": "#BLACKPINK 2nd ALBUM [BORN PINK]\nTracklist and DIGIPACK ver. detail page notice have been uploaded\n\n▶️https://t.co/kRL0xa16vy\n\n#블랙핑크 #2ndAlbum #BORNPINK #20220916 #OfflineRelease #YG https://t.co/uhus75L6M1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						127,
+						132
+					]
+				},
+				{
+					"text": "2ndAlbum",
+					"indices": [
+						133,
+						142
+					]
+				},
+				{
+					"text": "BORNPINK",
+					"indices": [
+						143,
+						152
+					]
+				},
+				{
+					"text": "OfflineRelease",
+					"indices": [
+						163,
+						178
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						179,
+						182
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/kRL0xa16vy",
+					"expanded_url": "https://www.facebook.com/BLACKPINKOFFICIAL/posts/628033835353088",
+					"display_url": "facebook.com/BLACKPINKOFFIC…",
+					"indices": [
+						102,
+						125
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1567722146419581000,
+					"id_str": "1567722146419580928",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/FcGq_GvaIAAe74a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcGq_GvaIAAe74a.jpg",
+					"url": "https://t.co/uhus75L6M1",
+					"display_url": "pic.twitter.com/uhus75L6M1",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1567722771526086656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1567722146419581000,
+					"id_str": "1567722146419580928",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/FcGq_GvaIAAe74a.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FcGq_GvaIAAe74a.jpg",
+					"url": "https://t.co/uhus75L6M1",
+					"display_url": "pic.twitter.com/uhus75L6M1",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1567722771526086656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/vkfEFSP9At",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vkfEFSP9At",
+							"expanded_url": "https://lnk.to/YG_BLACKPINK",
+							"display_url": "lnk.to/YG_BLACKPINK",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7364207,
+			"friends_count": 0,
+			"listed_count": 7716,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 655,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1560480890949873664/EDaaz9e6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1560480890949873664/EDaaz9e6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1662480989",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9483,
+		"favorite_count": 28332,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK 2nd ALBUM [BORN PINK]\nTracklist and DIGIPACK ver. detail page notice have been uploaded\n\n▶️https://t.co/kRL0xa16vy\n\n#블랙핑크 #2ndAlbum #BORNPINK #20220916 #OfflineRelease #YG https://t.co/uhus75L6M1"
+	},
+	{
 		"created_at": "Thu Sep 08 03:58:10 +0000 2022",
 		"id": 1567715521428336600,
 		"id_str": "1567723945151057920",
