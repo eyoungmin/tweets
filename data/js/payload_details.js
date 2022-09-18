b3ed0c213953f9c722d4c64a60aa9b7d32e47894
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-18 13:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91073,
-	"created_at": "Sun Sep 18 03:02:06 +0000 2022",
+	"tweets": 91074,
+	"created_at": "Sun Sep 18 04:02:08 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Sun Sep 18 03:40:10 +0000 2022",
+		"id": 1571333194271563800,
+		"id_str": "1571343295040585729",
+		"full_text": "케미 맛집 tripleS  tripleS Chemistry Overload\ntripleS 트리플에스 : SIGNAL PICKED\n\nhttps://t.co/Z4T4qD5HEK\n\n#tripleS #트리플에스 #トリプルS https://t.co/lgFFEvaque",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tripleS",
+					"indices": [
+						98,
+						106
+					]
+				},
+				{
+					"text": "트리플에스",
+					"indices": [
+						107,
+						113
+					]
+				},
+				{
+					"text": "トリプルS",
+					"indices": [
+						114,
+						120
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Z4T4qD5HEK",
+					"expanded_url": "http://youtu.be/Q0cyX_h4ntU",
+					"display_url": "youtu.be/Q0cyX_h4ntU",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1571325693870637000,
+					"id_str": "1571325693870637057",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1571325693870637057/pu/img/HeX7Tm7yVfMmZzDD.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1571325693870637057/pu/img/HeX7Tm7yVfMmZzDD.jpg",
+					"url": "https://t.co/lgFFEvaque",
+					"display_url": "pic.twitter.com/lgFFEvaque",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1571333194271563778/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1571325693870637000,
+					"id_str": "1571325693870637057",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1571325693870637057/pu/img/HeX7Tm7yVfMmZzDD.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1571325693870637057/pu/img/HeX7Tm7yVfMmZzDD.jpg",
+					"url": "https://t.co/lgFFEvaque",
+					"display_url": "pic.twitter.com/lgFFEvaque",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1571333194271563778/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 44096,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1571325693870637057/pu/pl/aI_PaxrCrtVJg5KC.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1571325693870637057/pu/vid/640x360/ZSXjHO7NYrcLY8QR.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1571325693870637057/pu/vid/480x270/Op8KaARZ-x-yuNPh.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1571325693870637057/pu/vid/1280x720/JX2UcVQtWINmkj3R.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1509037477369024500,
+			"id_str": "1509037477369024517",
+			"name": "tripleS official",
+			"screen_name": "triplescosmos",
+			"location": "",
+			"description": "tripleS official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14112,
+			"friends_count": 0,
+			"listed_count": 92,
+			"created_at": "Wed Mar 30 05:19:14 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 484,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1509037477369024517/1662980418",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 29,
+		"favorite_count": 121,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "케미 맛집 tripleS  tripleS Chemistry Overload\ntripleS 트리플에스 : SIGNAL PICKED\n\nhttps://t.co/Z4T4qD5HEK\n\n#tripleS #트리플에스 #トリプルS https://t.co/lgFFEvaque"
+	},
+	{
 		"created_at": "Sun Sep 18 02:40:19 +0000 2022",
 		"id": 1571324619113771000,
 		"id_str": "1571328234062778370",
