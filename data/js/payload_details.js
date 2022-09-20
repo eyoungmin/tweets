f8a04ee71d91577693656703001d0e4b159014da
Youngmin Lee <iameyoungmin@gmail.com>
2022-09-20 16:02:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91146,
-	"created_at": "Tue Sep 20 06:02:05 +0000 2022",
+	"tweets": 91147,
+	"created_at": "Tue Sep 20 07:02:06 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Tue Sep 20 06:40:10 +0000 2022",
+		"id": 1572111086211723300,
+		"id_str": "1572113370635030529",
+		"full_text": "오늘 오후 8시!! 저의 새로운 오브젝트를 만나 보실 수 있어요✌️\n\nToday(Sep.20th) at 8pm KST/7am EST!! Check out my new✌️\n\n今日午後８時！！私たちの新しいオブジェクトも会えます✌️\n\n#tripleS #트리플에스 #トリプルS https://t.co/hg0MmV4Rwq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tripleS",
+					"indices": [
+						126,
+						134
+					]
+				},
+				{
+					"text": "트리플에스",
+					"indices": [
+						135,
+						141
+					]
+				},
+				{
+					"text": "トリプルS",
+					"indices": [
+						142,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1572111065215012900,
+					"id_str": "1572111065215012864",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FdFCrzWaEAAZUM-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdFCrzWaEAAZUM-.jpg",
+					"url": "https://t.co/hg0MmV4Rwq",
+					"display_url": "pic.twitter.com/hg0MmV4Rwq",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1572111086211723265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1572111065215012900,
+					"id_str": "1572111065215012864",
+					"indices": [
+						149,
+						172
+					],
+					"media_url": "http://pbs.twimg.com/media/FdFCrzWaEAAZUM-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FdFCrzWaEAAZUM-.jpg",
+					"url": "https://t.co/hg0MmV4Rwq",
+					"display_url": "pic.twitter.com/hg0MmV4Rwq",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1572111086211723265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1509037477369024500,
+			"id_str": "1509037477369024517",
+			"name": "tripleS official",
+			"screen_name": "triplescosmos",
+			"location": "",
+			"description": "tripleS official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14181,
+			"friends_count": 0,
+			"listed_count": 93,
+			"created_at": "Wed Mar 30 05:19:14 +0000 2022",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 494,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1509037477369024517/1662980418",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 35,
+		"favorite_count": 108,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "오늘 오후 8시!! 저의 새로운 오브젝트를 만나 보실 수 있어요✌️\n\nToday(Sep.20th) at 8pm KST/7am EST!! Check out my new✌️\n\n今日午後８時！！私たちの新しいオブジェクトも会えます✌️\n\n#tripleS #트리플에스 #トリプルS https://t.co/hg0MmV4Rwq"
+	},
+	{
 		"created_at": "Tue Sep 20 05:40:48 +0000 2022",
 		"id": 1572091498799460400,
 		"id_str": "1572098430830346240",
