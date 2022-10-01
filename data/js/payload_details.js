7b4caef3c8d52775b5c6963d0887756804d2bc3c
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-01 20:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91572,
-	"created_at": "Sat Oct 01 10:02:00 +0000 2022",
+	"tweets": 91573,
+	"created_at": "Sat Oct 01 11:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,138 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 01 10:40:13 +0000 2022",
+		"id": 1576150915723059200,
+		"id_str": "1576160047591194626",
+		"full_text": "aespa X Blake Kathryn to release a digital art collection at the Sotheby's gallery in NYC! The meeting of a K-pop group  and a global NFT artist brings music and art from reality to virtual! \n\n#aespa #æspa #에스파 \n#BlakeKathryn #Sothebys",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			235
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "aespa",
+					"indices": [
+						193,
+						199
+					]
+				},
+				{
+					"text": "æspa",
+					"indices": [
+						200,
+						205
+					]
+				},
+				{
+					"text": "에스파",
+					"indices": [
+						206,
+						210
+					]
+				},
+				{
+					"text": "BlakeKathryn",
+					"indices": [
+						212,
+						225
+					]
+				},
+				{
+					"text": "Sothebys",
+					"indices": [
+						226,
+						235
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1576150913349480400,
+		"in_reply_to_status_id_str": "1576150913349480449",
+		"in_reply_to_user_id": 1277453652924366800,
+		"in_reply_to_user_id_str": "1277453652924366848",
+		"in_reply_to_screen_name": "aespa_official",
+		"user": {
+			"id": 1277453652924366800,
+			"id_str": "1277453652924366848",
+			"name": "aespa",
+			"screen_name": "aespa_official",
+			"location": "",
+			"description": "Girls",
+			"url": "https://t.co/rz16VWutuk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rz16VWutuk",
+							"expanded_url": "https://aespa.lnk.to/GirlsEP",
+							"display_url": "aespa.lnk.to/GirlsEP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3188727,
+			"friends_count": 2,
+			"listed_count": 9541,
+			"created_at": "Mon Jun 29 04:08:00 +0000 2020",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2045,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1531651727904370688/XmqAPigu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1277453652924366848/1654009204",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 426,
+		"favorite_count": 1952,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "aespa X Blake Kathryn to release a digital art collection at the Sotheby's gallery in NYC! The meeting of a K-pop group  and a global NFT artist brings music and art from reality to virtual! \n\n#aespa #æspa #에스파 \n#BlakeKathryn #Sothebys"
+	},
+	{
 		"created_at": "Sat Oct 01 09:09:36 +0000 2022",
 		"id": 1576135112470978600,
 		"id_str": "1576137241528877056",
