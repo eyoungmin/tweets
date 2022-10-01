fef2cb2f439ef8b0e9b9ed6999d18c1576a92358
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-01 22:02:01 +0900

    Added 1 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 01 12:56:29 +0000 2022",
+		"id": 1576193952373690400,
+		"id_str": "1576194338240876545",
+		"full_text": "#오늘의_자우림\n과 #파란불_방\n\n#분당파크콘서트 감사합니다. \n🔥🔥🔥🔥🔥🔥💜💜💜 https://t.co/qNXs59KOYO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			45
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "오늘의_자우림",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "파란불_방",
+					"indices": [
+						11,
+						17
+					]
+				},
+				{
+					"text": "분당파크콘서트",
+					"indices": [
+						19,
+						27
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1576193944966176800,
+					"id_str": "1576193944966176770",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_ECrrUoAIJ-Rd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_ECrrUoAIJ-Rd.jpg",
+					"url": "https://t.co/qNXs59KOYO",
+					"display_url": "pic.twitter.com/qNXs59KOYO",
+					"expanded_url": "https://twitter.com/love_yuna/status/1576193952373690368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1023,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1227,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 579,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1576193944966176800,
+					"id_str": "1576193944966176770",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_ECrrUoAIJ-Rd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_ECrrUoAIJ-Rd.jpg",
+					"url": "https://t.co/qNXs59KOYO",
+					"display_url": "pic.twitter.com/qNXs59KOYO",
+					"expanded_url": "https://twitter.com/love_yuna/status/1576193952373690368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1023,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1227,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 579,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1576193944957776000,
+					"id_str": "1576193944957775879",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_ECrpUcAcjan3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_ECrpUcAcjan3.jpg",
+					"url": "https://t.co/qNXs59KOYO",
+					"display_url": "pic.twitter.com/qNXs59KOYO",
+					"expanded_url": "https://twitter.com/love_yuna/status/1576193952373690368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1231,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 581,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1026,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1576193944970416000,
+					"id_str": "1576193944970416128",
+					"indices": [
+						46,
+						69
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd_ECrsVUAAMvH8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd_ECrsVUAAMvH8.jpg",
+					"url": "https://t.co/qNXs59KOYO",
+					"display_url": "pic.twitter.com/qNXs59KOYO",
+					"expanded_url": "https://twitter.com/love_yuna/status/1576193952373690368/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1026,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1231,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 581,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 274280,
+			"friends_count": 594,
+			"listed_count": 2746,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1539176139846848512/0LZtVgA4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1655803174",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 39,
+		"favorite_count": 76,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#오늘의_자우림\n과 #파란불_방\n\n#분당파크콘서트 감사합니다. \n🔥🔥🔥🔥🔥🔥💜💜💜 https://t.co/qNXs59KOYO"
+	},
+	{
 		"created_at": "Sat Oct 01 10:40:13 +0000 2022",
 		"id": 1576150915723059200,
 		"id_str": "1576160047591194626",
