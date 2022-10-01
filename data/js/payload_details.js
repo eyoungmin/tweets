b927c4fcc2b902136a1449c8cb9cd2cd85e7585c
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-01 15:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91568,
-	"created_at": "Sat Oct 01 05:02:00 +0000 2022",
+	"tweets": 91569,
+	"created_at": "Sat Oct 01 06:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 01 05:40:12 +0000 2022",
+		"id": 1576074428689141800,
+		"id_str": "1576084545199382530",
+		"full_text": "[🎥#안방1열비하인드] 순간포착📸 인기가요에 이런일이?! '블링크의 잃어버린 소리를 찾아서' 편\n\n #인기가요 #안방1열비하인드 #블랙핑크 #BLACKPINK @BLACKPINK @ygofficialblink\n\n📌https://t.co/3czWRZT5iM https://t.co/a9iwMibVt8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "안방1열비하인드",
+					"indices": [
+						2,
+						11
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						56,
+						61
+					]
+				},
+				{
+					"text": "안방1열비하인드",
+					"indices": [
+						62,
+						71
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						72,
+						77
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						78,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BLACKPINK",
+					"name": "BLACKPINKOFFICIAL",
+					"id": 1273850774380884000,
+					"id_str": "1273850774380883975",
+					"indices": [
+						89,
+						99
+					]
+				},
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						100,
+						116
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3czWRZT5iM",
+					"expanded_url": "https://youtu.be/nnvJ8J_ZUxU",
+					"display_url": "youtu.be/nnvJ8J_ZUxU",
+					"indices": [
+						119,
+						142
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1575828741736915000,
+					"id_str": "1575828741736914945",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd535DoUYAEoN96.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd535DoUYAEoN96.jpg",
+					"url": "https://t.co/a9iwMibVt8",
+					"display_url": "pic.twitter.com/a9iwMibVt8",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1576074428689141760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575828741736915000,
+					"id_str": "1575828741736914945",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/Fd535DoUYAEoN96.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fd535DoUYAEoN96.jpg",
+					"url": "https://t.co/a9iwMibVt8",
+					"display_url": "pic.twitter.com/a9iwMibVt8",
+					"expanded_url": "https://twitter.com/kpop_sbs/status/1576074428689141760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1131475077650772000,
+			"id_str": "1131475077650771968",
+			"name": "스브스케이팝 X INKIGAYO",
+			"screen_name": "kpop_sbs",
+			"location": "",
+			"description": "♡ 스브스케이팝 / SBS KPOP 공식 트위터 ♡ SBS의 예능/음악방송을 여러분께 더 많이 공유하고 소통하기 위한 채널입니다 :)",
+			"url": "https://t.co/dXfdxsnRTm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/dXfdxsnRTm",
+							"expanded_url": "https://www.youtube.com/user/SBSMusic1",
+							"display_url": "youtube.com/user/SBSMusic1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 736369,
+			"friends_count": 79,
+			"listed_count": 857,
+			"created_at": "Thu May 23 08:20:55 +0000 2019",
+			"favourites_count": 6358,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 6329,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1232179341775138818/Ry7b4M3d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1131475077650771968/1664077594",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1650,
+		"favorite_count": 4308,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[🎥#안방1열비하인드] 순간포착📸 인기가요에 이런일이?! '블링크의 잃어버린 소리를 찾아서' 편\n\n #인기가요 #안방1열비하인드 #블랙핑크 #BLACKPINK @BLACKPINK @ygofficialblink\n\n📌https://t.co/3czWRZT5iM https://t.co/a9iwMibVt8"
+	},
+	{
 		"created_at": "Sat Oct 01 04:40:10 +0000 2022",
 		"id": 1576059327865045000,
 		"id_str": "1576069436791988225",
