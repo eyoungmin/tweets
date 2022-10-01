daa03e77e3fcc898ee9a2046bce8a551055652f3
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-01 18:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91570,
-	"created_at": "Sat Oct 01 07:02:00 +0000 2022",
+	"tweets": 91571,
+	"created_at": "Sat Oct 01 09:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 01 08:40:10 +0000 2022",
+		"id": 1576119765470392300,
+		"id_str": "1576129834983514113",
+		"full_text": "[NewZips📹]\nWelcome to 뉴캠프🚞💨 EP.2\n\n🔗https://t.co/JEBGsq8lFG\n\n#NewJeans #뉴진스 #NewZips   \n#NewJeans_1st_EP #WeAreNewJeans",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NewJeans",
+					"indices": [
+						60,
+						69
+					]
+				},
+				{
+					"text": "뉴진스",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "NewZips",
+					"indices": [
+						75,
+						83
+					]
+				},
+				{
+					"text": "NewJeans_1st_EP",
+					"indices": [
+						87,
+						103
+					]
+				},
+				{
+					"text": "WeAreNewJeans",
+					"indices": [
+						104,
+						118
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JEBGsq8lFG",
+					"expanded_url": "https://youtu.be/W0JP-opIQqo",
+					"display_url": "youtu.be/W0JP-opIQqo",
+					"indices": [
+						35,
+						58
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1476476838180696000,
+			"id_str": "1476476838180696073",
+			"name": "NewJeans",
+			"screen_name": "NewJeans_ADOR",
+			"location": "",
+			"description": "NewJeans Official Twitter",
+			"url": "https://t.co/wITF1P8yFO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wITF1P8yFO",
+							"expanded_url": "http://newjeans.kr",
+							"display_url": "newjeans.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 533262,
+			"friends_count": 4,
+			"listed_count": 1661,
+			"created_at": "Thu Dec 30 08:54:56 +0000 2021",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 273,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1550133532991041537/hQ3QOSP0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1476476838180696073/1658415621",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 624,
+		"favorite_count": 1896,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[NewZips📹]\nWelcome to 뉴캠프🚞💨 EP.2\n\n🔗https://t.co/JEBGsq8lFG\n\n#NewJeans #뉴진스 #NewZips   \n#NewJeans_1st_EP #WeAreNewJeans"
+	},
+	{
 		"created_at": "Sat Oct 01 06:40:10 +0000 2022",
 		"id": 1576096118902956000,
 		"id_str": "1576099635189407744",
