73efd52667e3d6409df32910955091501ec9f07e
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-04 15:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91609,
-	"created_at": "Tue Oct 04 05:02:00 +0000 2022",
+	"tweets": 91610,
+	"created_at": "Tue Oct 04 06:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,284 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Tue Oct 04 05:40:12 +0000 2022",
+		"id": 1577163900797472800,
+		"id_str": "1577171709932838912",
+		"full_text": "💋\n#deardahlia https://t.co/aP3w4HAVuR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "deardahlia",
+					"indices": [
+						2,
+						13
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1577163888965333000,
+					"id_str": "1577163888965332993",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FeM2M2paMAEJ2nA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeM2M2paMAEJ2nA.jpg",
+					"url": "https://t.co/aP3w4HAVuR",
+					"display_url": "pic.twitter.com/aP3w4HAVuR",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1577163900797472768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1577163888965333000,
+					"id_str": "1577163888965332993",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FeM2M2paMAEJ2nA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeM2M2paMAEJ2nA.jpg",
+					"url": "https://t.co/aP3w4HAVuR",
+					"display_url": "pic.twitter.com/aP3w4HAVuR",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1577163900797472768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577163888986312700,
+					"id_str": "1577163888986312705",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FeM2M2uaUAECBHJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeM2M2uaUAECBHJ.jpg",
+					"url": "https://t.co/aP3w4HAVuR",
+					"display_url": "pic.twitter.com/aP3w4HAVuR",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1577163900797472768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577163888982134800,
+					"id_str": "1577163888982134785",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FeM2M2takAEeKk0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeM2M2takAEeKk0.jpg",
+					"url": "https://t.co/aP3w4HAVuR",
+					"display_url": "pic.twitter.com/aP3w4HAVuR",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1577163900797472768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1577163888977936400,
+					"id_str": "1577163888977936385",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/FeM2M2sagAEuj28.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeM2M2sagAEuj28.jpg",
+					"url": "https://t.co/aP3w4HAVuR",
+					"display_url": "pic.twitter.com/aP3w4HAVuR",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1577163900797472768/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1638,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1448497121867436000,
+			"id_str": "1448497121867436037",
+			"name": "nakedbibi",
+			"screen_name": "nakedbibi",
+			"location": "",
+			"description": "BIBI💧💦",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 90508,
+			"friends_count": 4,
+			"listed_count": 214,
+			"created_at": "Thu Oct 14 03:53:27 +0000 2021",
+			"favourites_count": 49,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576411206835109888/NsHSTOWf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576411206835109888/NsHSTOWf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1448497121867436037/1650748326",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 377,
+		"favorite_count": 1527,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "💋\n#deardahlia https://t.co/aP3w4HAVuR"
+	},
+	{
 		"created_at": "Tue Oct 04 04:40:10 +0000 2022",
 		"id": 1577156474526392300,
 		"id_str": "1577156600099475457",
