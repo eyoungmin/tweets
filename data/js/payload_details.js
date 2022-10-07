df8341330f52290a2e81b73d93850e0dbd1bec3e
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-07 16:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91721,
-	"created_at": "Fri Oct 07 06:02:02 +0000 2022",
+	"tweets": 91722,
+	"created_at": "Fri Oct 07 07:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 07 06:55:20 +0000 2022",
+		"id": 1578277559266996200,
+		"id_str": "1578277781028278272",
+		"full_text": "아이유 &amp; 손석구와 함께\n☃️따뜻한 겨울☃️ 준비하세요.\n이 겨울, 콜드제로 다운.\n𝒇𝒓𝒐𝒎 𝑩𝑳𝑨𝑪𝑲𝒀𝑨𝑲\n\nhttps://t.co/C36C5pBEIA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/C36C5pBEIA",
+					"expanded_url": "https://www.instagram.com/p/CjZcDs1Pto1/",
+					"display_url": "instagram.com/p/CjZcDs1Pto1/",
+					"indices": [
+						66,
+						89
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024486566096326700,
+			"id_str": "1024486566096326656",
+			"name": "아이유(IU) 공식 트위터",
+			"screen_name": "_IUofficial",
+			"location": "",
+			"description": "아이유(IU) Official Twitter",
+			"url": "https://t.co/26KBouiO60",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/26KBouiO60",
+							"expanded_url": "http://cafe.daum.net/IU",
+							"display_url": "cafe.daum.net/IU",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1509521,
+			"friends_count": 0,
+			"listed_count": 2388,
+			"created_at": "Wed Aug 01 02:46:46 +0000 2018",
+			"favourites_count": 54,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024486566096326656/1633359681",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 304,
+		"favorite_count": 1035,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "아이유 &amp; 손석구와 함께\n☃️따뜻한 겨울☃️ 준비하세요.\n이 겨울, 콜드제로 다운.\n𝒇𝒓𝒐𝒎 𝑩𝑳𝑨𝑪𝑲𝒀𝑨𝑲\n\nhttps://t.co/C36C5pBEIA"
+	},
+	{
 		"created_at": "Fri Oct 07 05:35:11 +0000 2022",
 		"id": 1578245502948507600,
 		"id_str": "1578257609592692737",
