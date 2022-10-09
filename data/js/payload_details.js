bc5d48bd58bcd43bfb2f18deb5d8ed7ef8c96b7b
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-10 00:19:58 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 91805,
-	"created_at": "Sun Oct 09 15:02:03 +0000 2022",
+	"tweets": 91806,
+	"created_at": "Sun Oct 09 15:19:57 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 09 15:09:36 +0000 2022",
+		"id": 1579125610513039400,
+		"id_str": "1579126941172772864",
+		"full_text": "SEULGI 슬기 '28 Reasons' Performance Video\n\nhttps://t.co/ltBeZ4l8NC\n\n#슬기 #SEULGI\n#레드벨벳 #RedVelvet \n#Performance_Video\n#28Reasons https://t.co/N6ElnPpvdP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "슬기",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "SEULGI",
+					"indices": [
+						71,
+						78
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						79,
+						84
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						85,
+						95
+					]
+				},
+				{
+					"text": "Performance_Video",
+					"indices": [
+						97,
+						115
+					]
+				},
+				{
+					"text": "28Reasons",
+					"indices": [
+						116,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ltBeZ4l8NC",
+					"expanded_url": "https://youtu.be/BVj9S-js8Vg",
+					"display_url": "youtu.be/BVj9S-js8Vg",
+					"indices": [
+						42,
+						65
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1579124462292676600,
+					"id_str": "1579124462292676608",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FeotVPcVsAA4VER.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeotVPcVsAA4VER.jpg",
+					"url": "https://t.co/N6ElnPpvdP",
+					"display_url": "pic.twitter.com/N6ElnPpvdP",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1579125610513039360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1579124462292676600,
+					"id_str": "1579124462292676608",
+					"indices": [
+						127,
+						150
+					],
+					"media_url": "http://pbs.twimg.com/media/FeotVPcVsAA4VER.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeotVPcVsAA4VER.jpg",
+					"url": "https://t.co/N6ElnPpvdP",
+					"display_url": "pic.twitter.com/N6ElnPpvdP",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1579125610513039360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "SEULGI The 1st Mini Album '28 Reasons'",
+			"url": "https://t.co/2lkB1hkeXM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2lkB1hkeXM",
+							"expanded_url": "https://seulgi.lnk.to/28Reasons",
+							"display_url": "seulgi.lnk.to/28Reasons",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5665619,
+			"friends_count": 2,
+			"listed_count": 11361,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 228,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2948,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1569345944289779712/ulkrUMH0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1569345944289779712/ulkrUMH0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1662994810",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4836,
+		"favorite_count": 8390,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SEULGI 슬기 '28 Reasons' Performance Video\n\nhttps://t.co/ltBeZ4l8NC\n\n#슬기 #SEULGI\n#레드벨벳 #RedVelvet \n#Performance_Video\n#28Reasons https://t.co/N6ElnPpvdP"
+	},
+	{
 		"created_at": "Sun Oct 09 14:40:10 +0000 2022",
 		"id": 1579094438948659200,
 		"id_str": "1579119535059136514",
