c4cb8c858773883960a6588c618ce72dbd18d4a2
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-25 01:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92187,
-	"created_at": "Mon Oct 24 15:02:08 +0000 2022",
+	"tweets": 92188,
+	"created_at": "Mon Oct 24 16:02:08 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 24 15:40:10 +0000 2022",
+		"id": 1584557117683310600,
+		"id_str": "1584570451472560129",
+		"full_text": "실리실키의 에피소드 2 SILKY !! \n재밌게 들으셨나요오💎\n\n여러분의 SILKY 최애곡은 무엇인가요! \n알려주시죠 ㅎㅎㅎ\n\nWhat’s your favorite song in “SILKY”?\nTell me about it💗\n\n#sillysilky #silly #silly #rnb #music #indie #pop #singasong #kpop https://t.co/nxfbIlofBA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "sillysilky",
+					"indices": [
+						128,
+						139
+					]
+				},
+				{
+					"text": "silly",
+					"indices": [
+						140,
+						146
+					]
+				},
+				{
+					"text": "silly",
+					"indices": [
+						147,
+						153
+					]
+				},
+				{
+					"text": "rnb",
+					"indices": [
+						154,
+						158
+					]
+				},
+				{
+					"text": "music",
+					"indices": [
+						159,
+						165
+					]
+				},
+				{
+					"text": "indie",
+					"indices": [
+						166,
+						172
+					]
+				},
+				{
+					"text": "pop",
+					"indices": [
+						173,
+						177
+					]
+				},
+				{
+					"text": "singasong",
+					"indices": [
+						178,
+						188
+					]
+				},
+				{
+					"text": "kpop",
+					"indices": [
+						189,
+						194
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1584557108925587500,
+					"id_str": "1584557108925587456",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff16S5PUYAAj9fb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff16S5PUYAAj9fb.jpg",
+					"url": "https://t.co/nxfbIlofBA",
+					"display_url": "pic.twitter.com/nxfbIlofBA",
+					"expanded_url": "https://twitter.com/silly__silky/status/1584557117683310593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1584557108925587500,
+					"id_str": "1584557108925587456",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff16S5PUYAAj9fb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff16S5PUYAAj9fb.jpg",
+					"url": "https://t.co/nxfbIlofBA",
+					"display_url": "pic.twitter.com/nxfbIlofBA",
+					"expanded_url": "https://twitter.com/silly__silky/status/1584557117683310593/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 960,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1474343943890677800,
+			"id_str": "1474343943890677765",
+			"name": "𝓢𝓲𝓵𝓵𝔂 𝓢𝓲𝓵𝓴𝔂",
+			"screen_name": "silly__silky",
+			"location": "",
+			"description": "✨ 𝓢𝓲𝓵𝓵𝔂 𝓢𝓲𝓵𝓴𝔂 ✨",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 28,
+			"friends_count": 2,
+			"listed_count": 1,
+			"created_at": "Fri Dec 24 11:39:40 +0000 2021",
+			"favourites_count": 15,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 25,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1580926387581353984/pLEDAQ9d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1580926387581353984/pLEDAQ9d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1474343943890677765/1652342994",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "실리실키의 에피소드 2 SILKY !! \n재밌게 들으셨나요오💎\n\n여러분의 SILKY 최애곡은 무엇인가요! \n알려주시죠 ㅎㅎㅎ\n\nWhat’s your favorite song in “SILKY”?\nTell me about it💗\n\n#sillysilky #silly #silly #rnb #music #indie #pop #singasong #kpop https://t.co/nxfbIlofBA"
+	},
+	{
 		"created_at": "Mon Oct 24 14:40:28 +0000 2022",
 		"id": 1584547987535310800,
 		"id_str": "1584555427051835392",
