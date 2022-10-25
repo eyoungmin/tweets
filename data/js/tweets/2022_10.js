64d5457aa6ad038628caeeebddbf2b40723e67a3
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-25 22:02:12 +0900

    Added 1 tweets

--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Tue Oct 25 12:40:13 +0000 2022",
+		"id": 1584885414539972600,
+		"id_str": "1584887555790618624",
+		"full_text": "금요일에 만나요💙 \n#Motospeed24 https://t.co/a2gLBixDKb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Motospeed24",
+					"indices": [
+						11,
+						23
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1584885403617996800,
+					"id_str": "1584885403617996801",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff6k4KRaMAEQpSC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff6k4KRaMAEQpSC.jpg",
+					"url": "https://t.co/a2gLBixDKb",
+					"display_url": "pic.twitter.com/a2gLBixDKb",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1584885414539972611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1584885403617996800,
+					"id_str": "1584885403617996801",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff6k4KRaMAEQpSC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff6k4KRaMAEQpSC.jpg",
+					"url": "https://t.co/a2gLBixDKb",
+					"display_url": "pic.twitter.com/a2gLBixDKb",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1584885414539972611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1584885403605422000,
+					"id_str": "1584885403605422083",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff6k4KOaUAMKBYM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff6k4KOaUAMKBYM.jpg",
+					"url": "https://t.co/a2gLBixDKb",
+					"display_url": "pic.twitter.com/a2gLBixDKb",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1584885414539972611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1584885403630588000,
+					"id_str": "1584885403630587904",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff6k4KUaUAARXqh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ff6k4KUaUAARXqh.jpg",
+					"url": "https://t.co/a2gLBixDKb",
+					"display_url": "pic.twitter.com/a2gLBixDKb",
+					"expanded_url": "https://twitter.com/nakedbibi/status/1584885414539972611/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 454,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1366,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1448497121867436000,
+			"id_str": "1448497121867436037",
+			"name": "nakedbibi",
+			"screen_name": "nakedbibi",
+			"location": "",
+			"description": "BIBI💧💦",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 92280,
+			"friends_count": 7,
+			"listed_count": 220,
+			"created_at": "Thu Oct 14 03:53:27 +0000 2021",
+			"favourites_count": 59,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 298,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576411206835109888/NsHSTOWf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576411206835109888/NsHSTOWf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1448497121867436037/1650748326",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 191,
+		"favorite_count": 662,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "금요일에 만나요💙 \n#Motospeed24 https://t.co/a2gLBixDKb"
+	},
+	{
 		"created_at": "Tue Oct 25 11:40:14 +0000 2022",
 		"id": 1584870353289252900,
 		"id_str": "1584872458695352320",
