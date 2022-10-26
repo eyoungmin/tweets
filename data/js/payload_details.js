9d546e4c9381e9989a3ac227633d4b02b7b9bdda
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-26 18:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92225,
-	"created_at": "Wed Oct 26 07:02:09 +0000 2022",
+	"tweets": 92226,
+	"created_at": "Wed Oct 26 09:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 26 08:40:10 +0000 2022",
+		"id": 1585186972716265500,
+		"id_str": "1585189530658918400",
+		"full_text": "tripleS\nAcid Angel from Asia\n\nNaver NOW.\n“The First Stage”\n2022.10.28 9pm KST/8am EST\n\nhttps://t.co/MUWk2Ib8U0\n\n#tripleS #트리플에스 #AcidAngelfromAsia #애시드엔젤프롬에이시아 #tripleS_AAA\n#Generation #ACCESS https://t.co/SUhHP6kE1l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			192
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tripleS",
+					"indices": [
+						112,
+						120
+					]
+				},
+				{
+					"text": "트리플에스",
+					"indices": [
+						121,
+						127
+					]
+				},
+				{
+					"text": "AcidAngelfromAsia",
+					"indices": [
+						128,
+						146
+					]
+				},
+				{
+					"text": "애시드엔젤프롬에이시아",
+					"indices": [
+						147,
+						159
+					]
+				},
+				{
+					"text": "tripleS_AAA",
+					"indices": [
+						160,
+						172
+					]
+				},
+				{
+					"text": "Generation",
+					"indices": [
+						173,
+						184
+					]
+				},
+				{
+					"text": "ACCESS",
+					"indices": [
+						185,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MUWk2Ib8U0",
+					"expanded_url": "https://now.naver.com/l/1739252",
+					"display_url": "now.naver.com/l/1739252",
+					"indices": [
+						87,
+						110
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1585186864285487000,
+					"id_str": "1585186864285487104",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff-3DexaEAAvxzG.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ff-3DexaEAAvxzG.png",
+					"url": "https://t.co/SUhHP6kE1l",
+					"display_url": "pic.twitter.com/SUhHP6kE1l",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1585186972716265472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1585186864285487000,
+					"id_str": "1585186864285487104",
+					"indices": [
+						193,
+						216
+					],
+					"media_url": "http://pbs.twimg.com/media/Ff-3DexaEAAvxzG.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ff-3DexaEAAvxzG.png",
+					"url": "https://t.co/SUhHP6kE1l",
+					"display_url": "pic.twitter.com/SUhHP6kE1l",
+					"expanded_url": "https://twitter.com/triplescosmos/status/1585186972716265472/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1509037477369024500,
+			"id_str": "1509037477369024517",
+			"name": "tripleS official",
+			"screen_name": "triplescosmos",
+			"location": "",
+			"description": "tripleS official",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18577,
+			"friends_count": 0,
+			"listed_count": 113,
+			"created_at": "Wed Mar 30 05:19:14 +0000 2022",
+			"favourites_count": 2,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 723,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1568977752501678081/6kQiFXx8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1509037477369024517/1662980418",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 198,
+		"favorite_count": 376,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "tripleS\nAcid Angel from Asia\n\nNaver NOW.\n“The First Stage”\n2022.10.28 9pm KST/8am EST\n\nhttps://t.co/MUWk2Ib8U0\n\n#tripleS #트리플에스 #AcidAngelfromAsia #애시드엔젤프롬에이시아 #tripleS_AAA\n#Generation #ACCESS https://t.co/SUhHP6kE1l"
+	},
+	{
 		"created_at": "Wed Oct 26 06:40:10 +0000 2022",
 		"id": 1585149224282251300,
 		"id_str": "1585159331493847040",
