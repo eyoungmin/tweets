d334106682681842c359468b1aad02f12def6a07
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-27 00:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92233,
-	"created_at": "Wed Oct 26 14:02:10 +0000 2022",
+	"tweets": 92234,
+	"created_at": "Wed Oct 26 15:02:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 26 14:40:10 +0000 2022",
+		"id": 1585278659967029200,
+		"id_str": "1585280127629492224",
+		"full_text": "from 🌞 to 🍁 thanks to everyone who came to our North America tour ❤️ https://t.co/nNjvjnKfqq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1585278654334001200,
+					"id_str": "1585278654334001154",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/FgAKiXWWYAIBcnD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgAKiXWWYAIBcnD.jpg",
+					"url": "https://t.co/nNjvjnKfqq",
+					"display_url": "pic.twitter.com/nNjvjnKfqq",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1585278659967029248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1146,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1146,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1585278654334001200,
+					"id_str": "1585278654334001154",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/FgAKiXWWYAIBcnD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FgAKiXWWYAIBcnD.jpg",
+					"url": "https://t.co/nNjvjnKfqq",
+					"display_url": "pic.twitter.com/nNjvjnKfqq",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1585278659967029248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1170,
+							"h": 1146,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1170,
+							"h": 1146,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 666,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "How To Let Go out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"url": "https://t.co/POgzOrDXO4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/POgzOrDXO4",
+							"expanded_url": "https://Sigrid.lnk.to/htlgspecialedition",
+							"display_url": "Sigrid.lnk.to/htlgspecialedi…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89378,
+			"friends_count": 271,
+			"listed_count": 333,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 6169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4459,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1651819275",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 119,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "from 🌞 to 🍁 thanks to everyone who came to our North America tour ❤️ https://t.co/nNjvjnKfqq"
+	},
+	{
 		"created_at": "Wed Oct 26 13:40:10 +0000 2022",
 		"id": 1585254923481993200,
 		"id_str": "1585265028713807873",
