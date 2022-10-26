c23abeff82d4b3d1c50af85b71b00a9352cbc858
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-26 13:02:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92223,
-	"created_at": "Wed Oct 26 02:02:13 +0000 2022",
+	"tweets": 92224,
+	"created_at": "Wed Oct 26 04:02:08 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 26 03:40:10 +0000 2022",
+		"id": 1585106844863037400,
+		"id_str": "1585114035145555970",
+		"full_text": "🧡 Gracias Buenos Aires \n\n📡 🍿Watch the show in cinemas worldwide Oct 28 / 29. Info / tickets https://t.co/8MXtkUjuIb https://t.co/IlwzGPAirp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8MXtkUjuIb",
+					"expanded_url": "http://www.coldplaycinema.live",
+					"display_url": "coldplaycinema.live",
+					"indices": [
+						92,
+						115
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1585106745399074800,
+					"id_str": "1585106745399074817",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1585106745399074817/pu/img/YD349Zxmy1-wwVKe.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1585106745399074817/pu/img/YD349Zxmy1-wwVKe.jpg",
+					"url": "https://t.co/IlwzGPAirp",
+					"display_url": "pic.twitter.com/IlwzGPAirp",
+					"expanded_url": "https://twitter.com/coldplay/status/1585106844863037440/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1585106745399074800,
+					"id_str": "1585106745399074817",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1585106745399074817/pu/img/YD349Zxmy1-wwVKe.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1585106745399074817/pu/img/YD349Zxmy1-wwVKe.jpg",
+					"url": "https://t.co/IlwzGPAirp",
+					"display_url": "pic.twitter.com/IlwzGPAirp",
+					"expanded_url": "https://twitter.com/coldplay/status/1585106844863037440/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 22725,
+						"variants": [
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585106745399074817/pu/vid/320x568/-i3DKmLjy8FcTrAN.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585106745399074817/pu/vid/480x852/l2Xa_WWO8YDhFU3q.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585106745399074817/pu/vid/720x1280/zXfAgJGRS33KUpdX.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1585106745399074817/pu/pl/UPyVZtfA1FTtFos4.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Santiago, Chile",
+			"description": "The official Twitter of the band Coldplay. Latest album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022/23.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23703520,
+			"friends_count": 779,
+			"listed_count": 42389,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1657,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9460,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1423,
+		"favorite_count": 7360,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🧡 Gracias Buenos Aires \n\n📡 🍿Watch the show in cinemas worldwide Oct 28 / 29. Info / tickets https://t.co/8MXtkUjuIb https://t.co/IlwzGPAirp"
+	},
+	{
 		"created_at": "Wed Oct 26 01:40:10 +0000 2022",
 		"id": 1585070547553382400,
 		"id_str": "1585083835502497792",
