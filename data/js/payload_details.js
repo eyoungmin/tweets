759705bf7ba3509db24f128f377d8fa7406e5359
Youngmin Lee <iameyoungmin@gmail.com>
2022-10-28 04:02:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92268,
-	"created_at": "Thu Oct 27 17:02:07 +0000 2022",
+	"tweets": 92269,
+	"created_at": "Thu Oct 27 19:02:11 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,230 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Thu Oct 27 18:40:23 +0000 2022",
+		"id": 1585690441642541000,
+		"id_str": "1585702970687201284",
+		"full_text": "Who's watching the live broadcast in costume this Halloween weekend? 🎃\n\n📡🍿 Watch the LIVE BROADCAST from BUENOS AIRES in cinemas worldwide Oct 28 / 29. Info / tickets https://t.co/vIuX5yvzut \n\n#ColdplayLiveBroadcast https://t.co/GKISSgaNWy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			215
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ColdplayLiveBroadcast",
+					"indices": [
+						193,
+						215
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vIuX5yvzut",
+					"expanded_url": "http://coldplaycinema.live",
+					"display_url": "coldplaycinema.live",
+					"indices": [
+						167,
+						190
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1585690364672868400,
+					"id_str": "1585690364672868382",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1585690364672868382/pu/img/jAZLRFCJAB82yiYx.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1585690364672868382/pu/img/jAZLRFCJAB82yiYx.jpg",
+					"url": "https://t.co/GKISSgaNWy",
+					"display_url": "pic.twitter.com/GKISSgaNWy",
+					"expanded_url": "https://twitter.com/coldplay/status/1585690441642541082/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1585690364672868400,
+					"id_str": "1585690364672868382",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1585690364672868382/pu/img/jAZLRFCJAB82yiYx.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1585690364672868382/pu/img/jAZLRFCJAB82yiYx.jpg",
+					"url": "https://t.co/GKISSgaNWy",
+					"display_url": "pic.twitter.com/GKISSgaNWy",
+					"expanded_url": "https://twitter.com/coldplay/status/1585690441642541082/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 13760,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1585690364672868382/pu/pl/LyknFJekQ0_fIo9Q.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585690364672868382/pu/vid/720x900/fxWRPV0Y67_QQ3dS.mp4?tag=12"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585690364672868382/pu/vid/320x400/DFYaPFQw3u3GY-Rs.mp4?tag=12"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1585690364672868382/pu/vid/480x600/id0BpEaVapBWqu33.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Buenos Aires, Argentina",
+			"description": "The official Twitter of the band Coldplay. Latest album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022/23.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23720784,
+			"friends_count": 779,
+			"listed_count": 42397,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1659,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9465,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 498,
+		"favorite_count": 3535,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Who's watching the live broadcast in costume this Halloween weekend? 🎃\n\n📡🍿 Watch the LIVE BROADCAST from BUENOS AIRES in cinemas worldwide Oct 28 / 29. Info / tickets https://t.co/vIuX5yvzut \n\n#ColdplayLiveBroadcast https://t.co/GKISSgaNWy"
+	},
+	{
 		"created_at": "Thu Oct 27 16:40:37 +0000 2022",
 		"id": 1585650552444457000,
 		"id_str": "1585672827365105666",
