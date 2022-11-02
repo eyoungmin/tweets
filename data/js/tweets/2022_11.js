0e970ab87453e82c55f3ea1b84d350b138ec4f67
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-03 04:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 02 18:40:10 +0000 2022",
+		"id": 1587874291227693000,
+		"id_str": "1587877240343232512",
+		"full_text": "멀어져야 또렷해지는 것들과\n\n혼자가 되어야 알게 되는 것들은\n\n괴로운 장거리 비행의  불편한 동반자\n\n이상한 세상에서 \n나는 행복을 찾아\n\n이상한 여행길에 올랐지. \n\n#스파이크존즈_괴물들이사는나라\n#서이제_두개골의안과밖 https://t.co/flDku0AXR6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "스파이크존즈_괴물들이사는나라",
+					"indices": [
+						94,
+						110
+					]
+				},
+				{
+					"text": "서이제_두개골의안과밖",
+					"indices": [
+						111,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1587874278820618200,
+					"id_str": "1587874278820618240",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FglDPmEVIAAL_HT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FglDPmEVIAAL_HT.jpg",
+					"url": "https://t.co/flDku0AXR6",
+					"display_url": "pic.twitter.com/flDku0AXR6",
+					"expanded_url": "https://twitter.com/love_yuna/status/1587874291227693057/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 672,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1186,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1457,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1587874278820618200,
+					"id_str": "1587874278820618240",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FglDPmEVIAAL_HT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FglDPmEVIAAL_HT.jpg",
+					"url": "https://t.co/flDku0AXR6",
+					"display_url": "pic.twitter.com/flDku0AXR6",
+					"expanded_url": "https://twitter.com/love_yuna/status/1587874291227693057/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 672,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1186,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1457,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1587874278845784000,
+					"id_str": "1587874278845784064",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FglDPmKVIAA31Gc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FglDPmKVIAA31Gc.jpg",
+					"url": "https://t.co/flDku0AXR6",
+					"display_url": "pic.twitter.com/flDku0AXR6",
+					"expanded_url": "https://twitter.com/love_yuna/status/1587874291227693057/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 672,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1138,
+							"h": 1152,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1138,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1587653547981959200,
+		"in_reply_to_status_id_str": "1587653547981959169",
+		"in_reply_to_user_id": 120741919,
+		"in_reply_to_user_id_str": "120741919",
+		"in_reply_to_screen_name": "love_yuna",
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275638,
+			"friends_count": 597,
+			"listed_count": 2740,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 457,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6950,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1667288883",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "멀어져야 또렷해지는 것들과\n\n혼자가 되어야 알게 되는 것들은\n\n괴로운 장거리 비행의  불편한 동반자\n\n이상한 세상에서 \n나는 행복을 찾아\n\n이상한 여행길에 올랐지. \n\n#스파이크존즈_괴물들이사는나라\n#서이제_두개골의안과밖 https://t.co/flDku0AXR6"
+	},
+	{
 		"created_at": "Wed Nov 02 13:40:10 +0000 2022",
 		"id": 1587443310997692400,
 		"id_str": "1587801745039695878",
