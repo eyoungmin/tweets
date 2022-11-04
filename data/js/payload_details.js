3c0fd3007f4d52e9eb5b5a183ef87968d650312f
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-05 04:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92381,
-	"created_at": "Fri Nov 04 17:02:05 +0000 2022",
+	"tweets": 92382,
+	"created_at": "Fri Nov 04 19:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 04 18:40:10 +0000 2022",
+		"id": 1588593840856117200,
+		"id_str": "1588602016523292672",
+		"full_text": "A Sky Full Of Stars // River Plate stadium // 29 October\n\nThank you to our official logistics partner @DeutschePostDHL for getting all our equipment to Buenos Aires with the lowest possible emissions.  PH https://t.co/74voGfWnNq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			204
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DeutschePostDHL",
+					"name": "DPDHL News",
+					"id": 197352549,
+					"id_str": "197352549",
+					"indices": [
+						102,
+						118
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1588593764041461800,
+					"id_str": "1588593764041461762",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1588593764041461762/pu/img/4-Otq1Tv70C42uY-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1588593764041461762/pu/img/4-Otq1Tv70C42uY-.jpg",
+					"url": "https://t.co/74voGfWnNq",
+					"display_url": "pic.twitter.com/74voGfWnNq",
+					"expanded_url": "https://twitter.com/coldplay/status/1588593840856117248/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1588593764041461800,
+					"id_str": "1588593764041461762",
+					"indices": [
+						205,
+						228
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1588593764041461762/pu/img/4-Otq1Tv70C42uY-.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1588593764041461762/pu/img/4-Otq1Tv70C42uY-.jpg",
+					"url": "https://t.co/74voGfWnNq",
+					"display_url": "pic.twitter.com/74voGfWnNq",
+					"expanded_url": "https://twitter.com/coldplay/status/1588593840856117248/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"duration_millis": 43920,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1588593764041461762/pu/vid/960x720/Do6uv02927jaxMKd.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1588593764041461762/pu/vid/480x360/Qg4ti0plMuthkGTm.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1588593764041461762/pu/pl/1O-NZd3hL4igETE_.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1588593764041461762/pu/vid/360x270/qmnNuFkzsXXZKtkg.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18863815,
+			"id_str": "18863815",
+			"name": "Coldplay",
+			"screen_name": "coldplay",
+			"location": "Buenos Aires, Argentina",
+			"description": "The official Twitter of the band Coldplay. Latest album Music Of The Spheres out now. Music Of The Spheres World Tour in 2022/23.",
+			"url": "https://t.co/zVkAnvGWS0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zVkAnvGWS0",
+							"expanded_url": "https://linktr.ee/coldplay",
+							"display_url": "linktr.ee/coldplay",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23750637,
+			"friends_count": 780,
+			"listed_count": 42304,
+			"created_at": "Sun Jan 11 11:04:45 +0000 2009",
+			"favourites_count": 1678,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 9483,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1417506973877211138/YIm7dOQH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18863815/1634283328",
+			"profile_link_color": "11518C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 878,
+		"favorite_count": 5739,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A Sky Full Of Stars // River Plate stadium // 29 October\n\nThank you to our official logistics partner @DeutschePostDHL for getting all our equipment to Buenos Aires with the lowest possible emissions.  PH https://t.co/74voGfWnNq"
+	},
+	{
 		"created_at": "Fri Nov 04 16:40:10 +0000 2022",
 		"id": 1588560349187694600,
 		"id_str": "1588571817211564034",
@@ -38,7 +255,7 @@ Grailbird.data.tweets_2022_11 =
 			"name": "sigrid",
 			"screen_name": "thisissigrid",
 			"location": "",
-			"description": "How To Let Go out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"description": "How To Let Go out now ❤️🔥❤️🔥❤️🔥",
 			"url": "https://t.co/POgzOrDXO4",
 			"entities": {
 				"url": {
