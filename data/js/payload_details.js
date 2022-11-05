75151af3d76e579ae9edbf333c259d998e45a31c
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-06 02:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92385,
-	"created_at": "Sat Nov 05 10:02:00 +0000 2022",
+	"tweets": 92386,
+	"created_at": "Sat Nov 05 17:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,192 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sat Nov 05 16:40:10 +0000 2022",
+		"id": 1588927452709261300,
+		"id_str": "1588934205974712323",
+		"full_text": "thank u sm Liverpoooool it was a lovely first night of the tour ♥️ xx \nsold out Cardiff tonight! photo by @Rory_Barnes https://t.co/lPaYFPgwlm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			118
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Rory_Barnes",
+					"name": "R O R Y B A R N E S",
+					"id": 541957735,
+					"id_str": "541957735",
+					"indices": [
+						106,
+						118
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1588927445494907000,
+					"id_str": "1588927445494906880",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg0BF9qXwAAG5-W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg0BF9qXwAAG5-W.jpg",
+					"url": "https://t.co/lPaYFPgwlm",
+					"display_url": "pic.twitter.com/lPaYFPgwlm",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1588927452709261319/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1588927445494907000,
+					"id_str": "1588927445494906880",
+					"indices": [
+						119,
+						142
+					],
+					"media_url": "http://pbs.twimg.com/media/Fg0BF9qXwAAG5-W.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fg0BF9qXwAAG5-W.jpg",
+					"url": "https://t.co/lPaYFPgwlm",
+					"display_url": "pic.twitter.com/lPaYFPgwlm",
+					"expanded_url": "https://twitter.com/thisissigrid/status/1588927452709261319/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 819042492,
+			"id_str": "819042492",
+			"name": "sigrid",
+			"screen_name": "thisissigrid",
+			"location": "",
+			"description": "How To Let Go out now ❤️‍🔥❤️‍🔥❤️‍🔥",
+			"url": "https://t.co/POgzOrDXO4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/POgzOrDXO4",
+							"expanded_url": "https://Sigrid.lnk.to/htlgspecialedition",
+							"display_url": "Sigrid.lnk.to/htlgspecialedi…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89247,
+			"friends_count": 271,
+			"listed_count": 333,
+			"created_at": "Wed Sep 12 07:27:30 +0000 2012",
+			"favourites_count": 6143,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4465,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1501618301633544195/J0-lKLKc_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/819042492/1651819275",
+			"profile_link_color": "99DFFF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 128,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "thank u sm Liverpoooool it was a lovely first night of the tour ♥️ xx \nsold out Cardiff tonight! photo by @Rory_Barnes https://t.co/lPaYFPgwlm"
+	},
+	{
 		"created_at": "Sat Nov 05 09:41:41 +0000 2022",
 		"id": 1588818412251201500,
 		"id_str": "1588828890146840576",
