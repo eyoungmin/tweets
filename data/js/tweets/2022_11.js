d162eae2921f1983740dff43ca28ad7087a2ed2b
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-08 17:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,10 +1,273 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Tue Nov 08 07:40:14 +0000 2022",
+		"id": 1589875604790608000,
+		"id_str": "1589885492480602112",
+		"full_text": "#트리플에스 #tripleS #tripleS_AAA #데뷔축하 \n\n드디어 데뷔했다. 고맙고 축하해♥\n(진) 평생 꽃길만 걸었으면 좋겠어 (심)\n\n모든 가능성의 아이돌!\ntripleS의 포토카드 Objekt가 곧 GS25에 출시 예정이래요!\n(자세한 내용은 11/11일에 댓글과 GS25 공식 인스타그램에서)\n#포카 #포토카드 #굿즈 #GS25 https://t.co/Dt1YOmyOUn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "트리플에스",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"text": "tripleS",
+					"indices": [
+						7,
+						15
+					]
+				},
+				{
+					"text": "tripleS_AAA",
+					"indices": [
+						16,
+						28
+					]
+				},
+				{
+					"text": "데뷔축하",
+					"indices": [
+						29,
+						34
+					]
+				},
+				{
+					"text": "포카",
+					"indices": [
+						172,
+						175
+					]
+				},
+				{
+					"text": "포토카드",
+					"indices": [
+						176,
+						181
+					]
+				},
+				{
+					"text": "굿즈",
+					"indices": [
+						182,
+						185
+					]
+				},
+				{
+					"text": "GS25",
+					"indices": [
+						186,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1589875133081997300,
+					"id_str": "1589875133081997313",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1589875133081997313/pu/img/wE_TbtPZq9JHVXct.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1589875133081997313/pu/img/wE_TbtPZq9JHVXct.jpg",
+					"url": "https://t.co/Dt1YOmyOUn",
+					"display_url": "pic.twitter.com/Dt1YOmyOUn",
+					"expanded_url": "https://twitter.com/funGS25/status/1589875604790607874/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1589875133081997300,
+					"id_str": "1589875133081997313",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1589875133081997313/pu/img/wE_TbtPZq9JHVXct.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1589875133081997313/pu/img/wE_TbtPZq9JHVXct.jpg",
+					"url": "https://t.co/Dt1YOmyOUn",
+					"display_url": "pic.twitter.com/Dt1YOmyOUn",
+					"expanded_url": "https://twitter.com/funGS25/status/1589875604790607874/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 15000,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1589875133081997313/pu/pl/FrOLm5HEt4InAjCo.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1589875133081997313/pu/vid/640x360/kd5ZYks1MzTmQsJh.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1589875133081997313/pu/vid/480x270/J3gJUcN3pt53k2SI.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1589875133081997313/pu/vid/1280x720/ajH9gnPXaG1vio8Z.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 132803402,
+			"id_str": "132803402",
+			"name": "GS25",
+			"screen_name": "funGS25",
+			"location": "",
+			"description": "재미있는 라이프스타일 플랫폼!\nGS25 공식 트위터입니다♥",
+			"url": "https://t.co/nG4AEUHv4Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nG4AEUHv4Q",
+							"expanded_url": "https://www.instagram.com/gs25_official/",
+							"display_url": "instagram.com/gs25_official/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16918,
+			"friends_count": 4194,
+			"listed_count": 205,
+			"created_at": "Wed Apr 14 06:33:24 +0000 2010",
+			"favourites_count": 2065,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19529,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117593335164592130/kWwlu-IS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117593335164592130/kWwlu-IS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/132803402/1667870501",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 151,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#트리플에스 #tripleS #tripleS_AAA #데뷔축하 \n\n드디어 데뷔했다. 고맙고 축하해♥\n(진) 평생 꽃길만 걸었으면 좋겠어 (심)\n\n모든 가능성의 아이돌!\ntripleS의 포토카드 Objekt가 곧 GS25에 출시 예정이래요!\n(자세한 내용은 11/11일에 댓글과 GS25 공식 인스타그램에서)\n#포카 #포토카드 #굿즈 #GS25 https://t.co/Dt1YOmyOUn"
+	},
+	{
 		"created_at": "Tue Nov 08 05:40:10 +0000 2022",
 		"id": 1589854729403699200,
 		"id_str": "1589855277913972738",
-		"full_text": "[💚📲]\n\nLINE公式アカウントにメッセージが届きました⭐️\n\n☑️お友達登録はこちら🧚‍♂️\nhttps://t.co/Ocf7Q7pR52 https://t.co/mivDFY8a96",
+		"full_text": "[💚📲]\n\nLINE公式アカウントにメッセージが届きました⭐️\n\n☑️お友達登録はこちら🧚♂️\nhttps://t.co/Ocf7Q7pR52 https://t.co/mivDFY8a96",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -183,7 +446,7 @@ Grailbird.data.tweets_2022_11 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ja",
-		"text": "[💚📲]\n\nLINE公式アカウントにメッセージが届きました⭐️\n\n☑️お友達登録はこちら🧚‍♂️\nhttps://t.co/Ocf7Q7pR52 https://t.co/mivDFY8a96"
+		"text": "[💚📲]\n\nLINE公式アカウントにメッセージが届きました⭐️\n\n☑️お友達登録はこちら🧚♂️\nhttps://t.co/Ocf7Q7pR52 https://t.co/mivDFY8a96"
 	},
 	{
 		"created_at": "Tue Nov 08 03:40:41 +0000 2022",
