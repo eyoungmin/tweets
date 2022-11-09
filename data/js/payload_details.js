ce4502d69bdce51d6307432cc94717b1ba2a6422
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-09 17:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92467,
-	"created_at": "Wed Nov 09 07:02:02 +0000 2022",
+	"tweets": 92468,
+	"created_at": "Wed Nov 09 08:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 09 07:56:05 +0000 2022",
+		"id": 1590237754658414600,
+		"id_str": "1590251868084588544",
+		"full_text": "지금 날씨에 입기 좋은 #깔깔이 #효민 이 선택한 러플 카라는 어때? #퀼팅재킷 \n#표예진 #클레어로즈 #clairerose #최수영 #소녀시대 #장재인\nhttps://t.co/LgpdK7Rjcc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "깔깔이",
+					"indices": [
+						13,
+						17
+					]
+				},
+				{
+					"text": "효민",
+					"indices": [
+						18,
+						21
+					]
+				},
+				{
+					"text": "퀼팅재킷",
+					"indices": [
+						39,
+						44
+					]
+				},
+				{
+					"text": "표예진",
+					"indices": [
+						46,
+						50
+					]
+				},
+				{
+					"text": "클레어로즈",
+					"indices": [
+						51,
+						57
+					]
+				},
+				{
+					"text": "clairerose",
+					"indices": [
+						58,
+						69
+					]
+				},
+				{
+					"text": "최수영",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						75,
+						80
+					]
+				},
+				{
+					"text": "장재인",
+					"indices": [
+						81,
+						85
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LgpdK7Rjcc",
+					"expanded_url": "https://www.allurekorea.com/?p=187323&ddw=128411&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "allurekorea.com/?p=187323&ddw=…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262307,
+			"id_str": "78262307",
+			"name": "allure korea",
+			"screen_name": "allurekorea",
+			"location": "Seoul",
+			"description": "beauty & style expert, Allure Magazine Korea",
+			"url": "https://t.co/OcX4RMNXUs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OcX4RMNXUs",
+							"expanded_url": "http://www.allurekorea.com",
+							"display_url": "allurekorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38091,
+			"friends_count": 1,
+			"listed_count": 299,
+			"created_at": "Tue Sep 29 09:19:06 +0000 2009",
+			"favourites_count": 396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7821,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262307/1666074741",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "지금 날씨에 입기 좋은 #깔깔이 #효민 이 선택한 러플 카라는 어때? #퀼팅재킷 \n#표예진 #클레어로즈 #clairerose #최수영 #소녀시대 #장재인\nhttps://t.co/LgpdK7Rjcc"
+	},
+	{
 		"created_at": "Wed Nov 09 06:49:43 +0000 2022",
 		"id": 1590222658083246000,
 		"id_str": "1590235164453441536",
