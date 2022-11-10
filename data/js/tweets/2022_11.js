441cdc66928ce549fe9fc6a7f9be9d77998152c4
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-11 00:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Thu Nov 10 15:00:26 +0000 2022",
+		"id": 1590720949233410000,
+		"id_str": "1590721048739139584",
+		"full_text": "#1111_HBD_YeoJin\n#Happy_YeoJin_Day\n\n이달의 소녀의 루비💎\n여진이의 생일을 축하합니다🧡\n\nRuby💎of LOOΠΔ\nHappy birthday to YeoJin🧡\n\n#이달의소녀 #여진 \n#LOONA #YeoJin https://t.co/WxZvCHOoG1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1111_HBD_YeoJin",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"text": "Happy_YeoJin_Day",
+					"indices": [
+						17,
+						34
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						106,
+						112
+					]
+				},
+				{
+					"text": "여진",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						118,
+						124
+					]
+				},
+				{
+					"text": "YeoJin",
+					"indices": [
+						125,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1590600062350266400,
+					"id_str": "1590600062350266368",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/FhLyVFAaEAAqR68.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhLyVFAaEAAqR68.jpg",
+					"url": "https://t.co/WxZvCHOoG1",
+					"display_url": "pic.twitter.com/WxZvCHOoG1",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1590720949233410049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590600062350266400,
+					"id_str": "1590600062350266368",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/FhLyVFAaEAAqR68.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhLyVFAaEAAqR68.jpg",
+					"url": "https://t.co/WxZvCHOoG1",
+					"display_url": "pic.twitter.com/WxZvCHOoG1",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1590720949233410049/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 986970,
+			"friends_count": 3,
+			"listed_count": 9027,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5247,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1201,
+		"favorite_count": 2075,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#1111_HBD_YeoJin\n#Happy_YeoJin_Day\n\n이달의 소녀의 루비💎\n여진이의 생일을 축하합니다🧡\n\nRuby💎of LOOΠΔ\nHappy birthday to YeoJin🧡\n\n#이달의소녀 #여진 \n#LOONA #YeoJin https://t.co/WxZvCHOoG1"
+	},
+	{
 		"created_at": "Thu Nov 10 12:56:20 +0000 2022",
 		"id": 1590675648304988200,
 		"id_str": "1590689818077380608",
