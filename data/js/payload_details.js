a561263f660033ce30e897e5e532e33efb1ccbe7
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-11 20:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92491,
-	"created_at": "Fri Nov 11 10:02:03 +0000 2022",
+	"tweets": 92492,
+	"created_at": "Fri Nov 11 11:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 11 10:22:49 +0000 2022",
+		"id": 1591005311019671600,
+		"id_str": "1591013572020039680",
+		"full_text": "[#🌳] 빼빼로데이 선물 추천\n▶ https://t.co/EVUu8YQghe\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/eWzOXzIZxB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "서현",
+					"indices": [
+						44,
+						47
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						48,
+						56
+					]
+				},
+				{
+					"text": "NAMOOCONTENTS",
+					"indices": [
+						57,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EVUu8YQghe",
+					"expanded_url": "https://naver.me/5UGZ4nYD",
+					"display_url": "naver.me/5UGZ4nYD",
+					"indices": [
+						19,
+						42
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1590875325629546500,
+					"id_str": "1590875325629546501",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FhPsrgoVQAUFPrl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhPsrgoVQAUFPrl.jpg",
+					"url": "https://t.co/eWzOXzIZxB",
+					"display_url": "pic.twitter.com/eWzOXzIZxB",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1591005311019671552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590875325629546500,
+					"id_str": "1590875325629546501",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FhPsrgoVQAUFPrl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhPsrgoVQAUFPrl.jpg",
+					"url": "https://t.co/eWzOXzIZxB",
+					"display_url": "pic.twitter.com/eWzOXzIZxB",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1591005311019671552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 960,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 544,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67003,
+			"friends_count": 14,
+			"listed_count": 283,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16554,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 326,
+		"favorite_count": 502,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#🌳] 빼빼로데이 선물 추천\n▶ https://t.co/EVUu8YQghe\n\n#서현 #SEOHYUN #NAMOOCONTENTS https://t.co/eWzOXzIZxB"
+	},
+	{
 		"created_at": "Fri Nov 11 09:43:04 +0000 2022",
 		"id": 1590992734348669000,
 		"id_str": "1591003566231023617",
