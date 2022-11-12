7dc512e0e8cab268c22dda03a439488ef7d28079
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-12 23:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92500,
-	"created_at": "Sat Nov 12 13:02:02 +0000 2022",
+	"tweets": 92501,
+	"created_at": "Sat Nov 12 14:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sat Nov 12 13:06:43 +0000 2022",
+		"id": 1591402763246014500,
+		"id_str": "1591417205127663616",
+		"full_text": "#오늘의_자우림\n\n믹싱 마지막 날!!!!🖤🖤🖤\n\n#자우림_겨울스페셜앨범 \n#MERRY_SPOOKY_XMAS https://t.co/3zWSMTS4dl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "오늘의_자우림",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"text": "자우림_겨울스페셜앨범",
+					"indices": [
+						27,
+						39
+					]
+				},
+				{
+					"text": "MERRY_SPOOKY_XMAS",
+					"indices": [
+						41,
+						59
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1591402754609537000,
+					"id_str": "1591402754609537026",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FhXMX6sUYAIesVI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhXMX6sUYAIesVI.jpg",
+					"url": "https://t.co/3zWSMTS4dl",
+					"display_url": "pic.twitter.com/3zWSMTS4dl",
+					"expanded_url": "https://twitter.com/love_yuna/status/1591402763246014467/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1083,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 903,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1591402754609537000,
+					"id_str": "1591402754609537026",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FhXMX6sUYAIesVI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhXMX6sUYAIesVI.jpg",
+					"url": "https://t.co/3zWSMTS4dl",
+					"display_url": "pic.twitter.com/3zWSMTS4dl",
+					"expanded_url": "https://twitter.com/love_yuna/status/1591402763246014467/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1083,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 903,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1591402754571849700,
+					"id_str": "1591402754571849728",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FhXMX6jVUAA7uLV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhXMX6jVUAA7uLV.jpg",
+					"url": "https://t.co/3zWSMTS4dl",
+					"display_url": "pic.twitter.com/3zWSMTS4dl",
+					"expanded_url": "https://twitter.com/love_yuna/status/1591402763246014467/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1591402754580193300,
+					"id_str": "1591402754580193280",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/FhXMX6lUoAAI1ef.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhXMX6lUoAAI1ef.jpg",
+					"url": "https://t.co/3zWSMTS4dl",
+					"display_url": "pic.twitter.com/3zWSMTS4dl",
+					"expanded_url": "https://twitter.com/love_yuna/status/1591402763246014467/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1083,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 903,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 511,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275902,
+			"friends_count": 597,
+			"listed_count": 2737,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 462,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1667288883",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 52,
+		"favorite_count": 137,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#오늘의_자우림\n\n믹싱 마지막 날!!!!🖤🖤🖤\n\n#자우림_겨울스페셜앨범 \n#MERRY_SPOOKY_XMAS https://t.co/3zWSMTS4dl"
+	},
+	{
 		"created_at": "Sat Nov 12 12:06:42 +0000 2022",
 		"id": 1591391628060946400,
 		"id_str": "1591402100776321024",
