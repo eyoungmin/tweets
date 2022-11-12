a4238fa423188a7f44545ba11eaca3539a4658ae
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-13 00:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92501,
-	"created_at": "Sat Nov 12 14:02:02 +0000 2022",
+	"tweets": 92502,
+	"created_at": "Sat Nov 12 15:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sat Nov 12 15:01:27 +0000 2022",
+		"id": 1591445713946304500,
+		"id_str": "1591446080163414016",
+		"full_text": "#1113_HBD_OliviaHye\n#Happy_OliviaHye_Day\n\n이달의 소녀의 아기늑대🐺\n올리비아 혜의 생일을 축하합니다🖤\n\nBaby wolf🐺of LOOΠΔ\nHappy birthday to Olivia Hye🖤\n\n#이달의소녀 #LOONA \n#OliviaHye https://t.co/vhGLtEBLGo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "1113_HBD_OliviaHye",
+					"indices": [
+						0,
+						19
+					]
+				},
+				{
+					"text": "Happy_OliviaHye_Day",
+					"indices": [
+						20,
+						40
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						126,
+						132
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						133,
+						139
+					]
+				},
+				{
+					"text": "OliviaHye",
+					"indices": [
+						141,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1590682109022064600,
+					"id_str": "1590682109022064642",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/FhM880rUUAI-_Rq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhM880rUUAI-_Rq.jpg",
+					"url": "https://t.co/vhGLtEBLGo",
+					"display_url": "pic.twitter.com/vhGLtEBLGo",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1591445713946304514/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590682109022064600,
+					"id_str": "1590682109022064642",
+					"indices": [
+						152,
+						175
+					],
+					"media_url": "http://pbs.twimg.com/media/FhM880rUUAI-_Rq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhM880rUUAI-_Rq.jpg",
+					"url": "https://t.co/vhGLtEBLGo",
+					"display_url": "pic.twitter.com/vhGLtEBLGo",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1591445713946304514/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 987025,
+			"friends_count": 3,
+			"listed_count": 9023,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5254,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1387,
+		"favorite_count": 2204,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#1113_HBD_OliviaHye\n#Happy_OliviaHye_Day\n\n이달의 소녀의 아기늑대🐺\n올리비아 혜의 생일을 축하합니다🖤\n\nBaby wolf🐺of LOOΠΔ\nHappy birthday to Olivia Hye🖤\n\n#이달의소녀 #LOONA \n#OliviaHye https://t.co/vhGLtEBLGo"
+	},
+	{
 		"created_at": "Sat Nov 12 13:06:43 +0000 2022",
 		"id": 1591402763246014500,
 		"id_str": "1591417205127663616",
