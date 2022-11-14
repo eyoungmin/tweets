60e8c2827ed710c90823d7d3bc0c0ffc44ecf6aa
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-14 23:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92520,
-	"created_at": "Mon Nov 14 13:02:03 +0000 2022",
+	"tweets": 92521,
+	"created_at": "Mon Nov 14 14:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,196 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Mon Nov 14 13:07:09 +0000 2022",
+		"id": 1592137632095809500,
+		"id_str": "1592142091097817088",
+		"full_text": "♣️ 세명의 왕 🖤 이야기 ♠️\n\n#자우림_겨울스페셜앨범 \n#Merry_Spooky_Xmas https://t.co/NnyZByR1bx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "자우림_겨울스페셜앨범",
+					"indices": [
+						19,
+						31
+					]
+				},
+				{
+					"text": "Merry_Spooky_Xmas",
+					"indices": [
+						33,
+						51
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1592137627309727700,
+					"id_str": "1592137627309727744",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/FhhovI1UYAA37lq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhhovI1UYAA37lq.jpg",
+					"url": "https://t.co/NnyZByR1bx",
+					"display_url": "pic.twitter.com/NnyZByR1bx",
+					"expanded_url": "https://twitter.com/love_yuna/status/1592137632095809538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1499,
+							"h": 1499,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1592137627309727700,
+					"id_str": "1592137627309727744",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/FhhovI1UYAA37lq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhhovI1UYAA37lq.jpg",
+					"url": "https://t.co/NnyZByR1bx",
+					"display_url": "pic.twitter.com/NnyZByR1bx",
+					"expanded_url": "https://twitter.com/love_yuna/status/1592137632095809538/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1499,
+							"h": 1499,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275898,
+			"friends_count": 597,
+			"listed_count": 2737,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 463,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6976,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1667288883",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 93,
+		"favorite_count": 129,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "♣️ 세명의 왕 🖤 이야기 ♠️\n\n#자우림_겨울스페셜앨범 \n#Merry_Spooky_Xmas https://t.co/NnyZByR1bx"
+	},
+	{
 		"created_at": "Mon Nov 14 12:07:08 +0000 2022",
 		"id": 1592114270602686500,
 		"id_str": "1592126984896684032",
