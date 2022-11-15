aed6a5bdc4d9b37d6ccd0c4436736cd545eea466
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-15 22:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92529,
-	"created_at": "Tue Nov 15 12:02:03 +0000 2022",
+	"tweets": 92530,
+	"created_at": "Tue Nov 15 13:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Tue Nov 15 12:07:23 +0000 2022",
+		"id": 1592476104396517400,
+		"id_str": "1592489435253178368",
+		"full_text": "안녕👋 영스트리트💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/m1tn6l96ZU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						12,
+						15
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						16,
+						22
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						23,
+						26
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						27,
+						37
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						38,
+						43
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						45,
+						54
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						55,
+						61
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						62,
+						65
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						66,
+						70
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1592476086838825000,
+					"id_str": "1592476086838824960",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FhmckE1VEAAxTZ1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhmckE1VEAAxTZ1.jpg",
+					"url": "https://t.co/m1tn6l96ZU",
+					"display_url": "pic.twitter.com/m1tn6l96ZU",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1592476104396517376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1592476086838825000,
+					"id_str": "1592476086838824960",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/FhmckE1VEAAxTZ1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhmckE1VEAAxTZ1.jpg",
+					"url": "https://t.co/m1tn6l96ZU",
+					"display_url": "pic.twitter.com/m1tn6l96ZU",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1592476104396517376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5718923,
+			"friends_count": 2,
+			"listed_count": 11456,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3033,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2353,
+		"favorite_count": 8541,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "안녕👋 영스트리트💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/m1tn6l96ZU"
+	},
+	{
 		"created_at": "Tue Nov 15 11:07:21 +0000 2022",
 		"id": 1592472850891702300,
 		"id_str": "1592474331111600128",
