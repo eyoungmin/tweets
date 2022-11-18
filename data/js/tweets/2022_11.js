39fb4d2f5893804429cab65443bdfc4b8dc90fd0
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-19 00:02:05 +0900

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 18 14:07:59 +0000 2022",
+		"id": 1593599216772796400,
+		"id_str": "1593606951526494209",
+		"full_text": "Bonjour~ YERI in Paris💜 l yerimiese VLOG\n\nhttps://t.co/z12Y3KU9Ks\n\n#예리 #YERI\n#예리미즈 #yerimiese https://t.co/aVrpHdrc9r",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "예리",
+					"indices": [
+						67,
+						70
+					]
+				},
+				{
+					"text": "YERI",
+					"indices": [
+						71,
+						76
+					]
+				},
+				{
+					"text": "예리미즈",
+					"indices": [
+						77,
+						82
+					]
+				},
+				{
+					"text": "yerimiese",
+					"indices": [
+						83,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/z12Y3KU9Ks",
+					"expanded_url": "http://youtu.be/PnbHKcUaUBI",
+					"display_url": "youtu.be/PnbHKcUaUBI",
+					"indices": [
+						42,
+						65
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1593599210951037000,
+					"id_str": "1593599210951036929",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh2aCgMUUAELEet.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fh2aCgMUUAELEet.jpg",
+					"url": "https://t.co/aVrpHdrc9r",
+					"display_url": "pic.twitter.com/aVrpHdrc9r",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1593599216772796416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1593599210951037000,
+					"id_str": "1593599210951036929",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh2aCgMUUAELEet.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fh2aCgMUUAELEet.jpg",
+					"url": "https://t.co/aVrpHdrc9r",
+					"display_url": "pic.twitter.com/aVrpHdrc9r",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1593599216772796416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5737156,
+			"friends_count": 2,
+			"listed_count": 11498,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3050,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3427,
+		"favorite_count": 10002,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Bonjour~ YERI in Paris💜 l yerimiese VLOG\n\nhttps://t.co/z12Y3KU9Ks\n\n#예리 #YERI\n#예리미즈 #yerimiese https://t.co/aVrpHdrc9r"
+	},
+	{
 		"created_at": "Fri Nov 18 12:51:52 +0000 2022",
 		"id": 1593574744837668900,
 		"id_str": "1593587795364192257",
