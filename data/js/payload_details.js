342a13bf7fc962e58c71d39c3dd1dcd3c2d39b26
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-20 00:19:59 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92572,
-	"created_at": "Sat Nov 19 10:02:04 +0000 2022",
+	"tweets": 92573,
+	"created_at": "Sat Nov 19 15:19:58 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sat Nov 19 15:08:18 +0000 2022",
+		"id": 1593981097716879400,
+		"id_str": "1593984518691569666",
+		"full_text": "#GMA\n\n💜💜💜\n\n#자우림 #jaurim #紫雨林\n#BestRockArtist https://t.co/FQxolAfM5f",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GMA",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "자우림",
+					"indices": [
+						11,
+						15
+					]
+				},
+				{
+					"text": "jaurim",
+					"indices": [
+						16,
+						23
+					]
+				},
+				{
+					"text": "紫雨林",
+					"indices": [
+						24,
+						28
+					]
+				},
+				{
+					"text": "BestRockArtist",
+					"indices": [
+						29,
+						44
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1593981091815137300,
+					"id_str": "1593981091815137285",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh71W5RUoAUBfQU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fh71W5RUoAUBfQU.jpg",
+					"url": "https://t.co/FQxolAfM5f",
+					"display_url": "pic.twitter.com/FQxolAfM5f",
+					"expanded_url": "https://twitter.com/love_yuna/status/1593981097716879361/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1593981091815137300,
+					"id_str": "1593981091815137285",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/Fh71W5RUoAUBfQU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fh71W5RUoAUBfQU.jpg",
+					"url": "https://t.co/FQxolAfM5f",
+					"display_url": "pic.twitter.com/FQxolAfM5f",
+					"expanded_url": "https://twitter.com/love_yuna/status/1593981097716879361/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 120741919,
+			"id_str": "120741919",
+			"name": "김윤아",
+			"screen_name": "love_yuna",
+			"location": "",
+			"description": "singer-songwriter / lead singer for Jaurim 紫雨林",
+			"url": "https://t.co/YE7aKeUGLd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YE7aKeUGLd",
+							"expanded_url": "https://www.youtube.com/c/loveyuna",
+							"display_url": "youtube.com/c/loveyuna",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275985,
+			"friends_count": 600,
+			"listed_count": 2736,
+			"created_at": "Sun Mar 07 11:53:42 +0000 2010",
+			"favourites_count": 478,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7002,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1587350693115199488/WeM4899U_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/120741919/1667288883",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 67,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#GMA\n\n💜💜💜\n\n#자우림 #jaurim #紫雨林\n#BestRockArtist https://t.co/FQxolAfM5f"
+	},
+	{
 		"created_at": "Sat Nov 19 09:58:21 +0000 2022",
 		"id": 1593891987031470000,
 		"id_str": "1593906516620234752",
