3957970dc769106690bb11241dca17e2d12a09b1
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-24 19:02:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92617,
-	"created_at": "Thu Nov 24 08:02:03 +0000 2022",
+	"tweets": 92618,
+	"created_at": "Thu Nov 24 10:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Thu Nov 24 09:04:08 +0000 2022",
+		"id": 1595701318143778800,
+		"id_str": "1595704812254859265",
+		"full_text": "잠시 후 6시 15분 (KST)\nMBC 2022 FIFA 카타르 월드컵 프리뷰쇼 \n카타르시스에 현진이 출연합니다🐱\n오늘도 어김없이 보러 오게 되는 \n#카타르월드컵 소식 본방사수 ⚽🏆\n\n#이달의소녀 #LOONA\n#현진 #HyunJin https://t.co/3E8bFXVLRv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "카타르월드컵",
+					"indices": [
+						83,
+						90
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						103,
+						109
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "현진",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "HyunJin",
+					"indices": [
+						121,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1595701313362219000,
+					"id_str": "1595701313362219008",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FiUR496UUAAuTvB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FiUR496UUAAuTvB.jpg",
+					"url": "https://t.co/3E8bFXVLRv",
+					"display_url": "pic.twitter.com/3E8bFXVLRv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1595701318143778816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1595701313362219000,
+					"id_str": "1595701313362219008",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FiUR496UUAAuTvB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FiUR496UUAAuTvB.jpg",
+					"url": "https://t.co/3E8bFXVLRv",
+					"display_url": "pic.twitter.com/3E8bFXVLRv",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1595701318143778816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 1280,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsJh5B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsJh5B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 987350,
+			"friends_count": 3,
+			"listed_count": 9036,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5283,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/770991945896255488/fRzzSHnM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1655716427",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 958,
+		"favorite_count": 3841,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "잠시 후 6시 15분 (KST)\nMBC 2022 FIFA 카타르 월드컵 프리뷰쇼 \n카타르시스에 현진이 출연합니다🐱\n오늘도 어김없이 보러 오게 되는 \n#카타르월드컵 소식 본방사수 ⚽🏆\n\n#이달의소녀 #LOONA\n#현진 #HyunJin https://t.co/3E8bFXVLRv"
+	},
+	{
 		"created_at": "Thu Nov 24 07:09:25 +0000 2022",
 		"id": 1595671560261496800,
 		"id_str": "1595675943825129473",
