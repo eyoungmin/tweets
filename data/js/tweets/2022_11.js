79ef110de6e60e7504a7e4b0dc5268a91b55a499
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-24 16:02:04 +0900

    Added 1 tweets

--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Thu Nov 24 06:10:33 +0000 2022",
+		"id": 1595659124414763000,
+		"id_str": "1595661127467139072",
+		"full_text": "Yerin Baek 10th Anniversary Interveiw with BEAKER\n⠀\n백예린 데뷔 10주년 프로젝트의 마지막 컨텐츠, 아티스트 인터뷰를 공개합니다💌 아티스트로서의 지난 10년과 앞으로의 백예린까지, 예린에게 직접 듣는 진솔한 이야기를 'Yerin Baek' 유튜브에서 풀버전으로 만나보세요!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1185935579168424000,
+			"id_str": "1185935579168423936",
+			"name": "Yerin Baek",
+			"screen_name": "yerinbaek",
+			"location": "",
+			"description": "#YerinBaek Official Twitter",
+			"url": "https://t.co/ocnoiYagVq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ocnoiYagVq",
+							"expanded_url": "https://www.youtube.com/YerinBaekOfficial",
+							"display_url": "youtube.com/YerinBaekOffic…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50176,
+			"friends_count": 2,
+			"listed_count": 240,
+			"created_at": "Sun Oct 20 15:07:39 +0000 2019",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 260,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1529030269046784000/5PCxmbkY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1529030269046784000/5PCxmbkY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1185935579168423936/1652253459",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 161,
+		"favorite_count": 252,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "Yerin Baek 10th Anniversary Interveiw with BEAKER\n⠀\n백예린 데뷔 10주년 프로젝트의 마지막 컨텐츠, 아티스트 인터뷰를 공개합니다💌 아티스트로서의 지난 10년과 앞으로의 백예린까지, 예린에게 직접 듣는 진솔한 이야기를 'Yerin Baek' 유튜브에서 풀버전으로 만나보세요!"
+	},
+	{
 		"created_at": "Thu Nov 24 05:09:21 +0000 2022",
 		"id": 1595644221087969300,
 		"id_str": "1595645724884013056",
