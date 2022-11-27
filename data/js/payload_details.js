de2f79229c34e1691e2357a834d185800d78b322
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-27 19:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92631,
-	"created_at": "Sun Nov 27 04:02:04 +0000 2022",
+	"tweets": 92632,
+	"created_at": "Sun Nov 27 10:02:03 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Sun Nov 27 09:46:43 +0000 2022",
+		"id": 1596790939875389400,
+		"id_str": "1596802692508049409",
+		"full_text": "나도 #한소희 처럼 피어싱 해볼까? 셀럽들의 페이크 피어싱!\n#제니 #블랙핑크 @ygofficialblink #박재범 #이영지 #방탄소년단 @bts_bighit #정국 #리정 #YGX https://t.co/SP7MJMQ6dG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "한소희",
+					"indices": [
+						3,
+						7
+					]
+				},
+				{
+					"text": "제니",
+					"indices": [
+						34,
+						37
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						38,
+						43
+					]
+				},
+				{
+					"text": "박재범",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "이영지",
+					"indices": [
+						66,
+						70
+					]
+				},
+				{
+					"text": "방탄소년단",
+					"indices": [
+						71,
+						77
+					]
+				},
+				{
+					"text": "정국",
+					"indices": [
+						90,
+						93
+					]
+				},
+				{
+					"text": "리정",
+					"indices": [
+						94,
+						97
+					]
+				},
+				{
+					"text": "YGX",
+					"indices": [
+						98,
+						102
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						44,
+						60
+					]
+				},
+				{
+					"screen_name": "bts_bighit",
+					"name": "BTS_official",
+					"id": 1409798257,
+					"id_str": "1409798257",
+					"indices": [
+						78,
+						89
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/SP7MJMQ6dG",
+					"expanded_url": "https://www.allurekorea.com/?p=187596&ddw=130358&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "allurekorea.com/?p=187596&ddw=…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262307,
+			"id_str": "78262307",
+			"name": "allure korea",
+			"screen_name": "allurekorea",
+			"location": "Seoul",
+			"description": "beauty & style expert, Allure Magazine Korea",
+			"url": "https://t.co/OcX4RMvOGk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OcX4RMvOGk",
+							"expanded_url": "http://www.allurekorea.com",
+							"display_url": "allurekorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38194,
+			"friends_count": 1,
+			"listed_count": 302,
+			"created_at": "Tue Sep 29 09:19:06 +0000 2009",
+			"favourites_count": 458,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7886,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262307/1668746717",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "나도 #한소희 처럼 피어싱 해볼까? 셀럽들의 페이크 피어싱!\n#제니 #블랙핑크 @ygofficialblink #박재범 #이영지 #방탄소년단 @bts_bighit #정국 #리정 #YGX https://t.co/SP7MJMQ6dG"
+	},
+	{
 		"created_at": "Sun Nov 27 03:52:36 +0000 2022",
 		"id": 1596700336839807000,
 		"id_str": "1596713576898568193",
