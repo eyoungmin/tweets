48cc17b2c1934f4efb3f1935292402286375c303
Youngmin Lee <iameyoungmin@gmail.com>
2022-11-30 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92661,
-	"created_at": "Wed Nov 30 04:02:04 +0000 2022",
+	"tweets": 92662,
+	"created_at": "Wed Nov 30 06:02:04 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 30 05:55:33 +0000 2022",
+		"id": 1597817702088933400,
+		"id_str": "1597831679262924800",
+		"full_text": "비주얼 난리 난 #레드벨벳 신곡 ‘#Birthday' 헤어 메이크업 스타일링까지 전격 분석!\n#레드벨벳의_생일파티에_초대할게 #웬디 #아이린 #슬기 #조이 #예리 #벌스데이 #RedVelvet @RVsmtown #The_ReVe_Festival_2022 \nhttps://t.co/MCzJ10ebE4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "레드벨벳",
+					"indices": [
+						9,
+						14
+					]
+				},
+				{
+					"text": "Birthday",
+					"indices": [
+						19,
+						28
+					]
+				},
+				{
+					"text": "레드벨벳의_생일파티에_초대할게",
+					"indices": [
+						52,
+						69
+					]
+				},
+				{
+					"text": "웬디",
+					"indices": [
+						70,
+						73
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						74,
+						78
+					]
+				},
+				{
+					"text": "슬기",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "예리",
+					"indices": [
+						87,
+						90
+					]
+				},
+				{
+					"text": "벌스데이",
+					"indices": [
+						91,
+						96
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						97,
+						107
+					]
+				},
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						118,
+						141
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RVsmtown",
+					"name": "Red Velvet",
+					"id": 873092428755894300,
+					"id_str": "873092428755894272",
+					"indices": [
+						108,
+						117
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MCzJ10ebE4",
+					"expanded_url": "https://www.allurekorea.com/?p=189609&ddw=130697&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "allurekorea.com/?p=189609&ddw=…",
+					"indices": [
+						143,
+						166
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262307,
+			"id_str": "78262307",
+			"name": "allure korea",
+			"screen_name": "allurekorea",
+			"location": "Seoul",
+			"description": "beauty & style expert, Allure Magazine Korea",
+			"url": "https://t.co/OcX4RMvOGk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OcX4RMvOGk",
+							"expanded_url": "http://www.allurekorea.com",
+							"display_url": "allurekorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38189,
+			"friends_count": 1,
+			"listed_count": 302,
+			"created_at": "Tue Sep 29 09:19:06 +0000 2009",
+			"favourites_count": 469,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7894,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262307/1668746717",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "비주얼 난리 난 #레드벨벳 신곡 ‘#Birthday' 헤어 메이크업 스타일링까지 전격 분석!\n#레드벨벳의_생일파티에_초대할게 #웬디 #아이린 #슬기 #조이 #예리 #벌스데이 #RedVelvet @RVsmtown #The_ReVe_Festival_2022 \nhttps://t.co/MCzJ10ebE4"
+	},
+	{
 		"created_at": "Wed Nov 30 03:10:37 +0000 2022",
 		"id": 1597786771273969700,
 		"id_str": "1597790171402891264",
