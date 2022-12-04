17d9faf7b725ed0b7a722565c0d9cac24bbe599e
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-05 00:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92700,
-	"created_at": "Sun Dec 04 13:02:00 +0000 2022",
+	"tweets": 92701,
+	"created_at": "Sun Dec 04 15:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Sun Dec 04 14:11:38 +0000 2022",
+		"id": 1599395097451561000,
+		"id_str": "1599406073168875520",
+		"full_text": "러비들!!!😘\n오마이갓 시간 너무 빠르잖아... 벌떼 첫 주가 벌써 이렇게 끝나다니... 컴백 첫 무대에서 러비들의 떼창을 처음 들은 게 오늘 같은데 말이지...!!!!! 이른 새벽부터 잠도 못 자구 응원하러 와줘서 너무 고맙구 ㅠㅠ 사후녹화까지ㅠㅠ 고마워!!!🥹 https://t.co/LfWWfaJN3T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1599395059216306200,
+					"id_str": "1599395059216306176",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjIxVRgaUAAyi-u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjIxVRgaUAAyi-u.jpg",
+					"url": "https://t.co/LfWWfaJN3T",
+					"display_url": "pic.twitter.com/LfWWfaJN3T",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1599395097451560960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1599395059216306200,
+					"id_str": "1599395059216306176",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjIxVRgaUAAyi-u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjIxVRgaUAAyi-u.jpg",
+					"url": "https://t.co/LfWWfaJN3T",
+					"display_url": "pic.twitter.com/LfWWfaJN3T",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1599395097451560960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1599395085640401000,
+					"id_str": "1599395085640400897",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjIxWz8aAAEtDs-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjIxWz8aAAEtDs-.jpg",
+					"url": "https://t.co/LfWWfaJN3T",
+					"display_url": "pic.twitter.com/LfWWfaJN3T",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1599395097451560960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1599395086248599600,
+					"id_str": "1599395086248599553",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjIxW2NaYAEqpwL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjIxW2NaYAEqpwL.jpg",
+					"url": "https://t.co/LfWWfaJN3T",
+					"display_url": "pic.twitter.com/LfWWfaJN3T",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1599395097451560960/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5772838,
+			"friends_count": 2,
+			"listed_count": 11548,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3125,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5730,
+		"favorite_count": 17876,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "러비들!!!😘\n오마이갓 시간 너무 빠르잖아... 벌떼 첫 주가 벌써 이렇게 끝나다니... 컴백 첫 무대에서 러비들의 떼창을 처음 들은 게 오늘 같은데 말이지...!!!!! 이른 새벽부터 잠도 못 자구 응원하러 와줘서 너무 고맙구 ㅠㅠ 사후녹화까지ㅠㅠ 고마워!!!🥹 https://t.co/LfWWfaJN3T"
+	},
+	{
 		"created_at": "Sun Dec 04 12:11:37 +0000 2022",
 		"id": 1599374907938590700,
 		"id_str": "1599375870463860736",
