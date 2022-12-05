294bae3fae9bbdcfe7cbc0f2e4a4b331281d2dde
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-05 15:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92702,
-	"created_at": "Mon Dec 05 02:02:00 +0000 2022",
+	"tweets": 92703,
+	"created_at": "Mon Dec 05 06:02:00 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,131 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 05 05:10:00 +0000 2022",
+		"id": 1599624573880000500,
+		"id_str": "1599632156220801024",
+		"full_text": "KWON YURI donates to young adults preparing for self-reliance in celebration of her birthday! She participated in the campaign’s YouTube shoot and made a donation to students’ college tuition fees. \n\n#유리 #YURI\n#소녀시대 #GirlsGeneration",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "유리",
+					"indices": [
+						200,
+						203
+					]
+				},
+				{
+					"text": "YURI",
+					"indices": [
+						204,
+						209
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						210,
+						215
+					]
+				},
+				{
+					"text": "GirlsGeneration",
+					"indices": [
+						216,
+						232
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1599624571451506700,
+		"in_reply_to_status_id_str": "1599624571451506697",
+		"in_reply_to_user_id": 391115625,
+		"in_reply_to_user_id_str": "391115625",
+		"in_reply_to_screen_name": "GirlsGeneration",
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3912620,
+			"friends_count": 5,
+			"listed_count": 6292,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 71,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3143,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 571,
+		"favorite_count": 1466,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "KWON YURI donates to young adults preparing for self-reliance in celebration of her birthday! She participated in the campaign’s YouTube shoot and made a donation to students’ college tuition fees. \n\n#유리 #YURI\n#소녀시대 #GirlsGeneration"
+	},
+	{
 		"created_at": "Mon Dec 05 01:09:58 +0000 2022",
 		"id": 1599569556636565500,
 		"id_str": "1599571751989411840",
