7087ddd3358c871ef362279a0886fe77371a9772
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-05 20:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92706,
-	"created_at": "Mon Dec 05 10:02:01 +0000 2022",
+	"tweets": 92707,
+	"created_at": "Mon Dec 05 11:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 05 10:48:56 +0000 2022",
+		"id": 1599715898742698000,
+		"id_str": "1599717451956072449",
+		"full_text": "HITC in Jakarta❤🤍❤🤍\nThank you for having me🤟🏻\naku cinta kamu❤\n\n@hitcjakarta\n#청하 #CHUNGHA https://t.co/RzvaoUxNOg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "청하",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "CHUNGHA",
+					"indices": [
+						80,
+						88
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hitcjakarta",
+					"name": "Head in The Clouds Jakarta",
+					"id": 1215511022607356000,
+					"id_str": "1215511022607355907",
+					"indices": [
+						63,
+						75
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1599715890970312700,
+					"id_str": "1599715890970312704",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNVII7VQAAFgnl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNVII7VQAAFgnl.jpg",
+					"url": "https://t.co/RzvaoUxNOg",
+					"display_url": "pic.twitter.com/RzvaoUxNOg",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1599715898742697985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1599715890970312700,
+					"id_str": "1599715890970312704",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNVII7VQAAFgnl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNVII7VQAAFgnl.jpg",
+					"url": "https://t.co/RzvaoUxNOg",
+					"display_url": "pic.twitter.com/RzvaoUxNOg",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1599715898742697985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1599715890966106000,
+					"id_str": "1599715890966106112",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNVII6VEAA12Ay.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNVII6VEAA12Ay.jpg",
+					"url": "https://t.co/RzvaoUxNOg",
+					"display_url": "pic.twitter.com/RzvaoUxNOg",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1599715898742697985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1599715891007979500,
+					"id_str": "1599715891007979521",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/FjNVIJEUAAErFLL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjNVIJEUAAErFLL.jpg",
+					"url": "https://t.co/RzvaoUxNOg",
+					"display_url": "pic.twitter.com/RzvaoUxNOg",
+					"expanded_url": "https://twitter.com/CHUNGHA_MNHent/status/1599715898742697985/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097557125742489600,
+			"id_str": "1097557125742489600",
+			"name": "CHUNG HA",
+			"screen_name": "CHUNGHA_MNHent",
+			"location": "",
+			"description": "CHUNG HA Official Twitter",
+			"url": "https://t.co/r3B14nlZdd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/r3B14nlZdd",
+							"expanded_url": "http://CHUNGHA.lnk.to/BareRarePt1",
+							"display_url": "CHUNGHA.lnk.to/BareRarePt1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 355057,
+			"friends_count": 1,
+			"listed_count": 2219,
+			"created_at": "Mon Feb 18 18:03:05 +0000 2019",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2591,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1542888088128524288/9HPz1AnF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097557125742489600/1656688172",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 156,
+		"favorite_count": 439,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in",
+		"text": "HITC in Jakarta❤🤍❤🤍\nThank you for having me🤟🏻\naku cinta kamu❤\n\n@hitcjakarta\n#청하 #CHUNGHA https://t.co/RzvaoUxNOg"
+	},
+	{
 		"created_at": "Mon Dec 05 09:56:34 +0000 2022",
 		"id": 1599690071774752800,
 		"id_str": "1599704271640883201",
