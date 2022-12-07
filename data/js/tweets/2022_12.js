6ca4a04decf37475c8c0916d1b2a57e78d8f10f5
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-07 23:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Wed Dec 07 13:12:19 +0000 2022",
+		"id": 1600475335715598300,
+		"id_str": "1600478313259991040",
+		"full_text": "Red Velvet 레드벨벳 ‘Birthday’ Performance Video Behind I RV Collection\n\nhttps://t.co/J3zfgMt5lb\n\n#The_ReVe_Festival_2022\n#Birthday #벌스데이\n#레드벨벳 #RedVelvet\n#비하인드 #Behind \n#RV_Collection #레벨컬렉션 https://t.co/EwICeB2x3U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			187
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						94,
+						117
+					]
+				},
+				{
+					"text": "Birthday",
+					"indices": [
+						118,
+						127
+					]
+				},
+				{
+					"text": "벌스데이",
+					"indices": [
+						128,
+						133
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						134,
+						139
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						140,
+						150
+					]
+				},
+				{
+					"text": "비하인드",
+					"indices": [
+						151,
+						156
+					]
+				},
+				{
+					"text": "Behind",
+					"indices": [
+						157,
+						164
+					]
+				},
+				{
+					"text": "RV_Collection",
+					"indices": [
+						166,
+						180
+					]
+				},
+				{
+					"text": "레벨컬렉션",
+					"indices": [
+						181,
+						187
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/J3zfgMt5lb",
+					"expanded_url": "https://youtu.be/zHhacq043to",
+					"display_url": "youtu.be/zHhacq043to",
+					"indices": [
+						69,
+						92
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1600475327267893200,
+					"id_str": "1600475327267893248",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/FjYH1JmUcAAV41g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjYH1JmUcAAV41g.jpg",
+					"url": "https://t.co/EwICeB2x3U",
+					"display_url": "pic.twitter.com/EwICeB2x3U",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1600475335715598349/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1600475327267893200,
+					"id_str": "1600475327267893248",
+					"indices": [
+						188,
+						211
+					],
+					"media_url": "http://pbs.twimg.com/media/FjYH1JmUcAAV41g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjYH1JmUcAAV41g.jpg",
+					"url": "https://t.co/EwICeB2x3U",
+					"display_url": "pic.twitter.com/EwICeB2x3U",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1600475335715598349/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5776690,
+			"friends_count": 2,
+			"listed_count": 11553,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3132,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4370,
+		"favorite_count": 10459,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Red Velvet 레드벨벳 ‘Birthday’ Performance Video Behind I RV Collection\n\nhttps://t.co/J3zfgMt5lb\n\n#The_ReVe_Festival_2022\n#Birthday #벌스데이\n#레드벨벳 #RedVelvet\n#비하인드 #Behind \n#RV_Collection #레벨컬렉션 https://t.co/EwICeB2x3U"
+	},
+	{
 		"created_at": "Wed Dec 07 12:12:19 +0000 2022",
 		"id": 1600449119780495400,
 		"id_str": "1600463212301803520",
