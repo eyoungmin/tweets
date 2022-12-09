8137b5b14b39acce9dbcab6faac8132174f912ce
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-09 19:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Fri Dec 09 09:57:33 +0000 2022",
+		"id": 1601139650370080800,
+		"id_str": "1601154070802096134",
+		"full_text": "#BLACKPINK ‘B.P.M.’ Roll #2\n\n🎬https://t.co/T9egckxDZ3\n\n#블랙핑크 #BPM #BORN_PINK_MEMORIES #Roll_2 #BORNPINK #YOUTUBE #YG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			116
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						55,
+						60
+					]
+				},
+				{
+					"text": "BPM",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "BORN_PINK_MEMORIES",
+					"indices": [
+						66,
+						85
+					]
+				},
+				{
+					"text": "Roll_2",
+					"indices": [
+						86,
+						93
+					]
+				},
+				{
+					"text": "BORNPINK",
+					"indices": [
+						94,
+						103
+					]
+				},
+				{
+					"text": "YOUTUBE",
+					"indices": [
+						104,
+						112
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						113,
+						116
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/T9egckxDZ3",
+					"expanded_url": "https://youtu.be/4hgm69uVCdQ",
+					"display_url": "youtu.be/4hgm69uVCdQ",
+					"indices": [
+						30,
+						53
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/vkfEFSP9At",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vkfEFSP9At",
+							"expanded_url": "https://lnk.to/YG_BLACKPINK",
+							"display_url": "lnk.to/YG_BLACKPINK",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8123210,
+			"friends_count": 0,
+			"listed_count": 7990,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 32,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 845,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1662480989",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4501,
+		"favorite_count": 9394,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK ‘B.P.M.’ Roll #2\n\n🎬https://t.co/T9egckxDZ3\n\n#블랙핑크 #BPM #BORN_PINK_MEMORIES #Roll_2 #BORNPINK #YOUTUBE #YG"
+	},
+	{
 		"created_at": "Fri Dec 09 06:24:44 +0000 2022",
 		"id": 1601091976266334200,
 		"id_str": "1601100514971815936",
