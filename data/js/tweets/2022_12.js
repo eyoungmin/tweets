9ddf006eeeb2a73f1f0bef2a40a93520620640dd
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-10 20:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,255 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Sat Dec 10 10:13:27 +0000 2022",
+		"id": 1601517084546138000,
+		"id_str": "1601520459777908736",
+		"full_text": "Red Velvet 레드벨벳 “Birthday” Dance Practice\n \nhttps://t.co/xyYVVei5q3  \n\n#RedVelvet #레드벨벳\n#Birthday #Dance_Practice #연습영상\n#SMP #SMPD #SMPFLOOR #슴피플로어 https://t.co/L09SEsxcuf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						71,
+						81
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						82,
+						87
+					]
+				},
+				{
+					"text": "Birthday",
+					"indices": [
+						88,
+						97
+					]
+				},
+				{
+					"text": "Dance_Practice",
+					"indices": [
+						98,
+						113
+					]
+				},
+				{
+					"text": "연습영상",
+					"indices": [
+						114,
+						119
+					]
+				},
+				{
+					"text": "SMP",
+					"indices": [
+						120,
+						124
+					]
+				},
+				{
+					"text": "SMPD",
+					"indices": [
+						125,
+						130
+					]
+				},
+				{
+					"text": "SMPFLOOR",
+					"indices": [
+						131,
+						140
+					]
+				},
+				{
+					"text": "슴피플로어",
+					"indices": [
+						141,
+						147
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xyYVVei5q3",
+					"expanded_url": "https://youtu.be/P2WVtvuyWlc",
+					"display_url": "youtu.be/P2WVtvuyWlc",
+					"indices": [
+						44,
+						67
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1601171984297365500,
+					"id_str": "1601171984297365505",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjiBb7RaEAEKp4Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjiBb7RaEAEKp4Y.jpg",
+					"url": "https://t.co/L09SEsxcuf",
+					"display_url": "pic.twitter.com/L09SEsxcuf",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1601517084546138112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1601171984297365500,
+					"id_str": "1601171984297365505",
+					"indices": [
+						148,
+						171
+					],
+					"media_url": "http://pbs.twimg.com/media/FjiBb7RaEAEKp4Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjiBb7RaEAEKp4Y.jpg",
+					"url": "https://t.co/L09SEsxcuf",
+					"display_url": "pic.twitter.com/L09SEsxcuf",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1601517084546138112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5783846,
+			"friends_count": 2,
+			"listed_count": 11563,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4608,
+		"favorite_count": 9257,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "Red Velvet 레드벨벳 “Birthday” Dance Practice\n \nhttps://t.co/xyYVVei5q3  \n\n#RedVelvet #레드벨벳\n#Birthday #Dance_Practice #연습영상\n#SMP #SMPD #SMPFLOOR #슴피플로어 https://t.co/L09SEsxcuf"
+	},
+	{
 		"created_at": "Sat Dec 10 09:49:44 +0000 2022",
 		"id": 1601501981096427500,
 		"id_str": "1601514493145624576",
