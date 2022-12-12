73a0781350fd8966e8e3e55056e565fe7d3f01d7
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-13 00:19:56 +0900

    Added 1 tweets

--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,294 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 12 15:13:57 +0000 2022",
+		"id": 1602317356948070400,
+		"id_str": "1602320859468689409",
+		"full_text": "Red Velvet X aespa 'Beautiful Christmas' MV Teaser\n\n2022 Winter SMTOWN : SMCU PALACE \n#RedVelvet, #aespa - 'Beautiful Christmas'\n\n➫ 2022.12.14 6PM (KST)\n\nhttps://t.co/MXCnwwBFZ8\n\n#SMTOWN2023 #SMCU_PALACE #2022_WINTER_SMTOWN #SMTOWN #SMTOWN_LIVE #SMCU #BeautifulChristmas https://t.co/AkJBgriX33",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						86,
+						96
+					]
+				},
+				{
+					"text": "aespa",
+					"indices": [
+						98,
+						104
+					]
+				},
+				{
+					"text": "SMTOWN2023",
+					"indices": [
+						179,
+						190
+					]
+				},
+				{
+					"text": "SMCU_PALACE",
+					"indices": [
+						191,
+						203
+					]
+				},
+				{
+					"text": "2022_WINTER_SMTOWN",
+					"indices": [
+						204,
+						223
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						224,
+						231
+					]
+				},
+				{
+					"text": "SMTOWN_LIVE",
+					"indices": [
+						232,
+						244
+					]
+				},
+				{
+					"text": "SMCU",
+					"indices": [
+						245,
+						250
+					]
+				},
+				{
+					"text": "BeautifulChristmas",
+					"indices": [
+						251,
+						270
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MXCnwwBFZ8",
+					"expanded_url": "https://youtu.be/bHekFBpgbd0",
+					"display_url": "youtu.be/bHekFBpgbd0",
+					"indices": [
+						154,
+						177
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1602263662579761200,
+					"id_str": "1602263662579761153",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1602263662579761153/img/IUdxWkzPkJOJLxre.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1602263662579761153/img/IUdxWkzPkJOJLxre.jpg",
+					"url": "https://t.co/AkJBgriX33",
+					"display_url": "pic.twitter.com/AkJBgriX33",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1602317356948070400/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1602263662579761200,
+					"id_str": "1602263662579761153",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1602263662579761153/img/IUdxWkzPkJOJLxre.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1602263662579761153/img/IUdxWkzPkJOJLxre.jpg",
+					"url": "https://t.co/AkJBgriX33",
+					"display_url": "pic.twitter.com/AkJBgriX33",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1602317356948070400/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 16161,
+						"variants": [
+							{
+								"bitrate": 10368000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1602263662579761153/vid/1920x1080/GHNsSEpxlWmawun0.mp4?tag=16"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1602263662579761153/pl/Ye9GBUfnjwnzUZca.m3u8?tag=16&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1602263662579761153/vid/640x360/zfRcprV7x0oE_b8c.mp4?tag=16"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1602263662579761153/vid/1280x720/v-rkDmb6gZJVPN0X.mp4?tag=16"
+							},
+							{
+								"bitrate": 288000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1602263662579761153/vid/480x270/nr98JfQhZaWBzS9L.mp4?tag=16"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5789627,
+			"friends_count": 2,
+			"listed_count": 11570,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3174,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14853,
+		"favorite_count": 20659,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Red Velvet X aespa 'Beautiful Christmas' MV Teaser\n\n2022 Winter SMTOWN : SMCU PALACE \n#RedVelvet, #aespa - 'Beautiful Christmas'\n\n➫ 2022.12.14 6PM (KST)\n\nhttps://t.co/MXCnwwBFZ8\n\n#SMTOWN2023 #SMCU_PALACE #2022_WINTER_SMTOWN #SMTOWN #SMTOWN_LIVE #SMCU #BeautifulChristmas https://t.co/AkJBgriX33"
+	},
+	{
 		"created_at": "Mon Dec 12 13:13:56 +0000 2022",
 		"id": 1602287459441655800,
 		"id_str": "1602290657556500480",
