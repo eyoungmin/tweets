4152b5a730ad8de8c4e9695625bbe1d93a579ded
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-12 23:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92764,
-	"created_at": "Mon Dec 12 13:02:02 +0000 2022",
+	"tweets": 92765,
+	"created_at": "Mon Dec 12 14:02:01 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 12 13:13:56 +0000 2022",
+		"id": 1602287459441655800,
+		"id_str": "1602290657556500480",
+		"full_text": "💌리브야‼️ 생일 축하해 🎂ㅣ Welcome to ReVe’s Birthday Party🎉\n\nhttps://t.co/IhkHJj52GP\n\n#The_ReVe_Festival_2022\n#Birthday #벌스데이\n#레드벨벳 #RedVelvet\n#리브야생일축하해\n#Welcome_to_ReVes_Birthday_Party https://t.co/ytjM8mkHok",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "The_ReVe_Festival_2022",
+					"indices": [
+						77,
+						100
+					]
+				},
+				{
+					"text": "Birthday",
+					"indices": [
+						101,
+						110
+					]
+				},
+				{
+					"text": "벌스데이",
+					"indices": [
+						111,
+						116
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						117,
+						122
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						123,
+						133
+					]
+				},
+				{
+					"text": "리브야생일축하해",
+					"indices": [
+						134,
+						143
+					]
+				},
+				{
+					"text": "Welcome_to_ReVes_Birthday_Party",
+					"indices": [
+						144,
+						176
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IhkHJj52GP",
+					"expanded_url": "https://youtu.be/DgQ251ulmSU",
+					"display_url": "youtu.be/DgQ251ulmSU",
+					"indices": [
+						52,
+						75
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1602287452860797000,
+					"id_str": "1602287452860796930",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/Fjx38vfVUAIwN6N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fjx38vfVUAIwN6N.jpg",
+					"url": "https://t.co/ytjM8mkHok",
+					"display_url": "pic.twitter.com/ytjM8mkHok",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1602287459441655808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1602287452860797000,
+					"id_str": "1602287452860796930",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/Fjx38vfVUAIwN6N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fjx38vfVUAIwN6N.jpg",
+					"url": "https://t.co/ytjM8mkHok",
+					"display_url": "pic.twitter.com/ytjM8mkHok",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1602287459441655808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5789524,
+			"friends_count": 2,
+			"listed_count": 11569,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3173,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4144,
+		"favorite_count": 8536,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "💌리브야‼️ 생일 축하해 🎂ㅣ Welcome to ReVe’s Birthday Party🎉\n\nhttps://t.co/IhkHJj52GP\n\n#The_ReVe_Festival_2022\n#Birthday #벌스데이\n#레드벨벳 #RedVelvet\n#리브야생일축하해\n#Welcome_to_ReVes_Birthday_Party https://t.co/ytjM8mkHok"
+	},
+	{
 		"created_at": "Mon Dec 12 12:58:23 +0000 2022",
 		"id": 1602272165738721300,
 		"id_str": "1602286743801106432",
