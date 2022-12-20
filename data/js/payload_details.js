c1b5580f1069fc20a5c8c2aedb11e7289e653723
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-21 01:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92835,
-	"created_at": "Tue Dec 20 12:02:03 +0000 2022",
+	"tweets": 92836,
+	"created_at": "Tue Dec 20 16:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,260 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Tue Dec 20 15:11:02 +0000 2022",
+		"id": 1605205630431760400,
+		"id_str": "1605219229086584832",
+		"full_text": "LOOΠΔ \nThe Origin Album [0]\n\n#현진 #HyunJin 💌\n\n2023.01.03 TUE 6PM (KST) \n\n#이달의소녀 #LOONA\n#Zero #FREESM https://t.co/3Ry7GT3Q8d",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "현진",
+					"indices": [
+						29,
+						32
+					]
+				},
+				{
+					"text": "HyunJin",
+					"indices": [
+						33,
+						41
+					]
+				},
+				{
+					"text": "이달의소녀",
+					"indices": [
+						72,
+						78
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						79,
+						85
+					]
+				},
+				{
+					"text": "Zero",
+					"indices": [
+						86,
+						91
+					]
+				},
+				{
+					"text": "FREESM",
+					"indices": [
+						92,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1605201787279278000,
+					"id_str": "1605201787279278082",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FkbShVOagAIKFI_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FkbShVOagAIKFI_.jpg",
+					"url": "https://t.co/3Ry7GT3Q8d",
+					"display_url": "pic.twitter.com/3Ry7GT3Q8d",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1605205630431760386/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1605201787279278000,
+					"id_str": "1605201787279278082",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FkbShVOagAIKFI_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FkbShVOagAIKFI_.jpg",
+					"url": "https://t.co/3Ry7GT3Q8d",
+					"display_url": "pic.twitter.com/3Ry7GT3Q8d",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1605205630431760386/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1605201787283472400,
+					"id_str": "1605201787283472384",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/FkbShVPagAAq2KJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FkbShVPagAAq2KJ.jpg",
+					"url": "https://t.co/3Ry7GT3Q8d",
+					"display_url": "pic.twitter.com/3Ry7GT3Q8d",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1605205630431760386/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 857,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 486,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1463,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsse3B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsse3B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 946541,
+			"friends_count": 3,
+			"listed_count": 8861,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5315,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1603259958807924736/kmMZLUmp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1603259958807924736/kmMZLUmp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1671081319",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 510,
+		"favorite_count": 1848,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "LOOΠΔ \nThe Origin Album [0]\n\n#현진 #HyunJin 💌\n\n2023.01.03 TUE 6PM (KST) \n\n#이달의소녀 #LOONA\n#Zero #FREESM https://t.co/3Ry7GT3Q8d"
+	},
+	{
 		"created_at": "Tue Dec 20 12:00:40 +0000 2022",
 		"id": 1605171260048056300,
 		"id_str": "1605171320748011520",
