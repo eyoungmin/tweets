d000e4db55026ddb3178f3928dc2165e2d830d7e
Youngmin Lee <iameyoungmin@gmail.com>
2022-12-26 22:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92867,
-	"created_at": "Mon Dec 26 12:02:02 +0000 2022",
+	"tweets": 92868,
+	"created_at": "Mon Dec 26 13:02:02 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Mon Dec 26 12:07:09 +0000 2022",
+		"id": 1607346770157383700,
+		"id_str": "1607347280889401345",
+		"full_text": "&lt;재벌집 막내아들&gt; 레이첼 역의 '소녀시대 티파니 영'이 이제 배우의 길을 걷는다.\n#재벌집막내아들 #티파니 #레이첼 #소녀시대 #RebornRich #Tiffany #TIFFANYYOUNG #KDrama\nhttps://t.co/0RAWfMfgs8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "재벌집막내아들",
+					"indices": [
+						53,
+						61
+					]
+				},
+				{
+					"text": "티파니",
+					"indices": [
+						62,
+						66
+					]
+				},
+				{
+					"text": "레이첼",
+					"indices": [
+						67,
+						71
+					]
+				},
+				{
+					"text": "소녀시대",
+					"indices": [
+						72,
+						77
+					]
+				},
+				{
+					"text": "RebornRich",
+					"indices": [
+						78,
+						89
+					]
+				},
+				{
+					"text": "Tiffany",
+					"indices": [
+						90,
+						98
+					]
+				},
+				{
+					"text": "TIFFANYYOUNG",
+					"indices": [
+						99,
+						112
+					]
+				},
+				{
+					"text": "KDrama",
+					"indices": [
+						113,
+						120
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0RAWfMfgs8",
+					"expanded_url": "https://www.gqkorea.co.kr/2022/12/26/%ED%8B%B0%ED%8C%8C%EB%8B%88-%EC%98%81-%EB%B3%B8%EA%B2%A9%EC%A0%81%EC%9D%B8-%ED%99%9C%EB%8F%99-%EB%82%98%EC%84%A0%EB%8B%A4-%EC%86%A1%EA%B0%95%ED%98%B8-%EB%B9%84%EC%99%80-%ED%95%9C%EC%86%A5%EB%B0%A5/?ddw=133441&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "gqkorea.co.kr/2022/12/26/%ED…",
+					"indices": [
+						121,
+						144
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262470,
+			"id_str": "78262470",
+			"name": "GQ KOREA",
+			"screen_name": "GQKOREA",
+			"location": "Seoul, Korea",
+			"description": "Be a Good Man, Don't be Quiet.",
+			"url": "https://t.co/pJOMTcQb22",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pJOMTcQb22",
+							"expanded_url": "http://www.gqkorea.co.kr",
+							"display_url": "gqkorea.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 144271,
+			"friends_count": 0,
+			"listed_count": 219,
+			"created_at": "Tue Sep 29 09:20:13 +0000 2009",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2573,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1292702653697294336/AgaytUyk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1292702653697294336/AgaytUyk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262470/1666589234",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;재벌집 막내아들&gt; 레이첼 역의 '소녀시대 티파니 영'이 이제 배우의 길을 걷는다.\n#재벌집막내아들 #티파니 #레이첼 #소녀시대 #RebornRich #Tiffany #TIFFANYYOUNG #KDrama\nhttps://t.co/0RAWfMfgs8"
+	},
+	{
 		"created_at": "Mon Dec 26 11:17:05 +0000 2022",
 		"id": 1607332166635225000,
 		"id_str": "1607334682320056324",
