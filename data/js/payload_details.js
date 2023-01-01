c11f919ae40d76a9fbf0e407080b57b3e18d4c79
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-01 13:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92892,
-	"created_at": "Sat Dec 31 19:02:04 +0000 2022",
+	"tweets": 92893,
+	"created_at": "Sun Jan 01 04:02:02 +0000 2023",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,223 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Sun Jan 01 03:34:27 +0000 2023",
+		"id": 1609389071213674500,
+		"id_str": "1609392583389896704",
+		"full_text": "[#NEWS] 서현, 'KBS 연기대상' 2관왕...\"나만의 정답 찾아가는 배우 되겠다\"\n▶ https://t.co/dVSXPZGNsB\n\n#서현 #2022KBS연기대상 #SEOHYUN #2022KBSDramaAwards https://t.co/2AYmANa8fe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			122
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NEWS",
+					"indices": [
+						1,
+						6
+					]
+				},
+				{
+					"text": "서현",
+					"indices": [
+						77,
+						80
+					]
+				},
+				{
+					"text": "2022KBS연기대상",
+					"indices": [
+						81,
+						93
+					]
+				},
+				{
+					"text": "SEOHYUN",
+					"indices": [
+						94,
+						102
+					]
+				},
+				{
+					"text": "2022KBSDramaAwards",
+					"indices": [
+						103,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/dVSXPZGNsB",
+					"expanded_url": "http://bit.ly/3GskQT9",
+					"display_url": "bit.ly/3GskQT9",
+					"indices": [
+						52,
+						75
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1609385646770225200,
+					"id_str": "1609385646770225153",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FlWvuAZaAAEXIdB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FlWvuAZaAAEXIdB.jpg",
+					"url": "https://t.co/2AYmANa8fe",
+					"display_url": "pic.twitter.com/2AYmANa8fe",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1609389071213674496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1217,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1609385646770225200,
+					"id_str": "1609385646770225153",
+					"indices": [
+						123,
+						146
+					],
+					"media_url": "http://pbs.twimg.com/media/FlWvuAZaAAEXIdB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FlWvuAZaAAEXIdB.jpg",
+					"url": "https://t.co/2AYmANa8fe",
+					"display_url": "pic.twitter.com/2AYmANa8fe",
+					"expanded_url": "https://twitter.com/NAMOOACTORS2004/status/1609389071213674496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1217,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 465359487,
+			"id_str": "465359487",
+			"name": "NAMOOACTORS",
+			"screen_name": "NAMOOACTORS2004",
+			"location": "Seoul, KOREA",
+			"description": "NAMOOACTORS 공식 덕질 계정 4차 산업혁명 시대에 발맞춰 나가는 \"어덕행덕\" \"덕라밸\"을 이뤄내겠습니다. 📸 고화질 아니면 죽음을 달라 \ninstagram: @namooactors youtube: namooofficial",
+			"url": "https://t.co/jYLf4TGV0Y",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jYLf4TGV0Y",
+							"expanded_url": "https://twitter.com/NAMOOACTORS2004",
+							"display_url": "twitter.com/NAMOOACTORS2004",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 67980,
+			"friends_count": 15,
+			"listed_count": 288,
+			"created_at": "Mon Jan 16 08:03:15 +0000 2012",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 16743,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1758741819/Untitled-5_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 230,
+		"favorite_count": 299,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[#NEWS] 서현, 'KBS 연기대상' 2관왕...\"나만의 정답 찾아가는 배우 되겠다\"\n▶ https://t.co/dVSXPZGNsB\n\n#서현 #2022KBS연기대상 #SEOHYUN #2022KBSDramaAwards https://t.co/2AYmANa8fe"
+	}
+]
