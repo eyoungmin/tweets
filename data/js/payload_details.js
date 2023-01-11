72b2fc1a69c64bc19176740c995eff3982aaece4
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-11 22:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92941,
-	"created_at": "Wed Jan 11 12:02:01 +0000 2023",
+	"tweets": 92942,
+	"created_at": "Wed Jan 11 13:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Wed Jan 11 12:21:00 +0000 2023",
+		"id": 1613143757993685000,
+		"id_str": "1613148973258395648",
+		"full_text": "SMTOWN LIVE 2023 : SMCU PALACE @ KWANGYA Behind the Scenes \n\nhttps://t.co/M65AjloIk4\n\n#SMTOWN2023 #SMCU_PALACE\n#2022_WINTER_SMTOWN\n#SMTOWN #SMTOWN_LIVE #SMCU https://t.co/JIohs8JqVm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SMTOWN2023",
+					"indices": [
+						86,
+						97
+					]
+				},
+				{
+					"text": "SMCU_PALACE",
+					"indices": [
+						98,
+						110
+					]
+				},
+				{
+					"text": "2022_WINTER_SMTOWN",
+					"indices": [
+						111,
+						130
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						131,
+						138
+					]
+				},
+				{
+					"text": "SMTOWN_LIVE",
+					"indices": [
+						139,
+						151
+					]
+				},
+				{
+					"text": "SMCU",
+					"indices": [
+						152,
+						157
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/M65AjloIk4",
+					"expanded_url": "https://youtu.be/nSKNmTuBlnk",
+					"display_url": "youtu.be/nSKNmTuBlnk",
+					"indices": [
+						61,
+						84
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1613143754134945800,
+					"id_str": "1613143754134945796",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/FmMJspTagAQ-EbB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmMJspTagAQ-EbB.jpg",
+					"url": "https://t.co/JIohs8JqVm",
+					"display_url": "pic.twitter.com/JIohs8JqVm",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1613143757993684992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1613143754134945800,
+					"id_str": "1613143754134945796",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/FmMJspTagAQ-EbB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmMJspTagAQ-EbB.jpg",
+					"url": "https://t.co/JIohs8JqVm",
+					"display_url": "pic.twitter.com/JIohs8JqVm",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1613143757993684992/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5831292,
+			"friends_count": 2,
+			"listed_count": 11584,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3266,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2274,
+		"favorite_count": 5639,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "SMTOWN LIVE 2023 : SMCU PALACE @ KWANGYA Behind the Scenes \n\nhttps://t.co/M65AjloIk4\n\n#SMTOWN2023 #SMCU_PALACE\n#2022_WINTER_SMTOWN\n#SMTOWN #SMTOWN_LIVE #SMCU https://t.co/JIohs8JqVm"
+	},
+	{
 		"created_at": "Wed Jan 11 11:20:58 +0000 2023",
 		"id": 1613132152757186600,
 		"id_str": "1613133865065009152",
