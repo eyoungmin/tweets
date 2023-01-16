ad2988fc738a0f555029084f018c1c134850e7f9
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-16 21:02:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92965,
-	"created_at": "Mon Jan 16 08:02:02 +0000 2023",
+	"tweets": 92966,
+	"created_at": "Mon Jan 16 12:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,10 +1,249 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 16 11:22:08 +0000 2023",
+		"id": 1614942946142613500,
+		"id_str": "1614946099156090882",
+		"full_text": "날씨가 또 추워졌죠?\n옷 따뜻하게 입으셔야해요💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/WH46hhA6a9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			86
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						28,
+						31
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						32,
+						38
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						39,
+						42
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						43,
+						53
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						61,
+						70
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						71,
+						77
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						78,
+						81
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						82,
+						86
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1614942923212357600,
+					"id_str": "1614942923212357637",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/FmluCEaagAUIsMz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmluCEaagAUIsMz.jpg",
+					"url": "https://t.co/WH46hhA6a9",
+					"display_url": "pic.twitter.com/WH46hhA6a9",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1614942946142613505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1614942923212357600,
+					"id_str": "1614942923212357637",
+					"indices": [
+						87,
+						110
+					],
+					"media_url": "http://pbs.twimg.com/media/FmluCEaagAUIsMz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmluCEaagAUIsMz.jpg",
+					"url": "https://t.co/WH46hhA6a9",
+					"display_url": "pic.twitter.com/WH46hhA6a9",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1614942946142613505/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5837122,
+			"friends_count": 2,
+			"listed_count": 11594,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3269,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1569,
+		"favorite_count": 4972,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "날씨가 또 추워졌죠?\n옷 따뜻하게 입으셔야해요💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/WH46hhA6a9"
+	},
+	{
 		"created_at": "Mon Jan 16 07:37:21 +0000 2023",
 		"id": 1614888366033539000,
 		"id_str": "1614889529177157634",
-		"full_text": "Three days in, still no news from the glorious Twitter management. Very cool indeed.\nFenix on iOS inexplicably still working 🤷‍♂️\n\nLet's continue working on #woolly, there's still a ton to do.\nGet it on TestFlight if you want to try a new Mastodon app.\nhttps://t.co/7yiczuu430",
+		"full_text": "Three days in, still no news from the glorious Twitter management. Very cool indeed.\nFenix on iOS inexplicably still working 🤷♂️\n\nLet's continue working on #woolly, there's still a ton to do.\nGet it on TestFlight if you want to try a new Mastodon app.\nhttps://t.co/7yiczuu430",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -132,7 +371,7 @@ Grailbird.data.tweets_2023_01 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "Three days in, still no news from the glorious Twitter management. Very cool indeed.\nFenix on iOS inexplicably still working 🤷‍♂️\n\nLet's continue working on #woolly, there's still a ton to do.\nGet it on TestFlight if you want to try a new Mastodon app.\nhttps://t.co/7yiczuu430"
+		"text": "Three days in, still no news from the glorious Twitter management. Very cool indeed.\nFenix on iOS inexplicably still working 🤷♂️\n\nLet's continue working on #woolly, there's still a ton to do.\nGet it on TestFlight if you want to try a new Mastodon app.\nhttps://t.co/7yiczuu430"
 	},
 	{
 		"created_at": "Mon Jan 16 06:07:11 +0000 2023",
