9473fe2f56a9b0ac4d2b504206b92a610f7bb319
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-19 11:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92974,
-	"created_at": "Wed Jan 18 13:02:01 +0000 2023",
+	"tweets": 92975,
+	"created_at": "Thu Jan 19 02:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Thu Jan 19 01:09:14 +0000 2023",
+		"id": 1615878952442269700,
+		"id_str": "1615879020285145091",
+		"full_text": "#BLACKPINK 2nd VINYL LP [BORN PINK] -LIMITED EDITION-\nDetail page notice has been uploaded\n\n▶️ https://t.co/d8ia5Huo5R\n\n#블랙핑크 #BORNPINK #2ndVINYLLP #LIMITEDEDITION #20230119 #RELEASE #YG https://t.co/ZkQzIxZOa7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						120,
+						125
+					]
+				},
+				{
+					"text": "BORNPINK",
+					"indices": [
+						126,
+						135
+					]
+				},
+				{
+					"text": "2ndVINYLLP",
+					"indices": [
+						136,
+						147
+					]
+				},
+				{
+					"text": "LIMITEDEDITION",
+					"indices": [
+						148,
+						163
+					]
+				},
+				{
+					"text": "RELEASE",
+					"indices": [
+						174,
+						182
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						183,
+						186
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/d8ia5Huo5R",
+					"expanded_url": "https://weverse.io/blackpink/notice/12367",
+					"display_url": "weverse.io/blackpink/noti…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1615878941021208600,
+					"id_str": "1615878941021208576",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/FmzBVeeagAANP-2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmzBVeeagAANP-2.jpg",
+					"url": "https://t.co/ZkQzIxZOa7",
+					"display_url": "pic.twitter.com/ZkQzIxZOa7",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1615878952442269696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1615878941021208600,
+					"id_str": "1615878941021208576",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/FmzBVeeagAANP-2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FmzBVeeagAANP-2.jpg",
+					"url": "https://t.co/ZkQzIxZOa7",
+					"display_url": "pic.twitter.com/ZkQzIxZOa7",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1615878952442269696/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/vkfEFSP9At",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vkfEFSP9At",
+							"expanded_url": "https://lnk.to/YG_BLACKPINK",
+							"display_url": "lnk.to/YG_BLACKPINK",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8362628,
+			"friends_count": 0,
+			"listed_count": 8045,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 32,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 877,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1662480989",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3015,
+		"favorite_count": 12244,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#BLACKPINK 2nd VINYL LP [BORN PINK] -LIMITED EDITION-\nDetail page notice has been uploaded\n\n▶️ https://t.co/d8ia5Huo5R\n\n#블랙핑크 #BORNPINK #2ndVINYLLP #LIMITEDEDITION #20230119 #RELEASE #YG https://t.co/ZkQzIxZOa7"
+	},
+	{
 		"created_at": "Wed Jan 18 12:08:57 +0000 2023",
 		"id": 1615680527301841000,
 		"id_str": "1615682656435589121",
