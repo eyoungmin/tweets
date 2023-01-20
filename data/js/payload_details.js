e743d0f646fbf4fda0b735ccb2c83d813dd42ba5
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-20 23:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92982,
-	"created_at": "Fri Jan 20 12:02:02 +0000 2023",
+	"tweets": 92983,
+	"created_at": "Fri Jan 20 14:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Fri Jan 20 13:23:06 +0000 2023",
+		"id": 1616420654571753500,
+		"id_str": "1616426092256198657",
+		"full_text": "‘2022 Winter SMTOWN : SMCU PALACE’ Jacket Behind the Scene Pt.2\n\nhttps://t.co/cC8b9OYUNb\n\n#Jacket #Behind\n#SMTOWN2023 #SMCU_PALACE \n#2022_WINTER_SMTOWN \n#SMTOWN #SMTOWN_LIVE #SMCU https://t.co/PN5IvFNZqf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Jacket",
+					"indices": [
+						90,
+						97
+					]
+				},
+				{
+					"text": "Behind",
+					"indices": [
+						98,
+						105
+					]
+				},
+				{
+					"text": "SMTOWN2023",
+					"indices": [
+						106,
+						117
+					]
+				},
+				{
+					"text": "SMCU_PALACE",
+					"indices": [
+						118,
+						130
+					]
+				},
+				{
+					"text": "2022_WINTER_SMTOWN",
+					"indices": [
+						132,
+						151
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						153,
+						160
+					]
+				},
+				{
+					"text": "SMTOWN_LIVE",
+					"indices": [
+						161,
+						173
+					]
+				},
+				{
+					"text": "SMCU",
+					"indices": [
+						174,
+						179
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cC8b9OYUNb",
+					"expanded_url": "https://youtu.be/znMImGqX5QU",
+					"display_url": "youtu.be/znMImGqX5QU",
+					"indices": [
+						65,
+						88
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1616420644014678000,
+					"id_str": "1616420644014678017",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/Fm6uAvYaYAEyrrx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fm6uAvYaYAEyrrx.jpg",
+					"url": "https://t.co/PN5IvFNZqf",
+					"display_url": "pic.twitter.com/PN5IvFNZqf",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1616420654571753475/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1616420644014678000,
+					"id_str": "1616420644014678017",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/Fm6uAvYaYAEyrrx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fm6uAvYaYAEyrrx.jpg",
+					"url": "https://t.co/PN5IvFNZqf",
+					"display_url": "pic.twitter.com/PN5IvFNZqf",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1616420654571753475/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5841247,
+			"friends_count": 2,
+			"listed_count": 11595,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3276,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2277,
+		"favorite_count": 5332,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "‘2022 Winter SMTOWN : SMCU PALACE’ Jacket Behind the Scene Pt.2\n\nhttps://t.co/cC8b9OYUNb\n\n#Jacket #Behind\n#SMTOWN2023 #SMCU_PALACE \n#2022_WINTER_SMTOWN \n#SMTOWN #SMTOWN_LIVE #SMCU https://t.co/PN5IvFNZqf"
+	},
+	{
 		"created_at": "Fri Jan 20 11:23:06 +0000 2023",
 		"id": 1616392708842750000,
 		"id_str": "1616395893145763842",
