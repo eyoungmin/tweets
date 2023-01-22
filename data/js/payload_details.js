a90d18dc4fd2b03b6ac95e271d433b4237207da7
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-22 18:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 92988,
-	"created_at": "Sun Jan 22 05:02:01 +0000 2023",
+	"tweets": 92989,
+	"created_at": "Sun Jan 22 09:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Sun Jan 22 09:00:59 +0000 2023",
+		"id": 1617084790464725000,
+		"id_str": "1617084901366587398",
+		"full_text": "#지수 의 포근한 민트 컬러, 레이스를 덧댄 카라 등 세상 러블리한 숏 코트는 어디 브랜드?\n@ygofficialblink @RVsmtown @RBW_MAMAMOO @officialDavichi @iamkmk #블랙핑크 #레드벨벳 #조이 #마마무 #문별 #다비치 #강민경\nhttps://t.co/b1Zj1pX7UO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "지수",
+					"indices": [
+						0,
+						3
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						117,
+						122
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						123,
+						128
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						129,
+						132
+					]
+				},
+				{
+					"text": "마마무",
+					"indices": [
+						133,
+						137
+					]
+				},
+				{
+					"text": "문별",
+					"indices": [
+						138,
+						141
+					]
+				},
+				{
+					"text": "다비치",
+					"indices": [
+						142,
+						146
+					]
+				},
+				{
+					"text": "강민경",
+					"indices": [
+						147,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ygofficialblink",
+					"name": "BLACKPINK GLOBAL BLINK",
+					"id": 878204370403250200,
+					"id_str": "878204370403250176",
+					"indices": [
+						52,
+						68
+					]
+				},
+				{
+					"screen_name": "RVsmtown",
+					"name": "Red Velvet",
+					"id": 873092428755894300,
+					"id_str": "873092428755894272",
+					"indices": [
+						69,
+						78
+					]
+				},
+				{
+					"screen_name": "RBW_MAMAMOO",
+					"name": "마마무(MAMAMOO)",
+					"id": 2295631308,
+					"id_str": "2295631308",
+					"indices": [
+						79,
+						91
+					]
+				},
+				{
+					"screen_name": "officialDavichi",
+					"name": "official_Davichi",
+					"id": 2859446406,
+					"id_str": "2859446406",
+					"indices": [
+						92,
+						108
+					]
+				},
+				{
+					"screen_name": "iamkmk",
+					"name": "강민경",
+					"id": 181823334,
+					"id_str": "181823334",
+					"indices": [
+						109,
+						116
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/b1Zj1pX7UO",
+					"expanded_url": "https://www.allurekorea.com/?p=124003&ddw=135785&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "allurekorea.com/?p=124003&ddw=…",
+					"indices": [
+						152,
+						175
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262307,
+			"id_str": "78262307",
+			"name": "allure korea",
+			"screen_name": "allurekorea",
+			"location": "Seoul",
+			"description": "beauty & style expert, Allure Magazine Korea",
+			"url": "https://t.co/OcX4RMNXUs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OcX4RMNXUs",
+							"expanded_url": "http://www.allurekorea.com",
+							"display_url": "allurekorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38160,
+			"friends_count": 1,
+			"listed_count": 303,
+			"created_at": "Tue Sep 29 09:19:06 +0000 2009",
+			"favourites_count": 623,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8054,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262307/1674017934",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#지수 의 포근한 민트 컬러, 레이스를 덧댄 카라 등 세상 러블리한 숏 코트는 어디 브랜드?\n@ygofficialblink @RVsmtown @RBW_MAMAMOO @officialDavichi @iamkmk #블랙핑크 #레드벨벳 #조이 #마마무 #문별 #다비치 #강민경\nhttps://t.co/b1Zj1pX7UO"
+	},
+	{
 		"created_at": "Sun Jan 22 05:00:57 +0000 2023",
 		"id": 1617024385193312300,
 		"id_str": "1617024494853586950",
