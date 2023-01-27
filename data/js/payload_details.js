3a78e56792d8ef369d06e9e24c28d30fb6f4de04
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-27 20:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93016,
-	"created_at": "Fri Jan 27 10:02:01 +0000 2023",
+	"tweets": 93017,
+	"created_at": "Fri Jan 27 11:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Fri Jan 27 10:02:27 +0000 2023",
+		"id": 1618904312104767500,
+		"id_str": "1618912309996552193",
+		"full_text": "블랙핑크가 파리에서 열린 자선 콘서트에서 프랑스 영부인 브지리트 마크롱 여사와 만났다. \n#블랙핑크 #Blackpink\nhttps://t.co/h1mgecWOWO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						50,
+						55
+					]
+				},
+				{
+					"text": "Blackpink",
+					"indices": [
+						56,
+						66
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/h1mgecWOWO",
+					"expanded_url": "https://www.wkorea.com/2023/01/27/%ed%94%84%eb%9e%91%ec%8a%a4-%ec%98%81%eb%b6%80%ec%9d%b8%eb%8f%84-%eb%b0%98%ed%95%9c-%eb%b8%94%eb%9e%99%ed%95%91%ed%81%ac/?ddw=136312&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2023/01/27/%ed…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5gwu0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5gwu0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 329347,
+			"friends_count": 0,
+			"listed_count": 866,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7332,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14638,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1673836319",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 26,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블랙핑크가 파리에서 열린 자선 콘서트에서 프랑스 영부인 브지리트 마크롱 여사와 만났다. \n#블랙핑크 #Blackpink\nhttps://t.co/h1mgecWOWO"
+	},
+	{
 		"created_at": "Fri Jan 27 09:12:29 +0000 2023",
 		"id": 1618898281064702000,
 		"id_str": "1618899735489945600",
