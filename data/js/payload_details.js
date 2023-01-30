0fbc58e60dc9b86adba649f930daadbab50c0fab
Youngmin Lee <iameyoungmin@gmail.com>
2023-01-30 20:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93020,
-	"created_at": "Mon Jan 30 10:02:02 +0000 2023",
+	"tweets": 93021,
+	"created_at": "Mon Jan 30 11:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 30 10:03:13 +0000 2023",
+		"id": 1619986466188431400,
+		"id_str": "1619999668632436736",
+		"full_text": "업그레이드 버전을 기다려볼 워.워.워 워아이니♡ \nhttps://t.co/K97iAb0qby\n#taeyang #빅뱅 #크러쉬 #블랙핑크 #blackpink #다나카 #동영배",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "taeyang",
+					"indices": [
+						52,
+						60
+					]
+				},
+				{
+					"text": "빅뱅",
+					"indices": [
+						61,
+						64
+					]
+				},
+				{
+					"text": "크러쉬",
+					"indices": [
+						65,
+						69
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						70,
+						75
+					]
+				},
+				{
+					"text": "blackpink",
+					"indices": [
+						76,
+						86
+					]
+				},
+				{
+					"text": "다나카",
+					"indices": [
+						87,
+						91
+					]
+				},
+				{
+					"text": "동영배",
+					"indices": [
+						92,
+						96
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/K97iAb0qby",
+					"expanded_url": "https://www.gqkorea.co.kr/2023/01/30/%EC%97%AC%EB%9F%AC%EB%B6%84-%EB%B0%88%EC%9D%84-%EB%94%B0%EB%9D%BC%ED%95%9C-%EC%97%AC%EB%9F%AC%EB%B6%84-5/?ddw=136496&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "gqkorea.co.kr/2023/01/30/%EC…",
+					"indices": [
+						28,
+						51
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262470,
+			"id_str": "78262470",
+			"name": "GQ KOREA",
+			"screen_name": "GQKOREA",
+			"location": "Seoul, Korea",
+			"description": "Be a Good Man, Don't be Quiet.",
+			"url": "https://t.co/pJOMTcz802",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/pJOMTcz802",
+							"expanded_url": "http://www.gqkorea.co.kr",
+							"display_url": "gqkorea.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 147773,
+			"friends_count": 0,
+			"listed_count": 223,
+			"created_at": "Tue Sep 29 09:20:13 +0000 2009",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2656,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1292702653697294336/AgaytUyk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1292702653697294336/AgaytUyk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262470/1674800756",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "업그레이드 버전을 기다려볼 워.워.워 워아이니♡ \nhttps://t.co/K97iAb0qby\n#taeyang #빅뱅 #크러쉬 #블랙핑크 #blackpink #다나카 #동영배"
+	},
+	{
 		"created_at": "Mon Jan 30 09:38:40 +0000 2023",
 		"id": 1619989371276660700,
 		"id_str": "1619993489663737856",
