35f4c90a8480fd5a5902cdbbaa3a040f5906a1ef
Youngmin Lee <iameyoungmin@gmail.com>
2023-02-02 17:02:01 +0900

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93029,
-	"created_at": "Wed Feb 01 12:02:01 +0000 2023",
+	"tweets": 93032,
+	"created_at": "Thu Feb 02 08:02:00 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,553 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Thu Feb 02 06:30:12 +0000 2023",
+		"id": 1621025645122982000,
+		"id_str": "1621033222829379584",
+		"full_text": "2023 SM ARTIST SEASON'S GREETINGS OFFICIAL MD #GirlsGenerationOhGG\n2/3 (금) 오후 3시부터 다양한 온라인 판매처를 통해 예약하실 수 있습니다. \n\n▶ Online Store \nSMTOWN &amp;STORE : https://t.co/JRDkAYU6v2 \nSM true Store : https://t.co/YMAiGUBWKo https://t.co/MwBZlizMdt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			214
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GirlsGenerationOhGG",
+					"indices": [
+						46,
+						66
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JRDkAYU6v2",
+					"expanded_url": "http://smtownandstore.com/",
+					"display_url": "smtownandstore.com",
+					"indices": [
+						150,
+						173
+					]
+				},
+				{
+					"url": "https://t.co/YMAiGUBWKo",
+					"expanded_url": "https://shp.ee/m8urbtz",
+					"display_url": "shp.ee/m8urbtz",
+					"indices": [
+						191,
+						214
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1621013157123870700,
+					"id_str": "1621013157123870720",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn7-4L6aUAAExv9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn7-4L6aUAAExv9.jpg",
+					"url": "https://t.co/MwBZlizMdt",
+					"display_url": "pic.twitter.com/MwBZlizMdt",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1621025645122981893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 470,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 803,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 267,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1621013157123870700,
+					"id_str": "1621013157123870720",
+					"indices": [
+						215,
+						238
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn7-4L6aUAAExv9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn7-4L6aUAAExv9.jpg",
+					"url": "https://t.co/MwBZlizMdt",
+					"display_url": "pic.twitter.com/MwBZlizMdt",
+					"expanded_url": "https://twitter.com/GirlsGeneration/status/1621025645122981893/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 470,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 803,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 267,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3914727,
+			"friends_count": 5,
+			"listed_count": 6358,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3181,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1353,
+		"favorite_count": 2738,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2023 SM ARTIST SEASON'S GREETINGS OFFICIAL MD #GirlsGenerationOhGG\n2/3 (금) 오후 3시부터 다양한 온라인 판매처를 통해 예약하실 수 있습니다. \n\n▶ Online Store \nSMTOWN &amp;STORE : https://t.co/JRDkAYU6v2 \nSM true Store : https://t.co/YMAiGUBWKo https://t.co/MwBZlizMdt"
+	},
+	{
+		"created_at": "Thu Feb 02 06:30:12 +0000 2023",
+		"id": 1621025651821260800,
+		"id_str": "1621033222338646016",
+		"full_text": "SM Global Shop : https://t.co/WbK4TsVNA5 \nYES24 : https://t.co/xs5csBAKER \n11st : https://t.co/nc1WtpjFOL \nYIZHIYU : https://t.co/jNOi66NE3Y",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			140
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WbK4TsVNA5",
+					"expanded_url": "http://smglobalshop.com/",
+					"display_url": "smglobalshop.com",
+					"indices": [
+						17,
+						40
+					]
+				},
+				{
+					"url": "https://t.co/xs5csBAKER",
+					"expanded_url": "http://m.yes24.com/Event/EventDetail?eventNo=229364",
+					"display_url": "m.yes24.com/Event/EventDet…",
+					"indices": [
+						50,
+						73
+					]
+				},
+				{
+					"url": "https://t.co/nc1WtpjFOL",
+					"expanded_url": "http://11st.kr/&B8eOaw",
+					"display_url": "11st.kr/&B8eOaw",
+					"indices": [
+						82,
+						105
+					]
+				},
+				{
+					"url": "https://t.co/jNOi66NE3Y",
+					"expanded_url": "http://withfans.com/web/index.html#/",
+					"display_url": "withfans.com/web/index.html…",
+					"indices": [
+						117,
+						140
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1621025645122982000,
+		"in_reply_to_status_id_str": "1621025645122981893",
+		"in_reply_to_user_id": 391115625,
+		"in_reply_to_user_id_str": "391115625",
+		"in_reply_to_screen_name": "GirlsGeneration",
+		"user": {
+			"id": 391115625,
+			"id_str": "391115625",
+			"name": "Girls' Generation",
+			"screen_name": "GirlsGeneration",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/itXzBV3cSH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/itXzBV3cSH",
+							"expanded_url": "https://youtube.com/user/GIRLSGENERATION",
+							"display_url": "youtube.com/user/GIRLSGENE…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3914727,
+			"friends_count": 5,
+			"listed_count": 6358,
+			"created_at": "Sat Oct 15 02:28:18 +0000 2011",
+			"favourites_count": 72,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3181,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1C171E",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1551220686538276865/mAgQFVeM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/391115625/1658674834",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 91,
+		"favorite_count": 347,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "tl",
+		"text": "SM Global Shop : https://t.co/WbK4TsVNA5 \nYES24 : https://t.co/xs5csBAKER \n11st : https://t.co/nc1WtpjFOL \nYIZHIYU : https://t.co/jNOi66NE3Y"
+	},
+	{
+		"created_at": "Thu Feb 02 06:28:02 +0000 2023",
+		"id": 1621026588421947400,
+		"id_str": "1621032678924636160",
+		"full_text": "2023 SM ARTIST SEASON'S GREETINGS OFFICIAL MD #RedVelvet\n2/3 (금) 오후 3시부터 다양한 온라인 판매처를 통해 예약하실 수 있습니다.\n \n▶ Online Store\nSMTOWN &amp;STORE : https://t.co/InoTF40iFN\nSM true Store : https://t.co/dytzheNVbf\nSM Global Shop : https://t.co/X34y7uGm58 https://t.co/3lknT5nKdX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			243
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						46,
+						56
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/InoTF40iFN",
+					"expanded_url": "http://smtownandstore.com/",
+					"display_url": "smtownandstore.com",
+					"indices": [
+						139,
+						162
+					]
+				},
+				{
+					"url": "https://t.co/dytzheNVbf",
+					"expanded_url": "https://shp.ee/m8urbtz",
+					"display_url": "shp.ee/m8urbtz",
+					"indices": [
+						179,
+						202
+					]
+				},
+				{
+					"url": "https://t.co/X34y7uGm58",
+					"expanded_url": "http://smglobalshop.com/",
+					"display_url": "smglobalshop.com",
+					"indices": [
+						220,
+						243
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1621025345888743400,
+					"id_str": "1621025345888743424",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn8J9qmacAAShh3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn8J9qmacAAShh3.jpg",
+					"url": "https://t.co/3lknT5nKdX",
+					"display_url": "pic.twitter.com/3lknT5nKdX",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1621026588421947392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 267,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 470,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 803,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1621025345888743400,
+					"id_str": "1621025345888743424",
+					"indices": [
+						244,
+						267
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn8J9qmacAAShh3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn8J9qmacAAShh3.jpg",
+					"url": "https://t.co/3lknT5nKdX",
+					"display_url": "pic.twitter.com/3lknT5nKdX",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1621026588421947392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 267,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 470,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 803,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5851083,
+			"friends_count": 2,
+			"listed_count": 11621,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3297,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1477,
+		"favorite_count": 4392,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "2023 SM ARTIST SEASON'S GREETINGS OFFICIAL MD #RedVelvet\n2/3 (금) 오후 3시부터 다양한 온라인 판매처를 통해 예약하실 수 있습니다.\n \n▶ Online Store\nSMTOWN &amp;STORE : https://t.co/InoTF40iFN\nSM true Store : https://t.co/dytzheNVbf\nSM Global Shop : https://t.co/X34y7uGm58 https://t.co/3lknT5nKdX"
+	},
+	{
 		"created_at": "Wed Feb 01 11:27:50 +0000 2023",
 		"id": 1620740270643101700,
 		"id_str": "1620745738983710721",
