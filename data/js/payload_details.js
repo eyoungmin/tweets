c0c4937a8000d95c14107076e01a6a9edfbbd1a8
Youngmin Lee <iameyoungmin@gmail.com>
2023-02-02 19:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93032,
-	"created_at": "Thu Feb 02 08:02:00 +0000 2023",
+	"tweets": 93033,
+	"created_at": "Thu Feb 02 10:02:00 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Thu Feb 02 09:30:14 +0000 2023",
+		"id": 1621070930024996900,
+		"id_str": "1621078528543555585",
+		"full_text": "【#SONEJAPAN】昨年12月24日(土)にFCサイト内「SPECIAL」にて公開した『SONE NOTE LIVE Vol.40』のメイキング映像を「BLOG」に公開✨ぜひご覧ください♬\n※会員様限定コンテンツのため、転載禁止です。\n#少女時代\n#YuRi\n#ユリ\n#SONENOTELIVE https://t.co/v4YgmMCv9Q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SONEJAPAN",
+					"indices": [
+						1,
+						11
+					]
+				},
+				{
+					"text": "少女時代",
+					"indices": [
+						120,
+						125
+					]
+				},
+				{
+					"text": "YuRi",
+					"indices": [
+						126,
+						131
+					]
+				},
+				{
+					"text": "ユリ",
+					"indices": [
+						132,
+						135
+					]
+				},
+				{
+					"text": "SONENOTELIVE",
+					"indices": [
+						136,
+						149
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1621015406436831200,
+					"id_str": "1621015406436831232",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn8A7HQaUAApCpS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn8A7HQaUAApCpS.jpg",
+					"url": "https://t.co/v4YgmMCv9Q",
+					"display_url": "pic.twitter.com/v4YgmMCv9Q",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1621070930024996864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1796,
+							"h": 1010,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1621015406436831200,
+					"id_str": "1621015406436831232",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/Fn8A7HQaUAApCpS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fn8A7HQaUAApCpS.jpg",
+					"url": "https://t.co/v4YgmMCv9Q",
+					"display_url": "pic.twitter.com/v4YgmMCv9Q",
+					"expanded_url": "https://twitter.com/gg_official_jp/status/1621070930024996864/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1796,
+							"h": 1010,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2585025200,
+			"id_str": "2585025200",
+			"name": "少女時代 JAPAN OFFICIAL",
+			"screen_name": "gg_official_jp",
+			"location": "",
+			"description": "No.1 girl group, Girls’ Generation!",
+			"url": "https://t.co/T70Fo3NBNn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/T70Fo3NBNn",
+							"expanded_url": "http://www.girls-generation.jp/",
+							"display_url": "girls-generation.jp",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 123822,
+			"friends_count": 0,
+			"listed_count": 551,
+			"created_at": "Tue Jun 24 03:33:35 +0000 2014",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 455,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1555086105711886337/zo-mAiti_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1555086105711886337/zo-mAiti_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2585025200/1659596396",
+			"profile_link_color": "0024B3",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 266,
+		"favorite_count": 573,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "【#SONEJAPAN】昨年12月24日(土)にFCサイト内「SPECIAL」にて公開した『SONE NOTE LIVE Vol.40』のメイキング映像を「BLOG」に公開✨ぜひご覧ください♬\n※会員様限定コンテンツのため、転載禁止です。\n#少女時代\n#YuRi\n#ユリ\n#SONENOTELIVE https://t.co/v4YgmMCv9Q"
+	},
+	{
 		"created_at": "Thu Feb 02 06:30:12 +0000 2023",
 		"id": 1621025645122982000,
 		"id_str": "1621033222829379584",
