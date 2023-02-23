0cfdc4b773230626ba1d8e4bba56818e13a40b6b
Youngmin Lee <iameyoungmin@gmail.com>
2023-02-23 21:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93114,
-	"created_at": "Thu Feb 23 11:02:01 +0000 2023",
+	"tweets": 93115,
+	"created_at": "Thu Feb 23 12:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,10 +1,285 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Thu Feb 23 11:32:52 +0000 2023",
+		"id": 1628713257522581500,
+		"id_str": "1628719536622174208",
+		"full_text": "두근두근 기대의 연속인 \n영스트리트‼️\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/QOJd2qDUTS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						23,
+						26
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						27,
+						33
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						34,
+						37
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						38,
+						48
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						49,
+						54
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						56,
+						65
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						66,
+						72
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						77,
+						81
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1628713175536513000,
+					"id_str": "1628713175536513029",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FppaAPzaMAUaqtd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FppaAPzaMAUaqtd.jpg",
+					"url": "https://t.co/QOJd2qDUTS",
+					"display_url": "pic.twitter.com/QOJd2qDUTS",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1628713257522581504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1628713175536513000,
+					"id_str": "1628713175536513029",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FppaAPzaMAUaqtd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FppaAPzaMAUaqtd.jpg",
+					"url": "https://t.co/QOJd2qDUTS",
+					"display_url": "pic.twitter.com/QOJd2qDUTS",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1628713257522581504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1628713215827013600,
+					"id_str": "1628713215827013632",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FppaCl5acAAM9_Z.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FppaCl5acAAM9_Z.jpg",
+					"url": "https://t.co/QOJd2qDUTS",
+					"display_url": "pic.twitter.com/QOJd2qDUTS",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1628713257522581504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5867831,
+			"friends_count": 2,
+			"listed_count": 11644,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3320,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1806,
+		"favorite_count": 6237,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "두근두근 기대의 연속인 \n영스트리트‼️\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/QOJd2qDUTS"
+	},
+	{
 		"created_at": "Thu Feb 23 10:08:56 +0000 2023",
 		"id": 1628696167755272200,
 		"id_str": "1628698413855555584",
-		"full_text": "💇‍♀️중단발부터 숏단발까지! 단발 박제했으면 하는 연예인은 누가 있을까요? \n\n#aespa #윈터 #TWICE #지효 #BLACKPINK #리사 #CLC #예은 #IZONE #권은비 \nhttps://t.co/9yB3UIkE3Z",
+		"full_text": "💇♀️중단발부터 숏단발까지! 단발 박제했으면 하는 연예인은 누가 있을까요? \n\n#aespa #윈터 #TWICE #지효 #BLACKPINK #리사 #CLC #예은 #IZONE #권은비 \nhttps://t.co/9yB3UIkE3Z",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -176,7 +451,7 @@ Grailbird.data.tweets_2023_02 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "💇‍♀️중단발부터 숏단발까지! 단발 박제했으면 하는 연예인은 누가 있을까요? \n\n#aespa #윈터 #TWICE #지효 #BLACKPINK #리사 #CLC #예은 #IZONE #권은비 \nhttps://t.co/9yB3UIkE3Z"
+		"text": "💇♀️중단발부터 숏단발까지! 단발 박제했으면 하는 연예인은 누가 있을까요? \n\n#aespa #윈터 #TWICE #지효 #BLACKPINK #리사 #CLC #예은 #IZONE #권은비 \nhttps://t.co/9yB3UIkE3Z"
 	},
 	{
 		"created_at": "Thu Feb 23 07:29:41 +0000 2023",
