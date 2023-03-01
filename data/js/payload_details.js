3beebef22a13a6965036f82e68d669bdf1da567c
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-01 14:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93138,
-	"created_at": "Tue Feb 28 18:02:01 +0000 2023",
+	"tweets": 93139,
+	"created_at": "Wed Mar 01 05:02:00 +0000 2023",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,139 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Wed Mar 01 04:31:04 +0000 2023",
+		"id": 1630780072985755600,
+		"id_str": "1630787716286668801",
+		"full_text": "[UAENA NOTICE]\n&lt;유애나&gt; 6기 팬클럽 회원 등업 상세 안내\n&lt;UAENA&gt; 6th fan-club level upgrade details\n\n▶ https://t.co/66cNTvgTCV\n\n#아이유 #IU\n#유애나 #UAENA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이유",
+					"indices": [
+						123,
+						127
+					]
+				},
+				{
+					"text": "IU",
+					"indices": [
+						128,
+						131
+					]
+				},
+				{
+					"text": "유애나",
+					"indices": [
+						132,
+						136
+					]
+				},
+				{
+					"text": "UAENA",
+					"indices": [
+						137,
+						143
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/66cNTvgTCV",
+					"expanded_url": "https://cafe.daum.net/IU/S2pt/1",
+					"display_url": "cafe.daum.net/IU/S2pt/1",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024486566096326700,
+			"id_str": "1024486566096326656",
+			"name": "아이유(IU) 공식 트위터",
+			"screen_name": "_IUofficial",
+			"location": "",
+			"description": "아이유(IU) Official Twitter",
+			"url": "https://t.co/26KBouiO60",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/26KBouiO60",
+							"expanded_url": "http://cafe.daum.net/IU",
+							"display_url": "cafe.daum.net/IU",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1591511,
+			"friends_count": 1,
+			"listed_count": 2432,
+			"created_at": "Wed Aug 01 02:46:46 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1469,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024486566096326656/1633359681",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 407,
+		"favorite_count": 1639,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[UAENA NOTICE]\n&lt;유애나&gt; 6기 팬클럽 회원 등업 상세 안내\n&lt;UAENA&gt; 6th fan-club level upgrade details\n\n▶ https://t.co/66cNTvgTCV\n\n#아이유 #IU\n#유애나 #UAENA"
+	}
+]
