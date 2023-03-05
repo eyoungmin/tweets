74deab28a77c2790321d482e37437650a3908363
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-05 18:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93157,
-	"created_at": "Sun Mar 05 05:02:00 +0000 2023",
+	"tweets": 93158,
+	"created_at": "Sun Mar 05 09:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,10 +1,200 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sun Mar 05 08:32:05 +0000 2023",
+		"id": 1632289975302787000,
+		"id_str": "1632297921700937729",
+		"full_text": "[LOONATHEWORLD]\n\nThank you Tokyo❤\nSee you again Orbit 👋🏻\n\n#이달의소녀 #LOONA https://t.co/MYL7n1ps8N",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "이달의소녀",
+					"indices": [
+						58,
+						64
+					]
+				},
+				{
+					"text": "LOONA",
+					"indices": [
+						65,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1632289967832711200,
+					"id_str": "1632289967832711170",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FqcPE9caQAIbnVR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqcPE9caQAIbnVR.jpg",
+					"url": "https://t.co/MYL7n1ps8N",
+					"display_url": "pic.twitter.com/MYL7n1ps8N",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1632289975302787072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1632289967832711200,
+					"id_str": "1632289967832711170",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/FqcPE9caQAIbnVR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqcPE9caQAIbnVR.jpg",
+					"url": "https://t.co/MYL7n1ps8N",
+					"display_url": "pic.twitter.com/MYL7n1ps8N",
+					"expanded_url": "https://twitter.com/loonatheworld/status/1632289975302787072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1366,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 770959712791597000,
+			"id_str": "770959712791597056",
+			"name": "이달의 소녀(LOOΠΔ)",
+			"screen_name": "loonatheworld",
+			"location": "",
+			"description": "이달의 소녀 공식 트위터입니다.",
+			"url": "https://t.co/t0tQcsse3B",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/t0tQcsse3B",
+							"expanded_url": "http://www.loonatheworld.com",
+							"display_url": "loonatheworld.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 908934,
+			"friends_count": 3,
+			"listed_count": 8703,
+			"created_at": "Wed Aug 31 12:21:34 +0000 2016",
+			"favourites_count": 35,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5345,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1613484545420177411/LZaVVlBl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1613484545420177411/LZaVVlBl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/770959712791597056/1673500664",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 289,
+		"favorite_count": 1211,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[LOONATHEWORLD]\n\nThank you Tokyo❤\nSee you again Orbit 👋🏻\n\n#이달의소녀 #LOONA https://t.co/MYL7n1ps8N"
+	},
+	{
 		"created_at": "Sun Mar 05 04:21:51 +0000 2023",
 		"id": 1632229450896253000,
 		"id_str": "1632234945899900928",
-		"full_text": "룰루레몬을 입고 #레드벨벳 #예리 배우 #예수정 모델 #요요 그리고 요가 명상 지도자 #비하 와 함께 웰비잉(Well-being)에 대해 이야기 나누어 보았습니다. 🧘🏻‍♀️\n👉🏻https://t.co/b76WdrPCw4 https://t.co/OMatNqRGtp",
+		"full_text": "룰루레몬을 입고 #레드벨벳 #예리 배우 #예수정 모델 #요요 그리고 요가 명상 지도자 #비하 와 함께 웰비잉(Well-being)에 대해 이야기 나누어 보았습니다. 🧘🏻♀️\n👉🏻https://t.co/b76WdrPCw4 https://t.co/OMatNqRGtp",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -219,7 +409,7 @@ Grailbird.data.tweets_2023_03 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "ko",
-		"text": "룰루레몬을 입고 #레드벨벳 #예리 배우 #예수정 모델 #요요 그리고 요가 명상 지도자 #비하 와 함께 웰비잉(Well-being)에 대해 이야기 나누어 보았습니다. 🧘🏻‍♀️\n👉🏻https://t.co/b76WdrPCw4 https://t.co/OMatNqRGtp"
+		"text": "룰루레몬을 입고 #레드벨벳 #예리 배우 #예수정 모델 #요요 그리고 요가 명상 지도자 #비하 와 함께 웰비잉(Well-being)에 대해 이야기 나누어 보았습니다. 🧘🏻♀️\n👉🏻https://t.co/b76WdrPCw4 https://t.co/OMatNqRGtp"
 	},
 	{
 		"created_at": "Sat Mar 04 19:46:36 +0000 2023",
