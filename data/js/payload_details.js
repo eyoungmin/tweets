a4393188fe238f9905c8a7b9bbec4a47929755f3
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-06 16:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93163,
-	"created_at": "Mon Mar 06 06:02:01 +0000 2023",
+	"tweets": 93164,
+	"created_at": "Mon Mar 06 07:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Mon Mar 06 06:35:29 +0000 2023",
+		"id": 1632624065713438700,
+		"id_str": "1632630964097622016",
+		"full_text": "Red Velvet 4th Concert : R to V\n\n📍올림픽공원 KSPO DOME\n\n➫2023.04.01(SAT) : 6PM (KST)\n➫2023.04.02(SUN) : 4PM (KST)\n\n🎟ACE OPEN\n23.03.07 (TUE) : 8PM (KST)\n\n🎟 TICKET OPEN\n23.03.09 (THU) : 8PM (KST)\n\n#RedVelvet #RtoV\n#RedVelvet4thConcert_RtoV https://t.co/keGylrclku",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			232
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						190,
+						200
+					]
+				},
+				{
+					"text": "RtoV",
+					"indices": [
+						201,
+						206
+					]
+				},
+				{
+					"text": "RedVelvet4thConcert_RtoV",
+					"indices": [
+						207,
+						232
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1632624058071421000,
+					"id_str": "1632624058071420928",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/Fqg-7kkakAA7ULs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fqg-7kkakAA7ULs.jpg",
+					"url": "https://t.co/keGylrclku",
+					"display_url": "pic.twitter.com/keGylrclku",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1632624065713438720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1414,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1632624058071421000,
+					"id_str": "1632624058071420928",
+					"indices": [
+						233,
+						256
+					],
+					"media_url": "http://pbs.twimg.com/media/Fqg-7kkakAA7ULs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fqg-7kkakAA7ULs.jpg",
+					"url": "https://t.co/keGylrclku",
+					"display_url": "pic.twitter.com/keGylrclku",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1632624065713438720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1414,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 849,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5877970,
+			"friends_count": 2,
+			"listed_count": 11675,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4832,
+		"favorite_count": 11025,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Red Velvet 4th Concert : R to V\n\n📍올림픽공원 KSPO DOME\n\n➫2023.04.01(SAT) : 6PM (KST)\n➫2023.04.02(SUN) : 4PM (KST)\n\n🎟ACE OPEN\n23.03.07 (TUE) : 8PM (KST)\n\n🎟 TICKET OPEN\n23.03.09 (THU) : 8PM (KST)\n\n#RedVelvet #RtoV\n#RedVelvet4thConcert_RtoV https://t.co/keGylrclku"
+	},
+	{
 		"created_at": "Mon Mar 06 05:12:00 +0000 2023",
 		"id": 1632599631543828500,
 		"id_str": "1632609954090696704",
