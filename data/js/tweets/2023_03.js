6ea801889de8dd1488dc619e29eacb8c67319c3e
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-09 15:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,163 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Thu Mar 09 05:12:53 +0000 2023",
+		"id": 1633694100695416800,
+		"id_str": "1633697340006739969",
+		"full_text": "새싹처럼 푸릇한 대학생 새내기 분위기! 성큼 다가온 봄에는 데님 패션 스타일링 5.\n\n#데님 #청청 #노윤서 #문가영 #조이 #허윤진 #지수\nhttps://t.co/Gqo9ev66a1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			102
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "데님",
+					"indices": [
+						48,
+						51
+					]
+				},
+				{
+					"text": "청청",
+					"indices": [
+						52,
+						55
+					]
+				},
+				{
+					"text": "노윤서",
+					"indices": [
+						56,
+						60
+					]
+				},
+				{
+					"text": "문가영",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						66,
+						69
+					]
+				},
+				{
+					"text": "허윤진",
+					"indices": [
+						70,
+						74
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						75,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Gqo9ev66a1",
+					"expanded_url": "https://www.allurekorea.com/?p=198324&ddw=139391&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "allurekorea.com/?p=198324&ddw=…",
+					"indices": [
+						79,
+						102
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262307,
+			"id_str": "78262307",
+			"name": "allure korea",
+			"screen_name": "allurekorea",
+			"location": "Seoul",
+			"description": "beauty & style expert, Allure Magazine Korea",
+			"url": "https://t.co/OcX4RMNXUs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OcX4RMNXUs",
+							"expanded_url": "http://www.allurekorea.com",
+							"display_url": "allurekorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 38069,
+			"friends_count": 1,
+			"listed_count": 301,
+			"created_at": "Tue Sep 29 09:19:06 +0000 2009",
+			"favourites_count": 717,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 8144,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EDECE9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme3/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1123776053632229378/Dt_b2R_a_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262307/1676687083",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "E3E2DE",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "새싹처럼 푸릇한 대학생 새내기 분위기! 성큼 다가온 봄에는 데님 패션 스타일링 5.\n\n#데님 #청청 #노윤서 #문가영 #조이 #허윤진 #지수\nhttps://t.co/Gqo9ev66a1"
+	},
+	{
 		"created_at": "Thu Mar 09 04:12:52 +0000 2023",
 		"id": 1633669227927470000,
 		"id_str": "1633682238230646785",
