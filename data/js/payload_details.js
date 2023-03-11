48a33edf05d1a4a49b6205cc3d430175d7b80539
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-12 00:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93192,
-	"created_at": "Sat Mar 11 14:02:01 +0000 2023",
+	"tweets": 93193,
+	"created_at": "Sat Mar 11 15:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,253 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 11 14:36:43 +0000 2023",
+		"id": 1634549173201797000,
+		"id_str": "1634564008782049280",
+		"full_text": "러비들 오늘 즐거웠어요!!\n곧 만나요💜\n\n#HappyYERIday\n#예리 #YERI \n#RedVelvet #레드벨벳 https://t.co/YYfIaTtFic",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HappyYERIday",
+					"indices": [
+						23,
+						36
+					]
+				},
+				{
+					"text": "예리",
+					"indices": [
+						37,
+						40
+					]
+				},
+				{
+					"text": "YERI",
+					"indices": [
+						41,
+						46
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						48,
+						58
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						59,
+						64
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1634549160237215700,
+					"id_str": "1634549160237215746",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq8VzQ_aMAIsKVI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq8VzQ_aMAIsKVI.jpg",
+					"url": "https://t.co/YYfIaTtFic",
+					"display_url": "pic.twitter.com/YYfIaTtFic",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1634549173201797121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1634549160237215700,
+					"id_str": "1634549160237215746",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq8VzQ_aMAIsKVI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq8VzQ_aMAIsKVI.jpg",
+					"url": "https://t.co/YYfIaTtFic",
+					"display_url": "pic.twitter.com/YYfIaTtFic",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1634549173201797121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1634549160241430500,
+					"id_str": "1634549160241430528",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq8VzRAagAAb-kh.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq8VzRAagAAb-kh.jpg",
+					"url": "https://t.co/YYfIaTtFic",
+					"display_url": "pic.twitter.com/YYfIaTtFic",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1634549173201797121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5882065,
+			"friends_count": 2,
+			"listed_count": 11660,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3339,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4958,
+		"favorite_count": 13601,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "러비들 오늘 즐거웠어요!!\n곧 만나요💜\n\n#HappyYERIday\n#예리 #YERI \n#RedVelvet #레드벨벳 https://t.co/YYfIaTtFic"
+	},
+	{
 		"created_at": "Sat Mar 11 13:13:30 +0000 2023",
 		"id": 1634539677616328700,
 		"id_str": "1634543068773859328",
