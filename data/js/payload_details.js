40fe60ca88f461851e86111035900d52405a5c21
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-12 04:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93193,
-	"created_at": "Sat Mar 11 15:02:01 +0000 2023",
+	"tweets": 93194,
+	"created_at": "Sat Mar 11 19:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sat Mar 11 18:48:31 +0000 2023",
+		"id": 1634617888672825300,
+		"id_str": "1634627379837509639",
+		"full_text": "It was a complete honour being invited to celebrate International\nWomen's Day with an incredible room of inspiring women at @AnnabelsMayfair with The Caring Family Foundation 🤍 and very special to play alongside the beautiful Audrey Riley string quartet https://t.co/ZJ4DderKiP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			253
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "AnnabelsMayfair",
+					"name": "Annabel's",
+					"id": 1435130792,
+					"id_str": "1435130792",
+					"indices": [
+						124,
+						140
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1634617882553245700,
+					"id_str": "1634617882553245700",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq9UTbkWYAQvM4y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq9UTbkWYAQvM4y.jpg",
+					"url": "https://t.co/ZJ4DderKiP",
+					"display_url": "pic.twitter.com/ZJ4DderKiP",
+					"expanded_url": "https://twitter.com/birdy/status/1634617888672825349/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 798,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1362,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 452,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1634617882553245700,
+					"id_str": "1634617882553245700",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq9UTbkWYAQvM4y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq9UTbkWYAQvM4y.jpg",
+					"url": "https://t.co/ZJ4DderKiP",
+					"display_url": "pic.twitter.com/ZJ4DderKiP",
+					"expanded_url": "https://twitter.com/birdy/status/1634617888672825349/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 798,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1362,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 452,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1634617882544951300,
+					"id_str": "1634617882544951297",
+					"indices": [
+						254,
+						277
+					],
+					"media_url": "http://pbs.twimg.com/media/Fq9UTbiX0AEWwn_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fq9UTbiX0AEWwn_.jpg",
+					"url": "https://t.co/ZJ4DderKiP",
+					"display_url": "pic.twitter.com/ZJ4DderKiP",
+					"expanded_url": "https://twitter.com/birdy/status/1634617888672825349/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 452,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1362,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 798,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 270901752,
+			"id_str": "270901752",
+			"name": "Birdy",
+			"screen_name": "birdy",
+			"location": "",
+			"description": "Raincatchers is yours, listen below 🌧🩰🔮 My fifth album ..P O R T R A i T S.. is out 14th July",
+			"url": "https://t.co/hvZ8uW6Pkl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hvZ8uW6Pkl",
+							"expanded_url": "https://birdy.lnk.to/Bio",
+							"display_url": "birdy.lnk.to/Bio",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 305999,
+			"friends_count": 175,
+			"listed_count": 1153,
+			"created_at": "Wed Mar 23 13:25:01 +0000 2011",
+			"favourites_count": 862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2521,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1629171726885027843/5g1NCOIB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1629171726885027843/5g1NCOIB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/270901752/1677837601",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 153,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It was a complete honour being invited to celebrate International\nWomen's Day with an incredible room of inspiring women at @AnnabelsMayfair with The Caring Family Foundation 🤍 and very special to play alongside the beautiful Audrey Riley string quartet https://t.co/ZJ4DderKiP"
+	},
+	{
 		"created_at": "Sat Mar 11 14:36:43 +0000 2023",
 		"id": 1634549173201797000,
 		"id_str": "1634564008782049280",
