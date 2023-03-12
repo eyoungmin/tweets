df2a1b210362444154fc7cbb2a32e2610329d5d4
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-13 00:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93195,
-	"created_at": "Sun Mar 12 01:02:02 +0000 2023",
+	"tweets": 93196,
+	"created_at": "Sun Mar 12 15:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Sun Mar 12 14:48:05 +0000 2023",
+		"id": 1634918133537591300,
+		"id_str": "1634929259444862982",
+		"full_text": "BoA with SMTOWN💛\nat BoA 20th Anniversary Live THE BoA : Musicality\n\n#BoA #보아 \n#BoA20thAnniversaryLive #THEBoAMusicality\n#SMTOWN https://t.co/KujoSJIYx4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BoA",
+					"indices": [
+						68,
+						72
+					]
+				},
+				{
+					"text": "보아",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "BoA20thAnniversaryLive",
+					"indices": [
+						78,
+						101
+					]
+				},
+				{
+					"text": "THEBoAMusicality",
+					"indices": [
+						102,
+						119
+					]
+				},
+				{
+					"text": "SMTOWN",
+					"indices": [
+						120,
+						127
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1634918125367083000,
+					"id_str": "1634918125367083010",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/FrBlX3NaQAICc59.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrBlX3NaQAICc59.jpg",
+					"url": "https://t.co/KujoSJIYx4",
+					"display_url": "pic.twitter.com/KujoSJIYx4",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1634918133537591297/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1634918125367083000,
+					"id_str": "1634918125367083010",
+					"indices": [
+						128,
+						151
+					],
+					"media_url": "http://pbs.twimg.com/media/FrBlX3NaQAICc59.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrBlX3NaQAICc59.jpg",
+					"url": "https://t.co/KujoSJIYx4",
+					"display_url": "pic.twitter.com/KujoSJIYx4",
+					"expanded_url": "https://twitter.com/SMTOWNGLOBAL/status/1634918133537591297/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 884378714,
+			"id_str": "884378714",
+			"name": "SMTOWN",
+			"screen_name": "SMTOWNGLOBAL",
+			"location": "",
+			"description": "SMEntertainment Group Official Twitter",
+			"url": "https://t.co/zjb7YW3ApX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zjb7YW3ApX",
+							"expanded_url": "http://www.smtown.com",
+							"display_url": "smtown.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11533227,
+			"friends_count": 56,
+			"listed_count": 13047,
+			"created_at": "Tue Oct 16 12:49:45 +0000 2012",
+			"favourites_count": 2043,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13926,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "F08BA0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1586422828147974144/eRSNnR48_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1586422828147974144/eRSNnR48_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/884378714/1667067652",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13292,
+		"favorite_count": 31661,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BoA with SMTOWN💛\nat BoA 20th Anniversary Live THE BoA : Musicality\n\n#BoA #보아 \n#BoA20thAnniversaryLive #THEBoAMusicality\n#SMTOWN https://t.co/KujoSJIYx4"
+	},
+	{
 		"created_at": "Sun Mar 12 00:36:49 +0000 2023",
 		"id": 1634705770838392800,
 		"id_str": "1634715032138272768",
