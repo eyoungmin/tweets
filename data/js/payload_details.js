b94cf6d4aff537cdedab29c6a81d6613699b8b81
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-15 21:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93216,
-	"created_at": "Wed Mar 15 10:02:01 +0000 2023",
+	"tweets": 93217,
+	"created_at": "Wed Mar 15 12:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 15 11:37:40 +0000 2023",
+		"id": 1635960510700216300,
+		"id_str": "1635968501704716290",
+		"full_text": "몸이 찌뿌둥하면\n기지개를 쫘악 켜고\n영스로 텐션업😆😆💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳  \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/XGHR8mXKeq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						32,
+						35
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						36,
+						42
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						43,
+						46
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						47,
+						57
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						58,
+						63
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						66,
+						75
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						76,
+						82
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						83,
+						86
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						87,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1635960501770526700,
+					"id_str": "1635960501770526720",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FrQZaJ6aIAAbk6v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrQZaJ6aIAAbk6v.jpg",
+					"url": "https://t.co/XGHR8mXKeq",
+					"display_url": "pic.twitter.com/XGHR8mXKeq",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1635960510700216322/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1635960501770526700,
+					"id_str": "1635960501770526720",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FrQZaJ6aIAAbk6v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrQZaJ6aIAAbk6v.jpg",
+					"url": "https://t.co/XGHR8mXKeq",
+					"display_url": "pic.twitter.com/XGHR8mXKeq",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1635960510700216322/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5884823,
+			"friends_count": 2,
+			"listed_count": 11672,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3352,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1646,
+		"favorite_count": 5275,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "몸이 찌뿌둥하면\n기지개를 쫘악 켜고\n영스로 텐션업😆😆💙\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳  \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/XGHR8mXKeq"
+	},
+	{
 		"created_at": "Wed Mar 15 09:14:29 +0000 2023",
 		"id": 1635928825086238700,
 		"id_str": "1635932470376816640",
