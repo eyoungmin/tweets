cbf56afc94b53ffc25acb6bccb8ae1339d32075a
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-20 13:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Mon Mar 20 03:15:48 +0000 2023",
+		"id": 1637652354479820800,
+		"id_str": "1637654143849431040",
+		"full_text": "블랙핑크 지수, 솔로 타이틀곡 '꽃',  오는 31일 오후 공개\n#지수 #BLACKPINK #블랙핑크 #JISOO #FIRSTSINGLEALBUM #ME #TITLE #꽃 #FLOWER \nhttps://t.co/iJkd6PsBvM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			128
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "지수",
+					"indices": [
+						36,
+						39
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						40,
+						50
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						51,
+						56
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						57,
+						63
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						64,
+						81
+					]
+				},
+				{
+					"text": "ME",
+					"indices": [
+						82,
+						85
+					]
+				},
+				{
+					"text": "TITLE",
+					"indices": [
+						86,
+						92
+					]
+				},
+				{
+					"text": "꽃",
+					"indices": [
+						93,
+						95
+					]
+				},
+				{
+					"text": "FLOWER",
+					"indices": [
+						96,
+						103
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iJkd6PsBvM",
+					"expanded_url": "https://www.wkorea.com/2023/03/20/%EB%B8%94%EB%9E%99%ED%95%91%ED%81%AC-%EC%A7%80%EC%88%98-%EC%86%94%EB%A1%9C-%ED%83%80%EC%9D%B4%ED%8B%80%EA%B3%A1-%EC%A0%9C%EB%AA%A9-%EA%BD%83/?ddw=140020&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2023/03/20/%EB…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5gwu0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5gwu0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 333800,
+			"friends_count": 0,
+			"listed_count": 868,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7325,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14853,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1677032622",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 24,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블랙핑크 지수, 솔로 타이틀곡 '꽃',  오는 31일 오후 공개\n#지수 #BLACKPINK #블랙핑크 #JISOO #FIRSTSINGLEALBUM #ME #TITLE #꽃 #FLOWER \nhttps://t.co/iJkd6PsBvM"
+	},
+	{
 		"created_at": "Mon Mar 20 02:35:39 +0000 2023",
 		"id": 1637635175072018400,
 		"id_str": "1637644040261836801",
