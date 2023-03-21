5e5ece5eacbb47f4b216af5da1365e62e8446393
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-21 18:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93245,
-	"created_at": "Tue Mar 21 08:02:02 +0000 2023",
+	"tweets": 93246,
+	"created_at": "Tue Mar 21 09:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Tue Mar 21 08:35:59 +0000 2023",
+		"id": 1638095693322805200,
+		"id_str": "1638097109332180998",
+		"full_text": "[NOTICE]\n\nIUAENA OFFICIAL SONIC RING\nSIZE 13호/15호 추가 안내\nIUAENA OFFICIAL SONIC RING\nSIZE 13/15 Additional INFO \n\n▶ https://t.co/J3QbKgP4E2\n\n#아이유 #IU\n#유애나 #UAENA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이유",
+					"indices": [
+						139,
+						143
+					]
+				},
+				{
+					"text": "IU",
+					"indices": [
+						144,
+						147
+					]
+				},
+				{
+					"text": "유애나",
+					"indices": [
+						148,
+						152
+					]
+				},
+				{
+					"text": "UAENA",
+					"indices": [
+						153,
+						159
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/J3QbKgP4E2",
+					"expanded_url": "https://cafe.daum.net/IU/NDuY/522",
+					"display_url": "cafe.daum.net/IU/NDuY/522",
+					"indices": [
+						114,
+						137
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024486566096326700,
+			"id_str": "1024486566096326656",
+			"name": "아이유(IU) 공식 트위터",
+			"screen_name": "_IUofficial",
+			"location": "",
+			"description": "아이유(IU) Official Twitter",
+			"url": "https://t.co/26KBouiO60",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/26KBouiO60",
+							"expanded_url": "http://cafe.daum.net/IU",
+							"display_url": "cafe.daum.net/IU",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1597756,
+			"friends_count": 1,
+			"listed_count": 2443,
+			"created_at": "Wed Aug 01 02:46:46 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1477,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024486566096326656/1633359681",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 435,
+		"favorite_count": 1492,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[NOTICE]\n\nIUAENA OFFICIAL SONIC RING\nSIZE 13호/15호 추가 안내\nIUAENA OFFICIAL SONIC RING\nSIZE 13/15 Additional INFO \n\n▶ https://t.co/J3QbKgP4E2\n\n#아이유 #IU\n#유애나 #UAENA"
+	},
+	{
 		"created_at": "Tue Mar 21 06:41:13 +0000 2023",
 		"id": 1638057706279035000,
 		"id_str": "1638068227350700032",
