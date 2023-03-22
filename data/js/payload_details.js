05f67ed36e863c2768a7a2ab0e08c6867770b8a8
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-22 13:02:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93248,
-	"created_at": "Tue Mar 21 16:02:01 +0000 2023",
+	"tweets": 93249,
+	"created_at": "Wed Mar 22 04:02:05 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,152 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 22 03:26:20 +0000 2023",
+		"id": 1638374971889307600,
+		"id_str": "1638381569336303618",
+		"full_text": "#OFFICIALBLINK \nPlease vote for the hashtag that will be used to celebrate #JISOO FIRST SINGLE ALBUM [ME]!\n#BLINK Show your support for JISOO by picking the hashtags💕\n\n⏰Mar 22,12PM ~ Mar 23, 12PM (KST)\n🗳Vote for each One Kor/Eng Hashtag\n\n#BLACKPINK #지수 #FIRSTSINGLEALBUM #ME",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OFFICIALBLINK",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						75,
+						81
+					]
+				},
+				{
+					"text": "BLINK",
+					"indices": [
+						107,
+						113
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						238,
+						248
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						249,
+						252
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						253,
+						270
+					]
+				},
+				{
+					"text": "ME",
+					"indices": [
+						271,
+						274
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1638374966986182700,
+		"in_reply_to_status_id_str": "1638374966986182656",
+		"in_reply_to_user_id": 878204370403250200,
+		"in_reply_to_user_id_str": "878204370403250176",
+		"in_reply_to_screen_name": "ygofficialblink",
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/5OX1WxHQ4P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5OX1WxHQ4P",
+							"expanded_url": "https://blackpink.lnk.to/BPSHOP",
+							"display_url": "blackpink.lnk.to/BPSHOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8913728,
+			"friends_count": 10,
+			"listed_count": 9498,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2662,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1662480677",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1232,
+		"favorite_count": 2100,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "#OFFICIALBLINK \nPlease vote for the hashtag that will be used to celebrate #JISOO FIRST SINGLE ALBUM [ME]!\n#BLINK Show your support for JISOO by picking the hashtags💕\n\n⏰Mar 22,12PM ~ Mar 23, 12PM (KST)\n🗳Vote for each One Kor/Eng Hashtag\n\n#BLACKPINK #지수 #FIRSTSINGLEALBUM #ME"
+	},
+	{
 		"created_at": "Tue Mar 21 15:26:11 +0000 2023",
 		"id": 1638193841739284500,
 		"id_str": "1638200339496050688",
