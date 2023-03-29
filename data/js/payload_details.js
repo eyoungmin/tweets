1d80724e7f0db8d488b52959c8cab43b16f7d1c4
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-29 21:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93306,
-	"created_at": "Wed Mar 29 11:02:02 +0000 2023",
+	"tweets": 93307,
+	"created_at": "Wed Mar 29 12:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 29 11:41:05 +0000 2023",
+		"id": 1641032719235686400,
+		"id_str": "1641042790942400512",
+		"full_text": "oh! Pottery Day with IRENE 💗\n\nhttps://t.co/1qBApRwvQy\n\n#HappyIRENEday\n#ItsReneBaeDay\n#아이린_생일축하_자신있나\n#레드벨벳 #아이린 #IRENE https://t.co/h8ddwOAU88",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HappyIRENEday",
+					"indices": [
+						55,
+						69
+					]
+				},
+				{
+					"text": "ItsReneBaeDay",
+					"indices": [
+						70,
+						84
+					]
+				},
+				{
+					"text": "아이린_생일축하_자신있나",
+					"indices": [
+						85,
+						99
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						100,
+						105
+					]
+				},
+				{
+					"text": "아이린",
+					"indices": [
+						106,
+						110
+					]
+				},
+				{
+					"text": "IRENE",
+					"indices": [
+						111,
+						117
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1qBApRwvQy",
+					"expanded_url": "https://youtu.be/FF-YsOFzHnM",
+					"display_url": "youtu.be/FF-YsOFzHnM",
+					"indices": [
+						30,
+						53
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1641032709396119600,
+					"id_str": "1641032709396119554",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/FsYejfxaQAIuvJH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsYejfxaQAIuvJH.jpg",
+					"url": "https://t.co/h8ddwOAU88",
+					"display_url": "pic.twitter.com/h8ddwOAU88",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1641032719235686403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641032709396119600,
+					"id_str": "1641032709396119554",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/FsYejfxaQAIuvJH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsYejfxaQAIuvJH.jpg",
+					"url": "https://t.co/h8ddwOAU88",
+					"display_url": "pic.twitter.com/h8ddwOAU88",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1641032719235686403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5898504,
+			"friends_count": 2,
+			"listed_count": 11686,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3376,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7354,
+		"favorite_count": 15300,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "oh! Pottery Day with IRENE 💗\n\nhttps://t.co/1qBApRwvQy\n\n#HappyIRENEday\n#ItsReneBaeDay\n#아이린_생일축하_자신있나\n#레드벨벳 #아이린 #IRENE https://t.co/h8ddwOAU88"
+	},
+	{
 		"created_at": "Wed Mar 29 10:43:18 +0000 2023",
 		"id": 1641015262601445400,
 		"id_str": "1641028252591353859",
