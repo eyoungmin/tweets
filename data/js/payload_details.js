856e849e2272cf81fa0d2f1cb0997b4a8c9aca29
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-31 16:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93332,
-	"created_at": "Fri Mar 31 06:02:03 +0000 2023",
+	"tweets": 93333,
+	"created_at": "Fri Mar 31 07:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 31 06:41:33 +0000 2023",
+		"id": 1641683347641610200,
+		"id_str": "1641692188445929473",
+		"full_text": "2023 Red Velvet 4th Concert : R to V\n\nOFFICIAL MD 2ND LINE UP ONLINE SALES NOTICE\n\n▶ Sales Day\n\n2023. 04. 03 (MON) 15:00 ~ 2023. 04. 10 (MON) 23:59 [KST] https://t.co/CIlbpVKJIA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1641683342574915600,
+					"id_str": "1641683342574915584",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FshuTVPakAArdiT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FshuTVPakAArdiT.jpg",
+					"url": "https://t.co/CIlbpVKJIA",
+					"display_url": "pic.twitter.com/CIlbpVKJIA",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1641683347641610240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 378,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 214,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 645,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641683342574915600,
+					"id_str": "1641683342574915584",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FshuTVPakAArdiT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FshuTVPakAArdiT.jpg",
+					"url": "https://t.co/CIlbpVKJIA",
+					"display_url": "pic.twitter.com/CIlbpVKJIA",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1641683347641610240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 378,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 214,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 645,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1641683332349202400,
+					"id_str": "1641683332349202433",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FshuSvJakAEsQDs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FshuSvJakAEsQDs.jpg",
+					"url": "https://t.co/CIlbpVKJIA",
+					"display_url": "pic.twitter.com/CIlbpVKJIA",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1641683347641610240/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 354,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 201,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 605,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5900841,
+			"friends_count": 2,
+			"listed_count": 11693,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3390,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2176,
+		"favorite_count": 4490,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "2023 Red Velvet 4th Concert : R to V\n\nOFFICIAL MD 2ND LINE UP ONLINE SALES NOTICE\n\n▶ Sales Day\n\n2023. 04. 03 (MON) 15:00 ~ 2023. 04. 10 (MON) 23:59 [KST] https://t.co/CIlbpVKJIA"
+	},
+	{
 		"created_at": "Fri Mar 31 05:18:55 +0000 2023",
 		"id": 1641666636208910300,
 		"id_str": "1641671393996595202",
