a3b99fd03f56e3c620055cf9217e8b105d2d180d
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-31 20:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,312 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 31 10:28:56 +0000 2023",
+		"id": 1641741491323150300,
+		"id_str": "1641749409116323841",
+		"full_text": "Thank you @youtubemusic for the support!❤️ Go listen to #JISOO 's First Single Album #ME now on #YouTubeMusic\n\n🎧 https://t.co/Vs9zvETvqh\n\n#지수 #BLACKPINK #블랙핑크 #FIRSTSINGLEALBUM #꽃 #FLOWER #YG https://t.co/EnQgkig0r5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JISOO",
+					"indices": [
+						56,
+						62
+					]
+				},
+				{
+					"text": "ME",
+					"indices": [
+						85,
+						88
+					]
+				},
+				{
+					"text": "YouTubeMusic",
+					"indices": [
+						96,
+						109
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						138,
+						141
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						142,
+						152
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						153,
+						158
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						159,
+						176
+					]
+				},
+				{
+					"text": "꽃",
+					"indices": [
+						177,
+						179
+					]
+				},
+				{
+					"text": "FLOWER",
+					"indices": [
+						180,
+						187
+					]
+				},
+				{
+					"text": "YG",
+					"indices": [
+						188,
+						191
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "youtubemusic",
+					"name": "YouTube Music",
+					"id": 880142286176231400,
+					"id_str": "880142286176231425",
+					"indices": [
+						10,
+						23
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Vs9zvETvqh",
+					"expanded_url": "http://bit.ly/3TZDNTb",
+					"display_url": "bit.ly/3TZDNTb",
+					"indices": [
+						113,
+						136
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1641741258002690000,
+					"id_str": "1641741258002690051",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1641741258002690051/img/aTjUPN-pmUJOwLXj.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1641741258002690051/img/aTjUPN-pmUJOwLXj.jpg",
+					"url": "https://t.co/EnQgkig0r5",
+					"display_url": "pic.twitter.com/EnQgkig0r5",
+					"expanded_url": "https://twitter.com/ygofficialblink/status/1641741491323150336/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1641741258002690000,
+					"id_str": "1641741258002690051",
+					"indices": [
+						192,
+						215
+					],
+					"media_url": "http://pbs.twimg.com/amplify_video_thumb/1641741258002690051/img/aTjUPN-pmUJOwLXj.jpg",
+					"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/1641741258002690051/img/aTjUPN-pmUJOwLXj.jpg",
+					"url": "https://t.co/EnQgkig0r5",
+					"display_url": "pic.twitter.com/EnQgkig0r5",
+					"expanded_url": "https://twitter.com/ygofficialblink/status/1641741491323150336/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1920,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 15000,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/amplify_video/1641741258002690051/pl/nAJW1G068tSrzUVL.m3u8?tag=16&container=fmp4"
+							},
+							{
+								"bitrate": 950000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1641741258002690051/vid/480x852/wJ8k5BuXSjTS1VgD.mp4?tag=16"
+							},
+							{
+								"bitrate": 10368000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1641741258002690051/vid/1080x1920/wzZe0T8gd-aAmVSy.mp4?tag=16"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1641741258002690051/vid/320x568/uSOJA-9_eIaMWAom.mp4?tag=16"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/amplify_video/1641741258002690051/vid/720x1280/CQ0yo_ts7ooR9iKr.mp4?tag=16"
+							}
+						]
+					},
+					"additional_media_info": {
+						"title": "",
+						"description": "",
+						"embeddable": true,
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/5OX1WxHQ4P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5OX1WxHQ4P",
+							"expanded_url": "https://blackpink.lnk.to/BPSHOP",
+							"display_url": "blackpink.lnk.to/BPSHOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8931416,
+			"friends_count": 10,
+			"listed_count": 9497,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2709,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1680235305",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3051,
+		"favorite_count": 9377,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you @youtubemusic for the support!❤️ Go listen to #JISOO 's First Single Album #ME now on #YouTubeMusic\n\n🎧 https://t.co/Vs9zvETvqh\n\n#지수 #BLACKPINK #블랙핑크 #FIRSTSINGLEALBUM #꽃 #FLOWER #YG https://t.co/EnQgkig0r5"
+	},
+	{
 		"created_at": "Fri Mar 31 09:42:41 +0000 2023",
 		"id": 1641727205981159400,
 		"id_str": "1641737770275143680",
