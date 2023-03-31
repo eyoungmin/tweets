bd31184e4c272328bace35b510c4acf4b05d1fb0
Youngmin Lee <iameyoungmin@gmail.com>
2023-03-31 21:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93338,
-	"created_at": "Fri Mar 31 11:02:02 +0000 2023",
+	"tweets": 93339,
+	"created_at": "Fri Mar 31 12:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Fri Mar 31 11:28:56 +0000 2023",
+		"id": 1641759241647079400,
+		"id_str": "1641764512209543171",
+		"full_text": "#OFFICIALBLINK\n\nJISOO FIRST SINGLE ALBUM [ME]\n타이틀곡 ‘꽃(FLOWER)’의 응원법을 공개합니다🥰\n\n지수의 첫 솔로 데뷔를\nBLINK의 꽃 같은 목소리로\n크게 응원해주세요🌹\n\n▶️ https://t.co/XClq5yEKbZ\n\n#JISOO #지수 #BLACKPINK #블랙핑크\n#FIRSTSINGLEALBUM #ME #꽃 #FLOWER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OFFICIALBLINK",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						147,
+						153
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						154,
+						157
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						158,
+						168
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						169,
+						174
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						175,
+						192
+					]
+				},
+				{
+					"text": "ME",
+					"indices": [
+						193,
+						196
+					]
+				},
+				{
+					"text": "꽃",
+					"indices": [
+						197,
+						199
+					]
+				},
+				{
+					"text": "FLOWER",
+					"indices": [
+						200,
+						207
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XClq5yEKbZ",
+					"expanded_url": "https://weverse.io/blackpink/notice/12972",
+					"display_url": "weverse.io/blackpink/noti…",
+					"indices": [
+						122,
+						145
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/5OX1WxHQ4P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5OX1WxHQ4P",
+							"expanded_url": "https://blackpink.lnk.to/BPSHOP",
+							"display_url": "blackpink.lnk.to/BPSHOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8931545,
+			"friends_count": 10,
+			"listed_count": 9497,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2712,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1680235305",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1333,
+		"favorite_count": 4274,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#OFFICIALBLINK\n\nJISOO FIRST SINGLE ALBUM [ME]\n타이틀곡 ‘꽃(FLOWER)’의 응원법을 공개합니다🥰\n\n지수의 첫 솔로 데뷔를\nBLINK의 꽃 같은 목소리로\n크게 응원해주세요🌹\n\n▶️ https://t.co/XClq5yEKbZ\n\n#JISOO #지수 #BLACKPINK #블랙핑크\n#FIRSTSINGLEALBUM #ME #꽃 #FLOWER"
+	},
+	{
 		"created_at": "Fri Mar 31 10:28:56 +0000 2023",
 		"id": 1641741491323150300,
 		"id_str": "1641749409116323841",
