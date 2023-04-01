bd513a2a554a9120e90a42301bdeac52e39a195b
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-01 14:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93346,
-	"created_at": "Fri Mar 31 17:02:03 +0000 2023",
+	"tweets": 93347,
+	"created_at": "Sat Apr 01 05:02:02 +0000 2023",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,203 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Sat Apr 01 04:38:44 +0000 2023",
+		"id": 1642014073448693800,
+		"id_str": "1642023668753498113",
+		"full_text": "[아이유의 딜리버리🚘] \n\n운전.. 쉽지 않네? l Ep.1\nDriving... It's not easy l Ep.1 \n\n🚘 https://t.co/SJrryrLqw5 \n\n#아이유 #IU https://t.co/SVQFNcjPve",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "아이유",
+					"indices": [
+						96,
+						100
+					]
+				},
+				{
+					"text": "IU",
+					"indices": [
+						101,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SJrryrLqw5",
+					"expanded_url": "https://youtu.be/liyVLEFGYSw",
+					"display_url": "youtu.be/liyVLEFGYSw",
+					"indices": [
+						70,
+						93
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1642014070995050500,
+					"id_str": "1642014070995050496",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/FsmbGQnaYAA15ql.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsmbGQnaYAA15ql.jpg",
+					"url": "https://t.co/SVQFNcjPve",
+					"display_url": "pic.twitter.com/SVQFNcjPve",
+					"expanded_url": "https://twitter.com/_IUofficial/status/1642014073448693761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1642014070995050500,
+					"id_str": "1642014070995050496",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/FsmbGQnaYAA15ql.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsmbGQnaYAA15ql.jpg",
+					"url": "https://t.co/SVQFNcjPve",
+					"display_url": "pic.twitter.com/SVQFNcjPve",
+					"expanded_url": "https://twitter.com/_IUofficial/status/1642014073448693761/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1152,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1024486566096326700,
+			"id_str": "1024486566096326656",
+			"name": "아이유(IU) 공식 트위터",
+			"screen_name": "_IUofficial",
+			"location": "",
+			"description": "아이유(IU) Official Twitter",
+			"url": "https://t.co/26KBouiO60",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/26KBouiO60",
+							"expanded_url": "http://cafe.daum.net/IU",
+							"display_url": "cafe.daum.net/IU",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1601601,
+			"friends_count": 1,
+			"listed_count": 2453,
+			"created_at": "Wed Aug 01 02:46:46 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 1480,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1374979417915547648/vKspl9Et_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1024486566096326656/1633359681",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2038,
+		"favorite_count": 5224,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[아이유의 딜리버리🚘] \n\n운전.. 쉽지 않네? l Ep.1\nDriving... It's not easy l Ep.1 \n\n🚘 https://t.co/SJrryrLqw5 \n\n#아이유 #IU https://t.co/SVQFNcjPve"
+	}
+]
