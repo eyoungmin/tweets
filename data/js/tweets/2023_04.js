fca50d9d7421a519962cd88daafb5e47dacbc66f
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-02 23:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,318 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 02 13:42:06 +0000 2023",
+		"id": 1642513553688252400,
+		"id_str": "1642522799645372416",
+		"full_text": "러비들 오늘 너무 즐거웠고 감동적이었어요 힝🥹🥹\n오늘 이 순간을 우리 잊지 말자구요!!\n집에 조심히 들어가요💗💛💙💚💜\n\n#RedVelvet #레드벨벳 #RtoV\n#RedVelvet4thConcert_RtoV https://t.co/9hZDNxP2MT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			115
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						67,
+						77
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						78,
+						83
+					]
+				},
+				{
+					"text": "RtoV",
+					"indices": [
+						84,
+						89
+					]
+				},
+				{
+					"text": "RedVelvet4thConcert_RtoV",
+					"indices": [
+						90,
+						115
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1642513523522834400,
+					"id_str": "1642513523522834432",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FsthWORakAA5ZwE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsthWORakAA5ZwE.jpg",
+					"url": "https://t.co/9hZDNxP2MT",
+					"display_url": "pic.twitter.com/9hZDNxP2MT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1642513553688252418/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1642513523522834400,
+					"id_str": "1642513523522834432",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FsthWORakAA5ZwE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsthWORakAA5ZwE.jpg",
+					"url": "https://t.co/9hZDNxP2MT",
+					"display_url": "pic.twitter.com/9hZDNxP2MT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1642513553688252418/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1642513531374563300,
+					"id_str": "1642513531374563334",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FsthWrhacAYOLKL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsthWrhacAYOLKL.jpg",
+					"url": "https://t.co/9hZDNxP2MT",
+					"display_url": "pic.twitter.com/9hZDNxP2MT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1642513553688252418/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1642513538303549400,
+					"id_str": "1642513538303549444",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FsthXFVaYAQssLb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsthXFVaYAQssLb.jpg",
+					"url": "https://t.co/9hZDNxP2MT",
+					"display_url": "pic.twitter.com/9hZDNxP2MT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1642513553688252418/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1642513546381770800,
+					"id_str": "1642513546381770752",
+					"indices": [
+						116,
+						139
+					],
+					"media_url": "http://pbs.twimg.com/media/FsthXjbaQAALZMN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FsthXjbaQAALZMN.jpg",
+					"url": "https://t.co/9hZDNxP2MT",
+					"display_url": "pic.twitter.com/9hZDNxP2MT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1642513553688252418/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 903,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 512,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1084,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5904089,
+			"friends_count": 2,
+			"listed_count": 11700,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3393,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10423,
+		"favorite_count": 21776,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "러비들 오늘 너무 즐거웠고 감동적이었어요 힝🥹🥹\n오늘 이 순간을 우리 잊지 말자구요!!\n집에 조심히 들어가요💗💛💙💚💜\n\n#RedVelvet #레드벨벳 #RtoV\n#RedVelvet4thConcert_RtoV https://t.co/9hZDNxP2MT"
+	},
+	{
 		"created_at": "Sun Apr 02 09:54:09 +0000 2023",
 		"id": 1642455920470036500,
 		"id_str": "1642465433046351872",
