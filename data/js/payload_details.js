b67990b5ef43ed5292eba5df8ae46deda544e47e
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-02 11:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93354,
-	"created_at": "Sun Apr 02 01:02:00 +0000 2023",
+	"tweets": 93355,
+	"created_at": "Sun Apr 02 02:02:00 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,128 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 02 01:29:19 +0000 2023",
+		"id": 1642331884314321000,
+		"id_str": "1642338387972562945",
+		"full_text": "음원 스트리밍 이벤트 진행 중 참여 시스템상 일부 문제가 있어 이벤트 기간을 연장하게 되었습니다.\n\n이벤트 참여에 불편을 드려 죄송하며 이벤트 기간을 연장하오니 팬 여러분들의 너른 양해 부탁 드립니다.\n\n▶ https://t.co/iYKnRgg0mB\n\n#꽃과_함께_찾아온_지수_솔로 #JISOO_Blooming_with_ME",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "꽃과_함께_찾아온_지수_솔로",
+					"indices": [
+						141,
+						157
+					]
+				},
+				{
+					"text": "JISOO_Blooming_with_ME",
+					"indices": [
+						158,
+						181
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iYKnRgg0mB",
+					"expanded_url": "https://weverse.io/blackpink/notice/12979",
+					"display_url": "weverse.io/blackpink/noti…",
+					"indices": [
+						116,
+						139
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1641621332851847200,
+		"in_reply_to_status_id_str": "1641621332851847169",
+		"in_reply_to_user_id": 878204370403250200,
+		"in_reply_to_user_id_str": "878204370403250176",
+		"in_reply_to_screen_name": "ygofficialblink",
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/5OX1WxHQ4P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5OX1WxHQ4P",
+							"expanded_url": "https://blackpink.lnk.to/BPSHOP",
+							"display_url": "blackpink.lnk.to/BPSHOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8934141,
+			"friends_count": 10,
+			"listed_count": 9500,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 70,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2718,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1680235305",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 142,
+		"favorite_count": 616,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "음원 스트리밍 이벤트 진행 중 참여 시스템상 일부 문제가 있어 이벤트 기간을 연장하게 되었습니다.\n\n이벤트 참여에 불편을 드려 죄송하며 이벤트 기간을 연장하오니 팬 여러분들의 너른 양해 부탁 드립니다.\n\n▶ https://t.co/iYKnRgg0mB\n\n#꽃과_함께_찾아온_지수_솔로 #JISOO_Blooming_with_ME"
+	},
+	{
 		"created_at": "Sun Apr 02 00:41:59 +0000 2023",
 		"id": 1642315911863632000,
 		"id_str": "1642326474328776704",
