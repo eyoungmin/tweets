c5270bc9e51a1ca4fee05b260fbc2f578c364286
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-03 11:02:02 +0900

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,142 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Mon Apr 03 01:19:44 +0000 2023",
+		"id": 1642697286357745700,
+		"id_str": "1642698363043807232",
+		"full_text": "블랙핑크 지수 '꽃', 연일 신기록 갱신중.\n #지수 #Jisoo #JISOO_Blooming_With_ME #JISOOFLOWER\nhttps://t.co/ASSy7rdYTr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "지수",
+					"indices": [
+						26,
+						29
+					]
+				},
+				{
+					"text": "Jisoo",
+					"indices": [
+						30,
+						36
+					]
+				},
+				{
+					"text": "JISOO_Blooming_With_ME",
+					"indices": [
+						37,
+						60
+					]
+				},
+				{
+					"text": "JISOOFLOWER",
+					"indices": [
+						61,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ASSy7rdYTr",
+					"expanded_url": "https://www.wkorea.com/2023/04/03/%eb%b8%94%eb%9e%99%ed%95%91%ed%81%ac-%ec%a7%80%ec%88%98-%ea%bd%83-%ec%8b%a0%ea%b8%b0%eb%a1%9d%ec%97%90-%eb%98%90-%ec%8b%a0%ea%b8%b0%eb%a1%9d/?ddw=140746&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2023/04/03/%eb…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5gwu0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5gwu0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 335039,
+			"friends_count": 0,
+			"listed_count": 870,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14919,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1679293637",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 105,
+		"favorite_count": 193,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "블랙핑크 지수 '꽃', 연일 신기록 갱신중.\n #지수 #Jisoo #JISOO_Blooming_With_ME #JISOOFLOWER\nhttps://t.co/ASSy7rdYTr"
+	},
+	{
 		"created_at": "Sun Apr 02 15:53:36 +0000 2023",
 		"id": 1642544918722740200,
 		"id_str": "1642555890707599360",
