4cb75ead20f6edfcc4e9c09fc51f11ce269ae32d
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-04 12:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93370,
-	"created_at": "Tue Apr 04 01:02:00 +0000 2023",
+	"tweets": 93371,
+	"created_at": "Tue Apr 04 03:02:00 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 04 02:20:00 +0000 2023",
+		"id": 1643072611436597200,
+		"id_str": "1643075918716346371",
+		"full_text": "내가 직접 블랙핑크를 프로듀싱 한다? 블랙핑크의 첫 모바일 게임 ‘블랙핑크 더 게임’이 나온다.\n#블랙핑크 #블랙핑크더게임 #blackpink \nhttps://t.co/cYT1HNLPoj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "블랙핑크",
+					"indices": [
+						54,
+						59
+					]
+				},
+				{
+					"text": "블랙핑크더게임",
+					"indices": [
+						60,
+						68
+					]
+				},
+				{
+					"text": "blackpink",
+					"indices": [
+						69,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cYT1HNLPoj",
+					"expanded_url": "https://www.wkorea.com/2023/04/04/%eb%b8%94%eb%9e%99%ed%95%91%ed%81%ac-%eb%aa%a8%eb%b0%94%ec%9d%bc-%ea%b2%8c%ec%9e%84-%eb%b8%94%eb%9e%99%ed%95%91%ed%81%ac-%eb%8d%94-%ea%b2%8c%ec%9e%84-%ec%b6%9c%ec%8b%9c/?ddw=140841&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2023/04/04/%eb…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5gwu0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5gwu0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 335379,
+			"friends_count": 0,
+			"listed_count": 870,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14930,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1679293637",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 15,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "내가 직접 블랙핑크를 프로듀싱 한다? 블랙핑크의 첫 모바일 게임 ‘블랙핑크 더 게임’이 나온다.\n#블랙핑크 #블랙핑크더게임 #blackpink \nhttps://t.co/cYT1HNLPoj"
+	},
+	{
 		"created_at": "Tue Apr 04 00:19:58 +0000 2023",
 		"id": 1643040690580906000,
 		"id_str": "1643045713146703872",
