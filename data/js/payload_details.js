934ddfdf07a11275d39e07b98ce7f67d2bf7288e
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-04 15:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93373,
-	"created_at": "Tue Apr 04 05:02:00 +0000 2023",
+	"tweets": 93374,
+	"created_at": "Tue Apr 04 06:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,149 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 04 05:20:03 +0000 2023",
+		"id": 1643116430836236300,
+		"id_str": "1643121228570304518",
+		"full_text": "이쯤 되니 궁금해지는 바로 그것, 지수가 뮤비에서 입은 그 옷 어디 건데?\n#지수 #꽃 #JISOO #JISOO_FLOWER #BlackPink \nhttps://t.co/Ebw93CSKKQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			105
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "지수",
+					"indices": [
+						42,
+						45
+					]
+				},
+				{
+					"text": "꽃",
+					"indices": [
+						46,
+						48
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						49,
+						55
+					]
+				},
+				{
+					"text": "JISOO_FLOWER",
+					"indices": [
+						56,
+						69
+					]
+				},
+				{
+					"text": "BlackPink",
+					"indices": [
+						70,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ebw93CSKKQ",
+					"expanded_url": "https://www.wkorea.com/2023/04/04/%eb%b8%94%eb%9e%99%ed%95%91%ed%81%ac-%ec%a7%80%ec%88%98-%eb%ae%a4%eb%b9%84-%ec%86%8d-%ea%b7%b8-%ec%98%b7-%ec%96%b4%eb%94%94-%ea%b1%b0/?ddw=140884&ds_ch=twitter&utm_source=twitter&utm_medium=SNS",
+					"display_url": "wkorea.com/2023/04/04/%eb…",
+					"indices": [
+						82,
+						105
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 78262636,
+			"id_str": "78262636",
+			"name": "W Korea",
+			"screen_name": "wkorea",
+			"location": "",
+			"description": "No.1 Conceptual Trend Magazine 'W KOREA'의 공식 트위터입니다.",
+			"url": "https://t.co/3pmvl5gwu0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3pmvl5gwu0",
+							"expanded_url": "http://www.wkorea.com",
+							"display_url": "wkorea.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 335384,
+			"friends_count": 0,
+			"listed_count": 870,
+			"created_at": "Tue Sep 29 09:21:19 +0000 2009",
+			"favourites_count": 7324,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14931,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "DD2E44",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227868024499097600/goQ9_G6Q_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/78262636/1679293637",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "333333",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 24,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "이쯤 되니 궁금해지는 바로 그것, 지수가 뮤비에서 입은 그 옷 어디 건데?\n#지수 #꽃 #JISOO #JISOO_FLOWER #BlackPink \nhttps://t.co/Ebw93CSKKQ"
+	},
+	{
 		"created_at": "Tue Apr 04 04:29:52 +0000 2023",
 		"id": 1643101085421256700,
 		"id_str": "1643108599105724418",
