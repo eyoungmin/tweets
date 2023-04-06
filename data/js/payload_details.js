1c56b5760268c9ed15d9bb1682bb886f0decc754
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-06 15:02:01 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93388,
-	"created_at": "Thu Apr 06 05:02:01 +0000 2023",
+	"tweets": 93389,
+	"created_at": "Thu Apr 06 06:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,248 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Thu Apr 06 05:30:25 +0000 2023",
+		"id": 1643841536587866000,
+		"id_str": "1643848615658438657",
+		"full_text": "HEEYY BLINKS!! Create a Short and let us know how you’re getting ready for our set, brought to you by #YouTubeShorts ⬇ \n#CoachellaOnYouTube @YouTube\n\nhttps://t.co/L7S7FKSRoX https://t.co/7VAOHCTQ9e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "YouTubeShorts",
+					"indices": [
+						102,
+						116
+					]
+				},
+				{
+					"text": "CoachellaOnYouTube",
+					"indices": [
+						120,
+						139
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						140,
+						148
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/L7S7FKSRoX",
+					"expanded_url": "http://yt.be/blackpinkcoachella",
+					"display_url": "yt.be/blackpinkcoach…",
+					"indices": [
+						150,
+						173
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1643841469508354000,
+					"id_str": "1643841469508354048",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1643841469508354048/pu/img/msV_rInsSZz9G7-i.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1643841469508354048/pu/img/msV_rInsSZz9G7-i.jpg",
+					"url": "https://t.co/7VAOHCTQ9e",
+					"display_url": "pic.twitter.com/7VAOHCTQ9e",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1643841536587866112/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1643841469508354000,
+					"id_str": "1643841469508354048",
+					"indices": [
+						174,
+						197
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1643841469508354048/pu/img/msV_rInsSZz9G7-i.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1643841469508354048/pu/img/msV_rInsSZz9G7-i.jpg",
+					"url": "https://t.co/7VAOHCTQ9e",
+					"display_url": "pic.twitter.com/7VAOHCTQ9e",
+					"expanded_url": "https://twitter.com/BLACKPINK/status/1643841536587866112/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"duration_millis": 14786,
+						"variants": [
+							{
+								"bitrate": 1280000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1643841469508354048/pu/vid/720x720/Hi7o3Stz4-yojeCt.mp4?tag=12"
+							},
+							{
+								"bitrate": 432000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1643841469508354048/pu/vid/320x320/XzOzGEi2qcAEQe_U.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1643841469508354048/pu/pl/zsecOoitnt44p8-G.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1643841469508354048/pu/vid/540x540/i4TMIeXsf0U6t-Ra.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1273850774380884000,
+			"id_str": "1273850774380883975",
+			"name": "BLACKPINKOFFICIAL",
+			"screen_name": "BLACKPINK",
+			"location": "",
+			"description": "BLΛƆKPIИK",
+			"url": "https://t.co/vkfEFSP9At",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vkfEFSP9At",
+							"expanded_url": "https://lnk.to/YG_BLACKPINK",
+							"display_url": "lnk.to/YG_BLACKPINK",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8860672,
+			"friends_count": 0,
+			"listed_count": 8282,
+			"created_at": "Fri Jun 19 05:32:29 +0000 2020",
+			"favourites_count": 32,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 973,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1570625324676943872/YmJlrpVp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1273850774380883975/1680233566",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4216,
+		"favorite_count": 16979,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "HEEYY BLINKS!! Create a Short and let us know how you’re getting ready for our set, brought to you by #YouTubeShorts ⬇ \n#CoachellaOnYouTube @YouTube\n\nhttps://t.co/L7S7FKSRoX https://t.co/7VAOHCTQ9e"
+	},
+	{
 		"created_at": "Thu Apr 06 04:30:24 +0000 2023",
 		"id": 1643825863639662600,
 		"id_str": "1643833510589878272",
