2961ee9fbb9d90bb8d3ea7feeabad2e591fa6eba
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-07 21:02:02 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93409,
-	"created_at": "Fri Apr 07 11:02:02 +0000 2023",
+	"tweets": 93410,
+	"created_at": "Fri Apr 07 12:02:01 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,269 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Fri Apr 07 11:43:16 +0000 2023",
+		"id": 1644294270806007800,
+		"id_str": "1644304830997966848",
+		"full_text": "Arrivederci! JOY💚 I TOD’S 2023 F/W SHOW, Harper's BAZAAR\n\nhttps://t.co/nE5hk8Sf1Y\n\n#RedVelvet #레드벨벳 \n#JOY #조이 \n#TODS #토즈 \n#Harpersbazaar #하퍼스바자\n#MILANO #밀라노\n#TODS2023FWSHOW https://t.co/d2wrbqHkik",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			172
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "RedVelvet",
+					"indices": [
+						83,
+						93
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						94,
+						99
+					]
+				},
+				{
+					"text": "JOY",
+					"indices": [
+						101,
+						105
+					]
+				},
+				{
+					"text": "조이",
+					"indices": [
+						106,
+						109
+					]
+				},
+				{
+					"text": "TODS",
+					"indices": [
+						111,
+						116
+					]
+				},
+				{
+					"text": "토즈",
+					"indices": [
+						117,
+						120
+					]
+				},
+				{
+					"text": "Harpersbazaar",
+					"indices": [
+						122,
+						136
+					]
+				},
+				{
+					"text": "하퍼스바자",
+					"indices": [
+						137,
+						143
+					]
+				},
+				{
+					"text": "MILANO",
+					"indices": [
+						144,
+						151
+					]
+				},
+				{
+					"text": "밀라노",
+					"indices": [
+						152,
+						156
+					]
+				},
+				{
+					"text": "TODS2023FWSHOW",
+					"indices": [
+						157,
+						172
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nE5hk8Sf1Y",
+					"expanded_url": "https://youtu.be/uXN3l_EiZc4",
+					"display_url": "youtu.be/uXN3l_EiZc4",
+					"indices": [
+						58,
+						81
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1644294262216093700,
+					"id_str": "1644294262216093703",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FtG0622acAcxSZE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtG0622acAcxSZE.jpg",
+					"url": "https://t.co/d2wrbqHkik",
+					"display_url": "pic.twitter.com/d2wrbqHkik",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1644294270806007810/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1644294262216093700,
+					"id_str": "1644294262216093703",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/FtG0622acAcxSZE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtG0622acAcxSZE.jpg",
+					"url": "https://t.co/d2wrbqHkik",
+					"display_url": "pic.twitter.com/d2wrbqHkik",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1644294270806007810/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5907810,
+			"friends_count": 2,
+			"listed_count": 11780,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3401,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2316,
+		"favorite_count": 5415,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Arrivederci! JOY💚 I TOD’S 2023 F/W SHOW, Harper's BAZAAR\n\nhttps://t.co/nE5hk8Sf1Y\n\n#RedVelvet #레드벨벳 \n#JOY #조이 \n#TODS #토즈 \n#Harpersbazaar #하퍼스바자\n#MILANO #밀라노\n#TODS2023FWSHOW https://t.co/d2wrbqHkik"
+	},
+	{
 		"created_at": "Fri Apr 07 10:30:45 +0000 2023",
 		"id": 1644279793956028400,
 		"id_str": "1644286583422558209",
