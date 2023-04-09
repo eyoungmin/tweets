e73553697eb96b1da2acf71bb1487bedaf29d758
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-09 10:02:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93422,
-	"created_at": "Sat Apr 08 13:02:01 +0000 2023",
+	"tweets": 93423,
+	"created_at": "Sun Apr 09 01:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 09 00:43:36 +0000 2023",
+		"id": 1644852625148354600,
+		"id_str": "1644863598789816322",
+		"full_text": "오늘의 동물농장 MC 조이입니다🥰\n오늘도 동물 친구들과 함께하는\n히이이일링 되는 일요일 보내세요‼️\n\n#조이 #JOY\n#레드벨벳 #RedVelvet \n#TV동물농장 https://t.co/AqoTnROED2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "조이",
+					"indices": [
+						57,
+						60
+					]
+				},
+				{
+					"text": "JOY",
+					"indices": [
+						61,
+						65
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						66,
+						71
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						72,
+						82
+					]
+				},
+				{
+					"text": "TV동물농장",
+					"indices": [
+						84,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1644652409946538000,
+					"id_str": "1644652409946537984",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FtL6pzHagAAqQCQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtL6pzHagAAqQCQ.jpg",
+					"url": "https://t.co/AqoTnROED2",
+					"display_url": "pic.twitter.com/AqoTnROED2",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1644852625148354564/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1535,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1644652409946538000,
+					"id_str": "1644652409946537984",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FtL6pzHagAAqQCQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FtL6pzHagAAqQCQ.jpg",
+					"url": "https://t.co/AqoTnROED2",
+					"display_url": "pic.twitter.com/AqoTnROED2",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1644852625148354564/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1535,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5909317,
+			"friends_count": 2,
+			"listed_count": 11784,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3402,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1187,
+		"favorite_count": 4842,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오늘의 동물농장 MC 조이입니다🥰\n오늘도 동물 친구들과 함께하는\n히이이일링 되는 일요일 보내세요‼️\n\n#조이 #JOY\n#레드벨벳 #RedVelvet \n#TV동물농장 https://t.co/AqoTnROED2"
+	},
+	{
 		"created_at": "Sat Apr 08 12:31:08 +0000 2023",
 		"id": 1644671430557069300,
 		"id_str": "1644679266477416450",
