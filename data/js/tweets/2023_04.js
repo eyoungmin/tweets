bc9d4a6c1f00c6e7477ae41945aadb90f12cd169
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-09 16:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,173 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Sun Apr 09 06:31:16 +0000 2023",
+		"id": 1644945738009309200,
+		"id_str": "1644951089890877440",
+		"full_text": "#BLACKPINKINYOURAREA\n\n잠시 후 3시 40분 (KST),\nSBS #인기가요 에\n#지수 가 출연합니다⏰\n\n활짝 꽃피운 지수의 꽃향기 가득한 무대!\n블링크 모두 본. 방. 사. 수.🌹\n\n#JISOO #BLACKPINK #블랙핑크\n#FIRSTSINGLEALBUM #ME #꽃 #FLOWER",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BLACKPINKINYOURAREA",
+					"indices": [
+						0,
+						20
+					]
+				},
+				{
+					"text": "인기가요",
+					"indices": [
+						45,
+						50
+					]
+				},
+				{
+					"text": "지수",
+					"indices": [
+						53,
+						56
+					]
+				},
+				{
+					"text": "JISOO",
+					"indices": [
+						111,
+						117
+					]
+				},
+				{
+					"text": "BLACKPINK",
+					"indices": [
+						118,
+						128
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						129,
+						134
+					]
+				},
+				{
+					"text": "FIRSTSINGLEALBUM",
+					"indices": [
+						135,
+						152
+					]
+				},
+				{
+					"text": "ME",
+					"indices": [
+						153,
+						156
+					]
+				},
+				{
+					"text": "꽃",
+					"indices": [
+						157,
+						159
+					]
+				},
+				{
+					"text": "FLOWER",
+					"indices": [
+						160,
+						167
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 878204370403250200,
+			"id_str": "878204370403250176",
+			"name": "BLACKPINK GLOBAL BLINK",
+			"screen_name": "ygofficialblink",
+			"location": "",
+			"description": "YG ENTERTAINMENT #BLACKPINK OFFICIAL BLINK TWITTER l #블랙핑크 팬클럽 블링크 공식 트위터입니다.",
+			"url": "https://t.co/5OX1WxHQ4P",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5OX1WxHQ4P",
+							"expanded_url": "https://blackpink.lnk.to/BPSHOP",
+							"display_url": "blackpink.lnk.to/BPSHOP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8943761,
+			"friends_count": 10,
+			"listed_count": 9515,
+			"created_at": "Fri Jun 23 10:53:32 +0000 2017",
+			"favourites_count": 71,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 2772,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/878207363269836801/hYvfoMgn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/878204370403250176/1680235305",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1376,
+		"favorite_count": 3660,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "#BLACKPINKINYOURAREA\n\n잠시 후 3시 40분 (KST),\nSBS #인기가요 에\n#지수 가 출연합니다⏰\n\n활짝 꽃피운 지수의 꽃향기 가득한 무대!\n블링크 모두 본. 방. 사. 수.🌹\n\n#JISOO #BLACKPINK #블랙핑크\n#FIRSTSINGLEALBUM #ME #꽃 #FLOWER"
+	},
+	{
 		"created_at": "Sun Apr 09 00:43:36 +0000 2023",
 		"id": 1644852625148354600,
 		"id_str": "1644863598789816322",
