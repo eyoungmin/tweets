aa3a14a19c214821ae373f6ab55aa17c532e1ca4
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-18 15:02:04 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 93507,
-	"created_at": "Tue Apr 18 03:02:01 +0000 2023",
+	"tweets": 93508,
+	"created_at": "Tue Apr 18 06:02:02 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,238 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 18 05:58:12 +0000 2023",
+		"id": 1648191430983311400,
+		"id_str": "1648204258666242048",
+		"full_text": "제목 초성 게임 시이작!\n#심규선 #Lucia #newsinglerelease #comingsoon #제목 #초성게임 #4월24일 #6PM https://t.co/z8z5iRPqzk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "심규선",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"text": "Lucia",
+					"indices": [
+						19,
+						25
+					]
+				},
+				{
+					"text": "newsinglerelease",
+					"indices": [
+						26,
+						43
+					]
+				},
+				{
+					"text": "comingsoon",
+					"indices": [
+						44,
+						55
+					]
+				},
+				{
+					"text": "제목",
+					"indices": [
+						56,
+						59
+					]
+				},
+				{
+					"text": "초성게임",
+					"indices": [
+						60,
+						65
+					]
+				},
+				{
+					"text": "4월24일",
+					"indices": [
+						66,
+						72
+					]
+				},
+				{
+					"text": "6PM",
+					"indices": [
+						73,
+						77
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1648191428332507100,
+					"id_str": "1648191428332507136",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/Ft-NXx-aUAAjtFS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ft-NXx-aUAAjtFS.jpg",
+					"url": "https://t.co/z8z5iRPqzk",
+					"display_url": "pic.twitter.com/z8z5iRPqzk",
+					"expanded_url": "https://twitter.com/ROXIEEEEEEEE/status/1648191430983311360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1648191428332507100,
+					"id_str": "1648191428332507136",
+					"indices": [
+						78,
+						101
+					],
+					"media_url": "http://pbs.twimg.com/media/Ft-NXx-aUAAjtFS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ft-NXx-aUAAjtFS.jpg",
+					"url": "https://t.co/z8z5iRPqzk",
+					"display_url": "pic.twitter.com/z8z5iRPqzk",
+					"expanded_url": "https://twitter.com/ROXIEEEEEEEE/status/1648191430983311360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 183427484,
+			"id_str": "183427484",
+			"name": "심규선 Lucia",
+			"screen_name": "ROXIEEEEEEEE",
+			"location": "",
+			"description": "🌌2022 단독 콘서트 '밤의 정원'",
+			"url": "https://t.co/q8FfaeWXfm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/q8FfaeWXfm",
+							"expanded_url": "https://blog.naver.com/luciatune",
+							"display_url": "blog.naver.com/luciatune",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10872,
+			"friends_count": 0,
+			"listed_count": 162,
+			"created_at": "Thu Aug 26 23:56:49 +0000 2010",
+			"favourites_count": 486,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2570,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "D5EDEA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1527257182239981569/t6Uj3vt1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1527257182239981569/t6Uj3vt1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/183427484/1661404317",
+			"profile_link_color": "F59BB1",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 16,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "제목 초성 게임 시이작!\n#심규선 #Lucia #newsinglerelease #comingsoon #제목 #초성게임 #4월24일 #6PM https://t.co/z8z5iRPqzk"
+	},
+	{
 		"created_at": "Tue Apr 18 02:33:42 +0000 2023",
 		"id": 1648144316773302300,
 		"id_str": "1648152797420961836",
