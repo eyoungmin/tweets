a59549e1707bc6f0d38fb102babb99cb69f95175
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-18 21:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 18 11:45:55 +0000 2023",
+		"id": 1648281125134155800,
+		"id_str": "1648291765453987840",
+		"full_text": "완디 특유의 깜찍함과 함께합니다😘\n투데이 이즈 영스트리트❗️\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/xSGUj6PkxT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웬디",
+					"indices": [
+						35,
+						38
+					]
+				},
+				{
+					"text": "WENDY",
+					"indices": [
+						39,
+						45
+					]
+				},
+				{
+					"text": "완디",
+					"indices": [
+						46,
+						49
+					]
+				},
+				{
+					"text": "RedVelvet",
+					"indices": [
+						50,
+						60
+					]
+				},
+				{
+					"text": "레드벨벳",
+					"indices": [
+						61,
+						66
+					]
+				},
+				{
+					"text": "웬디의영스트리트",
+					"indices": [
+						68,
+						77
+					]
+				},
+				{
+					"text": "영스트리트",
+					"indices": [
+						78,
+						84
+					]
+				},
+				{
+					"text": "영스",
+					"indices": [
+						85,
+						88
+					]
+				},
+				{
+					"text": "SBS",
+					"indices": [
+						89,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1648281110231806000,
+					"id_str": "1648281110231805954",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/Ft_e79FacAI28M8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ft_e79FacAI28M8.jpg",
+					"url": "https://t.co/xSGUj6PkxT",
+					"display_url": "pic.twitter.com/xSGUj6PkxT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1648281125134155776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1648281110231806000,
+					"id_str": "1648281110231805954",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/Ft_e79FacAI28M8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ft_e79FacAI28M8.jpg",
+					"url": "https://t.co/xSGUj6PkxT",
+					"display_url": "pic.twitter.com/xSGUj6PkxT",
+					"expanded_url": "https://twitter.com/RVsmtown/status/1648281125134155776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 873092428755894300,
+			"id_str": "873092428755894272",
+			"name": "Red Velvet",
+			"screen_name": "RVsmtown",
+			"location": "",
+			"description": "Red Velvet 'The ReVe Festival 2022 - Birthday'",
+			"url": "https://t.co/p4ULiRKs5o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p4ULiRKs5o",
+							"expanded_url": "https://RedVelvet.lnk.to/Birthday",
+							"display_url": "RedVelvet.lnk.to/Birthday",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5918588,
+			"friends_count": 2,
+			"listed_count": 11812,
+			"created_at": "Fri Jun 09 08:20:31 +0000 2017",
+			"favourites_count": 130,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3421,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1589276270671503360/6do7s7aC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/873092428755894272/1667746812",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1676,
+		"favorite_count": 5049,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "완디 특유의 깜찍함과 함께합니다😘\n투데이 이즈 영스트리트❗️\n\n#웬디 #WENDY #완디\n#RedVelvet #레드벨벳 \n#웬디의영스트리트 #영스트리트\n#영스 #SBS https://t.co/xSGUj6PkxT"
+	},
+	{
 		"created_at": "Tue Apr 18 09:33:48 +0000 2023",
 		"id": 1648250371587047400,
 		"id_str": "1648258519668252673",
