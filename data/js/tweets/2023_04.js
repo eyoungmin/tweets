5769c0d896faa96a1e079504e01d92052a6e71ab
Youngmin Lee <iameyoungmin@gmail.com>
2023-04-18 18:02:03 +0900

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,170 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Tue Apr 18 08:23:50 +0000 2023",
+		"id": 1648227609908740000,
+		"id_str": "1648240908897271808",
+		"full_text": "16살 나이차?\n강동원❤️로제 열애설의 진실은?!\n\n#로제 #강동원 #블핑 #블랙핑크 #blackpink #rose #강동원로제 #로제강동원 \n\nhttps://t.co/e8KITkLrND",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "로제",
+					"indices": [
+						29,
+						32
+					]
+				},
+				{
+					"text": "강동원",
+					"indices": [
+						33,
+						37
+					]
+				},
+				{
+					"text": "블핑",
+					"indices": [
+						38,
+						41
+					]
+				},
+				{
+					"text": "블랙핑크",
+					"indices": [
+						42,
+						47
+					]
+				},
+				{
+					"text": "blackpink",
+					"indices": [
+						48,
+						58
+					]
+				},
+				{
+					"text": "rose",
+					"indices": [
+						59,
+						64
+					]
+				},
+				{
+					"text": "강동원로제",
+					"indices": [
+						65,
+						71
+					]
+				},
+				{
+					"text": "로제강동원",
+					"indices": [
+						72,
+						78
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e8KITkLrND",
+					"expanded_url": "https://www.cosmopolitan.co.kr/article/76748?utm_source=twitter&utm_medium=social&utm_campaign=article",
+					"display_url": "cosmopolitan.co.kr/article/76748?…",
+					"indices": [
+						81,
+						104
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 160470854,
+			"id_str": "160470854",
+			"name": "cosmokorea",
+			"screen_name": "cosmokorea",
+			"location": "Seoul, Korea",
+			"description": "코스모폴리탄 코리아의 공식 트위터! 트렌디한 패션&뷰티, 연애, 커리어 팁을 다루는 싱글걸의 라이프스타일 바이블. FUN, FEARLESS, FEMALE을 위한 월간지입니다. 신상제품, 핫플레이스, 다양한 문화정보를 실시간으로 전합니다!",
+			"url": "https://t.co/deL88qNq6V",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/deL88qNq6V",
+							"expanded_url": "http://www.cosmopolitan.co.kr",
+							"display_url": "cosmopolitan.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 92807,
+			"friends_count": 6,
+			"listed_count": 412,
+			"created_at": "Mon Jun 28 07:40:22 +0000 2010",
+			"favourites_count": 876,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 14422,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074467831008219137/wQ37PNvE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/160470854/1585707489",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FA0F36",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "16살 나이차?\n강동원❤️로제 열애설의 진실은?!\n\n#로제 #강동원 #블핑 #블랙핑크 #blackpink #rose #강동원로제 #로제강동원 \n\nhttps://t.co/e8KITkLrND"
+	},
+	{
 		"created_at": "Tue Apr 18 05:58:12 +0000 2023",
 		"id": 1648191430983311400,
 		"id_str": "1648204258666242048",
